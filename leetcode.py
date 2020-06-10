37c456662aa762e2d3350a6f6dd231c4fa4c5cd0
Ye Gao <ye_gao@mfe.berkeley.edu>
2020-06-10 00:29:56 -0400

    local change

--- leetcode.py
+++ leetcode.py
@@ -848,12 +848,12 @@ class Solution:
 	}"""
 
     def removeDuplicates(self, nums: List[int]) -> int:
-        k = 0
-        for i in range(len(nums)):
-            if i == 0 or nums[i-1] != nums[i]: 
-                nums[k] = nums[i]
-                k += 1
-        return k 
+        i = 0
+        for num in nums:
+            if i < 1 or nums[i-1] < num: 
+                nums[i] = num
+                i += 1
+        return i
 
 
     """27. Remove Element (Easy)
