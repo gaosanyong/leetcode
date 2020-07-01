6f7921a2895355a2156633033adc0b6418bcb5ab
Ye Gao <ye_gao@mfe.berkeley.edu>
2020-07-01 12:28:27 -0400

    131-135

--- leetcode.py
+++ leetcode.py
@@ -4484,3 +4484,219 @@ class Solution:
             for j in range(n):
                 if board[i][j] == "O": board[i][j] = "X"
                 if board[i][j] == "#": board[i][j] = "O"
+
+
+    """131. Palindrome Partitioning (Medium)
+	Given a string s, partition s such that every substring of the partition 
+	is a palindrome. Return all possible palindrome partitioning of s.
+
+	Example:
+	Input: "aab"
+	Output:
+	[
+	  ["aa","b"],
+	  ["a","a","b"]
+	]"""
+
+    def partition(self, s: str) -> List[List[str]]:
+        #pre-processing 
+        palin = dict()
+        for k in range(len(s)):
+            for i, j in (k, k), (k, k+1):
+                while 0 <= i and j < len(s) and s[i] == s[j]: 
+                    palin.setdefault(i, []).append(j)
+                    i, j = i-1, j+1
+        
+        @lru_cache(None)
+        def fn(i): 
+            """Return palindrome partitioning of s[i:]"""
+            if i == len(s): return [[]] 
+            return [[s[i:ii+1]] + y for ii in palin[i] for y in fn(ii+1)]
+        
+        return fn(0)
+
+
+    """132. Palindrome Partitioning II (Hard)
+	Given a string s, partition s such that every substring of the partition is 
+	a palindrome. Return the minimum cuts needed for a palindrome partitioning of s.
+
+	Example:
+	Input: "aab"
+	Output: 1
+	Explanation: The palindrome partitioning ["aa","b"] could be produced using 
+	1 cut."""
+
+    def minCut(self, s: str) -> int:
+        #pre-processing
+        palin = dict()
+        for k in range(len(s)):
+            for i, j in (k, k), (k, k+1):
+                while 0 <= i and j < len(s) and s[i] == s[j]: 
+                    palin.setdefault(i, []).append(j)
+                    i, j = i-1, j+1
+                
+        @lru_cache(None)
+        def fn(i):
+            """Return minimum palindrome partitioning of s[i:]"""
+            if i == len(s): return 0
+            return min(1 + fn(ii+1) for ii in palin[i])
+        
+        return fn(0)-1
+
+
+    """133. Clone Graph (Medium)
+	Given a reference of a node in a connected undirected graph. Return a deep 
+	copy (clone) of the graph. Each node in the graph contains a val (int) and 
+	a list (List[Node]) of its neighbors.
+
+	class Node {
+	    public int val;
+	    public List<Node> neighbors;
+	}
+	 
+	Test case format:
+	For simplicity sake, each node's value is the same as the node's index 
+	(1-indexed). For example, the first node with val = 1, the second node with 
+	val = 2, and so on. The graph is represented in the test case using an 
+	adjacency list. Adjacency list is a collection of unordered lists used to 
+	represent a finite graph. Each list describes the set of neighbors of a 
+	node in the graph. The given node will always be the first node with val = 1. 
+	You must return the copy of the given node as a reference to the cloned graph.
+
+	Example 1:
+	Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+	Output: [[2,4],[1,3],[2,4],[1,3]]
+	Explanation: There are 4 nodes in the graph.
+	1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+	2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+	3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+	4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+
+	Example 2:
+	Input: adjList = [[]]
+	Output: [[]]
+	Explanation: Note that the input contains one empty list. The graph consists 
+	of only one node with val = 1 and it does not have any neighbors.
+
+	Example 3:
+	Input: adjList = []
+	Output: []
+	Explanation: This an empty graph, it does not have any nodes.
+	
+	Example 4:
+	Input: adjList = [[2],[1]]
+	Output: [[2],[1]]
+
+	Constraints:
+	+ 1 <= Node.val <= 100
+	+ Node.val is unique for each node.
+	+ Number of Nodes will not exceed 100.
+	+ There is no repeated edges and no self-loops in the graph.
+	+ The Graph is connected and all nodes can be visited starting from the given node."""
+
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        memo = dict()
+        
+        def fn(n): 
+            """Return (deep) clonded node"""
+            if n not in memo: 
+                cln = memo[n] = Node(n.val)
+                cln.neighbors = [fn(nn) for nn in n.neighbors]
+            return memo[n]
+            
+        return node and fn(node)
+
+
+    """134. Gas Station (Medium)
+	There are N gas stations along a circular route, where the amount of gas at 
+	station i is gas[i]. You have a car with an unlimited gas tank and it costs 
+	cost[i] of gas to travel from station i to its next station (i+1). You 
+	begin the journey with an empty tank at one of the gas stations. Return the 
+	starting gas station's index if you can travel around the circuit once in 
+	the clockwise direction, otherwise return -1.
+
+	Note:
+	If there exists a solution, it is guaranteed to be unique.
+	Both input arrays are non-empty and have the same length.
+	Each element in the input arrays is a non-negative integer.
+
+	Example 1:
+	Input: 
+	gas  = [1,2,3,4,5]
+	cost = [3,4,5,1,2]
+
+	Output: 3
+
+	Explanation:
+	Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+	Travel to station 4. Your tank = 4 - 1 + 5 = 8
+	Travel to station 0. Your tank = 8 - 2 + 1 = 7
+	Travel to station 1. Your tank = 7 - 3 + 2 = 6
+	Travel to station 2. Your tank = 6 - 4 + 3 = 5
+	Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+	Therefore, return 3 as the starting index.
+
+	Example 2:
+	Input: 
+	gas  = [2,3,4]
+	cost = [3,4,3]
+
+	Output: -1
+
+	Explanation:
+	You can't start at station 0 or 1, as there is not enough gas to travel to the next station. 
+	Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+	Travel to station 0. Your tank = 4 - 3 + 2 = 3
+	Travel to station 1. Your tank = 3 - 3 + 3 = 3
+	You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+	Therefore, you can't travel around the circuit once no matter where you start."""
+
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        ans = prefix = lowest = 0
+        for n, (g, c) in enumerate(zip(gas, cost), 1): 
+            prefix += g - c
+            if prefix < lowest: 
+                lowest = prefix
+                ans = n
+        return -1 if prefix < 0 else ans%n
+
+
+    """135. Candy (Hard)
+	There are N children standing in a line. Each child is assigned a rating 
+	value. You are giving candies to these children subjected to the following 
+	requirements:
+	+ Each child must have at least one candy.
+	+ Children with a higher rating get more candies than their neighbors.
+	
+	What is the minimum candies you must give?
+
+	Example 1:
+	Input: [1,0,2]
+	Output: 5
+	Explanation: You can allocate to the first, second and third child with 2, 
+	1, 2 candies respectively.
+
+	Example 2:
+	Input: [1,2,2]
+	Output: 4
+	Explanation: You can allocate to the first, second and third child with 1, 
+	2, 1 candies respectively. The third child gets 1 candy because it 
+	satisfies the above two conditions."""
+
+    def candy(self, ratings: List[int]) -> int:
+        if not ratings: return 0 # edge case 
+        
+        ans = 1
+        down, up = 0, 1
+        for i in range(1, len(ratings)):
+            if ratings[i-1] < ratings[i]: 
+                if down: down, up = 0, 1 #reset
+                up += 1
+                ans += up
+            elif ratings[i-1] == ratings[i]: 
+                down, up = 0, 1 #reset 
+                ans += 1
+            else: 
+                down += 1
+                ans += down if down < up else down + 1
+        return ans 
