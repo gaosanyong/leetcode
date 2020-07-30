7a3eea6bc969160a2eaa91309c7b9d40ca1e4d3d
Ye Gao <ye_gao@mfe.berkeley.edu>
2020-07-30 18:05:03 -0400

    270

--- leetcode.py
+++ leetcode.py
@@ -7215,6 +7215,83 @@ class Solution:
         return ans 
 
 
+
+    """263. Ugly Number (Easy)
+	Write a program to check whether a given number is an ugly number. Ugly 
+	numbers are positive numbers whose prime factors only include 2, 3, 5.
+
+	Example 1:
+	Input: 6
+	Output: true
+	Explanation: 6 = 2 × 3
+
+	Example 2:
+	Input: 8
+	Output: true
+	Explanation: 8 = 2 × 2 × 2
+
+	Example 3:
+	Input: 14
+	Output: false 
+	Explanation: 14 is not ugly since it includes another prime factor 7.
+
+	Note:
+	* 1 is typically treated as an ugly number.
+	* Input is within the 32-bit signed integer range: [−231,  231 − 1]."""
+
+    def isUgly(self, num: int) -> bool:
+        if num <= 0: return False #edge case 
+        
+        for f in 2, 3, 5: 
+            while num % f == 0: 
+                num //= f
+        return num == 1
+
+
+    """264. Ugly Number II (Medium)
+	Write a program to find the n-th ugly number. Ugly numbers are positive 
+	numbers whose prime factors only include 2, 3, 5. 
+
+	Example:
+	Input: n = 10
+	Output: 12
+	Explanation: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 
+	             10 ugly numbers.
+
+	Note:  
+	* 1 is typically treated as an ugly number.
+	* n does not exceed 1690."""
+
+    def nthUglyNumber(self, n: int) -> int:
+        ans = [1]*n
+        p2 = p3 = p5 = 0
+        for i in range(1, n): 
+            ans[i] = min(2*ans[p2], 3*ans[p3], 5*ans[p5])
+            if 2*ans[p2] == ans[i]: p2 += 1
+            if 3*ans[p3] == ans[i]: p3 += 1
+            if 5*ans[p5] == ans[i]: p5 += 1
+        return ans[-1]
+
+
+    """268. Missing Number (Easy)
+	Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, 
+	find the one that is missing from the array.
+
+	Example 1:
+	Input: [3,0,1]
+	Output: 2
+
+	Example 2:
+	Input: [9,6,4,2,3,5,7,0,1]
+	Output: 8
+
+	Note: Your algorithm should run in linear runtime complexity. Could you 
+	implement it using only constant extra space complexity?"""
+
+    def missingNumber(self, nums: List[int]) -> int:
+        return reduce(xor, (i^x for i, x in enumerate(nums, 1)))
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
