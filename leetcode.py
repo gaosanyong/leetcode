cbc176929ecbd023bdf916b1ef347ba0500466e5
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-03-02 23:32:27 -0500

    weekly 159 (1232-1235)

--- leetcode.py
+++ leetcode.py
@@ -17288,6 +17288,162 @@ class Solution:
         return arr[0] + lo * chg
 
 
+    """1232. Check If It Is a Straight Line (Easy)
+	You are given an array coordinates, coordinates[i] = [x, y], where [x, y] 
+	represents the coordinate of a point. Check if these points make a straight 
+	line in the XY plane.
+
+	Example 1:
+	Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+	Output: true
+
+	Example 2:
+	Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+	Output: false
+
+	Constraints:
+	* 2 <= coordinates.length <= 1000
+	* coordinates[i].length == 2
+	* -10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+	* coordinates contains no duplicate point."""
+
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        x0, y0 = coordinates[0]
+        x1, y1 = coordinates[1]
+        for x, y in coordinates[2:]: 
+            if (y1-y0)*(x-x0) != (y-y0)*(x1-x0): return False 
+        return True 
+
+
+    """1233. Remove Sub-Folders from the Filesystem (Medium)
+	Given a list of folders, remove all sub-folders in those folders and return 
+	in any order the folders after removing. If a folder[i] is located within 
+	another folder[j], it is called a sub-folder of it. The format of a path is 
+	one or more concatenated strings of the form: / followed by one or more 
+	lowercase English letters. For example, /leetcode and /leetcode/problems 
+	are valid paths while an empty string and / are not.
+
+	Example 1:
+	Input: folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"]
+	Output: ["/a","/c/d","/c/f"]
+	Explanation: Folders "/a/b/" is a subfolder of "/a" and "/c/d/e" is inside 
+	             of folder "/c/d" in our filesystem.
+
+	Example 2:
+	Input: folder = ["/a","/a/b/c","/a/b/d"]
+	Output: ["/a"]
+	Explanation: Folders "/a/b/c" and "/a/b/d/" will be removed because they 
+	             are subfolders of "/a".
+	
+	Example 3:
+	Input: folder = ["/a/b/c","/a/b/ca","/a/b/d"]
+	Output: ["/a/b/c","/a/b/ca","/a/b/d"]
+
+	Constraints:
+	* 1 <= folder.length <= 4 * 10^4
+	* 2 <= folder[i].length <= 100
+	* folder[i] contains only lowercase letters and '/'
+	* folder[i] always starts with character '/'
+	* Each folder name is unique."""
+
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        ans = []
+        for x in sorted(folder): 
+            if not ans or not x.startswith(ans[-1]+"/"): 
+                ans.append(x)
+        return ans 
+
+
+    """1234. Replace the Substring for Balanced String (Medium)
+	You are given a string containing only 4 kinds of characters 'Q', 'W', 'E' 
+	and 'R'. A string is said to be balanced if each of its characters appears 
+	n/4 times where n is the length of the string. Return the minimum length of 
+	the substring that can be replaced with any other string of the same length 
+	to make the original string s balanced. Return 0 if the string is already 
+	balanced.
+
+	Example 1:
+	Input: s = "QWER"
+	Output: 0
+	Explanation: s is already balanced.
+
+	Example 2:
+	Input: s = "QQWE"
+	Output: 1
+	Explanation: We need to replace a 'Q' to 'R', so that "RQWE" (or "QRWE") is balanced.
+
+	Example 3:
+	Input: s = "QQQW"
+	Output: 2
+	Explanation: We can replace the first "QQ" to "ER". 
+
+	Example 4:
+	Input: s = "QQQQ"
+	Output: 3
+	Explanation: We can replace the last 3 'Q' to make s = "QWER".
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s.length is a multiple of 4
+	* s contains only 'Q', 'W', 'E' and 'R'."""
+
+    def balancedString(self, s: str) -> int:
+        freq = {}
+        for c in s: freq[c] = 1 + freq.get(c, 0)
+            
+        ans = len(s)
+        ii = 0
+        for i, c in enumerate(s): 
+            freq[c] -= 1
+            while ii < len(s) and all(freq[x] <= len(s)//4 for x in freq): 
+                ans = min(ans, i-ii+1)
+                freq[s[ii]] += 1
+                ii += 1
+        return ans 
+
+
+    """1235. Maximum Profit in Job Scheduling (Hard)
+	We have n jobs, where every job is scheduled to be done from startTime[i] 
+	to endTime[i], obtaining a profit of profit[i]. You're given the startTime, 
+	endTime and profit arrays, return the maximum profit you can take such that 
+	there are no two jobs in the subset with overlapping time range. If you 
+	choose a job that ends at time X you will be able to start another job that 
+	starts at time X.
+
+	Example 1:
+	Input: startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]
+	Output: 120
+	Explanation: The subset chosen is the first and fourth job. Time range 
+	             [1-3]+[3-6] , we get profit of 120 = 50 + 70.
+
+	Example 2:
+	Input: startTime = [1,2,3,4,6], endTime = [3,5,10,6,9], profit = [20,20,100,70,60]
+	Output: 150
+	Explanation: The subset chosen is the first, fourth and fifth job. Profit 
+	             obtained 150 = 20 + 70 + 60.
+	
+	Example 3:
+	Input: startTime = [1,1,1], endTime = [2,3,4], profit = [5,6,4]
+	Output: 6
+
+	Constraints:
+	* 1 <= startTime.length == endTime.length == profit.length <= 5 * 10^4
+	* 1 <= startTime[i] < endTime[i] <= 10^9
+	* 1 <= profit[i] <= 10^4"""
+
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        startTime, endTime, profit = zip(*sorted(zip(startTime, endTime, profit)))
+        
+        @cache
+        def fn(i): 
+            """Return max profit starting from index i."""
+            if i == len(startTime): return 0 
+            ii = bisect_left(startTime, endTime[i])
+            return max(fn(i+1), profit[i] + fn(ii))
+        
+        return fn(0)
+
+
     """1243. Array Transformation (Easy)
 	Given an initial array arr, every day you produce a new array using the 
 	array of the previous day. On the i-th day, you do the following operations 
