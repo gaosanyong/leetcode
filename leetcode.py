3532cf139a35b4581c2433fca449db92409eb089
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-03-16 21:03:36 -0400

    weekly 190 (1455-1458)

--- leetcode.py
+++ leetcode.py
@@ -18490,6 +18490,182 @@ class Solution:
         return str(max(0, fn(target)))
 
 
+    """1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence (Easy)
+	Given a sentence that consists of some words separated by a single space, 
+	and a searchWord. You have to check if searchWord is a prefix of any word 
+	in sentence. Return the index of the word in sentence where searchWord is a 
+	prefix of this word (1-indexed). If searchWord is a prefix of more than one 
+	word, return the index of the first word (minimum index). If there is no 
+	such word return -1. A prefix of a string S is any leading contiguous 
+	substring of S.
+
+	Example 1:
+	Input: sentence = "i love eating burger", searchWord = "burg"
+	Output: 4
+	Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+
+	Example 2:
+	Input: sentence = "this problem is an easy problem", searchWord = "pro"
+	Output: 2
+	Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word 
+	             in the sentence, but we return 2 as it's the minimal index.
+	
+	Example 3:
+	Input: sentence = "i am tired", searchWord = "you"
+	Output: -1
+	Explanation: "you" is not a prefix of any word in the sentence.
+	
+	Example 4:
+	Input: sentence = "i use triple pillow", searchWord = "pill"
+	Output: 4
+
+	Example 5:
+	Input: sentence = "hello from the other side", searchWord = "they"
+	Output: -1
+
+	Constraints:
+	* 1 <= sentence.length <= 100
+	* 1 <= searchWord.length <= 10
+	* sentence consists of lowercase English letters and spaces.
+	* searchWord consists of lowercase English letters."""
+
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        for i, word in enumerate(sentence.split()):
+            if word.startswith(searchWord): return i+1
+        return -1 
+
+
+    """1456. Maximum Number of Vowels in a Substring of Given Length (Medium)
+	Given a string s and an integer k. Return the maximum number of vowel 
+	letters in any substring of s with length k. Vowel letters in English are 
+	(a, e, i, o, u).
+
+	Example 1:
+	Input: s = "abciiidef", k = 3
+	Output: 3
+	Explanation: The substring "iii" contains 3 vowel letters.
+
+	Example 2:
+	Input: s = "aeiou", k = 2
+	Output: 2
+	Explanation: Any substring of length 2 contains 2 vowels.
+
+	Example 3:
+	Input: s = "leetcode", k = 3
+	Output: 2
+	Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+	Example 4:
+	Input: s = "rhythms", k = 4
+	Output: 0
+	Explanation: We can see that s doesn't have any vowel letters.
+
+	Example 5:
+	Input: s = "tryhard", k = 4
+	Output: 1
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s consists of lowercase English letters.
+	* 1 <= k <= s.length"""
+
+    def maxVowels(self, s: str, k: int) -> int:
+        ans = cnt = 0
+        for i in range(len(s)): 
+            if s[i] in "aeiou": cnt += 1
+            if i >= k and s[i-k] in "aeiou": cnt -= 1
+            ans = max(ans, cnt)
+        return ans 
+
+
+    """1457. Pseudo-Palindromic Paths in a Binary Tree (Medium)
+	Given a binary tree where node values are digits from 1 to 9. A path in the 
+	binary tree is said to be pseudo-palindromic if at least one permutation of 
+	the node values in the path is a palindrome. Return the number of pseudo-
+	palindromic paths going from the root node to leaf nodes.
+
+	Example 1:
+	Input: root = [2,3,1,3,1,null,1]
+	Output: 2 
+	Explanation: The figure above represents the given binary tree. There are 
+	             three paths going from the root node to leaf nodes: the red 
+	             path [2,3,3], the green path [2,1,1], and the path [2,3,1]. 
+	             Among these paths only red path and green path are pseudo-
+	             palindromic paths since the red path [2,3,3] can be rearranged 
+	             in [3,2,3] (palindrome) and the green path [2,1,1] can be 
+	             rearranged in [1,2,1] (palindrome).
+
+	Example 2:
+	Input: root = [2,1,1,1,3,null,null,null,null,null,1]
+	Output: 1 
+	Explanation: The figure above represents the given binary tree. There are 
+	             three paths going from the root node to leaf nodes: the green 
+	             path [2,1,1], the path [2,1,3,1], and the path [2,1]. Among 
+	             these paths only the green path is pseudo-palindromic since 
+	             [2,1,1] can be rearranged in [1,2,1] (palindrome).
+	
+	Example 3:
+	Input: root = [9]
+	Output: 1
+
+	Constraints:
+	* The number of nodes in the tree is in the range [1, 10^5].
+	* 1 <= Node.val <= 9"""
+
+    def pseudoPalindromicPaths (self, root: TreeNode) -> int:
+        
+        def fn(node, mask): 
+            """Post-order traverse the tree and update ans."""
+            if not node: return 0 
+            mask ^= 1 << node.val
+            if not node.left and not node.right: # leaf node 
+                return 1 if mask & (mask-1) == 0 else 0
+            return fn(node.left, mask) + fn(node.right, mask)
+        
+        return fn(root, 0)
+
+
+    """1458. Max Dot Product of Two Subsequences (Hard)
+	Given two arrays nums1 and nums2. Return the maximum dot product between 
+	non-empty subsequences of nums1 and nums2 with the same length. A 
+	subsequence of a array is a new array which is formed from the original 
+	array by deleting some (can be none) of the characters without disturbing 
+	the relative positions of the remaining characters. (ie, [2,3,5] is a 
+	subsequence of [1,2,3,4,5] while [1,5,3] is not).
+
+	Example 1:
+	Input: nums1 = [2,1,-2,5], nums2 = [3,0,-6]
+	Output: 18
+	Explanation: Take subsequence [2,-2] from nums1 and subsequence [3,-6] from 
+	             nums2. Their dot product is (2*3 + (-2)*(-6)) = 18.
+
+	Example 2:
+	Input: nums1 = [3,-2], nums2 = [2,-6,7]
+	Output: 21
+	Explanation: Take subsequence [3] from nums1 and subsequence [7] from nums2.
+	             Their dot product is (3*7) = 21.
+	
+	Example 3:
+	Input: nums1 = [-1,-1], nums2 = [1,1]
+	Output: -1
+ 	Explanation: Take subsequence [-1] from nums1 and subsequence [1] from nums2.
+ 	             Their dot product is -1.
+
+	Constraints:
+	* 1 <= nums1.length, nums2.length <= 500
+	* -1000 <= nums1[i], nums2[i] <= 1000"""
+
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        
+        @cache
+        def fn(i, j): 
+            """Return max dot product of nums1[i:] and nums2[j:]."""
+            if i == len(nums1) or j == len(nums2): return -inf
+            return max(nums1[i]*nums2[j] + fn(i+1, j+1), nums1[i]*nums2[j], fn(i+1, j), fn(i, j+1))
+        
+        return fn(0, 0)
+
+
     """1469. Find All The Lonely Nodes (Easy)
 	In a binary tree, a lonely node is a node that is the only child of its 
 	parent node. The root of the tree is not lonely because it does not have a 
