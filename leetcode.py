74c2f6e8476ad7db23504b2808b15c0f918ba25e
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-03-23 12:51:51 -0400

    biweekly 24 (1413-1416)

--- leetcode.py
+++ leetcode.py
@@ -18437,6 +18437,199 @@ class Solution:
         return "Bob"
 
 
+    """1413. Minimum Value to Get Positive Step by Step Sum (Easy)
+	Given an array of integers nums, you start with an initial positive value 
+	startValue. In each iteration, you calculate the step by step sum of 
+	startValue plus elements in nums (from left to right). Return the minimum 
+	positive value of startValue such that the step by step sum is never less 
+	than 1.
+
+	Example 1:
+	Input: nums = [-3,2,-3,4,2]
+	Output: 5
+	Explanation: If you choose startValue = 4, in the third iteration your step 
+	             by step sum is less than 1.
+                 step by step sum
+                 startValue = 4 | startValue = 5 | nums
+                   (4 -3 ) = 1  | (5 -3 ) = 2    |  -3
+                   (1 +2 ) = 3  | (2 +2 ) = 4    |   2
+                   (3 -3 ) = 0  | (4 -3 ) = 1    |  -3
+                   (0 +4 ) = 4  | (1 +4 ) = 5    |   4
+                   (4 +2 ) = 6  | (5 +2 ) = 7    |   2
+
+	Example 2:
+	Input: nums = [1,2]
+	Output: 1
+	Explanation: Minimum start value should be positive. 
+	
+	Example 3:
+	Input: nums = [1,-2,-3]
+	Output: 5
+
+	Constraints:
+	* 1 <= nums.length <= 100
+	* -100 <= nums[i] <= 100"""
+
+    def minStartValue(self, nums: List[int]) -> int:
+        ans = prefix = 0
+        for x in nums: 
+            prefix += x
+            ans = min(ans, prefix)
+        return 1 - ans
+
+
+    """1414. Find the Minimum Number of Fibonacci Numbers Whose Sum Is K (Medium)
+	Given an integer k, return the minimum number of Fibonacci numbers whose 
+	sum is equal to k. The same Fibonacci number can be used multiple times. 
+	The Fibonacci numbers are defined as:
+	F1 = 1
+	F2 = 1
+	Fn = Fn-1 + Fn-2 for n > 2.
+	It is guaranteed that for the given constraints we can always find such 
+	Fibonacci numbers that sum up to k.
+
+	Example 1:
+	Input: k = 7
+	Output: 2 
+	Explanation: The Fibonacci numbers are: 1, 1, 2, 3, 5, 8, 13, ... For k = 7 
+	             we can use 2 + 5 = 7.
+
+	Example 2:
+	Input: k = 10
+	Output: 2 
+	Explanation: For k = 10 we can use 2 + 8 = 10.
+	
+	Example 3:
+	Input: k = 19
+	Output: 3 
+	Explanation: For k = 19 we can use 1 + 5 + 13 = 19.
+
+	Constraints: 1 <= k <= 10^9"""
+
+    def findMinFibonacciNumbers(self, k: int) -> int:
+        fibo = [1]
+        f0 = f1 = 1
+        while f1 < k: 
+            f0, f1 = f1, f0+f1
+            fibo.append(f1)
+        
+        ans = 0
+        while k: 
+            ans += 1
+            i = bisect_right(fibo, k) - 1
+            k -= fibo[i]
+        return ans 
+
+
+    """1415. The k-th Lexicographical String of All Happy Strings of Length n (Medium)
+	A happy string is a string that:
+	* consists only of letters of the set ['a', 'b', 'c'].
+	* s[i] != s[i + 1] for all values of i from 1 to s.length - 1 (string is 1-indexed).
+	For example, strings "abc", "ac", "b" and "abcbabcbcb" are all happy strings 
+	and strings "aa", "baa" and "ababbc" are not happy strings. Given two 
+	integers n and k, consider a list of all happy strings of length n sorted 
+	in lexicographical order. Return the kth string of this list or return an 
+	empty string if there are less than k happy strings of length n.
+
+	Example 1:
+	Input: n = 1, k = 3
+	Output: "c"
+	Explanation: The list ["a", "b", "c"] contains all happy strings of length 
+	             1. The third string is "c".
+
+	Example 2:
+	Input: n = 1, k = 4
+	Output: ""
+	Explanation: There are only 3 happy strings of length 1.
+	
+	Example 3:
+	Input: n = 3, k = 9
+	Output: "cab"
+	Explanation: There are 12 different happy string of length 3 
+	             ["aba", "abc", "aca", "acb", "bab", "bac", 
+	              "bca", "bcb", "cab", "cac", "cba", "cbc"]. 
+	              You will find the 9th string = "cab"
+
+	Example 4:
+	Input: n = 2, k = 7
+	Output: ""
+	
+	Example 5:
+	Input: n = 10, k = 100
+	Output: "abacbabacb"
+	 
+	Constraints:
+	* 1 <= n <= 10
+	* 1 <= k <= 100"""
+
+    def getHappyString(self, n: int, k: int) -> str:
+        k -= 1
+        if 3*2**(n-1) <= k: return "" # impossible
+        
+        mp = {"": "abc", "a": "bc", "b": "ac", "c": "ab"}
+        ans = [""]
+        for i in range(n): 
+            q, k = divmod(k, 2**(n-i-1))
+            ans.append(mp[ans[-1]][q])
+        return "".join(ans)
+
+
+    """1416. Restore The Array (Hard)
+	A program was supposed to print an array of integers. The program forgot to 
+	print whitespaces and the array is printed as a string of digits and all we 
+	know is that all integers in the array were in the range [1, k] and there 
+	are no leading zeros in the array. Given the string s and the integer k. 
+	There can be multiple ways to restore the array. Return the number of 
+	possible array that can be printed as a string s using the mentioned 
+	program. The number of ways could be very large so return it modulo 
+	10^9 + 7.
+
+	Example 1:
+	Input: s = "1000", k = 10000
+	Output: 1
+	Explanation: The only possible array is [1000]
+
+	Example 2:
+	Input: s = "1000", k = 10
+	Output: 0
+	Explanation: There cannot be an array that was printed this way and has all 
+	             integer >= 1 and <= 10.
+
+	Example 3:
+	Input: s = "1317", k = 2000
+	Output: 8
+	Explanation: Possible arrays are [1317],[131,7],[13,17],[1,317],[13,1,7],[1,31,7],[1,3,17],[1,3,1,7]
+	
+	Example 4:
+	Input: s = "2020", k = 30
+	Output: 1
+	Explanation: The only possible array is [20,20]. [2020] is invalid because 
+	             2020 > 30. [2,020] is ivalid because 020 contains leading zeros.
+	
+	Example 5:
+	Input: s = "1234567890", k = 90
+	Output: 34
+
+	Constraints:
+	* 1 <= s.length <= 10^5.
+	* s consists of only digits and doesn't contain leading zeros.
+	* 1 <= k <= 10^9."""
+
+    def numberOfArrays(self, s: str, k: int) -> int:
+        dp = [0]*(len(s)+1)
+        dp[-1] = sm = 1 
+        
+        ii = len(s)
+        for i in reversed(range(len(s))): 
+            if s[i] != "0": 
+                while ii - i - 1 > log10(k) or int(s[i:ii]) > k: 
+                    sm = (sm - dp[ii]) % 1_000_000_007
+                    ii -= 1
+                dp[i] = sm
+                sm = (sm + dp[i]) % 1_000_000_007
+        return dp[0]
+
+
     """1417. Reformat The String (Easy)
 	Given alphanumeric string s. (Alphanumeric string is a string consisting of 
 	lowercase English letters and digits). You have to find a permutation of 
