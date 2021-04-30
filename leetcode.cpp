f68941d3c68b5351a36aaf1a4bbf98b3c21b3c6b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-04-30 13:09:40 -0400

    April LeetCoding Challenge 2021 (970)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4200,6 +4200,45 @@ public:
     }
 
 
+    /*970. Powerful Integers (Medium)
+	Given three integers x, y, and bound, return a list of all the powerful 
+	integers that have a value less than or equal to bound. An integer is 
+	powerful if it can be represented as xi + yj for some integers i >= 0 and 
+	j >= 0. You may return the answer in any order. In your answer, each value 
+	should occur at most once.
+
+	Example 1:
+	Input: x = 2, y = 3, bound = 10
+	Output: [2,3,4,5,7,9,10]
+	Explanation: 2 = 20 + 30
+	             3 = 21 + 30
+	             4 = 20 + 31
+	             5 = 21 + 31
+	             7 = 22 + 31
+	             9 = 23 + 30
+	             10 = 20 + 32
+	
+	Example 2:
+	Input: x = 3, y = 5, bound = 15
+	Output: [2,4,6,8,10,14]
+
+	Constraints:
+	* 1 <= x, y <= 100
+	* 0 <= bound <= 10^6*/
+
+    vector<int> powerfulIntegers(int x, int y, int bound) {
+        unordered_set<int> ans; 
+        for (int xx = 1; xx <= bound; xx *= x) {
+            for (int yy = 1; xx + yy <= bound; yy *= y) {
+                ans.insert(xx + yy); 
+                if (y == 1) break; 
+            }
+            if (x == 1) break; 
+        }
+        return vector<int>(ans.begin(), ans.end()); 
+    }
+
+
     /*1074. Number of Submatrices That Sum to Target (Hard)
 	Given a matrix and a target, return the number of non-empty submatrices 
 	that sum to target. A submatrix x1, y1, x2, y2 is the set of all cells 
