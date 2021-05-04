9c9d1ccec9d02163047f693067d483785061645a
Ye Gao <gaoyescm@fb.com>
2021-05-04 11:32:24 -0400

    May LeetCoding Challenge 2021 (665)

--- leetcode.py
+++ leetcode.py
@@ -11947,6 +11947,37 @@ class Solution:
         return ans 
 
 
+    """665. Non-decreasing Array (Medium)
+	Given an array nums with n integers, your task is to check if it could 
+	become non-decreasing by modifying at most one element. We define an array 
+	is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) 
+	such that (0 <= i <= n - 2).
+
+	Example 1:
+	Input: nums = [4,2,3]
+	Output: true
+	Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+
+	Example 2:
+	Input: nums = [4,2,1]
+	Output: false
+	Explanation: You can't get a non-decreasing array by modify at most one element.
+
+	Constraints:
+	n == nums.length
+	1 <= n <= 10^4
+	-10^5 <= nums[i] <= 10^5"""
+
+    def checkPossibility(self, nums: List[int]) -> bool:
+        cnt = 0 
+        for i in range(1, len(nums)): 
+            if nums[i-1] > nums[i]: 
+                if (i-1 == 0 or nums[i-2] <= nums[i]) or (i+1 == len(nums) or nums[i-1] <= nums[i+1]): 
+                    cnt += 1
+                else: return False 
+        return cnt <= 1
+
+
     """667. Beautiful Arrangement II (Medium)
 	Given two integers n and k, you need to construct a list which contains n 
 	different positive integers ranging from 1 to n and obeys the following 
