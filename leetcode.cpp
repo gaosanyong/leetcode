a5fa57745643aa876fabd1a3cb7d8b26287190d7
Ye Gao <gaoyescm@fb.com>
2021-05-04 11:28:53 -0400

    May LeetCoding Challenge 2021 (665)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2072,6 +2072,41 @@ public:
     }
 
 
+    /*665. Non-decreasing Array (Medium)
+	Given an array nums with n integers, your task is to check if it could 
+	become non-decreasing by modifying at most one element. We define an array 
+	is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) 
+	such that (0 <= i <= n - 2).
+
+	Example 1:
+	Input: nums = [4,2,3]
+	Output: true
+	Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+
+	Example 2:
+	Input: nums = [4,2,1]
+	Output: false
+	Explanation: You can't get a non-decreasing array by modify at most one element.
+
+	Constraints:
+	* n == nums.length
+	* 1 <= n <= 10^4
+	* -10^5 <= nums[i] <= 10^5*/
+
+    bool checkPossibility(vector<int>& nums) {
+        int cnt = 0; 
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i-1] > nums[i]) {
+                if ((i==1 || nums[i-2] <= nums[i]) || (i+1 == nums.size() || nums[i-1] <= nums[i+1])) 
+                    ++cnt; 
+                else
+                    return false; 
+            }
+        }
+        return cnt <= 1; 
+    }
+
+
     /*680. Valid Palindrome II (Easy)
 	Given a non-empty string s, you may delete at most one character. Judge 
 	whether you can make it a palindrome.
