f913f9c753f92ff9d7666eedac1eb6a27e855e7a
Ye Gao <gaoyescm@fb.com>
2021-05-05 22:20:19 -0400

    May LeetCoding Challenge 2021 (45)

--- leetcode.cpp
+++ leetcode.cpp
@@ -488,6 +488,40 @@ public:
     }
 
 
+    /*45. Jump Game II (Medium)
+	Given an array of non-negative integers nums, you are initially positioned 
+	at the first index of the array. Each element in the array represents your 
+	maximum jump length at that position. Your goal is to reach the last index 
+	in the minimum number of jumps. You can assume that you can always reach 
+	the last index.
+
+	Example 1:
+	Input: nums = [2,3,1,1,4]
+	Output: 2
+	Explanation: The minimum number of jumps to reach the last index is 2. Jump 
+	             1 step from index 0 to 1, then 3 steps to the last index.
+	
+	Example 2:
+	Input: nums = [2,3,0,1,4]
+	Output: 2
+
+	Constraints:
+	* 1 <= nums.length <= 1000
+	* 0 <= nums[i] <= 10^5*/
+
+    int jump(vector<int>& nums) {
+        int ans = 0, prev = 0, curr = 0; 
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > prev) {
+                prev = curr; 
+                ++ans; 
+            }
+            curr = max(curr, i + nums[i]); 
+        }
+        return ans; 
+    }
+
+
     /*48. Rotate Image (Medium)
 	You are given an n x n 2D matrix representing an image, rotate the image by 
 	90 degrees (clockwise). You have to rotate the image in-place, which means 
