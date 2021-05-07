793c85162bfccf1b5f56bd0d951d9761a3d53b6f
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-05-07 10:21:15 -0400

    May LeetCoding Challenge 2021 (583)

--- leetcode.cpp
+++ leetcode.cpp
@@ -1951,6 +1951,42 @@ public:
     }
 
 
+    /*583. Delete Operation for Two Strings (Medium)
+	Given two strings word1 and word2, return the minimum number of steps 
+	required to make word1 and word2 the same. In one step, you can delete 
+	exactly one character in either string.
+
+	Example 1:
+	Input: word1 = "sea", word2 = "eat"
+	Output: 2
+	Explanation: You need one step to make "sea" to "ea" and another step to 
+	             make "eat" to "ea".
+
+	Example 2:
+	Input: word1 = "leetcode", word2 = "etco"
+	Output: 4
+
+	Constraints:
+	* 1 <= word1.length, word2.length <= 500
+	* word1 and word2 consist of only lowercase English letters.*/
+
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size(); 
+        vector<vector<int>> dp (m+1, vector<int>(n+1, 0)); 
+        for (int i = 0; i < m; ++i) dp[i][n] = m - i; 
+        for (int j = 0; j < n; ++j) dp[m][j] = n - j; 
+        for (int i = m-1; i >= 0; --i) {
+            for (int j = n-1; j >= 0; --j) {
+                if (word1[i] == word2[j]) 
+                    dp[i][j] = dp[i+1][j+1]; 
+                else
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1]); 
+            }
+        }
+        return dp[0][0]; 
+    }
+
+
     /*589. N-ary Tree Preorder Traversal (Easy)
 	Given the root of an n-ary tree, return the preorder traversal of its nodes' 
 	values. Nary-Tree input serialization is represented in their level order 
