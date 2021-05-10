d64f62cc66900f8a857941ed19ee63c4d0297083
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-05-09 23:42:21 -0400

    May LeetCoding Challenge 2021 (1354)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4487,6 +4487,62 @@ public:
     }
 
 
+    /*1354. Construct Target Array With Multiple Sums (Hard)
+	Given an array of integers target. From a starting array, A consisting of 
+	all 1's, you may perform the following procedure :
+	* let x be the sum of all elements currently in your array.
+	* choose index i, such that 0 <= i < target.size and set the value of A at 
+	  index i to x.
+	* You may repeat this procedure as many times as needed.
+	Return True if it is possible to construct the target array from A 
+	otherwise return False.
+
+	Example 1:
+	Input: target = [9,3,5]
+	Output: true
+	Explanation: Start with [1, 1, 1] 
+	[1, 1, 1], sum = 3 choose index 1
+	[1, 3, 1], sum = 5 choose index 2
+	[1, 3, 5], sum = 9 choose index 0
+	[9, 3, 5] Done
+
+	Example 2:
+	Input: target = [1,1,1,2]
+	Output: false
+	Explanation: Impossible to create target array from [1,1,1,1].
+
+	Example 3:
+	Input: target = [8,5]
+	Output: true
+
+	Constraints:
+	* N == target.length
+	* 1 <= target.length <= 5 * 10^4
+	* 1 <= target[i] <= 10^9*/
+
+    bool isPossible(vector<int>& target) {
+        if (target.size() == 1) return target[0] == 1; // edge case 
+        
+        long total = 0; 
+        priority_queue<int> pq; 
+        for (auto x : target) {
+            total += x; 
+            pq.push(x); 
+        }
+        
+        while (pq.top() > 1) {
+            int x = pq.top(); 
+            pq.pop(); 
+            total -= x; 
+            if (x <= total) return false; 
+            x = (x-1) % total + 1; 
+            total += x; 
+            pq.push(x); 
+        }
+        return true; 
+    }
+
+
     /*1642. Furthest Building You Can Reach (Medium)
 	You are given an integer array heights representing the heights of 
 	buildings, some bricks, and some ladders. You start your journey from 
