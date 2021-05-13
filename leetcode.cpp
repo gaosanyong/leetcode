a6f1e2f930b702f2a088b10846c5d38254e25083
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-05-13 12:51:17 -0400

    May LeetCoding Challenge 2021 (816)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2907,6 +2907,69 @@ public:
     }
 
 
+    /*816. Ambiguous Coordinates (Medium)
+	We had some 2-dimensional coordinates, like "(1, 3)" or "(2, 0.5)". Then, 
+	we removed all commas, decimal points, and spaces, and ended up with the 
+	string s. Return a list of strings representing all possibilities for what 
+	our original coordinates could have been. Our original representation never 
+	had extraneous zeroes, so we never started with numbers like "00", "0.0", 
+	"0.00", "1.0", "001", "00.01", or any other number that can be represented 
+	with less digits.  Also, a decimal point within a number never occurs 
+	without at least one digit occuring before it, so we never started with 
+	numbers like ".1". The final answer list can be returned in any order. Also 
+	note that all coordinates in the final answer have exactly one space 
+	between them (occurring after the comma.)
+
+	Example 1:
+	Input: s = "(123)"
+	Output: ["(1, 23)", "(12, 3)", "(1.2, 3)", "(1, 2.3)"]
+	
+	Example 2:
+	Input: s = "(00011)"
+	Output:  ["(0.001, 1)", "(0, 0.011)"]
+	Explanation: 0.0, 00, 0001 or 00.01 are not allowed.
+	
+	Example 3:
+	Input: s = "(0123)"
+	Output: ["(0, 123)", "(0, 12.3)", "(0, 1.23)", "(0.1, 23)", "(0.1, 2.3)", "(0.12, 3)"]
+	
+	Example 4:
+	Input: s = "(100)"
+	Output: [(10, 0)]
+	Explanation: 1.0 is not allowed.
+
+	Note:
+	* 4 <= s.length <= 12.
+	* s[0] = "(", s[s.length - 1] = ")", and the other elements in s are digits.*/
+
+    vector<string> ambiguousCoordinates(string s) {
+                
+        auto fn = [](string&& s) {
+            vector<string> ans; 
+            if (s.size() == 1) 
+                ans.push_back(s); 
+            else if (s.front() == '0' and s.back() == '0') ; 
+            else if (s.front() == '0') 
+                ans.push_back("0." + s.substr(1)); 
+            else if (s.back() == '0') 
+                ans.push_back(s); 
+            else {
+                for (int i = 1; i < s.size(); ++i)
+                    ans.push_back(s.substr(0, i) + "." + s.substr(i)); 
+                ans.push_back(s); 
+            }
+            return ans;
+        };
+        
+        vector<string> ans; 
+        for (int i = 2; i < s.size()-1; ++i) 
+            for (auto& x : fn(s.substr(1, i-1))) 
+                for (auto& y : fn(s.substr(i, s.size()-1-i))) 
+                    ans.push_back("(" + x + ", " + y + ")"); 
+        return ans; 
+    }
+
+
     /*819. Most Common Word (Easy)
 	Given a string paragraph and a string array of the banned words banned, 
 	return the most frequent word that is not banned. It is guaranteed there is 
