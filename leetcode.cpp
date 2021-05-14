8ed4c0badd56a8fa6f45317cc816699c474fdb62
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-05-14 11:50:38 -0400

    May LeetCoding Challenge 2021 (114)

--- leetcode.cpp
+++ leetcode.cpp
@@ -937,6 +937,45 @@ public:
     }
 
 
+    /*114. Flatten Binary Tree to Linked List (Medium)
+	Given the root of a binary tree, flatten the tree into a "linked list":
+	* The "linked list" should use the same TreeNode class where the right 
+	  child pointer points to the next node in the list and the left child 
+	  pointer is always null.
+	* The "linked list" should be in the same order as a pre-order traversal of 
+	  the binary tree.
+
+	Example 1:
+	Input: root = [1,2,5,3,4,null,6]
+	Output: [1,null,2,null,3,null,4,null,5,null,6]
+
+	Example 2:
+	Input: root = []
+	Output: []
+
+	Example 3:
+	Input: root = [0]
+	Output: [0]
+
+	Constraints:
+	* The number of nodes in the tree is in the range [0, 2000].
+	* -100 <= Node.val <= 100
+
+	Follow up: Can you flatten the tree in-place (with O(1) extra space)?*/
+
+    void flatten(TreeNode* root) {
+        
+        function<TreeNode*(TreeNode*, TreeNode*)> fn = [&fn](TreeNode* node, TreeNode* tail) {
+            if (!node) return tail; 
+            node->right = fn(node->left, fn(node->right, tail)); 
+            node->left = NULL; 
+            return node; 
+        };
+        
+        fn(root, NULL); 
+    }
+
+
     /*120. Triangle (Medium)
 	Given a triangle array, return the minimum path sum from top to bottom. For 
 	each step, you may move to an adjacent number of the row below. More 
