b28e2b36973d9068b7e36308b4e16c4bffd5b221
Ye Gao <gaoyescm@fb.com>
2021-05-18 11:46:47 -0400

    May LeetCoding Challenge 2021 (609)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2185,6 +2185,74 @@ public:
     }
 
 
+    /*609. Find Duplicate File in System (Medium)
+	Given a list paths of directory info, including the directory path, and all 
+	the files with contents in this directory, return all the duplicate files 
+	in the file system in terms of their paths. You may return the answer in 
+	any order. A group of duplicate files consists of at least two files that 
+	have the same content. A single directory info string in the input list has 
+	the following format:
+	* "root/d1/d2/.../dm f1.txt(f1_content) f2.txt(f2_content) ... fn.txt(fn_content)"
+
+	It means there are n files (f1.txt, f2.txt ... fn.txt) with content 
+	(f1_content, f2_content ... fn_content) respectively in the directory 
+	"root/d1/d2/.../dm". Note that n >= 1 and m >= 0. If m = 0, it means the 
+	directory is just the root directory. The output is a list of groups of 
+	duplicate file paths. For each group, it contains all the file paths of the 
+	files that have the same content. A file path is a string that has the 
+	following format:
+	*"directory_path/file_name.txt"
+
+	Example 1:
+	Input: paths = ["root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"]
+	Output: [["root/a/2.txt","root/c/d/4.txt","root/4.txt"],["root/a/1.txt","root/c/3.txt"]]
+
+	Example 2:
+	Input: paths = ["root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)"]
+	Output: [["root/a/2.txt","root/c/d/4.txt"],["root/a/1.txt","root/c/3.txt"]]
+
+	Constraints:
+	* 1 <= paths.length <= 2 * 104
+	* 1 <= paths[i].length <= 3000
+	* 1 <= sum(paths[i].length) <= 5 * 105
+	* paths[i] consist of English letters, digits, '/', '.', '(', ')', and ' '.
+	* You may assume no files or directories share the same name in the same directory.
+	* You may assume each given directory info represents a unique directory. A single blank space separates the directory path and file info.
+	 
+
+	Follow up:
+	* Imagine you are given a real file system, how will you search files? DFS or BFS?
+	* If the file content is very large (GB level), how will you modify your solution?
+	* If you can only read the file by 1kb each time, how will you modify your solution?
+	* What is the time complexity of your modified solution? What is the most time-consuming part and memory-consuming part of it? How to optimize?
+	* How to make sure the duplicated files you find are not false positive?*/
+
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        unordered_map<string, vector<string>> mp; 
+        
+        for (auto& path : paths) {
+            istringstream iss (path); 
+            string file; 
+            vector<string> files; 
+            while (iss >> file) 
+                files.push_back(file); 
+            
+            for (int i = 1; i < files.size(); ++i) {
+                int k = files[i].find("("); 
+                mp[files[i].substr(k)].push_back(files[0] + "/" + files[i].substr(0, k)); 
+            }
+        }
+        
+        vector<vector<string>> ans; 
+        for (auto x: mp) {
+            if (x.second.size() > 1) {
+                ans.push_back(x.second); 
+            }
+        }
+        return ans; 
+    }
+
+
     /*643. Maximum Average Subarray I (Easy)
 	Given an array consisting of n integers, find the contiguous subarray of 
 	given length k that has the maximum average value. And you need to output 
