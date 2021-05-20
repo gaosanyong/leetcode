4106edbacc12f5bdb4cacc25fbe03d709f772e61
Ye Gao <gaoyescm@fb.com>
2021-05-20 13:54:49 -0400

    May LeetCoding Challenge 2021 (630)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2325,6 +2325,58 @@ public:
     }
 
 
+    /*630. Course Schedule III (Hard)
+	There are n different online courses numbered from 1 to n. You are given an 
+	array courses where courses[i] = [durationi, lastDayi] indicate that the ith 
+	course should be taken continuously for durationi days and must be finished 
+	before or on lastDayi. You will start on the 1st day and you cannot take two 
+	or more courses simultaneously. Return the maximum number of courses that 
+	you can take.
+
+	Example 1:
+	Input: courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
+	Output: 3
+	Explanation: There are totally 4 courses, but you can take 3 courses at most:
+	             - First, take the 1st course, it costs 100 days so you will 
+	               finish it on the 100th day, and ready to take the next course 
+	               on the 101st day.
+	             - Second, take the 3rd course, it costs 1000 days so you will 
+	               finish it on the 1100th day, and ready to take the next 
+	               course on the 1101st day. 
+	             - Third, take the 2nd course, it costs 200 days so you will 
+	               finish it on the 1300th day. 
+	             - The 4th course cannot be taken now, since you will finish it 
+	               on the 3300th day, which exceeds the closed date.
+	
+	Example 2:
+	Input: courses = [[1,2]]
+	Output: 1
+
+	Example 3:
+	Input: courses = [[3,2],[4,3]]
+	Output: 0
+
+	Constraints:
+	* 1 <= courses.length <= 10^4
+	* 1 <= durationi, lastDayi <= 10^4*/
+
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(), [](auto& lhs, auto& rhs) { return lhs[1] < rhs[1]; }); 
+        
+        int prefix = 0; 
+        priority_queue<int> pq; 
+        for (auto& course : courses) {
+            prefix += course[0]; 
+            pq.push(course[0]); 
+            while (prefix > course[1]) {
+                prefix -= pq.top(); 
+                pq.pop(); 
+            }
+        }
+        return pq.size(); 
+    }
+
+
     /*643. Maximum Average Subarray I (Easy)
 	Given an array consisting of n integers, find the contiguous subarray of 
 	given length k that has the maximum average value. And you need to output 
--- leetcode.py
+++ leetcode.py
@@ -11811,6 +11811,51 @@ class Solution:
         return n <= 0
 
 
+    """630. Course Schedule III (Hard)
+	There are n different online courses numbered from 1 to n. You are given an 
+	array courses where courses[i] = [durationi, lastDayi] indicate that the ith 
+	course should be taken continuously for durationi days and must be finished 
+	before or on lastDayi. You will start on the 1st day and you cannot take two 
+	or more courses simultaneously. Return the maximum number of courses that 
+	you can take.
+
+	Example 1:
+	Input: courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
+	Output: 3
+	Explanation: There are totally 4 courses, but you can take 3 courses at most:
+	             - First, take the 1st course, it costs 100 days so you will 
+	               finish it on the 100th day, and ready to take the next course 
+	               on the 101st day.
+	             - Second, take the 3rd course, it costs 1000 days so you will 
+	               finish it on the 1100th day, and ready to take the next 
+	               course on the 1101st day. 
+	             - Third, take the 2nd course, it costs 200 days so you will 
+	               finish it on the 1300th day. 
+	             - The 4th course cannot be taken now, since you will finish it 
+	               on the 3300th day, which exceeds the closed date.
+	
+	Example 2:
+	Input: courses = [[1,2]]
+	Output: 1
+
+	Example 3:
+	Input: courses = [[3,2],[4,3]]
+	Output: 0
+
+	Constraints:
+	* 1 <= courses.length <= 10^4
+	* 1 <= durationi, lastDayi <= 10^4"""
+
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        prefix = 0 
+        pq = [] # max-heap 
+        for x, y in sorted(courses, key=lambda x: x[1]): 
+            prefix += x
+            heappush(pq, -x)
+            while prefix > y: prefix += heappop(pq)
+        return len(pq)
+
+
     """645. Set Mismatch (Easy)
 	You have a set of integers s, which originally contains all the numbers 
 	from 1 to n. Unfortunately, due to some error, one of the numbers in s got 
