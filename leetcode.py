b429447c62b761bb3a5eee16867c36677ca48043
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-05-26 00:31:28 -0400

    May LeetCoding Challenge 2021 (943)

--- leetcode.py
+++ leetcode.py
@@ -15312,6 +15312,64 @@ class UnionFind:
         return ans if ans < inf else 0
 
 
+    """943. Find the Shortest Superstring (Hard)
+	Given an array of strings words, return the smallest string that contains 
+	each string in words as a substring. If there are multiple valid strings of 
+	the smallest length, return any of them. You may assume that no string in 
+	words is a substring of another string in words.
+
+	Example 1:
+	Input: words = ["alex","loves","leetcode"]
+	Output: "alexlovesleetcode"
+	Explanation: All permutations of "alex","loves","leetcode" would also be 
+	             accepted.
+
+	Example 2:
+	Input: words = ["catg","ctaagt","gcta","ttca","atgcatc"]
+	Output: "gctaagttcatgcatc"
+
+	Constraints:
+	* 1 <= words.length <= 12
+	* 1 <= words[i].length <= 20
+	* words[i] consists of lowercase English letters.
+	* All the strings of words are unique."""
+
+    def shortestSuperstring(self, words: List[str]) -> str:
+        """Travelling sales person (TSP) O(n^2*2^n)"""
+        n = len(words)
+        graph = [[0]*n for _ in range(n)] # graph as adjacency matrix 
+        
+        for i in range(n):
+            for j in range(n): 
+                if i != j: 
+                    for k in range(len(words[j])): 
+                        if words[i].endswith(words[j][:k]): 
+                            graph[i][j] = len(words[j]) - k 
+                            
+        @cache
+        def fn(prev, mask): 
+            """Return length of shortest superstring & current choice of word."""
+            if mask == 0: return 0, None
+            vv, kk = inf, None
+            for k in range(n): 
+                if mask & 1<<k: 
+                    v, _ = fn(k, mask ^ 1<<k)
+                    offset = len(words[k]) if prev == -1 else graph[prev][k]
+                    if v + offset < vv: vv, kk = v + offset, k
+            return vv, kk
+        
+        ans = []
+        prev = -1 
+        mask = (1<<n) - 1
+        while mask: 
+            _, k = fn(prev, mask)
+            if ans: ans.append(words[k][-graph[prev][k]:])
+            else: ans.append(words[k])
+            prev = k
+            mask ^= 1<<k 
+        return "".join(ans)
+
+
     """945. Minimum Increment to Make Array Unique (Medium)
 	Given an array of integers A, a move consists of choosing any A[i], and 
 	incrementing it by 1. Return the least number of moves to make every value 
