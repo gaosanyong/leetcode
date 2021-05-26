416b3ae5ed60fc64e65556654d4372171e0ba03d
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-05-26 10:54:28 -0400

    May LeetCoding Challenge 2021 (1689)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5210,6 +5210,36 @@ public:
     }
 
 
+    /*1689. Partitioning Into Minimum Number Of Deci-Binary Numbers (Medium)
+	A decimal number is called deci-binary if each of its digits is either 0 or 
+	1 without any leading zeros. For example, 101 and 1100 are deci-binary, 
+	while 112 and 3001 are not. Given a string n that represents a positive 
+	decimal integer, return the minimum number of positive deci-binary numbers 
+	needed so that they sum up to n.
+
+	Example 1:
+	Input: n = "32"
+	Output: 3
+	Explanation: 10 + 11 + 11 = 32
+
+	Example 2:
+	Input: n = "82734"
+	Output: 8
+
+	Example 3:
+	Input: n = "27346209830709182346"
+	Output: 9
+
+	Constraints:
+	* 1 <= n.length <= 10^5
+	* n consists of only digits.
+	* n does not contain any leading zeros and represents a positive integer.*/
+
+    int minPartitions(string n) {
+        return *max_element(begin(n), end(n)) - '0'; 
+    }
+
+
     /*1816. Truncate Sentence (Easy)
 	A sentence is a list of words that are separated by a single space with no 
 	leading or trailing spaces. Each of the words consists of only uppercase 
