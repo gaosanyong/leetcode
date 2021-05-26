3561c227d072d1d9cf02af00855031fbb6895149
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-05-26 15:53:24 -0400

    May LeetCoding Challenge 2021 (943)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4709,6 +4709,75 @@ public:
     }
 
 
+    /*943. Find the Shortest Superstring (Hard)
+	Given an array of strings words, return the smallest string that contains 
+	each string in words as a substring. If there are multiple valid strings of 
+	the smallest length, return any of them. You may assume that no string in 
+	words is a substring of another string in words.
+
+	Example 1:
+	Input: words = ["alex","loves","leetcode"]
+	Output: "alexlovesleetcode"
+	Explanation: All permutations of "alex","loves","leetcode" would also be 
+	             accepted.
+
+	Example 2:
+	Input: words = ["catg","ctaagt","gcta","ttca","atgcatc"]
+	Output: "gctaagttcatgcatc"
+
+	Constraints:
+	* 1 <= words.length <= 12
+	* 1 <= words[i].length <= 20
+	* words[i] consists of lowercase English letters.
+	* All the strings of words are unique.*/
+
+    string shortestSuperstring(vector<string>& words) {
+        int n = size(words); 
+        
+        vector<vector<int>> ovlp (n, vector<int>(n)); 
+        for (int i = 0; i < n; ++i) 
+            for (int j = 0; j < n; ++j)
+                for (int k = 0; k < min(size(words[i]), size(words[j])); ++k)
+                    if (words[i].substr(size(words[i]) - k) == words[j].substr(0, k)) {
+                        ovlp[i][j] = k; 
+                    }
+        
+        vector<vector<int>> dp(1<<n, vector<int>(n)); // min length of mask ending at bit 
+        vector<vector<int>> parent(1<<n, vector<int>(n, -1)); 
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            for (int bit = 0; bit < n; ++bit) {
+                if (mask & 1<<bit) {
+                    int pmask = mask ^ (1<<bit); 
+                    if (pmask) {
+                        for (int i = 0; i < n; ++i) {
+                            if (pmask & 1<<i) {
+                                int val = dp[pmask][i] + size(words[bit]) - ovlp[i][bit]; 
+                                if (dp[mask][bit] == 0 || val < dp[mask][bit]) {
+                                    dp[mask][bit] = val; 
+                                    parent[mask][bit] = i; 
+                                }
+                            }
+                        }
+                    }
+                    else dp[mask][bit] = size(words[bit]); 
+                }
+            }
+        }
+        
+        auto it = min_element(begin(dp.back()), end(dp.back())); 
+        int mask = (1<<n) - 1, p = it - begin(dp.back()); 
+        string ans; 
+        while (mask) {
+            int pp = parent[mask][p]; 
+            if (pp == -1) ans = words[p] + ans; 
+            else ans = words[p].substr(ovlp[pp][p]) + ans; 
+            mask ^= 1 << p; 
+            p = pp; 
+        }
+        return ans; 
+    }
+
+
     /*944. Delete Columns to Make Sorted (Easy)
 	You are given an array of n strings strs, all of the same length. The 
 	strings can be arranged such that there is one on each line, making a grid. 
