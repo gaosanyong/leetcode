373f7413098cb80b82a75238ef99a33090a42e75
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-02 14:28:05 -0400

    1884 (egg dropping problem)

--- leetcode.cpp
+++ leetcode.cpp
@@ -8605,6 +8605,54 @@ public:
         }
         return -1; 
     }
+
+
+    /*1884. Egg Drop With 2 Eggs and N Floors (Medium)
+	You are given two identical eggs and you have access to a building with n 
+	floors labeled from 1 to n. You know that there exists a floor f where 
+	0 <= f <= n such that any egg dropped at a floor higher than f will break, 
+	and any egg dropped at or below floor f will not break. In each move, you 
+	may take an unbroken egg and drop it from any floor x (where 1 <= x <= n). 
+	If the egg breaks, you can no longer use it. However, if the egg does not 
+	break, you may reuse it in future moves. Return the minimum number of moves 
+	that you need to determine with certainty what the value of f is.
+
+	Example 1:
+	Input: n = 2
+	Output: 2
+	Explanation: We can drop the first egg from floor 1 and the second egg from floor 2.
+	             If the first egg breaks, we know that f = 0.
+	             If the second egg breaks but the first egg didn't, we know that f = 1.
+	             Otherwise, if both eggs survive, we know that f = 2.
+	
+	Example 2:
+	Input: n = 100
+	Output: 14
+	Explanation: One optimal strategy is:
+	- Drop the 1st egg at floor 9. If it breaks, we know f is between 0 and 8. 
+	  Drop the 2nd egg starting from floor 1 and going up one at a time to find 
+	  f within 7 more drops. Total drops is 1 + 7 = 8.
+	- If the 1st egg does not break, drop the 1st egg again at floor 22. If it 
+	  breaks, we know f is between 9 and 21. Drop the 2nd egg starting from 
+	  floor 10 and going up one at a time to find f within 12 more drops. Total 
+	  drops is 2 + 12 = 14.
+	- If the 1st egg does not break again, follow a similar process dropping 
+	  the 1st egg from floors 34, 45, 55, 64, 72, 79, 85, 90, 94, 97, 99, and 
+	  100.
+	Regardless of the outcome, it takes at most 14 drops to determine f.
+
+	Constraints: 1 <= n <= 1000*/
+
+    int twoEggDrop(int n) {
+        vector<vector<int>> dp(n+1, vector<int>(2, INT_MAX)); 
+        for (int i = 0; i <= n; ++i) dp[i][0] = i; 
+        dp[0][0] = dp[0][1] = 0; 
+        
+        for (int i = 1; i <= n; ++i) 
+            for (int ii = 1; ii <= i; ++ii) 
+                dp[i][1] = min(dp[i][1], 1 + max(dp[ii-1][0], dp[i-ii][1])); 
+        return dp[n][1]; 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -33375,6 +33375,57 @@ class Fenwick:
             if fn(len(dist)-1, k) + dist[-1] <= hoursBefore*speed: return k 
 
 
+    """1884. Egg Drop With 2 Eggs and N Floors (Medium)
+	You are given two identical eggs and you have access to a building with n 
+	floors labeled from 1 to n. You know that there exists a floor f where 
+	0 <= f <= n such that any egg dropped at a floor higher than f will break, 
+	and any egg dropped at or below floor f will not break. In each move, you 
+	may take an unbroken egg and drop it from any floor x (where 1 <= x <= n). 
+	If the egg breaks, you can no longer use it. However, if the egg does not 
+	break, you may reuse it in future moves. Return the minimum number of moves 
+	that you need to determine with certainty what the value of f is.
+
+	Example 1:
+	Input: n = 2
+	Output: 2
+	Explanation: We can drop the first egg from floor 1 and the second egg from floor 2.
+	             If the first egg breaks, we know that f = 0.
+	             If the second egg breaks but the first egg didn't, we know that f = 1.
+	             Otherwise, if both eggs survive, we know that f = 2.
+	
+	Example 2:
+	Input: n = 100
+	Output: 14
+	Explanation: One optimal strategy is:
+	- Drop the 1st egg at floor 9. If it breaks, we know f is between 0 and 8. 
+	  Drop the 2nd egg starting from floor 1 and going up one at a time to find 
+	  f within 7 more drops. Total drops is 1 + 7 = 8.
+	- If the 1st egg does not break, drop the 1st egg again at floor 22. If it 
+	  breaks, we know f is between 9 and 21. Drop the 2nd egg starting from 
+	  floor 10 and going up one at a time to find f within 12 more drops. Total 
+	  drops is 2 + 12 = 14.
+	- If the 1st egg does not break again, follow a similar process dropping 
+	  the 1st egg from floors 34, 45, 55, 64, 72, 79, 85, 90, 94, 97, 99, and 
+	  100.
+	Regardless of the outcome, it takes at most 14 drops to determine f.
+
+	Constraints: 1 <= n <= 1000"""
+
+    def twoEggDrop(self, n: int) -> int:
+        
+        @cache
+        def fn(n, k): 
+            """Return min moves for n floors and k eggs."""
+            if k == 1: return n 
+            if n == 0: return 0 
+            ans = inf 
+            for x in range(1, n+1): 
+                ans = min(ans, 1 + max(fn(x-1, k-1), fn(n-x, k)))
+            return ans 
+        
+        return fn(n, 2)
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
