51ec0eaac5f5adcfe49d8286ba24b32ec3f2d347
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-03 11:33:57 -0400

    June LeetCoding Challenge 2021 (1465)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5944,6 +5944,57 @@ public:
     }
 
 
+    /*1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts (Medium)
+	Given a rectangular cake with height h and width w, and two arrays of 
+	integers horizontalCuts and verticalCuts where horizontalCuts[i] is the 
+	distance from the top of the rectangular cake to the ith horizontal cut and 
+	similarly, verticalCuts[j] is the distance from the left of the rectangular 
+	cake to the jth vertical cut. Return the maximum area of a piece of cake 
+	after you cut at each horizontal and vertical position provided in the 
+	arrays horizontalCuts and verticalCuts. Since the answer can be a huge 
+	number, return this modulo 10^9 + 7.
+
+	Example 1:
+	Input: h = 5, w = 4, horizontalCuts = [1,2,4], verticalCuts = [1,3]
+	Output: 4 
+	Explanation: The figure above represents the given rectangular cake. Red 
+	             lines are the horizontal and vertical cuts. After you cut the 
+	             cake, the green piece of cake has the maximum area.
+	
+	Example 2:
+	Input: h = 5, w = 4, horizontalCuts = [3,1], verticalCuts = [1]
+	Output: 6
+	Explanation: The figure above represents the given rectangular cake. Red 
+	             lines are the horizontal and vertical cuts. After you cut the 
+	             cake, the green and yellow pieces of cake have the maximum 
+	             area.
+	
+	Example 3:
+	Input: h = 5, w = 4, horizontalCuts = [3], verticalCuts = [3]
+	Output: 9
+
+	Constraints:
+	* 2 <= h, w <= 10^9
+	* 1 <= horizontalCuts.length < min(h, 10^5)
+	* 1 <= verticalCuts.length < min(w, 10^5)
+	* 1 <= horizontalCuts[i] < h
+	* 1 <= verticalCuts[i] < w
+	* It is guaranteed that all elements in horizontalCuts are distinct.
+	* It is guaranteed that all elements in verticalCuts are distinct.*/
+
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        sort(begin(horizontalCuts), end(horizontalCuts));
+        sort(begin(verticalCuts), end(verticalCuts)); 
+        int hmx = max(horizontalCuts.front(), h - horizontalCuts.back()); 
+        for (int i = 1; i < size(horizontalCuts); ++i) 
+            hmx = max(hmx, horizontalCuts[i] - horizontalCuts[i-1]); 
+        int vmx = max(verticalCuts.front(), w - verticalCuts.back()); 
+        for (int i = 1; i < size(verticalCuts); ++i) 
+            vmx = max(vmx, verticalCuts[i] - verticalCuts[i-1]); 
+        return (long) hmx * vmx % 1'000'000'007; 
+    }
+
+
     /*1642. Furthest Building You Can Reach (Medium)
 	You are given an integer array heights representing the heights of 
 	buildings, some bricks, and some ladders. You start your journey from 
--- leetcode.py
+++ leetcode.py
@@ -22452,6 +22452,52 @@ class UnionFind:
         
         return fn(0, 0)
 
+
+    """1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts (Medium)
+	Given a rectangular cake with height h and width w, and two arrays of 
+	integers horizontalCuts and verticalCuts where horizontalCuts[i] is the 
+	distance from the top of the rectangular cake to the ith horizontal cut and 
+	similarly, verticalCuts[j] is the distance from the left of the rectangular 
+	cake to the jth vertical cut. Return the maximum area of a piece of cake 
+	after you cut at each horizontal and vertical position provided in the 
+	arrays horizontalCuts and verticalCuts. Since the answer can be a huge 
+	number, return this modulo 10^9 + 7.
+
+	Example 1:
+	Input: h = 5, w = 4, horizontalCuts = [1,2,4], verticalCuts = [1,3]
+	Output: 4 
+	Explanation: The figure above represents the given rectangular cake. Red 
+	             lines are the horizontal and vertical cuts. After you cut the 
+	             cake, the green piece of cake has the maximum area.
+	
+	Example 2:
+	Input: h = 5, w = 4, horizontalCuts = [3,1], verticalCuts = [1]
+	Output: 6
+	Explanation: The figure above represents the given rectangular cake. Red 
+	             lines are the horizontal and vertical cuts. After you cut the 
+	             cake, the green and yellow pieces of cake have the maximum 
+	             area.
+	
+	Example 3:
+	Input: h = 5, w = 4, horizontalCuts = [3], verticalCuts = [3]
+	Output: 9
+
+	Constraints:
+	* 2 <= h, w <= 10^9
+	* 1 <= horizontalCuts.length < min(h, 10^5)
+	* 1 <= verticalCuts.length < min(w, 10^5)
+	* 1 <= horizontalCuts[i] < h
+	* 1 <= verticalCuts[i] < w
+	* It is guaranteed that all elements in horizontalCuts are distinct.
+	* It is guaranteed that all elements in verticalCuts are distinct."""
+
+    def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+        horizontalCuts = [0] + sorted(horizontalCuts) + [h]
+        verticalCuts = [0] + sorted(verticalCuts) + [w]
+        m = max(horizontalCuts[i] - horizontalCuts[i-1] for i in range(1, len(horizontalCuts)))
+        n = max(verticalCuts[j] - verticalCuts[j-1] for j in range(1, len(verticalCuts)))
+        return m*n % 1_000_000_007
+
     
     """1467. Probability of a Two Boxes Having The Same Number of Distinct Balls (Hard)
 	Given 2n balls of k distinct colors. You will be given an integer array 
