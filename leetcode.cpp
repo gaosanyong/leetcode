17af45d6a073f73495a134a8c2880ebec87c2976
Ye Gao <gaoyescm@fb.com>
2021-06-07 11:27:34 -0400

    June LeetCoding Challenge 2021 (746)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3410,6 +3410,39 @@ public:
     }
 
 
+    /*746. Min Cost Climbing Stairs (Easy)
+	You are given an integer array cost where cost[i] is the cost of ith step 
+	on a staircase. Once you pay the cost, you can either climb one or two 
+	steps. You can either start from the step with index 0, or the step with 
+	index 1. Return the minimum cost to reach the top of the floor.
+
+	Example 1:
+	Input: cost = [10,15,20]
+	Output: 15
+	Explanation: Cheapest is: start on cost[1], pay that cost, and go to the 
+	             top.
+	
+	Example 2:
+	Input: cost = [1,100,1,1,1,100,1,1,100,1]
+	Output: 6
+	Explanation: Cheapest is: start on cost[0], and only step on 1s, skipping 
+	             cost[3].
+
+	Constraints:
+	* 2 <= cost.length <= 1000
+	* 0 <= cost[i] <= 999*/
+
+    int minCostClimbingStairs(vector<int>& cost) {
+        int f0 = 0, f1 = 0; 
+        for (int i = size(cost)-1; i >= 0; --i) {
+            int tmp = f0; 
+            f0 = cost[i] + min(f0, f1); 
+            f1 = tmp; 
+        }
+        return min(f0, f1); 
+    }
+
+
     /*762. Prime Number of Set Bits in Binary Representation (Easy)
 	Given two integers L and R, find the count of numbers in the range [L, R] 
 	(inclusive) having a prime number of set bits in their binary representation.
--- leetcode.py
+++ leetcode.py
@@ -12799,6 +12799,35 @@ class UnionFind:
         return fn(0)
 
 
+    """746. Min Cost Climbing Stairs (Easy)
+	You are given an integer array cost where cost[i] is the cost of ith step 
+	on a staircase. Once you pay the cost, you can either climb one or two 
+	steps. You can either start from the step with index 0, or the step with 
+	index 1. Return the minimum cost to reach the top of the floor.
+
+	Example 1:
+	Input: cost = [10,15,20]
+	Output: 15
+	Explanation: Cheapest is: start on cost[1], pay that cost, and go to the 
+	             top.
+	
+	Example 2:
+	Input: cost = [1,100,1,1,1,100,1,1,100,1]
+	Output: 6
+	Explanation: Cheapest is: start on cost[0], and only step on 1s, skipping 
+	             cost[3].
+
+	Constraints:
+	* 2 <= cost.length <= 1000
+	* 0 <= cost[i] <= 999"""
+
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        f0 = f1 = 0 
+        for c in reversed(cost): 
+            f0, f1 = c + min(f0, f1), f0
+        return min(f0, f1)
+
+
     """750. Number Of Corner Rectangles (Medium)
 	Given a grid where each entry is only 0 or 1, find the number of corner 
 	rectangles. A corner rectangle is 4 distinct 1s on the grid that form an 
