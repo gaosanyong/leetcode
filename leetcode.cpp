02f3130c5eabdf5736ca26e70291fad0ece50489
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-11 17:18:30 -0400

    Solve'em all (857)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4113,6 +4113,54 @@ public:
     }
 
 
+    /*857. Minimum Cost to Hire K Workers (Hard)
+	There are n workers. The i-th worker has a quality[i] and a minimum wage 
+	expectation wage[i]. Now we want to hire exactly k workers to form a paid 
+	group. When hiring a group of k workers, we must pay them according to the 
+	following rules:
+	* Every worker in the paid group should be paid in the ratio of their 
+	  quality compared to other workers in the paid group.
+	* Every worker in the paid group must be paid at least their minimum wage 
+	  expectation.
+	Return the least amount of money needed to form a paid group satisfying the 
+	above conditions.
+
+	Example 1:
+	Input: quality = [10,20,5], wage = [70,50,30], k = 2
+	Output: 105.00000
+	Explanation: We pay 70 to 0-th worker and 35 to 2-th worker.
+
+	Example 2:
+	Input: quality = [3,1,10,10,1], wage = [4,8,2,2,7], k = 3
+	Output: 30.66667
+	Explanation: We pay 4 to 0-th worker, 13.33333 to 2-th and 3-th workers 
+	             seperately. 
+
+	Note:
+	* 1 <= k <= n <= 10000, where n = quality.length = wage.length
+	* 1 <= quality[i] <= 10000
+	* 1 <= wage[i] <= 10000
+	* Answers within 10-5 of the correct answer will be considered correct.*/
+
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        vector<pair<int, int>> data; // zipped quality & wage 
+        for (int i = 0; i < size(quality); ++i) 
+            data.emplace_back(quality[i], wage[i]); 
+        
+        sort(begin(data), end(data), [](auto& lhs, auto& rhs) { return (double) lhs.second/lhs.first < (double) rhs.second/rhs.first; }); 
+        
+        double ans = DBL_MAX, rsm = 0; // range sum 
+        priority_queue<int> pq; // max-heap 
+        for (auto& [q, w] : data) {
+            rsm += q; 
+            pq.push(q); 
+            if (size(pq) > k) rsm -= pq.top(), pq.pop(); 
+            if (size(pq) == k) ans = min(ans, rsm * w/q); 
+        }
+        return ans; 
+    }
+
+
     /*859. Buddy Strings (Easy)
 	Given two strings a and b, return true if you can swap two letters in a so 
 	the result is equal to b, otherwise, return false. Swapping letters is 
--- leetcode.py
+++ leetcode.py
@@ -14748,6 +14748,46 @@ class UnionFind:
         return ans
 
 
+    """857. Minimum Cost to Hire K Workers (Hard)
+	There are n workers. The i-th worker has a quality[i] and a minimum wage 
+	expectation wage[i]. Now we want to hire exactly k workers to form a paid 
+	group. When hiring a group of k workers, we must pay them according to the 
+	following rules:
+	* Every worker in the paid group should be paid in the ratio of their 
+	  quality compared to other workers in the paid group.
+	* Every worker in the paid group must be paid at least their minimum wage 
+	  expectation.
+	Return the least amount of money needed to form a paid group satisfying the 
+	above conditions.
+
+	Example 1:
+	Input: quality = [10,20,5], wage = [70,50,30], k = 2
+	Output: 105.00000
+	Explanation: We pay 70 to 0-th worker and 35 to 2-th worker.
+
+	Example 2:
+	Input: quality = [3,1,10,10,1], wage = [4,8,2,2,7], k = 3
+	Output: 30.66667
+	Explanation: We pay 4 to 0-th worker, 13.33333 to 2-th and 3-th workers 
+	             seperately. 
+
+	Note:
+	* 1 <= k <= n <= 10000, where n = quality.length = wage.length
+	* 1 <= quality[i] <= 10000
+	* 1 <= wage[i] <= 10000
+	* Answers within 10-5 of the correct answer will be considered correct."""
+
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        ans, rsm = inf, 0
+        pq = [] # max-heap 
+        for q, w in sorted(zip(quality, wage), key=lambda x: x[1]/x[0]): 
+            rsm += q 
+            heappush(pq, -q)
+            if len(pq) > k: rsm += heappop(pq)
+            if len(pq) == k: ans = min(ans, rsm * w/q)
+        return ans 
+
+
     """865. Smallest Subtree with all the Deepest Nodes (Medium)
 	Given the root of a binary tree, the depth of each node is the shortest 
 	distance to the root. Return the smallest subtree such that it contains all 
