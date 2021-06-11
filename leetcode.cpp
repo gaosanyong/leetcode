bf9b57511019ffb351605f5e00524ae48f00a996
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-11 10:42:08 -0400

    June LeetCoding Challenge 2021 (1690)

--- leetcode.cpp
+++ leetcode.cpp
@@ -6370,6 +6370,53 @@ public:
     }
 
 
+    /*1690. Stone Game VII (Medium)
+	Alice and Bob take turns playing a game, with Alice starting first. There 
+	are n stones arranged in a row. On each player's turn, they can remove 
+	either the leftmost stone or the rightmost stone from the row and receive 
+	points equal to the sum of the remaining stones' values in the row. The 
+	winner is the one with the higher score when there are no stones left to 
+	remove. Bob found that he will always lose this game (poor Bob, he always 
+	loses), so he decided to minimize the score's difference. Alice's goal is 
+	to maximize the difference in the score. Given an array of integers stones 
+	where stones[i] represents the value of the ith stone from the left, return 
+	the difference in Alice and Bob's score if they both play optimally.
+
+	Example 1:
+	Input: stones = [5,3,1,4,2]
+	Output: 6
+	Explanation: 
+	- Alice removes 2 and gets 5 + 3 + 1 + 4 = 13 points. Alice = 13, Bob = 0, stones = [5,3,1,4].
+	- Bob removes 5 and gets 3 + 1 + 4 = 8 points. Alice = 13, Bob = 8, stones = [3,1,4].
+	- Alice removes 3 and gets 1 + 4 = 5 points. Alice = 18, Bob = 8, stones = [1,4].
+	- Bob removes 1 and gets 4 points. Alice = 18, Bob = 12, stones = [4].
+	- Alice removes 4 and gets 0 points. Alice = 18, Bob = 12, stones = [].
+	The score difference is 18 - 12 = 6.
+
+	Example 2:
+	Input: stones = [7,90,5,1,100,10,10,2]
+	Output: 122
+
+	Constraints:
+	* n == stones.length
+	* 2 <= n <= 1000
+	* 1 <= stones[i] <= 1000*/
+
+    int stoneGameVII(vector<int>& stones) {
+        vector<int> prefix = {0}; 
+        for (auto& x : stones) prefix.push_back(prefix.back() + x); 
+        
+        int n = size(stones); 
+        vector<vector<int>> dp(n, vector<int>(n, 0)); 
+        for (int i = n-1; i >= 0; --i) {
+            for (int j = i+1; j < n; ++j) {
+                dp[i][j] = max(prefix[j+1] - prefix[i+1] - dp[i+1][j], prefix[j] - prefix[i] - dp[i][j-1]); 
+            }
+        }
+        return dp[0][n-1]; 
+    }
+
+
     /*1695. Maximum Erasure Value (Medium)
 	You are given an array of positive integers nums and want to erase a 
 	subarray containing unique elements. The score you get by erasing the 
--- leetcode.py
+++ leetcode.py
@@ -27461,15 +27461,12 @@ class Fenwick:
     def stoneGameVII(self, stones: List[int]) -> int:
         prefix = [0]
         for x in stones: prefix.append(prefix[-1] + x)
-            
+        
         n = len(stones)
-        dp = [[0]*(n+1) for _ in range(n+1)]
-        
-        for lo in reversed(range(n+1)):
-            for hi in range(n+1): 
-                if lo < hi: 
-                    dp[lo][hi] = max(prefix[hi]-prefix[lo+1]-dp[lo+1][hi], prefix[hi-1]-prefix[lo]-dp[lo][hi-1])
-        
+        dp = [[0]*n for _ in range(n)]
+        for i in reversed(range(n)): 
+            for j in range(i+1, n): 
+                dp[i][j] = max(prefix[j+1] - prefix[i+1] - dp[i+1][j], prefix[j] - prefix[i] - dp[i][j-1])
         return dp[0][-1]
 
 
