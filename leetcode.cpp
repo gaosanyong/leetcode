63c7f911e761b96d592849c4f4d95e4bef235c2c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-11 11:48:02 -0400

    Solve'em all (553)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2228,6 +2228,56 @@ public:
     }
 
 
+    /*553. Optimal Division (Medium)
+	You are given an integer array nums. The adjacent integers in nums will 
+	perform the float division. For example, for nums = [2,3,4], we will 
+	evaluate the expression "2/3/4". However, you can add any number of 
+	parenthesis at any position to change the priority of operations. You want 
+	to add these parentheses such the value of the expression after the 
+	evaluation is maximum. Return the corresponding expression that has the 
+	maximum value in string format. Note: your expression should not contain 
+	redundant parenthesis.
+
+	Example 1:
+	Input: nums = [1000,100,10,2]
+	Output: "1000/(100/10/2)"
+	Explanation: 1000/(100/10/2) = 1000/((100/10)/2) = 200. However, the bold 
+	             parenthesis in "1000/((100/10)/2)" are redundant, since they 
+	             don't influence the operation priority. So you should return 
+	             "1000/(100/10/2)".
+	             Other cases:
+	             1000/(100/10)/2 = 50
+	             1000/(100/(10/2)) = 50
+	             1000/100/10/2 = 0.5
+	             1000/100/(10/2) = 2
+	
+	Example 2:
+	Input: nums = [2,3,4]
+	Output: "2/(3/4)"
+
+	Example 3:
+	Input: nums = [2]
+	Output: "2"
+
+	Constraints:
+	* 1 <= nums.length <= 10
+	* 2 <= nums[i] <= 1000
+	* There is only one optimal division for the given iput.*/
+
+    string optimalDivision(vector<int>& nums) {
+        string ans = to_string(nums[0]); 
+        if (size(nums) == 1) return ans;
+        
+        ans += "/"; 
+        if (size(nums) == 2) return ans + to_string(nums[1]); 
+        
+        ans += "(" + to_string(nums[1]); 
+        for (int i = 2; i < size(nums); ++i) 
+            ans += "/" + to_string(nums[i]); 
+        return ans + ")";
+    }
+
+
     /*554. Brick Wall (Medium)
 	There is a brick wall in front of you. The wall is rectangular and has 
 	several rows of bricks. The bricks have the same height but different width. 
