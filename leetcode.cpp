dcdc705ffc7d3bba871de7f67dfa53b470558c96
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-21 15:01:43 -0400

    June LeetCoding Challenge 2021 (629)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2848,6 +2848,43 @@ public:
     }
 
 
+    /*629. K Inverse Pairs Array (Hard)
+	For an integer array nums, an inverse pair is a pair of integers [i, j] 
+	where 0 <= i < j < nums.length and nums[i] > nums[j]. Given two integers n 
+	and k, return the number of different arrays consist of numbers from 1 to n 
+	such that there are exactly k inverse pairs. Since the answer can be huge, 
+	return it modulo 10^9 + 7.
+
+	Example 1:
+	Input: n = 3, k = 0
+	Output: 1
+	Explanation: Only the array [1,2,3] which consists of numbers from 1 to 3 
+	             has exactly 0 inverse pairs.
+	
+	Example 2:
+	Input: n = 3, k = 1
+	Output: 2
+	Explanation: The array [1,3,2] and [2,1,3] have exactly 1 inverse pair.
+
+	Constraints:
+	* 1 <= n <= 1000
+	* 0 <= k <= 1000*/
+
+    int kInversePairs(int n, int k) {
+        int const MOD = 1'000'000'007;
+        vector<vector<int>> dp(n+1, vector<int>(k+1)); 
+        dp[0][0] = 1; 
+        for (int i = 1; i <= n; ++i) {
+            dp[i][0] = 1; 
+            for (int j = 1; j <= k; ++j) {
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD; 
+                if (i <= j) dp[i][j] = (dp[i][j] - dp[i-1][j-i] + MOD) % MOD; 
+            }
+        }
+        return dp.back().back(); 
+    }
+
+
     /*630. Course Schedule III (Hard)
 	There are n different online courses numbered from 1 to n. You are given an 
 	array courses where courses[i] = [durationi, lastDayi] indicate that the ith 
--- leetcode.py
+++ leetcode.py
@@ -11942,6 +11942,40 @@ class Solution:
         return n <= 0
 
 
+    """629. K Inverse Pairs Array (Hard)
+	For an integer array nums, an inverse pair is a pair of integers [i, j] 
+	where 0 <= i < j < nums.length and nums[i] > nums[j]. Given two integers n 
+	and k, return the number of different arrays consist of numbers from 1 to n 
+	such that there are exactly k inverse pairs. Since the answer can be huge, 
+	return it modulo 10^9 + 7.
+
+	Example 1:
+	Input: n = 3, k = 0
+	Output: 1
+	Explanation: Only the array [1,2,3] which consists of numbers from 1 to 3 
+	             has exactly 0 inverse pairs.
+	
+	Example 2:
+	Input: n = 3, k = 1
+	Output: 2
+	Explanation: The array [1,3,2] and [2,1,3] have exactly 1 inverse pair.
+
+	Constraints:
+	* 1 <= n <= 1000
+	* 0 <= k <= 1000"""
+
+    def kInversePairs(self, n: int, k: int) -> int:
+        
+        @cache
+        def fn(n, k):
+            """Return number of ways for n numbers with k inverse pairs."""
+            if k == 0: return 1 
+            if n <= 0 or k < 0: return 0 
+            return fn(n-1, k) + fn(n, k-1) - fn(n-1, k-n)
+        
+        return fn(n, k) % 1_000_000_007
+
+
     """630. Course Schedule III (Hard)
 	There are n different online courses numbered from 1 to n. You are given an 
 	array courses where courses[i] = [durationi, lastDayi] indicate that the ith 
