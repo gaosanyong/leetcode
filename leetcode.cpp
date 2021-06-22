900c97d3e3efebf91903dacd14484c2cea5d2b70
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-22 12:49:36 -0400

    June LeetCoding Challenge 2021 (792)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3948,6 +3948,51 @@ public:
     }
 
 
+    /*792. Number of Matching Subsequences (Medium)
+	Given a string s and an array of strings words, return the number of 
+	words[i] that is a subsequence of s. A subsequence of a string is a new 
+	string generated from the original string with some characters (can be 
+	none) deleted without changing the relative order of the remaining 
+	characters. For example, "ace" is a subsequence of "abcde".
+
+	Example 1:
+	Input: s = "abcde", words = ["a","bb","acd","ace"]
+	Output: 3
+	Explanation: There are three strings in words that are a subsequence of s: 
+	             "a", "acd", "ace".
+	
+	Example 2:
+	Input: s = "dsahjpjauf", words = ["ahjpjau","ja","ahbwzgqnuk","tnmlanowax"]
+	Output: 2
+
+	Constraints:
+	* 1 <= s.length <= 5 * 10^4
+	* 1 <= words.length <= 5000
+	* 1 <= words[i].length <= 50
+	* s and words[i] consist of only lowercase English letters.*/
+
+    int numMatchingSubseq(string s, vector<string>& words) {
+        unordered_map<char, vector<int>> loc; 
+        for (int i = 0; i < s.size(); ++i) loc[s[i]].push_back(i); 
+        
+        int ans = 0; 
+        for (auto& word : words) {
+            int x = 0; 
+            bool found = true; 
+            for (auto& ch : word) {
+                auto it = lower_bound(loc[ch].begin(), loc[ch].end(), x); 
+                if (it == loc[ch].end()) {
+                    found = false;
+                    break; 
+                }
+                x = *it + 1;
+            }
+            if (found) ++ans; 
+        }
+        return ans; 
+    }
+
+
     /*795. Number of Subarrays with Bounded Maximum (Medium)
 	We are given an array nums of positive integers, and two positive integers 
 	left and right (left <= right). Return the number of (contiguous, non-empty) 
--- leetcode.py
+++ leetcode.py
@@ -13964,6 +13964,45 @@ class UnionFind:
         return "".join(sorted(T, key=lambda x: mp.get(x, 26)))
 
 
+    """792. Number of Matching Subsequences (Medium)
+	Given a string s and an array of strings words, return the number of 
+	words[i] that is a subsequence of s. A subsequence of a string is a new 
+	string generated from the original string with some characters (can be 
+	none) deleted without changing the relative order of the remaining 
+	characters. For example, "ace" is a subsequence of "abcde".
+
+	Example 1:
+	Input: s = "abcde", words = ["a","bb","acd","ace"]
+	Output: 3
+	Explanation: There are three strings in words that are a subsequence of s: 
+	             "a", "acd", "ace".
+	
+	Example 2:
+	Input: s = "dsahjpjauf", words = ["ahjpjau","ja","ahbwzgqnuk","tnmlanowax"]
+	Output: 2
+
+	Constraints:
+	* 1 <= s.length <= 5 * 10^4
+	* 1 <= words.length <= 5000
+	* 1 <= words[i].length <= 50
+	* s and words[i] consist of only lowercase English letters."""
+
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        loc = {}
+        for i, ch in enumerate(s):
+            loc.setdefault(ch, []).append(i)
+        
+        ans = 0
+        for word in words: 
+            x = 0
+            for ch in word: 
+                i = bisect_left(loc.get(ch, []), x)
+                if i == len(loc.get(ch, [])): break 
+                x = loc[ch][i] + 1
+            else: ans += 1
+        return ans 
+
+
     """795. Number of Subarrays with Bounded Maximum (Medium)
 	We are given an array A of positive integers, and two positive integers L 
 	and R (L <= R). Return the number of (contiguous, non-empty) subarrays such 
