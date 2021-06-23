ececcaaced7f6de60ee17b66a5372a828f4f373f
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-22 21:57:12 -0400

    Solve'em all (552)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2383,6 +2383,50 @@ public:
     }
 
 
+    /*552. Student Attendance Record II (Hard)
+	An attendance record for a student can be represented as a string where 
+	each character signifies whether the student was absent, late, or present 
+	on that day. The record only contains the following three characters:
+	* 'A': Absent.
+	* 'L': Late.
+	* 'P': Present.
+	Any student is eligible for an attendance award if they meet both of the 
+	following criteria:
+	* The student was absent ('A') for strictly fewer than 2 days total.
+	* The student was never late ('L') for 3 or more consecutive days.
+	Given an integer n, return the number of possible attendance records of 
+	length n that make a student eligible for an attendance award. The answer 
+	may be very large, so return it modulo 10^9 + 7.
+
+	Example 1:
+	Input: n = 2
+	Output: 8
+	Explanation: There are 8 records with length 2 that are eligible for an 
+	             award: "PP", "AP", "PA", "LP", "PL", "AL", "LA", "LL". Only 
+	             "AA" is not eligible because there are 2 absences (there need 
+	             to be fewer than 2).
+	
+	Example 2:
+	Input: n = 1
+	Output: 3
+
+	Example 3:
+	Input: n = 10101
+	Output: 183236316
+
+	Constraints: 1 <= n <= 10^5*/
+
+    int checkRecord(int n) {
+        vector<long> dp = {1, 2, 4}; 
+        for (int i = 3; i <= n; ++i) 
+            dp.push_back((dp[i-3] + dp[i-2] + dp[i-1]) % 1'000'000'007);
+        int ans = dp[n]; 
+        for (int i = 0; i < n; ++i) 
+            ans = (ans + dp[i] * dp[n-1-i]) % 1'000'000'007; 
+        return ans; 
+    }
+
+
     /*553. Optimal Division (Medium)
 	You are given an integer array nums. The adjacent integers in nums will 
 	perform the float division. For example, for nums = [2,3,4], we will 
--- leetcode.py
+++ leetcode.py
@@ -11631,6 +11631,49 @@ class Solution:
         return root 
  
 
+    """552. Student Attendance Record II (Hard)
+	An attendance record for a student can be represented as a string where 
+	each character signifies whether the student was absent, late, or present 
+	on that day. The record only contains the following three characters:
+	* 'A': Absent.
+	* 'L': Late.
+	* 'P': Present.
+	Any student is eligible for an attendance award if they meet both of the 
+	following criteria:
+	* The student was absent ('A') for strictly fewer than 2 days total.
+	* The student was never late ('L') for 3 or more consecutive days.
+	Given an integer n, return the number of possible attendance records of 
+	length n that make a student eligible for an attendance award. The answer 
+	may be very large, so return it modulo 10^9 + 7.
+
+	Example 1:
+	Input: n = 2
+	Output: 8
+	Explanation: There are 8 records with length 2 that are eligible for an 
+	             award: "PP", "AP", "PA", "LP", "PL", "AL", "LA", "LL". Only 
+	             "AA" is not eligible because there are 2 absences (there need 
+	             to be fewer than 2).
+	
+	Example 2:
+	Input: n = 1
+	Output: 3
+
+	Example 3:
+	Input: n = 10101
+	Output: 183236316
+
+	Constraints: 1 <= n <= 10^5"""
+
+    def checkRecord(self, n: int) -> int:
+        dp = [1, 2, 4]
+        for i in range(3, n+1): 
+            dp.append((dp[i-3] + dp[i-2] + dp[i-1]) % 1_000_000_007)
+        ans = dp[n] 
+        for i in range(n): 
+            ans = (ans + dp[i] * dp[n-1-i]) % 1_000_000_007
+        return ans
+
+
     """553. Optimal Division (Medium)
 	You are given an integer array nums. The adjacent integers in nums will 
 	perform the float division. For example, for nums = [2,3,4], we will 
