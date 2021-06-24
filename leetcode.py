19c898aef29b1ca84dfb6c9554347d1c165d2e9e
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-24 13:48:09 -0400

    Solve'em all (1040)

--- leetcode.py
+++ leetcode.py
@@ -17217,6 +17217,54 @@ class UnionFind:
         return root 
 
 
+    """1040. Moving Stones Until Consecutive II (Medium)
+	On an infinite number line, the position of the i-th stone is given by 
+	stones[i]. Call a stone an endpoint stone if it has the smallest or largest 
+	position. Each turn, you pick up an endpoint stone and move it to an 
+	unoccupied position so that it is no longer an endpoint stone. In particular, 
+	if the stones are at say, stones = [1,2,5], you cannot move the endpoint 
+	stone at position 5, since moving it to any position (such as 0, or 3) will 
+	still keep that stone as an endpoint stone. The game ends when you cannot 
+	make any more moves, ie. the stones are in consecutive positions. When the 
+	game ends, what is the minimum and maximum number of moves that you could 
+	have made?  Return the answer as an length 2 array: 
+	answer = [minimum_moves, maximum_moves]
+
+	Example 1:
+	Input: [7,4,9]
+	Output: [1,2]
+	Explanation: We can move 4 -> 8 for one move to finish the game. Or, we can 
+	             move 9 -> 5, 4 -> 6 for two moves to finish the game.
+	
+	Example 2:
+	Input: [6,5,4,3,10]
+	Output: [2,3]
+	Explanation: We can move 3 -> 8 then 10 -> 7 to finish the game. Or, we can 
+	             move 3 -> 7, 4 -> 8, 5 -> 9 to finish the game. Notice we 
+	             cannot move 10 -> 2 to finish the game, because that would be 
+	             an illegal move.
+	
+	Example 3:
+	Input: [100,101,104,102,103]
+	Output: [0,0]
+
+	Note:
+	* 3 <= stones.length <= 10^4
+	* 1 <= stones[i] <= 10^9
+	* stones[i] have distinct values."""
+
+    def numMovesStonesII(self, stones: List[int]) -> List[int]:
+        stones.sort()
+        high = max(stones[-1] - stones[1], stones[-2] - stones[0]) - (len(stones) - 2)
+        
+        ii, low = 0, inf
+        for i in range(len(stones)): 
+            while stones[i] - stones[ii] >= len(stones): ii += 1
+            if i - ii + 1 == stones[i] - stones[ii] + 1 == len(stones) - 1: low = min(low, 2)
+            else: low = min(low, len(stones) - (i - ii + 1))
+        return [low, high]
+
+
     """1041. Robot Bounded In Circle (Medium)
 	On an infinite plane, a robot initially stands at (0, 0) and faces north.  
 	The robot can receive one of three instructions:
