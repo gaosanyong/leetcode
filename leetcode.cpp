6da444746d61294a7a022ffaf95418741afde2a5
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-24 21:45:28 -0400

    Solve'em all (955)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5954,6 +5954,64 @@ public:
     }
 
 
+    /*955. Delete Columns to Make Sorted II (Medium)
+	You are given an array of n strings strs, all of the same length. We may 
+	choose any deletion indices, and we delete all the characters in those 
+	indices for each string. For example, if we have strs = ["abcdef","uvwxyz"] 
+	and deletion indices {0, 2, 3}, then the final array after deletions is 
+	["bef", "vyz"]. Suppose we chose a set of deletion indices answer such that 
+	after deletions, the final array has its elements in lexicographic order 
+	(i.e., strs[0] <= strs[1] <= strs[2] <= ... <= strs[n - 1]). Return the 
+	minimum possible value of answer.length.
+
+	Example 1:
+	Input: strs = ["ca","bb","ac"]
+	Output: 1
+	Explanation: After deleting the first column, strs = ["a", "b", "c"]. Now 
+	             strs is in lexicographic order (ie. strs[0] <= strs[1] <= strs[2]).
+	             We require at least 1 deletion since initially strs was not in 
+	             lexicographic order, so the answer is 1.
+	
+	Example 2:
+	Input: strs = ["xc","yb","za"]
+	Output: 0
+	Explanation: strs is already in lexicographic order, so we do not need to 
+	             delete anything. Note that the rows of strs are not 
+	             necessarily in lexicographic order: i.e., it is NOT 
+	             necessarily true that (strs[0][0] <= strs[0][1] <= ...)
+	
+	Example 3:
+	Input: strs = ["zyx","wvu","tsr"]
+	Output: 3
+	Explanation: We have to delete every column.
+
+	Constraints:
+	* n == strs.length
+	* 1 <= n <= 100
+	* 1 <= strs[i].length <= 100
+	* strs[i] consists of lowercase English letters.*/
+
+    int minDeletionSize(vector<string>& strs) {
+        int ans = 0, m = size(strs), n = size(strs[0]); 
+        vector<int> grp(m); 
+        for (int j = 0; j < n; ++j) {
+            bool found = false; 
+            for (int i = 1; i < m; ++i) 
+                if (grp[i-1] == grp[i] && strs[i-1][j] > strs[i][j]) {
+                    ++ans; 
+                    found = true; 
+                    break; 
+                }
+            if (!found) 
+                for (int i = 1; i < m; ++i) {
+                    grp[i] = max(grp[i-1], grp[i]); 
+                    if (grp[i-1] == grp[i] && strs[i-1][j] < strs[i][j]) grp[i] = i; 
+                }
+        }
+        return ans; 
+    }
+
+
     /*961. N-Repeated Element in Size 2N Array (Easy)
 	In a array nums of size 2 * n, there are n + 1 unique elements, and exactly 
 	one of these elements is repeated n times. Return the element repeated n 
--- leetcode.py
+++ leetcode.py
@@ -16305,6 +16305,58 @@ class UnionFind:
         return fn(root1, root2)
 
 
+    """955. Delete Columns to Make Sorted II (Medium)
+	You are given an array of n strings strs, all of the same length. We may 
+	choose any deletion indices, and we delete all the characters in those 
+	indices for each string. For example, if we have strs = ["abcdef","uvwxyz"] 
+	and deletion indices {0, 2, 3}, then the final array after deletions is 
+	["bef", "vyz"]. Suppose we chose a set of deletion indices answer such that 
+	after deletions, the final array has its elements in lexicographic order 
+	(i.e., strs[0] <= strs[1] <= strs[2] <= ... <= strs[n - 1]). Return the 
+	minimum possible value of answer.length.
+
+	Example 1:
+	Input: strs = ["ca","bb","ac"]
+	Output: 1
+	Explanation: After deleting the first column, strs = ["a", "b", "c"]. Now 
+	             strs is in lexicographic order (ie. strs[0] <= strs[1] <= strs[2]).
+	             We require at least 1 deletion since initially strs was not in 
+	             lexicographic order, so the answer is 1.
+	
+	Example 2:
+	Input: strs = ["xc","yb","za"]
+	Output: 0
+	Explanation: strs is already in lexicographic order, so we do not need to 
+	             delete anything. Note that the rows of strs are not 
+	             necessarily in lexicographic order: i.e., it is NOT 
+	             necessarily true that (strs[0][0] <= strs[0][1] <= ...)
+	
+	Example 3:
+	Input: strs = ["zyx","wvu","tsr"]
+	Output: 3
+	Explanation: We have to delete every column.
+
+	Constraints:
+	* n == strs.length
+	* 1 <= n <= 100
+	* 1 <= strs[i].length <= 100
+	* strs[i] consists of lowercase English letters."""
+
+    def minDeletionSize(self, strs: List[str]) -> int:
+        m, n = len(strs), len(strs[0]) # dimensions 
+        ans, grp = 0, [0]*m
+        for j in range(n): 
+            for i in range(1, m): 
+                if grp[i-1] == grp[i] and strs[i-1][j] > strs[i][j]: 
+                    ans += 1
+                    break
+            else: 
+                for i in range(1, m): 
+                    grp[i] = max(grp[i-1], grp[i])
+                    if grp[i-1] == grp[i] and strs[i-1][j] < strs[i][j]: grp[i] = i
+        return ans 
+
+
     """968. Binary Tree Cameras (Hard)
 	Given a binary tree, we install cameras on the nodes of the tree. Each 
 	camera at a node can monitor its parent, itself, and its immediate children.
