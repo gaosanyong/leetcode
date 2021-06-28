61e7b644a124258d602498207568b2af0e8d643a
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-28 12:59:14 -0400

    biweekly 55 (1909|1910|1911|1912)

--- leetcode.cpp
+++ leetcode.cpp
@@ -11716,6 +11716,157 @@ public:
         }
         return ans; 
     }
+
+
+    /*1909. Remove One Element to Make the Array Strictly Increasing (Easy)
+	Given a 0-indexed integer array nums, return true if it can be made 
+	strictly increasing after removing exactly one element, or false otherwise. 
+	If the array is already strictly increasing, return true. The array nums is 
+	strictly increasing if nums[i - 1] < nums[i] for each index 
+	(1 <= i < nums.length).
+
+	Example 1:
+	Input: nums = [1,2,10,5,7]
+	Output: true
+	Explanation: By removing 10 at index 2 from nums, it becomes [1,2,5,7].
+	             [1,2,5,7] is strictly increasing, so return true.
+	
+	Example 2:
+	Input: nums = [2,3,1,2]
+	Output: false
+	Explanation: [3,1,2] is the result of removing the element at index 0.
+	             [2,1,2] is the result of removing the element at index 1.
+	             [2,3,2] is the result of removing the element at index 2.
+	             [2,3,1] is the result of removing the element at index 3.
+	             No resulting array is strictly increasing, so return false.
+	
+	Example 3:
+	Input: nums = [1,1,1]
+	Output: false
+	Explanation: The result of removing any element is [1,1]. [1,1] is not 
+	             strictly increasing, so return false.
+
+	Example 4:
+	Input: nums = [1,2,3]
+	Output: true
+	Explanation: [1,2,3] is already strictly increasing, so return true.
+
+	Constraints:
+	* 2 <= nums.length <= 1000
+	* 1 <= nums[i] <= 1000*/
+
+    bool canBeIncreasing(vector<int>& nums) {
+        int prev = INT_MIN; 
+        bool seen = false; 
+        for (int i = 0; i < size(nums); ++i) {
+            if (prev < nums[i]) prev = nums[i]; 
+            else {
+                if (seen) return false; 
+                seen = true; 
+                if (i == 1 || nums[i-2] < nums[i]) prev = nums[i]; 
+            }
+        }
+        return true; 
+    }
+
+
+    /*1910. Remove All Occurrences of a Substring (Medium)
+	Given two strings s and part, perform the following operation on s until 
+	all occurrences of the substring part are removed:
+	* Find the leftmost occurrence of the substring part and remove it from s.
+	Return s after removing all occurrences of part. A substring is a 
+	contiguous sequence of characters in a string.
+
+	Example 1:
+	Input: s = "daabcbaabcbc", part = "abc"
+	Output: "dab"
+	Explanation: The following operations are done:
+	             - s = "daabcbaabcbc", remove "abc" starting at index 2, so s = "dabaabcbc".
+	             - s = "dabaabcbc", remove "abc" starting at index 4, so s = "dababc".
+	             - s = "dababc", remove "abc" starting at index 3, so s = "dab".
+	             Now s has no occurrences of "abc".
+	
+	Example 2:
+	Input: s = "axxxxyyyyb", part = "xy"
+	Output: "ab"
+	Explanation: The following operations are done:
+	             - s = "axxxxyyyyb", remove "xy" starting at index 4 so s = "axxxyyyb".
+	             - s = "axxxyyyb", remove "xy" starting at index 3 so s = "axxyyb".
+	             - s = "axxyyb", remove "xy" starting at index 2 so s = "axyb".
+	             - s = "axyb", remove "xy" starting at index 1 so s = "ab".
+	             Now s has no occurrences of "xy".
+
+	Constraints:
+	* 1 <= s.length <= 1000
+	* 1 <= part.length <= 1000
+	* s and part consists of lowercase English letters.*/
+
+    string removeOccurrences(string s, string part) {
+        vector<int> lps = {0}; 
+        int k = 0; 
+        for (int i = 1; i < size(part); ++i) {
+            while (k && part[k] != part[i]) k = lps[k-1]; 
+            if (part[k] == part[i]) ++k; 
+            lps.push_back(k); 
+        }
+        
+        vector<pair<char, int>> stk; 
+        for (auto& ch : s) {
+            int k = size(stk) ? stk.back().second : 0; 
+            while (k && part[k] != ch) k = lps[k-1]; 
+            if (part[k] == ch) ++k; 
+            stk.emplace_back(ch, k); 
+            if (k == size(part)) 
+                while (k--) stk.pop_back(); 
+        }
+        
+        string ans; 
+        for (auto& [ch, k] : stk) ans.push_back(ch); 
+        return ans; 
+    }
+
+
+    /*1911. Maximum Alternating Subsequence Sum (Medium)
+	The alternating sum of a 0-indexed array is defined as the sum of the 
+	elements at even indices minus the sum of the elements at odd indices.
+	* For example, the alternating sum of [4,2,5,3] is (4 + 5) - (2 + 3) = 4.
+	Given an array nums, return the maximum alternating sum of any subsequence 
+	of nums (after reindexing the elements of the subsequence). A subsequence 
+	of an array is a new array generated from the original array by deleting 
+	some elements (possibly none) without changing the remaining elements' 
+	relative order. For example, [2,7,4] is a subsequence of [4,2,3,7,2,1,4] 
+	(the underlined elements), while [2,4,2] is not.
+
+	Example 1:
+	Input: nums = [4,2,5,3]
+	Output: 7
+	Explanation: It is optimal to choose the subsequence [4,2,5] with 
+	             alternating sum (4 + 5) - 2 = 7.
+
+	Example 2:
+	Input: nums = [5,6,7,8]
+	Output: 8
+	Explanation: It is optimal to choose the subsequence [8] with alternating 
+	             sum 8.
+	
+	Example 3:
+	Input: nums = [6,2,1,2,4,5]
+	Output: 10
+	Explanation: It is optimal to choose the subsequence [6,1,5] with 
+	             alternating sum (6 + 5) - 1 = 10.
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* 1 <= nums[i] <= 10^5*/
+
+    long long maxAlternatingSum(vector<int>& nums) {
+        long long ans = 0; 
+        for (int i = 1; i < size(nums); ++i) {
+            if (nums[i-1] > nums[i]) 
+                ans += nums[i-1] - nums[i]; 
+        }
+        return ans + nums.back(); 
+    }
 };
 
 
@@ -12421,6 +12572,112 @@ public:
         int ans = 0; 
         for (auto x : nums1) {
             ans += freq[tot - x]; 
+        }
+        return ans; 
+    }
+};
+
+
+/*1912. Design Movie Rental System (Hard)
+You have a movie renting company consisting of n shops. You want to implement a 
+renting system that supports searching for, booking, and returning movies. The 
+system should also support generating a report of the currently rented movies.
+Each movie is given as a 2D integer array entries where 
+entries[i] = [shopi, moviei, pricei] indicates that there is a copy of movie 
+moviei at shop shopi with a rental price of pricei. Each shop carries at most 
+one copy of a movie moviei. 
+
+The system should support the following functions:
+* Search: Finds the cheapest 5 shops that have an unrented copy of a given 
+  movie. The shops should be sorted by price in ascending order, and in case of 
+  a tie, the one with the smaller shopi should appear first. If there are less 
+  than 5 matching shops, then all of them should be returned. If no shop has an 
+  unrented copy, then an empty list should be returned.
+* Rent: Rents an unrented copy of a given movie from a given shop.
+* Drop: Drops off a previously rented copy of a given movie at a given shop.
+* Report: Returns the cheapest 5 rented movies (possibly of the same movie ID) 
+  as a 2D list res where res[j] = [shopj, moviej] describes that the jth 
+  cheapest rented movie moviej was rented from the shop shopj. The movies in 
+  res should be sorted by price in ascending order, and in case of a tie, the 
+  one with the smaller shopj should appear first, and if there is still tie, 
+  the one with the smaller moviej should appear first. If there are fewer than 
+  5 rented movies, then all of them should be returned. If no movies are 
+  currently being rented, then an empty list should be returned.
+
+Implement the MovieRentingSystem class:
+* MovieRentingSystem(int n, int[][] entries) Initializes the MovieRentingSystem 
+  object with n shops and the movies in entries.
+* List<Integer> search(int movie) Returns a list of shops that have an unrented 
+  copy of the given movie as described above.
+* void rent(int shop, int movie) Rents the given movie from the given shop.
+* void drop(int shop, int movie) Drops off a previously rented movie at the 
+  given shop.
+* List<List<Integer>> report() Returns a list of cheapest rented movies as 
+  described above.
+
+Note: The test cases will be generated such that rent will only be called if 
+the shop has an unrented copy of the movie, and drop will only be called if the 
+shop had previously rented out the movie.
+
+Example 1:
+Input: ["MovieRentingSystem", "search", "rent", "rent", "report", "drop", "search"]
+       [[3, [[0, 1, 5], [0, 2, 6], [0, 3, 7], [1, 1, 4], [1, 2, 7], [2, 1, 5]]], [1], [0, 1], [1, 2], [], [1, 2], [2]]
+Output: [null, [1, 0, 2], null, null, [[0, 1], [1, 2]], null, [0, 1]]
+Explanation: 
+MovieRentingSystem movieRentingSystem = new MovieRentingSystem(3, [[0, 1, 5], [0, 2, 6], [0, 3, 7], [1, 1, 4], [1, 2, 7], [2, 1, 5]]);
+movieRentingSystem.search(1);  // return [1, 0, 2], Movies of ID 1 are unrented at shops 1, 0, and 2. Shop 1 is cheapest; shop 0 and 2 are the same price, so order by shop number.
+movieRentingSystem.rent(0, 1); // Rent movie 1 from shop 0. Unrented movies at shop 0 are now [2,3].
+movieRentingSystem.rent(1, 2); // Rent movie 2 from shop 1. Unrented movies at shop 1 are now [1].
+movieRentingSystem.report();   // return [[0, 1], [1, 2]]. Movie 1 from shop 0 is cheapest, followed by movie 2 from shop 1.
+movieRentingSystem.drop(1, 2); // Drop off movie 2 at shop 1. Unrented movies at shop 1 are now [1,2].
+movieRentingSystem.search(2);  // return [0, 1]. Movies of ID 2 are unrented at shops 0 and 1. Shop 0 is cheapest, followed by shop 1.
+
+Constraints:
+* 1 <= n <= 3 * 10^5
+* 1 <= entries.length <= 10^5
+* 0 <= shopi < n
+* 1 <= moviei, pricei <= 10^4
+* Each shop carries at most one copy of a movie moviei.
+* At most 105 calls in total will be made to search, rent, drop and report.*/
+
+class MovieRentingSystem {
+    unordered_map<int, set<array<int,2>>> avail; 
+    map<array<int,2> int> cost; 
+    set<array<int,3>> rented; 
+public:
+    MovieRentingSystem(int n, vector<vector<int>>& entries) {
+        for (auto& entry : entries) {
+            int shop = entry[0], movie = entry[1], price = entry[2]; 
+            avail.insert({movie, {price, shop}}); 
+            cost.insert({{shop, movie}, price}); 
+        }
+    }
+    
+    vector<int> search(int movie) {
+        if (avail.find(movie) == avail.end()) return {}; 
+        vector<int> ans; 
+        for (auto it = begin(avail[movie]), k = 0; it != end(avail[end]) && k < 5; ++it, ++k) {
+            ans.push_back(*it[1]); 
+        }
+        return ans; 
+    }
+    
+    void rent(int shop, int movie) {
+        int price = cost[{shop, movie}]; 
+        avail[movie].erase({price, shop}); 
+        rented.insert({price, shop, movie}); 
+    }
+    
+    void drop(int shop, int movie) {
+        int price = cost[{shop, movie}]; 
+        avail[movie].insert({price, shop}); 
+        rented.erase({price, shop, movie}); 
+    }
+    
+    vector<vector<int>> report() {
+        vector<vector<int>> ans; 
+        for (auto it = begin(rented), k = 0; it != end(rented) && k < 5; ++it, ++k) {
+            ans.push_back(*it); 
         }
         return ans; 
     }
