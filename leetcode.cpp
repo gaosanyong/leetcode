0391dab8a273f33542fb1aafed98fd69bc903071
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-28 11:38:18 -0400

    June LeetCoding Challenge 2021-06-28 (1047)

--- leetcode.cpp
+++ leetcode.cpp
@@ -6730,6 +6730,41 @@ public:
     }
 
 
+    /*1047. Remove All Adjacent Duplicates In String (Easy)
+	You are given a string s consisting of lowercase English letters. A 
+	duplicate removal consists of choosing two adjacent and equal letters and 
+	removing them. We repeatedly make duplicate removals on s until we no 
+	longer can. Return the final string after all such duplicate removals have 
+	been made. It can be proven that the answer is unique.
+
+	Example 1:
+	Input: s = "abbaca"
+	Output: "ca"
+	Explanation: For example, in "abbaca" we could remove "bb" since the 
+	             letters are adjacent and equal, and this is the only possible 
+	             move.  The result of this move is that the string is "aaca", 
+	             of which only "aa" is possible, so the final string is "ca".
+	
+	Example 2:
+	Input: s = "azxxzy"
+	Output: "ay"
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s consists of lowercase English letters.*/
+
+    string removeDuplicates(string s) {
+        vector<char> stk; 
+        for (auto& ch : s) {
+            if (size(stk) && stk.back() == ch) stk.pop_back(); 
+            else stk.push_back(ch); 
+        }
+        string ans; 
+        for (auto& ch : stk) ans.push_back(ch); 
+        return ans; 
+    }
+
+
     /*1048. Longest String Chain (Medium)
 	Given a list of words, each word consists of English lowercase letters. 
 	Let's say word1 is a predecessor of word2 if and only if we can add exactly 
