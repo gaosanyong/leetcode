61ec2d390500bf678b6bcddf113dd86c66a916ef
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-29 12:26:10 -0400

    Solve'em all (780)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4124,6 +4124,47 @@ public:
     }
 
 
+    /*780. Reaching Points (Hard)
+	Given four integers sx, sy, tx, and ty, return true if it is possible to 
+	convert the point (sx, sy) to the point (tx, ty) through some operations, 
+	or false otherwise. The allowed operation on some point (x, y) is to 
+	convert it to either (x, x + y) or (x + y, y).
+
+	Example 1:
+	Input: sx = 1, sy = 1, tx = 3, ty = 5
+	Output: true
+	Explanation: One series of moves that transforms the starting point to the 
+	             target is:
+	             (1, 1) -> (1, 2)
+	             (1, 2) -> (3, 2)
+	             (3, 2) -> (3, 5)
+	
+	Example 2:
+	Input: sx = 1, sy = 1, tx = 2, ty = 2
+	Output: false
+
+	Example 3:
+	Input: sx = 1, sy = 1, tx = 1, ty = 1
+	Output: true
+
+	Constraints: 1 <= sx, sy, tx, ty <= 10^9*/
+
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+        while (sx < tx || sy < ty) {
+            if (tx > ty) {
+                int k = (tx - sx)/ty; 
+                if (k == 0) break; 
+                tx -= k * ty; 
+            } else {
+                int k = (ty - sy)/tx; 
+                if (k == 0) break; 
+                ty -= k * tx; 
+            }
+        }
+        return sx == tx && sy == ty; 
+    }
+
+
     /*792. Number of Matching Subsequences (Medium)
 	Given a string s and an array of strings words, return the number of 
 	words[i] that is a subsequence of s. A subsequence of a string is a new 
--- leetcode.py
+++ leetcode.py
@@ -13812,6 +13812,44 @@ class UnionFind:
         return bin(K-1).count("1") & 1
 
 
+    """780. Reaching Points (Hard)
+	Given four integers sx, sy, tx, and ty, return true if it is possible to 
+	convert the point (sx, sy) to the point (tx, ty) through some operations, 
+	or false otherwise. The allowed operation on some point (x, y) is to 
+	convert it to either (x, x + y) or (x + y, y).
+
+	Example 1:
+	Input: sx = 1, sy = 1, tx = 3, ty = 5
+	Output: true
+	Explanation: One series of moves that transforms the starting point to the 
+	             target is:
+	             (1, 1) -> (1, 2)
+	             (1, 2) -> (3, 2)
+	             (3, 2) -> (3, 5)
+	
+	Example 2:
+	Input: sx = 1, sy = 1, tx = 2, ty = 2
+	Output: false
+
+	Example 3:
+	Input: sx = 1, sy = 1, tx = 1, ty = 1
+	Output: true
+
+	Constraints: 1 <= sx, sy, tx, ty <= 109"""
+
+    def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
+        while sx < tx or sy < ty: 
+            if tx > ty: 
+                k = (tx - sx)//ty 
+                if k == 0: break 
+                tx -= k * ty 
+            else: 
+                k = (ty - sy)//tx 
+                if k == 0: break 
+                ty -= k * tx 
+        return sx == tx and sy == ty
+
+
     """781. Rabbits in Forest (Medium)
 	In a forest, each rabbit has some color. Some subset of rabbits (possibly 
 	all of them) tell you how many other rabbits have the same color as them. 
