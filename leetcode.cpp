9bfaf13ebde0c76bd0128d958801e96d41b0a546
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-29 10:29:19 -0400

    June LeetCoding Challenge 2021-06-29 (1004)

--- leetcode.cpp
+++ leetcode.cpp
@@ -6579,6 +6579,41 @@ public:
     }
 
 
+    /*1004. Max Consecutive Ones III (Medium)
+	Given a binary array nums and an integer k, return the maximum number of 
+	consecutive 1's in the array if you can flip at most k 0's.
+
+	Example 1:
+	Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+	Output: 6
+	Explanation: [1,1,1,0,0,1,1,1,1,1,1] 
+	             Bolded numbers were flipped from 0 to 1. The longest subarray 
+	             is underlined.
+	
+	Example 2:
+	Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+	Output: 10
+	Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+	             Bolded numbers were flipped from 0 to 1. The longest subarray 
+	             is underlined.
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* nums[i] is either 0 or 1.
+	* 0 <= k <= nums.length*/
+
+    int longestOnes(vector<int>& nums, int k) {
+        int ans = 0; 
+        for (int i = 0, ii = 0; i < size(nums); ++i) {
+            if (nums[i] == 0) --k; 
+            for (; k < 0; ++ii) 
+                if (nums[ii] == 0) ++k; 
+            ans = max(ans, i - ii + 1); 
+        }
+        return ans; 
+    }
+
+
     /*1005. Maximize Sum Of Array After K Negations (Easy)
 	Given an array nums of integers, we must modify the array in the following 
 	way: we choose an i and replace nums[i] with -nums[i], and we repeat this 
--- leetcode.py
+++ leetcode.py
@@ -16640,6 +16640,40 @@ class UnionFind:
         return ans
 
 
+    """1004. Max Consecutive Ones III (Medium)
+	Given a binary array nums and an integer k, return the maximum number of 
+	consecutive 1's in the array if you can flip at most k 0's.
+
+	Example 1:
+	Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+	Output: 6
+	Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+	             Bolded numbers were flipped from 0 to 1. The longest subarray 
+	             is underlined.
+	
+	Example 2:
+	Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+	Output: 10
+	Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+	             Bolded numbers were flipped from 0 to 1. The longest subarray 
+	             is underlined.
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* nums[i] is either 0 or 1.
+	* 0 <= k <= nums.length"""
+
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        ans = ii = 0 
+        for i in range(len(nums)): 
+            if nums[i] == 0: k -= 1
+            while k < 0: 
+                if nums[ii] == 0: k += 1
+                ii += 1
+            ans = max(ans, i - ii + 1)
+        return ans 
+
+
     """1014. Best Sightseeing Pair (Medium)
 	Given an array A of positive integers, A[i] represents the value of the 
 	i-th sightseeing spot, and two sightseeing spots i and j have distance 
