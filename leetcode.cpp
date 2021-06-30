534f36ace29ea92dc620d296743aa5b26c13b36d
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-06-30 15:59:30 -0400

    Solve'em all (798)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4418,6 +4418,58 @@ public:
     }
 
 
+    /*798. Smallest Rotation with Highest Score (Hard)
+	Given an array nums, we may rotate it by a non-negative integer k so that 
+	the array becomes nums[k], nums[k+1], nums[k+2], ... nums[nums.length - 1], 
+	nums[0], nums[1], ..., nums[k-1].  Afterward, any entries that are less 
+	than or equal to their index are worth 1 point. For example, if we have 
+	[2, 4, 1, 3, 0], and we rotate by k = 2, it becomes [1, 3, 0, 2, 4]. This 
+	is worth 3 points because 1 > 0 [no points], 3 > 1 [no points], 
+	0 <= 2 [one point], 2 <= 3 [one point], 4 <= 4 [one point]. Over all 
+	possible rotations, return the rotation index k that corresponds to the 
+	highest score we could receive. If there are multiple answers, return the 
+	smallest such index k.
+
+	Example 1:
+	Input: [2, 3, 1, 4, 0]
+	Output: 3
+	Explanation: Scores for each k are listed below: 
+	             k = 0,  nums = [2,3,1,4,0],    score 2
+	             k = 1,  nums = [3,1,4,0,2],    score 3
+	             k = 2,  nums = [1,4,0,2,3],    score 3
+	             k = 3,  nums = [4,0,2,3,1],    score 4
+	             k = 4,  nums = [0,2,3,1,4],    score 3
+	             So we should choose k = 3, which has the highest score.
+
+	Example 2:
+	Input: [1, 3, 0, 2, 4]
+	Output: 0
+	Explanation: nums will always have 3 points no matter how it shifts. So we 
+	             will choose the smallest k, which is 0.
+	
+	Note:
+	* nums will have length at most 20000.
+	* nums[i] will be in the range [0, nums.length].*/
+
+    int bestRotation(vector<int>& nums) {
+        int n = size(nums); 
+
+        vector<int> diff(n, 1); 
+        for (int i = 0; i < n; ++i) 
+            diff[(i+n-nums[i]+1) % n]--; 
+        
+        int ans = 0, prefix = 0, mx = INT_MIN; 
+        for (int i = 0; i < n; ++i) {
+            prefix += diff[i]; 
+            if (prefix > mx) {
+                mx = prefix; 
+                ans = i; 
+            }
+        }
+        return ans; 
+    }
+
+
     /*816. Ambiguous Coordinates (Medium)
 	We had some 2-dimensional coordinates, like "(1, 3)" or "(2, 0.5)". Then, 
 	we removed all commas, decimal points, and spaces, and ended up with the 
--- leetcode.py
+++ leetcode.py
@@ -14366,6 +14366,52 @@ class UnionFind:
         return fn(0)
 
 
+    """798. Smallest Rotation with Highest Score (Hard)
+	Given an array nums, we may rotate it by a non-negative integer k so that 
+	the array becomes nums[k], nums[k+1], nums[k+2], ... nums[nums.length - 1], 
+	nums[0], nums[1], ..., nums[k-1].  Afterward, any entries that are less 
+	than or equal to their index are worth 1 point. For example, if we have 
+	[2, 4, 1, 3, 0], and we rotate by k = 2, it becomes [1, 3, 0, 2, 4]. This 
+	is worth 3 points because 1 > 0 [no points], 3 > 1 [no points], 
+	0 <= 2 [one point], 2 <= 3 [one point], 4 <= 4 [one point]. Over all 
+	possible rotations, return the rotation index k that corresponds to the 
+	highest score we could receive. If there are multiple answers, return the 
+	smallest such index k.
+
+	Example 1:
+	Input: [2, 3, 1, 4, 0]
+	Output: 3
+	Explanation: Scores for each k are listed below: 
+	             k = 0,  nums = [2,3,1,4,0],    score 2
+	             k = 1,  nums = [3,1,4,0,2],    score 3
+	             k = 2,  nums = [1,4,0,2,3],    score 3
+	             k = 3,  nums = [4,0,2,3,1],    score 4
+	             k = 4,  nums = [0,2,3,1,4],    score 3
+	             So we should choose k = 3, which has the highest score.
+
+	Example 2:
+	Input: [1, 3, 0, 2, 4]
+	Output: 0
+	Explanation: nums will always have 3 points no matter how it shifts. So we 
+	             will choose the smallest k, which is 0.
+	
+	Note:
+	* nums will have length at most 20000.
+	* nums[i] will be in the range [0, nums.length]."""
+
+    def bestRotation(self, nums: List[int]) -> int:
+        diff = [1] * len(nums)
+        for i, x in enumerate(nums): 
+            diff[(i-x+1)%len(nums)] -= 1
+        
+        ans = prefix = 0 
+        mx = -inf 
+        for i, x in enumerate(diff): 
+            prefix += x
+            if prefix > mx: mx, ans = prefix, i
+        return ans 
+
+
     """800. Similar RGB Color (Easy)
 	In the following, every capital letter represents some hexadecimal digit 
 	from 0 to f. The red-green-blue color "#AABBCC" can be written as "#ABC" in 
