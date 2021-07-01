130cb2d363ad6d2801cc4ef8e8f5cbc6ca4b1fa6
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-01 17:10:43 -0400

    Solve'em all (815)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4543,6 +4543,63 @@ public:
     }
 
 
+    /*815. Bus Routes (Hard)
+	You are given an array routes representing bus routes where routes[i] is a 
+	bus route that the ith bus repeats forever. For example, if 
+	routes[0] = [1, 5, 7], this means that the 0th bus travels in the sequence 
+	1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... forever. You will start at the bus 
+	stop source (You are not on any bus initially), and you want to go to the 
+	bus stop target. You can travel between bus stops by buses only. Return the 
+	least number of buses you must take to travel from source to target. Return 
+	-1 if it is not possible.
+
+	Example 1:
+	Input: routes = [[1,2,7],[3,6,7]], source = 1, target = 6
+	Output: 2
+	Explanation: The best strategy is take the first bus to the bus stop 7, 
+	             then take the second bus to the bus stop 6.
+
+	Example 2:
+	Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
+	Output: -1
+
+	Constraints:
+	* 1 <= routes.length <= 500.
+	* 1 <= routes[i].length <= 10^5
+	* All the values of routes[i] are unique.
+	* sum(routes[i].length) <= 10^5
+	* 0 <= routes[i][j] < 10^6
+	* 0 <= source, target < 10^6*/
+
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        unordered_map<int, vector<int>> mp; 
+        for (int i = 0; i < size(routes); ++i) 
+            for (auto& x : routes[i]) 
+                mp[x].push_back(i); 
+        
+        int ans = 0; 
+        queue<int> q; q.push(source); 
+        unordered_set<int> seen = {source}; 
+        
+        while (size(q)) {
+            for (int n = size(q); n > 0; --n) {
+                int x = q.front(); q.pop(); 
+                if (x == target) return ans; 
+                for (auto i : mp[x]) {
+                    for (auto xx : routes[i]) 
+                        if (seen.find(xx) == seen.end()) {
+                            seen.insert(xx); 
+                            q.push(xx); 
+                        }
+                    routes[i].clear(); 
+                }
+            }
+            ++ans; 
+        }
+        return -1; 
+    }
+
+
     /*816. Ambiguous Coordinates (Medium)
 	We had some 2-dimensional coordinates, like "(1, 3)" or "(2, 0.5)". Then, 
 	we removed all commas, decimal points, and spaces, and ended up with the 
--- leetcode.py
+++ leetcode.py
@@ -14514,6 +14514,58 @@ class UnionFind:
         return fn(len(A), K)
 
 
+    """815. Bus Routes (Hard)
+	You are given an array routes representing bus routes where routes[i] is a 
+	bus route that the ith bus repeats forever. For example, if 
+	routes[0] = [1, 5, 7], this means that the 0th bus travels in the sequence 
+	1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... forever. You will start at the bus 
+	stop source (You are not on any bus initially), and you want to go to the 
+	bus stop target. You can travel between bus stops by buses only. Return the 
+	least number of buses you must take to travel from source to target. Return 
+	-1 if it is not possible.
+
+	Example 1:
+	Input: routes = [[1,2,7],[3,6,7]], source = 1, target = 6
+	Output: 2
+	Explanation: The best strategy is take the first bus to the bus stop 7, 
+	             then take the second bus to the bus stop 6.
+	
+	Example 2:
+	Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
+	Output: -1
+
+	Constraints:
+	* 1 <= routes.length <= 500.
+	* 1 <= routes[i].length <= 10^5
+	* All the values of routes[i] are unique.
+	* sum(routes[i].length) <= 10^5
+	* 0 <= routes[i][j] < 10^6
+	* 0 <= source, target < 10^6"""
+
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        mp = {}
+        for i, route in enumerate(routes): 
+            for x in route: 
+                mp.setdefault(x, []).append(i)
+        
+        ans = 0 
+        seen = {source}
+        queue = [source]
+        while queue: 
+            newq = []
+            for x in queue: 
+                if x == target: return ans 
+                for i in mp[x]: 
+                    for xx in routes[i]: 
+                        if xx not in seen: 
+                            seen.add(xx)
+                            newq.append(xx)
+                    routes[i] = []
+            ans += 1
+            queue = newq
+        return -1 
+
+
     """816. Ambiguous Coordinates (Medium)
 	We had some 2-dimensional coordinates, like "(1, 3)" or "(2, 0.5)".  Then, 
 	we removed all commas, decimal points, and spaces, and ended up with the 
