ccfd627bdabc1ffc9650c0383ddafea4378ac66d
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-01 11:42:56 -0400

    July LeetCoding Challenge 2021-07-01 (89)

--- leetcode.cpp
+++ leetcode.cpp
@@ -778,6 +778,46 @@ public:
     }
 
 
+    /*89. Gray Code (Medium)
+	An n-bit gray code sequence is a sequence of 2n integers where:
+	* Every integer is in the inclusive range [0, 2n - 1],
+	* The first integer is 0,
+	* An integer appears no more than once in the sequence,
+	* The binary representation of every pair of adjacent integers differs by 
+	  exactly one bit, and
+	* The binary representation of the first and last integers differs by 
+	  exactly one bit.
+	Given an integer n, return any valid n-bit gray code sequence.
+
+	Example 1:
+	Input: n = 2
+	Output: [0,1,3,2]
+	Explanation: The binary representation of [0,1,3,2] is [00,01,11,10].
+	             - 00 and 01 differ by one bit
+	             - 01 and 11 differ by one bit
+	             - 11 and 10 differ by one bit
+	             - 10 and 00 differ by one bit
+	             [0,2,3,1] is also a valid gray code sequence, whose binary 
+	             representation is [00,10,11,01].
+	             - 00 and 10 differ by one bit
+	             - 10 and 11 differ by one bit
+	             - 11 and 01 differ by one bit
+	             - 01 and 00 differ by one bit
+	
+	Example 2:
+	Input: n = 1
+	Output: [0,1]
+
+	Constraints: 1 <= n <= 16*/
+
+    vector<int> grayCode(int n) {
+        vector<int> ans(1); 
+        for (int i = 1; i < (1 << n); ++i) 
+            ans.push_back(ans.back() ^ (i & -i)); 
+        return ans; 
+    }
+
+
     /*92. Reverse Linked List II (Medium)
 	Given the head of a singly linked list and two integers left and right 
 	where left <= right, reverse the nodes of the list from position left to 
