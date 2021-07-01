38782873eaf4d642ccbbfe82340fbb85a996dc1f
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-01 14:30:27 -0400

    Solve'em all (810)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4510,6 +4510,39 @@ public:
     }
 
 
+    /*810. Chalkboard XOR Game (Hard)
+	We are given non-negative integers nums[i] which are written on a 
+	chalkboard. Alice and Bob take turns erasing exactly one number from the 
+	chalkboard, with Alice starting first.  If erasing a number causes the 
+	bitwise XOR of all the elements of the chalkboard to become 0, then that 
+	player loses.  (Also, we'll say the bitwise XOR of one element is that 
+	element itself, and the bitwise XOR of no elements is 0.) Also, if any 
+	player starts their turn with the bitwise XOR of all the elements of the 
+	chalkboard equal to 0, then that player wins. Return True if and only if 
+	Alice wins the game, assuming both players play optimally.
+
+	Example:
+	Input: nums = [1, 1, 2]
+	Output: false
+	Explanation: Alice has two choices: erase 1 or erase 2. If she erases 1, 
+	             the nums array becomes [1, 2]. The bitwise XOR of all the 
+	             elements of the chalkboard is 1 XOR 2 = 3. Now Bob can remove 
+	             any element he wants, because Alice will be the one to erase 
+	             the last element and she will lose. If Alice erases 2 first, 
+	             now nums becomes [1, 1]. The bitwise XOR of all the elements 
+	             of the chalkboard is 1 XOR 1 = 0. Alice will lose.
+
+	Notes:
+	* 1 <= N <= 1000. 
+	* 0 <= nums[i] <= 2^16.*/
+
+    bool xorGame(vector<int>& nums) {
+        int val = 0; 
+        for (auto& x : nums) val ^= x; 
+        return val == 0 || size(nums) % 2 == 0; 
+    }
+
+
     /*816. Ambiguous Coordinates (Medium)
 	We had some 2-dimensional coordinates, like "(1, 3)" or "(2, 0.5)". Then, 
 	we removed all commas, decimal points, and spaces, and ended up with the 
--- leetcode.py
+++ leetcode.py
@@ -14446,6 +14446,36 @@ class UnionFind:
         return "#" + "".join(fn(color[i:i+2]) for i in (1,3,5))
 
 
+    """810. Chalkboard XOR Game (Hard)
+	We are given non-negative integers nums[i] which are written on a 
+	chalkboard. Alice and Bob take turns erasing exactly one number from the 
+	chalkboard, with Alice starting first.  If erasing a number causes the 
+	bitwise XOR of all the elements of the chalkboard to become 0, then that 
+	player loses.  (Also, we'll say the bitwise XOR of one element is that 
+	element itself, and the bitwise XOR of no elements is 0.) Also, if any 
+	player starts their turn with the bitwise XOR of all the elements of the 
+	chalkboard equal to 0, then that player wins. Return True if and only if 
+	Alice wins the game, assuming both players play optimally.
+
+	Example:
+	Input: nums = [1, 1, 2]
+	Output: false
+	Explanation: Alice has two choices: erase 1 or erase 2. If she erases 1, 
+	             the nums array becomes [1, 2]. The bitwise XOR of all the 
+	             elements of the chalkboard is 1 XOR 2 = 3. Now Bob can remove 
+	             any element he wants, because Alice will be the one to erase 
+	             the last element and she will lose. If Alice erases 2 first, 
+	             now nums becomes [1, 1]. The bitwise XOR of all the elements 
+	             of the chalkboard is 1 XOR 1 = 0. Alice will lose.
+
+	Notes:
+	* 1 <= N <= 1000. 
+	* 0 <= nums[i] <= 2^16."""
+
+    def xorGame(self, nums: List[int]) -> bool:
+        return reduce(xor, nums) == 0 or not len(nums)&1
+
+
     """813. Largest Sum of Averages (Medium)
 	We partition a row of numbers A into at most K adjacent (non-empty) groups, 
 	then our score is the sum of the average of each group. What is the largest 
