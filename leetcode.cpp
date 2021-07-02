cd3ca32204f43e90f87caddfc7328356ae02b07b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-02 15:42:35 -0400

    Solve'em all (847)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5078,6 +5078,58 @@ public:
     }
 
 
+    /*847. Shortest Path Visiting All Nodes (Hard)
+	You have an undirected, connected graph of n nodes labeled from 0 to n - 1. 
+	You are given an array graph where graph[i] is a list of all the nodes 
+	connected with node i by an edge. Return the length of the shortest path 
+	that visits every node. You may start and stop at any node, you may revisit 
+	nodes multiple times, and you may reuse edges.
+
+	Example 1:
+	Input: graph = [[1,2,3],[0],[0],[0]]
+	Output: 4
+	Explanation: One possible path is [1,0,2,0,3]
+
+	Example 2:
+	Input: graph = [[1],[0,2,4],[1,3,4],[2],[1,2]]
+	Output: 4
+	Explanation: One possible path is [0,1,4,2,3]
+
+	Constraints:
+	* n == graph.length
+	* 1 <= n <= 12
+	* 0 <= graph[i].length < n
+	* graph[i] does not contain i.
+	* If graph[a] contains b, then graph[b] contains a.
+	* The input graph is always connected.*/
+
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int n = graph.size(); 
+        queue<pair<int, int>> q; 
+        vector<vector<bool>> seen(n, vector<bool>(1 << n, false)); 
+        
+        for (int i = 0; i < n; ++i) {
+            q.emplace(i, 1 << i);
+            seen[i][1 << i] = true;  
+        }
+        
+        int ans = 0; 
+        while (q.size()) {
+            for (int k = q.size(); k; --k) {
+                auto [i, mask] = q.front(); q.pop(); 
+                if (mask == (1 << n) - 1) return ans;  
+                for (auto& ii : graph[i]) 
+                    if (!seen[ii][mask | (1 << ii)]) {
+                        q.emplace(ii, mask | (1 << ii));
+                        seen[ii][mask | (1 << ii)] = true; 
+                    }
+            }
+            ++ans; 
+        }
+        return ans; 
+    }
+
+
     /*852. Peak Index in a Mountain Array (Easy)
 	Let's call an array arr a mountain if the following properties hold:
 	* arr.length >= 3
--- leetcode.py
+++ leetcode.py
@@ -15245,6 +15245,47 @@ class UnionFind:
         return ans 
 
 
+    """847. Shortest Path Visiting All Nodes (Hard)
+	You have an undirected, connected graph of n nodes labeled from 0 to n - 1. 
+	You are given an array graph where graph[i] is a list of all the nodes 
+	connected with node i by an edge. Return the length of the shortest path 
+	that visits every node. You may start and stop at any node, you may revisit 
+	nodes multiple times, and you may reuse edges.
+
+	Example 1:
+	Input: graph = [[1,2,3],[0],[0],[0]]
+	Output: 4
+	Explanation: One possible path is [1,0,2,0,3]
+
+	Example 2:
+	Input: graph = [[1],[0,2,4],[1,3,4],[2],[1,2]]
+	Output: 4
+	Explanation: One possible path is [0,1,4,2,3]
+
+	Constraints:
+	* n == graph.length
+	* 1 <= n <= 12
+	* 0 <= graph[i].length < n
+	* graph[i] does not contain i.
+	* If graph[a] contains b, then graph[b] contains a.
+	* The input graph is always connected."""
+
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        queue = [(i, 1 << i) for i in range(len(graph))]
+        seen = set(queue)
+        ans = 0
+        while queue: 
+            newq = []
+            for i, mask in queue: 
+                if mask == (1 << len(graph)) - 1: return ans 
+                for ii in graph[i]: 
+                    if (ii, mask | (1 << ii)) not in seen: 
+                        newq.append((ii, mask | (1 << ii)))
+                        seen.add((ii, mask | (1 << ii)))
+            queue = newq
+            ans += 1
+
+
     """848. Shifting Letters (Medium)
 	We have a string S of lowercase letters, and an integer array shifts. Call 
 	the shift of a letter, the next letter in the alphabet, (wrapping around so 
