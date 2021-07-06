71c78345dc000fd7fcaf9cbcae34fe3bdecd77fe
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-06 18:59:56 -0400

    July LeetCoding Challenge 2021-07-06 (1338)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7729,6 +7729,60 @@ public:
     }
 
 
+    /*1338. Reduce Array Size to The Half (Medium)
+	Given an array arr.  You can choose a set of integers and remove all the 
+	occurrences of these integers in the array. Return the minimum size of the 
+	set so that at least half of the integers of the array are removed.
+
+	Example 1:
+	Input: arr = [3,3,3,3,5,5,5,2,2,7]
+	Output: 2
+	Explanation: Choosing {3,7} will make the new array [5,5,5,2,2] which has 
+	             size 5 (i.e equal to half of the size of the old array). 
+	             Possible sets of size 2 are {3,5},{3,2},{5,2}. Choosing set 
+	             {2,7} is not possible as it will make the new array 
+	             [3,3,3,3,5,5,5] which has size greater than half of the size 
+	             of the old array.
+	
+	Example 2:
+	Input: arr = [7,7,7,7,7,7]
+	Output: 1
+	Explanation: The only possible set you can choose is {7}. This will make 
+	             the new array empty.
+	
+	Example 3:
+	Input: arr = [1,9]
+	Output: 1
+
+	Example 4:
+	Input: arr = [1000,1000,3,7]
+	Output: 1
+
+	Example 5:
+	Input: arr = [1,2,3,4,5,6,7,8,9,10]
+	Output: 5
+
+	Constraints:
+	* 1 <= arr.length <= 10^5
+	* arr.length is even.
+	* 1 <= arr[i] <= 10^5*/
+
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int, int> freq; 
+        for (auto& x : arr) freq[x]++; 
+        
+        vector<int> vals; 
+        for (auto& [key, val] : freq) vals.push_back(val); 
+        
+        sort(vals.begin(), vals.end(), greater<int>()); 
+        for (int i = 0, ans = 0; i < vals.size(); ++i) {
+            ans += vals[i]; 
+            if (ans >= arr.size()/2) return i + 1; 
+        }
+        return -1; 
+    }
+
+
     /*1354. Construct Target Array With Multiple Sums (Hard)
 	Given an array of integers target. From a starting array, A consisting of 
 	all 1's, you may perform the following procedure :
