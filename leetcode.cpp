ff9bf2cf4fa7f5f022f742a9132a41b147d3bca9
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-13 10:02:23 -0400

    July LeetCoding Challenge 2021-07-13 (162)

--- leetcode.cpp
+++ leetcode.cpp
@@ -1519,6 +1519,41 @@ public:
     }
 
 
+    /*162. Find Peak Element (Medium)
+	A peak element is an element that is strictly greater than its neighbors. 
+	Given an integer array nums, find a peak element, and return its index. If 
+	the array contains multiple peaks, return the index to any of the peaks.
+	You may imagine that nums[-1] = nums[n] = -∞. You must write an algorithm 
+	that runs in O(log n) time.
+
+	Example 1:
+	Input: nums = [1,2,3,1]
+	Output: 2
+	Explanation: 3 is a peak element and your function should return the index 
+	             number 2.
+	
+	Example 2:
+	Input: nums = [1,2,1,3,5,6,4]
+	Output: 5
+	Explanation: Your function can return either index number 1 where the peak 
+	             element is 2, or index number 5 where the peak element is 6.
+
+	Constraints:
+	* 1 <= nums.length <= 1000
+	* -2^31 <= nums[i] <= 2^31 - 1
+	* nums[i] != nums[i + 1] for all valid i.*/
+
+    int findPeakElement(vector<int>& nums) {
+        int lo = 0, hi = nums.size()-1; 
+        while (lo < hi) {
+            int mid = lo + (hi - lo)/2; 
+            if (nums[mid] > nums[mid+1]) hi = mid;
+            else lo = mid + 1; 
+        }
+        return lo; 
+    }
+
+
     /*204. Count Primes (Easy)
 	Count the number of prime numbers less than a non-negative number, n.
 
