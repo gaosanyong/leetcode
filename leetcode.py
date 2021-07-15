d43b202fef9e7115b5feef9d831d0ef80a493e73
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-15 15:57:10 -0400

    July LeetCoding Challenge 2021-07-15 (611)

--- leetcode.py
+++ leetcode.py
@@ -12096,6 +12096,39 @@ class Solution:
         return n <= 0
 
 
+    """611. Valid Triangle Number (Medium)
+	Given an integer array nums, return the number of triplets chosen from the 
+	array that can make triangles if we take them as side lengths of a triangle.
+
+	Example 1:
+	Input: nums = [2,2,3,4]
+	Output: 3
+	Explanation: Valid combinations are: 
+	             2,3,4 (using the first 2)
+	             2,3,4 (using the second 2)
+	             2,2,3
+	
+	Example 2:
+	Input: nums = [4,2,3,4]
+	Output: 4
+
+	Constraints:
+	* 1 <= nums.length <= 1000
+	* 0 <= nums[i] <= 1000"""
+
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = 0
+        for i in range(len(nums)): 
+            lo, hi = 0, i-1
+            while lo < hi: 
+                if nums[lo] + nums[hi] > nums[i]:
+                    ans += hi - lo 
+                    hi -= 1
+                else: lo += 1
+        return ans 
+
+
     """629. K Inverse Pairs Array (Hard)
 	For an integer array nums, an inverse pair is a pair of integers [i, j] 
 	where 0 <= i < j < nums.length and nums[i] > nums[j]. Given two integers n 
