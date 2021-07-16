3beab62ed1225322d79a27ca5c31453de6470ae7
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-16 15:12:42 -0400

    Solve'em all (768)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4468,6 +4468,44 @@ public:
     }
 
 
+    /*768. Max Chunks To Make Sorted II (Hard)
+	You are given an integer array arr. We split arr into some number of chunks 
+	(i.e., partitions), and individually sort each chunk. After concatenating 
+	them, the result should equal the sorted array. Return the largest number 
+	of chunks we can make to sort the array.
+
+	Example 1:
+	Input: arr = [5,4,3,2,1]
+	Output: 1
+	Explanation: Splitting into two or more chunks will not return the required 
+	             result. For example, splitting into [5, 4], [3, 2, 1] will 
+	             result in [4, 5, 1, 2, 3], which isn't sorted.
+	
+	Example 2:
+	Input: arr = [2,1,3,4,4]
+	Output: 4
+	Explanation: We can split into two chunks, such as [2, 1], [3, 4, 4]. 
+	             However, splitting into [2, 1], [3], [4], [4] is the highest 
+	             number of chunks possible.
+
+	Constraints:
+	* 1 <= arr.length <= 2000
+	* 0 <= arr[i] <= 10^8*/
+
+    int maxChunksToSorted(vector<int>& arr) {
+        stack<int> stk; 
+        for (auto& x : arr) {
+            int most = x; 
+            while (stk.size() && stk.top() > x) {
+                most = max(most, stk.top()); 
+                stk.pop(); 
+            }
+            stk.push(most); 
+        }
+        return stk.size(); 
+    }
+
+
     /*778. Swim in Rising Water (Hard)
 	On an N x N grid, each square grid[i][j] represents the elevation at that 
 	point (i,j). Now rain starts to fall. At time t, the depth of the water 
--- leetcode.py
+++ leetcode.py
@@ -13757,6 +13757,39 @@ class UnionFind:
         return "".join(ans)
 
 
+    """768. Max Chunks To Make Sorted II (Hard)
+	You are given an integer array arr. We split arr into some number of chunks 
+	(i.e., partitions), and individually sort each chunk. After concatenating 
+	them, the result should equal the sorted array. Return the largest number 
+	of chunks we can make to sort the array.
+
+	Example 1:
+	Input: arr = [5,4,3,2,1]
+	Output: 1
+	Explanation: Splitting into two or more chunks will not return the required 
+	             result. For example, splitting into [5, 4], [3, 2, 1] will 
+	             result in [4, 5, 1, 2, 3], which isn't sorted.
+	
+	Example 2:
+	Input: arr = [2,1,3,4,4]
+	Output: 4
+	Explanation: We can split into two chunks, such as [2, 1], [3, 4, 4]. 
+	             However, splitting into [2, 1], [3], [4], [4] is the highest 
+	             number of chunks possible.
+
+	Constraints:
+	* 1 <= arr.length <= 2000
+	* 0 <= arr[i] <= 10^8"""
+
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        stack = []
+        for i, x in enumerate(arr): 
+            most = x
+            while stack and stack[-1] > x: most = max(most, stack.pop())
+            stack.append(most)
+        return len(stack)
+
+
     """769. Max Chunks To Make Sorted (Medium)
 	Given an array arr that is a permutation of [0, 1, ..., arr.length - 1], we 
 	split the array into some number of "chunks" (partitions), and individually 
