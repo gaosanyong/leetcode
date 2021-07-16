00bbb38341236bedbb1342513ae54d74583be06c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-16 14:30:10 -0400

    Solve'em all (668)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3691,6 +3691,40 @@ public:
     }
 
 
+    /*668. Kth Smallest Number in Multiplication Table (Hard)
+	Nearly everyone has used the Multiplication Table. The multiplication table 
+	of size m x n is an integer matrix mat where mat[i][j] == i * j (1-indexed).
+	Given three integers m, n, and k, return the kth smallest element in the 
+	m x n multiplication table.
+
+	Example 1:
+	Input: m = 3, n = 3, k = 5
+	Output: 3
+	Explanation: The 5th smallest number is 3.
+
+	Example 2:
+	Input: m = 2, n = 3, k = 6
+	Output: 6
+	Explanation: The 6th smallest number is 6.
+
+	Constraints:
+	* 1 <= m, n <= 3 * 10^4
+	* 1 <= k <= m * n*/
+
+    int findKthNumber(int m, int n, int k) {
+        int lo = 0, hi = m*n + 1; 
+        while (lo < hi) {
+            int mid = lo + (hi - lo)/2; 
+            int x = 0; 
+            for (int i = 1; i <= m; ++i)
+                x += min(n, mid/i); 
+            if (x < k) lo = mid + 1; 
+            else hi = mid; 
+        }
+        return lo; 
+    }
+
+
     /*680. Valid Palindrome II (Easy)
 	Given a non-empty string s, you may delete at most one character. Judge 
 	whether you can make it a palindrome.
--- leetcode.py
+++ leetcode.py
@@ -12705,6 +12705,36 @@ class Solution:
         return ans 
 
 
+    """668. Kth Smallest Number in Multiplication Table (Hard)
+	Nearly everyone has used the Multiplication Table. The multiplication table 
+	of size m x n is an integer matrix mat where mat[i][j] == i * j (1-indexed).
+	Given three integers m, n, and k, return the kth smallest element in the 
+	m x n multiplication table.
+
+	Example 1:
+	Input: m = 3, n = 3, k = 5
+	Output: 3
+	Explanation: The 5th smallest number is 3.
+
+	Example 2:
+	Input: m = 2, n = 3, k = 6
+	Output: 6
+	Explanation: The 6th smallest number is 6.
+
+	Constraints:
+	* 1 <= m, n <= 3 * 10^4
+	* 1 <= k <= m * n"""
+
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+        lo, hi = 0, m*n + 1
+        while lo < hi: 
+            mid = lo + hi >> 1
+            x = sum(min(n, mid//i) for i in range(1, m+1))
+            if x < k: lo = mid + 1
+            else: hi = mid 
+        return lo 
+
+
     """678. Valid Parenthesis String (Medium)
 	Given a string containing only three types of characters: '(', ')' and '*', 
 	write a function to check whether this string is valid. We define the 
