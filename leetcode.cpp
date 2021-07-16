a2b20e68c415cf99a6fc8b400ff48deec5c0cd47
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-16 11:52:39 -0400

    July LeetCoding Challenge 2021-07-16 (18)

--- leetcode.cpp
+++ leetcode.cpp
@@ -224,6 +224,48 @@ public:
     }
 
 
+    /*18. 4Sum (Medium)
+	Given an array nums of n integers, return an array of all the unique 
+	quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+	* 0 <= a, b, c, d < n
+	* a, b, c, and d are distinct.
+	* nums[a] + nums[b] + nums[c] + nums[d] == target
+	You may return the answer in any order.
+
+	Example 1:
+	Input: nums = [1,0,-1,0,-2,2], target = 0
+	Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+	Example 2:
+	Input: nums = [2,2,2,2,2], target = 8
+	Output: [[2,2,2,2]]
+
+	Constraints:
+	* 1 <= nums.length <= 200
+	* -10^9 <= nums[i] <= 10^9
+	* -10^9 <= target <= 10^9*/
+
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end()); 
+
+        vector<vector<int>> ans; 
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i && nums[i-1] == nums[i]) continue; 
+            for (int j = i+1; j < nums.size(); ++j) {
+                if (i+1 < j && nums[j-1] == nums[j]) continue; 
+                int lo = j+1, hi = nums.size()-1; 
+                while (lo < hi) {
+                    int sm = nums[i] + nums[j] + nums[lo] + nums[hi]; 
+                    if (sm < target || (j+1 < lo && nums[lo-1] == nums[lo])) ++lo; 
+                    else if (sm > target || (hi+1 < nums.size() && nums[hi] == nums[hi+1])) --hi; 
+                    else ans.push_back({nums[i], nums[j], nums[lo++], nums[hi--]}); 
+                }
+            }
+        }
+        return ans; 
+    }
+
+
     /*19. Remove Nth Node From End of List (Medium)
 	Given the head of a linked list, remove the nth node from the end of the 
 	list and return its head. Follow up: Could you do this in one pass?
