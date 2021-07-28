91dfc4c678c92965897471c5b90ea9592dad53ed
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-27 22:17:42 -0400

    Solve'em all (964)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7541,6 +7541,61 @@ public:
     }
 
 
+    /*964. Least Operators to Express Number (Hard)
+	Given a single positive integer x, we will write an expression of the form 
+	x (op1) x (op2) x (op3) x ... where each operator op1, op2, etc. is either 
+	addition, subtraction, multiplication, or division (+, -, *, or /). For 
+	example, with x = 3, we might write 3 * 3 / 3 + 3 - 3 which is a value of 3.
+	When writing such an expression, we adhere to the following conventions:
+	* The division operator (/) returns rational numbers.
+	* There are no parentheses placed anywhere.
+	* We use the usual order of operations: multiplication and division happen 
+	  before addition and subtraction.
+	* It is not allowed to use the unary negation operator (-). For example, 
+	  "x - x" is a valid expression as it only uses subtraction, but "-x + x" 
+	  is not because it uses negation.
+	We would like to write an expression with the least number of operators 
+	such that the expression equals the given target. Return the least number 
+	of operators used.
+
+	Example 1:
+	Input: x = 3, target = 19
+	Output: 5
+	Explanation: 3 * 3 + 3 * 3 + 3 / 3. The expression contains 5 operations.
+	
+	Example 2:
+	Input: x = 5, target = 501
+	Output: 8
+	Explanation: 5 * 5 * 5 * 5 - 5 * 5 * 5 + 5 / 5. The expression contains 8 
+	             operations.
+	
+	Example 3:
+	Input: x = 100, target = 100000000
+	Output: 3
+	Explanation: 100 * 100 * 100 * 100. The expression contains 3 operations.
+
+	Constraints:
+	* 2 <= x <= 100
+	* 1 <= target <= 2 * 10^8*/
+
+    int leastOpsExpressTarget(int x, int target) {
+        unordered_map<int, int> memo; 
+        
+        function<int(int)> fn = [&](int val) {
+            if (val < x) return min(2*val-1, 2*(x-val)); 
+            if (memo.find(val) == memo.end()) {
+                int k = log(val)/log(x); 
+                memo[val] = k + fn(val - pow(x, k)); 
+                if (pow(x, k+1) < 2*val) 
+                    memo[val] = min(memo[val], k+1+fn(pow(x, k+1) - val)); 
+            }
+            return memo[val]; 
+        }; 
+        
+        return fn(target); 
+    }
+
+
     /*965. Univalued Binary Tree (Easy)
 	A binary tree is univalued if every node in the tree has the same value.
 	Return true if and only if the given tree is univalued.
