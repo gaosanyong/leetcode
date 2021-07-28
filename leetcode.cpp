eccc30009cc04d93d06660fae37968a0710c3a1f
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-07-28 15:55:11 -0400

    Solve'em all (843)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5902,6 +5902,61 @@ public:
     }
 
 
+    /*843. Guess the Word (Hard)
+	This is an interactive problem. You are given an array of unique strings 
+	wordlist where wordlist[i] is 6 letters long, and one word in this list is 
+	chosen as secret. You may call Master.guess(word) to guess a word. The 
+	guessed word should have type string and must be from the original list 
+	with 6 lowercase letters. This function returns an integer type, 
+	representing the number of exact matches (value and position) of your guess 
+	to the secret word. Also, if your guess is not in the given wordlist, it 
+	will return -1 instead. For each test case, you have exactly 10 guesses to 
+	guess the word. At the end of any number of calls, if you have made 10 or 
+	fewer calls to Master.guess and at least one of these guesses was secret, 
+	then you pass the test case.
+
+	Example 1:
+	Input: secret = "acckzz", wordlist = ["acckzz","ccbazz","eiowzz","abcczz"], numguesses = 10
+	Output: You guessed the secret word correctly.
+	Explanation: master.guess("aaaaaa") returns -1, because "aaaaaa" is not in wordlist.
+	             master.guess("acckzz") returns 6, because "acckzz" is secret and has all 6 matches.
+	             master.guess("ccbazz") returns 3, because "ccbazz" has 3 matches.
+	             master.guess("eiowzz") returns 2, because "eiowzz" has 2 matches.
+	             master.guess("abcczz") returns 4, because "abcczz" has 4 matches.
+	             We made 5 calls to master.guess and one of them was the secret, so we pass the test case.
+
+	Example 2:
+	Input: secret = "hamada", wordlist = ["hamada","khaled"], numguesses = 10
+	Output: You guessed the secret word correctly.
+
+	Constraints:
+	* 1 <= wordlist.length <= 100
+	* wordlist[i].length == 6
+	* wordlist[i] consist of lowercase English letters.
+	* All the strings of wordlist are unique.
+	* secret exists in wordlist.
+	* numguesses == 10*/
+
+    void findSecretWord(vector<string>& wordlist, Master& master) {
+        srand(1); 
+        random_shuffle(wordlist.begin(), wordlist.end()); 
+        for (int step = 0; step < 10; ++step) 
+            if (wordlist.size()) {
+                string w = wordlist.back(); 
+                wordlist.pop_back(); 
+                int m = master.guess(w); 
+                vector<string> temp; 
+                for (auto& ww : wordlist) {
+                    int cnt = 0; 
+                    for (int i = 0; i < w.size(); ++i) 
+                        if (w[i] == ww[i]) ++cnt; 
+                    if (cnt == m) temp.push_back(ww); 
+                }
+                wordlist = temp; 
+            }
+    }
+
+
     /*847. Shortest Path Visiting All Nodes (Hard)
 	You have an undirected, connected graph of n nodes labeled from 0 to n - 1. 
 	You are given an array graph where graph[i] is a list of all the nodes 
