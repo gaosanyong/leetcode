333c6496f7ec6d2c276e732087345ded6ae8e271
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-02 10:19:27 -0400

    Solve'em all (828)'

--- leetcode.cpp
+++ leetcode.cpp
@@ -5832,6 +5832,49 @@ public:
     }
 
 
+    /*828. Count Unique Characters of All Substrings of a Given String (Hard)
+	Let's define a function countUniqueChars(s) that returns the number of 
+	unique characters on s. 
+	* For example if s = "LEETCODE" then "L", "T", "C", "O", "D" are the unique 
+	characters since they appear only once in s, therefore countUniqueChars(s) = 5.
+	Given a string s, return the sum of countUniqueChars(t) where t is a 
+	substring of s. Notice that some substrings can be repeated so in this case 
+	you have to count the repeated ones too.
+
+	Example 1:
+	Input: s = "ABC"
+	Output: 10
+	Explanation: All possible substrings are: "A","B","C","AB","BC" and "ABC".
+	             Evey substring is composed with only unique letters. Sum of 
+	             lengths of all substring is 1 + 1 + 1 + 2 + 2 + 3 = 10
+	
+	Example 2:
+	Input: s = "ABA"
+	Output: 8
+	Explanation: The same as example 1, except countUniqueChars("ABA") = 1.
+
+	Example 3:
+	Input: s = "LEETCODE"
+	Output: 92
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s consists of uppercase English letters only.*/
+
+    int uniqueLetterString(string s) {
+        vector<vector<int>> locs(26, vector<int>(1, -1)); 
+        for (int i = 0; i < s.size(); ++i) locs[s[i] - 'A'].push_back(i); 
+        
+        int ans = 0; 
+        for (int i = 0; i < 26; ++i) {
+            locs[i].push_back(s.size()); 
+            for (int k = 1; k < locs[i].size()-1; ++k) 
+                ans += (locs[i][k] - locs[i][k-1]) * (locs[i][k+1] - locs[i][k]); 
+        }
+        return ans; 
+    }
+
+
     /*830. Positions of Large Groups (Easy)
 	In a string s of lowercase letters, these letters form consecutive groups 
 	of the same character. For example, a string like s = "abbxxxxzyy" has the 
