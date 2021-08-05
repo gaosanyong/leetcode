5101c4492b717a3fa6249f7d82d882343da04b99
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-05 11:11:14 -0400

    August LeetCoding Challenge 2021-08-05 (877)

--- leetcode.cpp
+++ leetcode.cpp
@@ -6849,6 +6849,44 @@ public:
     }
 
 
+    /*877. Stone Game (Medium)
+	Alex and Lee play a game with piles of stones. There are an even number of 
+	piles arranged in a row, and each pile has a positive integer number of 
+	stones piles[i]. The objective of the game is to end with the most stones.  
+	The total number of stones is odd, so there are no ties. Alex and Lee take 
+	turns, with Alex starting first. Each turn, a player takes the entire pile 
+	of stones from either the beginning or the end of the row. This continues 
+	until there are no more piles left, at which point the person with the most 
+	stones wins. Assuming Alex and Lee play optimally, return True if and only 
+	if Alex wins the game.
+
+	Example 1:
+	Input: piles = [5,3,4,5]
+	Output: true
+	Explanation: 
+	Alex starts first, and can only take the first 5 or the last 5.
+	Say he takes the first 5, so that the row becomes [3, 4, 5].
+	If Lee takes 3, then the board is [4, 5], and Alex takes 5 to win with 10 points.
+	If Lee takes the last 5, then the board is [3, 4], and Alex takes 4 to win with 9 points.
+	This demonstrated that taking the first 5 was a winning move for Alex, so we return true.
+
+	Constraints:
+	* 2 <= piles.length <= 500
+	* piles.length is even.
+	* 1 <= piles[i] <= 500
+	* sum(piles) is odd.*/
+
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size(); 
+        vector<vector<int>> dp(n, vector<int>(n)); 
+        for (int i = n-1; i >= 0; --i) 
+            for (int j = i; j < n; ++j) 
+                if (i == j) dp[i][j] = piles[i]; 
+                else dp[i][j] = max(piles[i] + dp[i+1][j], piles[j] + dp[i][j-1]); 
+        return dp[0][n-1] > 0; 
+    }
+
+
     /*883. Projection Area of 3D Shapes (Easy)
 	You are given an n x n grid where we place some 1 x 1 x 1 cubes that are 
 	axis-aligned with the x, y, and z axes. Each value v = grid[i][j] 
--- leetcode.py
+++ leetcode.py
@@ -16439,6 +16439,44 @@ class UnionFind:
         return ans 
 
 
+    """877. Stone Game (Medium)
+	Alex and Lee play a game with piles of stones. There are an even number of 
+	piles arranged in a row, and each pile has a positive integer number of 
+	stones piles[i]. The objective of the game is to end with the most stones.  
+	The total number of stones is odd, so there are no ties. Alex and Lee take 
+	turns, with Alex starting first. Each turn, a player takes the entire pile 
+	of stones from either the beginning or the end of the row. This continues 
+	until there are no more piles left, at which point the person with the most 
+	stones wins. Assuming Alex and Lee play optimally, return True if and only 
+	if Alex wins the game.
+
+	Example 1:
+	Input: piles = [5,3,4,5]
+	Output: true
+	Explanation: 
+	Alex starts first, and can only take the first 5 or the last 5.
+	Say he takes the first 5, so that the row becomes [3, 4, 5].
+	If Lee takes 3, then the board is [4, 5], and Alex takes 5 to win with 10 points.
+	If Lee takes the last 5, then the board is [3, 4], and Alex takes 4 to win with 9 points.
+	This demonstrated that taking the first 5 was a winning move for Alex, so we return true.
+
+	Constraints:
+	* 2 <= piles.length <= 500
+	* piles.length is even.
+	* 1 <= piles[i] <= 500
+	* sum(piles) is odd."""
+
+    def stoneGame(self, piles: List[int]) -> bool:
+        
+        @cache
+        def fn(lo, hi): 
+            """Return diff when picking stones from lo to hi (inclusive)."""
+            if lo == hi: return piles[lo]
+            return max(piles[lo] - fn(lo+1, hi), piles[hi] - fn(lo, hi-1))
+        
+        return fn(0, len(piles)-1) > 0
+
+
     """881. Boats to Save People (Medium)
 	The i-th person has weight people[i], and each boat can carry a maximum 
 	weight of limit. Each boat carries at most 2 people at the same time, 
