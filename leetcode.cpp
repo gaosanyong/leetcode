776d1db0652ab0585a0ec8ecf32806ee66c6d874
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-06 12:05:11 -0400

    August LeetCoding Challenge 2021-08-06 (429)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2791,6 +2791,42 @@ public:
     }
 
 
+    /*429. N-ary Tree Level Order Traversal (Medium)
+	Given an n-ary tree, return the level order traversal of its nodes' values. 
+	Nary-Tree input serialization is represented in their level order traversal, 
+	each group of children is separated by the null value (See examples).
+
+	Example 1:
+	Input: root = [1,null,3,2,4,null,5,6]
+	Output: [[1],[3,2,4],[5,6]]
+
+	Example 2:
+	Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+	Output: [[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+
+	Constraints:
+	* The height of the n-ary tree is less than or equal to 1000
+	* The total number of nodes is between [0, 10^4]*/
+
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> ans; 
+        if (root) {
+            queue<Node*> q; 
+            q.push(root); 
+            while (q.size()) {
+                vector<int> vals; 
+                for (int sz = q.size(); sz; --sz) {
+                    Node* node = q.front(); q.pop(); 
+                    vals.push_back(node->val); 
+                    for (auto& child : node->children) q.push(child); 
+                }
+                ans.push_back(vals); 
+            }
+        }
+        return ans; 
+    }
+
+
     /*446. Arithmetic Slices II - Subsequence (Hard)
 	Given an integer array nums, return the number of all the arithmetic 
 	subsequences of nums. A sequence of numbers is called arithmetic if it 
--- leetcode.py
+++ leetcode.py
@@ -10969,6 +10969,37 @@ class Solution:
         return head 
 
 
+    """429. N-ary Tree Level Order Traversal (Medium)
+	Given an n-ary tree, return the level order traversal of its nodes' values. 
+	Nary-Tree input serialization is represented in their level order traversal, 
+	each group of children is separated by the null value (See examples).
+
+	Example 1:
+	Input: root = [1,null,3,2,4,null,5,6]
+	Output: [[1],[3,2,4],[5,6]]
+
+	Example 2:
+	Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+	Output: [[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+
+	Constraints:
+	* The height of the n-ary tree is less than or equal to 1000
+	* The total number of nodes is between [0, 10^4]"""
+
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        ans = []
+        if root: 
+            queue = [root]
+            while queue: 
+                newq, vals = [], []
+                for x in queue: 
+                    vals.append(x.val)
+                    newq.extend(x.children)
+                ans.append(vals)
+                queue = newq
+        return ans 
+
+
     """446. Arithmetic Slices II - Subsequence (Hard)
 	Given an integer array nums, return the number of all the arithmetic 
 	subsequences of nums. A sequence of numbers is called arithmetic if it 
