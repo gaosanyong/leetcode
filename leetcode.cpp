8c2539058ab30395ac3169ccd9c67388d5a0186b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-09 11:30:57 -0400

    August LeetCoding Challenge 2021-08-09 (415)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2791,6 +2791,42 @@ public:
     }
 
 
+    /*415. Add Strings (Easy)
+	Given two non-negative integers, num1 and num2 represented as string, 
+	return the sum of num1 and num2 as a string. You must solve the problem 
+	without using any built-in library for handling large integers (such as 
+	BigInteger). You must also not convert the inputs to integers directly.
+
+	Example 1:
+	Input: num1 = "11", num2 = "123"
+	Output: "134"
+
+	Example 2:
+	Input: num1 = "456", num2 = "77"
+	Output: "533"
+
+	Example 3:
+	Input: num1 = "0", num2 = "0"
+	Output: "0"
+
+	Constraints:
+	* 1 <= num1.length, num2.length <= 10^4
+	* num1 and num2 consist of only digits.
+	* num1 and num2 don't have any leading zeros except for the zero itself.*/
+
+    string addStrings(string num1, string num2) {
+        string ans;
+        for (int i = num1.size()-1, j = num2.size()-1, carry = 0; 0 <= i || 0 <= j || carry; --i, --j) {
+            if (0 <= i) carry += num1[i] - '0'; 
+            if (0 <= j) carry += num2[j] - '0'; 
+            ans.push_back(carry % 10 + '0'); 
+            carry /= 10; 
+        }
+        reverse(ans.begin(), ans.end()); 
+        return ans; 
+    }
+
+
     /*429. N-ary Tree Level Order Traversal (Medium)
 	Given an n-ary tree, return the level order traversal of its nodes' values. 
 	Nary-Tree input serialization is represented in their level order traversal, 
--- leetcode.py
+++ leetcode.py
@@ -10810,6 +10810,39 @@ class Solution:
         return ans 
 
 
+    """415. Add Strings (Easy)
+	Given two non-negative integers, num1 and num2 represented as string, 
+	return the sum of num1 and num2 as a string. You must solve the problem 
+	without using any built-in library for handling large integers (such as 
+	BigInteger). You must also not convert the inputs to integers directly.
+
+	Example 1:
+	Input: num1 = "11", num2 = "123"
+	Output: "134"
+
+	Example 2:
+	Input: num1 = "456", num2 = "77"
+	Output: "533"
+
+	Example 3:
+	Input: num1 = "0", num2 = "0"
+	Output: "0"
+
+	Constraints:
+	* 1 <= num1.length, num2.length <= 10^4
+	* num1 and num2 consist of only digits.
+	* num1 and num2 don't have any leading zeros except for the zero itself."""
+
+    def addStrings(self, num1: str, num2: str) -> str:
+        ans = []
+        carry = 0
+        for x, y in zip_longest(reversed(num1), reversed(num2), fillvalue="0"): 
+            carry, x = divmod(carry + int(x) + int(y), 10)
+            ans.append(x)
+        if carry: ans.append(carry)
+        return "".join(map(str, reversed(ans)))
+
+
     """418. Sentence Screen Fitting (Medium)
 	Given a rows x cols screen and a sentence represented by a list of non-
 	empty words, find how many times the given sentence can be fitted on the 
