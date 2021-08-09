6692ff09132bdc6865aee115f64df33e09279a68
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-09 17:34:44 -0400

    biweekly 58 (1957|1958|1959|1960)

--- leetcode.cpp
+++ leetcode.cpp
@@ -15917,6 +15917,231 @@ public:
     }
 
 
+    /*1957. Delete Characters to Make Fancy String (Easy)
+	A fancy string is a string where no three consecutive characters are equal. 
+	Given a string s, delete the minimum possible number of characters from s 
+	to make it fancy. Return the final string after the deletion. It can be 
+	shown that the answer will always be unique.
+
+	Example 1:
+	Input: s = "leeetcode"
+	Output: "leetcode"
+	Explanation: Remove an 'e' from the first group of 'e's to create 
+	             "leetcode". No three consecutive characters are equal, so 
+	             return "leetcode".
+	
+	Example 2:
+	Input: s = "aaabaaaa"
+	Output: "aabaa"
+	Explanation: Remove an 'a' from the first group of 'a's to create "aabaaaa".
+	             Remove two 'a's from the second group of 'a's to create "aabaa".
+	             No three consecutive characters are equal, so return "aabaa".
+	
+	Example 3:
+	Input: s = "aab"
+	Output: "aab"
+	Explanation: No three consecutive characters are equal, so return "aab".
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s consists only of lowercase English letters.*/
+
+    string makeFancyString(string s) {
+        int ii = 1; 
+        for (int i = 2; i < s.size(); ++i) {
+            if (s[ii-1] == s[ii] && s[ii] == s[i]) continue; 
+            s[++ii] = s[i]; 
+        }
+        return s.substr(0, ++ii); 
+    }
+
+
+    /*1958. Check if Move is Legal (Medium)
+	You are given a 0-indexed 8 x 8 grid board, where board[r][c] represents 
+	the cell (r, c) on a game board. On the board, free cells are represented 
+	by '.', white cells are represented by 'W', and black cells are represented 
+	by 'B'. Each move in this game consists of choosing a free cell and 
+	changing it to the color you are playing as (either white or black). 
+	However, a move is only legal if, after changing it, the cell becomes the 
+	endpoint of a good line (horizontal, vertical, or diagonal). A good line is 
+	a line of three or more cells (including the endpoints) where the endpoints 
+	of the line are one color, and the remaining cells in the middle are the 
+	opposite color (no cells in the line are free). Given two integers rMove 
+	and cMove and a character color representing the color you are playing as 
+	(white or black), return true if changing cell (rMove, cMove) to color 
+	color is a legal move, or false if it is not legal.
+
+	Example 1:
+	Input: board = [[".",".",".","B",".",".",".","."],
+	                [".",".",".","W",".",".",".","."],
+	                [".",".",".","W",".",".",".","."],
+	                [".",".",".","W",".",".",".","."],
+	                ["W","B","B",".","W","W","W","B"],
+	                [".",".",".","B",".",".",".","."],
+	                [".",".",".","B",".",".",".","."],
+	                [".",".",".","W",".",".",".","."]], 
+	       rMove = 4, cMove = 3, color = "B"
+	Output: true
+	Explanation: '.', 'W', and 'B' are represented by the colors blue, white, 
+	             and black respectively, and cell (rMove, cMove) is marked with 
+	             an 'X'. The two good lines with the chosen cell as an endpoint 
+	             are annotated above with the red rectangles.
+	
+	Example 2:
+	Input: board = [[".",".",".",".",".",".",".","."],
+	                [".","B",".",".","W",".",".","."],
+	                [".",".","W",".",".",".",".","."],
+	                [".",".",".","W","B",".",".","."],
+	                [".",".",".",".",".",".",".","."],
+	                [".",".",".",".","B","W",".","."],
+	                [".",".",".",".",".",".","W","."],
+	                [".",".",".",".",".",".",".","B"]], 
+	       rMove = 4, cMove = 4, color = "W"
+	Output: false
+	Explanation: While there are good lines with the chosen cell as a middle 
+	             cell, there are no good lines with the chosen cell as an 
+	             endpoint.
+
+	Constraints:
+	* board.length == board[r].length == 8
+	* 0 <= rMove, cMove < 8
+	* board[rMove][cMove] == '.'
+	* color is either 'B' or 'W'.*/
+
+    bool checkMove(vector<vector<char>>& board, int rMove, int cMove, char color) {
+        int dir[9] = {1, 0, -1, 0, 1, 1, -1, -1, 1}; 
+        for (int k = 0; k < 8; ++k) {
+            for (int i = rMove, j = cMove, step = 0; 0 <= i && i < 8 && 0 <= j && j < 8; i += dir[k], j += dir[k+1], ++step) {
+                if (board[i][j] == color && step > 1) return true; 
+                if ((board[i][j] == '.' && step > 0) || (board[i][j] == color && step == 1)) break; 
+            }
+        }
+        return false; 
+    }
+
+
+    /*1959. Minimum Total Space Wasted With K Resizing Operations (Medium)
+	You are currently designing a dynamic array. You are given a 0-indexed 
+	integer array nums, where nums[i] is the number of elements that will be in 
+	the array at time i. In addition, you are given an integer k, the maximum 
+	number of times you can resize the array (to any size). The size of the 
+	array at time t, sizet, must be at least nums[t] because there needs to be 
+	enough space in the array to hold all the elements. The space wasted at 
+	time t is defined as sizet - nums[t], and the total space wasted is the sum 
+	of the space wasted across every time t where 0 <= t < nums.length. Return 
+	the minimum total space wasted if you can resize the array at most k times.
+	Note: The array can have any size at the start and does not count towards 
+	the number of resizing operations.
+
+	Example 1:
+	Input: nums = [10,20], k = 0
+	Output: 10
+	Explanation: size = [20,20]. We can set the initial size to be 20. The 
+	             total wasted space is (20 - 10) + (20 - 20) = 10.
+	
+	Example 2:
+	Input: nums = [10,20,30], k = 1
+	Output: 10
+	Explanation: size = [20,20,30]. We can set the initial size to be 20 and 
+	             resize to 30 at time 2. The total wasted space is 
+	             (20 - 10) + (20 - 20) + (30 - 30) = 10.
+	
+	Example 3:
+	Input: nums = [10,20,15,30,20], k = 2
+	Output: 15
+	Explanation: size = [10,20,20,30,30]. We can set the initial size to 10, 
+	             resize to 20 at time 1, and resize to 30 at time 3. The total 
+	             wasted space is 
+	             (10 - 10) + (20 - 20) + (20 - 15) + (30 - 30) + (30 - 20) = 15.
+
+	Constraints:
+	* 1 <= nums.length <= 200
+	* 1 <= nums[i] <= 10^6
+	* 0 <= k <= nums.length - 1*/
+
+    int minSpaceWastedKResizing(vector<int>& nums, int k) {
+        int n = nums.size(); 
+        vector<vector<int>> dp(n+1, vector<int>(k+1, INT_MAX)); 
+        
+        for (int j = 0; j <= k; ++j) dp[n][j] = 0; 
+        int total = 0, most = 0; 
+        for (int i = n-1; i >= 0; --i) {
+            most = max(most, nums[i]); 
+            total += nums[i]; 
+            dp[i][0] = most * (n - i) - total; 
+        }
+        
+        for (int i = n-1; i >= 0; --i) 
+            for (int j = 1; j <= k; ++j) {
+                total = most = 0; 
+                for (int ii = i; ii < n; ++ii) {
+                    total += nums[ii]; 
+                    most = max(most, nums[ii]); 
+                    dp[i][j] = min(dp[i][j], most * (ii - i + 1) - total + dp[ii+1][j-1]); 
+                }
+            }
+        return dp[0][k]; 
+    }
+
+
+    /*1960. Maximum Product of the Length of Two Palindromic Substrings (Hard)
+	You are given a 0-indexed string s and are tasked with finding two non-
+	intersecting palindromic substrings of odd length such that the product of 
+	their lengths is maximized. More formally, you want to choose four integers 
+	i, j, k, l such that 0 <= i <= j < k <= l < s.length and both the 
+	substrings s[i...j] and s[k...l] are palindromes and have odd lengths. 
+	s[i...j] denotes a substring from index i to index j inclusive. Return the 
+	maximum possible product of the lengths of the two non-intersecting 
+	palindromic substrings. A palindrome is a string that is the same forward 
+	and backward. A substring is a contiguous sequence of characters in a 
+	string.
+
+	Example 1:
+	Input: s = "ababbb"
+	Output: 9
+	Explanation: Substrings "aba" and "bbb" are palindromes with odd length. 
+	             product = 3 * 3 = 9.
+	
+	Example 2:
+	Input: s = "zaaaxbbby"
+	Output: 9
+	Explanation: Substrings "aaa" and "bbb" are palindromes with odd length. 
+	             product = 3 * 3 = 9.
+
+	Constraints:
+	* 2 <= s.length <= 10^5
+	* s consists of lowercase English letters.*/
+
+    long long maxProduct(string s) {
+        int n = s.size(); 
+        
+        function<vector<int>(string)> fn = [&](string s) {
+            vector<int> ans(n, 1), hlen(n, 0); 
+            int center = 0, right = 0; 
+            for (int i = 0; i < n; ++i) {
+                if (i < right) hlen[i] = min(right - i, hlen[2*center - i]); 
+                while (0 <= i-1-hlen[i] && i+1+hlen[i] < n && s[i-1-hlen[i]] == s[i+1+hlen[i]]) {
+                    ++hlen[i]; 
+                    ans[i+hlen[i]] = max(ans[i+hlen[i]], 2*hlen[i] + 1); 
+                }
+                if (right < i + hlen[i]) {
+                    center = i; 
+                    right = i + hlen[i]; 
+                }
+            }
+            for (int i = 1; i < n; ++i) ans[i] = max(ans[i-1], ans[i]); 
+            return ans; 
+        }; 
+        
+        auto prefix = fn(s); 
+        reverse(s.begin(), s.end()); 
+        auto suffix = fn(s); 
+        long long ans = 0ll; 
+        for (int i = 1; i < n; ++i) ans = max(ans, (long long) prefix[i-1] * suffix[n-1-i]); 
+        return ans; 
+    }
+
+
     /*1961. Check If String Is a Prefix of Array (Easy)
 	Given a string s and an array of strings words, determine whether s is a 
 	prefix string of words. A string s is a prefix string of words if s can be 
--- leetcode.py
+++ leetcode.py
@@ -38585,6 +38585,215 @@ class Trie:
         return s2
 
 
+    """1957. Delete Characters to Make Fancy String (Easy)
+	A fancy string is a string where no three consecutive characters are equal. 
+	Given a string s, delete the minimum possible number of characters from s 
+	to make it fancy. Return the final string after the deletion. It can be 
+	shown that the answer will always be unique.
+
+	Example 1:
+	Input: s = "leeetcode"
+	Output: "leetcode"
+	Explanation: Remove an 'e' from the first group of 'e's to create 
+	             "leetcode". No three consecutive characters are equal, so 
+	             return "leetcode".
+	
+	Example 2:
+	Input: s = "aaabaaaa"
+	Output: "aabaa"
+	Explanation: Remove an 'a' from the first group of 'a's to create "aabaaaa".
+	             Remove two 'a's from the second group of 'a's to create "aabaa".
+	             No three consecutive characters are equal, so return "aabaa".
+	
+	Example 3:
+	Input: s = "aab"
+	Output: "aab"
+	Explanation: No three consecutive characters are equal, so return "aab".
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s consists only of lowercase English letters."""
+
+    def makeFancyString(self, s: str) -> str:
+        stack = []
+        for ch in s: 
+            if len(stack) > 1 and stack[-2] == stack[-1] == ch: continue 
+            stack.append(ch)
+        return "".join(stack)
+
+
+    """1958. Check if Move is Legal (Medium)
+	You are given a 0-indexed 8 x 8 grid board, where board[r][c] represents 
+	the cell (r, c) on a game board. On the board, free cells are represented 
+	by '.', white cells are represented by 'W', and black cells are represented 
+	by 'B'. Each move in this game consists of choosing a free cell and 
+	changing it to the color you are playing as (either white or black). 
+	However, a move is only legal if, after changing it, the cell becomes the 
+	endpoint of a good line (horizontal, vertical, or diagonal). A good line is 
+	a line of three or more cells (including the endpoints) where the endpoints 
+	of the line are one color, and the remaining cells in the middle are the 
+	opposite color (no cells in the line are free). Given two integers rMove 
+	and cMove and a character color representing the color you are playing as 
+	(white or black), return true if changing cell (rMove, cMove) to color 
+	color is a legal move, or false if it is not legal.
+
+	Example 1:
+	Input: board = [[".",".",".","B",".",".",".","."],
+	                [".",".",".","W",".",".",".","."],
+	                [".",".",".","W",".",".",".","."],
+	                [".",".",".","W",".",".",".","."],
+	                ["W","B","B",".","W","W","W","B"],
+	                [".",".",".","B",".",".",".","."],
+	                [".",".",".","B",".",".",".","."],
+	                [".",".",".","W",".",".",".","."]], 
+	       rMove = 4, cMove = 3, color = "B"
+	Output: true
+	Explanation: '.', 'W', and 'B' are represented by the colors blue, white, 
+	             and black respectively, and cell (rMove, cMove) is marked with 
+	             an 'X'. The two good lines with the chosen cell as an endpoint 
+	             are annotated above with the red rectangles.
+	
+	Example 2:
+	Input: board = [[".",".",".",".",".",".",".","."],
+	                [".","B",".",".","W",".",".","."],
+	                [".",".","W",".",".",".",".","."],
+	                [".",".",".","W","B",".",".","."],
+	                [".",".",".",".",".",".",".","."],
+	                [".",".",".",".","B","W",".","."],
+	                [".",".",".",".",".",".","W","."],
+	                [".",".",".",".",".",".",".","B"]], 
+	       rMove = 4, cMove = 4, color = "W"
+	Output: false
+	Explanation: While there are good lines with the chosen cell as a middle 
+	             cell, there are no good lines with the chosen cell as an 
+	             endpoint.
+
+	Constraints:
+	* board.length == board[r].length == 8
+	* 0 <= rMove, cMove < 8
+	* board[rMove][cMove] == '.'
+	* color is either 'B' or 'W'."""
+
+    def checkMove(self, board: List[List[str]], rMove: int, cMove: int, color: str) -> bool:
+        for di, dj in (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1): 
+            i, j = rMove+di, cMove+dj
+            step = 0
+            while 0 <= i < 8 and 0 <= j < 8: 
+                if board[i][j] == color and step: return True 
+                if board[i][j] == "." or board[i][j] == color and not step: break 
+                i, j = i+di, j+dj
+                step += 1
+        return False
+
+
+    """1959. Minimum Total Space Wasted With K Resizing Operations (Medium)
+	You are currently designing a dynamic array. You are given a 0-indexed 
+	integer array nums, where nums[i] is the number of elements that will be in 
+	the array at time i. In addition, you are given an integer k, the maximum 
+	number of times you can resize the array (to any size). The size of the 
+	array at time t, sizet, must be at least nums[t] because there needs to be 
+	enough space in the array to hold all the elements. The space wasted at 
+	time t is defined as sizet - nums[t], and the total space wasted is the sum 
+	of the space wasted across every time t where 0 <= t < nums.length. Return 
+	the minimum total space wasted if you can resize the array at most k times.
+	Note: The array can have any size at the start and does not count towards 
+	the number of resizing operations.
+
+	Example 1:
+	Input: nums = [10,20], k = 0
+	Output: 10
+	Explanation: size = [20,20]. We can set the initial size to be 20. The 
+	             total wasted space is (20 - 10) + (20 - 20) = 10.
+	
+	Example 2:
+	Input: nums = [10,20,30], k = 1
+	Output: 10
+	Explanation: size = [20,20,30]. We can set the initial size to be 20 and 
+	             resize to 30 at time 2. The total wasted space is 
+	             (20 - 10) + (20 - 20) + (30 - 30) = 10.
+	
+	Example 3:
+	Input: nums = [10,20,15,30,20], k = 2
+	Output: 15
+	Explanation: size = [10,20,20,30,30]. We can set the initial size to 10, 
+	             resize to 20 at time 1, and resize to 30 at time 3. The total 
+	             wasted space is 
+	             (10 - 10) + (20 - 20) + (20 - 15) + (30 - 30) + (30 - 20) = 15.
+
+	Constraints:
+	* 1 <= nums.length <= 200
+	* 1 <= nums[i] <= 10^6
+	* 0 <= k <= nums.length - 1"""
+
+    def minSpaceWastedKResizing(self, nums: List[int], k: int) -> int:
+        
+        @cache
+        def fn(i, k): 
+            """Return min waste from i with k ops."""
+            if i == len(nums): return 0
+            if k < 0: return inf 
+            ans = inf
+            rmx = -inf # range max 
+            rsm = 0 # range sum 
+            for j in range(i, len(nums)): 
+                rmx = max(rmx, nums[j])
+                rsm += nums[j]
+                ans = min(ans, rmx*(j-i+1) - rsm + fn(j+1, k-1))
+            return ans 
+        
+        return fn(0, k)
+
+
+    """1960. Maximum Product of the Length of Two Palindromic Substrings (Hard)
+	You are given a 0-indexed string s and are tasked with finding two non-
+	intersecting palindromic substrings of odd length such that the product of 
+	their lengths is maximized. More formally, you want to choose four integers 
+	i, j, k, l such that 0 <= i <= j < k <= l < s.length and both the 
+	substrings s[i...j] and s[k...l] are palindromes and have odd lengths. 
+	s[i...j] denotes a substring from index i to index j inclusive. Return the 
+	maximum possible product of the lengths of the two non-intersecting 
+	palindromic substrings. A palindrome is a string that is the same forward 
+	and backward. A substring is a contiguous sequence of characters in a 
+	string.
+
+	Example 1:
+	Input: s = "ababbb"
+	Output: 9
+	Explanation: Substrings "aba" and "bbb" are palindromes with odd length. 
+	             product = 3 * 3 = 9.
+	
+	Example 2:
+	Input: s = "zaaaxbbby"
+	Output: 9
+	Explanation: Substrings "aaa" and "bbb" are palindromes with odd length. 
+	             product = 3 * 3 = 9.
+
+	Constraints:
+	* 2 <= s.length <= 10^5
+	* s consists of lowercase English letters."""
+
+    def maxProduct(self, s: str) -> int:
+        n = len(s)
+        
+        def fn(s): 
+            """Return length of longest odd palindroms ending at index."""
+            ans = [1]*n
+            hlen = [0]*n # half-length
+            center = right = 0 
+            for i in range(n): 
+                if i < right: hlen[i] = min(right - i, hlen[2*center - i])
+                while 0 <= i-1-hlen[i] and i+1+hlen[i] < len(s) and s[i-1-hlen[i]] == s[i+1+hlen[i]]: 
+                    hlen[i] += 1
+                    ans[i+hlen[i]] = max(ans[i+hlen[i]], 2*hlen[i]+1)
+                if right < i+hlen[i]: center, right = i, i+hlen[i]
+            for i in range(1, n): ans[i] = max(ans[i-1], ans[i])
+            return ans
+        
+        prefix = fn(s)
+        suffix = fn(s[::-1])
+        return max(prefix[i-1]*suffix[~i] for i in range(1, n))
+
+
     """1961. Check If String Is a Prefix of Array (Easy)
 	Given a string s and an array of strings words, determine whether s is a 
 	prefix string of words. A string s is a prefix string of words if s can be 
