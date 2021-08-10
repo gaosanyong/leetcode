4e7f0e5ac7d9e47290081e12961185d712187a64
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-10 11:08:10 -0400

    August LeetCoding Challenge 2021-08-10 (926)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7642,6 +7642,42 @@ public:
     }
 
 
+    /*926. Flip String to Monotone Increasing (Medium)
+	A binary string is monotone increasing if it consists of some number of 0's 
+	(possibly none), followed by some number of 1's (also possibly none). You 
+	are given a binary string s. You can flip s[i] changing it from 0 to 1 or 
+	from 1 to 0. Return the minimum number of flips to make s monotone 
+	increasing.
+
+	Example 1:
+	Input: s = "00110"
+	Output: 1
+	Explanation: We flip the last digit to get 00111.
+
+	Example 2:
+	Input: s = "010110"
+	Output: 2
+	Explanation: We flip to get 011111, or alternatively 000111.
+
+	Example 3:
+	Input: s = "00011000"
+	Output: 2
+	Explanation: We flip to get 00000000.
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s[i] is either '0' or '1'.*/
+
+    int minFlipsMonoIncr(string s) {
+        int ones = 0, flip = 0; 
+        for (auto& ch : s) {
+            if (ch == '1') ones += 1; 
+            else flip = min(ones, flip + 1); 
+        }
+        return flip; 
+    }
+
+
     /*929. Unique Email Addresses (Easy)
 	Every valid email consists of a local name and a domain name, separated by 
 	the '@' sign. Besides lowercase letters, the email may contain one or more 
