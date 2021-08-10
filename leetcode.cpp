ed4bc1fcc11e5298aa31fa81d32acf9649360ccb
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-09 22:36:17 -0400

    Update 1960

--- leetcode.cpp
+++ leetcode.cpp
@@ -16113,31 +16113,34 @@ public:
 	* s consists of lowercase English letters.*/
 
     long long maxProduct(string s) {
-        int n = s.size(); 
-        
-        function<vector<int>(string)> fn = [&](string s) {
-            vector<int> ans(n, 1), hlen(n, 0); 
-            int center = 0, right = 0; 
-            for (int i = 0; i < n; ++i) {
-                if (i < right) hlen[i] = min(right - i, hlen[2*center - i]); 
-                while (0 <= i-1-hlen[i] && i+1+hlen[i] < n && s[i-1-hlen[i]] == s[i+1+hlen[i]]) {
-                    ++hlen[i]; 
-                    ans[i+hlen[i]] = max(ans[i+hlen[i]], 2*hlen[i] + 1); 
-                }
-                if (right < i + hlen[i]) {
-                    center = i; 
-                    right = i + hlen[i]; 
-                }
+        int n = s.size(), center = 0, right = 0; 
+        vector<int> hlen(n, 0), prefix(n, 0), suffix(n, 0); 
+        for (int i = 0; i < n; ++i) {
+            if (i < right) hlen[i] = min(right - i, hlen[2*center - i]); 
+            while (0 <= i-1-hlen[i] && i+1+hlen[i] < n && s[i-1-hlen[i]] == s[i+1+hlen[i]]) ++hlen[i]; 
+            if (right < i + hlen[i]) {
+                center = i; 
+                right = i + hlen[i]; 
             }
-            for (int i = 1; i < n; ++i) ans[i] = max(ans[i-1], ans[i]); 
-            return ans; 
-        }; 
+        }
         
-        auto prefix = fn(s); 
-        reverse(s.begin(), s.end()); 
-        auto suffix = fn(s); 
+        for (int i = 0; i < n; ++i) {
+            prefix[i+hlen[i]] = max(prefix[i+hlen[i]], 2*hlen[i] + 1); 
+            suffix[i-hlen[i]] = max(suffix[i-hlen[i]], 2*hlen[i] + 1); 
+        }
+        
+        for (int i = 1; i < n; ++i) {
+            prefix[n-1-i] = max(prefix[n-1-i], prefix[n-i]-2); 
+            suffix[i] = max(suffix[i], suffix[i-1]-2); 
+        }
+        
+        for (int i = 1; i < n; ++i) {
+            prefix[i] = max(prefix[i-1], prefix[i]); 
+            suffix[n-1-i] = max(suffix[n-1-i], suffix[n-i]); 
+        }
+        
         long long ans = 0ll; 
-        for (int i = 1; i < n; ++i) ans = max(ans, (long long) prefix[i-1] * suffix[n-1-i]); 
+        for (int i = 1; i < n; ++i) ans = max(ans, (long long) prefix[i-1] * suffix[i]); 
         return ans; 
     }
 
