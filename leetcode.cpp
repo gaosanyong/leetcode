234c379b9190d70de5a267d8ac79ca63377a326c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-11 18:47:50 -0400

    Solve'em all (1966)

--- leetcode.cpp
+++ leetcode.cpp
@@ -16470,6 +16470,79 @@ public:
         }
         return ans; 
     }
+
+
+    /*1966. Binary Searchable Numbers in an Unsorted Array (Medium)
+	Consider a function that implements an algorithm similar to Binary Search. 
+	The function has two input parameters: sequence is a sequence of integers, 
+	and target is an integer value. The purpose of the function is to find if 
+	the target exists in the sequence. The pseudocode of the function is as 
+	follows:
+
+	func(sequence, target)
+	  while sequence is not empty
+	    randomly choose an element from sequence as the pivot
+	    if pivot = target, return true
+	    else if pivot < target, remove pivot and all elements to its left from the sequence
+	    else, remove pivot and all elements to its right from the sequence
+	  end while
+	  return false
+	
+	When the sequence is sorted, the function works correctly for all values. 
+	When the sequence is not sorted, the function does not work for all values, 
+	but may still work for some values. Given an integer array nums, 
+	representing the sequence, that contains unique numbers and may or may not 
+	be sorted, return the number of values that are guaranteed to be found 
+	using the function, for every possible pivot selection.
+
+	Example 1:
+	Input: nums = [7]
+	Output: 1
+	Explanation: Searching for value 7 is guaranteed to be found. Since the 
+	             sequence has only one element, 7 will be chosen as the pivot. 
+	             Because the pivot equals the target, the function will return 
+	             true.
+	
+	Example 2:
+	Input: nums = [-1,5,2]
+	Output: 1
+	Explanation: Searching for value -1 is guaranteed to be found. If -1 was 
+	             chosen as the pivot, the function would return true. If 5 was 
+	             chosen as the pivot, 5 and 2 would be removed. In the next 
+	             loop, the sequence would have only -1 and the function would 
+	             return true. If 2 was chosen as the pivot, 2 would be removed. 
+	             In the next loop, the sequence would have -1 and 5. No matter 
+	             which number was chosen as the next pivot, the function would 
+	             find -1 and return true. Searching for value 5 is NOT 
+	             guaranteed to be found. If 2 was chosen as the pivot, -1, 5 
+	             and 2 would be removed. The sequence would be empty and the 
+	             function would return false. Searching for value 2 is NOT 
+	             guaranteed to be found. If 5 was chosen as the pivot, 5 and 2 
+	             would be removed. In the next loop, the sequence would have 
+	             only -1 and the function would return false. Because only -1 
+	             is guaranteed to be found, you should return 1.
+	 
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* -10^5 <= nums[i] <= 10^5
+	* All the values of nums are unique.
+
+	Follow-up: If nums has duplicates, would you modify your algorithm? If so, 
+	           how?*/
+
+    int binarySearchableNumbers(vector<int>& nums) {
+        vector<int> suffix = {INT_MAX}; 
+        for (int i = nums.size()-1; i >= 0; --i) 
+            suffix.push_back(min(suffix.back(), nums[i])); 
+        reverse(suffix.begin(), suffix.end()); 
+        
+        int prefix = INT_MIN, ans = 0; 
+        for (int i = 0; i < nums.size(); ++i) {
+            prefix = max(prefix, nums[i]); 
+            if (prefix == nums[i] && nums[i] == suffix[i]) ++ans; 
+        }
+        return ans; 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -39060,6 +39060,77 @@ class Trie:
         return ans 
 
 
+    """1966. Binary Searchable Numbers in an Unsorted Array (Medium)
+	Consider a function that implements an algorithm similar to Binary Search. 
+	The function has two input parameters: sequence is a sequence of integers, 
+	and target is an integer value. The purpose of the function is to find if 
+	the target exists in the sequence. The pseudocode of the function is as 
+	follows:
+
+	func(sequence, target)
+	  while sequence is not empty
+	    randomly choose an element from sequence as the pivot
+	    if pivot = target, return true
+	    else if pivot < target, remove pivot and all elements to its left from the sequence
+	    else, remove pivot and all elements to its right from the sequence
+	  end while
+	  return false
+	
+	When the sequence is sorted, the function works correctly for all values. 
+	When the sequence is not sorted, the function does not work for all values, 
+	but may still work for some values. Given an integer array nums, 
+	representing the sequence, that contains unique numbers and may or may not 
+	be sorted, return the number of values that are guaranteed to be found 
+	using the function, for every possible pivot selection.
+
+	Example 1:
+	Input: nums = [7]
+	Output: 1
+	Explanation: Searching for value 7 is guaranteed to be found. Since the 
+	             sequence has only one element, 7 will be chosen as the pivot. 
+	             Because the pivot equals the target, the function will return 
+	             true.
+	
+	Example 2:
+	Input: nums = [-1,5,2]
+	Output: 1
+	Explanation: Searching for value -1 is guaranteed to be found. If -1 was 
+	             chosen as the pivot, the function would return true. If 5 was 
+	             chosen as the pivot, 5 and 2 would be removed. In the next 
+	             loop, the sequence would have only -1 and the function would 
+	             return true. If 2 was chosen as the pivot, 2 would be removed. 
+	             In the next loop, the sequence would have -1 and 5. No matter 
+	             which number was chosen as the next pivot, the function would 
+	             find -1 and return true. Searching for value 5 is NOT 
+	             guaranteed to be found. If 2 was chosen as the pivot, -1, 5 
+	             and 2 would be removed. The sequence would be empty and the 
+	             function would return false. Searching for value 2 is NOT 
+	             guaranteed to be found. If 5 was chosen as the pivot, 5 and 2 
+	             would be removed. In the next loop, the sequence would have 
+	             only -1 and the function would return false. Because only -1 
+	             is guaranteed to be found, you should return 1.
+	 
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* -10^5 <= nums[i] <= 10^5
+	* All the values of nums are unique.
+
+	Follow-up: If nums has duplicates, would you modify your algorithm? If so, 
+	           how?"""
+
+    def binarySearchableNumbers(self, nums: List[int]) -> int:
+        suffix = [inf]
+        for x in reversed(nums): suffix.append(min(suffix[-1], x))
+        suffix = suffix[::-1]
+        
+        ans = 0
+        prefix = -inf 
+        for i, x in enumerate(nums): 
+            prefix = max(prefix, x)
+            if prefix == x == suffix[i]: ans += 1
+        return ans 
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
