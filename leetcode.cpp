6e1ee47b78098bfa2b03e644556a1782f8b1d316
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-11 19:34:02 -0400

    Solve'em all (1940)

--- leetcode.cpp
+++ leetcode.cpp
@@ -15334,6 +15334,48 @@ public:
     }
 
 
+    /*1940. Longest Common Subsequence Between Sorted Arrays (Medium)
+	Given an array of integer arrays arrays where each arrays[i] is sorted in 
+	strictly increasing order, return an integer array representing the longest 
+	common subsequence between all the arrays. A subsequence is a sequence that 
+	can be derived from another sequence by deleting some elements (possibly 
+	none) without changing the order of the remaining elements.
+
+	Example 1:
+	Input: arrays = [[1,3,4],
+	                 [1,4,7,9]]
+	Output: [1,4]
+	Explanation: The longest common subsequence in the two arrays is [1,4].
+
+	Example 2:
+	Input: arrays = [[2,3,6,8],
+	                 [1,2,3,5,6,7,10],
+	                 [2,3,4,6,9]]
+	Output: [2,3,6]
+	Explanation: The longest common subsequence in all three arrays is [2,3,6].
+
+	Example 3:
+	Input: arrays = [[1,2,3,4,5],
+	                 [6,7,8]]
+	Output: []
+	Explanation: There is no common subsequence between the two arrays.
+
+	Constraints:
+	* 2 <= arrays.length <= 100
+	* 1 <= arrays[i].length <= 100
+	* 1 <= arrays[i][j] <= 100
+	* arrays[i] is sorted in strictly increasing order.*/
+
+    vector<int> longestCommomSubsequence(vector<vector<int>>& arrays) {
+        vector<int> ans; 
+        unordered_map<int, int> freq; 
+        for (auto& array : arrays) 
+            for (auto& x : array) 
+                if (++freq[x] == arrays.size()) ans.push_back(x); 
+        return ans; 
+    }
+
+
     /*1941. Check if All Characters Have Equal Number of Occurrences (Easy)
 	Given a string s, return true if s is a good string, or false otherwise. A 
 	string s is good if all the characters that appear in s have the same 
