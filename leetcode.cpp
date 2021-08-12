4e301d93349bb5c874a879fa82aa731eba7aa39d
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-12 12:32:52 -0400

    Solve'em all (1933)

--- leetcode.cpp
+++ leetcode.cpp
@@ -15153,6 +15153,50 @@ public:
     }
 
 
+    /*1933. Check if String Is Decomposable Into Value-Equal Substrings (Easy)
+	A value-equal string is a string where all characters are the same. 
+	* For example, "1111" and "33" are value-equal strings.
+	* In contrast, "123" is not a value-equal string.
+	Given a digit string s, decompose the string into some number of 
+	consecutive value-equal substrings where exactly one substring has a length 
+	of 2 and the remaining substrings have a length of 3. Return true if you 
+	can decompose s according to the above rules. Otherwise, return false. A 
+	substring is a contiguous sequence of characters in a string.
+
+	Example 1:
+	Input: s = "000111000"
+	Output: false
+	Explanation: s cannot be decomposed according to the rules because 
+	             ["000", "111", "000"] does not have a substring of length 2.
+	
+	Example 2:
+	Input: s = "00011111222"
+	Output: true
+	Explanation: s can be decomposed into ["000", "111", "11", "222"].
+
+	Example 3:
+	Input: s = "011100022233"
+	Output: false
+	Explanation: s cannot be decomposed according to the rules because of the 
+	             first '0'.
+
+	Constraints:
+	* 1 <= s.length <= 1000
+	* s consists of only digits '0' through '9'.*/
+
+    bool isDecomposable(string s) {
+        int ii = 0, found = 0; 
+        for (int i = 0; i <= s.size(); ++i) 
+            if (i == s.size() || (i and s[i-1] != s[i])) {
+                int r = (i - ii) % 3; 
+                ii = i; 
+                if (r == 2 && not found) found = 1; 
+                else if (r) return false; 
+            }
+        return found; 
+    }
+
+
     /*1935. Maximum Number of Words You Can Type (Easy)
 	There is a malfunctioning keyboard where some letter keys do not work. All 
 	other keys on the keyboard work properly. Given a string text of words 
--- leetcode.py
+++ leetcode.py
@@ -37831,6 +37831,46 @@ class Fenwick:
         return root
 
 
+    """1933. Check if String Is Decomposable Into Value-Equal Substrings (Easy)
+	A value-equal string is a string where all characters are the same. 
+	* For example, "1111" and "33" are value-equal strings.
+	* In contrast, "123" is not a value-equal string.
+	Given a digit string s, decompose the string into some number of 
+	consecutive value-equal substrings where exactly one substring has a length 
+	of 2 and the remaining substrings have a length of 3. Return true if you 
+	can decompose s according to the above rules. Otherwise, return false. A 
+	substring is a contiguous sequence of characters in a string.
+
+	Example 1:
+	Input: s = "000111000"
+	Output: false
+	Explanation: s cannot be decomposed according to the rules because 
+	             ["000", "111", "000"] does not have a substring of length 2.
+	
+	Example 2:
+	Input: s = "00011111222"
+	Output: true
+	Explanation: s can be decomposed into ["000", "111", "11", "222"].
+
+	Example 3:
+	Input: s = "011100022233"
+	Output: false
+	Explanation: s cannot be decomposed according to the rules because of the 
+	             first '0'.
+
+	Constraints:
+	* 1 <= s.length <= 1000
+	* s consists of only digits '0' through '9'."""
+
+    def isDecomposable(self, s: str) -> bool:
+        found = False 
+        for _, grp in groupby(s): 
+            r = len(list(grp)) % 3
+            if r == 2 and not found: found = True 
+            elif r != 0: return False 
+        return found
+
+
     """1935. Maximum Number of Words You Can Type (Easy)
 	There is a malfunctioning keyboard where some letter keys do not work. All 
 	other keys on the keyboard work properly. Given a string text of words 
