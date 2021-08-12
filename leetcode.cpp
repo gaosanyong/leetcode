32a5ee5cecb87186f93bd7e10b901ba80ebe1caa
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-12 17:57:23 -0400

    Solve'em all (1059)

--- leetcode.cpp
+++ leetcode.cpp
@@ -9454,6 +9454,74 @@ public:
     }
 
 
+    /*1059. All Paths from Source Lead to Destination (Medium)
+	Given the edges of a directed graph where edges[i] = [ai, bi] indicates 
+	there is an edge between nodes ai and bi, and two nodes source and 
+	destination of this graph, determine whether or not all paths starting from 
+	source eventually, end at destination, that is:
+	* At least one path exists from the source node to the destination node
+	* If a path exists from the source node to a node with no outgoing edges, 
+	  then that node is equal to destination.
+	* The number of possible paths from source to destination is a finite number.
+	Return true if and only if all roads from source lead to destination.
+
+	Example 1:
+	Input: n = 3, edges = [[0,1],[0,2]], source = 0, destination = 2
+	Output: false
+	Explanation: It is possible to reach and get stuck on both node 1 and node 2.
+
+	Example 2:
+	Input: n = 4, edges = [[0,1],[0,3],[1,2],[2,1]], source = 0, destination = 3
+	Output: false
+	Explanation: We have two possibilities: to end at node 3, or to loop over 
+	             node 1 and node 2 indefinitely.
+	
+	Example 3:
+	Input: n = 4, edges = [[0,1],[0,2],[1,3],[2,3]], source = 0, destination = 3
+	Output: true
+
+	Example 4:
+	Input: n = 3, edges = [[0,1],[1,1],[1,2]], source = 0, destination = 2
+	Output: false
+	Explanation: All paths from the source node end at the destination node, 
+	             but there are an infinite number of paths, such as 0-1-2, 
+	             0-1-1-2, 0-1-1-1-2, 0-1-1-1-1-2, and so on.
+	
+	Example 5:
+	Input: n = 2, edges = [[0,1],[1,1]], source = 0, destination = 1
+	Output: false
+	Explanation: There is infinite self-loop at destination node.
+
+	Constraints:
+	* 1 <= n <= 10^4
+	* 0 <= edges.length <= 10^4
+	* edges.length == 2
+	* 0 <= ai, bi <= n - 1
+	* 0 <= source <= n - 1
+	* 0 <= destination <= n - 1
+	* The given graph may have self-loops and parallel edges.*/
+
+    bool leadsToDestination(int n, vector<vector<int>>& edges, int source, int destination) {
+        unordered_map<int, vector<int>> graph; 
+        for (auto&& edge : edges) 
+            graph[edge[0]].push_back(edge[1]); 
+        
+        vector<int> color(n, 0); 
+        
+        function<bool(int)> fn = [&](int x) {
+            if (color[x]) return color[x] == 1; 
+            if (!graph[x].size()) return x == destination; 
+            color[x] = -1; 
+            for (auto&& xx : graph[x]) 
+                if (!fn(xx)) return false; 
+            color[x] = 1; 
+            return true; 
+        }; 
+        
+        return fn(source); 
+    }
+
+
     /*1074. Number of Submatrices That Sum to Target (Hard)
 	Given a matrix and a target, return the number of non-empty submatrices 
 	that sum to target. A submatrix x1, y1, x2, y2 is the set of all cells 
--- leetcode.py
+++ leetcode.py
@@ -19454,6 +19454,71 @@ class UnionFind:
         return f"{len(err)-k+sum(err[:k])-sum(err[k:]):.3f}"
 
 
+    """1059. All Paths from Source Lead to Destination (Medium)
+	Given the edges of a directed graph where edges[i] = [ai, bi] indicates 
+	there is an edge between nodes ai and bi, and two nodes source and 
+	destination of this graph, determine whether or not all paths starting from 
+	source eventually, end at destination, that is:
+	* At least one path exists from the source node to the destination node
+	* If a path exists from the source node to a node with no outgoing edges, 
+	  then that node is equal to destination.
+	* The number of possible paths from source to destination is a finite number.
+	Return true if and only if all roads from source lead to destination.
+
+	Example 1:
+	Input: n = 3, edges = [[0,1],[0,2]], source = 0, destination = 2
+	Output: false
+	Explanation: It is possible to reach and get stuck on both node 1 and node 2.
+
+	Example 2:
+	Input: n = 4, edges = [[0,1],[0,3],[1,2],[2,1]], source = 0, destination = 3
+	Output: false
+	Explanation: We have two possibilities: to end at node 3, or to loop over 
+	             node 1 and node 2 indefinitely.
+	
+	Example 3:
+	Input: n = 4, edges = [[0,1],[0,2],[1,3],[2,3]], source = 0, destination = 3
+	Output: true
+
+	Example 4:
+	Input: n = 3, edges = [[0,1],[1,1],[1,2]], source = 0, destination = 2
+	Output: false
+	Explanation: All paths from the source node end at the destination node, 
+	             but there are an infinite number of paths, such as 0-1-2, 
+	             0-1-1-2, 0-1-1-1-2, 0-1-1-1-1-2, and so on.
+	
+	Example 5:
+	Input: n = 2, edges = [[0,1],[1,1]], source = 0, destination = 1
+	Output: false
+	Explanation: There is infinite self-loop at destination node.
+
+	Constraints:
+	* 1 <= n <= 10^4
+	* 0 <= edges.length <= 10^4
+	* edges.length == 2
+	* 0 <= ai, bi <= n - 1
+	* 0 <= source <= n - 1
+	* 0 <= destination <= n - 1
+	* The given graph may have self-loops and parallel edges."""
+
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = {}
+        for u, v in edges: graph.setdefault(u, []).append(v)
+        
+        def fn(x): 
+            """Return True if all paths starting from x lead to destination."""
+            if x not in graph: return x == destination
+            if color[x]: return color[x] == 1
+            color[x] = -1 
+            for xx in graph[x]: 
+                if not fn(xx): return False 
+            color[x] = 1
+            return True 
+        
+        color = [0] * n 
+        return fn(source)
+
+
     """1064. Fixed Point (Easy)
 	Given an array of distinct integers arr, where arr is sorted in ascending 
 	order, return the smallest index i that satisfies arr[i] == i. If there is 
