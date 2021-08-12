a62bf8ea1e8d106089b49e410c3b37d0fae75212
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-12 12:03:29 -0400

    Solve'em all (1055)

--- leetcode.cpp
+++ leetcode.cpp
@@ -9292,6 +9292,55 @@ public:
     }
 
 
+    /*1055. Shortest Way to Form String (Medium)
+	A subsequence of a string is a new string that is formed from the original 
+	string by deleting some (can be none) of the characters without disturbing 
+	the relative positions of the remaining characters. (i.e., "ace" is a 
+	subsequence of "abcde" while "aec" is not). Given two strings source and 
+	target, return the minimum number of subsequences of source such that their 
+	concatenation equals target. If the task is impossible, return -1.
+
+	Example 1:
+	Input: source = "abc", target = "abcbc"
+	Output: 2
+	Explanation: The target "abcbc" can be formed by "abc" and "bc", which are 
+	             subsequences of source "abc".
+	
+	Example 2:
+	Input: source = "abc", target = "acdbc"
+	Output: -1
+	Explanation: The target string cannot be constructed from the subsequences 
+	             of source string due to the character "d" in target string.
+	
+	Example 3:
+	Input: source = "xyz", target = "xzyxz"
+	Output: 3
+	Explanation: The target string can be constructed as follows 
+	             "xz" + "y" + "xz".
+
+	Constraints:
+	* 1 <= source.length, target.length <= 1000
+	* source and target consist of lowercase English letters.*/
+
+    int shortestWay(string source, string target) {
+        unordered_map<char, vector<int>> locs; 
+        for (int i = 0; i < source.size(); ++i) 
+            locs[source[i]].push_back(i); 
+        
+        int ans = 1, i = 0; 
+        for (auto& x : target) {
+            if (locs.find(x) == locs.end()) return -1; 
+            auto it = lower_bound(locs[x].begin(), locs[x].end(), i); 
+            if (it == locs[x].end()) {
+                ++ans; 
+                it = locs[x].begin(); 
+            }
+            i = (*it) + 1; 
+        }
+        return ans; 
+    }
+
+
     /*1074. Number of Submatrices That Sum to Target (Hard)
 	Given a matrix and a target, return the number of non-empty submatrices 
 	that sum to target. A submatrix x1, y1, x2, y2 is the set of all cells 
--- leetcode.py
+++ leetcode.py
@@ -19264,6 +19264,52 @@ class UnionFind:
         return ans 
 
 
+    """1055. Shortest Way to Form String (Medium)
+	A subsequence of a string is a new string that is formed from the original 
+	string by deleting some (can be none) of the characters without disturbing 
+	the relative positions of the remaining characters. (i.e., "ace" is a 
+	subsequence of "abcde" while "aec" is not). Given two strings source and 
+	target, return the minimum number of subsequences of source such that their 
+	concatenation equals target. If the task is impossible, return -1.
+
+	Example 1:
+	Input: source = "abc", target = "abcbc"
+	Output: 2
+	Explanation: The target "abcbc" can be formed by "abc" and "bc", which are 
+	             subsequences of source "abc".
+	
+	Example 2:
+	Input: source = "abc", target = "acdbc"
+	Output: -1
+	Explanation: The target string cannot be constructed from the subsequences 
+	             of source string due to the character "d" in target string.
+	
+	Example 3:
+	Input: source = "xyz", target = "xzyxz"
+	Output: 3
+	Explanation: The target string can be constructed as follows 
+	             "xz" + "y" + "xz".
+
+	Constraints:
+	* 1 <= source.length, target.length <= 1000
+	* source and target consist of lowercase English letters."""
+
+    def shortestWay(self, source: str, target: str) -> int:
+        locs = {}
+        for i, x in enumerate(source): 
+            locs.setdefault(x, []).append(i)
+        
+        ans = i = 0
+        for x in target: 
+            if x not in locs: return -1 
+            k = bisect_left(locs[x], i) 
+            if k == len(locs[x]): 
+                k = 0
+                ans += 1
+            i = locs[x][k] + 1
+        return ans + 1
+
+
     """1056. Confusing Number (Easy)
 	Given a number N, return true if and only if it is a confusing number, 
 	which satisfies the following condition:
