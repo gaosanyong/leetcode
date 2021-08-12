9808740a2ef0f555d143f119b8d9fd98ae349979
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-12 15:29:47 -0400

    Solve'em all (10570

--- leetcode.cpp
+++ leetcode.cpp
@@ -9341,6 +9341,66 @@ public:
     }
 
 
+    /*1057. Campus Bikes (Medium)
+	On a campus represented on the X-Y plane, there are n workers and m bikes, 
+	with n <= m. You are given an array workers of length n where 
+	workers[i] = [xi, yi] is the position of the ith worker. You are also given 
+	an array bikes of length m where bikes[j] = [xj, yj] is the position of the 
+	jth bike. All the given positions are unique. Assign a bike to each worker. 
+	Among the available bikes and workers, we choose the (workeri, bikej) pair 
+	with the shortest Manhattan distance between each other and assign the bike 
+	to that worker. If there are multiple (workeri, bikej) pairs with the same 
+	shortest Manhattan distance, we choose the pair with the smallest worker 
+	index. If there are multiple ways to do that, we choose the pair with the 
+	smallest bike index. Repeat this process until there are no available 
+	workers. Return an array answer of length n, where answer[i] is the index 
+	(0-indexed) of the bike that the ith worker is assigned to. The Manhattan 
+	distance between two points p1 and p2 is 
+	Manhattan(p1, p2) = |p1.x - p2.x| + |p1.y - p2.y|.
+
+	Example 1:
+	Input: workers = [[0,0],[2,1]], bikes = [[1,2],[3,3]]
+	Output: [1,0]
+	Explanation: Worker 1 grabs Bike 0 as they are closest (without ties), and 
+	             Worker 0 is assigned Bike 1. So the output is [1, 0].
+	
+	Example 2:
+	Input: workers = [[0,0],[1,1],[2,0]], bikes = [[1,0],[2,2],[2,1]]
+	Output: [0,2,1]
+	Explanation: Worker 0 grabs Bike 0 at first. Worker 1 and Worker 2 share 
+	             the same distance to Bike 2, thus Worker 1 is assigned to Bike 
+	             2, and Worker 2 will take Bike 1. So the output is [0,2,1].
+
+	Constraints:
+	* n == workers.length
+	* m == bikes.length
+	* 1 <= n <= m <= 1000
+	* workers[i].length == bikes[j].length == 2
+	* 0 <= xi, yi < 1000
+	* 0 <= xj, yj < 1000
+	* All worker and bike locations are unique.*/
+
+    vector<int> assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
+        vector<vector<int>> vals; 
+        for (int i = 0; i < workers.size(); ++i) 
+            for (int j = 0; j < bikes.size(); ++j) {
+                int dist = abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1]); 
+                vals.push_back({dist, i, j}); 
+            }
+        
+        sort(vals.begin(), vals.end()); 
+        vector<int> ans(workers.size(), -1), seen(bikes.size(), 0); 
+        for (auto& x : vals) {
+            int i = x[1], j = x[2]; 
+            if (ans[i] == -1 && !seen[j]) {
+                ans[i] = j; 
+                seen[j] = 1; 
+            }
+        }
+        return ans; 
+    }
+
+
     /*1074. Number of Submatrices That Sum to Target (Hard)
 	Given a matrix and a target, return the number of non-empty submatrices 
 	that sum to target. A submatrix x1, y1, x2, y2 is the set of all cells 
--- leetcode.py
+++ leetcode.py
@@ -19355,6 +19355,60 @@ class UnionFind:
         return nn != N
 
 
+    """1057. Campus Bikes (Medium)
+	On a campus represented on the X-Y plane, there are n workers and m bikes, 
+	with n <= m. You are given an array workers of length n where 
+	workers[i] = [xi, yi] is the position of the ith worker. You are also given 
+	an array bikes of length m where bikes[j] = [xj, yj] is the position of the 
+	jth bike. All the given positions are unique. Assign a bike to each worker. 
+	Among the available bikes and workers, we choose the (workeri, bikej) pair 
+	with the shortest Manhattan distance between each other and assign the bike 
+	to that worker. If there are multiple (workeri, bikej) pairs with the same 
+	shortest Manhattan distance, we choose the pair with the smallest worker 
+	index. If there are multiple ways to do that, we choose the pair with the 
+	smallest bike index. Repeat this process until there are no available 
+	workers. Return an array answer of length n, where answer[i] is the index 
+	(0-indexed) of the bike that the ith worker is assigned to. The Manhattan 
+	distance between two points p1 and p2 is 
+	Manhattan(p1, p2) = |p1.x - p2.x| + |p1.y - p2.y|.
+
+	Example 1:
+	Input: workers = [[0,0],[2,1]], bikes = [[1,2],[3,3]]
+	Output: [1,0]
+	Explanation: Worker 1 grabs Bike 0 as they are closest (without ties), and 
+	             Worker 0 is assigned Bike 1. So the output is [1, 0].
+	
+	Example 2:
+	Input: workers = [[0,0],[1,1],[2,0]], bikes = [[1,0],[2,2],[2,1]]
+	Output: [0,2,1]
+	Explanation: Worker 0 grabs Bike 0 at first. Worker 1 and Worker 2 share 
+	             the same distance to Bike 2, thus Worker 1 is assigned to Bike 
+	             2, and Worker 2 will take Bike 1. So the output is [0,2,1].
+
+	Constraints:
+	* n == workers.length
+	* m == bikes.length
+	* 1 <= n <= m <= 1000
+	* workers[i].length == bikes[j].length == 2
+	* 0 <= xi, yi < 1000
+	* 0 <= xj, yj < 1000
+	* All worker and bike locations are unique."""
+
+    def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> List[int]:
+        vals = [] 
+        for i, (x, y) in enumerate(workers): 
+            for j, (xx, yy) in enumerate(bikes): 
+                vals.append((abs(x - xx) + abs(y - yy), i, j))
+        
+        ans = [-1] * len(workers)
+        seen = [False] * len(bikes)
+        for _, i, j in sorted(vals): 
+            if ans[i] == -1 and not seen[j]: 
+                ans[i] = j 
+                seen[j] = True 
+        return ans 
+
+
     """1064. Fixed Point (Easy)
 	Given an array of distinct integers arr, where arr is sorted in ascending 
 	order, return the smallest index i that satisfies arr[i] == i. If there is 
