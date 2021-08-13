17a337e4d5fe2d4ca845f116fe13a2053a34e370
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-13 12:02:38 -0400

    Solve'em all (1874)

--- leetcode.cpp
+++ leetcode.cpp
@@ -13280,6 +13280,42 @@ public:
     }
 
 
+    /*1874. Minimize Product Sum of Two Arrays (Medium)
+	The product sum of two equal-length arrays a and b is equal to the sum of 
+	a[i] * b[i] for all 0 <= i < a.length (0-indexed). For example, if 
+	a = [1,2,3,4] and b = [5,2,3,1], the product sum would be 
+	1*5 + 2*2 + 3*3 + 4*1 = 22. Given two arrays nums1 and nums2 of length n, 
+	return the minimum product sum if you are allowed to rearrange the order 
+	of the elements in nums1. 
+
+	Example 1:
+	Input: nums1 = [5,3,4,2], nums2 = [4,2,2,5]
+	Output: 40
+	Explanation: We can rearrange nums1 to become [3,5,4,2]. The product sum of 
+	             [3,5,4,2] and [4,2,2,5] is 3*4 + 5*2 + 4*2 + 2*5 = 40.
+	
+	Example 2:
+	Input: nums1 = [2,1,4,5,7], nums2 = [3,2,4,8,6]
+	Output: 65
+	Explanation: We can rearrange nums1 to become [5,7,4,1,2]. The product sum 
+	             of [5,7,4,1,2] and [3,2,4,8,6] is 
+	             5*3 + 7*2 + 4*4 + 1*8 + 2*6 = 65.
+
+	Constraints:
+	* n == nums1.length == nums2.length
+	* 1 <= n <= 10^5
+	* 1 <= nums1[i], nums2[i] <= 100*/
+
+    int minProductSum(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end()); 
+        sort(nums2.begin(), nums2.end(), greater<int>());
+        int ans = 0; 
+        for (int i = 0; i < nums1.size(); ++i)
+            ans += nums1[i] * nums2[i]; 
+        return ans; 
+    }
+
+
     /*1876. Substrings of Size Three with Distinct Characters (Easy)
 	A string is good if there are no repeated characters. Given a string s, 
 	return the number of good substrings of length three in s. Note that if 
--- leetcode.py
+++ leetcode.py
@@ -36188,6 +36188,36 @@ class Fenwick:
         return fn(1)
 
 
+    """1874. Minimize Product Sum of Two Arrays (Medium)
+	The product sum of two equal-length arrays a and b is equal to the sum of 
+	a[i] * b[i] for all 0 <= i < a.length (0-indexed). For example, if 
+	a = [1,2,3,4] and b = [5,2,3,1], the product sum would be 
+	1*5 + 2*2 + 3*3 + 4*1 = 22. Given two arrays nums1 and nums2 of length n, 
+	return the minimum product sum if you are allowed to rearrange the order 
+	of the elements in nums1. 
+
+	Example 1:
+	Input: nums1 = [5,3,4,2], nums2 = [4,2,2,5]
+	Output: 40
+	Explanation: We can rearrange nums1 to become [3,5,4,2]. The product sum of 
+	             [3,5,4,2] and [4,2,2,5] is 3*4 + 5*2 + 4*2 + 2*5 = 40.
+	
+	Example 2:
+	Input: nums1 = [2,1,4,5,7], nums2 = [3,2,4,8,6]
+	Output: 65
+	Explanation: We can rearrange nums1 to become [5,7,4,1,2]. The product sum 
+	             of [5,7,4,1,2] and [3,2,4,8,6] is 
+	             5*3 + 7*2 + 4*4 + 1*8 + 2*6 = 65.
+
+	Constraints:
+	* n == nums1.length == nums2.length
+	* 1 <= n <= 10^5
+	* 1 <= nums1[i], nums2[i] <= 100"""
+
+    def minProductSum(self, nums1: List[int], nums2: List[int]) -> int:
+        return sum(x1*x2 for x1, x2 in zip(sorted(nums1), sorted(nums2, reverse=True)))
+
+
     """1876. Substrings of Size Three with Distinct Characters (Easy)
 	A string is good if there are no repeated characters. Given a string s, 
 	return the number of good substrings of length three in s. Note that if 
