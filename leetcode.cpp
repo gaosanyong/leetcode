206e8d7f123403b3bea6d63a94bf070fc870dba9
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-13 10:57:17 -0400

    August LeetCoding Challenge Week2

--- leetcode.cpp
+++ leetcode.cpp
@@ -2344,6 +2344,48 @@ public:
     }
 
 
+    /*276. Paint Fence (Medium)
+	You are painting a fence of n posts with k different colors. You must paint 
+	the posts following these rules:
+	* Every post must be painted exactly one color.
+	* There cannot be three or more consecutive posts with the same color.
+	Given the two integers n and k, return the number of ways you can paint the 
+	fence.
+
+	Example 1:
+	Input: n = 3, k = 2
+	Output: 6
+	Explanation: All the possibilities are shown. Note that painting all the 
+	             posts red or all the posts green is invalid because there 
+	             cannot be three posts in a row with the same color.
+	
+	Example 2:
+	Input: n = 1, k = 1
+	Output: 1
+
+	Example 3:
+	Input: n = 7, k = 2
+	Output: 42
+
+	Constraints:
+	* 1 <= n <= 50
+	* 1 <= k <= 10^5
+	* The testcases are generated such that the answer is in the range 
+	  [0, 2^31 - 1] for the given n and k.*/
+
+    int numWays(int n, int k) {
+        if (n == 0) return 0; 
+        if (n == 1) return k; 
+        int f0 = k, f1 = k*k; 
+        for (int i = 2; i < n; ++i) {
+            int tmp = f1; 
+            f1 = (f0 + f1) * (k-1); 
+            f0 = tmp; 
+        }
+        return f1; 
+    }
+
+
     /*278. First Bad Version (Easy)
 	You are a product manager and currently leading a team to develop a new 
 	product. Unfortunately, the latest version of your product fails the 
