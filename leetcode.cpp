4cb0768b9696089f1f6532cf6ce904c63d2a4f51
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-13 08:08:15 -0400

    Solve'em all (1061)

--- leetcode.cpp
+++ leetcode.cpp
@@ -9564,6 +9564,79 @@ public:
     }
 
 
+    /*1061. Lexicographically Smallest Equivalent String (Medium)
+	You are given two strings of the same length s1 and s2 and a string baseStr.
+	We say s1[i] and s2[i] are equivalent characters.
+	* For example, if s1 = "abc" and s2 = "cde", then we have 'a' == 'c', 
+	  'b' == 'd', and 'c' == 'e'.
+	Equivalent characters follow the usual rules of any equivalence relation:
+	* Reflexivity: 'a' == 'a'.
+	* Symmetry: 'a' == 'b' implies 'b' == 'a'.
+	* Transitivity: 'a' == 'b' and 'b' == 'c' implies 'a' == 'c'.
+	For example, given the equivalency information from s1 = "abc" and 
+	s2 = "cde", "acd" and "aab" are equivalent strings of baseStr = "eed", and 
+	"aab" is the lexicographically smallest equivalent string of baseStr. 
+	Return the lexicographically smallest equivalent string of baseStr by using 
+	the equivalency information from s1 and s2.
+
+	Example 1:
+	Input: s1 = "parker", s2 = "morris", baseStr = "parser"
+	Output: "makkek"
+	Explanation: Based on the equivalency information in s1 and s2, we can 
+	             group their characters as [m,p], [a,o], [k,r,s], [e,i]. The 
+	             characters in each group are equivalent and sorted in 
+	             lexicographical order. So the answer is "makkek".
+	
+	Example 2:
+	Input: s1 = "hello", s2 = "world", baseStr = "hold"
+	Output: "hdld"
+	Explanation: Based on the equivalency information in s1 and s2, we can 
+	             group their characters as [h,w], [d,e,o], [l,r]. So only the 
+	             second letter 'o' in baseStr is changed to 'd', the answer is 
+	             "hdld".
+	
+	Example 3:
+	Input: s1 = "leetcode", s2 = "programs", baseStr = "sourcecode"
+	Output: "aauaaaaada"
+	Explanation: We group the equivalent characters in s1 and s2 as 
+	             [a,o,e,r,s,c], [l,p], [g,t] and [d,m], thus all letters in 
+	             baseStr except 'u' and 'd' are transformed to 'a', the answer 
+	             is "aauaaaaada".
+
+	Constraints:
+	* 1 <= s1.length, s2.length, baseStr <= 1000
+	* s1.length == s2.length
+	* s1, s2, and baseStr consist of lowercase English letters.
+	
+class UnionFind {
+unordered_map<char, char> parent; 
+public: 
+    char find(char p) {
+        if (!parent.count(p)) parent[p] = p; 
+        if (p != parent[p]) 
+            parent[p] = find(parent[p]); 
+        return parent[p]; 
+    }
+    
+    void connect(char p, char q) {
+        char prt = find(p), qrt = find(q); 
+        if (prt < qrt) swap(prt, qrt); 
+        parent[prt] = qrt; 
+    }
+}; */
+
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        UnionFind* uf = new UnionFind(); 
+        for (int i = 0; i < s1.size(); ++i) 
+            uf->connect(s1[i], s2[i]); 
+        string ans; 
+        for (auto& ch : baseStr) 
+            ans.push_back(uf->find(ch)); 
+        delete uf; // release heap memory
+        return ans; 
+    }
+
+
     /*1074. Number of Submatrices That Sum to Target (Hard)
 	Given a matrix and a target, return the number of non-empty submatrices 
 	that sum to target. A submatrix x1, y1, x2, y2 is the set of all cells 
--- leetcode.py
+++ leetcode.py
@@ -19559,6 +19559,70 @@ class UnionFind:
         return nums[0] + k + lo - 1
 
 
+    """1061. Lexicographically Smallest Equivalent String (Medium)
+	You are given two strings of the same length s1 and s2 and a string baseStr.
+	We say s1[i] and s2[i] are equivalent characters.
+	* For example, if s1 = "abc" and s2 = "cde", then we have 'a' == 'c', 
+	  'b' == 'd', and 'c' == 'e'.
+	Equivalent characters follow the usual rules of any equivalence relation:
+	* Reflexivity: 'a' == 'a'.
+	* Symmetry: 'a' == 'b' implies 'b' == 'a'.
+	* Transitivity: 'a' == 'b' and 'b' == 'c' implies 'a' == 'c'.
+	For example, given the equivalency information from s1 = "abc" and 
+	s2 = "cde", "acd" and "aab" are equivalent strings of baseStr = "eed", and 
+	"aab" is the lexicographically smallest equivalent string of baseStr. 
+	Return the lexicographically smallest equivalent string of baseStr by using 
+	the equivalency information from s1 and s2.
+
+	Example 1:
+	Input: s1 = "parker", s2 = "morris", baseStr = "parser"
+	Output: "makkek"
+	Explanation: Based on the equivalency information in s1 and s2, we can 
+	             group their characters as [m,p], [a,o], [k,r,s], [e,i]. The 
+	             characters in each group are equivalent and sorted in 
+	             lexicographical order. So the answer is "makkek".
+	
+	Example 2:
+	Input: s1 = "hello", s2 = "world", baseStr = "hold"
+	Output: "hdld"
+	Explanation: Based on the equivalency information in s1 and s2, we can 
+	             group their characters as [h,w], [d,e,o], [l,r]. So only the 
+	             second letter 'o' in baseStr is changed to 'd', the answer is 
+	             "hdld".
+	
+	Example 3:
+	Input: s1 = "leetcode", s2 = "programs", baseStr = "sourcecode"
+	Output: "aauaaaaada"
+	Explanation: We group the equivalent characters in s1 and s2 as 
+	             [a,o,e,r,s,c], [l,p], [g,t] and [d,m], thus all letters in 
+	             baseStr except 'u' and 'd' are transformed to 'a', the answer 
+	             is "aauaaaaada".
+
+	Constraints:
+	* 1 <= s1.length, s2.length, baseStr <= 1000
+	* s1.length == s2.length
+	* s1, s2, and baseStr consist of lowercase English letters.
+
+class UnionFind: 
+    def __init__(self): 
+        self.parent = dict(zip(ascii_lowercase, ascii_lowercase))
+        
+    def find(self, p): 
+        if p != self.parent[p]: 
+            self.parent[p] = self.find(self.parent[p])
+        return self.parent[p]
+    
+    def union(self, p, q): 
+        prt, qrt = self.find(p), self.find(q)
+        if prt < qrt: prt, qrt = qrt, prt
+        self.parent[prt] = qrt"""
+        
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        uf = UnionFind()
+        for c1, c2 in zip(s1, s2): uf.union(c1, c2)
+        return "".join(uf.find(ch) for ch in baseStr)
+
+
     """1064. Fixed Point (Easy)
 	Given an array of distinct integers arr, where arr is sorted in ascending 
 	order, return the smallest index i that satisfies arr[i] == i. If there is 
