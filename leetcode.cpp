b491d81e099c99616f4508778f0d6708c278a0d6
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-13 09:54:17 -0400

    August LeetCoding Challenge 2021-08-13 (73)

--- leetcode.cpp
+++ leetcode.cpp
@@ -819,6 +819,47 @@ public:
     }
 
 
+    /*73. Set Matrix Zeroes (Medium)
+	Given an m x n integer matrix matrix, if an element is 0, set its entire 
+	row and column to 0's, and return the matrix. You must do it in place.
+
+	Example 1:
+	Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+	Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+	Example 2:
+	Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+	Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+	Constraints:
+	* m == matrix.length
+	* n == matrix[0].length
+	* 1 <= m, n <= 200
+	* -2^31 <= matrix[i][j] <= 2^31 - 1
+
+	Follow up:
+	* A straightforward solution using O(mn) space is probably a bad idea.
+	* A simple improvement uses O(m + n) space, but still not the best solution.
+	* Could you devise a constant space solution?*/
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size(), flag = 0; 
+        for (int i = 0; i < m; ++i) {
+            if (matrix[i][0] == 0) flag = 1; 
+            for (int j = 1; j < n; ++j) 
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0; 
+        }
+        
+        for (int i = m-1; i >= 0; --i) {
+            for (int j = n-1; j >= 1; --j) 
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) 
+                    matrix[i][j] = 0; 
+            if (flag) matrix[i][0] = 0; 
+        }
+    }
+
+
 	/*83. Remove Duplicates from Sorted List (Easy)
 	Given the head of a sorted linked list, delete all duplicates such that 
 	each element appears only once. Return the linked list sorted as well.
@@ -9607,7 +9648,7 @@ public:
 	* 1 <= s1.length, s2.length, baseStr <= 1000
 	* s1.length == s2.length
 	* s1, s2, and baseStr consist of lowercase English letters.
-	
+
 class UnionFind {
 unordered_map<char, char> parent; 
 public: 
