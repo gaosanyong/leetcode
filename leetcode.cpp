0303582cd16a8cc0c21bed43873b53408b21f8d9
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-13 12:11:20 -0400

    Solve'em all (1490)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10159,6 +10159,42 @@ public:
     }
 
 
+    /*1490. Clone N-ary Tree (Medium)
+	Given a root of an N-ary tree, return a deep copy (clone) of the tree. Each 
+	node in the n-ary tree contains a val (int) and a list (List[Node]) of its 
+	children.
+
+	class Node {
+	    public int val;
+	    public List<Node> children;
+	}
+	
+	Nary-Tree input serialization is represented in their level order traversal, 
+	each group of children is separated by the null value (See examples).
+
+	Example 1:
+	Input: root = [1,null,3,2,4,null,5,6]
+	Output: [1,null,3,2,4,null,5,6]
+
+	Example 2:
+	Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+	Output: [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+
+	Constraints:
+	* The depth of the n-ary tree is less than or equal to 1000.
+	* The total number of nodes is between [0, 104].
+
+	Follow up: Can your solution work for the graph problem?*/
+
+    Node* cloneTree(Node* root) {
+        if (!root) return nullptr; 
+        Node* ans = new Node(root->val); 
+        for (auto& child : root->children) 
+            ans->children.push_back(cloneTree(child));
+        return ans; 
+    }
+
+
     /*1642. Furthest Building You Can Reach (Medium)
 	You are given an integer array heights representing the heights of 
 	buildings, some bricks, and some ladders. You start your journey from 
