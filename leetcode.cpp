f52c983f6e64fa657478c70d7b763cfa8f8b3558
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-16 14:22:06 -0400

    Solve'em all (1971)

--- leetcode.cpp
+++ leetcode.cpp
@@ -17597,6 +17597,60 @@ public:
         }
         return -1; 
     }
+
+
+    /*1971. Find if Path Exists in Graph (Easy)
+	There is a bi-directional graph with n vertices, where each vertex is 
+	labeled from 0 to n - 1 (inclusive). The edges in the graph are represented 
+	as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a 
+	bi-directional edge between vertex ui and vertex vi. Every vertex pair is 
+	connected by at most one edge, and no vertex has an edge to itself. You 
+	want to determine if there is a valid path that exists from vertex start to 
+	vertex end. Given edges and the integers n, start, and end, return true if 
+	there is a valid path from start to end, or false otherwise.
+
+	Example 1:
+	Input: n = 3, edges = [[0,1],[1,2],[2,0]], start = 0, end = 2
+	Output: true
+	Explanation: There are two paths from vertex 0 to vertex 2:
+	             - 0 → 1 → 2
+	             - 0 → 2
+	
+	Example 2:
+	Input: n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4,3]], start = 0, end = 5
+	Output: false
+	Explanation: There is no path from vertex 0 to vertex 5.
+
+	Constraints:
+	* 1 <= n <= 2 * 10^5
+	* 0 <= edges.length <= 2 * 10^5
+	* edges[i].length == 2
+	* 1 <= ui, vi <= n - 1
+	* ui != vi
+	* 1 <= start, end <= n - 1
+	* There are no duplicate edges.
+	* There are no self edges.*/
+
+    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
+        unordered_map<int, vector<int>> graph; 
+        for (auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]); 
+            graph[edge[1]].push_back(edge[0]); 
+        }
+        stack<int> stk; 
+        stk.push(start); 
+        unordered_set<int> seen = {start}; 
+        while (stk.size()) {
+            int n = stk.top(); stk.pop(); 
+            if (n == end) return true; 
+            for (auto& nn : graph[n]) 
+                if (!seen.count(nn)) {
+                    seen.insert(nn); 
+                    stk.push(nn); 
+                }
+        }
+        return false; 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -39962,6 +39962,56 @@ class UnionFind:
                     uf.union(x, xx)
 
 
+    """1971. Find if Path Exists in Graph (Easy)
+	There is a bi-directional graph with n vertices, where each vertex is 
+	labeled from 0 to n - 1 (inclusive). The edges in the graph are represented 
+	as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a 
+	bi-directional edge between vertex ui and vertex vi. Every vertex pair is 
+	connected by at most one edge, and no vertex has an edge to itself. You 
+	want to determine if there is a valid path that exists from vertex start to 
+	vertex end. Given edges and the integers n, start, and end, return true if 
+	there is a valid path from start to end, or false otherwise.
+
+	Example 1:
+	Input: n = 3, edges = [[0,1],[1,2],[2,0]], start = 0, end = 2
+	Output: true
+	Explanation: There are two paths from vertex 0 to vertex 2:
+	             - 0 → 1 → 2
+	             - 0 → 2
+	
+	Example 2:
+	Input: n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4,3]], start = 0, end = 5
+	Output: false
+	Explanation: There is no path from vertex 0 to vertex 5.
+
+	Constraints:
+	* 1 <= n <= 2 * 10^5
+	* 0 <= edges.length <= 2 * 10^5
+	* edges[i].length == 2
+	* 1 <= ui, vi <= n - 1
+	* ui != vi
+	* 1 <= start, end <= n - 1
+	* There are no duplicate edges.
+	* There are no self edges."""
+
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        graph = {}
+        for u, v in edges: 
+            graph.setdefault(u, []).append(v)
+            graph.setdefault(v, []).append(u)
+        
+        seen = {start}
+        stack = [start]
+        while stack: 
+            n = stack.pop()
+            if n == end: return True 
+            for nn in graph.get(n, []): 
+                if nn not in seen: 
+                    seen.add(nn)
+                    stack.append(nn)
+        return False 
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
