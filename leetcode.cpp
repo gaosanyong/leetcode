e8d3326c3f7fb4995b3f616297b704fc7ec8e34b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-17 17:33:44 -0400

    Solve'em all (1612)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10587,6 +10587,58 @@ public:
     }
 
 
+    /*1612. Check If Two Expression Trees are Equivalent (Medium)
+	A binary expression tree is a kind of binary tree used to represent 
+	arithmetic expressions. Each node of a binary expression tree has either 
+	zero or two children. Leaf nodes (nodes with 0 children) correspond to 
+	operands (variables), and internal nodes (nodes with two children) 
+	correspond to the operators. In this problem, we only consider the '+' 
+	operator (i.e. addition). You are given the roots of two binary expression 
+	trees, root1 and root2. Return true if the two binary expression trees are 
+	equivalent. Otherwise, return false. Two binary expression trees are 
+	equivalent if they evaluate to the same value regardless of what the 
+	variables are set to. 
+
+	Follow up: What will you change in your solution if the tree also supports 
+	           the '-' operator (i.e. subtraction)?
+
+	Example 1:
+	Input: root1 = [x], root2 = [x]
+	Output: true
+
+	Example 2:
+	Input: root1 = [+,a,+,null,null,b,c], root2 = [+,+,a,b,c]
+	Output: true
+	Explaination: a + (b + c) == (b + c) + a
+
+	Example 3:
+	Input: root1 = [+,a,+,null,null,b,c], root2 = [+,+,a,b,d]
+	Output: false
+	Explaination: a + (b + c) != (b + d) + a
+
+	Constraints:
+	* The number of nodes in both trees are equal, odd and, in the range [1, 4999].
+	* Node.val is '+' or a lower-case English letter.
+	* It's guaranteed that the tree given is a valid binary expression tree.*/
+
+    bool checkEquivalence(Node* root1, Node* root2) {
+        unordered_map<char, int> freq; 
+        
+        function<void(Node*, int)> fn = [&] (Node* x, int k) {
+            if (x) {
+                freq[x->val] += k; 
+                if (freq[x->val] == 0) freq.erase(x->val); 
+                fn(x->left, k); 
+                fn(x->right, k); 
+            }
+        }; 
+        
+        fn(root1, 1); 
+        fn(root2, -1); 
+        return freq.empty(); 
+    }
+
+
     /*1642. Furthest Building You Can Reach (Medium)
 	You are given an integer array heights representing the heights of 
 	buildings, some bricks, and some ladders. You start your journey from 
--- leetcode.py
+++ leetcode.py
@@ -27978,6 +27978,55 @@ class UnionFind:
         return self.minimumOneBitOperations((b>>1)^b^n) + b
 
 
+    """1612. Check If Two Expression Trees are Equivalent (Medium)
+	A binary expression tree is a kind of binary tree used to represent 
+	arithmetic expressions. Each node of a binary expression tree has either 
+	zero or two children. Leaf nodes (nodes with 0 children) correspond to 
+	operands (variables), and internal nodes (nodes with two children) 
+	correspond to the operators. In this problem, we only consider the '+' 
+	operator (i.e. addition). You are given the roots of two binary expression 
+	trees, root1 and root2. Return true if the two binary expression trees are 
+	equivalent. Otherwise, return false. Two binary expression trees are 
+	equivalent if they evaluate to the same value regardless of what the 
+	variables are set to. 
+
+	Follow up: What will you change in your solution if the tree also supports 
+	           the '-' operator (i.e. subtraction)?
+
+	Example 1:
+	Input: root1 = [x], root2 = [x]
+	Output: true
+
+	Example 2:
+	Input: root1 = [+,a,+,null,null,b,c], root2 = [+,+,a,b,c]
+	Output: true
+	Explaination: a + (b + c) == (b + c) + a
+
+	Example 3:
+	Input: root1 = [+,a,+,null,null,b,c], root2 = [+,+,a,b,d]
+	Output: false
+	Explaination: a + (b + c) != (b + d) + a
+
+	Constraints:
+	* The number of nodes in both trees are equal, odd and, in the range [1, 4999].
+	* Node.val is '+' or a lower-case English letter.
+	* It's guaranteed that the tree given is a valid binary expression tree."""
+
+    def checkEquivalence(self, root1: 'Node', root2: 'Node') -> bool:
+        freq = defaultdict(int)
+        
+        def fn(x, k): 
+            if not x: return 
+            freq[x.val] += k
+            if freq[x.val] == 0: freq.pop(x.val)
+            fn(x.left, k)
+            fn(x.right, k)
+                
+        fn(root1, 1)
+        fn(root2, -1)
+        return not freq
+
+
     """1618. Maximum Font to Fit a Sentence in a Screen (Medium)
 	You are given a string text. We want to display text on a screen of width w 
 	and height h. You can choose any font size from array fonts, which contains 
