615455d6d4acf9abefe6e89407f285bfad50229c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-17 16:01:45 -0400

    Solve'em all (1836)

--- leetcode.py
+++ leetcode.py
@@ -35284,6 +35284,46 @@ class Fenwick:
         return reduce(xor, arr1) & reduce(xor, arr2)
 
 
+    """1836. Remove Duplicates From an Unsorted Linked List (Medium)
+	Given the head of a linked list, find all the values that appear more than 
+	once in the list and delete the nodes that have any of those values. Return 
+	the linked list after the deletions.
+
+	Example 1:
+	Input: head = [1,2,3,2]
+	Output: [1,3]
+	Explanation: 2 appears twice in the linked list, so all 2's should be 
+	             deleted. After deleting all 2's, we are left with [1,3].
+	
+	Example 2:
+	Input: head = [2,1,1,2]
+	Output: []
+	Explanation: 2 and 1 both appear twice. All the elements should be deleted.
+
+	Example 3:
+	Input: head = [3,2,2,1,3,2,4]
+	Output: [1,4]
+	Explanation: 3 appears twice and 2 appears three times. After deleting all 
+	             3's and 2's, we are left with [1,4].
+
+	Constraints:
+	* The number of nodes in the list is in the range [1, 10^5]
+	* 1 <= Node.val <= 10^5"""
+
+    def deleteDuplicatesUnsorted(self, head: ListNode) -> ListNode:
+        freq = defaultdict(int)
+        node = head
+        while node: 
+            freq[node.val] += 1
+            node = node.next 
+        
+        dummy = node = ListNode(next=head)
+        while node.next: 
+            if freq[node.next.val] > 1: node.next = node.next.next
+            else: node = node.next 
+        return dummy.next
+
+
     """1837. Sum of Digits in Base K (Easy)
 	Given an integer n (in base 10) and a base k, return the sum of the digits 
 	of n after converting n from base 10 to base k. After converting, each 
