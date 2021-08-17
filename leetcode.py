cd3a2b39a5dbfa620ce6a1ab855f28a375a763ef
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-17 17:43:02 -0400

    Solve'em all (702)

--- leetcode.py
+++ leetcode.py
@@ -13416,6 +13416,45 @@ class Solution:
         return max(dfs(i, j) for i in range(m) for j in range(n))
 
 
+    """702. Search in a Sorted Array of Unknown Size (Medium)
+	This is an interactive problem. You have a sorted array of unique elements 
+	and an unknown size. You do not have an access to the array but you can use 
+	the ArrayReader interface to access it. You can call ArrayReader.get(i) 
+	that:
+	* returns the value at the ith index (0-indexed) of the secret array (i.e., 
+	  secret[i]), or
+	* returns 2^31 - 1 if the i is out of the boundary of the array.
+	You are also given an integer target. Return the index k of the hidden 
+	array where secret[k] == target or return -1 otherwise. You must write an 
+	algorithm with O(log n) runtime complexity.
+
+	Example 1:
+	Input: secret = [-1,0,3,5,9,12], target = 9
+	Output: 4
+	Explanation: 9 exists in secret and its index is 4.
+
+	Example 2:
+	Input: secret = [-1,0,3,5,9,12], target = 2
+	Output: -1
+	Explanation: 2 does not exist in secret so return -1.
+
+	Constraints:
+	* 1 <= secret.length <= 10^4
+	* -10^4 <= secret[i], target <= 10^4
+	* secret is sorted in a strictly increasing order."""
+
+    def search(self, reader, target):
+        lo, hi = 0, 10_000
+        while lo <= hi: 
+            mid = lo + hi >> 1
+            val = reader.get(mid)
+            if val < target: 
+                lo = mid + 1
+            elif val == target: return mid 
+            else: hi = mid - 1
+        return -1
+
+
     """709. To Lower Case (Easy)
 	Given a string s, return the string after replacing every uppercase letter 
 	with the same lowercase letter.
