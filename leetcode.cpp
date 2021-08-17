ccb95e3920dbb20fe3f19c84d018561e8ca0a9d3
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-17 18:02:38 -0400

    Solve'em all (1101)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10034,6 +10034,82 @@ public:
     }
 
 
+    /*1101. The Earliest Moment When Everyone Become Friends (Medium)
+	There are n people in a social group labeled from 0 to n - 1. You are given 
+	an array logs where logs[i] = [timestampi, xi, yi] indicates that xi and yi 
+	will be friends at the time timestampi. Friendship is symmetric. That means 
+	if a is friends with b, then b is friends with a. Also, person a is 
+	acquainted with a person b if a is friends with b, or a is a friend of 
+	someone acquainted with b. Return the earliest time for which every person 
+	became acquainted with every other person. If there is no such earliest 
+	time, return -1.
+
+	Example 1:
+	Input: logs = [[20190101,0,1],[20190104,3,4],[20190107,2,3],[20190211,1,5],
+	               [20190224,2,4],[20190301,0,3],[20190312,1,2],[20190322,4,5]], 
+	               n = 6
+	Output: 20190301
+	Explanation: 
+	The first event occurs at timestamp = 20190101 and after 0 and 1 become friends we have the following friendship groups [0,1], [2], [3], [4], [5].
+	The second event occurs at timestamp = 20190104 and after 3 and 4 become friends we have the following friendship groups [0,1], [2], [3,4], [5].
+	The third event occurs at timestamp = 20190107 and after 2 and 3 become friends we have the following friendship groups [0,1], [2,3,4], [5].
+	The fourth event occurs at timestamp = 20190211 and after 1 and 5 become friends we have the following friendship groups [0,1,5], [2,3,4].
+	The fifth event occurs at timestamp = 20190224 and as 2 and 4 are already friends anything happens.
+	The sixth event occurs at timestamp = 20190301 and after 0 and 3 become friends we have that all become friends.
+	
+	Example 2:
+	Input: logs = [[0,2,0],[1,0,1],[3,0,3],[4,1,2],[7,3,1]], n = 4
+	Output: 3
+
+	Constraints:
+	* 2 <= n <= 100
+	* 1 <= logs.length <= 10^4
+	* logs[i].length == 3
+	* 0 <= timestampi <= 10^9
+	* 0 <= xi, yi <= n - 1
+	* xi != yi
+	* All the values timestampi are unique.
+	* All the pairs (xi, yi) occur at most one time in the input.
+
+class UnionFind {
+    vector<int> parent, rank; 
+public: 
+    UnionFind(int n) {
+        parent.resize(n); 
+        iota(parent.begin(), parent.end(), 0); 
+        rank = vector<int>(n, 1); 
+    }
+    
+    int find(int p) {
+        if (p != parent[p]) 
+            parent[p] = find(parent[p]); 
+        return parent[p]; 
+    }
+    
+    bool connect(int p, int q) {
+        int prt = find(p), qrt = find(q); 
+        if (prt == qrt) return false; 
+        if (rank[prt] > rank[qrt]) swap(prt, qrt); 
+        parent[prt] = qrt; 
+        rank[qrt] += rank[prt]; 
+        return true; 
+    }
+};*/
+
+    int earliestAcq(vector<vector<int>>& logs, int n) {
+        int ans = -1; 
+        UnionFind* uf = new UnionFind(n); 
+        sort(logs.begin(), logs.end()); 
+        for (auto& log : logs) 
+            if (uf->connect(log[1], log[2]) && --n == 1) {
+                ans = log[0]; 
+                break; 
+            } 
+        delete uf; 
+        return ans; 
+    }
+
+
     /*1209. Remove All Adjacent Duplicates in String II (Medium)
 	Given a string s, a k duplicate removal consists of choosing k adjacent and 
 	equal letters from s and removing them causing the left and the right side 
--- leetcode.py
+++ leetcode.py
@@ -13448,8 +13448,7 @@ class Solution:
         while lo <= hi: 
             mid = lo + hi >> 1
             val = reader.get(mid)
-            if val < target: 
-                lo = mid + 1
+            if val < target: lo = mid + 1
             elif val == target: return mid 
             else: hi = mid - 1
         return -1
@@ -20473,6 +20472,71 @@ class UnionFind:
                 if freq[s[i-k]] == 0: freq.pop(s[i-k])
             if i+1 >= k and len(freq) == k: ans += 1
         return ans 
+
+
+    """1101. The Earliest Moment When Everyone Become Friends (Medium)
+	There are n people in a social group labeled from 0 to n - 1. You are given 
+	an array logs where logs[i] = [timestampi, xi, yi] indicates that xi and yi 
+	will be friends at the time timestampi. Friendship is symmetric. That means 
+	if a is friends with b, then b is friends with a. Also, person a is 
+	acquainted with a person b if a is friends with b, or a is a friend of 
+	someone acquainted with b. Return the earliest time for which every person 
+	became acquainted with every other person. If there is no such earliest 
+	time, return -1.
+
+	Example 1:
+	Input: logs = [[20190101,0,1],[20190104,3,4],[20190107,2,3],[20190211,1,5],
+	               [20190224,2,4],[20190301,0,3],[20190312,1,2],[20190322,4,5]], 
+	               n = 6
+	Output: 20190301
+	Explanation: 
+	The first event occurs at timestamp = 20190101 and after 0 and 1 become friends we have the following friendship groups [0,1], [2], [3], [4], [5].
+	The second event occurs at timestamp = 20190104 and after 3 and 4 become friends we have the following friendship groups [0,1], [2], [3,4], [5].
+	The third event occurs at timestamp = 20190107 and after 2 and 3 become friends we have the following friendship groups [0,1], [2,3,4], [5].
+	The fourth event occurs at timestamp = 20190211 and after 1 and 5 become friends we have the following friendship groups [0,1,5], [2,3,4].
+	The fifth event occurs at timestamp = 20190224 and as 2 and 4 are already friends anything happens.
+	The sixth event occurs at timestamp = 20190301 and after 0 and 3 become friends we have that all become friends.
+	
+	Example 2:
+	Input: logs = [[0,2,0],[1,0,1],[3,0,3],[4,1,2],[7,3,1]], n = 4
+	Output: 3
+
+	Constraints:
+	* 2 <= n <= 100
+	* 1 <= logs.length <= 10^4
+	* logs[i].length == 3
+	* 0 <= timestampi <= 10^9
+	* 0 <= xi, yi <= n - 1
+	* xi != yi
+	* All the values timestampi are unique.
+	* All the pairs (xi, yi) occur at most one time in the input.
+
+class UnionFind: 
+    
+    def __init__(self, n): 
+        self.parent = list(range(n))
+        self.rank = [1] * n
+        
+    def find(self, p): 
+        if p != self.parent[p]: 
+            self.parent[p] = self.find(self.parent[p])
+        return self.parent[p]
+    
+    
+    def union(self, p, q): 
+        prt, qrt = self.find(p), self.find(q)
+        if prt == qrt: return False 
+        if self.rank[prt] > self.rank[qrt]: prt, qrt = qrt, prt
+        self.parent[prt] = qrt
+        self.rank[qrt] += self.rank[prt]
+        return True"""
+        
+    def earliestAcq(self, logs: List[List[int]], n: int) -> int:
+        uf = UnionFind(n)
+        for t, u, v in sorted(logs): 
+            if uf.union(u, v): n -= 1
+            if n == 1: return t
+        return -1 
 
 
     """1103. Distribute Candies to People (Easy)
