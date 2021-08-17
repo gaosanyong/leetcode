4078dfa8b89e87442b3207a78d9dd615c1a2d943
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-17 18:46:23 -0400

    Solve'em all (1907)

--- leetcode.sql
+++ leetcode.sql
@@ -3209,6 +3209,57 @@ WHERE YEAR(time_stamp) = 2020
 GROUP BY user_id
 
 
+/*1907. Count Salary Categories (Medium)
+SQL Schema
+Table: Accounts
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| account_id  | int  |
+| income      | int  |
++-------------+------+
+account_id is the primary key for this table. Each row contains information 
+about the monthly income for one bank account.
+
+Write an SQL query to report the number of bank accounts of each salary 
+category. The salary categories are:
+* "Low Salary": All the salaries strictly less than $20000.
+* "Average Salary": All the salaries in the inclusive range [$20000, $50000].
+* "High Salary": All the salaries strictly greater than $50000.
+The result table must contain all three categories. If there are no accounts in 
+a category, then report 0. Return the result table in any order. The query 
+result format is in the following example.
+
+Accounts table:
++------------+--------+
+| account_id | income |
++------------+--------+
+| 3          | 108939 |
+| 2          | 12747  |
+| 8          | 87709  |
+| 6          | 91796  |
++------------+--------+
+
+Result table:
++----------------+----------------+
+| category       | accounts_count |
++----------------+----------------+
+| Low Salary     | 1              |
+| Average Salary | 0              |
+| High Salary    | 3              |
++----------------+----------------+
+
+Low Salary: Account 2.
+Average Salary: No accounts.
+High Salary: Accounts 3, 6, and 8.*/
+
+SELECT "Low Salary" As category, COUNT(*) AS accounts_count FROM Accounts WHERE income < 20000
+UNION 
+SELECT "Average Salary" AS category, COUNT(*) AS accounts_count FROM Accounts WHERE 20000 <= income AND income <= 50000
+UNION
+SELECT "High Salary" AS category, COUNT(*) AS accounts_count FROM Accounts WHERE 50000 < income
+
+
 /*1934. Confirmation Rate (Medium)
 SQL Schema
 Table: Signups
