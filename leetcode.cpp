6557a399f7b0b1e889856bf4ab9d0971e0fb866a
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-17 15:02:07 -0400

    Solve'em all (1522)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10538,6 +10538,55 @@ public:
     }
 
 
+    /*1522. Diameter of N-Ary Tree (Medium)
+	Given a root of an N-ary tree, you need to compute the length of the 
+	diameter of the tree. The diameter of an N-ary tree is the length of the 
+	longest path between any two nodes in the tree. This path may or may not 
+	pass through the root. (Nary-Tree input serialization is represented in 
+	their level order traversal, each group of children is separated by the 
+	null value.)
+
+	Example 1:
+	Input: root = [1,null,3,2,4,null,5,6]
+	Output: 3
+	Explanation: Diameter is shown in red color.
+
+	Example 2:
+	Input: root = [1,null,2,null,3,4,null,5,null,6]
+	Output: 4
+
+	Example 3:
+	Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,
+	               null,12,null,13,null,null,14]
+	Output: 7
+
+	Constraints:
+	* The depth of the n-ary tree is less than or equal to 1000.
+	* The total number of nodes is between [1, 10^4].*/
+
+    int diameter(Node* root) {
+        int ans = 0; 
+        
+        function<int(Node*)> fn = [&](Node* node) {
+            if (!node) return 0; 
+            int mx0 = 0, mx1 = 0; 
+            for (auto& child : node->children) {
+                int val = fn(child); 
+                if (val >= mx0) {
+                    mx1 = mx0; 
+                    mx0 = val; 
+                } else if (val > mx1) 
+                    mx1 = val; 
+            }
+            ans = max(ans, mx0 + mx1); 
+            return 1 + mx0; 
+        }; 
+        
+        fn(root); 
+        return ans; 
+    }
+
+
     /*1642. Furthest Building You Can Reach (Medium)
 	You are given an integer array heights representing the heights of 
 	buildings, some bricks, and some ladders. You start your journey from 
@@ -12074,13 +12123,13 @@ public:
 
 
     /*1830. Minimum Number of Operations to Make String Sorted (Hard)
-	You are given a string s (0-indexed)​​​​​​. You are asked to perform the 
-	following operation on s​​​​​​ until you get a sorted string:
+	You are given a string s (0-indexed). You are asked to perform the 
+	following operation on s until you get a sorted string:
 	* Find the largest index i such that 1 <= i < s.length and s[i] < s[i - 1].
 	* Find the largest index j such that i <= j < s.length and s[k] < s[i - 1] 
 	  for all the possible values of k in the range [i, j] inclusive.
-	* Swap the two characters at indices i - 1​​​​ and j​​​​​.
-	* Reverse the suffix starting at index i​​​​​​.
+	* Swap the two characters at indices i - 1 and j.
+	* Reverse the suffix starting at index i.
 	Return the number of operations needed to make the string sorted. Since the 
 	answer can be too large, return it modulo 109 + 7.
 
@@ -12111,7 +12160,7 @@ public:
 
 	Constraints:
 	* 1 <= s.length <= 3000
-	* s​​​​​​ consists only of lowercase English letters.*/
+	* s consists only of lowercase English letters.*/
 
     int makeStringSorted(string s) {
         int freq[26] = {}, n = size(s), MOD = 1'000'000'007; 
--- leetcode.py
+++ leetcode.py
@@ -26592,6 +26592,51 @@ class UnionFind:
         return ans 
 
 
+    """1522. Diameter of N-Ary Tree (Medium)
+	Given a root of an N-ary tree, you need to compute the length of the 
+	diameter of the tree. The diameter of an N-ary tree is the length of the 
+	longest path between any two nodes in the tree. This path may or may not 
+	pass through the root. (Nary-Tree input serialization is represented in 
+	their level order traversal, each group of children is separated by the 
+	null value.)
+
+	Example 1:
+	Input: root = [1,null,3,2,4,null,5,6]
+	Output: 3
+	Explanation: Diameter is shown in red color.
+
+	Example 2:
+	Input: root = [1,null,2,null,3,4,null,5,null,6]
+	Output: 4
+
+	Example 3:
+	Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,
+	               null,12,null,13,null,null,14]
+	Output: 7
+
+	Constraints:
+	* The depth of the n-ary tree is less than or equal to 1000.
+	* The total number of nodes is between [1, 10^4]."""
+
+    def diameter(self, root: 'Node') -> int:
+        
+        def fn(node): 
+            """Return node's height."""
+            nonlocal ans 
+            if not node: return 0 
+            mx1 = mx2 = 0 
+            for child in node.children: 
+                val = fn(child)
+                if val >= mx1: mx1, mx2 = val, mx1
+                elif val > mx2: mx2 = val 
+            ans = max(ans, mx1 + mx2)
+            return mx1 + 1
+        
+        ans = 0
+        fn(root)
+        return ans 
+
+
     """1523. Count Odd Numbers in an Interval Range (Easy)
 	Given two non-negative integers low and high. Return the count of odd 
 	numbers between low and high (inclusive).
