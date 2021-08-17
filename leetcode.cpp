673464f171308e1e5720a024303b0c0beb43277b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-17 14:12:39 -0400

    Solve'em all (723)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4944,6 +4944,95 @@ public:
     }
 
 
+    /*723. Candy Crush (Medium)
+	This question is about implementing a basic elimination algorithm for Candy 
+	Crush. Given an m x n integer array board representing the grid of candy 
+	where board[i][j] represents the type of candy. A value of board[i][j] == 0 
+	represents that the cell is empty. The given board represents the state of 
+	the game following the player's move. Now, you need to restore the board to 
+	a stable state by crushing candies according to the following rules:
+	* If three or more candies of the same type are adjacent vertically or 
+	  horizontally, crush them all at the same time - these positions become 
+	  empty.
+	* After crushing all candies simultaneously, if an empty space on the board 
+	  has candies on top of itself, then these candies will drop until they hit 
+	  a candy or bottom at the same time. No new candies will drop outside the 
+	  top boundary.
+	* After the above steps, there may exist more candies that can be crushed. 
+	  If so, you need to repeat the above steps.
+	* If there does not exist more candies that can be crushed (i.e., the board 
+	  is stable), then return the current board.
+	You need to perform the above rules until the board becomes stable, then 
+	return the stable board.
+
+	Example 1:
+	Input: board = [[110,  5,112,113, 114],
+	                [210,211,  5,213, 214],
+	                [310,311,  3,313, 314],
+	                [410,411,412,  5, 414],
+	                [  5,  1,512,  3,   3],
+	                [610,  4,  1,613, 614],
+	                [710,  1,  2,713, 714],
+	                [810,  1,  2,  1,   1],
+	                [  1,  1,  2,  2,   2],
+	                [  4,  1,  4,  4,1014]]
+	Output: [[  0,  0,  0,  0,   0],
+	         [  0,  0,  0,  0,   0],
+	         [  0,  0,  0,  0,   0],
+	         [110,  0,  0,  0, 114],
+	         [210,  0,  0,  0, 214],
+	         [310,  0,  0,113, 314],
+	         [410,  0,  0,213, 414],
+	         [610,211,112,313, 614],
+	         [710,311,412,613, 714],
+	         [810,411,512,713,1014]]
+	Example 2:
+
+	Input: board = [[1,3,5,5,2],
+	                [3,4,3,3,1],
+	                [3,2,4,5,2],
+	                [2,4,4,5,5],
+	                [1,4,4,1,1]]
+	Output: [[1,3,0,0,0],
+	         [3,4,0,5,2],
+	         [3,2,0,3,1],
+	         [2,4,0,5,2],
+	         [1,4,3,1,1]]
+
+	Constraints:
+	* m == board.length
+	* n == board[i].length
+	* 3 <= m, n <= 50
+	* 1 <= board[i][j] <= 2000*/
+
+    vector<vector<int>> candyCrush(vector<vector<int>>& board) {
+        int m = board.size(), n = board[0].size(); 
+        
+        while (true) {
+            vector<pair<int, int>> vals; 
+            for (int i = 0; i < m; ++i) 
+                for (int j = 0; j < n; ++j) 
+                    if (board[i][j]) 
+                        if ((0 <= i-2 && board[i-2][j] == board[i-1][j] && board[i-1][j] == board[i][j]) 
+                            || (0 <= i-1 && i+1 < m && board[i-1][j] == board[i][j] && board[i][j] == board[i+1][j]) 
+                            || (i+2 < m && board[i][j] == board[i+1][j] && board[i+1][j] == board[i+2][j]) 
+                            || (0 <= j-2 && board[i][j-2] == board[i][j-1] && board[i][j-1] == board[i][j]) 
+                            || (0 <= j-1 && j+1 < n && board[i][j-1] == board[i][j] && board[i][j] == board[i][j+1]) 
+                            || (j+2 < n && board[i][j] == board[i][j+1] && board[i][j+1] == board[i][j+2])) 
+                            vals.emplace_back(i, j);
+            if (vals.empty()) break; 
+            for (auto& [i, j] : vals) board[i][j] = 0; 
+            
+            for (int j = 0; j < n; ++j) {
+                int ii = m-1; 
+                for (int i = m-1; i >= 0; --i) 
+                    if (board[i][j]) swap(board[ii--][j], board[i][j]); 
+            }
+        }
+        return board; 
+    }
+
+
     /*724. Find Pivot Index (Easy)
 	Given an array of integers nums, calculate the pivot index of this array. 
 	The pivot index is the index where the sum of all the numbers strictly to 
--- leetcode.py
+++ leetcode.py
@@ -13529,6 +13529,95 @@ class UnionFind:
         return ans 
 
 
+    """723. Candy Crush (Medium)
+	This question is about implementing a basic elimination algorithm for Candy 
+	Crush. Given an m x n integer array board representing the grid of candy 
+	where board[i][j] represents the type of candy. A value of board[i][j] == 0 
+	represents that the cell is empty. The given board represents the state of 
+	the game following the player's move. Now, you need to restore the board to 
+	a stable state by crushing candies according to the following rules:
+	* If three or more candies of the same type are adjacent vertically or 
+	  horizontally, crush them all at the same time - these positions become 
+	  empty.
+	* After crushing all candies simultaneously, if an empty space on the board 
+	  has candies on top of itself, then these candies will drop until they hit 
+	  a candy or bottom at the same time. No new candies will drop outside the 
+	  top boundary.
+	* After the above steps, there may exist more candies that can be crushed. 
+	  If so, you need to repeat the above steps.
+	* If there does not exist more candies that can be crushed (i.e., the board 
+	  is stable), then return the current board.
+	You need to perform the above rules until the board becomes stable, then 
+	return the stable board.
+
+	Example 1:
+	Input: board = [[110,  5,112,113, 114],
+	                [210,211,  5,213, 214],
+	                [310,311,  3,313, 314],
+	                [410,411,412,  5, 414],
+	                [  5,  1,512,  3,   3],
+	                [610,  4,  1,613, 614],
+	                [710,  1,  2,713, 714],
+	                [810,  1,  2,  1,   1],
+	                [  1,  1,  2,  2,   2],
+	                [  4,  1,  4,  4,1014]]
+	Output: [[  0,  0,  0,  0,   0],
+	         [  0,  0,  0,  0,   0],
+	         [  0,  0,  0,  0,   0],
+	         [110,  0,  0,  0, 114],
+	         [210,  0,  0,  0, 214],
+	         [310,  0,  0,113, 314],
+	         [410,  0,  0,213, 414],
+	         [610,211,112,313, 614],
+	         [710,311,412,613, 714],
+	         [810,411,512,713,1014]]
+	Example 2:
+
+	Input: board = [[1,3,5,5,2],
+	                [3,4,3,3,1],
+	                [3,2,4,5,2],
+	                [2,4,4,5,5],
+	                [1,4,4,1,1]]
+	Output: [[1,3,0,0,0],
+	         [3,4,0,5,2],
+	         [3,2,0,3,1],
+	         [2,4,0,5,2],
+	         [1,4,3,1,1]]
+
+	Constraints:
+	* m == board.length
+	* n == board[i].length
+	* 3 <= m, n <= 50
+	* 1 <= board[i][j] <= 2000"""
+
+    def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
+        m, n = len(board), len(board[0]) 
+        
+        while True: 
+            vals = []
+            for i in range(m): 
+                for j in range(n): 
+                    if board[i][j]: 
+                        if 0 <= i-2 and board[i-2][j] == board[i-1][j] == board[i][j] \
+                        or 0 <= i-1 and i+1 < m and board[i-1][j] == board[i][j] == board[i+1][j] \
+                        or i+2 < m and board[i][j] == board[i+1][j] == board[i+2][j] \
+                        or 0 <= j-2 and board[i][j-2] == board[i][j-1] == board[i][j] \
+                        or 0 <= j-1 and j+1 < n and board[i][j-1] == board[i][j] == board[i][j+1] \
+                        or j+2 < n and board[i][j] == board[i][j+1] == board[i][j+2]: vals.append((i, j))
+                            
+            if not vals: break
+            for i, j in vals: board[i][j] = 0
+            
+            # precipitate 
+            for j in range(n):
+                ii = m-1 
+                for i in reversed(range(m)): 
+                    if board[i][j]: 
+                        board[ii][j], board[i][j] = board[i][j], board[ii][j]
+                        ii -= 1
+        return board 
+
+
     """725. Split Linked List in Parts (Medium)
 	Given a (singly) linked list with head node root, write a function to split 
 	the linked list into k consecutive linked list "parts". The length of each 
