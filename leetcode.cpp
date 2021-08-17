7863323819db21bb0dc63ae6aa7e6822ab852a47
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-17 11:11:26 -0400

    August LeetCoding Challenge 2021-08-17 (1448)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10153,6 +10153,53 @@ public:
     }
 
 
+    /*1448. Count Good Nodes in Binary Tree (Medium)
+	Given a binary tree root, a node X in the tree is named good if in the path 
+	from root to X there are no nodes with a value greater than X. Return the 
+	number of good nodes in the binary tree.
+
+	Example 1:
+	Input: root = [3,1,4,3,null,1,5]
+	Output: 4
+	Explanation: Nodes in blue are good. Root Node (3) is always a good node.
+	Node 4 -> (3,4) is the maximum value in the path starting from the root.
+	Node 5 -> (3,4,5) is the maximum value in the path
+	Node 3 -> (3,1,3) is the maximum value in the path.
+	
+	Example 2:
+	Input: root = [3,3,null,4,2]
+	Output: 3
+	Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+	Example 3:
+	Input: root = [1]
+	Output: 1
+	Explanation: Root is considered as good.
+
+	Constraints:
+	* The number of nodes in the binary tree is in the range [1, 10^5].
+	* Each node's value is between [-10^4, 10^4].*/
+
+    int goodNodes(TreeNode* root) {
+        int ans = 0; 
+        if (root) {
+            stack<pair<TreeNode*, int>> stk; 
+            stk.emplace(root, root->val); 
+            while (stk.size()) {
+                auto [node, val] = stk.top(); 
+                stk.pop(); 
+                if (node->val >= val) {
+                    ++ans; 
+                    val = node->val; 
+                }
+                if (node->left) stk.emplace(node->left, val); 
+                if (node->right) stk.emplace(node->right, val); 
+            }
+        }
+        return ans; 
+    }
+
+
     /*1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts (Medium)
 	Given a rectangular cake with height h and width w, and two arrays of 
 	integers horizontalCuts and verticalCuts where horizontalCuts[i] is the 
