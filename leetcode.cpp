ea39b36683c92b283f040dce5f9872170a209f62
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-18 14:27:31 -0400

    Solve'em all (1586)

--- leetcode.cpp
+++ leetcode.cpp
@@ -19063,6 +19063,92 @@ public:
 };
 
 
+/*1586. Binary Search Tree Iterator II (Medium)
+Implement the BSTIterator class that represents an iterator over the in-order 
+traversal of a binary search tree (BST):
+* BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. 
+  The root of the BST is given as part of the constructor. The pointer should 
+  be initialized to a non-existent number smaller than any element in the BST.
+* boolean hasNext() Returns true if there exists a number in the traversal to 
+  the right of the pointer, otherwise returns false.
+* int next() Moves the pointer to the right, then returns the number at the 
+  pointer.
+* boolean hasPrev() Returns true if there exists a number in the traversal to 
+  the left of the pointer, otherwise returns false.
+* int prev() Moves the pointer to the left, then returns the number at the 
+  pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the 
+first call to next() will return the smallest element in the BST. You may 
+assume that next() and prev() calls will always be valid. That is, there will 
+be at least a next/previous number in the in-order traversal when next()/prev() 
+is called.
+
+Example 1:
+Input: ["BSTIterator", "next", "next", "prev", "next", "hasNext", "next", "next", "next", "hasNext", "hasPrev", "prev", "prev"]
+       [[[7, 3, 15, null, null, 9, 20]], [null], [null], [null], [null], [null], [null], [null], [null], [null], [null], [null], [null]]
+Output: [null, 3, 7, 3, 7, true, 9, 15, 20, false, true, 15, 9]
+Explanation: 
+// The underlined element is where the pointer currently is.
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]); // state is   [3, 7, 9, 15, 20]
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 3
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 7
+bSTIterator.prev(); // state becomes [3, 7, 9, 15, 20], return 3
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 7
+bSTIterator.hasNext(); // return true
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 9
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 15
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 20
+bSTIterator.hasNext(); // return false
+bSTIterator.hasPrev(); // return true
+bSTIterator.prev(); // state becomes [3, 7, 9, 15, 20], return 15
+bSTIterator.prev(); // state becomes [3, 7, 9, 15, 20], return 9
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 10^5].
+* 0 <= Node.val <= 10^6
+* At most 105 calls will be made to hasNext, next, hasPrev, and prev.
+
+Follow up: Could you solve the problem without precalculating the values of the 
+           tree?*/
+
+class BSTIterator {
+    vector<int> vals; 
+    int i = -1; 
+public:
+    BSTIterator(TreeNode* root) {
+        TreeNode* node = root; 
+        stack<TreeNode*> stk; 
+        while (stk.size() || node) {
+            if (node) {
+                stk.push(node); 
+                node = node->left; 
+            } else {
+                node = stk.top(); 
+                stk.pop(); 
+                vals.push_back(node->val); 
+                node = node->right; 
+            }
+        }
+    }
+    
+    bool hasNext() {
+        return i+1 < vals.size(); 
+    }
+    
+    int next() {
+        return vals[++i]; 
+    }
+    
+    bool hasPrev() {
+        return 0 <= i-1; 
+    }
+    
+    int prev() {
+        return vals[--i]; 
+    }
+};
+
+
 /*1797. Design Authentication Manager (Medium)
 There is an authentication system that works with authentication tokens. For 
 each session, the user will receive a new authentication token that will expire 
--- leetcode.py
+++ leetcode.py
@@ -43479,6 +43479,86 @@ class SubrectangleQueries:
         return self.rectangle[row][col]
 
 
+"""1586. Binary Search Tree Iterator II (Medium)
+Implement the BSTIterator class that represents an iterator over the in-order 
+traversal of a binary search tree (BST):
+* BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. 
+  The root of the BST is given as part of the constructor. The pointer should 
+  be initialized to a non-existent number smaller than any element in the BST.
+* boolean hasNext() Returns true if there exists a number in the traversal to 
+  the right of the pointer, otherwise returns false.
+* int next() Moves the pointer to the right, then returns the number at the 
+  pointer.
+* boolean hasPrev() Returns true if there exists a number in the traversal to 
+  the left of the pointer, otherwise returns false.
+* int prev() Moves the pointer to the left, then returns the number at the 
+  pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the 
+first call to next() will return the smallest element in the BST. You may 
+assume that next() and prev() calls will always be valid. That is, there will 
+be at least a next/previous number in the in-order traversal when next()/prev() 
+is called.
+
+Example 1:
+Input: ["BSTIterator", "next", "next", "prev", "next", "hasNext", "next", "next", "next", "hasNext", "hasPrev", "prev", "prev"]
+       [[[7, 3, 15, null, null, 9, 20]], [null], [null], [null], [null], [null], [null], [null], [null], [null], [null], [null], [null]]
+Output: [null, 3, 7, 3, 7, true, 9, 15, 20, false, true, 15, 9]
+Explanation: 
+// The underlined element is where the pointer currently is.
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]); // state is   [3, 7, 9, 15, 20]
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 3
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 7
+bSTIterator.prev(); // state becomes [3, 7, 9, 15, 20], return 3
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 7
+bSTIterator.hasNext(); // return true
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 9
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 15
+bSTIterator.next(); // state becomes [3, 7, 9, 15, 20], return 20
+bSTIterator.hasNext(); // return false
+bSTIterator.hasPrev(); // return true
+bSTIterator.prev(); // state becomes [3, 7, 9, 15, 20], return 15
+bSTIterator.prev(); // state becomes [3, 7, 9, 15, 20], return 9
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 10^5].
+* 0 <= Node.val <= 10^6
+* At most 105 calls will be made to hasNext, next, hasPrev, and prev.
+
+Follow up: Could you solve the problem without precalculating the values of the 
+           tree?"""
+
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.i = -1 
+        self.vals = []
+        self.stack = []
+        while root: 
+            self.stack.append(root)
+            root = root.left 
+
+    def hasNext(self) -> bool:
+        return self.i+1 < len(self.vals) or self.stack
+
+    def next(self) -> int:
+        self.i += 1
+        if self.i == len(self.vals): 
+            node = self.stack.pop()
+            self.vals.append(node.val)
+            node = node.right 
+            while node: 
+                self.stack.append(node)
+                node = node.left 
+        return self.vals[self.i]
+
+    def hasPrev(self) -> bool:
+        return 0 <= self.i-1
+
+    def prev(self) -> int:
+        self.i -= 1
+        return self.vals[self.i]
+
+
 """1603. Design Parking System (Easy)
 Design a parking system for a parking lot. The parking lot has three kinds of 
 parking spaces: big, medium, and small, with a fixed number of slots for each 
