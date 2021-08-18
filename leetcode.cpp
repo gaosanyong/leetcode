c9b34580cc3702cb0e0c140ad1bc6ad08ddc9a9d
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-18 11:47:17 -0400

    Solve'em all (1794)

--- leetcode.cpp
+++ leetcode.cpp
@@ -11235,6 +11235,52 @@ public:
     }
 
 
+    /*1794. Count Pairs of Equal Substrings With Minimum Difference (Medium)
+	You are given two strings firstString and secondString that are 0-indexed 
+	and consist only of lowercase English letters. Count the number of index 
+	quadruples (i,j,a,b) that satisfy the following conditions:
+	* 0 <= i <= j < firstString.length
+	* 0 <= a <= b < secondString.length
+	* The substring of firstString that starts at the ith character and ends at 
+	  the jth character (inclusive) is equal to the substring of secondString 
+	  that starts at the ath character and ends at the bth character (inclusive).
+	* j - a is the minimum possible value among all quadruples that satisfy the 
+	  previous conditions.
+	Return the number of such quadruples.
+
+	Example 1:
+	Input: firstString = "abcd", secondString = "bccda"
+	Output: 1
+	Explanation: The quadruple (0,0,4,4) is the only one that satisfies all the 
+	             conditions and minimizes j - a.
+	
+	Example 2:
+	Input: firstString = "ab", secondString = "cd"
+	Output: 0
+	Explanation: There are no quadruples satisfying all the conditions.
+
+	Constraints:
+	* 1 <= firstString.length, secondString.length <= 2 * 10^5
+	* Both strings consist only of lowercase English letters.*/
+
+    int countQuadruples(string firstString, string secondString) {
+        unordered_map<char, int> prefix, suffix; 
+        for (int i = firstString.size()-1; i >= 0; --i) prefix[firstString[i]] = i; 
+        for (int i = 0; i < secondString.size(); ++i) suffix[secondString[i]] = i; 
+        
+        int most = INT_MIN, ans = 0; 
+        for (auto&& ch : "abcdefghijklmnopqrstuvwxyz") 
+            if (prefix.count(ch) && suffix.count(ch)) {
+                int val = suffix[ch] - prefix[ch]; 
+                if (val > most) {
+                    most = val; 
+                    ans = 1; 
+                } else if (val == most) ++ans; 
+            }
+        return ans; 
+    }
+
+
     /*1796. Second Largest Digit in a String (Easy)
 	Given an alphanumeric string s, return the second largest numerical digit 
 	that appears in s, or -1 if it does not exist. An alphanumeric string is a 
--- leetcode.py
+++ leetcode.py
@@ -34116,6 +34116,50 @@ class Fenwick:
         return ans 
 
 
+    """1794. Count Pairs of Equal Substrings With Minimum Difference (Medium)
+	You are given two strings firstString and secondString that are 0-indexed 
+	and consist only of lowercase English letters. Count the number of index 
+	quadruples (i,j,a,b) that satisfy the following conditions:
+	* 0 <= i <= j < firstString.length
+	* 0 <= a <= b < secondString.length
+	* The substring of firstString that starts at the ith character and ends at 
+	  the jth character (inclusive) is equal to the substring of secondString 
+	  that starts at the ath character and ends at the bth character (inclusive).
+	* j - a is the minimum possible value among all quadruples that satisfy the 
+	  previous conditions.
+	Return the number of such quadruples.
+
+	Example 1:
+	Input: firstString = "abcd", secondString = "bccda"
+	Output: 1
+	Explanation: The quadruple (0,0,4,4) is the only one that satisfies all the 
+	             conditions and minimizes j - a.
+	
+	Example 2:
+	Input: firstString = "ab", secondString = "cd"
+	Output: 0
+	Explanation: There are no quadruples satisfying all the conditions.
+
+	Constraints:
+	* 1 <= firstString.length, secondString.length <= 2 * 10^5
+	* Both strings consist only of lowercase English letters."""
+
+    def countQuadruples(self, firstString: str, secondString: str) -> int:
+        prefix = {}
+        suffix = {}
+        for i, x in enumerate(firstString): prefix.setdefault(x, i)
+        for i, x in enumerate(secondString): suffix[x] = i 
+        
+        ans = 0 
+        most = -inf 
+        for x in ascii_lowercase: 
+            if x in prefix and x in suffix: 
+                val = suffix[x] - prefix[x]
+                if val > most: ans, most = 1, val
+                elif val == most: ans += 1
+        return ans
+
+
     """1796. Second Largest Digit in a String (Easy)
 	Given an alphanumeric string s, return the second largest numerical digit 
 	that appears in s, or -1 if it does not exist. An alphanumeric string is a 
