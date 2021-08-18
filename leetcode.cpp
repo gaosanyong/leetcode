16240b340cc09e21b36a7e47245f15c39f156b92
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-18 12:57:03 -0400

    Solve'em all (1244)

--- leetcode.cpp
+++ leetcode.cpp
@@ -19003,6 +19003,66 @@ public:
 };
 
 
+/*1244. Design A Leaderboard (Medium)
+Design a Leaderboard class, which has 3 functions:
+* addScore(playerId, score): Update the leaderboard by adding score to the 
+  given player's score. If there is no player with such id in the leaderboard, 
+  add him to the leaderboard with the given score.
+* top(K): Return the score sum of the top K players.
+* reset(playerId): Reset the score of the player with the given id to 0 (in 
+  other words erase it from the leaderboard). It is guaranteed that the player 
+  was added to the leaderboard before calling this function.
+Initially, the leaderboard is empty.
+
+Example 1:
+Input: ["Leaderboard","addScore","addScore","addScore","addScore","addScore","top","reset","reset","addScore","top"]
+       [[],[1,73],[2,56],[3,39],[4,51],[5,4],[1],[1],[2],[2,51],[3]]
+Output: [null,null,null,null,null,null,73,null,null,null,141]
+Explanation: 
+Leaderboard leaderboard = new Leaderboard ();
+leaderboard.addScore(1,73);   // leaderboard = [[1,73]];
+leaderboard.addScore(2,56);   // leaderboard = [[1,73],[2,56]];
+leaderboard.addScore(3,39);   // leaderboard = [[1,73],[2,56],[3,39]];
+leaderboard.addScore(4,51);   // leaderboard = [[1,73],[2,56],[3,39],[4,51]];
+leaderboard.addScore(5,4);    // leaderboard = [[1,73],[2,56],[3,39],[4,51],[5,4]];
+leaderboard.top(1);           // returns 73;
+leaderboard.reset(1);         // leaderboard = [[2,56],[3,39],[4,51],[5,4]];
+leaderboard.reset(2);         // leaderboard = [[3,39],[4,51],[5,4]];
+leaderboard.addScore(2,51);   // leaderboard = [[2,51],[3,39],[4,51],[5,4]];
+leaderboard.top(3);           // returns 141 = 51 + 51 + 39;
+ 
+Constraints:
+* 1 <= playerId, K <= 10000
+* It's guaranteed that K is less than or equal to the current number of players.
+* 1 <= score <= 100
+* There will be at most 1000 function calls.*/
+
+class Leaderboard {
+    multiset<int> scores; 
+    unordered_map<int, int> mp; 
+public:
+    Leaderboard() {}
+    
+    void addScore(int playerId, int score) {
+        if (mp.count(playerId)) scores.erase(scores.find(mp[playerId])); 
+        mp[playerId] += score; 
+        scores.insert(mp[playerId]); 
+    }
+    
+    int top(int K) {
+        int ans = 0; 
+        auto it = scores.rbegin(); 
+        while (K-- && it != scores.rend()) ans += *it++; 
+        return ans; 
+    }
+    
+    void reset(int playerId) {
+        scores.erase(scores.find(mp[playerId])); 
+        mp.erase(playerId); 
+    }
+};
+
+
 /*1797. Design Authentication Manager (Medium)
 There is an authentication system that works with authentication tokens. For 
 each session, the user will receive a new authentication token that will expire 
--- leetcode.py
+++ leetcode.py
@@ -43203,6 +43203,61 @@ class Skiplist:
         return ans 
 
 
+"""1244. Design A Leaderboard (Medium)
+Design a Leaderboard class, which has 3 functions:
+* addScore(playerId, score): Update the leaderboard by adding score to the 
+  given player's score. If there is no player with such id in the leaderboard, 
+  add him to the leaderboard with the given score.
+* top(K): Return the score sum of the top K players.
+* reset(playerId): Reset the score of the player with the given id to 0 (in 
+  other words erase it from the leaderboard). It is guaranteed that the player 
+  was added to the leaderboard before calling this function.
+Initially, the leaderboard is empty.
+
+Example 1:
+Input: ["Leaderboard","addScore","addScore","addScore","addScore","addScore","top","reset","reset","addScore","top"]
+       [[],[1,73],[2,56],[3,39],[4,51],[5,4],[1],[1],[2],[2,51],[3]]
+Output: [null,null,null,null,null,null,73,null,null,null,141]
+Explanation: 
+Leaderboard leaderboard = new Leaderboard ();
+leaderboard.addScore(1,73);   // leaderboard = [[1,73]];
+leaderboard.addScore(2,56);   // leaderboard = [[1,73],[2,56]];
+leaderboard.addScore(3,39);   // leaderboard = [[1,73],[2,56],[3,39]];
+leaderboard.addScore(4,51);   // leaderboard = [[1,73],[2,56],[3,39],[4,51]];
+leaderboard.addScore(5,4);    // leaderboard = [[1,73],[2,56],[3,39],[4,51],[5,4]];
+leaderboard.top(1);           // returns 73;
+leaderboard.reset(1);         // leaderboard = [[2,56],[3,39],[4,51],[5,4]];
+leaderboard.reset(2);         // leaderboard = [[3,39],[4,51],[5,4]];
+leaderboard.addScore(2,51);   // leaderboard = [[2,51],[3,39],[4,51],[5,4]];
+leaderboard.top(3);           // returns 141 = 51 + 51 + 39;
+ 
+Constraints:
+* 1 <= playerId, K <= 10000
+* It's guaranteed that K is less than or equal to the current number of players.
+* 1 <= score <= 100
+* There will be at most 1000 function calls."""
+
+class Leaderboard:
+
+    def __init__(self):
+        self.mp = {}
+        self.data = []
+
+    def addScore(self, playerId: int, score: int) -> None:
+        if playerId in self.mp: 
+            self.data.remove(self.mp[playerId])
+            self.mp[playerId] += score 
+        else: self.mp[playerId] = score
+        insort(self.data, self.mp[playerId])
+
+    def top(self, K: int) -> int:
+        return sum(self.data[-K:])
+
+    def reset(self, playerId: int) -> None:
+        self.data.remove(self.mp[playerId])
+        self.mp.pop(playerId)
+
+
 """1357. Apply Discount Every n Orders (Medium)
 There is a sale in a supermarket, there will be a discount every n customer. 
 There are some products in the supermarket where the id of the i-th product is 
