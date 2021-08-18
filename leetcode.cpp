93875cb87227710e4f76ff8fdcf7186a5c8afadb
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-18 15:29:18 -0400

    Solve'em all (1772)

--- leetcode.cpp
+++ leetcode.cpp
@@ -11071,6 +11071,61 @@ public:
     }
 
 
+    /*1772. Sort Features by Popularity (Medium)
+	You are given a string array features where features[i] is a single word 
+	that represents the name of a feature of the latest product you are working 
+	on. You have made a survey where users have reported which features they 
+	like. You are given a string array responses, where each responses[i] is a 
+	string containing space-separated words. The popularity of a feature is the 
+	number of responses[i] that contain the feature. You want to sort the 
+	features in non-increasing order by their popularity. If two features have 
+	the same popularity, order them by their original index in features. Notice 
+	that one response could contain the same feature multiple times; this 
+	feature is only counted once in its popularity. Return the features in 
+	sorted order.
+
+	Example 1:
+	Input: features = ["cooler","lock","touch"], 
+	       responses = ["i like cooler cooler","lock touch cool","locker like touch"]
+	Output: ["touch","cooler","lock"]
+	Explanation: appearances("cooler") = 1, appearances("lock") = 1, 
+	             appearances("touch") = 2. Since "cooler" and "lock" both had 1 
+	             appearance, "cooler" comes first because "cooler" came first 
+	             in the features array.
+	
+	Example 2:
+	Input: features = ["a","aa","b","c"], responses = ["a","a aa","a a a a a","b a"]
+	Output: ["a","aa","b","c"]
+
+	Constraints:
+	* 1 <= features.length <= 10^4
+	* 1 <= features[i].length <= 10
+	* features contains no duplicates.
+	* features[i] consists of lowercase letters.
+	* 1 <= responses.length <= 10^2
+	* 1 <= responses[i].length <= 10^3
+	* responses[i] consists of lowercase letters and spaces.
+	* responses[i] contains no two consecutive spaces.
+	* responses[i] has no leading or trailing spaces.*/
+
+    vector<string> sortFeatures(vector<string>& features, vector<string>& responses) {
+        unordered_map<string, int> freq; 
+        
+        for (auto& response : responses) {
+            istringstream iss(response); 
+            string buf; 
+            unordered_set<string> words; 
+            while (iss >> buf) words.insert(buf); 
+            
+            for (auto& feature : features) 
+                if (words.count(feature)) freq[feature]++; 
+        }
+        
+        stable_sort(features.begin(), features.end(), [&](auto& lhs, auto& rhs) { return freq[lhs] > freq[rhs]; } ); 
+        return features; 
+    }
+
+
     /*1790. Check if One String Swap Can Make Strings Equal (Easy)
 	You are given two strings s1 and s2 of equal length. A string swap is an 
 	operation where you choose two indices in a string (not necessarily 
--- leetcode.py
+++ leetcode.py
@@ -33442,6 +33442,52 @@ class Fenwick:
         return ans 
 
 
+    """1772. Sort Features by Popularity (Medium)
+	You are given a string array features where features[i] is a single word 
+	that represents the name of a feature of the latest product you are working 
+	on. You have made a survey where users have reported which features they 
+	like. You are given a string array responses, where each responses[i] is a 
+	string containing space-separated words. The popularity of a feature is the 
+	number of responses[i] that contain the feature. You want to sort the 
+	features in non-increasing order by their popularity. If two features have 
+	the same popularity, order them by their original index in features. Notice 
+	that one response could contain the same feature multiple times; this 
+	feature is only counted once in its popularity. Return the features in 
+	sorted order.
+
+	Example 1:
+	Input: features = ["cooler","lock","touch"], 
+	       responses = ["i like cooler cooler","lock touch cool","locker like touch"]
+	Output: ["touch","cooler","lock"]
+	Explanation: appearances("cooler") = 1, appearances("lock") = 1, 
+	             appearances("touch") = 2. Since "cooler" and "lock" both had 1 
+	             appearance, "cooler" comes first because "cooler" came first 
+	             in the features array.
+	
+	Example 2:
+	Input: features = ["a","aa","b","c"], responses = ["a","a aa","a a a a a","b a"]
+	Output: ["a","aa","b","c"]
+
+	Constraints:
+	* 1 <= features.length <= 10^4
+	* 1 <= features[i].length <= 10
+	* features contains no duplicates.
+	* features[i] consists of lowercase letters.
+	* 1 <= responses.length <= 10^2
+	* 1 <= responses[i].length <= 10^3
+	* responses[i] consists of lowercase letters and spaces.
+	* responses[i] contains no two consecutive spaces.
+	* responses[i] has no leading or trailing spaces."""
+
+    def sortFeatures(self, features: List[str], responses: List[str]) -> List[str]:
+        freq = defaultdict(int)
+        for response in responses: 
+            words = set(response.split())
+            for feature in features: 
+                if feature in words: freq[feature] += 1
+        return sorted(features, key=lambda x: freq.get(x, 0), reverse=True)
+
+
     """1773. Count Items Matching a Rule (Easy)
 	You are given an array items, where each items[i] = [typei, colori, namei] 
 	describes the type, color, and name of the ith item. You are also given a 
