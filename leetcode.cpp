4e286670e8122988116fbd0ce114fcc00105c91f
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-19 10:20:43 -0400

    August LeetCoding Challenge 2021-08-19 (1339)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10423,6 +10423,53 @@ public:
     }
 
 
+    /*1339. Maximum Product of Splitted Binary Tree (Medium)
+	Given the root of a binary tree, split the binary tree into two subtrees by 
+	removing one edge such that the product of the sums of the subtrees is 
+	maximized. Return the maximum product of the sums of the two subtrees. 
+	Since the answer may be too large, return it modulo 10^9 + 7. Note that you 
+	need to maximize the answer before taking the mod and not after taking it.
+
+	Example 1:
+	Input: root = [1,2,3,4,5,6]
+	Output: 110
+	Explanation: Remove the red edge and get 2 binary trees with sum 11 and 10. 
+	             Their product is 110 (11*10)
+	
+	Example 2:
+	Input: root = [1,null,2,3,4,null,null,5,6]
+	Output: 90
+	Explanation: Remove the red edge and get 2 binary trees with sum 15 and 6.
+	             Their product is 90 (15*6)
+	
+	Example 3:
+	Input: root = [2,3,9,10,7,8,6,5,4,11,1]
+	Output: 1025
+
+	Example 4:
+	Input: root = [1,1]
+	Output: 1
+
+	Constraints:
+	* The number of nodes in the tree is in the range [2, 5 * 10^4].
+	* 1 <= Node.val <= 10^4*/
+
+    int maxProduct(TreeNode* root) {
+        vector<long> vals; 
+        
+        function<long(TreeNode*)> fn = [&](TreeNode* node) {
+            if (!node) return 0l; 
+            long ans = node->val + fn(node->left) + fn(node->right); 
+            vals.push_back(ans); 
+            return ans; 
+        }; 
+        
+        long total = fn(root), ans = 0; 
+        for (auto& x : vals) ans = max(ans, (total-x)*x); 
+        return ans % 1'000'000'007; 
+    }
+
+
     /*1354. Construct Target Array With Multiple Sums (Hard)
 	Given an array of integers target. From a starting array, A consisting of 
 	all 1's, you may perform the following procedure :
--- leetcode.py
+++ leetcode.py
@@ -23960,6 +23960,51 @@ class UnionFind:
             if ans >= len(arr)//2: return i + 1
 
 
+    """1339. Maximum Product of Splitted Binary Tree (Medium)
+	Given the root of a binary tree, split the binary tree into two subtrees by 
+	removing one edge such that the product of the sums of the subtrees is 
+	maximized. Return the maximum product of the sums of the two subtrees. 
+	Since the answer may be too large, return it modulo 10^9 + 7. Note that you 
+	need to maximize the answer before taking the mod and not after taking it.
+
+	Example 1:
+	Input: root = [1,2,3,4,5,6]
+	Output: 110
+	Explanation: Remove the red edge and get 2 binary trees with sum 11 and 10. 
+	             Their product is 110 (11*10)
+	
+	Example 2:
+	Input: root = [1,null,2,3,4,null,null,5,6]
+	Output: 90
+	Explanation: Remove the red edge and get 2 binary trees with sum 15 and 6.
+	             Their product is 90 (15*6)
+	
+	Example 3:
+	Input: root = [2,3,9,10,7,8,6,5,4,11,1]
+	Output: 1025
+
+	Example 4:
+	Input: root = [1,1]
+	Output: 1
+
+	Constraints:
+	* The number of nodes in the tree is in the range [2, 5 * 10^4].
+	* 1 <= Node.val <= 10^4"""
+
+    def maxProduct(self, root: Optional[TreeNode]) -> int:
+        vals = []
+        
+        def fn(node): 
+            """Return sum of sub-tree."""
+            if not node: return 0 
+            ans = node.val + fn(node.left) + fn(node.right)
+            vals.append(ans)
+            return ans
+        
+        total = fn(root)
+        return max((total-x)*x for x in vals) % 1_000_000_007
+
+
     """1349. Maximum Students Taking Exam (Hard)
 	Given a m * n matrix seats  that represent seats distributions in a 
 	classroom. If a seat is broken, it is denoted by '#' character otherwise it 
