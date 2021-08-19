519805b3b2b70770840c649f3937854602fa414a
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-19 09:26:30 -0400

    solve'em all (1908)

--- leetcode.cpp
+++ leetcode.cpp
@@ -15743,6 +15743,73 @@ public:
     }
 
 
+    /*1908. Game of Nim (Medium)
+	Alice and Bob take turns playing a game with Alice starting first. In this 
+	game, there are n piles of stones. On each player's turn, the player should 
+	remove any positive number of stones from a non-empty pile of his or her 
+	choice. The first player who cannot make a move loses, and the other player 
+	wins. Given an integer array piles, where piles[i] is the number of stones 
+	in the ith pile, return true if Alice wins, or false if Bob wins. Both 
+	Alice and Bob play optimally.
+
+	Example 1:
+	Input: piles = [1]
+	Output: true
+	Explanation: There is only one possible scenario:
+	- On the first turn, Alice removes one stone from the first pile. piles = [0].
+	- On the second turn, there are no stones left for Bob to remove. Alice wins.
+
+	Example 2:
+	Input: piles = [1,1]
+	Output: false
+	Explanation: It can be proven that Bob will always win. One possible scenario is:
+	- On the first turn, Alice removes one stone from the first pile. piles = [0,1].
+	- On the second turn, Bob removes one stone from the second pile. piles = [0,0].
+	- On the third turn, there are no stones left for Alice to remove. Bob wins.
+
+	Example 3:
+	Input: piles = [1,2,3]
+	Output: false
+	Explanation: It can be proven that Bob will always win. One possible scenario is:
+	- On the first turn, Alice removes three stones from the third pile. piles = [1,2,0].
+	- On the second turn, Bob removes one stone from the second pile. piles = [1,1,0].
+	- On the third turn, Alice removes one stone from the first pile. piles = [0,1,0].
+	- On the fourth turn, Bob removes one stone from the second pile. piles = [0,0,0].
+	- On the fifth turn, there are no stones left for Alice to remove. Bob wins.
+	 
+	Constraints:
+	* n == piles.length
+	* 1 <= n <= 7
+	* 1 <= piles[i] <= 7
+	 
+
+	Follow-up: Could you find a linear time solution? Although the linear time 
+	           solution may be beyond the scope of an interview, it could be 
+	           interesting to know.*/
+
+    bool nimGame(vector<int>& piles) {
+        unordered_map<int, bool> memo; 
+        
+        function<bool(int)> fn = [&](int mask) {
+            if (!memo.count(mask)) {
+                memo[mask] = false; 
+                for (int i = 0; i < piles.size(); ++i) {
+                    int val = (mask >> 3*i) & 7; 
+                    for (int x = 1; x <= val; ++x) {
+                        int mask0 = mask - (x << 3*i); 
+                        if (!fn(mask0)) return memo[mask] = true; 
+                    }
+                }
+            }
+            return memo[mask]; 
+        }; 
+        
+        int mask = 0; 
+        for (int i = 0; i < piles.size(); ++i) mask |= piles[i] << 3*i; 
+        return fn(mask); 
+    }
+
+
     /*1909. Remove One Element to Make the Array Strictly Increasing (Easy)
 	Given a 0-indexed integer array nums, return true if it can be made 
 	strictly increasing after removing exactly one element, or false otherwise. 
--- leetcode.py
+++ leetcode.py
@@ -38411,6 +38411,67 @@ class Fenwick:
         return ans
 
 
+    """1908. Game of Nim (Medium)
+	Alice and Bob take turns playing a game with Alice starting first. In this 
+	game, there are n piles of stones. On each player's turn, the player should 
+	remove any positive number of stones from a non-empty pile of his or her 
+	choice. The first player who cannot make a move loses, and the other player 
+	wins. Given an integer array piles, where piles[i] is the number of stones 
+	in the ith pile, return true if Alice wins, or false if Bob wins. Both 
+	Alice and Bob play optimally.
+
+	Example 1:
+	Input: piles = [1]
+	Output: true
+	Explanation: There is only one possible scenario:
+	- On the first turn, Alice removes one stone from the first pile. piles = [0].
+	- On the second turn, there are no stones left for Bob to remove. Alice wins.
+
+	Example 2:
+	Input: piles = [1,1]
+	Output: false
+	Explanation: It can be proven that Bob will always win. One possible scenario is:
+	- On the first turn, Alice removes one stone from the first pile. piles = [0,1].
+	- On the second turn, Bob removes one stone from the second pile. piles = [0,0].
+	- On the third turn, there are no stones left for Alice to remove. Bob wins.
+
+	Example 3:
+	Input: piles = [1,2,3]
+	Output: false
+	Explanation: It can be proven that Bob will always win. One possible scenario is:
+	- On the first turn, Alice removes three stones from the third pile. piles = [1,2,0].
+	- On the second turn, Bob removes one stone from the second pile. piles = [1,1,0].
+	- On the third turn, Alice removes one stone from the first pile. piles = [0,1,0].
+	- On the fourth turn, Bob removes one stone from the second pile. piles = [0,0,0].
+	- On the fifth turn, there are no stones left for Alice to remove. Bob wins.
+	 
+	Constraints:
+	* n == piles.length
+	* 1 <= n <= 7
+	* 1 <= piles[i] <= 7
+	 
+
+	Follow-up: Could you find a linear time solution? Although the linear time 
+	           solution may be beyond the scope of an interview, it could be 
+	           interesting to know."""
+
+    def nimGame(self, piles: List[int]) -> bool:
+        mask = 0
+        for i, x in enumerate(piles): mask |= x << 3*i
+        
+        @cache
+        def fn(mask): 
+            """Return True if current player can win by playing optimally."""
+            for i in range(len(piles)): 
+                val = (mask >> 3*i) & 7
+                for k in range(1, val+1): 
+                    mask0 = mask - (k << 3*i)
+                    if not fn(mask0): return True 
+            return False 
+        
+        return fn(mask)
+
+
     """1909. Remove One Element to Make the Array Strictly Increasing (Easy)
 	Given a 0-indexed integer array nums, return true if it can be made 
 	strictly increasing after removing exactly one element, or false otherwise. 
