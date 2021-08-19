c5827dcb851b79dcf904fb1dc59335bedcde096c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-18 23:22:43 -0400

    Solve'em all (1973)

--- leetcode.cpp
+++ leetcode.cpp
@@ -18509,6 +18509,49 @@ public:
         }
         return false; 
     }
+
+
+    /*1973. Count Nodes Equal to Sum of Descendants (Medium)
+	Given the root of a binary tree, return the number of nodes where the value 
+	of the node is equal to the sum of the values of its descendants. A 
+	descendant of a node x is any node that is on the path from node x to some 
+	leaf node. The sum is considered to be 0 if the node has no descendants.
+
+	Example 1:
+	Input: root = [10,3,4,2,1]
+	Output: 2
+	Explanation: For the node with value 10: The sum of its descendants is 
+	             3+4+2+1 = 10. For the node with value 3: The sum of its 
+	             descendants is 2+1 = 3.
+	
+	Example 2:
+	Input: root = [2,3,null,2,null]
+	Output: 0
+	Explanation: No node has a value that is equal to the sum of its descendants.
+	
+	Example 3:
+	Input: root = [0]
+	Output: 1
+	Explanation: For the node with value 0: The sum of its descendants is 0 
+	             since it has no descendants.
+
+	Constraints:
+	* The number of nodes in the tree is in the range [1, 10^5].
+	* 0 <= Node.val <= 10^5*/
+
+    int equalToDescendants(TreeNode* root) {
+        int ans = 0; 
+        
+        function<long(TreeNode*)> fn = [&](TreeNode* node) {
+            if (!node) return 0l; 
+            long sm = fn(node->left) + fn(node->right); 
+            if (sm == node->val) ++ans; 
+            return sm + node->val; 
+        }; 
+        
+        fn(root); 
+        return ans; 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -40719,6 +40719,49 @@ class UnionFind:
         return False 
 
 
+    """1973. Count Nodes Equal to Sum of Descendants (Medium)
+	Given the root of a binary tree, return the number of nodes where the value 
+	of the node is equal to the sum of the values of its descendants. A 
+	descendant of a node x is any node that is on the path from node x to some 
+	leaf node. The sum is considered to be 0 if the node has no descendants.
+
+	Example 1:
+	Input: root = [10,3,4,2,1]
+	Output: 2
+	Explanation: For the node with value 10: The sum of its descendants is 
+	             3+4+2+1 = 10. For the node with value 3: The sum of its 
+	             descendants is 2+1 = 3.
+	
+	Example 2:
+	Input: root = [2,3,null,2,null]
+	Output: 0
+	Explanation: No node has a value that is equal to the sum of its descendants.
+	
+	Example 3:
+	Input: root = [0]
+	Output: 1
+	Explanation: For the node with value 0: The sum of its descendants is 0 
+	             since it has no descendants.
+
+	Constraints:
+	* The number of nodes in the tree is in the range [1, 10^5].
+	* 0 <= Node.val <= 10^5"""
+
+    def equalToDescendants(self, root: Optional[TreeNode]) -> int:
+        
+        def fn(node):
+            """Return sum of nodes' value on sub-tree."""
+            nonlocal ans
+            if not node: return 0 
+            sm = fn(node.left) + fn(node.right)
+            if sm == node.val: ans += 1
+            return sm + node.val 
+        
+        ans = 0 
+        fn(root)
+        return ans
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
