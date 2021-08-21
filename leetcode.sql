9f30422e20049b9100f0a1be91faf95f05251b06
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-20 21:58:43 -0400

    solve'em all (1949)

--- leetcode.sql
+++ leetcode.sql
@@ -3513,6 +3513,67 @@ WHERE
     AND TIMESTAMPDIFF(SECOND, a.time_stamp, b.time_stamp) <= 86400
 
 
+/*1949. Strong Friendship (Medium)
+SQL Schema
+Table: Friendship
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| user1_id    | int  |
+| user2_id    | int  |
++-------------+------+
+(user1_id, user2_id) is the primary key for this table. Each row of this table 
+indicates that the users user1_id and user2_id are friends. Note that 
+user1_id < user2_id. A friendship between a pair of friends x and y is strong 
+if x and y have at least three common friends.
+
+Write an SQL query to find all the strong friendships. Note that the result 
+table should not contain duplicates with user1_id < user2_id. Return the result 
+table in any order. The query result format is in the following example:
+
+Friendship table:
++----------+----------+
+| user1_id | user2_id |
++----------+----------+
+| 1        | 2        |
+| 1        | 3        |
+| 2        | 3        |
+| 1        | 4        |
+| 2        | 4        |
+| 1        | 5        |
+| 2        | 5        |
+| 1        | 7        |
+| 3        | 7        |
+| 1        | 6        |
+| 3        | 6        |
+| 2        | 6        |
++----------+----------+
+
+Result table:
++----------+----------+---------------+
+| user1_id | user2_id | common_friend |
++----------+----------+---------------+
+| 1        | 2        | 4             |
+| 1        | 3        | 3             |
++----------+----------+---------------+
+Users 1 and 2 have 4 common friends (3, 4, 5, and 6). Users 1 and 3 have 3 
+common friends (2, 6, and 7). We did not include the friendship of users 2 and 
+3 because they only have two common friends (1 and 6).*/
+
+WITH Cte AS (
+    SELECT user1_id, user2_id FROM Friendship 
+    UNION
+    SELECT user2_id AS user1_id, user1_id AS user2_id FROM Friendship
+)
+SELECT a.user1_id AS user1_id, b.user2_id AS user2_id, COUNT(*) AS common_friend
+FROM 
+    Cte a 
+    JOIN Cte b ON a.user2_id = b.user1_id AND a.user1_id < b.user2_id
+    JOIN Friendship c ON a.user1_id = c.user1_id AND b.user2_id = c.user2_id
+GROUP BY 1, 2
+HAVING COUNT(*) >= 3
+
+
 /*1951. All the Pairs With the Maximum Number of Common Followers (Medium)
 SQL Schema
 Table: Relations
