9da438a582861d8f716b61b8305c006885b20fe4
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-20 22:14:58 -0400

    solve'em all (1151)

--- leetcode.py
+++ leetcode.py
@@ -21618,6 +21618,48 @@ class UnionFind:
         return nums[k + len(nums)//2] == target 
 
 
+    """1151. Minimum Swaps to Group All 1's Together (Medium)
+	Given a binary array data, return the minimum number of swaps required to 
+	group all 1’s present in the array together in any place in the array.
+
+	Example 1:
+	Input: data = [1,0,1,0,1]
+	Output: 1
+	Explanation: There are 3 ways to group all 1's together:
+	             [1,1,1,0,0] using 1 swap.
+	             [0,1,1,1,0] using 2 swaps.
+	             [0,0,1,1,1] using 1 swap.
+	             The minimum is 1.
+	
+	Example 2:
+	Input: data = [0,0,0,1,0]
+	Output: 0
+	Explanation: Since there is only one 1 in the array, no swaps needed.
+	
+	Example 3:
+	Input: data = [1,0,1,0,1,0,0,1,1,0,1]
+	Output: 3
+	Explanation: One possible solution that uses 3 swaps is 
+	             [0,0,0,0,0,1,1,1,1,1,1].
+	
+	Example 4:
+	Input: data = [1,0,1,0,1,0,1,1,1,0,1,0,0,1,1,1,0,0,1,1,1,0,1,0,1,1,0,0,0,1,1,1,1,0,0,1]
+	Output: 8
+
+	Constraints:
+	* 1 <= data.length <= 10^5
+	* data[i] is 0 or 1."""
+
+    def minSwaps(self, data: List[int]) -> int:
+        k = sum(data) # window size 
+        ans = val = 0 
+        for i, x in enumerate(data): 
+            val += x
+            if i >= k: val -= data[i-k]
+            if i+1 >= k: ans = max(ans, val)
+        return k - ans
+
+
     """1160. Find Words That Can Be Formed by Characters (Easy)
 	You are given an array of strings words and a string chars. A string is 
 	good if it can be formed by characters from chars (each character can only 
