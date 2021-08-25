29f7357672dd00fab0254f394e547544cab6a124
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-25 15:09:24 -0400

    solve'em all (1182)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10724,6 +10724,48 @@ public:
     }
 
 
+    /*1182. Shortest Distance to Target Color (Medium)
+	You are given an array colors, in which there are three colors: 1, 2 and 3.
+	You are also given some queries. Each query consists of two integers i and 
+	c, return the shortest distance between the given index i and the target 
+	color c. If there is no solution return -1.
+
+	Example 1:
+	Input: colors = [1,1,2,1,3,2,2,3,3], queries = [[1,3],[2,2],[6,1]]
+	Output: [3,0,3]
+	Explanation: The nearest 3 from index 1 is at index 4 (3 steps away).
+	             The nearest 2 from index 2 is at index 2 itself (0 steps away).
+	             The nearest 1 from index 6 is at index 3 (3 steps away).
+	
+	Example 2:
+	Input: colors = [1,2], queries = [[0,3]]
+	Output: [-1]
+	Explanation: There is no 3 in the array.
+
+	Constraints:
+	* 1 <= colors.length <= 5*10^4
+	* 1 <= colors[i] <= 3
+	* 1 <= queries.length <= 5*10^4
+	* queries[i].length == 2
+	* 0 <= queries[i][0] < colors.length
+	* 1 <= queries[i][1] <= 3*/
+
+    vector<int> shortestDistanceColor(vector<int>& colors, vector<vector<int>>& queries) {
+        unordered_map<int, vector<int>> locs; 
+        for (int i = 0; i < colors.size(); ++i) locs[colors[i]].push_back(i); 
+        
+        vector<int> ans; 
+        for (auto& query : queries) {
+            int i = query[0], c = query[1], val = INT_MAX; 
+            auto it = lower_bound(locs[c].begin(), locs[c].end(), i); 
+            if (it != locs[c].end()) val = *it - i; 
+            if (it != locs[c].begin()) val = min(val, i - *prev(it)); 
+            ans.push_back(val < INT_MAX ? val : -1); 
+        }
+        return ans; 
+    }
+
+
     /*1209. Remove All Adjacent Duplicates in String II (Medium)
 	Given a string s, a k duplicate removal consists of choosing k adjacent and 
 	equal letters from s and removing them causing the left and the right side 
--- leetcode.py
+++ leetcode.py
@@ -22254,6 +22254,46 @@ class UnionFind:
         return ans 
 
 
+    """1182. Shortest Distance to Target Color (Medium)
+	You are given an array colors, in which there are three colors: 1, 2 and 3.
+	You are also given some queries. Each query consists of two integers i and 
+	c, return the shortest distance between the given index i and the target 
+	color c. If there is no solution return -1.
+
+	Example 1:
+	Input: colors = [1,1,2,1,3,2,2,3,3], queries = [[1,3],[2,2],[6,1]]
+	Output: [3,0,3]
+	Explanation: The nearest 3 from index 1 is at index 4 (3 steps away).
+	             The nearest 2 from index 2 is at index 2 itself (0 steps away).
+	             The nearest 1 from index 6 is at index 3 (3 steps away).
+	
+	Example 2:
+	Input: colors = [1,2], queries = [[0,3]]
+	Output: [-1]
+	Explanation: There is no 3 in the array.
+
+	Constraints:
+	* 1 <= colors.length <= 5*10^4
+	* 1 <= colors[i] <= 3
+	* 1 <= queries.length <= 5*10^4
+	* queries[i].length == 2
+	* 0 <= queries[i][0] < colors.length
+	* 1 <= queries[i][1] <= 3"""
+
+    def shortestDistanceColor(self, colors: List[int], queries: List[List[int]]) -> List[int]:
+        locs = defaultdict(list)
+        for i, x in enumerate(colors): locs[x].append(i)
+        
+        ans = []
+        for i, c in queries: 
+            k = bisect_left(locs[c], i)
+            val = inf 
+            if k < len(locs[c]): val = locs[c][k] - i
+            if k: val = min(val, i - locs[c][k-1])
+            ans.append(val if val < inf else -1)
+        return ans 
+
+
     """1184. Distance Between Bus Stops (Easy)
 	A bus has n stops numbered from 0 to n - 1 that form a circle. We know the 
 	distance between all pairs of neighboring stops where distance[i] is the 
