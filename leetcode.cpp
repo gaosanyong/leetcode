c2712f79fe4fc4ad8b3b929b3917772d0f959aa8
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-25 12:48:15 -0400

    solve'em all (1166)

--- leetcode.cpp
+++ leetcode.cpp
@@ -20468,6 +20468,64 @@ public:
 };
 
 
+/*1166. Design File System (Medium)
+You are asked to design a file system that allows you to create new paths and 
+associate them with different values. The format of a path is one or more 
+concatenated strings of the form: / followed by one or more lowercase English 
+letters. For example, "/leetcode" and "/leetcode/problems" are valid paths 
+while an empty string "" and "/" are not. Implement the FileSystem class:
+* bool createPath(string path, int value) Creates a new path and associates a 
+  value to it if possible and returns true. Returns false if the path already 
+  exists or its parent path doesn't exist.
+* int get(string path) Returns the value associated with path or returns -1 if 
+  the path doesn't exist.
+
+Example 1:
+Input: ["FileSystem","createPath","get"]
+       [[],["/a",1],["/a"]]
+Output: [null,true,1]
+Explanation: 
+FileSystem fileSystem = new FileSystem();
+fileSystem.createPath("/a", 1); // return true
+fileSystem.get("/a"); // return 1
+
+Example 2:
+Input: ["FileSystem","createPath","createPath","get","createPath","get"]
+       [[],["/leet",1],["/leet/code",2],["/leet/code"],["/c/d",1],["/c"]]
+Output: [null,true,true,2,false,-1]
+Explanation: 
+FileSystem fileSystem = new FileSystem();
+fileSystem.createPath("/leet", 1); // return true
+fileSystem.createPath("/leet/code", 2); // return true
+fileSystem.get("/leet/code"); // return 2
+fileSystem.createPath("/c/d", 1); // return false because the parent path "/c" doesn't exist.
+fileSystem.get("/c"); // return -1 because this path doesn't exist.
+
+Constraints:
+* The number of calls to the two functions is less than or equal to 104 in total.
+* 2 <= path.length <= 100
+* 1 <= value <= 10^9*/
+
+class FileSystem {
+    unordered_map<string, int> mp; 
+public:
+    FileSystem() {}
+    
+    bool createPath(string path, int value) {
+        if (mp.count(path)) return false; 
+        string parent = path.substr(0, path.rfind('/')); 
+        if (parent.size() && !mp.count(parent)) return false; 
+        mp[path] = value; 
+        return true; 
+    }
+    
+    int get(string path) {
+        if (mp.count(path)) return mp[path]; 
+        return -1; 
+    }
+};
+
+
 /*1244. Design A Leaderboard (Medium)
 Design a Leaderboard class, which has 3 functions:
 * addScore(playerId, score): Update the leaderboard by adding score to the 
--- leetcode.py
+++ leetcode.py
@@ -44334,6 +44334,60 @@ class SnapshotArray:
         return self.data[index][lo-1][1]
 
 
+"""1166. Design File System (Medium)
+You are asked to design a file system that allows you to create new paths and 
+associate them with different values. The format of a path is one or more 
+concatenated strings of the form: / followed by one or more lowercase English 
+letters. For example, "/leetcode" and "/leetcode/problems" are valid paths 
+while an empty string "" and "/" are not. Implement the FileSystem class:
+* bool createPath(string path, int value) Creates a new path and associates a 
+  value to it if possible and returns true. Returns false if the path already 
+  exists or its parent path doesn't exist.
+* int get(string path) Returns the value associated with path or returns -1 if 
+  the path doesn't exist.
+
+Example 1:
+Input: ["FileSystem","createPath","get"]
+       [[],["/a",1],["/a"]]
+Output: [null,true,1]
+Explanation: 
+FileSystem fileSystem = new FileSystem();
+fileSystem.createPath("/a", 1); // return true
+fileSystem.get("/a"); // return 1
+
+Example 2:
+Input: ["FileSystem","createPath","createPath","get","createPath","get"]
+       [[],["/leet",1],["/leet/code",2],["/leet/code"],["/c/d",1],["/c"]]
+Output: [null,true,true,2,false,-1]
+Explanation: 
+FileSystem fileSystem = new FileSystem();
+fileSystem.createPath("/leet", 1); // return true
+fileSystem.createPath("/leet/code", 2); // return true
+fileSystem.get("/leet/code"); // return 2
+fileSystem.createPath("/c/d", 1); // return false because the parent path "/c" doesn't exist.
+fileSystem.get("/c"); // return -1 because this path doesn't exist.
+
+Constraints:
+* The number of calls to the two functions is less than or equal to 104 in total.
+* 2 <= path.length <= 100
+* 1 <= value <= 10^9"""
+
+class FileSystem:
+
+    def __init__(self):
+        self.mp = {}
+
+    def createPath(self, path: str, value: int) -> bool:
+        if path in self.mp: return False
+        parent = path[:path.rfind("/")]
+        if parent and parent not in self.mp: return False
+        self.mp[path] = value
+        return True 
+
+    def get(self, path: str) -> int:
+        return self.mp.get(path, -1)
+
+
 """1195. Fizz Buzz Multithreaded (Medium)
 Write a program that outputs the string representation of numbers from 1 to n, 
 however:
