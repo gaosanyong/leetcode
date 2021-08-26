df115b2ef23053abb99b4817c8b916a35097c926
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-25 23:01:03 -0400

    solve'em all (1918)

--- leetcode.cpp
+++ leetcode.cpp
@@ -17012,6 +17012,64 @@ public:
     }
 
 
+    /*1918. Kth Smallest Subarray Sum (Medium)
+	Given an integer array nums of length n and an integer k, return the kth 
+	smallest subarray sum. A subarray is defined as a non-empty contiguous 
+	sequence of elements in an array. A subarray sum is the sum of all elements 
+	in the subarray.
+
+	Example 1:
+	Input: nums = [2,1,3], k = 4
+	Output: 3
+	Explanation: The subarrays of [2,1,3] are:
+	             - [2] with sum 2
+	             - [1] with sum 1
+	             - [3] with sum 3
+	             - [2,1] with sum 3
+	             - [1,3] with sum 4
+	             - [2,1,3] with sum 6 
+	             Ordering the sums from smallest to largest gives 1, 2, 3, 3, 
+	             4, 6. The 4th smallest is 3.
+	
+	Example 2:
+	Input: nums = [3,3,5,5], k = 7
+	Output: 10
+	Explanation: The subarrays of [3,3,5,5] are:
+	             - [3] with sum 3
+	             - [3] with sum 3
+	             - [5] with sum 5
+	             - [5] with sum 5
+	             - [3,3] with sum 6
+	             - [3,5] with sum 8
+	             - [5,5] with sum 10
+	             - [3,3,5], with sum 11
+	             - [3,5,5] with sum 13
+	             - [3,3,5,5] with sum 16
+	             Ordering the sums from smallest to largest gives 3, 3, 5, 5, 
+	             6, 8, 10, 11, 13, 16. The 7th smallest is 10.
+
+	Constraints:
+	* n == nums.length
+	* 1 <= n <= 2 * 10^4
+	* 1 <= nums[i] <= 5 * 10^4
+	* 1 <= k <= n * (n + 1) / 2*/
+
+    int kthSmallestSubarraySum(vector<int>& nums, int k) {
+        int lo = 0, hi = accumulate(nums.begin(), nums.end(), 0); 
+        while (lo < hi) {
+            int mid = lo + (hi - lo)/2, cnt = 0; 
+            for (int i = 0, ii = 0, rsm = 0; i < nums.size(); ++i) {
+                rsm += nums[i]; 
+                while (rsm > mid) rsm -= nums[ii++]; 
+                cnt += i - ii + 1; 
+            }
+            if (cnt < k) lo = mid + 1;
+            else hi = mid; 
+        }
+        return lo; 
+    }
+
+
     /*1925. Count Square Sum Triples (Easy)
 	A square triple (a,b,c) is a triple where a, b, and c are integers and 
 	a^2 + b^2 = c^2. Given an integer n, return the number of square triples 
--- leetcode.py
+++ leetcode.py
@@ -39379,6 +39379,69 @@ class Fenwick:
         return ans 
 
 
+    """1918. Kth Smallest Subarray Sum (Medium)
+	Given an integer array nums of length n and an integer k, return the kth 
+	smallest subarray sum. A subarray is defined as a non-empty contiguous 
+	sequence of elements in an array. A subarray sum is the sum of all elements 
+	in the subarray.
+
+	Example 1:
+	Input: nums = [2,1,3], k = 4
+	Output: 3
+	Explanation: The subarrays of [2,1,3] are:
+	             - [2] with sum 2
+	             - [1] with sum 1
+	             - [3] with sum 3
+	             - [2,1] with sum 3
+	             - [1,3] with sum 4
+	             - [2,1,3] with sum 6 
+	             Ordering the sums from smallest to largest gives 1, 2, 3, 3, 
+	             4, 6. The 4th smallest is 3.
+	
+	Example 2:
+	Input: nums = [3,3,5,5], k = 7
+	Output: 10
+	Explanation: The subarrays of [3,3,5,5] are:
+	             - [3] with sum 3
+	             - [3] with sum 3
+	             - [5] with sum 5
+	             - [5] with sum 5
+	             - [3,3] with sum 6
+	             - [3,5] with sum 8
+	             - [5,5] with sum 10
+	             - [3,3,5], with sum 11
+	             - [3,5,5] with sum 13
+	             - [3,3,5,5] with sum 16
+	             Ordering the sums from smallest to largest gives 3, 3, 5, 5, 
+	             6, 8, 10, 11, 13, 16. The 7th smallest is 10.
+
+	Constraints:
+	* n == nums.length
+	* 1 <= n <= 2 * 10^4
+	* 1 <= nums[i] <= 5 * 10^4
+	* 1 <= k <= n * (n + 1) / 2"""
+
+    def kthSmallestSubarraySum(self, nums: List[int], k: int) -> int:
+        
+        def fn(x):
+            """Return number of subarrays sums <= x."""
+            ans = rsm = ii = 0 
+            for i in range(len(nums)): 
+                rsm += nums[i]
+                while rsm > x: # sliding window 
+                    rsm -= nums[ii]
+                    ii += 1
+                ans += i - ii + 1
+            return ans 
+        
+        lo, hi = 0, sum(nums)
+        while lo < hi: 
+            mid = lo + hi >> 1
+            if fn(mid) < k: lo = mid + 1
+            else: hi = mid
+        return lo 
+
+
     """1926. Nearest Exit from Entrance in Maze (Medium)
 	You are given an m x n matrix maze (0-indexed) with empty cells 
 	(represented as '.') and walls (represented as '+'). You are also given the 
