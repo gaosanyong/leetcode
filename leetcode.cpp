983bd02618db01ce59457ba37049f912636a1254
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-26 13:03:02 -0400

    solve'em all (1533)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3386,10 +3386,7 @@ public:
             if (i == destination[0] && j == destination[1]) return true; 
             for (int k = 0; k < 4; ++k) {
                 int ii = i, jj = j, di = dir[k], dj = dir[k+1]; 
-                while (0 <= ii+di && ii+di < m && 0 <= jj+dj && jj+dj < n && maze[ii+di][jj+dj] != 1) {
-                    ii += di; 
-                    jj += dj; 
-                }
+                for (; 0 <= ii+di && ii+di < m && 0 <= jj+dj && jj+dj < n && maze[ii+di][jj+dj] != 1; ii += di, jj += dj); 
                 if (maze[ii][jj] == 0) {
                     stk.emplace(ii, jj); 
                     maze[ii][jj] = 2; 
@@ -11707,6 +11704,71 @@ public:
         
         fn(root); 
         return ans; 
+    }
+
+
+    /*1533. Find the Index of the Large Integer (Medium)
+	We have an integer array arr, where all the integers in arr are equal 
+	except for one integer which is larger than the rest of the integers. You 
+	will not be given direct access to the array, instead, you will have an API 
+	ArrayReader which have the following functions:
+	* int compareSub(int l, int r, int x, int y): where 
+	  0 <= l, r, x, y < ArrayReader.length(), l <= r and x <= y. The function 
+	  compares the sum of sub-array arr[l..r] with the sum of the sub-array 
+	  arr[x..y] and returns:
+	  - 1 if arr[l]+arr[l+1]+...+arr[r] > arr[x]+arr[x+1]+...+arr[y].
+	  - 0 if arr[l]+arr[l+1]+...+arr[r] == arr[x]+arr[x+1]+...+arr[y].
+	  - -1 if arr[l]+arr[l+1]+...+arr[r] < arr[x]+arr[x+1]+...+arr[y].
+	* int length(): Returns the size of the array.
+	You are allowed to call compareSub() 20 times at most. You can assume both 
+	functions work in O(1) time. Return the index of the array arr which has 
+	the largest integer.
+
+	Follow-up:
+	* What if there are two numbers in arr that are bigger than all other 
+	  numbers?
+	* What if there is one number that is bigger than other numbers and one 
+	  number that is smaller than other numbers?
+
+	Example 1:
+	Input: arr = [7,7,7,7,10,7,7,7]
+	Output: 4
+	Explanation: The following calls to the API
+	reader.compareSub(0, 0, 1, 1) // returns 0 this is a query comparing the 
+	                              // sub-array (0, 0) with the sub array (1, 1), 
+	                              // (i.e. compares arr[0] with arr[1]).
+	Thus we know that arr[0] and arr[1] doesn't contain the largest element.
+	reader.compareSub(2, 2, 3, 3) // returns 0, we can exclude arr[2] and arr[3].
+	reader.compareSub(4, 4, 5, 5) // returns 1, thus for sure arr[4] is the 
+	                              // largest element in the array.
+	Notice that we made only 3 calls, so the answer is valid.
+
+	Example 2:
+	Input: nums = [6,6,12]
+	Output: 2
+
+	Constraints:
+	* 2 <= arr.length <= 5 * 10^5
+	* 1 <= arr[i] <= 100
+	* All elements of arr are equal except for one element which is larger than 
+	  all other elements.*/
+
+    int getIndex(ArrayReader &reader) {
+        int lo = 0, hi = reader.length()-1; 
+        while (lo <= hi) {
+            int mid = lo + (hi - lo)/2; 
+            if ((hi-lo) & 1) {
+                int res = reader.compareSub(lo, mid, mid+1, hi); 
+                if (res == 1) hi = mid; 
+                else lo = mid + 1; 
+            } else {
+                int res = reader.compareSub(lo, mid, mid, hi); 
+                if (res == 1) hi = mid-1; 
+                else if (res == 0) return mid; 
+                else lo = mid+1; 
+            }
+        }
+        return -1; 
     }
 
 
--- leetcode.py
+++ leetcode.py
@@ -27903,6 +27903,67 @@ class UnionFind:
         return fn(0, k, "", 0)
 
 
+    """1533. Find the Index of the Large Integer (Medium)
+	We have an integer array arr, where all the integers in arr are equal 
+	except for one integer which is larger than the rest of the integers. You 
+	will not be given direct access to the array, instead, you will have an API 
+	ArrayReader which have the following functions:
+	* int compareSub(int l, int r, int x, int y): where 
+	  0 <= l, r, x, y < ArrayReader.length(), l <= r and x <= y. The function 
+	  compares the sum of sub-array arr[l..r] with the sum of the sub-array 
+	  arr[x..y] and returns:
+	  - 1 if arr[l]+arr[l+1]+...+arr[r] > arr[x]+arr[x+1]+...+arr[y].
+	  - 0 if arr[l]+arr[l+1]+...+arr[r] == arr[x]+arr[x+1]+...+arr[y].
+	  - -1 if arr[l]+arr[l+1]+...+arr[r] < arr[x]+arr[x+1]+...+arr[y].
+	* int length(): Returns the size of the array.
+	You are allowed to call compareSub() 20 times at most. You can assume both 
+	functions work in O(1) time. Return the index of the array arr which has 
+	the largest integer.
+
+	Follow-up:
+	* What if there are two numbers in arr that are bigger than all other 
+	  numbers?
+	* What if there is one number that is bigger than other numbers and one 
+	  number that is smaller than other numbers?
+
+	Example 1:
+	Input: arr = [7,7,7,7,10,7,7,7]
+	Output: 4
+	Explanation: The following calls to the API
+	reader.compareSub(0, 0, 1, 1) // returns 0 this is a query comparing the 
+	                              // sub-array (0, 0) with the sub array (1, 1), 
+	                              // (i.e. compares arr[0] with arr[1]).
+	Thus we know that arr[0] and arr[1] doesn't contain the largest element.
+	reader.compareSub(2, 2, 3, 3) // returns 0, we can exclude arr[2] and arr[3].
+	reader.compareSub(4, 4, 5, 5) // returns 1, thus for sure arr[4] is the 
+	                              // largest element in the array.
+	Notice that we made only 3 calls, so the answer is valid.
+
+	Example 2:
+	Input: nums = [6,6,12]
+	Output: 2
+
+	Constraints:
+	* 2 <= arr.length <= 5 * 10^5
+	* 1 <= arr[i] <= 100
+	* All elements of arr are equal except for one element which is larger than 
+	  all other elements."""
+
+    def getIndex(self, reader: 'ArrayReader') -> int:
+        lo, hi = 0, reader.length()-1
+        while lo <= hi: 
+            mid = lo + hi >> 1
+            if (hi - lo) & 1: 
+                res = reader.compareSub(lo, mid, mid+1, hi)
+                if res == 1: hi = mid
+                else: lo = mid + 1
+            else: 
+                res = reader.compareSub(lo, mid, mid, hi)
+                if res == 1: hi = mid-1
+                elif res == 0: return mid 
+                else: lo = mid+1
+
+
     """1554. Strings Differ by One Character (Medium)
 	Given a list of strings dict where all the strings are of the same length.
 	Return True if there are 2 strings that only differ by 1 character in the 
