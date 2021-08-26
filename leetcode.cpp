45c02673a45d6ba728dcde7c533dba5723cd713b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-25 22:34:13 -0400

    solve'em all (1229)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10872,6 +10872,47 @@ public:
     }
 
 
+    /*1229. Meeting Scheduler (Medium)
+	Given the availability time slots arrays slots1 and slots2 of two people 
+	and a meeting duration duration, return the earliest time slot that works 
+	for both of them and is of duration duration. If there is no common time 
+	slot that satisfies the requirements, return an empty array. The format of 
+	a time slot is an array of two elements [start, end] representing an 
+	inclusive time range from start to end. It is guaranteed that no two 
+	availability slots of the same person intersect with each other. That is, 
+	for any two time slots [start1, end1] and [start2, end2] of the same person, 
+	either start1 > end2 or start2 > end1.
+
+	Example 1:
+	Input: slots1 = [[10,50],[60,120],[140,210]], slots2 = [[0,15],[60,70]], duration = 8
+	Output: [60,68]
+
+	Example 2:
+	Input: slots1 = [[10,50],[60,120],[140,210]], slots2 = [[0,15],[60,70]], duration = 12
+	Output: []
+
+	Constraints:
+	* 1 <= slots1.length, slots2.length <= 10^4
+	* slots1[i].length, slots2[i].length == 2
+	* slots1[i][0] < slots1[i][1]
+	* slots2[i][0] < slots2[i][1]
+	* 0 <= slots1[i][j], slots2[i][j] <= 10^9
+	* 1 <= duration <= 10^6*/
+
+    vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
+        sort(slots1.begin(), slots1.end()); 
+        sort(slots2.begin(), slots2.end()); 
+        int i = 0, j = 0; 
+        for (int i = 0, j = 0; i < slots1.size() && j < slots2.size(); ) {
+            int start = max(slots1[i][0], slots2[j][0]), end = min(slots1[i][1], slots2[j][1]); 
+            if (start + duration <= end) return {start, start + duration}; 
+            if (slots1[i][1] <= slots2[j][1]) ++i; 
+            else ++j;
+        }
+        return {}; 
+    }
+
+
     /*1236. Web Crawler (Medium)
 	Given a url startUrl and an interface HtmlParser, implement a web crawler 
 	to crawl all links that are under the same hostname as startUrl. Return all 
--- leetcode.py
+++ leetcode.py
@@ -23645,6 +23645,46 @@ class UnionFind:
         return arr[0] + lo * chg
 
 
+    """1229. Meeting Scheduler (Medium)
+	Given the availability time slots arrays slots1 and slots2 of two people 
+	and a meeting duration duration, return the earliest time slot that works 
+	for both of them and is of duration duration. If there is no common time 
+	slot that satisfies the requirements, return an empty array. The format of 
+	a time slot is an array of two elements [start, end] representing an 
+	inclusive time range from start to end. It is guaranteed that no two 
+	availability slots of the same person intersect with each other. That is, 
+	for any two time slots [start1, end1] and [start2, end2] of the same person, 
+	either start1 > end2 or start2 > end1.
+
+	Example 1:
+	Input: slots1 = [[10,50],[60,120],[140,210]], slots2 = [[0,15],[60,70]], duration = 8
+	Output: [60,68]
+
+	Example 2:
+	Input: slots1 = [[10,50],[60,120],[140,210]], slots2 = [[0,15],[60,70]], duration = 12
+	Output: []
+
+	Constraints:
+	* 1 <= slots1.length, slots2.length <= 10^4
+	* slots1[i].length, slots2[i].length == 2
+	* slots1[i][0] < slots1[i][1]
+	* slots2[i][0] < slots2[i][1]
+	* 0 <= slots1[i][j], slots2[i][j] <= 10^9
+	* 1 <= duration <= 10^6"""
+
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+        slots1.sort()
+        slots2.sort()
+        i = j = 0 
+        while i < len(slots1) and j < len(slots2): 
+            start = max(slots1[i][0], slots2[j][0])
+            end = min(slots1[i][1], slots2[j][1])
+            if start + duration <= end: return [start, start + duration]
+            if slots1[i][1] <= slots2[j][1]: i += 1
+            else: j += 1
+        return []
+
+
     """1232. Check If It Is a Straight Line (Easy)
 	You are given an array coordinates, coordinates[i] = [x, y], where [x, y] 
 	represents the coordinate of a point. Check if these points make a straight 
