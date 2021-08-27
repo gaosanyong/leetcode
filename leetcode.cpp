16c64d71332a87ec4ee2ad1f1570cd0dfca881ab
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-26 22:13:22 -0400

    solve'em all (1066)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10505,6 +10505,57 @@ public:
     }
 
 
+    /*1066. Campus Bikes II (Medium)
+	On a campus represented as a 2D grid, there are n workers and m bikes, with 
+	n <= m. Each worker and bike is a 2D coordinate on this grid. We assign one 
+	unique bike to each worker so that the sum of the Manhattan distances 
+	between each worker and their assigned bike is minimized. Return the 
+	minimum possible sum of Manhattan distances between each worker and their 
+	assigned bike. The Manhattan distance between two points p1 and p2 is 
+	Manhattan(p1, p2) = |p1.x - p2.x| + |p1.y - p2.y|.
+
+	Example 1:
+	Input: workers = [[0,0],[2,1]], bikes = [[1,2],[3,3]]
+	Output: 6
+	Explanation: We assign bike 0 to worker 0, bike 1 to worker 1. The 
+	             Manhattan distance of both assignments is 3, so the output is 
+	             6.
+	
+	Example 2:
+	Input: workers = [[0,0],[1,1],[2,0]], bikes = [[1,0],[2,2],[2,1]]
+	Output: 4
+	Explanation: We first assign bike 0 to worker 0, then assign bike 1 to 
+	             worker 1 or worker 2, bike 2 to worker 2 or worker 1. Both 
+	             assignments lead to sum of the Manhattan distances as 4.
+	
+	Example 3:
+	Input: workers = [[0,0],[1,0],[2,0],[3,0],[4,0]], 
+	       bikes = [[0,999],[1,999],[2,999],[3,999],[4,999]]
+	Output: 4995
+
+	Constraints:
+	* n == workers.length
+	* m == bikes.length
+	* 1 <= n <= m <= 10
+	* workers[i].length == 2
+	* bikes[i].length == 2
+	* 0 <= workers[i][0], workers[i][1], bikes[i][0], bikes[i][1] < 1000
+	* All the workers and the bikes locations are unique.*/
+
+    int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
+        int n = workers.size(), m = bikes.size(); 
+        vector<vector<int>> dp(n+1, vector<int>(1<<m, 0l)); 
+        for (int i = n-1; i >= 0; --i) 
+            for (int mask = 0; mask < (1 << m); ++mask) {
+                dp[i][mask] = 100'000; 
+                for (int j = 0; j < m; ++j) 
+                    if ((mask & (1 << j)) == 0) 
+                        dp[i][mask] = min(dp[i][mask], abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1]) + dp[i+1][mask^(1<<j)]); 
+            }
+        return dp[0][0]; 
+    }
+
+
     /*1074. Number of Submatrices That Sum to Target (Hard)
 	Given a matrix and a target, return the number of non-empty submatrices 
 	that sum to target. A submatrix x1, y1, x2, y2 is the set of all cells 
--- leetcode.py
+++ leetcode.py
@@ -20312,6 +20312,58 @@ class UnionFind:
         return sorted(ans)
 
 
+    """1066. Campus Bikes II (Medium)
+	On a campus represented as a 2D grid, there are n workers and m bikes, with 
+	n <= m. Each worker and bike is a 2D coordinate on this grid. We assign one 
+	unique bike to each worker so that the sum of the Manhattan distances 
+	between each worker and their assigned bike is minimized. Return the 
+	minimum possible sum of Manhattan distances between each worker and their 
+	assigned bike. The Manhattan distance between two points p1 and p2 is 
+	Manhattan(p1, p2) = |p1.x - p2.x| + |p1.y - p2.y|.
+
+	Example 1:
+	Input: workers = [[0,0],[2,1]], bikes = [[1,2],[3,3]]
+	Output: 6
+	Explanation: We assign bike 0 to worker 0, bike 1 to worker 1. The 
+	             Manhattan distance of both assignments is 3, so the output is 
+	             6.
+	
+	Example 2:
+	Input: workers = [[0,0],[1,1],[2,0]], bikes = [[1,0],[2,2],[2,1]]
+	Output: 4
+	Explanation: We first assign bike 0 to worker 0, then assign bike 1 to 
+	             worker 1 or worker 2, bike 2 to worker 2 or worker 1. Both 
+	             assignments lead to sum of the Manhattan distances as 4.
+	
+	Example 3:
+	Input: workers = [[0,0],[1,0],[2,0],[3,0],[4,0]], 
+	       bikes = [[0,999],[1,999],[2,999],[3,999],[4,999]]
+	Output: 4995
+
+	Constraints:
+	* n == workers.length
+	* m == bikes.length
+	* 1 <= n <= m <= 10
+	* workers[i].length == 2
+	* bikes[i].length == 2
+	* 0 <= workers[i][0], workers[i][1], bikes[i][0], bikes[i][1] < 1000
+	* All the workers and the bikes locations are unique."""
+
+    def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> int:
+        n, m = len(workers), len(bikes)
+        
+        @cache
+        def fn(i, mask):
+            """Return min Manhattan distance of assigning workers[i:] to available bikes."""
+            if i == n: return 0 
+            ans = inf 
+            for j in range(m): 
+                if not mask & (1<<j): 
+                    ans = min(ans, abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1]) + fn(i+1, mask ^ (1<<j)))
+            return ans 
+        
+        return fn(0, 0)
+
 
     """1072. Flip Columns For Maximum Number of Equal Rows (Medium)
 	Given a matrix consisting of 0s and 1s, we may choose any number of columns 
