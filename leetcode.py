c2f727698034b130e57d3464dc9004d5c89ea95c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-27 14:44:01 -0400

    solve'em all (1891)

--- leetcode.py
+++ leetcode.py
@@ -38787,6 +38787,59 @@ class Fenwick:
         return (ans - sum(packages)) % 1_000_000_007 if ans < inf else -1 
 
 
+    """1891. Cutting Ribbons (Medium)
+	You are given an integer array ribbons, where ribbons[i] represents the 
+	length of the ith ribbon, and an integer k. You may cut any of the ribbons 
+	into any number of segments of positive integer lengths, or perform no cuts 
+	at all. For example, if you have a ribbon of length 4, you can:
+	* Keep the ribbon of length 4,
+	* Cut it into one ribbon of length 3 and one ribbon of length 1,
+	* Cut it into two ribbons of length 2,
+	* Cut it into one ribbon of length 2 and two ribbons of length 1, or
+	* Cut it into four ribbons of length 1.
+	Your goal is to obtain k ribbons of all the same positive integer length. 
+	You are allowed to throw away any excess ribbon as a result of cutting.
+	Return the maximum possible positive integer length that you can obtain k 
+	ribbons of, or 0 if you cannot obtain k ribbons of the same length.
+
+	Example 1:
+	Input: ribbons = [9,7,5], k = 3
+	Output: 5
+	Explanation:
+	- Cut the first ribbon to two ribbons, one of length 5 and one of length 4.
+	- Cut the second ribbon to two ribbons, one of length 5 and one of length 2.
+	- Keep the third ribbon as it is.
+	Now you have 3 ribbons of length 5.
+	
+	Example 2:
+	Input: ribbons = [7,5,9], k = 4
+	Output: 4
+	Explanation:
+	- Cut the first ribbon to two ribbons, one of length 4 and one of length 3.
+	- Cut the second ribbon to two ribbons, one of length 4 and one of length 1.
+	- Cut the third ribbon to three ribbons, two of length 4 and one of length 1.
+	Now you have 4 ribbons of length 4.
+
+	Example 3:
+	Input: ribbons = [5,7,9], k = 22
+	Output: 0
+	Explanation: You cannot obtain k ribbons of the same positive integer length.
+
+	Constraints:
+	* 1 <= ribbons.length <= 10^5
+	* 1 <= ribbons[i] <= 10^5
+	* 1 <= k <= 10^9"""
+
+    def maxLength(self, ribbons: List[int], k: int) -> int:
+        # last-true binary search 
+        lo, hi = 0, 100_000
+        while lo < hi: 
+            mid = lo + hi + 1 >> 1
+            if sum(ribbon//mid for ribbon in ribbons) < k: hi = mid - 1
+            else: lo = mid
+        return lo 
+
+
     """1893. Check if All the Integers in a Range Are Covered (Easy)
 	You are given a 2D integer array ranges and two integers left and right. 
 	Each ranges[i] = [starti, endi] represents an inclusive interval between 
