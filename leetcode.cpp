3ec47c109f68e4b047238e957400cc15bc15bc0e
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-27 16:16:02 -0400

    solve'em all (533)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3611,6 +3611,69 @@ public:
     }
 
 
+    /*533. Lonely Pixel II (Medium)
+	Given an m x n picture consisting of black 'B' and white 'W' pixels and an 
+	integer target, return the number of black lonely pixels. A black lonely 
+	pixel is a character 'B' that located at a specific position (r, c) where:
+	* Row r and column c both contain exactly target black pixels.
+	* For all rows that have a black pixel at column c, they should be exactly 
+	  the same as row r.
+
+	Example 1:
+	Input: picture = [["W","B","W","B","B","W"],
+	                  ["W","B","W","B","B","W"],
+	                  ["W","B","W","B","B","W"],
+	                  ["W","W","B","W","B","W"]], target = 3
+	Output: 6
+	Explanation: All the green 'B' are the black pixels we need (all 'B's at 
+	             column 1 and 3). Take 'B' at row r = 0 and column c = 1 as an 
+	             example:
+	             - Rule 1, row r = 0 and column c = 1 both have exactly 
+	               target = 3 black pixels. 
+	             - Rule 2, the rows have black pixel at column c = 1 are row 0, 
+	               row 1 and row 2. They are exactly the same as row r = 0.
+	
+	Example 2:
+	Input: picture = [["W","W","B"],["W","W","B"],["W","W","B"]], target = 1
+	Output: 0
+
+	Constraints:
+	* m == picture.length
+	* n == picture[i].length
+	* 1 <= m, n <= 200
+	* picture[i][j] is 'W' or 'B'.
+	* 1 <= target <= min(m, n)*/
+
+    int findBlackPixel(vector<vector<char>>& picture, int target) {
+        int m = picture.size(), n = picture[0].size(); 
+        vector<string> keys; 
+        unordered_map<string, int> freq; 
+        vector<int> rows(m, 0), cols(n, 0); 
+        for (int i = 0; i < m; ++i) {
+            string key; 
+            for (int j = 0; j < n; ++j) {
+                key.push_back(picture[i][j]); 
+                if (picture[i][j] == 'B') {
+                    ++rows[i]; 
+                    ++cols[j]; 
+                }
+            }
+            keys.push_back(key); 
+            ++freq[key]; 
+        }
+        
+        int ans = 0; 
+        for (int i = 0; i < m; ++i) {
+            string key = keys[i]; 
+            if (freq[key] == target) 
+                for (int j = 0; j < n; ++j) 
+                    if (picture[i][j] == 'B' && rows[i] == target && cols[j] == target) 
+                        ++ans; 
+        }
+        return ans; 
+    }
+
+
     /*536. Construct Binary Tree from String (Medium)
 	You need to construct a binary tree from a string consisting of parenthesis 
 	and integers. The whole input represents a binary tree. It contains an 
--- leetcode.py
+++ leetcode.py
@@ -11928,6 +11928,60 @@ class UnionFind:
         return len(ans)
 
 
+    """533. Lonely Pixel II (Medium)
+	Given an m x n picture consisting of black 'B' and white 'W' pixels and an 
+	integer target, return the number of black lonely pixels. A black lonely 
+	pixel is a character 'B' that located at a specific position (r, c) where:
+	* Row r and column c both contain exactly target black pixels.
+	* For all rows that have a black pixel at column c, they should be exactly 
+	  the same as row r.
+
+	Example 1:
+	Input: picture = [["W","B","W","B","B","W"],
+	                  ["W","B","W","B","B","W"],
+	                  ["W","B","W","B","B","W"],
+	                  ["W","W","B","W","B","W"]], target = 3
+	Output: 6
+	Explanation: All the green 'B' are the black pixels we need (all 'B's at 
+	             column 1 and 3). Take 'B' at row r = 0 and column c = 1 as an 
+	             example:
+	             - Rule 1, row r = 0 and column c = 1 both have exactly 
+	               target = 3 black pixels. 
+	             - Rule 2, the rows have black pixel at column c = 1 are row 0, 
+	               row 1 and row 2. They are exactly the same as row r = 0.
+	
+	Example 2:
+	Input: picture = [["W","W","B"],["W","W","B"],["W","W","B"]], target = 1
+	Output: 0
+
+	Constraints:
+	* m == picture.length
+	* n == picture[i].length
+	* 1 <= m, n <= 200
+	* picture[i][j] is 'W' or 'B'.
+	* 1 <= target <= min(m, n)"""
+
+    def findBlackPixel(self, picture: List[List[str]], target: int) -> int:
+        m, n = len(picture), len(picture[0])
+        freq = defaultdict(int)
+        rows = [0] * m
+        cols = [0] * n
+        for i in range(m): 
+            for j in range(n): 
+                if picture[i][j] == "B": 
+                    rows[i] += 1
+                    cols[j] += 1
+            freq["".join(picture[i])] += 1
+        
+        ans = 0
+        for i in range(m):
+            key = "".join(picture[i])
+            if freq[key] == target: 
+                for j in range(n): 
+                    if picture[i][j] == "B" and rows[i] == cols[j] == target: ans += 1
+        return ans 
+
+
     """536. Construct Binary Tree from String (Medium)
 	You need to construct a binary tree from a string consisting of parenthesis 
 	and integers. The whole input represents a binary tree. It contains an 
