dcd1d06c82e6925930d7cf786f3e1b7f7684e7b0
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-27 18:04:12 -0400

    solve'em all (1983)

--- leetcode.cpp
+++ leetcode.cpp
@@ -20611,6 +20611,54 @@ public:
         }
         return ans; 
     }
+
+
+    /*1983. Widest Pair of Indices With Equal Range Sum (Medium)
+	You are given two 0-indexed binary arrays nums1 and nums2. Find the widest 
+	pair of indices (i, j) such that i <= j and 
+	nums1[i] + nums1[i+1] + ... + nums1[j] == nums2[i] + nums2[i+1] + ... + nums2[j].
+	The widest pair of indices is the pair with the largest distance between i 
+	and j. The distance between a pair of indices is defined as j - i + 1. 
+	Return the distance of the widest pair of indices. If no pair of indices 
+	meets the conditions, return 0.
+
+	Example 1:
+	Input: nums1 = [1,1,0,1], nums2 = [0,1,1,0]
+	Output: 3
+	Explanation: If i = 1 and j = 3:
+	             nums1[1] + nums1[2] + nums1[3] = 1 + 0 + 1 = 2.
+	             nums2[1] + nums2[2] + nums2[3] = 1 + 1 + 0 = 2.
+	             The distance between i and j is j - i + 1 = 3 - 1 + 1 = 3.
+	
+	Example 2:
+	Input: nums1 = [0,1], nums2 = [1,1]
+	Output: 1
+	Explanation: If i = 1 and j = 1:
+	             nums1[1] = 1.
+	             nums2[1] = 1.
+	             The distance between i and j is j - i + 1 = 1 - 1 + 1 = 1.
+	
+	Example 3:
+	Input: nums1 = [0], nums2 = [1]
+	Output: 0
+	Explanation: There are no pairs of indices that meet the requirements.
+
+	Constraints:
+	* n == nums1.length == nums2.length
+	* 1 <= n <= 105
+	* nums1[i] is either 0 or 1.
+	* nums2[i] is either 0 or 1.*/
+
+    int widestPairOfIndices(vector<int>& nums1, vector<int>& nums2) {
+        int ans = 0, prefix = 0; 
+        unordered_map<int, int> seen = {{0, -1}}; 
+        for (int i = 0; i < nums1.size(); ++i) {
+            prefix += nums1[i] - nums2[i]; 
+            if (seen.count(prefix)) ans = max(ans, i - seen[prefix]); 
+            else seen[prefix] = i; 
+        }
+        return ans; 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -42565,6 +42565,52 @@ class UnionFind:
         return ans 
 
 
+    """1983. Widest Pair of Indices With Equal Range Sum (Medium)
+	You are given two 0-indexed binary arrays nums1 and nums2. Find the widest 
+	pair of indices (i, j) such that i <= j and 
+	nums1[i] + nums1[i+1] + ... + nums1[j] == nums2[i] + nums2[i+1] + ... + nums2[j].
+	The widest pair of indices is the pair with the largest distance between i 
+	and j. The distance between a pair of indices is defined as j - i + 1. 
+	Return the distance of the widest pair of indices. If no pair of indices 
+	meets the conditions, return 0.
+
+	Example 1:
+	Input: nums1 = [1,1,0,1], nums2 = [0,1,1,0]
+	Output: 3
+	Explanation: If i = 1 and j = 3:
+	             nums1[1] + nums1[2] + nums1[3] = 1 + 0 + 1 = 2.
+	             nums2[1] + nums2[2] + nums2[3] = 1 + 1 + 0 = 2.
+	             The distance between i and j is j - i + 1 = 3 - 1 + 1 = 3.
+	
+	Example 2:
+	Input: nums1 = [0,1], nums2 = [1,1]
+	Output: 1
+	Explanation: If i = 1 and j = 1:
+	             nums1[1] = 1.
+	             nums2[1] = 1.
+	             The distance between i and j is j - i + 1 = 1 - 1 + 1 = 1.
+	
+	Example 3:
+	Input: nums1 = [0], nums2 = [1]
+	Output: 0
+	Explanation: There are no pairs of indices that meet the requirements.
+
+	Constraints:
+	* n == nums1.length == nums2.length
+	* 1 <= n <= 105
+	* nums1[i] is either 0 or 1.
+	* nums2[i] is either 0 or 1."""
+
+    def widestPairOfIndices(self, nums1: List[int], nums2: List[int]) -> int:
+        ans = prefix = 0
+        seen = {0: -1}
+        for i in range(len(nums1)): 
+            prefix += nums1[i] - nums2[i]
+            if prefix in seen: ans = max(ans, i - seen[prefix])
+            seen.setdefault(prefix, i)
+        return ans 
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
