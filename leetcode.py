5629ed5bbccac5570047abe17eb6aab85c4a189e
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-30 12:43:23 -0400

    August LeetCoding Challenge 2021-08-31 (598)

--- leetcode.py
+++ leetcode.py
@@ -12695,6 +12695,41 @@ class UnionFind:
         return ans 
 
 
+    """598. Range Addition II (Easy)
+	You are given an m x n matrix M initialized with all 0's and an array of 
+	operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented 
+	by one for all 0 <= x < ai and 0 <= y < bi. Count and return the number of 
+	maximum integers in the matrix after performing all the operations.
+
+	Example 1:
+	Input: m = 3, n = 3, ops = [[2,2],[3,3]]
+	Output: 4
+	Explanation: The maximum integer in M is 2, and there are four of it in M. 
+	             So return 4.
+	
+	Example 2:
+	Input: m = 3, n = 3, ops = [[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],
+	                            [3,3],[2,2],[3,3],[3,3],[3,3]]
+	Output: 4
+	
+	Example 3:
+	Input: m = 3, n = 3, ops = []
+	Output: 9
+
+	Constraints:
+	* 1 <= m, n <= 4 * 10^4
+	* 1 <= ops.length <= 10^4
+	* ops[i].length == 2
+	* 1 <= ai <= m
+	* 1 <= bi <= n"""
+
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        for x, y in ops: 
+            m = min(m, x)
+            n = min(n, y)
+        return m * n
+
+
     """600. Non-negative Integers without Consecutive Ones (Hard)
 	Given a positive integer n, return the number of the integers in the range 
 	[0, n] whose binary representations do not contain consecutive ones.
