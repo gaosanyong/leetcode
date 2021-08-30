f682ac8403727bf6f446db89dbb66f2e20078a8a
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-30 15:37:12 -0400

    solve'em all (265)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2469,6 +2469,55 @@ public:
     }
 
 
+    /*265. Paint House II (Hard)
+	There are a row of n houses, each house can be painted with one of the k 
+	colors. The cost of painting each house with a certain color is different. 
+	You have to paint all the houses such that no two adjacent houses have the 
+	same color. The cost of painting each house with a certain color is 
+	represented by an n x k cost matrix costs. For example, costs[0][0] is the 
+	cost of painting house 0 with color 0; costs[1][2] is the cost of painting 
+	house 1 with color 2, and so on... Return the minimum cost to paint all 
+	houses.
+
+	Example 1:
+	Input: costs = [[1,5,3],[2,9,4]]
+	Output: 5
+	Explanation: Paint house 0 into color 0, paint house 1 into color 2. 
+	             Minimum cost: 1 + 4 = 5; Or paint house 0 into color 2, paint 
+	             house 1 into color 0. Minimum cost: 3 + 2 = 5.
+	
+	Example 2:
+	Input: costs = [[1,3],[2,4]]
+	Output: 5
+
+	Constraints:
+	* costs.length == n
+	* costs[i].length == k
+	* 1 <= n <= 100
+	* 2 <= k <= 20
+	* 1 <= costs[i][j] <= 20
+
+	Follow up: Could you solve it in O(nk) runtime?*/
+
+    int minCostII(vector<vector<int>>& costs) {
+        int n = costs.size(), k = costs[0].size(); 
+        for (int i = 1; i < n; ++i) {
+            int m0 = -1, m1 = -1; 
+            for (int j = 0; j < k; ++j) 
+                if (m0 == -1 || costs[i-1][j] <= costs[i-1][m0]) {
+                    m1 = m0; 
+                    m0 = j; 
+                } else if (m1 == -1 || costs[i-1][j] < costs[i-1][m1]) 
+                    m1 = j; 
+            
+            for (int j = 0; j < k; ++j) 
+                if (j != m0) costs[i][j] += costs[i-1][m0]; 
+                else costs[i][j] += costs[i-1][m1]; 
+        }
+        return *min_element(costs[n-1].begin(), costs[n-1].end()); 
+    }
+
+
     /*268. Missing Number (Easy)
 	Given an array nums containing n distinct numbers in the range [0, n], 
 	return the only number in the range that is missing from the array. 
--- leetcode.py
+++ leetcode.py
@@ -7981,6 +7981,49 @@ class UnionFind:
         return ans[-1]
 
 
+    """265. Paint House II (Hard)
+	There are a row of n houses, each house can be painted with one of the k 
+	colors. The cost of painting each house with a certain color is different. 
+	You have to paint all the houses such that no two adjacent houses have the 
+	same color. The cost of painting each house with a certain color is 
+	represented by an n x k cost matrix costs. For example, costs[0][0] is the 
+	cost of painting house 0 with color 0; costs[1][2] is the cost of painting 
+	house 1 with color 2, and so on... Return the minimum cost to paint all 
+	houses.
+
+	Example 1:
+	Input: costs = [[1,5,3],[2,9,4]]
+	Output: 5
+	Explanation: Paint house 0 into color 0, paint house 1 into color 2. 
+	             Minimum cost: 1 + 4 = 5; Or paint house 0 into color 2, paint 
+	             house 1 into color 0. Minimum cost: 3 + 2 = 5.
+	
+	Example 2:
+	Input: costs = [[1,3],[2,4]]
+	Output: 5
+
+	Constraints:
+	* costs.length == n
+	* costs[i].length == k
+	* 1 <= n <= 100
+	* 2 <= k <= 20
+	* 1 <= costs[i][j] <= 20
+
+	Follow up: Could you solve it in O(nk) runtime?"""
+
+    def minCostII(self, costs: List[List[int]]) -> int:
+        n, k = len(costs), len(costs[0])
+        for i in range(1, n): 
+            m0 = m1 = -1
+            for j in range(k): 
+                if m0 == -1 or costs[i-1][j] <= costs[i-1][m0]: m0, m1 = j, m0
+                elif m1 == -1 or costs[i-1][j] < costs[i-1][m1]: m1 = j
+            for j in range(k): 
+                if j == m0: costs[i][j] += costs[i-1][m1]
+                else: costs[i][j] += costs[i-1][m0]
+        return min(costs[-1])
+
+
     """266. Palindrome Permutation (Easy)
 	Given a string, determine if a permutation of the string could form a 
 	palindrome.
