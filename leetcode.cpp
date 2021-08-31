fe3c44a083795fad8d85481efcd2cc3273776516
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-31 16:18:37 -0400

    solve'em all (1215)

--- leetcode.cpp
+++ leetcode.cpp
@@ -11605,6 +11605,40 @@ public:
     }
 
 
+    /*1215. Stepping Numbers (Medium)
+	A stepping number is an integer such that all of its adjacent digits have 
+	an absolute difference of exactly 1. For example, 321 is a stepping number 
+	while 421 is not. Given two integers low and high, return a sorted list of 
+	all the stepping numbers in the inclusive range [low, high].
+	 
+	Example 1:
+	Input: low = 0, high = 21
+	Output: [0,1,2,3,4,5,6,7,8,9,10,12,21]
+
+	Example 2:
+	Input: low = 10, high = 15
+	Output: [10,12]
+
+	Constraints: 0 <= low <= high <= 2 * 10^9*/
+
+    vector<int> countSteppingNumbers(int low, int high) {
+        vector<int> ans; 
+        if (low == 0) ans.push_back(0); 
+        queue<long> q; 
+        for (int x = 1; x <= 9; ++x) q.push(x); 
+        
+        while (q.size()) {
+            long x = q.front(); q.pop(); 
+            if (x <= high) {
+                if (low <= x) ans.push_back(x); 
+                if (0 < x%10) q.push(10*x + x%10 - 1); 
+                if (x%10 < 9) q.push(10*x + x%10 + 1); 
+            }
+        }
+        return ans; 
+    }
+
+
     /*1229. Meeting Scheduler (Medium)
 	Given the availability time slots arrays slots1 and slots2 of two people 
 	and a meeting duration duration, return the earliest time slot that works 
--- leetcode.py
+++ leetcode.py
@@ -23777,6 +23777,34 @@ class UnionFind:
         return False 
 
 
+    """1215. Stepping Numbers (Medium)
+	A stepping number is an integer such that all of its adjacent digits have 
+	an absolute difference of exactly 1. For example, 321 is a stepping number 
+	while 421 is not. Given two integers low and high, return a sorted list of 
+	all the stepping numbers in the inclusive range [low, high].
+	 
+	Example 1:
+	Input: low = 0, high = 21
+	Output: [0,1,2,3,4,5,6,7,8,9,10,12,21]
+
+	Example 2:
+	Input: low = 10, high = 15
+	Output: [10,12]
+
+	Constraints: 0 <= low <= high <= 2 * 10^9"""
+
+    def countSteppingNumbers(self, low: int, high: int) -> List[int]:
+        ans = [0]
+        queue = deque([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        while queue: 
+            x = queue.popleft()
+            if x <= high: 
+                ans.append(x)
+                for dd in x%10-1, x%10+1: 
+                    if 0 <= dd < 10: queue.append(10*x+dd)
+        return [x for x in ans if low <= x]
+
+
     """1217. Minimum Cost to Move Chips to The Same Position (Easy)
 	We have n chips, where the position of the ith chip is position[i]. We need 
 	to move all the chips to the same position. In one step, we can change the 
