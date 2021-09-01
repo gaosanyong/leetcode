2085ca3ee9de41ec725b5b6993127414c8affbeb
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-08-31 22:10:37 -0400

    solve'em all (755)

--- leetcode.cpp
+++ leetcode.cpp
@@ -6354,6 +6354,94 @@ public:
     }
 
 
+    /*755. Pour Water (Medium)
+	You are given an elevation map represents as an integer array heights where 
+	heights[i] representing the height of the terrain at index i. The width at 
+	each index is 1. You are also given two integers volume and k. volume units 
+	of water will fall at index k. Water first drops at the index k and rests 
+	on top of the highest terrain or water at that index. Then, it flows 
+	according to the following rules:
+	* If the droplet would eventually fall by moving left, then move left.
+	* Otherwise, if the droplet would eventually fall by moving right, then 
+	  move right.
+	* Otherwise, rise to its current position.
+	Here, "eventually fall" means that the droplet will eventually be at a 
+	lower level if it moves in that direction. Also, level means the height of 
+	the terrain plus any water in that column. We can assume there is 
+	infinitely high terrain on the two sides out of bounds of the array. Also, 
+	there could not be partial water being spread out evenly on more than one 
+	grid block, and each unit of water has to be in exactly one block.
+
+	Example 1:
+	Input: heights = [2,1,1,2,1,2,2], volume = 4, k = 3
+	Output: [2,2,2,3,2,2,2]
+	Explanation: The first drop of water lands at index k = 3. When moving left 
+	             or right, the water can only move to the same level or a lower 
+	             level. (By level, we mean the total height of the terrain plus 
+	             any water in that column.) Since moving left will eventually 
+	             make it fall, it moves left. (A droplet "made to fall" means 
+	             go to a lower height than it was at previously.) Since moving 
+	             left will not make it fall, it stays in place. The next 
+	             droplet falls at index k = 3. Since the new droplet moving 
+	             left will eventually make it fall, it moves left. Notice that 
+	             the droplet still preferred to move left, even though it could 
+	             move right (and moving right makes it fall quicker.) The third 
+	             droplet falls at index k = 3. Since moving left would not 
+	             eventually make it fall, it tries to move right. Since moving 
+	             right would eventually make it fall, it moves right. Finally, 
+	             the fourth droplet falls at index k = 3. Since moving left 
+	             would not eventually make it fall, it tries to move right. 
+	             Since moving right would not eventually make it fall, it stays 
+	             in place.
+
+	Example 2:
+	Input: heights = [1,2,3,4], volume = 2, k = 2
+	Output: [2,3,3,4]
+	Explanation: The last droplet settles at index 1, since moving further left 
+	             would not cause it to eventually fall to a lower height.
+	
+	Example 3:
+	Input: heights = [3,1,3], volume = 5, k = 1
+	Output: [4,4,4]
+
+	Constraints:
+	* 1 <= heights.length <= 100
+	* 0 <= heights[i] <= 99
+	* 0 <= volume <= 2000
+	* 0 <= k < heights.length*/
+
+    vector<int> pourWater(vector<int>& heights, int volume, int k) {
+        stack<int> left, right; 
+        int lo = k, hi = k, i = 0; 
+        
+        while (volume--) {
+            
+            for (; lo && heights[lo-1] <= heights[lo]; --lo) 
+                if (heights[lo-1] < heights[lo]) left.push(lo-1); 
+            
+            for (; hi+1 < heights.size() && heights[hi] >= heights[hi+1]; ++hi) 
+                if (heights[hi] > heights[hi+1]) right.push(hi+1); 
+            
+            if (left.size()) {
+                i = left.top(); 
+                ++heights[i]; 
+                if (heights[i] == heights[i+1]) left.pop(); 
+                if (lo <= i-1) left.push(i-1); 
+            } else if (right.size()) {
+                i = right.top(); 
+                ++heights[i]; 
+                if (heights[i-1] == heights[i]) right.pop(); 
+                if (i+1 <= hi) right.push(i+1); 
+            } else {
+                ++heights[k]; 
+                if (k && heights[k-1] < heights[k]) left.push(k-1); 
+                if (k+1 < heights.size() && heights[k] > heights[k+1]) right.push(k+1); 
+            }
+        }
+        return heights; 
+    }
+
+
     /*762. Prime Number of Set Bits in Binary Representation (Easy)
 	Given two integers L and R, find the count of numbers in the range [L, R] 
 	(inclusive) having a prime number of set bits in their binary representation.
--- leetcode.py
+++ leetcode.py
@@ -14777,6 +14777,91 @@ class UnionFind:
         return k if not k*(k+1)//2 - target & 1 else k + 1 + k%2
 
 
+    """755. Pour Water (Medium)
+	You are given an elevation map represents as an integer array heights where 
+	heights[i] representing the height of the terrain at index i. The width at 
+	each index is 1. You are also given two integers volume and k. volume units 
+	of water will fall at index k. Water first drops at the index k and rests 
+	on top of the highest terrain or water at that index. Then, it flows 
+	according to the following rules:
+	* If the droplet would eventually fall by moving left, then move left.
+	* Otherwise, if the droplet would eventually fall by moving right, then 
+	  move right.
+	* Otherwise, rise to its current position.
+	Here, "eventually fall" means that the droplet will eventually be at a 
+	lower level if it moves in that direction. Also, level means the height of 
+	the terrain plus any water in that column. We can assume there is 
+	infinitely high terrain on the two sides out of bounds of the array. Also, 
+	there could not be partial water being spread out evenly on more than one 
+	grid block, and each unit of water has to be in exactly one block.
+
+	Example 1:
+	Input: heights = [2,1,1,2,1,2,2], volume = 4, k = 3
+	Output: [2,2,2,3,2,2,2]
+	Explanation: The first drop of water lands at index k = 3. When moving left 
+	             or right, the water can only move to the same level or a lower 
+	             level. (By level, we mean the total height of the terrain plus 
+	             any water in that column.) Since moving left will eventually 
+	             make it fall, it moves left. (A droplet "made to fall" means 
+	             go to a lower height than it was at previously.) Since moving 
+	             left will not make it fall, it stays in place. The next 
+	             droplet falls at index k = 3. Since the new droplet moving 
+	             left will eventually make it fall, it moves left. Notice that 
+	             the droplet still preferred to move left, even though it could 
+	             move right (and moving right makes it fall quicker.) The third 
+	             droplet falls at index k = 3. Since moving left would not 
+	             eventually make it fall, it tries to move right. Since moving 
+	             right would eventually make it fall, it moves right. Finally, 
+	             the fourth droplet falls at index k = 3. Since moving left 
+	             would not eventually make it fall, it tries to move right. 
+	             Since moving right would not eventually make it fall, it stays 
+	             in place.
+
+	Example 2:
+	Input: heights = [1,2,3,4], volume = 2, k = 2
+	Output: [2,3,3,4]
+	Explanation: The last droplet settles at index 1, since moving further left 
+	             would not cause it to eventually fall to a lower height.
+	
+	Example 3:
+	Input: heights = [3,1,3], volume = 5, k = 1
+	Output: [4,4,4]
+
+	Constraints:
+	* 1 <= heights.length <= 100
+	* 0 <= heights[i] <= 99
+	* 0 <= volume <= 2000
+	* 0 <= k < heights.length"""
+
+    def pourWater(self, heights: List[int], volume: int, k: int) -> List[int]:
+        left, right = [], [] # 2 stacks 
+        lo = hi = k
+        for _ in range(volume): 
+            
+            while lo and heights[lo-1] <= heights[lo]: 
+                if heights[lo-1] < heights[lo]: left.append(lo-1)
+                lo -= 1
+            while hi+1 < len(heights) and heights[hi] >= heights[hi+1]: 
+                if heights[hi] > heights[hi+1]: right.append(hi+1)
+                hi += 1
+                
+            if left: 
+                i = left[-1]
+                heights[i] += 1
+                if heights[i] == heights[i+1]: left.pop()
+                if lo <= i-1: left.append(i-1)
+            elif right: 
+                i = right[-1]
+                heights[i] += 1
+                if heights[i-1] == heights[i]: right.pop()
+                if i+1 <= hi: right.append(i+1)
+            else: 
+                heights[k] += 1
+                if k and heights[k-1] < heights[k]: left.append(k-1)
+                if k+1 < len(heights) and heights[k] > heights[k+1]: right.append(k+1)
+        return heights
+
+
     """756. Pyramid Transition Matrix (Medium)
 	We are stacking blocks to form a pyramid. Each block has a color which is a 
 	one letter string. We are allowed to place any color block C on top of two 
