16817dfb6db6ff07ab6c31413c8c83dbd884023c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-01 21:37:43 -0400

    solve'em all (1989)

--- leetcode.cpp
+++ leetcode.cpp
@@ -21439,6 +21439,65 @@ public:
         }
         return (f0 + f1 + (f0 || binary[0] == '0')) % MOD; 
     }
+
+
+    /*1989. Maximum Number of People That Can Be Caught in Tag (Medium)
+	You are playing a game of tag with your friends. In tag, people are divided 
+	into two teams: people who are "it", and people who are not "it". The 
+	people who are "it" want to catch as many people as possible who are not 
+	"it". You are given a 0-indexed integer array team containing only zeros 
+	(denoting people who are not "it") and ones (denoting people who are "it"), 
+	and an integer dist. A person who is "it" at index i can catch any one 
+	person whose index is in the range [i - dist, i + dist] (inclusive) and is 
+	not "it". Return the maximum number of people that the people who are "it" 
+	can catch.
+
+	Example 1:
+	Input: team = [0,1,0,1,0], dist = 3
+	Output: 2
+	Explanation: The person who is "it" at index 1 can catch people in the 
+	             range [i-dist, i+dist] = [1-3, 1+3] = [-2, 4]. They can catch 
+	             the person who is not "it" at index 2. The person who is "it" 
+	             at index 3 can catch people in the range [i-dist, i+dist] = 
+	             [3-3, 3+3] = [0, 6]. They can catch the person who is not "it" 
+	             at index 0. The person who is not "it" at index 4 will not be 
+	             caught because the people at indices 1 and 3 are already 
+	             catching one person.
+	
+	Example 2:
+	Input: team = [1], dist = 1
+	Output: 0
+	Explanation: There are no people who are not "it" to catch.
+	
+	Example 3:
+	Input: team = [0], dist = 1
+	Output: 0
+	Explanation: There are no people who are "it" to catch people.
+
+	Constraints:
+	* 1 <= team.length <= 10^5
+	* 0 <= team[i] <= 1
+	* 1 <= dist <= team.length*/
+
+    int catchMaximumAmountofPeople(vector<int>& team, int dist) {
+        int ans = 0; 
+        queue<int> q0, q1; 
+        for (int i = 0; i < team.size(); ++i) {
+            if (q0.size() && q0.front() < i-dist) q0.pop(); 
+            if (q1.size() && q1.front() < i-dist) q1.pop(); 
+            if (team[i]) 
+                if (q0.size()) {
+                    ++ans; 
+                    q0.pop(); 
+                } else q1.push(i); 
+            else 
+                if (q1.size()) {
+                    ++ans; 
+                    q1.pop(); 
+                } else q0.push(i); 
+        }
+        return ans; 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -43215,6 +43215,63 @@ class UnionFind:
         return (f0 + f1 + int("0" in binary)) % 1_000_000_007
 
 
+    """1989. Maximum Number of People That Can Be Caught in Tag (Medium)
+	You are playing a game of tag with your friends. In tag, people are divided 
+	into two teams: people who are "it", and people who are not "it". The 
+	people who are "it" want to catch as many people as possible who are not 
+	"it". You are given a 0-indexed integer array team containing only zeros 
+	(denoting people who are not "it") and ones (denoting people who are "it"), 
+	and an integer dist. A person who is "it" at index i can catch any one 
+	person whose index is in the range [i - dist, i + dist] (inclusive) and is 
+	not "it". Return the maximum number of people that the people who are "it" 
+	can catch.
+
+	Example 1:
+	Input: team = [0,1,0,1,0], dist = 3
+	Output: 2
+	Explanation: The person who is "it" at index 1 can catch people in the 
+	             range [i-dist, i+dist] = [1-3, 1+3] = [-2, 4]. They can catch 
+	             the person who is not "it" at index 2. The person who is "it" 
+	             at index 3 can catch people in the range [i-dist, i+dist] = 
+	             [3-3, 3+3] = [0, 6]. They can catch the person who is not "it" 
+	             at index 0. The person who is not "it" at index 4 will not be 
+	             caught because the people at indices 1 and 3 are already 
+	             catching one person.
+	
+	Example 2:
+	Input: team = [1], dist = 1
+	Output: 0
+	Explanation: There are no people who are not "it" to catch.
+	
+	Example 3:
+	Input: team = [0], dist = 1
+	Output: 0
+	Explanation: There are no people who are "it" to catch people.
+
+	Constraints:
+	* 1 <= team.length <= 10^5
+	* 0 <= team[i] <= 1
+	* 1 <= dist <= team.length"""
+
+    def catchMaximumAmountofPeople(self, team: List[int], dist: int) -> int:
+        ans = 0 
+        q0, q1 = deque(), deque()
+        for i, x in enumerate(team): 
+            if q0 and q0[0] < i-dist: q0.popleft()
+            if q1 and q1[0] < i-dist: q1.popleft()
+            if x == 0: 
+                if q1: 
+                    ans += 1
+                    q1.popleft()
+                else: q0.append(i)
+            else: 
+                if q0: 
+                    ans += 1
+                    q0.popleft()
+                else: q1.append(i)
+        return ans 
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
