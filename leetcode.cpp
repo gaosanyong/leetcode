56b51bc874462901b1b47247fb355f1a74b608c0
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-07 09:37:09 -0400

    September LeetCoding Challenge 2021-09-07 (206)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2184,6 +2184,41 @@ public:
     }
 
 
+    /*206. Reverse Linked List (Easy)
+	Given the head of a singly linked list, reverse the list, and return the 
+	reversed list.
+
+	Example 1:
+	Input: head = [1,2,3,4,5]
+	Output: [5,4,3,2,1]
+
+	Example 2:
+	Input: head = [1,2]
+	Output: [2,1]
+
+	Example 3:
+	Input: head = []
+	Output: []
+
+	Constraints:
+	* The number of nodes in the list is the range [0, 5000].
+	* -5000 <= Node.val <= 5000
+
+	Follow up: A linked list can be reversed either iteratively or recursively. 
+	           Could you implement both?*/
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr, *node = head; 
+        while (node) {
+            ListNode* temp = node->next; 
+            node->next = prev; 
+            prev = node; 
+            node = temp; 
+        }
+        return prev; 
+    }
+
+
     /*234. Palindrome Linked List (Easy)
 	Given the head of a singly linked list, return true if it is a palindrome.
 
