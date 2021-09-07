334d0ede66dfc45dd75739db33b713d37253da17
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-07 17:16:02 -0400

    solve'em all (708)

--- leetcode.cpp
+++ leetcode.cpp
@@ -6228,6 +6228,62 @@ public:
     }
 
 
+    /*708. Insert into a Sorted Circular Linked List (Medium)
+	Given a Circular Linked List node, which is sorted in ascending order, 
+	write a function to insert a value insertVal into the list such that it 
+	remains a sorted circular list. The given node can be a reference to any 
+	single node in the list and may not necessarily be the smallest value in 
+	the circular list. If there are multiple suitable places for insertion, 
+	you may choose any place to insert the new value. After the insertion, the 
+	circular list should remain sorted. If the list is empty (i.e., the given 
+	node is null), you should create a new single circular list and return the 
+	reference to that single node. Otherwise, you should return the originally 
+	given node.
+
+	Example 1:
+	Input: head = [3,4,1], insertVal = 2
+	Output: [3,4,1,2]
+	Explanation: In the figure above, there is a sorted circular list of three 
+	             elements. You are given a reference to the node with value 3, 
+	             and we need to insert 2 into the list. The new node should be 
+	             inserted between node 1 and node 3. After the insertion, the 
+	             list should look like this, and we should still return node 3.
+
+	Example 2:
+	Input: head = [], insertVal = 1
+	Output: [1]
+	Explanation: The list is empty (given head is null). We create a new single 
+	             circular list and return the reference to that single node.
+	
+	Example 3:
+	Input: head = [1], insertVal = 0
+	Output: [1,0]
+
+	Constraints:
+	* 0 <= Number of Nodes <= 5 * 10^4
+	* -10^6 <= Node.val, insertVal <= 10^6*/
+
+    Node* insert(Node* head, int insertVal) {
+        Node* temp = new Node(insertVal); 
+        if (!head) {
+            temp->next = temp; 
+            return temp; 
+        }
+        
+        Node* prev = head, *node = head->next; 
+        while (true) {
+            if (prev->val <= insertVal && insertVal <= node->val) break; 
+            if (prev->val > node->val && (insertVal < node->val || prev->val < insertVal)) break; 
+            prev = node; 
+            node = node->next; 
+            if (prev == head) break; 
+        }
+        prev->next = temp; 
+        temp->next = node; 
+        return head; 
+    }
+
+
     /*709. To Lower Case (Easy)
 	Implement function ToLowerCase() that has a string parameter str, and 
 	returns the same string in lowercase.
--- leetcode.py
+++ leetcode.py
@@ -14362,6 +14362,57 @@ class UnionFind:
         return -1
 
 
+    """708. Insert into a Sorted Circular Linked List (Medium)
+	Given a Circular Linked List node, which is sorted in ascending order, 
+	write a function to insert a value insertVal into the list such that it 
+	remains a sorted circular list. The given node can be a reference to any 
+	single node in the list and may not necessarily be the smallest value in 
+	the circular list. If there are multiple suitable places for insertion, 
+	you may choose any place to insert the new value. After the insertion, the 
+	circular list should remain sorted. If the list is empty (i.e., the given 
+	node is null), you should create a new single circular list and return the 
+	reference to that single node. Otherwise, you should return the originally 
+	given node.
+
+	Example 1:
+	Input: head = [3,4,1], insertVal = 2
+	Output: [3,4,1,2]
+	Explanation: In the figure above, there is a sorted circular list of three 
+	             elements. You are given a reference to the node with value 3, 
+	             and we need to insert 2 into the list. The new node should be 
+	             inserted between node 1 and node 3. After the insertion, the 
+	             list should look like this, and we should still return node 3.
+
+	Example 2:
+	Input: head = [], insertVal = 1
+	Output: [1]
+	Explanation: The list is empty (given head is null). We create a new single 
+	             circular list and return the reference to that single node.
+	
+	Example 3:
+	Input: head = [1], insertVal = 0
+	Output: [1,0]
+
+	Constraints:
+	* 0 <= Number of Nodes <= 5 * 10^4
+	* -10^6 <= Node.val, insertVal <= 10^6"""
+
+    def insert(self, head: 'Node', insertVal: int) -> 'Node':
+        temp = Node(insertVal)
+        if not head: 
+            temp.next = temp
+            return temp
+        prev, node = head, head.next
+        while True: 
+            if prev.val <= insertVal <= node.val: break 
+            if prev.val > node.val and (insertVal < node.val or prev.val < insertVal): break
+            prev, node = node, node.next
+            if prev == head: break 
+        prev.next = temp
+        temp.next = node
+        return head
+
+
     """709. To Lower Case (Easy)
 	Given a string s, return the string after replacing every uppercase letter 
 	with the same lowercase letter.
