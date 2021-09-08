d0d78de63fab1e03c9dec801a81cee021df14367
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-08 10:44:01 -0400

    September LeetCoding Challenge 2021-09-08 (848)

--- leetcode.cpp
+++ leetcode.cpp
@@ -8327,6 +8327,43 @@ public:
     }
 
 
+    /*848. Shifting Letters (Medium)
+	You are given a string s of lowercase English letters and an integer array 
+	shifts of the same length. Call the shift() of a letter, the next letter in 
+	the alphabet, (wrapping around so that 'z' becomes 'a'). 
+
+	* For example, shift('a') = 'b', shift('t') = 'u', and shift('z') = 'a'.
+
+	Now for each shifts[i] = x, we want to shift the first i + 1 letters of s, 
+	x times. Return the final string after all such shifts to s are applied.
+
+	Example 1:
+	Input: s = "abc", shifts = [3,5,9]
+	Output: "rpl"
+	Explanation: We start with "abc".
+	After shifting the first 1 letters of s by 3, we have "dbc".
+	After shifting the first 2 letters of s by 5, we have "igc".
+	After shifting the first 3 letters of s by 9, we have "rpl", the answer.
+
+	Example 2:
+	Input: s = "aaa", shifts = [1,2,3]
+	Output: "gfd"
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s consists of lowercase English letters.
+	* shifts.length == s.length
+	* 0 <= shifts[i] <= 10^9*/
+
+    string shiftingLetters(string s, vector<int>& shifts) {
+        for (int i = s.size()-1; i >= 0; --i) {
+            if (i+1 < s.size()) shifts[i] = (shifts[i] + shifts[i+1]) % 26; 
+            s[i] = (s[i] - 'a' + shifts[i]) % 26 + 'a'; 
+        }
+        return s; 
+    }
+
+
     /*850. Rectangle Area II (Hard)
 	We are given a list of (axis-aligned) rectangles. Each 
 	rectangle[i] = [xi1, yi1, xi2, yi2], where (xi1, yi1) are the coordinates 
