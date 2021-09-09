51d09875fbdc15554a2fa2420059354efb3ced60
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-09 14:51:55 -0400

    solve'em all (681)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5927,6 +5927,52 @@ public:
     }
 
 
+    /*681. Next Closest Time (Medium)
+	Given a time represented in the format "HH:MM", form the next closest time 
+	by reusing the current digits. There is no limit on how many times a digit 
+	can be reused. You may assume the given input string is always valid. For 
+	example, "01:34", "12:09" are all valid. "1:34", "12:9" are all invalid.
+
+	Example 1:
+	Input: time = "19:34"
+	Output: "19:39"
+	Explanation: The next closest time choosing from digits 1, 9, 3, 4, is 
+	             19:39, which occurs 5 minutes later. It is not 19:33, because 
+	             this occurs 23 hours and 59 minutes later.
+	
+	Example 2:
+	Input: time = "23:59"
+	Output: "22:22"
+	Explanation: The next closest time choosing from digits 2, 3, 5, 9, is 
+	             22:22. It may be assumed that the returned time is next day's 
+	             time since it is smaller than the input time numerically.
+
+	Constraints:
+	* time.length == 5
+	* time is a valid time in the form "HH:MM".
+	* 0 <= HH < 24
+	* 0 <= MM < 60*/
+
+    string nextClosestTime(string time) {
+        vector<char> digit = {time[0], time[1], time[3], time[4]}; 
+        sort(digit.begin(), digit.end()); 
+        
+        vector<char> upper = {'2', '9', '0', '5', '9'}; 
+        if (time[0] == '2') upper[1] = '3'; 
+        
+        string suffix; 
+        for (int i = 4; i >= 0; --i) 
+            if (i == 2) suffix = ":" + suffix; 
+            else {
+                auto k = find(digit.begin(), digit.end(), time[i]); 
+                for (auto it = next(k); it != digit.end(); ++it) 
+                    if (time[i] < *it && *it <= upper[i]) return time.substr(0, i) + *it + suffix; 
+                suffix = digit[0] + suffix; 
+            }
+        return suffix;
+    }
+
+
     /*682. Baseball Game (Easy)
 	You are keeping score for a baseball game with strange rules. The game 
 	consists of several rounds, where the scores of past rounds may affect 
--- leetcode.py
+++ leetcode.py
@@ -14120,6 +14120,49 @@ class UnionFind:
         return True 
 
 
+    """681. Next Closest Time (Medium)
+	Given a time represented in the format "HH:MM", form the next closest time 
+	by reusing the current digits. There is no limit on how many times a digit 
+	can be reused. You may assume the given input string is always valid. For 
+	example, "01:34", "12:09" are all valid. "1:34", "12:9" are all invalid.
+
+	Example 1:
+	Input: time = "19:34"
+	Output: "19:39"
+	Explanation: The next closest time choosing from digits 1, 9, 3, 4, is 
+	             19:39, which occurs 5 minutes later. It is not 19:33, because 
+	             this occurs 23 hours and 59 minutes later.
+	
+	Example 2:
+	Input: time = "23:59"
+	Output: "22:22"
+	Explanation: The next closest time choosing from digits 2, 3, 5, 9, is 
+	             22:22. It may be assumed that the returned time is next day's 
+	             time since it is smaller than the input time numerically.
+
+	Constraints:
+	* time.length == 5
+	* time is a valid time in the form "HH:MM".
+	* 0 <= HH < 24
+	* 0 <= MM < 60"""
+
+    def nextClosestTime(self, time: str) -> str:
+        digit = sorted([time[0], time[1], time[3], time[4]])
+        
+        upper = ["2", "9", None, "5", "9"]
+        if time[0] == "2": upper[1] = "3"
+        
+        suffix = ""
+        for i in reversed(range(5)): 
+            if i == 2: suffix = ":" + suffix
+            else: 
+                k = digit.index(time[i])
+                for kk in range(k+1, 4): 
+                    if time[i] < digit[kk] <= upper[i]: return time[:i] + digit[kk] + suffix
+                suffix = digit[0] + suffix
+        return suffix
+
+
     """686. Repeated String Match (Medium)
 	Given two strings a and b, return the minimum number of times you should 
 	repeat string a so that string b is a substring of it. If it is impossible 
