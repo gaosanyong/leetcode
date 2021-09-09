0c31092e36c0551e67c50e5208f707635734b581
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-08 22:25:19 -0400

    solve'em all (1152)

--- leetcode.cpp
+++ leetcode.cpp
@@ -12244,6 +12244,87 @@ public:
     }
 
 
+    /*1152. Analyze User Website Visit Pattern (Medium)
+	You are given two string arrays username and website and an integer array 
+	timestamp. All the given arrays are of the same length and the tuple 
+	[username[i], website[i], timestamp[i]] indicates that the user username[i] 
+	visited the website website[i] at time timestamp[i]. A pattern is a list of 
+	three websites (not necessarily distinct).
+
+	* For example, ["home", "away", "love"], ["leetcode", "love", "leetcode"], 
+	  and ["luffy", "luffy", "luffy"] are all patterns.
+	The score of a pattern is the number of users that visited all the websites 
+	in the pattern in the same order they appeared in the pattern.
+	* For example, if the pattern is ["home", "away", "love"], the score is the 
+	  number of users x such that x visited "home" then visited "away" and 
+	  visited "love" after that.
+	* Similarly, if the pattern is ["leetcode", "love", "leetcode"], the score 
+	  is the number of users x such that x visited "leetcode" then visited 
+	  "love" and visited "leetcode" one more time after that.
+	* Also, if the pattern is ["luffy", "luffy", "luffy"], the score is the 
+	  number of users x such that x visited "luffy" three different times at 
+	  different timestamps.
+	Return the pattern with the largest score. If there is more than one 
+	pattern with the same largest score, return the lexicographically smallest 
+	such pattern.
+
+	Example 1:
+	Input: username = ["joe","joe","joe","james","james","james","james","mary","mary","mary"], 
+	       timestamp = [1,2,3,4,5,6,7,8,9,10], 
+	       website = ["home","about","career","home","cart","maps","home","home","about","career"]
+	Output: ["home","about","career"]
+	Explanation: The tuples in this example are:
+	["joe","home",1],["joe","about",2],["joe","career",3],["james","home",4],["james","cart",5],["james","maps",6],["james","home",7],["mary","home",8],["mary","about",9], and ["mary","career",10].
+	The pattern ("home", "about", "career") has score 2 (joe and mary).
+	The pattern ("home", "cart", "maps") has score 1 (james).
+	The pattern ("home", "cart", "home") has score 1 (james).
+	The pattern ("home", "maps", "home") has score 1 (james).
+	The pattern ("cart", "maps", "home") has score 1 (james).
+	The pattern ("home", "home", "home") has score 0 (no user visited home 3 times).
+	
+	Example 2:
+	Input: username = ["ua","ua","ua","ub","ub","ub"], 
+	       timestamp = [1,2,3,4,5,6], 
+	       website = ["a","b","a","a","b","c"]
+	Output: ["a","b","a"]
+
+	Constraints:
+	* 3 <= username.length <= 50
+	* 1 <= username[i].length <= 10
+	* timestamp.length == username.length
+	* 1 <= timestamp[i] <= 109
+	* website.length == username.length
+	* 1 <= website[i].length <= 10
+	* username[i] and website[i] consist of lowercase English letters.
+	* It is guaranteed that there is at least one user who visited at least three websites.
+	* All the tuples [username[i], timestamp[i], website[i]] are unique.*/
+
+    vector<string> mostVisitedPattern(vector<string>& username, vector<int>& timestamp, vector<string>& website) {
+        unordered_map<string, map<int, string>> mp; 
+        for (int i = 0; i < username.size(); ++i) 
+            mp[username[i]][timestamp[i]] = website[i]; 
+        
+        unordered_map<string, int> freq; 
+        for (auto& [key, v] : mp) {
+            unordered_set<string> seen; 
+            for (auto i = v.begin(); i != v.end(); ++i) 
+                for (auto j = next(i); j != v.end(); ++j) 
+                    for (auto k = next(j); k != v.end(); ++k) 
+                        seen.insert(i->second + "#" + j->second + "#" + k->second); 
+            for (auto& x : seen) freq[x]++; 
+        }
+        string ans; 
+        int most = 0; 
+        for (auto& [k, v] : freq) 
+            if (v > most || (v == most && k < ans)) {
+                ans = k; 
+                most = v; 
+            }
+        int i = ans.find("#"), j = ans.find("#", i+1); 
+        return {ans.substr(0, i), ans.substr(i+1, j-i-1), ans.substr(j+1)}; 
+    }
+
+
     /*1168. Optimize Water Distribution in a Village (Hard)
 	There are n houses in a village. We want to supply water for all the houses 
 	by building wells and laying pipes. For each house i, we can either build a 
--- leetcode.py
+++ leetcode.py
@@ -22946,6 +22946,78 @@ class UnionFind:
         return k - ans
 
 
+    """1152. Analyze User Website Visit Pattern (Medium)
+	You are given two string arrays username and website and an integer array 
+	timestamp. All the given arrays are of the same length and the tuple 
+	[username[i], website[i], timestamp[i]] indicates that the user username[i] 
+	visited the website website[i] at time timestamp[i]. A pattern is a list of 
+	three websites (not necessarily distinct).
+
+	* For example, ["home", "away", "love"], ["leetcode", "love", "leetcode"], 
+	  and ["luffy", "luffy", "luffy"] are all patterns.
+	The score of a pattern is the number of users that visited all the websites 
+	in the pattern in the same order they appeared in the pattern.
+	* For example, if the pattern is ["home", "away", "love"], the score is the 
+	  number of users x such that x visited "home" then visited "away" and 
+	  visited "love" after that.
+	* Similarly, if the pattern is ["leetcode", "love", "leetcode"], the score 
+	  is the number of users x such that x visited "leetcode" then visited 
+	  "love" and visited "leetcode" one more time after that.
+	* Also, if the pattern is ["luffy", "luffy", "luffy"], the score is the 
+	  number of users x such that x visited "luffy" three different times at 
+	  different timestamps.
+	Return the pattern with the largest score. If there is more than one 
+	pattern with the same largest score, return the lexicographically smallest 
+	such pattern.
+
+	Example 1:
+	Input: username = ["joe","joe","joe","james","james","james","james","mary","mary","mary"], 
+	       timestamp = [1,2,3,4,5,6,7,8,9,10], 
+	       website = ["home","about","career","home","cart","maps","home","home","about","career"]
+	Output: ["home","about","career"]
+	Explanation: The tuples in this example are:
+	["joe","home",1],["joe","about",2],["joe","career",3],["james","home",4],["james","cart",5],["james","maps",6],["james","home",7],["mary","home",8],["mary","about",9], and ["mary","career",10].
+	The pattern ("home", "about", "career") has score 2 (joe and mary).
+	The pattern ("home", "cart", "maps") has score 1 (james).
+	The pattern ("home", "cart", "home") has score 1 (james).
+	The pattern ("home", "maps", "home") has score 1 (james).
+	The pattern ("cart", "maps", "home") has score 1 (james).
+	The pattern ("home", "home", "home") has score 0 (no user visited home 3 times).
+	
+	Example 2:
+	Input: username = ["ua","ua","ua","ub","ub","ub"], 
+	       timestamp = [1,2,3,4,5,6], 
+	       website = ["a","b","a","a","b","c"]
+	Output: ["a","b","a"]
+
+	Constraints:
+	* 3 <= username.length <= 50
+	* 1 <= username[i].length <= 10
+	* timestamp.length == username.length
+	* 1 <= timestamp[i] <= 109
+	* website.length == username.length
+	* 1 <= website[i].length <= 10
+	* username[i] and website[i] consist of lowercase English letters.
+	* It is guaranteed that there is at least one user who visited at least three websites.
+	* All the tuples [username[i], timestamp[i], website[i]] are unique."""
+
+    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        mp = {}
+        for t, u, w in sorted(zip(timestamp, username, website)): 
+            mp.setdefault(u, []).append(w)
+        
+        freq = defaultdict(int)
+        for key, val in mp.items(): 
+            seen = set()
+            for i in range(len(val)):
+                for j in range(i+1, len(val)):
+                    for k in range(j+1, len(val)): 
+                        seen.add((val[i], val[j], val[k]))
+            for x in seen: freq[x] += 1
+        
+        return min(freq, key=lambda x: (-freq[x], x))
+
+
     """1160. Find Words That Can Be Formed by Characters (Easy)
 	You are given an array of strings words and a string chars. A string is 
 	good if it can be formed by characters from chars (each character can only 
