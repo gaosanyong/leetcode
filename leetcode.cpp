6b88818f0f74fc97ba996252b39a589804ee54fe
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-09 15:59:36 -0400

    solve'em all (776)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7331,6 +7331,43 @@ public:
     }
 
 
+    /*776. Split BST (Medium)
+	Given the root of a binary search tree (BST) and an integer target, split 
+	the tree into two subtrees where one subtree has nodes that are all smaller 
+	or equal to the target value, while the other subtree has all nodes that 
+	are greater than the target value. It Is not necessarily the case that the 
+	tree contains a node with the value target. Additionally, most of the 
+	structure of the original tree should remain. Formally, for any child c 
+	with parent p in the original tree, if they are both in the same subtree 
+	after the split, then node c should still have the parent p. Return an 
+	array of the two roots of the two subtrees.
+
+	Example 1:
+	Input: root = [4,2,6,1,3,5,7], target = 2
+	Output: [[2,1],[4,3,6,null,null,5,7]]
+
+	Example 2:
+	Input: root = [1], target = 1
+	Output: [[1],[]]
+
+	Constraints:
+	* The number of nodes in the tree is in the range [1, 50].
+	* 0 <= Node.val, target <= 1000*/
+
+    vector<TreeNode*> splitBST(TreeNode* root, int target) {
+        if (!root) return {nullptr, nullptr}; 
+        if (root->val <= target) {
+            auto nodes = splitBST(root->right, target); 
+            root->right = nodes[0];
+            return {root, nodes[1]}; 
+        } else {
+            auto nodes = splitBST(root->left, target); 
+            root->left = nodes[1]; 
+            return {nodes[0], root}; 
+        }
+    }
+
+
     /*778. Swim in Rising Water (Hard)
 	On an N x N grid, each square grid[i][j] represents the elevation at that 
 	point (i,j). Now rain starts to fall. At time t, the depth of the water 
--- leetcode.py
+++ leetcode.py
@@ -15802,6 +15802,46 @@ class UnionFind:
         return -1 
 
 
+    """776. Split BST (Medium)
+	Given the root of a binary search tree (BST) and an integer target, split 
+	the tree into two subtrees where one subtree has nodes that are all smaller 
+	or equal to the target value, while the other subtree has all nodes that 
+	are greater than the target value. It Is not necessarily the case that the 
+	tree contains a node with the value target. Additionally, most of the 
+	structure of the original tree should remain. Formally, for any child c 
+	with parent p in the original tree, if they are both in the same subtree 
+	after the split, then node c should still have the parent p. Return an 
+	array of the two roots of the two subtrees.
+
+	Example 1:
+	Input: root = [4,2,6,1,3,5,7], target = 2
+	Output: [[2,1],[4,3,6,null,null,5,7]]
+
+	Example 2:
+	Input: root = [1], target = 1
+	Output: [[1],[]]
+
+	Constraints:
+	* The number of nodes in the tree is in the range [1, 50].
+	* 0 <= Node.val, target <= 1000"""
+
+    def splitBST(self, root: Optional[TreeNode], target: int) -> List[Optional[TreeNode]]:
+        
+        def fn(node):
+            """Return splitted BST."""
+            if not node: return None, None
+            if node.val <= target: 
+                left, right = fn(node.right)
+                node.right = left
+                return node, right
+            else: 
+                left, right = fn(node.left)
+                node.left = right
+                return left, node
+                
+        return fn(root)
+
+
     """777. Swap Adjacent in LR String (Medium)
 	In a string composed of 'L', 'R', and 'X' characters, like "RXXLRXRXL", a 
 	move consists of either replacing one occurrence of "XL" with "LX", or 
