89eeda3ce02db146db352654a04a9bf1b56b5582
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-09 21:08:46 -0400

    solve'em all (439)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3463,6 +3463,58 @@ public:
     }
 
 
+    /*439. Ternary Expression Parser (Medium)
+	Given a string expression representing arbitrarily nested ternary 
+	expressions, evaluate the expression, and return the result of it. You can 
+	always assume that the given expression is valid and only contains digits, 
+	'?', ':', 'T', and 'F' where 'T' is true and 'F' is false. All the numbers 
+	in the expression are one-digit numbers (i.e., in the range [0, 9]). The 
+	conditional expressions group right-to-left (as usual in most languages), 
+	and the result of the expression will always evaluate to either a digit, 
+	'T' or 'F'.
+
+	Example 1:
+	Input: expression = "T?2:3"
+	Output: "2"
+	Explanation: If true, then result is 2; otherwise result is 3.
+
+	Example 2:
+	Input: expression = "F?1:T?4:5"
+	Output: "4"
+	Explanation: The conditional expressions group right-to-left. Using 
+	             parenthesis, it is read/evaluated as: 
+	             "(F ? 1 : (T ? 4 : 5))" --> "(F ? 1 : 4)" --> "4" 
+	             or "(F ? 1 : (T ? 4 : 5))" --> "(T ? 4 : 5)" --> "4"
+	
+	Example 3:
+	Input: expression = "T?T?F:5:3"
+	Output: "F"
+	Explanation: The conditional expressions group right-to-left. Using 
+	             parenthesis, it is read/evaluated as:
+	             "(T ? (T ? F : 5) : 3)" --> "(T ? F : 3)" --> "F"
+	             "(T ? (T ? F : 5) : 3)" --> "(T ? F : 5)" --> "F"
+
+	Constraints:
+	* 5 <= expression.length <= 10^4
+	* expression consists of digits, 'T', 'F', '?', and ':'.
+	* It is guaranteed that expression is a valid ternary expression and that 
+	  each number is a one-digit number.*/
+
+    string parseTernary(string expression) {
+        stack<char> stk; 
+        for (int i = expression.size()-1; i >= 0; --i) 
+            if (stk.size() && stk.top() == '?') {
+                stk.pop(); 
+                char t = stk.top(); stk.pop(); 
+                char f = stk.top(); stk.pop(); 
+                if (expression[i] == 'T') stk.push(t); 
+                else stk.push(f); 
+            } else if (expression[i] != ':') 
+                stk.push(expression[i]); 
+        return string(1, stk.top()); 
+    }
+
+
     /*446. Arithmetic Slices II - Subsequence (Hard)
 	Given an integer array nums, return the number of all the arithmetic 
 	subsequences of nums. A sequence of numbers is called arithmetic if it 
--- leetcode.py
+++ leetcode.py
@@ -11224,6 +11224,56 @@ class UnionFind:
         return ans 
 
 
+    """439. Ternary Expression Parser (Medium)
+	Given a string expression representing arbitrarily nested ternary 
+	expressions, evaluate the expression, and return the result of it. You can 
+	always assume that the given expression is valid and only contains digits, 
+	'?', ':', 'T', and 'F' where 'T' is true and 'F' is false. All the numbers 
+	in the expression are one-digit numbers (i.e., in the range [0, 9]). The 
+	conditional expressions group right-to-left (as usual in most languages), 
+	and the result of the expression will always evaluate to either a digit, 
+	'T' or 'F'.
+
+	Example 1:
+	Input: expression = "T?2:3"
+	Output: "2"
+	Explanation: If true, then result is 2; otherwise result is 3.
+
+	Example 2:
+	Input: expression = "F?1:T?4:5"
+	Output: "4"
+	Explanation: The conditional expressions group right-to-left. Using 
+	             parenthesis, it is read/evaluated as: 
+	             "(F ? 1 : (T ? 4 : 5))" --> "(F ? 1 : 4)" --> "4" 
+	             or "(F ? 1 : (T ? 4 : 5))" --> "(T ? 4 : 5)" --> "4"
+	
+	Example 3:
+	Input: expression = "T?T?F:5:3"
+	Output: "F"
+	Explanation: The conditional expressions group right-to-left. Using 
+	             parenthesis, it is read/evaluated as:
+	             "(T ? (T ? F : 5) : 3)" --> "(T ? F : 3)" --> "F"
+	             "(T ? (T ? F : 5) : 3)" --> "(T ? F : 5)" --> "F"
+
+	Constraints:
+	* 5 <= expression.length <= 10^4
+	* expression consists of digits, 'T', 'F', '?', and ':'.
+	* It is guaranteed that expression is a valid ternary expression and that 
+	  each number is a one-digit number."""
+
+    def parseTernary(self, expression: str) -> str:
+        stack = []
+        for ch in reversed(expression): 
+            if stack and stack[-1] == "?":  
+                stack.pop() # pop out "?"
+                t = stack.pop()
+                f = stack.pop()
+                if ch == "T": stack.append(t)
+                else: stack.append(f)
+            elif ch != ":": stack.append(ch)
+        return stack[-1]
+
+
     """446. Arithmetic Slices II - Subsequence (Hard)
 	Given an integer array nums, return the number of all the arithmetic 
 	subsequences of nums. A sequence of numbers is called arithmetic if it 
