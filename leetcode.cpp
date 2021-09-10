46786299cf3fc58d21322aebceb6fbf7dd0e0482
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-09 22:42:37 -0400

    solve'em all (1181)

--- leetcode.cpp
+++ leetcode.cpp
@@ -12710,6 +12710,70 @@ public:
     }
 
 
+    /*1181. Before and After Puzzle (Medium)
+	Given a list of phrases, generate a list of Before and After puzzles. A 
+	phrase is a string that consists of lowercase English letters and spaces 
+	only. No space appears in the start or the end of a phrase. There are no 
+	consecutive spaces in a phrase. Before and After puzzles are phrases that 
+	are formed by merging two phrases where the last word of the first phrase 
+	is the same as the first word of the second phrase. Return the Before and 
+	After puzzles that can be formed by every two phrases phrases[i] and 
+	phrases[j] where i != j. Note that the order of matching two phrases 
+	matters, we want to consider both orders. You should return a list of 
+	distinct strings sorted lexicographically.
+
+	Example 1:
+	Input: phrases = ["writing code","code rocks"]
+	Output: ["writing code rocks"]
+
+	Example 2:
+	Input: phrases = ["mission statement",
+	                  "a quick bite to eat",
+	                  "a chip off the old block",
+	                  "chocolate bar",
+	                  "mission impossible",
+	                  "a man on a mission",
+	                  "block party",
+	                  "eat my words",
+	                  "bar of soap"]
+	Output: ["a chip off the old block party",
+	         "a man on a mission impossible",
+	         "a man on a mission statement",
+	         "a quick bite to eat my words",
+	         "chocolate bar of soap"]
+
+	Example 3:
+	Input: phrases = ["a","b","a"]
+	Output: ["a"]
+
+	Constraints:
+	* 1 <= phrases.length <= 100
+	* 1 <= phrases[i].length <= 100*/
+
+    vector<string> beforeAndAfterPuzzles(vector<string>& phrases) {
+        unordered_set<string> st; 
+        unordered_map<string, vector<int>> prefix, suffix; 
+        
+        for (int i = 0; i < phrases.size(); ++i) {
+            int k = phrases[i].find(" "), kk = phrases[i].rfind(" "); 
+            string first = k == string::npos ? phrases[i] : phrases[i].substr(0, k); 
+            string last = kk == string::npos ? phrases[i] : phrases[i].substr(kk+1); 
+            for (auto& j : suffix[first]) 
+                st.insert(phrases[j] + phrases[i].substr(first.size())); 
+            
+            for (auto& j : prefix[last]) 
+                st.insert(phrases[i] + phrases[j].substr(last.size())); 
+            
+            prefix[first].push_back(i); 
+            suffix[last].push_back(i); 
+        }
+        
+        vector<string> ans(st.begin(), st.end()); 
+        sort(ans.begin(), ans.end()); 
+        return ans; 
+    }
+
+
     /*1182. Shortest Distance to Target Color (Medium)
 	You are given an array colors, in which there are three colors: 1, 2 and 3.
 	You are also given some queries. Each query consists of two integers i and 
--- leetcode.py
+++ leetcode.py
@@ -23727,6 +23727,61 @@ class UnionFind:
         return ans 
 
 
+    """1181. Before and After Puzzle (Medium)
+	Given a list of phrases, generate a list of Before and After puzzles. A 
+	phrase is a string that consists of lowercase English letters and spaces 
+	only. No space appears in the start or the end of a phrase. There are no 
+	consecutive spaces in a phrase. Before and After puzzles are phrases that 
+	are formed by merging two phrases where the last word of the first phrase 
+	is the same as the first word of the second phrase. Return the Before and 
+	After puzzles that can be formed by every two phrases phrases[i] and 
+	phrases[j] where i != j. Note that the order of matching two phrases 
+	matters, we want to consider both orders. You should return a list of 
+	distinct strings sorted lexicographically.
+
+	Example 1:
+	Input: phrases = ["writing code","code rocks"]
+	Output: ["writing code rocks"]
+
+	Example 2:
+	Input: phrases = ["mission statement",
+	                  "a quick bite to eat",
+	                  "a chip off the old block",
+	                  "chocolate bar",
+	                  "mission impossible",
+	                  "a man on a mission",
+	                  "block party",
+	                  "eat my words",
+	                  "bar of soap"]
+	Output: ["a chip off the old block party",
+	         "a man on a mission impossible",
+	         "a man on a mission statement",
+	         "a quick bite to eat my words",
+	         "chocolate bar of soap"]
+
+	Example 3:
+	Input: phrases = ["a","b","a"]
+	Output: ["a"]
+
+	Constraints:
+	* 1 <= phrases.length <= 100
+	* 1 <= phrases[i].length <= 100"""
+
+    def beforeAndAfterPuzzles(self, phrases: List[str]) -> List[str]:
+        ans = set()
+        prefix = {}
+        suffix = {}
+        for i, x in enumerate(phrases): 
+            words = x.split()
+            for j in prefix.get(words[-1], []): 
+                ans.add(x[:-len(words[-1])] + phrases[j])
+            for j in suffix.get(words[0], []): 
+                ans.add(phrases[j] + x[len(words[0]):])
+            prefix.setdefault(words[0], []).append(i)
+            suffix.setdefault(words[-1], []).append(i)
+        return sorted(ans)
+
+
     """1182. Shortest Distance to Target Color (Medium)
 	You are given an array colors, in which there are three colors: 1, 2 and 3.
 	You are also given some queries. Each query consists of two integers i and 
