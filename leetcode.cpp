f116b54344bcf6b19af4c2a8d0151a76831f87fe
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-09 21:43:05 -0400

    solve'em all (737)

--- leetcode.cpp
+++ leetcode.cpp
@@ -6933,6 +6933,96 @@ public:
     }
 
 
+    /*737. Sentence Similarity II (Medium)
+	We can represent a sentence as an array of words, for example, the sentence 
+	"I am happy with leetcode" can be represented as 
+	arr = ["I","am",happy","with","leetcode"]. Given two sentences sentence1 
+	and sentence2 each represented as a string array and given an array of 
+	string pairs similarPairs where similarPairs[i] = [xi, yi] indicates that 
+	the two words xi and yi are similar. Return true if sentence1 and sentence2 
+	are similar, or false if they are not similar. Two sentences are similar if:
+	* They have the same length (i.e., the same number of words)
+	* sentence1[i] and sentence2[i] are similar.
+	Notice that a word is always similar to itself, also notice that the 
+	similarity relation is transitive. For example, if the words a and b are 
+	similar, and the words b and c are similar, then a and c are similar.
+
+	Example 1:
+	Input: sentence1 = ["great","acting","skills"], 
+	       sentence2 = ["fine","drama","talent"], 
+	       similarPairs = [["great","good"],["fine","good"],["drama","acting"],["skills","talent"]]
+	Output: true
+	Explanation: The two sentences have the same length and each word i of 
+	             sentence1 is also similar to the corresponding word in 
+	             sentence2.
+	
+	Example 2:
+	Input: sentence1 = ["I","love","leetcode"], 
+	       sentence2 = ["I","love","onepiece"], 
+	       similarPairs = [["manga","onepiece"],["platform","anime"],["leetcode","platform"],["anime","manga"]]
+	Output: true
+	Explanation: "leetcode" --> "platform" --> "anime" --> "manga" --> "onepiece".
+	             Since "leetcode is similar to "onepiece" and the first two 
+	             words are the same, the two sentences are similar.
+	
+	Example 3:
+	Input: sentence1 = ["I","love","leetcode"], 
+	       sentence2 = ["I","love","onepiece"], 
+	       similarPairs = [["manga","hunterXhunter"],["platform","anime"],["leetcode","platform"],["anime","manga"]]
+	Output: false
+	Explanation: "leetcode" is not similar to "onepiece".
+
+	Constraints:
+	* 1 <= sentence1.length, sentence2.length <= 1000
+	* 1 <= sentence1[i].length, sentence2[i].length <= 20
+	* sentence1[i] and sentence2[i] consist of lower-case and upper-case English letters.
+	* 0 <= similarPairs.length <= 2000
+	* similarPairs[i].length == 2
+	* 1 <= xi.length, yi.length <= 20
+	* xi and yi consist of English letters.
+
+	class UnionFind {
+	    unordered_map<string, string> parent; 
+	    unordered_map<string, int> rank; 
+	public: 
+	    string find(string p) {
+	        if (!parent.count(p)) {
+	            parent[p] = p; 
+	            rank[p] = 1; 
+	        }
+	        if (p != parent[p]) 
+	            parent[p] = find(parent[p]); 
+	        return parent[p]; 
+	    }
+	    
+	    bool connect(string p, string q) {
+	        string prt = find(p), qrt = find(q); 
+	        if (prt == qrt) return false; 
+	        if (rank[prt] > rank[qrt]) swap(prt, qrt); 
+	        parent[prt] = qrt; 
+	        rank[qrt] += rank[prt]; 
+	        return true; 
+	    }
+	};*/
+
+    bool areSentencesSimilarTwo(vector<string>& sentence1, vector<string>& sentence2, vector<vector<string>>& similarPairs) {
+        if (sentence1.size() != sentence2.size()) return false; 
+        
+        UnionFind *uf = new UnionFind(); 
+        for (auto& sp : similarPairs) 
+            uf->connect(sp[0], sp[1]); 
+        
+        bool ans = true; 
+        for (int i = 0; i < sentence1.size(); ++i) 
+            if (uf->find(sentence1[i]) != uf->find(sentence2[i])) {
+                ans = false; 
+                break; 
+            }
+        delete uf; 
+        return ans; 
+    }
+
+
     /*742. Closest Leaf in a Binary Tree (Medium)
 	Given the root of a binary tree where every node has a unique value and a 
 	target integer k, return the value of the nearest leaf node to the target k 
--- leetcode.py
+++ leetcode.py
@@ -15047,6 +15047,84 @@ class UnionFind:
         return True 
 
 
+    """737. Sentence Similarity II (Medium)
+	We can represent a sentence as an array of words, for example, the sentence 
+	"I am happy with leetcode" can be represented as 
+	arr = ["I","am",happy","with","leetcode"]. Given two sentences sentence1 
+	and sentence2 each represented as a string array and given an array of 
+	string pairs similarPairs where similarPairs[i] = [xi, yi] indicates that 
+	the two words xi and yi are similar. Return true if sentence1 and sentence2 
+	are similar, or false if they are not similar. Two sentences are similar if:
+	* They have the same length (i.e., the same number of words)
+	* sentence1[i] and sentence2[i] are similar.
+	Notice that a word is always similar to itself, also notice that the 
+	similarity relation is transitive. For example, if the words a and b are 
+	similar, and the words b and c are similar, then a and c are similar.
+
+	Example 1:
+	Input: sentence1 = ["great","acting","skills"], 
+	       sentence2 = ["fine","drama","talent"], 
+	       similarPairs = [["great","good"],["fine","good"],["drama","acting"],["skills","talent"]]
+	Output: true
+	Explanation: The two sentences have the same length and each word i of 
+	             sentence1 is also similar to the corresponding word in 
+	             sentence2.
+	
+	Example 2:
+	Input: sentence1 = ["I","love","leetcode"], 
+	       sentence2 = ["I","love","onepiece"], 
+	       similarPairs = [["manga","onepiece"],["platform","anime"],["leetcode","platform"],["anime","manga"]]
+	Output: true
+	Explanation: "leetcode" --> "platform" --> "anime" --> "manga" --> "onepiece".
+	             Since "leetcode is similar to "onepiece" and the first two 
+	             words are the same, the two sentences are similar.
+	
+	Example 3:
+	Input: sentence1 = ["I","love","leetcode"], 
+	       sentence2 = ["I","love","onepiece"], 
+	       similarPairs = [["manga","hunterXhunter"],["platform","anime"],["leetcode","platform"],["anime","manga"]]
+	Output: false
+	Explanation: "leetcode" is not similar to "onepiece".
+
+	Constraints:
+	* 1 <= sentence1.length, sentence2.length <= 1000
+	* 1 <= sentence1[i].length, sentence2[i].length <= 20
+	* sentence1[i] and sentence2[i] consist of lower-case and upper-case English letters.
+	* 0 <= similarPairs.length <= 2000
+	* similarPairs[i].length == 2
+	* 1 <= xi.length, yi.length <= 20
+	* xi and yi consist of English letters.
+
+	class UnionFind: 
+
+	    def __init__(self): 
+	        self.parent = {}
+	        self.rank = defaultdict(lambda: 1)
+	        
+	    def find(self, p): 
+	        if p not in self.parent: self.parent[p] = p
+	        if p != self.parent[p]: 
+	            self.parent[p] = self.find(self.parent[p])
+	        return self.parent[p]
+	    
+	    def union(self, p, q): 
+	        prt, qrt = self.find(p), self.find(q)
+	        if prt == qrt: return False
+	        if self.rank[prt] > self.rank[qrt]: prt, qrt = qrt, prt
+	        self.parent[prt] = qrt
+	        self.rank[qrt] += self.rank[prt]
+	        return True"""
+
+    def areSentencesSimilarTwo(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        if len(sentence1) != len(sentence2): return False
+        
+        uf = UnionFind()
+        for x, y in similarPairs: uf.union(x, y)
+        for x, y in zip(sentence1, sentence2): 
+            if uf.find(x) != uf.find(y): return False 
+        return True 
+
+
     """739. Daily Temperatures (Medium)
 	Given a list of daily temperatures T, return a list such that, for each day 
 	in the input, tells you how many days you would have to wait until a warmer 
