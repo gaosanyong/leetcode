8e6fa0309bc9417cb2d60d2092a512b140552b0a
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-14 08:50:48 -0400

    September LeetCoding Challenge 2021-09-14 (917)

--- leetcode.py
+++ leetcode.py
@@ -18869,6 +18869,42 @@ class UnionFind:
         return [x for x in A if not freq - Counter(x)]
 
 
+    """917. Reverse Only Letters (Easy)
+	Given a string s, reverse the string according to the following rules:
+	* All the characters that are not English letters remain in the same position.
+	* All the English letters (lowercase or uppercase) should be reversed.
+	Return s after reversing it.
+
+	Example 1:
+	Input: s = "ab-cd"
+	Output: "dc-ba"
+
+	Example 2:
+	Input: s = "a-bC-dEf-ghIj"
+	Output: "j-Ih-gfE-dCba"
+
+	Example 3:
+	Input: s = "Test1ng-Leet=code-Q!"
+	Output: "Qedo1ct-eeLg=ntse-T!"
+
+	Constraints:
+	* 1 <= s.length <= 100
+	* s consists of characters with ASCII values in the range [33, 122].
+	* s does not contain '\"' or '\\'."""
+
+    def reverseOnlyLetters(self, s: str) -> str:
+        s = list(s)
+        lo, hi = 0, len(s)-1
+        while lo < hi: 
+            if not s[lo].isalpha(): lo += 1
+            elif not s[hi].isalpha(): hi -= 1
+            else: 
+                s[lo], s[hi] = s[hi], s[lo]
+                lo += 1
+                hi -= 1
+        return "".join(s)
+
+
     """920. Number of Music Playlists (Hard)
 	Your music player contains n different songs and she wants to listen to 
 	goal (not necessarily different) songs during your trip.  You create a 
