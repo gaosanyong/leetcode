3657f11ab0c7fb776387ea3464c6cfb01c1aa01f
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-14 12:47:48 -0400

    solve'em all (882)

--- leetcode.cpp
+++ leetcode.cpp
@@ -9515,6 +9515,78 @@ public:
     }
 
 
+    /*882. Reachable Nodes In Subdivided Graph (Hard)
+	You are given an undirected graph (the "original graph") with n nodes 
+	labeled from 0 to n - 1. You decide to subdivide each edge in the graph 
+	into a chain of nodes, with the number of new nodes varying between each 
+	edge. The graph is given as a 2D array of edges where 
+	edges[i] = [ui, vi, cnti] indicates that there is an edge between nodes ui 
+	and vi in the original graph, and cnti is the total number of new nodes 
+	that you will subdivide the edge into. Note that cnti == 0 means you will 
+	not subdivide the edge. To subdivide the edge [ui, vi], replace it with 
+	(cnti + 1) new edges and cnti new nodes. The new nodes are x1, x2, ..., 
+	xcnti, and the new edges are [ui, x1], [x1, x2], [x2, x3], ..., 
+	[xcnti11, xcnti], [xcnti, vi]. In this new graph, you want to know how many 
+	nodes are reachable from the node 0, where a node is reachable if the 
+	distance is maxMoves or less. Given the original graph and maxMoves, return 
+	the number of nodes that are reachable from node 0 in the new graph.
+
+	Example 1:
+	Input: edges = [[0,1,10],[0,2,1],[1,2,2]], maxMoves = 6, n = 3
+	Output: 13
+	Explanation: The edge subdivisions are shown in the image above. The nodes 
+	             that are reachable are highlighted in yellow.
+
+	Example 2:
+	Input: edges = [[0,1,4],[1,2,6],[0,2,8],[1,3,1]], maxMoves = 10, n = 4
+	Output: 23
+	
+	Example 3:
+	Input: edges = [[1,2,4],[1,4,5],[1,3,1],[2,3,4],[3,4,5]], maxMoves = 17, n = 5
+	Output: 1
+	Explanation: Node 0 is disconnected from the rest of the graph, so only 
+	             node 0 is reachable.
+
+	Constraints:
+	* 0 <= edges.length <= min(n * (n - 1) / 2, 10^4)
+	* edges[i].length == 3
+	* 0 <= ui < vi < n
+	* There are no multiple edges in the graph.
+	* 0 <= cnti <= 10^4
+	* 0 <= maxMoves <= 10^9
+	* 1 <= n <= 3000*/
+
+    int reachableNodes(vector<vector<int>>& edges, int maxMoves, int n) {
+        unordered_map<int, unordered_map<int, int>> graph; 
+        for (auto& e : edges) 
+            graph[e[0]][e[1]] = graph[e[1]][e[0]] = e[2]; 
+        
+        int ans = 0; 
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq; 
+        pq.emplace(0, 0); 
+        
+        vector<bool> seen(n); 
+        vector<vector<int>> used(n, vector<int>(n)); 
+        
+        while (pq.size()) {
+            auto [x, u] = pq.top(); pq.pop(); 
+            if (!seen[u]) {
+                ++ans; 
+                seen[u] = true; 
+                for (auto& [v, w] : graph[u]) 
+                    if (!used[u][v]) {
+                        if (used[v][u] < graph[v][u]) {
+                            used[u][v] = min(maxMoves - x, graph[v][u] - used[v][u]); 
+                            ans += used[u][v]; 
+                        }
+                        if (x + w + 1 <= maxMoves && !seen[v]) pq.emplace(x + w + 1, v); 
+                    }
+            }
+        }
+        return ans; 
+    }
+
+
     /*883. Projection Area of 3D Shapes (Easy)
 	You are given an n x n grid where we place some 1 x 1 x 1 cubes that are 
 	axis-aligned with the x, y, and z axes. Each value v = grid[i][j] 
--- leetcode.py
+++ leetcode.py
@@ -18419,6 +18419,71 @@ class UnionFind:
         return ans + (lo == hi)
 
 
+    """882. Reachable Nodes In Subdivided Graph (Hard)
+	You are given an undirected graph (the "original graph") with n nodes 
+	labeled from 0 to n - 1. You decide to subdivide each edge in the graph 
+	into a chain of nodes, with the number of new nodes varying between each 
+	edge. The graph is given as a 2D array of edges where 
+	edges[i] = [ui, vi, cnti] indicates that there is an edge between nodes ui 
+	and vi in the original graph, and cnti is the total number of new nodes 
+	that you will subdivide the edge into. Note that cnti == 0 means you will 
+	not subdivide the edge. To subdivide the edge [ui, vi], replace it with 
+	(cnti + 1) new edges and cnti new nodes. The new nodes are x1, x2, ..., 
+	xcnti, and the new edges are [ui, x1], [x1, x2], [x2, x3], ..., 
+	[xcnti11, xcnti], [xcnti, vi]. In this new graph, you want to know how many 
+	nodes are reachable from the node 0, where a node is reachable if the 
+	distance is maxMoves or less. Given the original graph and maxMoves, return 
+	the number of nodes that are reachable from node 0 in the new graph.
+
+	Example 1:
+	Input: edges = [[0,1,10],[0,2,1],[1,2,2]], maxMoves = 6, n = 3
+	Output: 13
+	Explanation: The edge subdivisions are shown in the image above. The nodes 
+	             that are reachable are highlighted in yellow.
+
+	Example 2:
+	Input: edges = [[0,1,4],[1,2,6],[0,2,8],[1,3,1]], maxMoves = 10, n = 4
+	Output: 23
+	
+	Example 3:
+	Input: edges = [[1,2,4],[1,4,5],[1,3,1],[2,3,4],[3,4,5]], maxMoves = 17, n = 5
+	Output: 1
+	Explanation: Node 0 is disconnected from the rest of the graph, so only 
+	             node 0 is reachable.
+
+	Constraints:
+	* 0 <= edges.length <= min(n * (n - 1) / 2, 10^4)
+	* edges[i].length == 3
+	* 0 <= ui < vi < n
+	* There are no multiple edges in the graph.
+	* 0 <= cnti <= 10^4
+	* 0 <= maxMoves <= 10^9
+	* 1 <= n <= 3000"""
+
+    def reachableNodes(self, edges: List[List[int]], maxMoves: int, n: int) -> int:
+        graph = defaultdict(dict)
+        for u, v, w in edges: graph[u][v] = graph[v][u] = w
+        
+        ans = 0
+        pq = [(0, 0)] # min-heap 
+        
+        seen = [False] * n
+        used = defaultdict(int)
+        
+        while pq: 
+            x, u = heappop(pq)
+            if not seen[u]: 
+                ans += 1
+                seen[u] = True 
+                for v, c in graph[u].items(): 
+                    if not used[u, v]: 
+                        if used[v, u] < graph[v][u]: 
+                            used[u, v] = min(maxMoves - x, graph[v][u] - used[v, u])
+                            ans += used[u, v]
+                        if x + c + 1 <= maxMoves and not seen[v]: heappush(pq, (x + c + 1, v))
+        return ans 
+
+
     """889. Construct Binary Tree from Preorder and Postorder Traversal (Medium)
 	Return any binary tree that matches the given preorder and postorder 
 	traversals. Values in the traversals pre and post are distinct positive 
