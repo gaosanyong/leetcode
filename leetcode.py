ef22f979e7f1d2f36361fedac8065f0933f13d2b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-15 09:05:21 -0400

    September LeetCoding Challenge 2021-09-15 (978)

--- leetcode.py
+++ leetcode.py
@@ -20058,6 +20058,46 @@ class UnionFind:
         return sum(odd)
 
 
+    """978. Longest Turbulent Subarray (Medium)
+	Given an integer array arr, return the length of a maximum size turbulent 
+	subarray of arr. A subarray is turbulent if the comparison sign flips 
+	between each adjacent pair of elements in the subarray. More formally, a 
+	subarray [arr[i], arr[i + 1], ..., arr[j]] of arr is said to be turbulent 
+	if and only if:
+	* For i <= k < j:
+	  + arr[k] > arr[k + 1] when k is odd, and
+	  + arr[k] < arr[k + 1] when k is even.
+	* Or, for i <= k < j:
+	  + arr[k] > arr[k + 1] when k is even, and
+	  + arr[k] < arr[k + 1] when k is odd.
+
+	Example 1:
+	Input: arr = [9,4,2,10,7,8,8,1,9]
+	Output: 5
+	Explanation: arr[1] > arr[2] < arr[3] > arr[4] < arr[5]
+
+	Example 2:
+	Input: arr = [4,8,12,16]
+	Output: 2
+
+	Example 3:
+	Input: arr = [100]
+	Output: 1
+
+	Constraints:
+	* 1 <= arr.length <= 4 * 10^4
+	* 0 <= arr[i] <= 10^9"""
+
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        ans = cnt = 0
+        for i in range(len(arr)): 
+            if i >= 2 and (arr[i-2] > arr[i-1] < arr[i] or arr[i-2] < arr[i-1] > arr[i]): cnt += 1
+            elif i >= 1 and arr[i-1] != arr[i]: cnt = 2
+            else: cnt = 1
+            ans = max(ans, cnt)
+        return ans 
+
+
     """980. Unique Paths III (Hard)
 	On a 2-dimensional grid, there are 4 types of squares:
 	* 1 represents the starting square.  There is exactly one starting square.
