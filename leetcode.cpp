1dafb838387a66ad98c21dae71c6b2abbf2c2032
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-15 16:19:40 -0400

    solve'em all (1000)

--- leetcode.cpp
+++ leetcode.cpp
@@ -11652,6 +11652,61 @@ public:
     }
 
 
+    /*1000. Minimum Cost to Merge Stones (Hard)
+	There are n piles of stones arranged in a row. The ith pile has stones[i] 
+	stones. A move consists of merging exactly k consecutive piles into one 
+	pile, and the cost of this move is equal to the total number of stones in 
+	these k piles. Return the minimum cost to merge all piles of stones into 
+	one pile. If it is impossible, return -1.
+
+	Example 1:
+	Input: stones = [3,2,4,1], k = 2
+	Output: 20
+	Explanation: We start with [3, 2, 4, 1].
+	             We merge [3, 2] for a cost of 5, and we are left with [5, 4, 1].
+	             We merge [4, 1] for a cost of 5, and we are left with [5, 5].
+	             We merge [5, 5] for a cost of 10, and we are left with [10].
+	             The total cost was 20, and this is the minimum possible.
+	
+	Example 2:
+	Input: stones = [3,2,4,1], k = 3
+	Output: -1
+	Explanation: After any merge operation, there are 2 piles left, and we 
+	             can't merge anymore.  So the task is impossible.
+	
+	Example 3:
+	Input: stones = [3,5,1,2,6], k = 3
+	Output: 25
+	Explanation: We start with [3, 5, 1, 2, 6].
+	             We merge [5, 1, 2] for a cost of 8, and we are left with [3, 8, 6].
+	             We merge [3, 8, 6] for a cost of 17, and we are left with [17].
+	             The total cost was 25, and this is the minimum possible.
+
+	Constraints:
+	* n == stones.length
+	* 1 <= n <= 30
+	* 1 <= stones[i] <= 100
+	* 2 <= k <= 30*/
+
+    int mergeStones(vector<int>& stones, int k) {
+        int n = stones.size(); 
+        if ((n-1) % (k-1)) return -1; // impossible 
+        
+        vector<int> prefix(1, 0); 
+        for (auto& x : stones) prefix.push_back(prefix.back() + x); 
+        
+        vector<vector<int>> dp(n, vector<int>(n+1)); // dp[lo][hi] min cost merging [lo:hi]
+        for (int lo = n-k; lo >= 0; --lo) 
+            for (int hi = lo+k; hi <= n; ++hi) {
+                dp[lo][hi] = INT_MAX; 
+                for (int mid = lo+1; mid < hi; mid += k-1) 
+                    dp[lo][hi] = min(dp[lo][hi], dp[lo][mid] + dp[mid][hi]); 
+                if ((hi-lo-1) % (k-1) == 0) dp[lo][hi] += prefix[hi] - prefix[lo]; 
+            }
+        return dp[0].back(); 
+    }
+
+
     /*1002. Find Common Characters (Easy)
 	Given an array words of strings made only from lowercase letters, return a 
 	list of all characters that show up in all strings within the list 
--- leetcode.py
+++ leetcode.py
@@ -20286,6 +20286,61 @@ class UnionFind:
         return ans
 
 
+    """1000. Minimum Cost to Merge Stones (Hard)
+	There are n piles of stones arranged in a row. The ith pile has stones[i] 
+	stones. A move consists of merging exactly k consecutive piles into one 
+	pile, and the cost of this move is equal to the total number of stones in 
+	these k piles. Return the minimum cost to merge all piles of stones into 
+	one pile. If it is impossible, return -1.
+
+	Example 1:
+	Input: stones = [3,2,4,1], k = 2
+	Output: 20
+	Explanation: We start with [3, 2, 4, 1].
+	             We merge [3, 2] for a cost of 5, and we are left with [5, 4, 1].
+	             We merge [4, 1] for a cost of 5, and we are left with [5, 5].
+	             We merge [5, 5] for a cost of 10, and we are left with [10].
+	             The total cost was 20, and this is the minimum possible.
+	
+	Example 2:
+	Input: stones = [3,2,4,1], k = 3
+	Output: -1
+	Explanation: After any merge operation, there are 2 piles left, and we 
+	             can't merge anymore.  So the task is impossible.
+	
+	Example 3:
+	Input: stones = [3,5,1,2,6], k = 3
+	Output: 25
+	Explanation: We start with [3, 5, 1, 2, 6].
+	             We merge [5, 1, 2] for a cost of 8, and we are left with [3, 8, 6].
+	             We merge [3, 8, 6] for a cost of 17, and we are left with [17].
+	             The total cost was 25, and this is the minimum possible.
+
+	Constraints:
+	* n == stones.length
+	* 1 <= n <= 30
+	* 1 <= stones[i] <= 100
+	* 2 <= k <= 30"""
+
+    def mergeStones(self, stones: List[int], k: int) -> int:
+        if (len(stones)-1) % (k-1): return -1 # impossible
+        
+        prefix = [0]
+        for x in stones: prefix.append(prefix[-1] + x)
+        
+        @cache
+        def fn(lo, hi): 
+            """Return min cost of merging stones[lo:hi]."""
+            if hi - lo < k: return 0 # not enough stones
+            ans = inf 
+            for mid in range(lo+1, hi, k-1): 
+                ans = min(ans, fn(lo, mid) + fn(mid, hi))
+            if (hi-lo-1) % (k-1) == 0: ans += prefix[hi] - prefix[lo]
+            return ans 
+        
+        return fn(0, len(stones))
+
+
     """1004. Max Consecutive Ones III (Medium)
 	Given a binary array nums and an integer k, return the maximum number of 
 	consecutive 1's in the array if you can flip at most k 0's.
