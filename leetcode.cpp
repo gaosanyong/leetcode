085d96825d55942ca1f85a6397f7d26cd5068d8c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-17 10:33:59 -0400

    solve'em all(887)

--- leetcode.cpp
+++ leetcode.cpp
@@ -9889,6 +9889,48 @@ public:
     }
 
 
+    /*887. Super Egg Drop (Hard)
+	You are given k identical eggs and you have access to a building with n 
+	floors labeled from 1 to n. You know that there exists a floor f where 
+	0 <= f <= n such that any egg dropped at a floor higher than f will break, 
+	and any egg dropped at or below floor f will not break. Each move, you may 
+	take an unbroken egg and drop it from any floor x (where 1 <= x <= n). If 
+	the egg breaks, you can no longer use it. However, if the egg does not 
+	break, you may reuse it in future moves. Return the minimum number of moves 
+	that you need to determine with certainty what the value of f is.
+
+	Example 1:
+	Input: k = 1, n = 2
+	Output: 2
+	Explanation: Drop the egg from floor 1. If it breaks, we know that f = 0.
+	             Otherwise, drop the egg from floor 2. If it breaks, we know 
+	             that f = 1. If it does not break, then we know f = 2. Hence, 
+	             we need at minimum 2 moves to determine with certainty what 
+	             the value of f is.
+	
+	Example 2:
+	Input: k = 2, n = 6
+	Output: 3
+
+	Example 3:
+	Input: k = 3, n = 14
+	Output: 4
+
+	Constraints:
+	* 1 <= k <= 100
+	* 1 <= n <= 10^4*/
+
+    int superEggDrop(int k, int n) {
+        vector<int> dp(k+1); 
+        for (int i = 1; i <= n; ++i) 
+            for (int j = k; j >= 1; --j) {
+                dp[j] += 1 + dp[j-1]; 
+                if (dp[j] >= n) return i; 
+            }
+        return -1; 
+    }
+
+
     /*888. Fair Candy Swap (Easy)
 	Alice and Bob have candy bars of different sizes: A[i] is the size of the 
 	i-th bar of candy that Alice has, and B[j] is the size of the j-th bar of 
--- leetcode.py
+++ leetcode.py
@@ -18623,6 +18623,48 @@ class UnionFind:
         return ans 
 
 
+    """887. Super Egg Drop (Hard)
+	You are given k identical eggs and you have access to a building with n 
+	floors labeled from 1 to n. You know that there exists a floor f where 
+	0 <= f <= n such that any egg dropped at a floor higher than f will break, 
+	and any egg dropped at or below floor f will not break. Each move, you may 
+	take an unbroken egg and drop it from any floor x (where 1 <= x <= n). If 
+	the egg breaks, you can no longer use it. However, if the egg does not 
+	break, you may reuse it in future moves. Return the minimum number of moves 
+	that you need to determine with certainty what the value of f is.
+
+	Example 1:
+	Input: k = 1, n = 2
+	Output: 2
+	Explanation: Drop the egg from floor 1. If it breaks, we know that f = 0.
+	             Otherwise, drop the egg from floor 2. If it breaks, we know 
+	             that f = 1. If it does not break, then we know f = 2. Hence, 
+	             we need at minimum 2 moves to determine with certainty what 
+	             the value of f is.
+	
+	Example 2:
+	Input: k = 2, n = 6
+	Output: 3
+
+	Example 3:
+	Input: k = 3, n = 14
+	Output: 4
+
+	Constraints:
+	* 1 <= k <= 100
+	* 1 <= n <= 10^4"""
+
+    def superEggDrop(self, k: int, n: int) -> int:
+        
+        @cache
+        def fn(m, k): 
+            """Return max floor reachable with m moves and k eggs."""
+            if m == 0 or k == 0: return 0 
+            return 1 + fn(m-1, k-1) + fn(m-1, k)
+        
+        return next(m for m in range(1, n+1) if fn(m, k) >= n)
+
+
     """889. Construct Binary Tree from Preorder and Postorder Traversal (Medium)
 	Return any binary tree that matches the given preorder and postorder 
 	traversals. Values in the traversals pre and post are distinct positive 
