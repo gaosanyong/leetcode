d4e68039b13a1f399707b29badb0434c9c772b70
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-20 08:44:24 -0400

    September LeetCoding Challenge 2021-09-20 (1275)

--- leetcode.py
+++ leetcode.py
@@ -26297,6 +26297,79 @@ class UnionFind:
         return fn(0)[1]
 
 
+    """1275. Find Winner on a Tic Tac Toe Game (Easy)
+	Tic-tac-toe is played by two players A and B on a 3 x 3 grid. Here are the 
+	rules of Tic-Tac-Toe:
+	* Players take turns placing characters into empty squares (" ").
+	* The first player A always places "X" characters, while the second player 
+	  B always places "O" characters.
+	* "X" and "O" characters are always placed into empty squares, never on 
+	  filled ones.
+	* The game ends when there are 3 of the same (non-empty) character filling 
+	  any row, column, or diagonal.
+	* The game also ends if all squares are non-empty.
+	* No more moves can be played if the game is over.
+	Given an array moves where each element is another array of size 2 
+	corresponding to the row and column of the grid where they mark their 
+	respective character in the order in which A and B play. Return the winner 
+	of the game if it exists (A or B), in case the game ends in a draw return 
+	"Draw", if there are still movements to play return "Pending". You can 
+	assume that moves is valid (It follows the rules of Tic-Tac-Toe), the grid 
+	is initially empty and A will play first.
+
+	Example 1:
+	Input: moves = [[0,0],[2,0],[1,1],[2,1],[2,2]]
+	Output: "A"
+	Explanation: "A" wins, he always plays first.
+	             "X  "    "X  "    "X  "    "X  "    "X  "
+	             "   " -> "   " -> " X " -> " X " -> " X "
+	             "   "    "O  "    "O  "    "OO "    "OOX"
+	
+	Example 2:
+	Input: moves = [[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]
+	Output: "B"
+	Explanation: "B" wins.
+	             "X  "    "X  "    "XX "    "XXO"    "XXO"    "XXO"
+	             "   " -> " O " -> " O " -> " O " -> "XO " -> "XO " 
+	             "   "    "   "    "   "    "   "    "   "    "O  "
+	
+	Example 3:
+	Input: moves = [[0,0],[1,1],[2,0],[1,0],[1,2],[2,1],[0,1],[0,2],[2,2]]
+	Output: "Draw"
+	Explanation: The game ends in a draw since there are no moves to make.
+	             "XXO"
+	             "OOX"
+	             "XOX"
+	
+	Example 4:
+	Input: moves = [[0,0],[1,1]]
+	Output: "Pending"
+	Explanation: The game has not finished yet.
+	             "X  "
+	             " O "
+	             "   "
+
+	Constraints:
+	* 1 <= moves.length <= 9
+	* moves[i].length == 2
+	* 0 <= moves[i][j] <= 2
+	* There are no repeated elements on moves.
+	* moves follow the rules of tic tac toe."""
+
+    def tictactoe(self, moves: List[List[int]]) -> str:
+        score = [[0]*8 for _ in range(2)]
+        
+        for p, (i, j) in enumerate(moves):
+            p %= 2
+            score[p][i] += 1
+            score[p][3+j] += 1
+            if i == j: score[p][6] += 1
+            if i+j == 2: score[p][7] += 1
+            if any(x == 3 for x in score[p]): return ["A", "B"][p]
+            
+        return "Pending" if len(moves) < 9 else "Draw"
+
+
     """1307. Verbal Arithmetic Puzzle (Hard)
 	Given an equation, represented by words on left side and the result on 
 	right side. You need to check if the equation is solvable under the 
