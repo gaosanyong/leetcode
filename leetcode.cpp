ce578e88d6be4bbdb0901da22cbc8b2aa072b2e2
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-21 10:35:23 -0400

    September LeetCoding Challenge 2021-09-21 (485)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3953,6 +3953,33 @@ public:
     }
 
 
+    /*485. Max Consecutive Ones (Easy)
+	Given a binary array nums, return the maximum number of consecutive 1's in 
+	the array.
+
+	Example 1:
+	Input: nums = [1,1,0,1,1,1]
+	Output: 3
+	Explanation: The first two digits or the last three digits are consecutive 
+	             1s. The maximum number of consecutive 1s is 3.
+	
+	Example 2:
+	Input: nums = [1,0,1,1,0,1]
+	Output: 2
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* nums[i] is either 0 or 1.*/
+
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans = 0, cnt = 0; 
+        for (auto &x : nums) 
+            if (x) ans = max(ans, ++cnt); 
+            else cnt = 0; 
+        return ans; 
+    }
+
+
     /*490. The Maze (Medium)
 	There is a ball in a maze with empty spaces (represented as 0) and walls 
 	(represented as 1). The ball can go through the empty spaces by rolling up, 
--- leetcode.py
+++ leetcode.py
@@ -11599,6 +11599,33 @@ class Trie:
         return ans 
 
 
+    """485. Max Consecutive Ones (Easy)
+	Given a binary array nums, return the maximum number of consecutive 1's in 
+	the array.
+
+	Example 1:
+	Input: nums = [1,1,0,1,1,1]
+	Output: 3
+	Explanation: The first two digits or the last three digits are consecutive 
+	             1s. The maximum number of consecutive 1s is 3.
+	
+	Example 2:
+	Input: nums = [1,0,1,1,0,1]
+	Output: 2
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* nums[i] is either 0 or 1."""
+
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        ans = cnt = 0 
+        for x in nums:  
+            if x: cnt += 1
+            else: cnt = 0 
+            ans = max(ans, cnt)
+        return ans 
+
+
     """487. Max Consecutive Ones II (Medium)
 	Given a binary array, find the maximum number of consecutive 1s in this 
 	array if you can flip at most one 0.
