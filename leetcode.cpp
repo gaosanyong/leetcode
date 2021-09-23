a21f7e77b3596b07447029a47c4417adae8a33d3
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-23 10:46:38 -0400

    September LeetCoding Challenge 2021-09-23 (1328)

--- leetcode.cpp
+++ leetcode.cpp
@@ -14022,6 +14022,55 @@ public:
     }
 
 
+    /*1328. Break a Palindrome (Medium)
+	Given a palindromic string of lowercase English letters palindrome, replace 
+	exactly one character with any lowercase English letter so that the 
+	resulting string is not a palindrome and that it is the lexicographically 
+	smallest one possible. Return the resulting string. If there is no way to 
+	replace a character to make it not a palindrome, return an empty string. A 
+	string a is lexicographically smaller than a string b (of the same length) 
+	if in the first position where a and b differ, a has a character strictly 
+	smaller than the corresponding character in b. For example, "abcc" is 
+	lexicographically smaller than "abcd" because the first position they 
+	differ is at the fourth character, and 'c' is smaller than 'd'.
+
+	Example 1:
+	Input: palindrome = "abccba"
+	Output: "aaccba"
+	Explanation: There are many ways to make "abccba" not a palindrome, such as 
+	             "zbccba", "aaccba", and "abacba". Of all the ways, "aaccba" is 
+	             the lexicographically smallest.
+	
+	Example 2:
+	Input: palindrome = "a"
+	Output: ""
+	Explanation: There is no way to replace a single character to make "a" not 
+	             a palindrome, so return an empty string.
+	
+	Example 3:
+	Input: palindrome = "aa"
+	Output: "ab"
+
+	Example 4:
+	Input: palindrome = "aba"
+	Output: "abb"
+
+	Constraints:
+	* 1 <= palindrome.length <= 1000
+	* palindrome consists of only lowercase English letters.*/
+
+    string breakPalindrome(string palindrome) {
+        if (palindrome.size() == 1) return ""; 
+        for (int i = 0; i < palindrome.size()/2; ++i) 
+            if (palindrome[i] != 'a') {
+                palindrome[i] = 'a'; 
+                return palindrome; 
+            }
+        palindrome.back() = 'b'; 
+        return palindrome; 
+    }
+
+
     /*1338. Reduce Array Size to The Half (Medium)
 	Given an array arr.  You can choose a set of integers and remove all the 
 	occurrences of these integers in the array. Return the minimum size of the 
--- leetcode.py
+++ leetcode.py
@@ -26687,6 +26687,54 @@ class UnionFind:
         return fn(0)
 
 
+    """1328. Break a Palindrome (Medium)
+	Given a palindromic string of lowercase English letters palindrome, replace 
+	exactly one character with any lowercase English letter so that the 
+	resulting string is not a palindrome and that it is the lexicographically 
+	smallest one possible. Return the resulting string. If there is no way to 
+	replace a character to make it not a palindrome, return an empty string. A 
+	string a is lexicographically smaller than a string b (of the same length) 
+	if in the first position where a and b differ, a has a character strictly 
+	smaller than the corresponding character in b. For example, "abcc" is 
+	lexicographically smaller than "abcd" because the first position they 
+	differ is at the fourth character, and 'c' is smaller than 'd'.
+
+	Example 1:
+	Input: palindrome = "abccba"
+	Output: "aaccba"
+	Explanation: There are many ways to make "abccba" not a palindrome, such as 
+	             "zbccba", "aaccba", and "abacba". Of all the ways, "aaccba" is 
+	             the lexicographically smallest.
+	
+	Example 2:
+	Input: palindrome = "a"
+	Output: ""
+	Explanation: There is no way to replace a single character to make "a" not 
+	             a palindrome, so return an empty string.
+	
+	Example 3:
+	Input: palindrome = "aa"
+	Output: "ab"
+
+	Example 4:
+	Input: palindrome = "aba"
+	Output: "abb"
+
+	Constraints:
+	* 1 <= palindrome.length <= 1000
+	* palindrome consists of only lowercase English letters."""
+
+    def breakPalindrome(self, palindrome: str) -> str:
+        if len(palindrome) == 1: return "" # edge case 
+        palindrome = list(palindrome)
+        for i in range(len(palindrome)//2): 
+            if palindrome[i] != "a": 
+                palindrome[i] = "a"
+                break
+        else: palindrome[-1] = "b"
+        return "".join(palindrome)
+
+
     """1329. Sort the Matrix Diagonally (Medium)
 	A matrix diagonal is a diagonal line of cells starting from some cell in 
 	either the topmost row or leftmost column and going in the bottom-right 
