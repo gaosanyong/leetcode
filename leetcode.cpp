da1a0010f2de76dbaeb4ae0f4bac4683da75461b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-24 10:35:21 -0400

    solve'em all (977)

--- leetcode.cpp
+++ leetcode.cpp
@@ -11511,6 +11511,41 @@ public:
     }
 
 
+    /*977. Squares of a Sorted Array (Easy)
+	Given an integer array nums sorted in non-decreasing order, return an array 
+	of the squares of each number sorted in non-decreasing order.
+
+	Example 1:
+	Input: nums = [-4,-1,0,3,10]
+	Output: [0,1,9,16,100]
+	Explanation: After squaring, the array becomes [16,1,0,9,100]. After 
+	             sorting, it becomes [0,1,9,16,100].
+	
+	Example 2:
+	Input: nums = [-7,-3,2,3,11]
+	Output: [4,9,9,49,121]
+
+	Constraints:
+	* 1 <= nums.length <= 10^4
+	* -10^4 <= nums[i] <= 10^4
+	* nums is sorted in non-decreasing order.
+
+	Follow up: Squaring each element and sorting the new array is very trivial, 
+	           could you find an O(n) solution using a different approach?*/
+
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size(); 
+        vector<int> ans(n); 
+        for (int i = n-1, lo = 0, hi = n-1; i >= 0; --i) {
+            if (abs(nums[lo]) >= abs(nums[hi])) 
+                ans[i] = pow(nums[lo++], 2); 
+            else
+                ans[i] = pow(nums[hi--], 2); 
+        }
+        return ans; 
+    }
+
+
     /*978. Longest Turbulent Subarray (Medium)
 	Given an integer array arr, return the length of a maximum size turbulent 
 	subarray of arr. A subarray is turbulent if the comparison sign flips 
--- leetcode.py
+++ leetcode.py
@@ -20206,6 +20206,41 @@ class UnionFind:
         return sum(odd)
 
 
+    """977. Squares of a Sorted Array (Easy)
+	Given an integer array nums sorted in non-decreasing order, return an array 
+	of the squares of each number sorted in non-decreasing order.
+
+	Example 1:
+	Input: nums = [-4,-1,0,3,10]
+	Output: [0,1,9,16,100]
+	Explanation: After squaring, the array becomes [16,1,0,9,100]. After 
+	             sorting, it becomes [0,1,9,16,100].
+	
+	Example 2:
+	Input: nums = [-7,-3,2,3,11]
+	Output: [4,9,9,49,121]
+
+	Constraints:
+	* 1 <= nums.length <= 10^4
+	* -10^4 <= nums[i] <= 10^4
+	* nums is sorted in non-decreasing order.
+
+	Follow up: Squaring each element and sorting the new array is very trivial, 
+	           could you find an O(n) solution using a different approach?"""
+
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = [0] * len(nums)
+        lo, hi = 0, len(nums)-1
+        for i in reversed(range(len(nums))):
+            if abs(nums[lo]) >= abs(nums[hi]): 
+                ans[i] = nums[lo] * nums[lo]
+                lo += 1
+            else: 
+                ans[i] = nums[hi] * nums[hi]
+                hi -= 1
+        return ans
+
+
     """978. Longest Turbulent Subarray (Medium)
 	Given an integer array arr, return the length of a maximum size turbulent 
 	subarray of arr. A subarray is turbulent if the comparison sign flips 
