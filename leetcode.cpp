c06521a158240b5e1717171829bb360dac0547db
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-24 16:27:00 -0400

    solve'em all (116)

--- leetcode.cpp
+++ leetcode.cpp
@@ -1817,6 +1817,57 @@ public:
     }
 
 
+    /*116. Populating Next Right Pointers in Each Node (Medium)
+	You are given a perfect binary tree where all leaves are on the same level, 
+	and every parent has two children. The binary tree has the following 
+	definition:
+	    struct Node {
+	      int val;
+	      Node *left;
+	      Node *right;
+	      Node *next;
+	    }
+	Populate each next pointer to point to its next right node. If there is no 
+	next right node, the next pointer should be set to NULL. Initially, all 
+	next pointers are set to NULL.
+
+	Example 1:
+	Input: root = [1,2,3,4,5,6,7]
+	Output: [1,#,2,3,#,4,5,6,7,#]
+	Explanation: Given the above perfect binary tree (Figure A), your function 
+	             should populate each next pointer to point to its next right 
+	             node, just like in Figure B. The serialized output is in level 
+	             order as connected by the next pointers, with '#' signifying 
+	             the end of each level.
+	
+	Example 2:
+	Input: root = []
+	Output: []
+
+	Constraints:
+	* The number of nodes in the tree is in the range [0, 2^12 - 1].
+	* -1000 <= Node.val <= 1000
+
+	Follow-up:
+	* You may only use constant extra space.
+	* The recursive approach is fine. You may assume implicit stack space does 
+	  not count as extra space for this problem.*/
+
+    Node* connect(Node* root) {
+        Node* head = root; 
+        while (head && head->left) {
+            Node* node = head; 
+            while (node) {
+                node->left->next = node->right; 
+                if (node->next) node->right->next = node->next->left; 
+                node = node->next; 
+            }
+            head = head->left; 
+        }
+        return root; 
+    }
+
+
     /*118. Pascal's Triangle (Easy)
 	Given an integer numRows, return the first numRows of Pascal's triangle. In 
 	Pascal's triangle, each number is the sum of the two numbers directly above 
