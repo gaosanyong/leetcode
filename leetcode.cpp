b1daabb350afe8f493cc8d74e8efa44b3a2796ad
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-24 10:14:32 -0400

    September LeetCoding Challenge 2021-09-24 (1137)

--- leetcode.cpp
+++ leetcode.cpp
@@ -13270,6 +13270,34 @@ public:
     }
 
 
+    /*1137. N-th Tribonacci Number (Easy)
+	The Tribonacci sequence Tn is defined as follows: 
+	* T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+	Given n, return the value of Tn.
+
+	Example 1:
+	Input: n = 4
+	Output: 4
+	Explanation: T_3 = 0 + 1 + 1 = 2
+	             T_4 = 1 + 1 + 2 = 4
+	
+	Example 2:
+	Input: n = 25
+	Output: 1389537
+
+	Constraints:
+	* 0 <= n <= 37
+	* The answer is guaranteed to fit within a 32-bit integer, ie. 
+	  answer <= 2^31 - 1.*/
+
+    int tribonacci(int n) {
+        long dp[3] = {0, 1, 1}; 
+        for (int i = 0; i < n; ++i) 
+            dp[i%3] = dp[0] + dp[1] + dp[2]; 
+        return dp[n%3]; 
+    }
+
+
     /*1151. Minimum Swaps to Group All 1's Together (Medium)
 	Given a binary array data, return the minimum number of swaps required to 
 	group all 1’s present in the array together in any place in the array.
--- leetcode.py
+++ leetcode.py
@@ -23430,15 +23430,14 @@ class UnionFind:
 
     """1137. N-th Tribonacci Number (Easy)
 	The Tribonacci sequence Tn is defined as follows: 
-	T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+	* T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
 	Given n, return the value of Tn.
 
 	Example 1:
 	Input: n = 4
 	Output: 4
-	Explanation:
-	T_3 = 0 + 1 + 1 = 2
-	T_4 = 1 + 1 + 2 = 4
+	Explanation: T_3 = 0 + 1 + 1 = 2
+	             T_4 = 1 + 1 + 2 = 4
 
 	Example 2:
 	Input: n = 25
@@ -23446,7 +23445,8 @@ class UnionFind:
 
 	Constraints:
 	* 0 <= n <= 37
-	* The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1."""
+	* The answer is guaranteed to fit within a 32-bit integer, ie. 
+	  answer <= 2^31 - 1."""
 
     def tribonacci(self, n: int) -> int:
         t0, t1, t2 = 0, 1, 1
