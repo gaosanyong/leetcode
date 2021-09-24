9c8592dd8ae90629aa92f8ee1cd69b7ab5729e99
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-24 11:02:58 -0400

    solve'em all (344)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3387,6 +3387,30 @@ public:
     }
 
 
+    /*344. Reverse String (Easy)
+	Write a function that reverses a string. The input string is given as an 
+	array of characters s.
+
+	Example 1:
+	Input: s = ["h","e","l","l","o"]
+	Output: ["o","l","l","e","h"]
+
+	Example 2:
+	Input: s = ["H","a","n","n","a","h"]
+	Output: ["h","a","n","n","a","H"]
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s[i] is a printable ascii character.
+
+	Follow up: Do not allocate extra space for another array. You must do this 
+	           by modifying the input array in-place with O(1) extra memory.*/
+
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(), s.end()); 
+    }
+
+
     /*350. Intersection of Two Arrays II (Easy)
 	Given two integer arrays nums1 and nums2, return an array of their 
 	intersection. Each element in the result must appear as many times as it 
--- leetcode.py
+++ leetcode.py
@@ -9814,6 +9814,33 @@ class UnionFind:
         return fn(n)
 
 
+    """344. Reverse String (Easy)
+	Write a function that reverses a string. The input string is given as an 
+	array of characters s.
+
+	Example 1:
+	Input: s = ["h","e","l","l","o"]
+	Output: ["o","l","l","e","h"]
+
+	Example 2:
+	Input: s = ["H","a","n","n","a","h"]
+	Output: ["h","a","n","n","a","H"]
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s[i] is a printable ascii character.
+
+	Follow up: Do not allocate extra space for another array. You must do this 
+	           by modifying the input array in-place with O(1) extra memory."""
+
+    def reverseString(self, s: List[str]) -> None:
+        lo, hi = 0, len(s)-1
+        while lo < hi: 
+            s[lo], s[hi] = s[hi], s[lo]
+            lo += 1
+            hi -= 1
+
+
     """347. Top K Frequent Elements (Medium)
 	Given a non-empty array of integers, return the k most frequent elements.
 
