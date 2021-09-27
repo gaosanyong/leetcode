9ab5b3eeb1acd400c9151999df4b5b69e14bc4d1
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-27 15:10:01 -0400

    update 189, 929

--- leetcode.cpp
+++ leetcode.cpp
@@ -2329,6 +2329,46 @@ public:
     }
 
 
+    /*189. Rotate Array (Medium)
+	Given an array, rotate the array to the right by k steps, where k is non-
+	negative.
+
+	Example 1:
+	Input: nums = [1,2,3,4,5,6,7], k = 3
+	Output: [5,6,7,1,2,3,4]
+	Explanation:
+	rotate 1 steps to the right: [7,1,2,3,4,5,6]
+	rotate 2 steps to the right: [6,7,1,2,3,4,5]
+	rotate 3 steps to the right: [5,6,7,1,2,3,4]
+	Example 2:
+
+	Input: nums = [-1,-100,3,99], k = 2
+	Output: [3,99,-1,-100]
+	Explanation: 
+	rotate 1 steps to the right: [99,-1,-100,3]
+	rotate 2 steps to the right: [3,99,-1,-100]
+	 
+
+	Constraints:
+
+	1 <= nums.length <= 105
+	-231 <= nums[i] <= 231 - 1
+	0 <= k <= 105
+	 
+
+	Follow up:
+
+	Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+	Could you do it in-place with O(1) extra space?*/
+
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        for (int i = 0; i < gcd(n, k); ++i) 
+            for (int ii = (i+k)%n; i != ii; ii = (ii+k)%n) 
+                swap(nums[i], nums[ii]); 
+    }
+
+
     /*204. Count Primes (Easy)
 	Count the number of prime numbers less than a non-negative number, n.
 
--- leetcode.py
+++ leetcode.py
@@ -19486,6 +19486,49 @@ class UnionFind:
         return flip 
 
 
+    """929. Unique Email Addresses (Easy)
+	Every valid email consists of a local name and a domain name, separated by 
+	the '@' sign. Besides lowercase letters, the email may contain one or more 
+	'.' or '+'. For example, in "alice@leetcode.com", "alice" is the local name, 
+	and "leetcode.com" is the domain name. If you add periods '.' between some 
+	characters in the local name part of an email address, mail sent there will 
+	be forwarded to the same address without dots in the local name. Note that 
+	this rule does not apply to domain names. For example, "alice.z@leetcode.com" 
+	and "alicez@leetcode.com" forward to the same email address. If you add a 
+	plus '+' in the local name, everything after the first plus sign will be 
+	ignored. This allows certain emails to be filtered. Note that this rule 
+	does not apply to domain names. For example, "m.y+name@email.com" will be 
+	forwarded to "my@email.com". It is possible to use both of these rules at 
+	the same time. Given an array of strings emails where we send one email to 
+	each email[i], return the number of different addresses that actually 
+	receive mails.
+
+	Example 1:
+	Input: emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+	Output: 2
+	Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails.
+
+	Example 2:
+	Input: emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+	Output: 3
+
+	Constraints:
+	* 1 <= emails.length <= 100
+	* 1 <= emails[i].length <= 100
+	* email[i] consist of lowercase English letters, '+', '.' and '@'.
+	* Each emails[i] contains exactly one '@' character.
+	* All local and domain names are non-empty.
+	* Local names do not start with a '+' character."""
+
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        seen = set()
+        for email in emails: 
+            local, domain = email.split("@")
+            local = local.split("+")[0].replace(".", "")
+            seen.add("@".join((local, domain)))
+        return len(seen)
+
+
     """930. Binary Subarrays With Sum (Medium)
 	In an array A of 0s and 1s, how many non-empty subarrays have sum S?
 
