d728a8bef0353b8f0b4fad781ba82422159c1872
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-29 18:23:41 -0400

    solve'em all (517)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4613,6 +4613,53 @@ public:
     }
 
 
+    /*517. Super Washing Machines (Hard)
+	You have n super washing machines on a line. Initially, each washing 
+	machine has some dresses or is empty. For each move, you could choose any m 
+	(1 <= m <= n) washing machines, and pass one dress of each washing machine 
+	to one of its adjacent washing machines at the same time. Given an integer 
+	array machines representing the number of dresses in each washing machine 
+	from left to right on the line, return the minimum number of moves to make 
+	all the washing machines have the same number of dresses. If it is not 
+	possible to do it, return -1.
+
+	Example 1:
+	Input: machines = [1,0,5]
+	Output: 3
+	Explanation: 1st move:    1     0 <-- 5    =>    1     1     4
+	             2nd move:    1 <-- 1 <-- 4    =>    2     1     3
+	             3rd move:    2     1 <-- 3    =>    2     2     2
+	
+	Example 2:
+	Input: machines = [0,3,0]
+	Output: 2
+	Explanation: 1st move:    0 <-- 3     0    =>    1     2     0
+	             2nd move:    1     2 --> 0    =>    1     1     1
+	
+	Example 3:
+	Input: machines = [0,2,0]
+	Output: -1
+	Explanation: It's impossible to make all three washing machines have the 
+	             same number of dresses.
+
+	Constraints:
+	* n == machines.length
+	* 1 <= n <= 10^4
+	* 0 <= machines[i] <= 10^5*/
+
+    int findMinMoves(vector<int>& machines) {
+        int total = accumulate(machines.begin(), machines.end(), 0); 
+        if (total % machines.size()) return -1; // impossible
+
+        int avg = total / machines.size(), ans = 0, prefix = 0; 
+        for (auto& x : machines) {
+            ans = max({ans, abs(prefix), x - avg}); 
+            prefix += x - avg; 
+        }
+        return ans; 
+    }
+
+
     /*522. Longest Uncommon Subsequence II (Medium)
 	Given an array of strings strs, return the length of the longest uncommon 
 	subsequence between them. If the longest uncommon subsequence does not 
