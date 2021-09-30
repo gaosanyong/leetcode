f501c0ee1191f5b401ba0899f409d9d18ce13b2f
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-09-30 12:56:45 -0400

    solve'em all (719)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7440,6 +7440,57 @@ public:
     }
 
 
+    /*719. Find K-th Smallest Pair Distance (Hard)
+	The distance of a pair of integers a and b is defined as the absolute 
+	difference between a and b. Given an integer array nums and an integer k, 
+	return the kth smallest distance among all the pairs nums[i] and nums[j] 
+	where 0 <= i < j < nums.length.
+
+	Example 1:
+	Input: nums = [1,3,1], k = 1
+	Output: 0
+	Explanation: Here are all the pairs: (1,3) -> 2
+	                                     (1,1) -> 0
+	                                     (3,1) -> 2
+	             Then the 1st smallest distance pair is (1,1), and its distance 
+	             is 0.
+	
+	Example 2:
+	Input: nums = [1,1,1], k = 2
+	Output: 0
+
+	Example 3:
+	Input: nums = [1,6,1], k = 3
+	Output: 5
+
+	Constraints:
+	* n == nums.length
+	* 2 <= n <= 10^4
+	* 0 <= nums[i] <= 10^6
+	* 1 <= k <= n * (n - 1) / 2*/
+
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end()); 
+        
+        auto fn = [&](int x) {
+            int ans = 0; 
+            for (int i = 0, ii = 0; i < nums.size(); ++i) {
+                for (; ii < i && nums[i] - nums[ii] > x; ++ii); 
+                ans += i - ii; 
+            }
+            return ans; 
+        }; 
+        
+        int lo = 0, hi = nums.back() - nums.front(); 
+        while (lo < hi) {
+            int mid = lo + (hi - lo)/2, y = fn(mid); 
+            if (y < k) lo = mid + 1; 
+            else hi = mid; 
+        }
+        return lo; 
+    }
+
+
     /*720. Longest Word in Dictionary (Easy)
 	Given an array of strings words representing an English Dictionary, return 
 	the longest word in words that can be built one character at a time by 
--- leetcode.py
+++ leetcode.py
@@ -15142,6 +15142,55 @@ class Trie:
         return "".join(s)
 
 
+    """719. Find K-th Smallest Pair Distance (Hard)
+	The distance of a pair of integers a and b is defined as the absolute 
+	difference between a and b. Given an integer array nums and an integer k, 
+	return the kth smallest distance among all the pairs nums[i] and nums[j] 
+	where 0 <= i < j < nums.length.
+
+	Example 1:
+	Input: nums = [1,3,1], k = 1
+	Output: 0
+	Explanation: Here are all the pairs: (1,3) -> 2
+	                                     (1,1) -> 0
+	                                     (3,1) -> 2
+	             Then the 1st smallest distance pair is (1,1), and its distance 
+	             is 0.
+	
+	Example 2:
+	Input: nums = [1,1,1], k = 2
+	Output: 0
+
+	Example 3:
+	Input: nums = [1,6,1], k = 3
+	Output: 5
+
+	Constraints:
+	* n == nums.length
+	* 2 <= n <= 10^4
+	* 0 <= nums[i] <= 10^6
+	* 1 <= k <= n * (n - 1) / 2"""
+
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        
+        def fn(x): 
+            """Return count of pairs <= x."""
+            ans = ii = 0
+            for i in range(len(nums)):
+                while ii < i and nums[i] - nums[ii] > x: ii += 1
+                ans += i - ii
+            return ans 
+        
+        lo, hi = 0, nums[-1] - nums[0]
+        while lo < hi: 
+            mid = lo + hi >> 1
+            kk = fn(mid)
+            if kk < k: lo = mid + 1
+            else: hi = mid
+        return lo
+
+
     """721. Accounts Merge (Medium)
 	Given a list of accounts where each element accounts[i] is a list of 
 	strings, where the first element accounts[i][0] is a name, and the rest of 
