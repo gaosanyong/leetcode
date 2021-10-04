cb938099c7a7366cfae2fa546e8cd4268f70fa0a
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-04 15:55:02 -0400

    update 21

--- leetcode.cpp
+++ leetcode.cpp
@@ -451,20 +451,17 @@ public:
 	*/
 
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode dummy(0); 
-        ListNode* node = &dummy; 
-        while (l1 and l2) {
+        ListNode dummy(0), *node = &dummy; 
+        while (l1 && l2) 
             if (l1->val < l2->val) {
-                node->next = l1; 
+                node = node->next = l1; 
                 l1 = l1->next; 
             } else {
-                node->next = l2; 
+                node = node->next = l2; 
                 l2 = l2->next; 
             }
-            node = node->next; 
-        }
         node->next = l1 ? l1 : l2; 
-        return dummy.next; 
+        return dummy.next;
     }
 
 
--- leetcode.py
+++ leetcode.py
@@ -696,20 +696,35 @@ class Solution:
 
 
     """21. Merge Two Sorted Lists (Easy)
-	Merge two sorted linked lists and return it as a new list. The new list 
+	Merge two sorted linked lists and return it as a sorted list. The list 
 	should be made by splicing together the nodes of the first two lists.
 
-	Example:
+	Example 1:
+	Input: l1 = [1,2,4], l2 = [1,3,4]
+	Output: [1,1,2,3,4,4]
 
-	Input: 1->2->4, 1->3->4
-	Output: 1->1->2->3->4->4"""
+	Example 2:
+	Input: l1 = [], l2 = []
+	Output: []
+
+	Example 3:
+	Input: l1 = [], l2 = [0]
+	Output: [0]
+
+	Constraints:
+	* The number of nodes in both lists is in the range [0, 50].
+	* -100 <= Node.val <= 100
+	* Both l1 and l2 are sorted in non-decreasing order."""
     
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = node = ListNode()
-        while l1 and l2: 
-            if l1.val > l2.val: l1, l2 = l2, l1
-            node.next = l1
-            l1, node = l1.next, node.next
+        while l1 and l2:
+            if l1.val <= l2.val: 
+                node.next = node = l1
+                l1 = l1.next 
+            else: 
+                node.next = node = l2
+                l2 = l2.next 
         node.next = l1 or l2
         return dummy.next 
 
