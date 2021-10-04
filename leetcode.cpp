1a4f1288d85b6b000b23a23d56b987abd4b7948d
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-04 15:21:01 -0400

    solve'em all (704)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7496,6 +7496,39 @@ public:
     }
 
 
+    /*704. Binary Search (Easy)
+	Given an array of integers nums which is sorted in ascending order, and an 
+	integer target, write a function to search target in nums. If target exists, 
+	then return its index. Otherwise, return -1. You must write an algorithm 
+	with O(log n) runtime complexity.
+
+	Example 1:
+	Input: nums = [-1,0,3,5,9,12], target = 9
+	Output: 4
+	Explanation: 9 exists in nums and its index is 4
+
+	Example 2:
+	Input: nums = [-1,0,3,5,9,12], target = 2
+	Output: -1
+	Explanation: 2 does not exist in nums so return -1
+
+	Constraints:
+	* 1 <= nums.length <= 10^4
+	* -10^4 < nums[i], target < 10^4
+	* All the integers in nums are unique.
+	* nums is sorted in ascending order.*/
+
+    int search(vector<int>& nums, int target) {
+        for (int lo = 0, hi = nums.size(); lo < hi; ) {
+            int mid = lo + (hi - lo)/2; 
+            if (nums[mid] == target) return mid; 
+            else if (nums[mid] < target) lo = mid + 1; 
+            else hi = mid; 
+        }
+        return -1; 
+    }
+
+
     /*708. Insert into a Sorted Circular Linked List (Medium)
 	Given a Circular Linked List node, which is sorted in ascending order, 
 	write a function to insert a value insertVal into the list such that it 
