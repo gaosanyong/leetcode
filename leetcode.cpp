9e64ac7d63b32644a7504ba9e7608709a4d39922
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-05 16:07:38 -0400

    solve'em all (829)

--- leetcode.cpp
+++ leetcode.cpp
@@ -9880,6 +9880,35 @@ public:
     }
 
 
+    /*829. Consecutive Numbers Sum (Hard)
+	Given an integer n, return the number of ways you can write n as the sum of 
+	consecutive positive integers.
+
+	Example 1:
+	Input: n = 5
+	Output: 2
+	Explanation: 5 = 2 + 3
+
+	Example 2:
+	Input: n = 9
+	Output: 3
+	Explanation: 9 = 4 + 5 = 2 + 3 + 4
+
+	Example 3:
+	Input: n = 15
+	Output: 4
+	Explanation: 15 = 8 + 7 = 4 + 5 + 6 = 1 + 2 + 3 + 4 + 5
+
+	Constraints: 1 <= n <= 10^9*/
+
+    int consecutiveNumbersSum(int n) {
+        int ans = 0; 
+        for (int x = 1; x*x < 2*n; ++x) 
+            if ((n - x*(x+1)/2) % x == 0) ++ans; 
+        return ans; 
+    }
+
+
     /*830. Positions of Large Groups (Easy)
 	In a string s of lowercase letters, these letters form consecutive groups 
 	of the same character. For example, a string like s = "abbxxxxzyy" has the 
--- leetcode.py
+++ leetcode.py
@@ -18157,6 +18157,34 @@ class UnionFind:
         return ans 
 
 
+    """829. Consecutive Numbers Sum (Hard)
+	Given an integer n, return the number of ways you can write n as the sum of 
+	consecutive positive integers.
+
+	Example 1:
+	Input: n = 5
+	Output: 2
+	Explanation: 5 = 2 + 3
+
+	Example 2:
+	Input: n = 9
+	Output: 3
+	Explanation: 9 = 4 + 5 = 2 + 3 + 4
+
+	Example 3:
+	Input: n = 15
+	Output: 4
+	Explanation: 15 = 8 + 7 = 4 + 5 + 6 = 1 + 2 + 3 + 4 + 5
+
+	Constraints: 1 <= n <= 10^9"""
+
+    def consecutiveNumbersSum(self, n: int) -> int:
+        ans = 0
+        for x in range(1, int(sqrt(2*n))+1): 
+            if (n - x*(x+1)//2) % x == 0: ans += 1
+        return ans 
+
+
     """831. Masking Personal Information (Medium)
 	We are given a personal information string S, which may represent either an 
 	email address or a phone number. We would like to mask this personal 
