887343cccc0fe1df7714ab0d63be7a331e9215d9
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-05 22:16:51 -0400

    October LeetCoding Challenge 2021-10-06 (442)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4253,6 +4253,41 @@ public:
     }
 
 
+    /*442. Find All Duplicates in an Array (Medium)
+	Given an integer array nums of length n where all the integers of nums are 
+	in the range [1, n] and each integer appears once or twice, return an array 
+	of all the integers that appears twice. You must write an algorithm that 
+	runs in O(n) time and uses only constant extra space.
+
+	Example 1:
+	Input: nums = [4,3,2,7,8,2,3,1]
+	Output: [2,3]
+
+	Example 2:
+	Input: nums = [1,1,2]
+	Output: [1]
+
+	Example 3:
+	Input: nums = [1]
+	Output: []
+
+	Constraints:
+	* n == nums.length
+	* 1 <= n <= 10^5
+	* 1 <= nums[i] <= n
+	* Each element in nums appears once or twice.*/
+
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans; 
+        for (auto x : nums) {
+            x = abs(x); 
+            if (nums[x-1] < 0) ans.push_back(x); 
+            nums[x-1] *= -1; 
+        }
+        return ans; 
+    }
+
+
     /*446. Arithmetic Slices II - Subsequence (Hard)
 	Given an integer array nums, return the number of all the arithmetic 
 	subsequences of nums. A sequence of numbers is called arithmetic if it 
