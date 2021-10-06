71555858f6f59f654785636a4fce741e18ae14ae
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-06 12:35:56 -0400

    update 189

--- leetcode.cpp
+++ leetcode.cpp
@@ -2475,30 +2475,24 @@ public:
 	Example 1:
 	Input: nums = [1,2,3,4,5,6,7], k = 3
 	Output: [5,6,7,1,2,3,4]
-	Explanation:
-	rotate 1 steps to the right: [7,1,2,3,4,5,6]
-	rotate 2 steps to the right: [6,7,1,2,3,4,5]
-	rotate 3 steps to the right: [5,6,7,1,2,3,4]
+	Explanation: rotate 1 steps to the right: [7,1,2,3,4,5,6]
+	             rotate 2 steps to the right: [6,7,1,2,3,4,5]
+	             rotate 3 steps to the right: [5,6,7,1,2,3,4]
+	
 	Example 2:
-
 	Input: nums = [-1,-100,3,99], k = 2
 	Output: [3,99,-1,-100]
-	Explanation: 
-	rotate 1 steps to the right: [99,-1,-100,3]
-	rotate 2 steps to the right: [3,99,-1,-100]
-	 
+	Explanation: rotate 1 steps to the right: [99,-1,-100,3]
+	             rotate 2 steps to the right: [3,99,-1,-100]
 
 	Constraints:
+	* 1 <= nums.length <= 10^5
+	* -2^31 <= nums[i] <= 2^31 - 1
+	* 0 <= k <= 10^5
 
-	1 <= nums.length <= 105
-	-231 <= nums[i] <= 231 - 1
-	0 <= k <= 105
-	 
-
-	Follow up:
-
-	Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
-	Could you do it in-place with O(1) extra space?*/
+	Follow up: Try to come up with as many solutions as you can. There are at 
+	least three different ways to solve this problem. Could you do it in-place 
+	with O(1) extra space?*/
 
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
