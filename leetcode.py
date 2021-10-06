e542ca05ba2b5d93ef7bfb3e75b26bc5129543a2
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-06 14:18:31 -0400

    solve'em all (701)

--- leetcode.py
+++ leetcode.py
@@ -15184,6 +15184,50 @@ class Trie:
         return node 
 
 
+    """701. Insert into a Binary Search Tree (Medium)
+	You are given the root node of a binary search tree (BST) and a value to 
+	insert into the tree. Return the root node of the BST after the insertion. 
+	It is guaranteed that the new value does not exist in the original BST.
+	Notice that there may exist multiple valid ways for the insertion, as long 
+	as the tree remains a BST after insertion. You can return any of them.
+
+	Example 1:
+	Input: root = [4,2,7,1,3], val = 5
+	Output: [4,2,7,1,3,5]
+	Explanation: Another accepted tree is:
+
+	Example 2:
+	Input: root = [40,20,60,10,30,50,70], val = 25
+	Output: [40,20,60,10,30,50,70,null,null,25]
+
+	Example 3:
+	Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+	Output: [4,2,7,1,3,5]
+
+	Constraints:
+	* The number of nodes in the tree will be in the range [0, 10^4].
+	* -10^8 <= Node.val <= 10^8
+	* All the values Node.val are unique.
+	* -10^8 <= val <= 10^8
+	* It's guaranteed that val does not exist in the original BST."""
+
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root: return TreeNode(val)
+        node = root
+        while node: 
+            if node.val < val: 
+                if node.right: node = node.right
+                else: 
+                    node.right = TreeNode(val)
+                    break 
+            else: 
+                if node.left: node = node.left
+                else: 
+                    node.left = TreeNode(val)
+                    break 
+        return root
+
+
     """702. Search in a Sorted Array of Unknown Size (Medium)
 	This is an interactive problem. You have a sorted array of unique elements 
 	and an unknown size. You do not have an access to the array but you can use 
