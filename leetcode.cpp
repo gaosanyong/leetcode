9db9563c8f65f78ff1ca23cdfb664c36d1d94f24
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-06 21:40:12 -0400

    solve'em all (190)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2604,6 +2604,47 @@ public:
     }
 
 
+    /*190. Reverse Bits (Easy)
+	Reverse bits of a given 32 bits unsigned integer.
+
+	Note:
+	* Note that in some languages, such as Java, there is no unsigned integer 
+	  type. In this case, both input and output will be given as a signed 
+	  integer type. They should not affect your implementation, as the 
+	  integer's internal binary representation is the same, whether it is 
+	  signed or unsigned.
+	* In Java, the compiler represents the signed integers using 2's complement 
+	  notation. Therefore, in Example 2 above, the input represents the signed 
+	  integer -3 and the output represents the signed integer -1073741825.
+
+	Example 1:
+	Input: n = 00000010100101000001111010011100
+	Output:    964176192 (00111001011110000010100101000000)
+	Explanation: The input binary string 00000010100101000001111010011100 
+	             represents the unsigned integer 43261596, so return 964176192 
+	             which its binary representation is 
+	             00111001011110000010100101000000.
+	
+	Example 2:
+	Input: n = 11111111111111111111111111111101
+	Output:   3221225471 (10111111111111111111111111111111)
+	Explanation: The input binary string 11111111111111111111111111111101 
+	             represents the unsigned integer 4294967293, so return 
+	             3221225471 which its binary representation is 
+	             10111111111111111111111111111111.
+
+	Constraints: The input must be a binary string of length 32
+
+	Follow up: If this function is called many times, how would you optimize it?*/
+
+    uint32_t reverseBits(uint32_t n) {
+        int ans = 0; 
+        for (int i = 0; i < 32; ++i) 
+            if (n & 1 << i) ans ^= 1 << 31-i; 
+        return ans; 
+    }
+
+
     /*191. Number of 1 Bits (Easy)
 	Write a function that takes an unsigned integer and returns the number of 
 	'1' bits it has (also known as the Hamming weight).
