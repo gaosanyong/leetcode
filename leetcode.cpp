2294896d37f1c1f10ac4408b1a0b614090ad6701
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-06 22:05:44 -0400

    solve'em all (53)

--- leetcode.cpp
+++ leetcode.cpp
@@ -948,6 +948,42 @@ public:
     }
 
 
+    /*53. Maximum Subarray (Easy)
+	Given an integer array nums, find the contiguous subarray (containing at 
+	least one number) which has the largest sum and return its sum. A subarray 
+	is a contiguous part of an array.
+
+	Example 1:
+	Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+	Output: 6
+	Explanation: [4,-1,2,1] has the largest sum = 6.
+
+	Example 2:
+	Input: nums = [1]
+	Output: 1
+
+	Example 3:
+	Input: nums = [5,4,-1,7,8]
+	Output: 23
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* -10^4 <= nums[i] <= 10^4
+
+	Follow up: If you have figured out the O(n) solution, try coding another 
+	           solution using the divide and conquer approach, which is more 
+	           subtle.*/
+
+    int maxSubArray(vector<int>& nums) {
+        int ans = INT_MIN, val = 0; 
+        for (auto& x : nums) {
+            val = max(0, val) + x; 
+            ans = max(ans, val); 
+        }
+        return ans; 
+    }
+
+
     /*54. Spiral Matrix (Medium)
 	Given an m x n matrix, return all elements of the matrix in spiral order.
 
