a91028b23f60391baa3ad5972e2f11d635328554
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-06 21:30:12 -0400

    solve'em all (136)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2223,6 +2223,36 @@ public:
     }
 
 
+    /*136. Single Number (Easy)
+	Given a non-empty array of integers nums, every element appears twice 
+	except for one. Find that single one. You must implement a solution with a 
+	linear runtime complexity and use only constant extra space.
+
+	Example 1:
+	Input: nums = [2,2,1]
+	Output: 1
+
+	Example 2:
+	Input: nums = [4,1,2,1,2]
+	Output: 4
+
+	Example 3:
+	Input: nums = [1]
+	Output: 1
+
+	Constraints:
+	* 1 <= nums.length <= 3 * 10^4
+	* -3 * 10^4 <= nums[i] <= 3 * 10^4
+	* Each element in the array appears twice except for one element which 
+	  appears only once.*/
+
+    int singleNumber(vector<int>& nums) {
+        int ans = 0; 
+        for (auto& x : nums) ans ^= x; 
+        return ans; 
+    }
+
+
     /*144. Binary Tree Preorder Traversal (Medium)
 	Given the root of a binary tree, return the preorder traversal of its 
 	nodes' values.
