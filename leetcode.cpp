961ac146da70dc480409bf750136d76698b5d823
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-09 11:49:01 -0400

    solve'em all (212)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3159,6 +3159,92 @@ public:
     }
 
 
+    /*212. Word Search II (Hard)
+	Given an m x n board of characters and a list of strings words, return all 
+	words on the board. Each word must be constructed from letters of 
+	sequentially adjacent cells, where adjacent cells are horizontally or 
+	vertically neighboring. The same letter cell may not be used more than once 
+	in a word.
+
+	Example 1:
+	Input: board = [["o","a","a","n"],
+	                ["e","t","a","e"],
+	                ["i","h","k","r"],
+	                ["i","f","l","v"]], 
+	       words = ["oath","pea","eat","rain"]
+	Output: ["eat","oath"]
+	
+	Example 2:
+	Input: board = [["a","b"],
+	                ["c","d"]], 
+	       words = ["abcb"]
+	Output: []
+
+	Constraints:
+	* m == board.length
+	* n == board[i].length
+	* 1 <= m, n <= 12
+	* board[i][j] is a lowercase English letter.
+	* 1 <= words.length <= 3 * 104
+	* 1 <= words[i].length <= 10
+	* words[i] consists of lowercase English letters.
+	* All the strings of words are unique.
+
+    class TrieNode {
+    public: 
+        TrieNode* next[26] = {nullptr}; 
+        bool isword = false; 
+    };    
+
+    class Trie {
+    public: 
+        TrieNode* root; 
+        Trie() {
+            root = new TrieNode(); 
+        }
+        
+        void insert(string word) {
+            TrieNode* node = root; 
+            for (auto& ch : word) {
+                if (!node->next[ch-'a']) node->next[ch-'a'] = new TrieNode(); 
+                node = node->next[ch-'a']; 
+            }
+            node->isword = true; 
+        }
+    }; */
+
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        Trie* trie = new Trie(); 
+        for (auto& word : words) trie->insert(word); 
+        
+        int m = board.size(), n = board[0].size(), dir[5] = {-1, 0, 1, 0, -1}; 
+        vector<string> ans; 
+        string stk; 
+        
+        function<void(int, int, TrieNode*)> fn = [&](int i, int j, TrieNode* node) {
+            if (node->isword) {
+                ans.push_back(stk); 
+                node->isword = false; // avoid repetitions
+            } 
+            if (0 <= i && i < m && 0 <= j && j < n && board[i][j] != '#' && node->next[board[i][j]-'a']) {
+                stk.push_back(board[i][j]); 
+                board[i][j] = '#'; 
+                for (int k = 0; k < 4; ++k) {
+                    int ii = i + dir[k], jj = j + dir[k+1]; 
+                    fn(ii, jj, node->next[stk.back()-'a']); 
+                }
+                board[i][j] = stk.back(); 
+                stk.pop_back(); 
+            }
+        };
+        
+        for (int i = 0; i < m; ++i) 
+            for (int j = 0; j < n; ++j) 
+                fn(i, j, trie->root); 
+        return ans; 
+    }
+
+
     /*213. House Robber II (Medium)
 	You are a professional robber planning to rob houses along a street. Each 
 	house has a certain amount of money stashed. All houses at this place are 
