44df24f74fd6a2fdbf57e72c6a9ea8813ebb3334
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-09 16:31:57 -0400

    solve'em all (75)

--- leetcode.cpp
+++ leetcode.cpp
@@ -1284,6 +1284,46 @@ public:
     }
 
 
+    /*75. Sort Colors (Medium)
+	Given an array nums with n objects colored red, white, or blue, sort them 
+	in-place so that objects of the same color are adjacent, with the colors in 
+	the order red, white, and blue. We will use the integers 0, 1, and 2 to 
+	represent the color red, white, and blue, respectively. You must solve this 
+	problem without using the library's sort function.
+
+	Example 1:
+	Input: nums = [2,0,2,1,1,0]
+	Output: [0,0,1,1,2,2]
+
+	Example 2:
+	Input: nums = [2,0,1]
+	Output: [0,1,2]
+
+	Example 3:
+	Input: nums = [0]
+	Output: [0]
+
+	Example 4:
+	Input: nums = [1]
+	Output: [1]
+
+	Constraints:
+	* n == nums.length
+	* 1 <= n <= 300
+	* nums[i] is 0, 1, or 2.
+
+	Follow up: Could you come up with a one-pass algorithm using only constant 
+	           extra space?*/
+
+    void sortColors(vector<int>& nums) {
+        for(int lo = 0, mid = 0, hi = nums.size()-1; mid <= hi; ) {
+            if (nums[mid] == 0) swap(nums[lo++], nums[mid++]); 
+            else if (nums[mid] == 2) swap(nums[mid], nums[hi--]); 
+            else ++mid; 
+        }
+    }
+
+
     /*77. Combinations (Medium)
 	Given two integers n and k, return all possible combinations of k numbers 
 	out of the range [1, n]. You may return the answer in any order.
