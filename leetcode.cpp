748c3c8bb0538819c4547d0ae6a337f70c0e34c0
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-09 10:57:08 -0400

    solve'em all (740)

--- leetcode.cpp
+++ leetcode.cpp
@@ -9141,6 +9141,53 @@ public:
     }
 
 
+    /*740. Delete and Earn (Medium)
+	You are given an integer array nums. You want to maximize the number of 
+	points you get by performing the following operation any number of times:
+	* Pick any nums[i] and delete it to earn nums[i] points. Afterwards, you 
+	  must delete every element equal to nums[i] - 1 and every element equal 
+	  to nums[i] + 1.
+	Return the maximum number of points you can earn by applying the above 
+	operation some number of times.
+
+	Example 1:
+	Input: nums = [3,4,2]
+	Output: 6
+	Explanation: You can perform the following operations:
+	             - Delete 4 to earn 4 points. Consequently, 3 is also deleted. 
+	               nums = [2].
+	             - Delete 2 to earn 2 points. nums = [].
+	             You earn a total of 6 points.
+	
+	Example 2:
+	Input: nums = [2,2,3,3,3,4]
+	Output: 9
+	Explanation: You can perform the following operations:
+	             - Delete a 3 to earn 3 points. All 2's and 4's are also 
+	               deleted. nums = [3,3].
+	             - Delete a 3 again to earn 3 points. nums = [3].
+	             - Delete a 3 once more to earn 3 points. nums = [].
+	             You earn a total of 9 points.
+
+	Constraints:
+	* 1 <= nums.length <= 2 * 10^4
+	* 1 <= nums[i] <= 10^4*/
+
+    int deleteAndEarn(vector<int>& nums) {
+        map<int, int> freq; 
+        for (auto& x : nums) ++freq[x]; 
+        
+        int f0 = 0, f1 = 0, prev = -1; 
+        for (auto& [k, v] : freq) {
+            int f2 = prev + 1 == k ? f0 + k*v : max(f0, f1) + k*v; 
+            f0 = f1; 
+            f1 = max(f1, f2); 
+            prev = k; 
+        }
+        return f1; 
+    }
+
+
     /*742. Closest Leaf in a Binary Tree (Medium)
 	Given the root of a binary tree where every node has a unique value and a 
 	target integer k, return the value of the nearest leaf node to the target k 
