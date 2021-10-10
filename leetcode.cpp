c9ae1e0ebf4cb5d56e979a082f130ce05e589de1
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-10 17:28:37 -0400

    solve'em all (986)

--- leetcode.cpp
+++ leetcode.cpp
@@ -13796,6 +13796,52 @@ public:
     }
 
 
+    /*986. Interval List Intersections (Medium)
+	You are given two lists of closed intervals, firstList and secondList, 
+	where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. 
+	Each list of intervals is pairwise disjoint and in sorted order. Return the 
+	intersection of these two interval lists. A closed interval [a, b] (with 
+	a <= b) denotes the set of real numbers x with a <= x <= b. The 
+	intersection of two closed intervals is a set of real numbers that are 
+	either empty or represented as a closed interval. For example, the 
+	intersection of [1, 3] and [2, 4] is [2, 3].
+
+	Example 1:
+	Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+	Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+
+	Example 2:
+	Input: firstList = [[1,3],[5,9]], secondList = []
+	Output: []
+
+	Example 3:
+	Input: firstList = [], secondList = [[4,8],[10,12]]
+	Output: []
+
+	Example 4:
+	Input: firstList = [[1,7]], secondList = [[3,10]]
+	Output: [[3,7]]
+
+	Constraints:
+	* 0 <= firstList.length, secondList.length <= 1000
+	* firstList.length + secondList.length >= 1
+	* 0 <= starti < endi <= 10^9
+	* endi < starti+1
+	* 0 <= startj < endj <= 10^9
+	* endj < startj+1*/
+
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>> ans; 
+        for (int i = 0, j = 0; i < firstList.size() && j < secondList.size(); ) {
+            if (firstList[i][0] <= secondList[j][1] && secondList[j][0] <= firstList[i][1]) 
+                ans.push_back({max(firstList[i][0], secondList[j][0]), min(firstList[i][1], secondList[j][1])}); 
+            if (firstList[i][1] <= secondList[j][1]) ++i; 
+            else ++j; 
+        }
+        return ans; 
+    }
+
+
     /*989. Add to Array-Form of Integer (Easy)
 	The array-form of an integer num is an array representing its digits in 
 	left to right order. 
--- leetcode.py
+++ leetcode.py
@@ -21337,6 +21337,51 @@ class UnionFind:
         return ans 
 
 
+    """986. Interval List Intersections (Medium)
+	You are given two lists of closed intervals, firstList and secondList, 
+	where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. 
+	Each list of intervals is pairwise disjoint and in sorted order. Return the 
+	intersection of these two interval lists. A closed interval [a, b] (with 
+	a <= b) denotes the set of real numbers x with a <= x <= b. The 
+	intersection of two closed intervals is a set of real numbers that are 
+	either empty or represented as a closed interval. For example, the 
+	intersection of [1, 3] and [2, 4] is [2, 3].
+
+	Example 1:
+	Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+	Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+
+	Example 2:
+	Input: firstList = [[1,3],[5,9]], secondList = []
+	Output: []
+
+	Example 3:
+	Input: firstList = [], secondList = [[4,8],[10,12]]
+	Output: []
+
+	Example 4:
+	Input: firstList = [[1,7]], secondList = [[3,10]]
+	Output: [[3,7]]
+
+	Constraints:
+	* 0 <= firstList.length, secondList.length <= 1000
+	* firstList.length + secondList.length >= 1
+	* 0 <= starti < endi <= 10^9
+	* endi < starti+1
+	* 0 <= startj < endj <= 10^9
+	* endj < startj+1"""
+
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        ans = []
+        i = j = 0
+        while i < len(firstList) and j < len(secondList): 
+            if firstList[i][0] <= secondList[j][1] and secondList[j][0] <= firstList[i][1]: 
+                ans.append([max(firstList[i][0], secondList[j][0]), min(firstList[i][1], secondList[j][1])])
+            if firstList[i][1] <= secondList[j][1]: i += 1
+            else: j += 1
+        return ans
+
+
     """992. Subarrays with K Different Integers (Hard)
 	Given an array nums of positive integers, call a (contiguous, not 
 	necessarily distinct) subarray of nums good if the number of different 
