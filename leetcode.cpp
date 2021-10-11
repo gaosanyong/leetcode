3dd4aede4abbc8c37c686f90c70536ebb8b50c9b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-11 16:06:33 -0400

    solve'em all (862)

--- leetcode.cpp
+++ leetcode.cpp
@@ -11766,6 +11766,44 @@ public:
     }
 
 
+    /*862. Shortest Subarray with Sum at Least K (Hard)
+	Given an integer array nums and an integer k, return the length of the 
+	shortest non-empty subarray of nums with a sum of at least k. If there is 
+	no such subarray, return -1. A subarray is a contiguous part of an array.
+
+	Example 1:
+	Input: nums = [1], k = 1
+	Output: 1
+
+	Example 2:
+	Input: nums = [1,2], k = 4
+	Output: -1
+
+	Example 3:
+	Input: nums = [2,-1,2], k = 3
+	Output: 3
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* -10^5 <= nums[i] <= 10^5
+	* 1 <= k <= 10^9*/
+
+    int shortestSubarray(vector<int>& nums, int k) {
+        deque<pair<int, int>> dq = {{-1, 0}}; 
+        int prefix = 0, ans = INT_MAX; 
+        for (int i = 0; i < nums.size(); ++i) {
+            prefix += nums[i]; 
+            while (dq.size() && prefix - dq.front().second >= k) {
+                ans = min(ans, i - dq.front().first); 
+                dq.pop_front(); 
+            }
+            while (dq.size() && dq.back().second >= prefix) dq.pop_back(); 
+            dq.emplace_back(i, prefix); 
+        }
+        return ans < INT_MAX ? ans : -1; 
+    }
+
+
     /*866. Prime Palindrome (Medium)
 	Given an integer n, return the smallest prime palindrome greater than or 
 	equal to n. An integer is prime if it has exactly two divisors: 1 and 
--- leetcode.py
+++ leetcode.py
@@ -19305,6 +19305,40 @@ class UnionFind:
         return ans 
 
 
+    """862. Shortest Subarray with Sum at Least K (Hard)
+	Given an integer array nums and an integer k, return the length of the 
+	shortest non-empty subarray of nums with a sum of at least k. If there is 
+	no such subarray, return -1. A subarray is a contiguous part of an array.
+
+	Example 1:
+	Input: nums = [1], k = 1
+	Output: 1
+
+	Example 2:
+	Input: nums = [1,2], k = 4
+	Output: -1
+
+	Example 3:
+	Input: nums = [2,-1,2], k = 3
+	Output: 3
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* -10^5 <= nums[i] <= 10^5
+	* 1 <= k <= 10^9"""
+
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        ans = inf 
+        queue = deque([(-1, 0)])
+        prefix = 0
+        for i, x in enumerate(nums): 
+            prefix += x
+            while queue and prefix - queue[0][1] >= k: ans = min(ans, i - queue.popleft()[0])
+            while queue and queue[-1][1] >= prefix: queue.pop()
+            queue.append((i, prefix))
+        return ans if ans < inf else -1 
+
+
     """865. Smallest Subtree with all the Deepest Nodes (Medium)
 	Given the root of a binary tree, the depth of each node is the shortest 
 	distance to the root. Return the smallest subtree such that it contains all 
