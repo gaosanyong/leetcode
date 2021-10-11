bd045eb9bdf0fb5cf8266bd706d8cee70bfe3acb
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-10 21:33:46 -0400

    solve'em all (438)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5020,6 +5020,43 @@ public:
     }
 
 
+    /*438. Find All Anagrams in a String (Medium)
+	Given two strings s and p, return an array of all the start indices of p's 
+	anagrams in s. You may return the answer in any order. An Anagram is a word 
+	or phrase formed by rearranging the letters of a different word or phrase, 
+	typically using all the original letters exactly once.
+
+	Example 1:
+	Input: s = "cbaebabacd", p = "abc"
+	Output: [0,6]
+	Explanation: The substring with start index = 0 is "cba", which is an 
+	             anagram of "abc". The substring with start index = 6 is "bac", 
+	             which is an anagram of "abc".
+	
+	Example 2:
+	Input: s = "abab", p = "ab"
+	Output: [0,1,2]
+	Explanation: The substring with start index = 0 is "ab", which is an 
+	             anagram of "ab". The substring with start index = 1 is "ba", 
+	             which is an anagram of "ab". The substring with start 
+	             index = 2 is "ab", which is an anagram of "ab".
+
+	Constraints:
+	* 1 <= s.length, p.length <= 3 * 10^4
+	* s and p consist of lowercase English letters.*/
+
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans, freq(26); 
+        for (auto& ch : p) ++freq[ch-'a']; 
+        for (int i = 0; i < s.size(); ++i) {
+            --freq[s[i]-'a']; 
+            if (p.size() <= i) ++freq[s[i-p.size()]-'a']; 
+            if (p.size() <= i+1 && all_of(freq.begin(), freq.end(), [](auto& x) {return x == 0;})) ans.push_back(i-p.size()+1); 
+        }
+        return ans; 
+    }
+
+
     /*439. Ternary Expression Parser (Medium)
 	Given a string expression representing arbitrarily nested ternary 
 	expressions, evaluate the expression, and return the result of it. You can 
--- leetcode.py
+++ leetcode.py
@@ -11423,6 +11423,41 @@ class UnionFind:
         return ans 
 
 
+    """438. Find All Anagrams in a String (Medium)
+	Given two strings s and p, return an array of all the start indices of p's 
+	anagrams in s. You may return the answer in any order. An Anagram is a word 
+	or phrase formed by rearranging the letters of a different word or phrase, 
+	typically using all the original letters exactly once.
+
+	Example 1:
+	Input: s = "cbaebabacd", p = "abc"
+	Output: [0,6]
+	Explanation: The substring with start index = 0 is "cba", which is an 
+	             anagram of "abc". The substring with start index = 6 is "bac", 
+	             which is an anagram of "abc".
+	
+	Example 2:
+	Input: s = "abab", p = "ab"
+	Output: [0,1,2]
+	Explanation: The substring with start index = 0 is "ab", which is an 
+	             anagram of "ab". The substring with start index = 1 is "ba", 
+	             which is an anagram of "ab". The substring with start 
+	             index = 2 is "ab", which is an anagram of "ab".
+
+	Constraints:
+	* 1 <= s.length, p.length <= 3 * 10^4
+	* s and p consist of lowercase English letters."""
+
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        freq = Counter(p)
+        ans = []
+        for i, x in enumerate(s): 
+            freq[x] -= 1; 
+            if len(p) <= i: freq[s[i-len(p)]] += 1
+            if all(x == 0 for x in freq.values()): ans.append(i-len(p)+1)
+        return ans 
+
+
     """439. Ternary Expression Parser (Medium)
 	Given a string expression representing arbitrarily nested ternary 
 	expressions, evaluate the expression, and return the result of it. You can 
