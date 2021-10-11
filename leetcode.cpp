e166ea947b1ed7bd5412ffa4be7d03a00eaa059b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-10 21:21:17 -0400

    solve'em all (713)

--- leetcode.cpp
+++ leetcode.cpp
@@ -8662,6 +8662,39 @@ public:
     }
 
 
+    /*713. Subarray Product Less Than K (Medium)
+	Given an array of integers nums and an integer k, return the number of 
+	contiguous subarrays where the product of all the elements in the subarray 
+	is strictly less than k.
+
+	Example 1:
+	Input: nums = [10,5,2,6], k = 100
+	Output: 8
+	Explanation: The 8 subarrays that have product less than 100 are: 
+	             [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]. Note 
+	             that [10, 5, 2] is not included as the product of 100 is not 
+	             strictly less than k.
+	
+	Example 2:
+	Input: nums = [1,2,3], k = 0
+	Output: 0
+
+	Constraints:
+	* 1 <= nums.length <= 3 * 10^4
+	* 1 <= nums[i] <= 1000
+	* 0 <= k <= 10^6*/
+
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int ans = 0; 
+        for (int i = 0, ii = 0, val = 1; i < nums.size(); ++i) {
+            val *= nums[i]; 
+            while (ii <= i && k <= val) val /= nums[ii++]; 
+            ans += i - ii + 1; 
+        }
+        return ans; 
+    }
+
+
     /*717. 1-bit and 2-bit Characters (Easy)
 	We have two special characters. The first character can be represented by 
 	one bit 0. The second character can be represented by two bits (10 or 11).
--- leetcode.py
+++ leetcode.py
@@ -15391,6 +15391,40 @@ class Trie:
         return "".join(s)
 
 
+    """713. Subarray Product Less Than K (Medium)
+	Given an array of integers nums and an integer k, return the number of 
+	contiguous subarrays where the product of all the elements in the subarray 
+	is strictly less than k.
+
+	Example 1:
+	Input: nums = [10,5,2,6], k = 100
+	Output: 8
+	Explanation: The 8 subarrays that have product less than 100 are: 
+	             [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]. Note 
+	             that [10, 5, 2] is not included as the product of 100 is not 
+	             strictly less than k.
+	
+	Example 2:
+	Input: nums = [1,2,3], k = 0
+	Output: 0
+
+	Constraints:
+	* 1 <= nums.length <= 3 * 10^4
+	* 1 <= nums[i] <= 1000
+	* 0 <= k <= 10^6"""
+
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        ans = ii = 0
+        prod = 1
+        for i, x in enumerate(nums): 
+            prod *= x
+            while ii <= i and k <= prod: 
+                prod //= nums[ii]
+                ii += 1
+            ans += i - ii + 1
+        return ans 
+
+
     """719. Find K-th Smallest Pair Distance (Hard)
 	The distance of a pair of integers a and b is defined as the absolute 
 	difference between a and b. Given an integer array nums and an integer k, 
