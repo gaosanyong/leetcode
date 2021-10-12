3ce47a1f7b467192477da771b1dd435400bc901e
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-12 15:29:53 -0400

    solve'em all (560

--- leetcode.cpp
+++ leetcode.cpp
@@ -7005,6 +7005,35 @@ public:
     }
 
 
+    /*560. Subarray Sum Equals K (Medium)
+	Given an array of integers nums and an integer k, return the total number 
+	of continuous subarrays whose sum equals to k.
+
+	Example 1:
+	Input: nums = [1,1,1], k = 2
+	Output: 2
+
+	Example 2:
+	Input: nums = [1,2,3], k = 3
+	Output: 2
+
+	Constraints:
+	* 1 <= nums.length <= 2 * 10^4
+	* -1000 <= nums[i] <= 1000
+	* -10^7 <= k <= 10^7*/
+
+    int subarraySum(vector<int>& nums, int k) {
+        int ans = 0, prefix = 0; 
+        unordered_map<int, int> freq = {{0, 1}}; 
+        for (auto& x : nums) {
+            prefix += x; 
+            ans += freq[prefix - k]; 
+            ++freq[prefix]; 
+        }
+        return ans; 
+    }
+
+
     /*561. Array Partition I (Easy)
 	Given an integer array nums of 2n integers, group these integers into n 
 	pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) 
--- leetcode.py
+++ leetcode.py
@@ -13502,6 +13502,33 @@ class Trie:
         return " ".join(w[::-1] for w in s.split())
 
 
+    """560. Subarray Sum Equals K (Medium)
+	Given an array of integers nums and an integer k, return the total number 
+	of continuous subarrays whose sum equals to k.
+
+	Example 1:
+	Input: nums = [1,1,1], k = 2
+	Output: 2
+
+	Example 2:
+	Input: nums = [1,2,3], k = 3
+	Output: 2
+
+	Constraints:
+	* 1 <= nums.length <= 2 * 10^4
+	* -1000 <= nums[i] <= 1000
+	* -10^7 <= k <= 10^7"""
+
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ans = prefix = 0
+        freq = defaultdict(int, {0:1})
+        for x in nums: 
+            prefix += x
+            ans += freq[prefix - k]
+            freq[prefix] += 1
+        return ans 
+
+
     """562. Longest Line of Consecutive One in Matrix (Medium)
 	Given a 01 matrix M, find the longest line of consecutive one in the matrix. 
 	The line could be horizontal, vertical, diagonal or anti-diagonal.
