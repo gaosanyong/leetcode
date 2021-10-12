0ef97db2088a61e776d0093582b48c62a3d5dcb3
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-12 14:25:33 -0400

    update 240

--- leetcode.py
+++ leetcode.py
@@ -7355,11 +7355,11 @@ class Solution:
 	* -10^9 <= target <= 10^9"""
 
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m, n = len(matrix), len(matrix[0]) # dimensions
+        m, n = len(matrix), len(matrix[0])
         i, j = 0, n-1
         while i < m and 0 <= j: 
-            if matrix[i][j] == target: return True
-            elif matrix[i][j] < target: i += 1
+            if matrix[i][j] < target: i += 1
+            elif matrix[i][j] == target: return True 
             else: j -= 1
         return False 
 
