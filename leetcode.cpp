3d5a7522c31314f8d27922fe03b6d995455bb53f
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-12 14:42:34 -0400

    solve'em all (409)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5073,6 +5073,42 @@ public:
     }
 
 
+    /*409. Longest Palindrome (Easy)
+	Given a string s which consists of lowercase or uppercase letters, return 
+	the length of the longest palindrome that can be built with those letters.
+	Letters are case sensitive, for example, "Aa" is not considered a 
+	palindrome here.
+
+	Example 1:
+	Input: s = "abccccdd"
+	Output: 7
+	Explanation: One longest palindrome that can be built is "dccaccd", whose 
+	             length is 7.
+	
+	Example 2:
+	Input: s = "a"
+	Output: 1
+
+	Example 3:
+	Input: s = "bb"
+	Output: 2
+
+	Constraints:
+	* 1 <= s.length <= 2000
+	* s consists of lowercase and/or uppercase English letters only.*/
+
+    int longestPalindrome(string s) {
+        unordered_map<char, int> freq; 
+        for (auto& ch : s) ++freq[ch]; 
+        int ans = 0, odd = 0; 
+        for (auto& [k, v] : freq) {
+            if (v&1) odd = 1; 
+            ans += v/2*2; 
+        }
+        return ans + odd; 
+    }
+
+
     /*415. Add Strings (Easy)
 	Given two non-negative integers, num1 and num2 represented as string, 
 	return the sum of num1 and num2 as a string. You must solve the problem 
--- leetcode.py
+++ leetcode.py
@@ -11212,6 +11212,39 @@ class UnionFind:
         return i == len(word) and j == len(abbr)
 
 
+    """409. Longest Palindrome (Easy)
+	Given a string s which consists of lowercase or uppercase letters, return 
+	the length of the longest palindrome that can be built with those letters.
+	Letters are case sensitive, for example, "Aa" is not considered a 
+	palindrome here.
+
+	Example 1:
+	Input: s = "abccccdd"
+	Output: 7
+	Explanation: One longest palindrome that can be built is "dccaccd", whose 
+	             length is 7.
+	
+	Example 2:
+	Input: s = "a"
+	Output: 1
+
+	Example 3:
+	Input: s = "bb"
+	Output: 2
+
+	Constraints:
+	* 1 <= s.length <= 2000
+	* s consists of lowercase and/or uppercase English letters only."""
+
+    def longestPalindrome(self, s: str) -> int:
+        freq = Counter(s)
+        ans = odd = 0 
+        for v in freq.values(): 
+            if v&1: odd = 1
+            ans += v//2*2
+        return ans + odd
+
+
     """413. Arithmetic Slices (Medium)
 	A sequence of numbers is called arithmetic if it consists of at least three 
 	elements and if the difference between any two consecutive elements is the 
