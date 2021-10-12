d9a7c4f42a838c4c01231ed0433708c53455909d
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-11 22:27:11 -0400

    solve'em all (374)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4696,6 +4696,49 @@ public:
     }
 
 
+    /*374. Guess Number Higher or Lower (Easy)
+	We are playing the Guess Game. The game is as follows:
+	* I pick a number from 1 to n. You have to guess which number I picked.
+	* Every time you guess wrong, I will tell you whether the number I picked 
+	  is higher or lower than your guess.
+	You call a pre-defined API int guess(int num), which returns 3 possible 
+	results:
+	* -1: The number I picked is lower than your guess (i.e. pick < num).
+	* 1: The number I picked is higher than your guess (i.e. pick > num).
+	* 0: The number I picked is equal to your guess (i.e. pick == num).
+	Return the number that I picked.
+
+	Example 1:
+	Input: n = 10, pick = 6
+	Output: 6
+
+	Example 2:
+	Input: n = 1, pick = 1
+	Output: 1
+
+	Example 3:
+	Input: n = 2, pick = 1
+	Output: 1
+
+	Example 4:
+	Input: n = 2, pick = 2
+	Output: 2
+
+	Constraints:
+	* 1 <= n <= 2^31 - 1
+	* 1 <= pick <= n*/
+
+    int guessNumber(int n) {
+        for (int lo = 1, hi = n; lo <= hi; ) {
+            int mid = lo + (hi - lo)/2, val = guess(mid); 
+            if (val == -1) hi = mid - 1; 
+            else if (val == 0) return mid; 
+            else lo = mid + 1; 
+        }
+        return -1; 
+    }
+
+
     /*377. Combination Sum IV (Medium)
 	Given an array of distinct integers nums and a target integer target, 
 	return the number of possible combinations that add up to target. The 
--- leetcode.py
+++ leetcode.py
@@ -10404,6 +10404,48 @@ class UnionFind:
         return ans 
 
 
+    """374. Guess Number Higher or Lower (Easy)
+	We are playing the Guess Game. The game is as follows:
+	* I pick a number from 1 to n. You have to guess which number I picked.
+	* Every time you guess wrong, I will tell you whether the number I picked 
+	  is higher or lower than your guess.
+	You call a pre-defined API int guess(int num), which returns 3 possible 
+	results:
+	* -1: The number I picked is lower than your guess (i.e. pick < num).
+	* 1: The number I picked is higher than your guess (i.e. pick > num).
+	* 0: The number I picked is equal to your guess (i.e. pick == num).
+	Return the number that I picked.
+
+	Example 1:
+	Input: n = 10, pick = 6
+	Output: 6
+
+	Example 2:
+	Input: n = 1, pick = 1
+	Output: 1
+
+	Example 3:
+	Input: n = 2, pick = 1
+	Output: 1
+
+	Example 4:
+	Input: n = 2, pick = 2
+	Output: 2
+
+	Constraints:
+	* 1 <= n <= 2^31 - 1
+	* 1 <= pick <= n"""
+
+    def guessNumber(self, n: int) -> int:
+        lo, hi = 1, n
+        while lo <= hi: 
+            mid = lo + hi >> 1
+            val = guess(mid)
+            if val == -1: hi = mid - 1
+            elif val == 0: return mid 
+            else: lo = mid + 1
+
+
     """375. Guess Number Higher or Lower II (Medium)
 	We are playing the Guess Game. The game is as follows:
 	I pick a number from 1 to n. You have to guess which number I picked. Every 
