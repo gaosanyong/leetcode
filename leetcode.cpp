5ee7c8ae826a98c2cefdc08e8b206b65db3853c1
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-11 22:17:09 -0400

    solve'em all (879)

--- leetcode.cpp
+++ leetcode.cpp
@@ -12288,6 +12288,51 @@ public:
     }
 
 
+    /*879. Profitable Schemes (Hard)
+	There is a group of n members, and a list of various crimes they could 
+	commit. The ith crime generates a profit[i] and requires group[i] members 
+	to participate in it. If a member participates in one crime, that member 
+	can't participate in another crime. Let's call a profitable scheme any 
+	subset of these crimes that generates at least minProfit profit, and the 
+	total number of members participating in that subset of crimes is at most 
+	n. Return the number of schemes that can be chosen. Since the answer may 
+	be very large, return it modulo 10^9 + 7.
+
+	Example 1:
+	Input: n = 5, minProfit = 3, group = [2,2], profit = [2,3]
+	Output: 2
+	Explanation: To make a profit of at least 3, the group could either commit 
+	             crimes 0 and 1, or just crime 1. In total, there are 2 schemes.
+	
+	Example 2:
+	Input: n = 10, minProfit = 5, group = [2,3,5], profit = [6,7,8]
+	Output: 7
+	Explanation: To make a profit of at least 5, the group could commit any 
+	             crimes, as long as they commit one. There are 7 possible 
+	             schemes: (0), (1), (2), (0,1), (0,2), (1,2), and (0,1,2).
+
+	Constraints:
+	* 1 <= n <= 100
+	* 0 <= minProfit <= 100
+	* 1 <= group.length <= 100
+	* 1 <= group[i] <= 100
+	* profit.length == group.length
+	* 0 <= profit[i] <= 100*/
+
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        const int MOD = 1'000'000'007; 
+        vector<vector<int>> dp(1+minProfit, vector<int>(1+n)); 
+        dp[0][0] = 1; 
+        for (int k = 0; k < group.size(); ++k) 
+            for (int i = minProfit; i >= 0; --i) 
+                for (int j = n-group[k]; j >= 0; --j) 
+                    dp[min(i+profit[k], minProfit)][j+group[k]] = (dp[min(i+profit[k], minProfit)][j+group[k]] + dp[i][j]) % MOD; 
+        int ans = 0; 
+        for (auto& x : dp[minProfit]) ans = (ans + x) % MOD; 
+        return ans; 
+    }
+
+
     /*882. Reachable Nodes In Subdivided Graph (Hard)
 	You are given an undirected graph (the "original graph") with n nodes 
 	labeled from 0 to n - 1. You decide to subdivide each edge in the graph 
--- leetcode.py
+++ leetcode.py
@@ -19669,6 +19669,47 @@ class UnionFind:
         return fn(0, len(piles)-1) > 0
 
 
+    """879. Profitable Schemes (Hard)
+	There is a group of n members, and a list of various crimes they could 
+	commit. The ith crime generates a profit[i] and requires group[i] members 
+	to participate in it. If a member participates in one crime, that member 
+	can't participate in another crime. Let's call a profitable scheme any 
+	subset of these crimes that generates at least minProfit profit, and the 
+	total number of members participating in that subset of crimes is at most 
+	n. Return the number of schemes that can be chosen. Since the answer may 
+	be very large, return it modulo 10^9 + 7.
+
+	Example 1:
+	Input: n = 5, minProfit = 3, group = [2,2], profit = [2,3]
+	Output: 2
+	Explanation: To make a profit of at least 3, the group could either commit 
+	             crimes 0 and 1, or just crime 1. In total, there are 2 schemes.
+	
+	Example 2:
+	Input: n = 10, minProfit = 5, group = [2,3,5], profit = [6,7,8]
+	Output: 7
+	Explanation: To make a profit of at least 5, the group could commit any 
+	             crimes, as long as they commit one. There are 7 possible 
+	             schemes: (0), (1), (2), (0,1), (0,2), (1,2), and (0,1,2).
+
+	Constraints:
+	* 1 <= n <= 100
+	* 0 <= minProfit <= 100
+	* 1 <= group.length <= 100
+	* 1 <= group[i] <= 100
+	* profit.length == group.length
+	* 0 <= profit[i] <= 100"""
+
+    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        dp = [[0]*(1 + n) for _ in range(1 + minProfit)]
+        dp[0][0] = 1
+        for p, g in zip(profit, group): 
+            for i in range(minProfit, -1, -1): 
+                for j in range(n - g, -1, -1): 
+                    dp[min(i+p, minProfit)][j + g] += dp[i][j]
+        return sum(dp[minProfit]) % 1_000_000_007
+
+
     """881. Boats to Save People (Medium)
 	The i-th person has weight people[i], and each boat can carry a maximum 
 	weight of limit. Each boat carries at most 2 people at the same time, 
