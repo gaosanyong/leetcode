4106ac02a9ef6a89d25e13eb3c7377fd29ff8312
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-12 12:51:32 -0400

    solve'em all (1567)

--- leetcode.py
+++ leetcode.py
@@ -31750,6 +31750,53 @@ class UnionFind:
         return k 
 
 
+    """1567. Maximum Length of Subarray With Positive Product (Medium)
+	Given an array of integers nums, find the maximum length of a subarray 
+	where the product of all its elements is positive. A subarray of an array 
+	is a consecutive sequence of zero or more values taken out of that array.
+	Return the maximum length of a subarray with positive product.
+
+	Example 1:
+	Input: nums = [1,-2,-3,4]
+	Output: 4
+	Explanation: The array nums already has a positive product of 24.
+
+	Example 2:
+	Input: nums = [0,1,-2,-3,-4]
+	Output: 3
+	Explanation: The longest subarray with positive product is [1,-2,-3] which 
+	             has a product of 6. Notice that we cannot include 0 in the 
+	             subarray since that'll make the product 0 which is not 
+	             positive.
+	
+	Example 3:
+	Input: nums = [-1,-2,-3,0,1]
+	Output: 2
+	Explanation: The longest subarray with positive product is [-1,-2] or 
+	             [-2,-3].
+	
+	Example 4:
+	Input: nums = [-1,2]
+	Output: 1
+
+	Example 5:
+	Input: nums = [1,2,3,5,-6,4,0,10]
+	Output: 4
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* -10^9 <= nums[i] <= 10^9"""
+
+    def getMaxLen(self, nums: List[int]) -> int:
+        ans = pos = neg = 0
+        for x in nums: 
+            if x > 0: pos, neg = 1 + pos, 1 + neg if neg else 0
+            elif x < 0: pos, neg = 1 + neg if neg else 0, 1 + pos
+            else: pos = neg = 0 # reset 
+            ans = max(ans, pos)
+        return ans 
+
+
     """1580. Put Boxes Into the Warehouse II (Medium)
 	You are given two arrays of positive integers, boxes and warehouse, 
 	representing the heights of some boxes of unit width and the heights of n 
