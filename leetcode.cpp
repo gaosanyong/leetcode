383575d6dd2068e1dd84f5b1f59b46481c2e3ba9
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-12 21:47:01 -0400

    solve'em all (940)

--- leetcode.cpp
+++ leetcode.cpp
@@ -13685,6 +13685,43 @@ public:
     }
 
 
+    /*940. Distinct Subsequences II (Hard)
+	Given a string s, return the number of distinct non-empty subsequences of s. 
+	Since the answer may be very large, return it modulo 10^9 + 7. A 
+	subsequence of a string is a new string that is formed from the original 
+	string by deleting some (can be none) of the characters without disturbing 
+	the relative positions of the remaining characters. (i.e., "ace" is a 
+	subsequence of "abcde" while "aec" is not.
+
+	Example 1:
+	Input: s = "abc"
+	Output: 7
+	Explanation: The 7 distinct subsequences are "a", "b", "c", "ab", "ac", 
+	             "bc", and "abc".
+	
+	Example 2:
+	Input: s = "aba"
+	Output: 6
+	Explanation: The 6 distinct subsequences are "a", "b", "ab", "aa", "ba", 
+	             and "aba".
+	
+	Example 3:
+	Input: s = "aaa"
+	Output: 3
+	Explanation: The 3 distinct subsequences are "a", "aa" and "aaa".
+
+	Constraints:
+	* 1 <= s.length <= 2000
+	* s consists of lowercase English letters.*/
+
+    int distinctSubseqII(string s) {
+        vector<long> freq(26); 
+        for (int i = s.size()-1; i >= 0; --i) 
+            freq[s[i]-'a'] = accumulate(freq.begin(), freq.end(), 1l) % 1'000'000'007; 
+        return accumulate(freq.begin(), freq.end(), 0l) % 1'000'000'007; 
+    }
+
+
     /*941. Valid Mountain Array (Easy)
 	Given an array of integers arr, return true if and only if it is a valid 
 	mountain array. Recall that arr is a mountain array if and only if:
--- leetcode.py
+++ leetcode.py
@@ -20974,6 +20974,41 @@ class UnionFind:
         return ans if ans < inf else 0
 
 
+    """940. Distinct Subsequences II (Hard)
+	Given a string s, return the number of distinct non-empty subsequences of s. 
+	Since the answer may be very large, return it modulo 10^9 + 7. A 
+	subsequence of a string is a new string that is formed from the original 
+	string by deleting some (can be none) of the characters without disturbing 
+	the relative positions of the remaining characters. (i.e., "ace" is a 
+	subsequence of "abcde" while "aec" is not.
+
+	Example 1:
+	Input: s = "abc"
+	Output: 7
+	Explanation: The 7 distinct subsequences are "a", "b", "c", "ab", "ac", 
+	             "bc", and "abc".
+	
+	Example 2:
+	Input: s = "aba"
+	Output: 6
+	Explanation: The 6 distinct subsequences are "a", "b", "ab", "aa", "ba", 
+	             and "aba".
+	
+	Example 3:
+	Input: s = "aaa"
+	Output: 3
+	Explanation: The 3 distinct subsequences are "a", "aa" and "aaa".
+
+	Constraints:
+	* 1 <= s.length <= 2000
+	* s consists of lowercase English letters."""
+
+    def distinctSubseqII(self, s: str) -> int:
+        freq = [0]*26 
+        for i in reversed(range(len(s))): freq[ord(s[i])-97] = (1 + sum(freq)) % 1_000_000_007
+        return sum(freq) % 1_000_000_007
+
+
     """943. Find the Shortest Superstring (Hard)
 	Given an array of strings words, return the smallest string that contains 
 	each string in words as a substring. If there are multiple valid strings of 
