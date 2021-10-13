4732f300f57b4de3a462f89709b4b1f32d875a35
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-13 11:12:45 -0400

    October LeetCoding Challenge 2021-10-13 (1008)

--- leetcode.cpp
+++ leetcode.cpp
@@ -15116,6 +15116,46 @@ public:
     }
 
 
+    /*1008. Construct Binary Search Tree from Preorder Traversal (Medium)
+	Given an array of integers preorder, which represents the preorder 
+	traversal of a BST (i.e., binary search tree), construct the tree and 
+	return its root. It is guaranteed that there is always possible to find a 
+	binary search tree with the given requirements for the given test cases.
+	A binary search tree is a binary tree where for every node, any descendant 
+	of Node.left has a value strictly less than Node.val, and any descendant of 
+	Node.right has a value strictly greater than Node.val. A preorder traversal 
+	of a binary tree displays the value of the node first, then traverses 
+	Node.left, then traverses Node.right.
+
+	Example 1:
+	Input: preorder = [8,5,1,7,10,12]
+	Output: [8,5,10,1,7,null,12]
+
+	Example 2:
+	Input: preorder = [1,3]
+	Output: [1,null,3]
+
+	Constraints:
+	* 1 <= preorder.length <= 100
+	* 1 <= preorder[i] <= 10^8
+	* All the values of preorder are unique.*/
+
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        TreeNode *root = nullptr, *node = nullptr; 
+        stack<TreeNode*> stk;
+        for (auto& x : preorder) 
+            if (!root) root = node = new TreeNode(x); 
+            else if (x < node->val) {
+                stk.push(node); 
+                node = node->left = new TreeNode(x); 
+            } else {
+                while (stk.size() && stk.top()->val < x) node = stk.top(), stk.pop(); 
+                node = node->right = new TreeNode(x); 
+            }
+        return root; 
+    }
+
+
     /*1036. Escape a Large Maze (Hard)
 	There is a 1 million by 1 million grid on an XY-plane, and the coordinates 
 	of each grid square are (x, y). We start at the source = [sx, sy] square 
--- leetcode.py
+++ leetcode.py
@@ -22113,6 +22113,44 @@ class UnionFind:
         return ans 
 
 
+    """1008. Construct Binary Search Tree from Preorder Traversal (Medium)
+	Given an array of integers preorder, which represents the preorder 
+	traversal of a BST (i.e., binary search tree), construct the tree and 
+	return its root. It is guaranteed that there is always possible to find a 
+	binary search tree with the given requirements for the given test cases.
+	A binary search tree is a binary tree where for every node, any descendant 
+	of Node.left has a value strictly less than Node.val, and any descendant of 
+	Node.right has a value strictly greater than Node.val. A preorder traversal 
+	of a binary tree displays the value of the node first, then traverses 
+	Node.left, then traverses Node.right.
+
+	Example 1:
+	Input: preorder = [8,5,1,7,10,12]
+	Output: [8,5,10,1,7,null,12]
+
+	Example 2:
+	Input: preorder = [1,3]
+	Output: [1,null,3]
+
+	Constraints:
+	* 1 <= preorder.length <= 100
+	* 1 <= preorder[i] <= 10^8
+	* All the values of preorder are unique."""
+
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        root = None
+        stack = []
+        for x in preorder: 
+            if not root: root = node = TreeNode(x)
+            elif x < node.val: 
+                stack.append(node)
+                node.left = node = TreeNode(x)
+            else: 
+                while stack and stack[-1].val < x: node = stack.pop()
+                node.right = node = TreeNode(x)
+        return root
+
+
     """1014. Best Sightseeing Pair (Medium)
 	Given an array A of positive integers, A[i] represents the value of the 
 	i-th sightseeing spot, and two sightseeing spots i and j have distance 
