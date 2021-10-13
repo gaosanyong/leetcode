4ef6b3d888a67f3f6c812c1e989f060cb3994c30
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-13 11:29:04 -0400

    update 203

--- leetcode.cpp
+++ leetcode.cpp
@@ -3280,10 +3280,9 @@ public:
 
     ListNode* removeElements(ListNode* head, int val) {
         ListNode dummy(0, head), *node = &dummy; 
-        while (node->next) {
+        while (node->next) 
             if (node->next->val == val) node->next = node->next->next; 
-            else node = node->next; 
-        }
+            else node = node->next;
         return dummy.next; 
     }
 
