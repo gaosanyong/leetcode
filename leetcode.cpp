12021c813873091fd9c42c6f25776c8d5a5a549c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-13 11:48:02 -0400

    solve'em all (117)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2389,6 +2389,59 @@ public:
     }
 
 
+    /*117. Populating Next Right Pointers in Each Node II (Medium)
+	Given a binary tree
+	    struct Node {
+	      int val;
+	      Node *left;
+	      Node *right;
+	      Node *next;
+	    }
+	Populate each next pointer to point to its next right node. If there is no 
+	next right node, the next pointer should be set to NULL. Initially, all 
+	next pointers are set to NULL.
+
+	Example 1:
+	Input: root = [1,2,3,4,5,null,7]
+	Output: [1,#,2,3,#,4,5,7,#]
+	Explanation: Given the above binary tree (Figure A), your function should 
+	             populate each next pointer to point to its next right node, 
+	             just like in Figure B. The serialized output is in level order 
+	             as connected by the next pointers, with '#' signifying the end 
+	             of each level.
+	
+	Example 2:
+	Input: root = []
+	Output: []
+
+	Constraints:
+	* The number of nodes in the tree is in the range [0, 6000].
+	* -100 <= Node.val <= 100
+
+	Follow-up:
+	* You may only use constant extra space.
+	* The recursive approach is fine. You may assume implicit stack space does 
+	  not count as extra space for this problem.*/
+
+    Node* connect(Node* root) {
+        Node *head = root; 
+        while (head) {
+            Node *node = head, *prev = nullptr, *next = nullptr;  
+            while (node) {
+                if (node->left) 
+                    if (prev) prev = prev->next = node->left; 
+                    else prev = next = node->left; 
+                if (node->right) 
+                    if (prev) prev = prev->next = node->right; 
+                    else prev = next = node->right; 
+                node = node->next; 
+            }
+            head = next; 
+        }
+        return root; 
+    }
+
+
     /*118. Pascal's Triangle (Easy)
 	Given an integer numRows, return the first numRows of Pascal's triangle. In 
 	Pascal's triangle, each number is the sum of the two numbers directly above 
