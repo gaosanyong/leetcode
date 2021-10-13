b1c7e2ec1bee402b76f9fba210af36d549173b97
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-13 15:52:30 -0400

    solve'em all (122)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2540,6 +2540,44 @@ public:
     }
 
 
+    /*122. Best Time to Buy and Sell Stock II (Medium)
+	You are given an integer array prices where prices[i] is the price of a 
+	given stock on the ith day. On each day, you may decide to buy and/or sell 
+	the stock. You can only hold at most one share of the stock at any time. 
+	However, you can buy it then immediately sell it on the same day. Find and 
+	return the maximum profit you can achieve.
+
+	Example 1:
+	Input: prices = [7,1,5,3,6,4]
+	Output: 7
+	Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), 
+	             profit = 5-1 = 4. Then buy on day 4 (price = 3) and sell on 
+	             day 5 (price = 6), profit = 6-3 = 3. Total profit is 4 + 3 = 7.
+	
+	Example 2:
+	Input: prices = [1,2,3,4,5]
+	Output: 4
+	Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), 
+	             profit = 5-1 = 4. Total profit is 4.
+	
+	Example 3:
+	Input: prices = [7,6,4,3,1]
+	Output: 0
+	Explanation: There is no way to make a positive profit, so we never buy the 
+	             stock to achieve the maximum profit of 0.
+
+	Constraints:
+	* 1 <= prices.length <= 3 * 10^4
+	* 0 <= prices[i] <= 10^4*/
+
+    int maxProfit(vector<int>& prices) {
+        int ans = 0; 
+        for (int i = 1; i < prices.size(); ++i) 
+            ans += max(0, prices[i] - prices[i-1]); 
+        return ans; 
+    }
+
+
     /*126. Word Ladder II (Hard)
 	A transformation sequence from word beginWord to word endWord using a 
 	dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> 
--- leetcode.py
+++ leetcode.py
@@ -4157,7 +4157,6 @@ class Solution:
         return sum(max(0, prices[i] - prices[i-1]) for i in range(1, len(prices))) 
 
 
-
     """123. Best Time to Buy and Sell Stock III (Hard)
 	Say you have an array for which the ith element is the price of a given 
 	stock on day i. Design an algorithm to find the maximum profit. You may 
