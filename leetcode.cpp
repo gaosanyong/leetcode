5b35ef74635bcef7e9656ef4e3e9c5542c4318f8
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-15 11:31:37 -0400

    October LeetCoding Challenge 2021-10-15 (309)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4565,6 +4565,40 @@ public:
     }
 
 
+    /*309. Best Time to Buy and Sell Stock with Cooldown (Medium)
+	You are given an array prices where prices[i] is the price of a given stock 
+	on the ith day. Find the maximum profit you can achieve. You may complete 
+	as many transactions as you like (i.e., buy one and sell one share of the 
+	stock multiple times) with the following restrictions:
+	* After you sell your stock, you cannot buy stock on the next day (i.e., 
+	  cooldown one day).
+	Note: You may not engage in multiple transactions simultaneously (i.e., you 
+	must sell the stock before you buy again).
+
+	Example 1:
+	Input: prices = [1,2,3,0,2]
+	Output: 3
+	Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+	Example 2:
+	Input: prices = [1]
+	Output: 0
+
+	Constraints:
+	* 1 <= prices.length <= 5000
+	* 0 <= prices[i] <= 1000*/
+
+    int maxProfit(vector<int>& prices) {
+        int sell = 0, cooldown = 0, buy = INT_MAX; 
+        for (auto& x : prices) {
+            buy = min(buy, x - cooldown); 
+            cooldown = sell; 
+            sell = max(sell, x - buy); 
+        }
+        return sell; 
+    }
+
+
     /*318. Maximum Product of Word Lengths (Medium)
 	Given a string array words, return the maximum value of 
 	length(word[i]) * length(word[j]) where the two words do not share common 
