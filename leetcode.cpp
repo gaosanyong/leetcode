e72a03aeb67288a73efe35266e48ad92790b9a72
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-14 23:18:55 -0400

    solve'em all (1001)

--- leetcode.cpp
+++ leetcode.cpp
@@ -15376,6 +15376,79 @@ public:
     }
 
 
+    /*1001. Grid Illumination (Hard)
+	There is a 2D grid of size n x n where each cell of this grid has a lamp 
+	that is initially turned off. You are given a 2D array of lamp positions 
+	lamps, where lamps[i] = [rowi, coli] indicates that the lamp at 
+	grid[rowi][coli] is turned on. Even if the same lamp is listed more than 
+	once, it is turned on. When a lamp is turned on, it illuminates its cell 
+	and all other cells in the same row, column, or diagonal. You are also 
+	given another 2D array queries, where queries[j] = [rowj, colj]. For the 
+	jth query, determine whether grid[rowj][colj] is illuminated or not. After 
+	answering the jth query, turn off the lamp at grid[rowj][colj] and its 8 
+	adjacent lamps if they exist. A lamp is adjacent if its cell shares either 
+	a side or corner with grid[rowj][colj]. Return an array of integers ans, 
+	where ans[j] should be 1 if the cell in the jth query was illuminated, or 0 
+	if the lamp was not.
+
+	Example 1:
+	Input: n = 5, lamps = [[0,0],[4,4]], queries = [[1,1],[1,0]]
+	Output: [1,0]
+	Explanation: We have the initial grid with all lamps turned off. In the 
+	             above picture we see the grid after turning on the lamp at 
+	             grid[0][0] then turning on the lamp at grid[4][4]. The 0th 
+	             query asks if the lamp at grid[1][1] is illuminated or not 
+	             (the blue square). It is illuminated, so set ans[0] = 1. Then, 
+	             we turn off all lamps in the red square. The 1st query asks if 
+	             the lamp at grid[1][0] is illuminated or not (the blue square). 
+	             It is not illuminated, so set ans[1] = 0. Then, we turn off 
+	             all lamps in the red rectangle.
+
+	Example 2:
+	Input: n = 5, lamps = [[0,0],[4,4]], queries = [[1,1],[1,1]]
+	Output: [1,1]
+
+	Example 3:
+	Input: n = 5, lamps = [[0,0],[0,4]], queries = [[0,4],[0,1],[1,4]]
+	Output: [1,1,0]
+
+	Constraints:
+	* 1 <= n <= 109
+	* 0 <= lamps.length <= 20000
+	* 0 <= queries.length <= 20000
+	* lamps[i].length == 2
+	* 0 <= rowi, coli < n
+	* queries[j].length == 2
+	* 0 <= rowj, colj < n*/
+
+    vector<int> gridIllumination(int n, vector<vector<int>>& lamps, vector<vector<int>>& queries) {
+        set<pair<int, int>> ss; 
+        unordered_map<int, int> rows, cols, anti, diag; 
+        
+        for (auto& lamp : lamps) {
+            int i = lamp[0], j = lamp[1]; 
+            if (ss.insert({i, j}).second) {
+                ++rows[i]; ++anti[i+j]; 
+                ++cols[j]; ++diag[i-j]; 
+            }
+        }
+        
+        vector<int> ans; 
+        for (auto& query : queries) {
+            int i = query[0], j = query[1]; 
+            if (rows[i] || cols[j] || anti[i+j] || diag[i-j]) ans.push_back(1); 
+            else ans.push_back(0); 
+            for (int ii = i-1; ii <= i+1; ++ii) 
+                for (int jj = j-1; jj <= j+1; ++jj) 
+                    if (ss.erase({ii, jj})) {
+                        --rows[ii]; --anti[ii+jj]; 
+                        --cols[jj]; --diag[ii-jj]; 
+                    }
+        }
+        return ans; 
+    }
+
+
     /*1002. Find Common Characters (Easy)
 	Given an array words of strings made only from lowercase letters, return a 
 	list of all characters that show up in all strings within the list 
--- leetcode.py
+++ leetcode.py
@@ -22241,6 +22241,78 @@ class UnionFind:
         return fn(0, len(stones))
 
 
+    """1001. Grid Illumination (Hard)
+	There is a 2D grid of size n x n where each cell of this grid has a lamp 
+	that is initially turned off. You are given a 2D array of lamp positions 
+	lamps, where lamps[i] = [rowi, coli] indicates that the lamp at 
+	grid[rowi][coli] is turned on. Even if the same lamp is listed more than 
+	once, it is turned on. When a lamp is turned on, it illuminates its cell 
+	and all other cells in the same row, column, or diagonal. You are also 
+	given another 2D array queries, where queries[j] = [rowj, colj]. For the 
+	jth query, determine whether grid[rowj][colj] is illuminated or not. After 
+	answering the jth query, turn off the lamp at grid[rowj][colj] and its 8 
+	adjacent lamps if they exist. A lamp is adjacent if its cell shares either 
+	a side or corner with grid[rowj][colj]. Return an array of integers ans, 
+	where ans[j] should be 1 if the cell in the jth query was illuminated, or 0 
+	if the lamp was not.
+
+	Example 1:
+	Input: n = 5, lamps = [[0,0],[4,4]], queries = [[1,1],[1,0]]
+	Output: [1,0]
+	Explanation: We have the initial grid with all lamps turned off. In the 
+	             above picture we see the grid after turning on the lamp at 
+	             grid[0][0] then turning on the lamp at grid[4][4]. The 0th 
+	             query asks if the lamp at grid[1][1] is illuminated or not 
+	             (the blue square). It is illuminated, so set ans[0] = 1. Then, 
+	             we turn off all lamps in the red square. The 1st query asks if 
+	             the lamp at grid[1][0] is illuminated or not (the blue square). 
+	             It is not illuminated, so set ans[1] = 0. Then, we turn off 
+	             all lamps in the red rectangle.
+
+	Example 2:
+	Input: n = 5, lamps = [[0,0],[4,4]], queries = [[1,1],[1,1]]
+	Output: [1,1]
+
+	Example 3:
+	Input: n = 5, lamps = [[0,0],[0,4]], queries = [[0,4],[0,1],[1,4]]
+	Output: [1,1,0]
+
+	Constraints:
+	* 1 <= n <= 109
+	* 0 <= lamps.length <= 20000
+	* 0 <= queries.length <= 20000
+	* lamps[i].length == 2
+	* 0 <= rowi, coli < n
+	* queries[j].length == 2
+	* 0 <= rowj, colj < n"""
+
+    def gridIllumination(self, n: int, lamps: List[List[int]], queries: List[List[int]]) -> List[int]:
+        lamps = {(i, j) for i, j in lamps}
+        rows = defaultdict(int)
+        cols = defaultdict(int)
+        anti = defaultdict(int)
+        diag = defaultdict(int)
+        for i, j in lamps: 
+            rows[i] += 1
+            cols[j] += 1
+            anti[i+j] += 1
+            diag[i-j] += 1
+        
+        ans = []
+        for i, j in queries: 
+            if rows[i] or cols[j] or anti[i+j] or diag[i-j]: ans.append(1)
+            else: ans.append(0)
+            for ii in range(i-1, i+2): 
+                for jj in range(j-1, j+2): 
+                    if (ii, jj) in lamps: 
+                        lamps.remove((ii, jj))
+                        rows[ii] -= 1
+                        cols[jj] -= 1
+                        anti[ii+jj] -= 1
+                        diag[ii-jj] -= 1
+        return ans 
+
+
     """1004. Max Consecutive Ones III (Medium)
 	Given a binary array nums and an integer k, return the maximum number of 
 	consecutive 1's in the array if you can flip at most k 0's.
