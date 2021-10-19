3126cfee0d5ff53eb846f279ac1b01d1535e17d9
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-19 14:38:33 -0400

    solve'em all (707)

--- leetcode.cpp
+++ leetcode.cpp
@@ -30827,6 +30827,122 @@ public:
 };
 
 
+/*707. Design Linked List (Medium)
+Design your implementation of the linked list. You can choose to use a singly 
+or doubly linked list. A node in a singly linked list should have two 
+attributes: val and next. val is the value of the current node, and next is a 
+pointer/reference to the next node. If you want to use the doubly linked list, 
+you will need one more attribute prev to indicate the previous node in the 
+linked list. Assume all nodes in the linked list are 0-indexed. Implement the 
+MyLinkedList class:
+* MyLinkedList() Initializes the MyLinkedList object.
+* int get(int index) Get the value of the indexth node in the linked list. If 
+  the index is invalid, return -1.
+* void addAtHead(int val) Add a node of value val before the first element of 
+  the linked list. After the insertion, the new node will be the first node of 
+  the linked list.
+* void addAtTail(int val) Append a node of value val as the last element of the 
+  linked list.
+* void addAtIndex(int index, int val) Add a node of value val before the 
+  indexth node in the linked list. If index equals the length of the linked 
+  list, the node will be appended to the end of the linked list. If index is 
+  greater than the length, the node will not be inserted.
+* void deleteAtIndex(int index) Delete the indexth node in the linked list, if 
+  the index is valid.
+
+Example 1:
+Input: ["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+       [[], [1], [3], [1, 2], [1], [1], [1]]
+Output: [null, null, null, null, 2, null, 3]
+Explanation: 
+MyLinkedList myLinkedList = new MyLinkedList();
+myLinkedList.addAtHead(1);
+myLinkedList.addAtTail(3);
+myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+myLinkedList.get(1);              // return 2
+myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+myLinkedList.get(1);              // return 3
+ 
+Constraints:
+* 0 <= index, val <= 1000
+* Please do not use the built-in LinkedList library.
+* At most 2000 calls will be made to get, addAtHead, addAtTail, addAtIndex and 
+  deleteAtIndex.
+
+struct Node {
+    int val = 0; 
+    Node *prev, *next; 
+    Node(int val, Node* prev=nullptr, Node* next=nullptr) {
+        this->val = val; 
+        this->prev = prev; 
+        this->next = next; 
+    }
+};*/
+
+class MyLinkedList {
+    int size = 0; 
+    Node *head = nullptr, *tail = nullptr; 
+    
+    Node* _moveTo(int index) {
+        Node* node = nullptr; 
+        if (0 <= index && index <= size) 
+            if (index <= size/2) {
+                node = head->next; 
+                for (int i = 0; i < index; ++i) node = node->next; 
+            } else {
+                node = tail; 
+                for (int i = 0; i < size-index; ++i) node = node->prev; 
+            }
+        return node; 
+    }
+    
+public:
+    MyLinkedList() {
+        head = new Node(-1); 
+        tail = new Node(-1); 
+        head->next = tail; 
+        tail->prev = head; 
+    }
+    
+    ~MyLinkedList() {
+        delete head; 
+        delete tail; 
+    }
+    
+    int get(int index) {
+        Node* node = _moveTo(index); 
+        return node ? node->val : -1; 
+    }
+    
+    void addAtHead(int val) {
+        addAtIndex(0, val); 
+    }
+    
+    void addAtTail(int val) {
+        addAtIndex(size, val); 
+    }
+    
+    void addAtIndex(int index, int val) {
+        Node* node = _moveTo(index); 
+        if (node) {
+            ++size; 
+            Node* temp = new Node(val, node->prev, node); 
+            node->prev = node->prev->next = temp; 
+        }
+    }
+    
+    void deleteAtIndex(int index) {
+        Node* node = _moveTo(index); 
+        if (node && node != tail) {
+            --size; 
+            node->next->prev = node->prev; 
+            node->prev->next = node->next; 
+            delete node; 
+        }
+    }
+};
+
+
 /*729. My Calendar I (Medium)
 Implement a MyCalendar class to store your events. A new event can be added if 
 adding the event will not cause a double booking. Your class will have the 
--- leetcode.py
+++ leetcode.py
@@ -51181,6 +51181,99 @@ class MyHashMap:
             node = node.next 
 
 
+"""707. Design Linked List (Medium)
+Design your implementation of the linked list. You can choose to use a singly 
+or doubly linked list. A node in a singly linked list should have two 
+attributes: val and next. val is the value of the current node, and next is a 
+pointer/reference to the next node. If you want to use the doubly linked list, 
+you will need one more attribute prev to indicate the previous node in the 
+linked list. Assume all nodes in the linked list are 0-indexed. Implement the 
+MyLinkedList class:
+* MyLinkedList() Initializes the MyLinkedList object.
+* int get(int index) Get the value of the indexth node in the linked list. If 
+  the index is invalid, return -1.
+* void addAtHead(int val) Add a node of value val before the first element of 
+  the linked list. After the insertion, the new node will be the first node of 
+  the linked list.
+* void addAtTail(int val) Append a node of value val as the last element of the 
+  linked list.
+* void addAtIndex(int index, int val) Add a node of value val before the 
+  indexth node in the linked list. If index equals the length of the linked 
+  list, the node will be appended to the end of the linked list. If index is 
+  greater than the length, the node will not be inserted.
+* void deleteAtIndex(int index) Delete the indexth node in the linked list, if 
+  the index is valid.
+
+Example 1:
+Input: ["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+       [[], [1], [3], [1, 2], [1], [1], [1]]
+Output: [null, null, null, null, 2, null, 3]
+Explanation: 
+MyLinkedList myLinkedList = new MyLinkedList();
+myLinkedList.addAtHead(1);
+myLinkedList.addAtTail(3);
+myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+myLinkedList.get(1);              // return 2
+myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+myLinkedList.get(1);              // return 3
+ 
+Constraints:
+* 0 <= index, val <= 1000
+* Please do not use the built-in LinkedList library.
+* At most 2000 calls will be made to get, addAtHead, addAtTail, addAtIndex and 
+  deleteAtIndex."""
+
+class ListNode: 
+    
+    def __init__(self, val=0, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = ListNode(-1)
+        self.tail = ListNode(-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head 
+        self.size = 0
+
+    def _moveTo(self, index: int) -> ListNode: 
+        if 0 <= index <= self.size: 
+            if index <= self.size//2: 
+                node = self.head.next 
+                for _ in range(index): node = node.next 
+            else: 
+                node = self.tail
+                for _ in range(self.size-index): node = node.prev 
+            return node 
+    
+    def get(self, index: int) -> int:
+        node = self._moveTo(index)
+        return node.val if node else -1 
+
+    def addAtHead(self, val: int) -> None:
+        self.addAtIndex(0, val)
+
+    def addAtTail(self, val: int) -> None:
+        self.addAtIndex(self.size, val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        node = self._moveTo(index)
+        if node: 
+            self.size += 1
+            node.prev.next = node.prev = ListNode(val, node.prev, node)
+
+    def deleteAtIndex(self, index: int) -> None:
+        node = self._moveTo(index)
+        if node and node != self.tail: 
+            self.size -= 1
+            node.prev.next = node.next
+            node.next.prev = node.prev
+
+
 """716. Max Stack (Easy)
 Design a max stack data structure that supports the stack operations and 
 supports finding the stack's maximum element.
