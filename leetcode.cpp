d916e2bcef67c133715566980765466d0a3624a2
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-19 22:05:15 -0400

    October LeetCoding Challenge 2021-10-20 (151)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3134,6 +3134,61 @@ public:
     }
 
 
+    /*151. Reverse Words in a String (Medium)
+	Given an input string s, reverse the order of the words. A word is defined 
+	as a sequence of non-space characters. The words in s will be separated by 
+	at least one space. Return a string of the words in reverse order 
+	concatenated by a single space. Note that s may contain leading or trailing 
+	spaces or multiple spaces between two words. The returned string should 
+	only have a single space separating the words. Do not include any extra 
+	spaces.
+
+	Example 1:
+	Input: s = "the sky is blue"
+	Output: "blue is sky the"
+
+	Example 2:
+	Input: s = "  hello world  "
+	Output: "world hello"
+	Explanation: Your reversed string should not contain leading or trailing 
+	             spaces.
+	
+	Example 3:
+	Input: s = "a good   example"
+	Output: "example good a"
+	Explanation: You need to reduce multiple spaces between two words to a 
+	             single space in the reversed string.
+	
+	Example 4:
+	Input: s = "  Bob    Loves  Alice   "
+	Output: "Alice Loves Bob"
+
+	Example 5:
+	Input: s = "Alice does not even like bob"
+	Output: "bob like even not does Alice"
+
+	Constraints:
+	* 1 <= s.length <= 10^4
+	* s contains English letters (upper-case and lower-case), digits, and 
+	  spaces ' '.
+	* There is at least one word in s.
+	 
+	Follow-up: If the string data type is mutable in your language, can you 
+	           solve it in-place with O(1) extra space?*/
+
+    string reverseWords(string s) {
+        vector<string> words; 
+        istringstream iss(s); 
+        string buf, ans; 
+        while (iss >> buf) words.push_back(buf); 
+        for (int i = words.size()-1; i >= 0; --i) {
+            if (ans.size()) ans.push_back(' '); 
+            ans += words[i]; 
+        }
+        return ans; 
+    }
+
+
     /*152. Maximum Product Subarray (Medium)
 	Given an integer array nums, find a contiguous non-empty subarray within 
 	the array that has the largest product, and return the product. It is 
