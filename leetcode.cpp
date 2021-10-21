f118f1be6e33e9585caa39af027118e804a4ff56
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-21 11:58:30 -0400

    solve'em all (413)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5868,6 +5868,38 @@ public:
     }
 
 
+    /*413. Arithmetic Slices (Medium)
+	An integer array is called arithmetic if it consists of at least three 
+	elements and if the difference between any two consecutive elements is the 
+	same. For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic 
+	sequences. Given an integer array nums, return the number of arithmetic 
+	subarrays of nums. A subarray is a contiguous subsequence of the array.
+
+	Example 1:
+	Input: nums = [1,2,3,4]
+	Output: 3
+	Explanation: We have 3 arithmetic slices in nums: [1, 2, 3], [2, 3, 4] and 
+	             [1,2,3,4] itself.
+	
+	Example 2:
+	Input: nums = [1]
+	Output: 0
+
+	Constraints:
+	* 1 <= nums.length <= 5000
+	* -1000 <= nums[i] <= 1000*/
+
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int ans = 0; 
+        for (int i = 2, cnt = 0; i < nums.size(); ++i) {
+            if (nums[i-2] - nums[i-1] == nums[i-1] - nums[i]) ++cnt; 
+            else cnt = 0; 
+            ans += cnt; 
+        }
+        return ans; 
+    }
+
+
     /*415. Add Strings (Easy)
 	Given two non-negative integers, num1 and num2 represented as string, 
 	return the sum of num1 and num2 as a string. You must solve the problem 
