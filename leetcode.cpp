d058944c397455ad329743eef0e017002ddec70e
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-22 15:49:25 -0400

    update 70

--- leetcode.cpp
+++ leetcode.cpp
@@ -1609,21 +1609,21 @@ public:
         int m = board.size(), n = board[0].size(), dir[5] = {-1, 0, 1, 0, -1}; 
         
         function<bool(int, int, int)> fn = [&](int i, int j, int k) {
-            if (k == word.size()) return true; 
-            if (0 <= i && i < m && 0 <= j && j < n && board[i][j] == word[k]) {
+            if (board[i][j] == word[k]) {
+                if (k == word.size()-1) return true; 
                 char temp = board[i][j]; 
                 board[i][j] = '#'; 
                 for (int x = 0; x < 4; ++x) {
                     int ii = i + dir[x], jj = j + dir[x+1]; 
-                    if (fn(ii, jj, k+1)) return true;
+                    if (0 <= ii && ii < m && 0 <= jj && jj < n && fn(ii, jj, k+1)) return true; 
                 }
-                board[i][j] = temp; // backtracking
+                board[i][j] = temp; 
             }
             return false; 
         }; 
         
         for (int i = 0; i < m; ++i) 
-            for (int j = 0; j < n; ++j)
+            for (int j = 0; j < n; ++j) 
                 if (fn(i, j, 0)) return true; 
         return false; 
     }
