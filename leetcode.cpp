ec27a28ecfe257c531cf21ccc273c165ed000445
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-22 13:43:49 -0400

    solve'em all (119)

--- leetcode.cpp
+++ leetcode.cpp
@@ -2680,6 +2680,39 @@ public:
     }
 
 
+    /*119. Pascal's Triangle II (Easy)
+	Given an integer rowIndex, return the rowIndexth (0-indexed) row of the 
+	Pascal's triangle. In Pascal's triangle, each number is the sum of the two 
+	numbers directly above it as shown:
+	 
+	Example 1:
+	Input: rowIndex = 3
+	Output: [1,3,3,1]
+
+	Example 2:
+	Input: rowIndex = 0
+	Output: [1]
+
+	Example 3:
+	Input: rowIndex = 1
+	Output: [1,1]
+
+	Constraints: 0 <= rowIndex <= 33
+
+	Follow up: Could you optimize your algorithm to use only O(rowIndex) extra 
+	           space?*/
+
+    vector<int> getRow(int rowIndex) {
+        vector<int> ans; 
+        for (int i = 0; i <= rowIndex; ++i) {
+            ans.push_back(1); 
+            for (int j = ans.size()-2; j > 0; --j) 
+                ans[j] += ans[j-1]; 
+        }
+        return ans; 
+    }
+
+
     /*120. Triangle (Medium)
 	Given a triangle array, return the minimum path sum from top to bottom. For 
 	each step, you may move to an adjacent number of the row below. More 
