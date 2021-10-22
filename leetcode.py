e90874ff44f08d872dfe589a76cd78a334d366de
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-21 23:44:31 -0400

    solve'em all (1314)

--- leetcode.py
+++ leetcode.py
@@ -28588,6 +28588,42 @@ class UnionFind:
         return fn(0, 0, 0)
 
 
+    """1314. Matrix Block Sum (Medium)
+	Given a m x n matrix mat and an integer k, return a matrix answer where 
+	each answer[i][j] is the sum of all elements mat[r][c] for:
+	* i - k <= r <= i + k,
+	* j - k <= c <= j + k, and
+	* (r, c) is a valid position in the matrix.
+
+	Example 1:
+	Input: mat = [[1,2,3],[4,5,6],[7,8,9]], k = 1
+	Output: [[12,21,16],[27,45,33],[24,39,28]]
+
+	Example 2:
+	Input: mat = [[1,2,3],[4,5,6],[7,8,9]], k = 2
+	Output: [[45,45,45],[45,45,45],[45,45,45]]
+
+	Constraints:
+	* m == mat.length
+	* n == mat[i].length
+	* 1 <= m, n, k <= 100
+	* 1 <= mat[i][j] <= 100"""
+
+    def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        prefix = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(m):
+            for j in range(n): 
+                prefix[i+1][j+1] = mat[i][j] + prefix[i][j+1] + prefix[i+1][j] - prefix[i][j]
+        ans = [[0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n): 
+                r0, r1 = max(0, i-k), min(m-1, i+k)
+                c0, c1 = max(0, j-k), min(n-1, j+k)
+                ans[i][j] = prefix[r1+1][c1+1] - prefix[r0][c1+1] - prefix[r1+1][c0] + prefix[r0][c0]
+        return ans 
+
+
     """1317. Convert Integer to the Sum of Two No-Zero Integers (Easy)
 	Given an integer n. No-Zero integer is a positive integer which doesn't 
 	contain any 0 in its decimal representation. Return a list of two integers 
