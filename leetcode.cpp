e75da012f87a8dff5e6100a8435ca7514395aed4
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-21 23:16:07 -0400

    solve'em all (450)

--- leetcode.cpp
+++ leetcode.cpp
@@ -6235,6 +6235,54 @@ public:
     }
 
 
+    /*450. Delete Node in a BST (Medium)
+	Given a root node reference of a BST and a key, delete the node with the 
+	given key in the BST. Return the root node reference (possibly updated) of 
+	the BST. Basically, the deletion can be divided into two stages:
+	* Search for a node to remove.
+	* If the node is found, delete the node.
+
+	Example 1:
+	Input: root = [5,3,6,2,4,null,7], key = 3
+	Output: [5,4,6,2,null,null,7]
+	Explanation: Given key to delete is 3. So we find the node with value 3 and 
+	             delete it. One valid answer is [5,4,6,2,null,null,7], shown in 
+	             the above BST. Please notice that another valid answer is 
+	             [5,2,6,null,4,null,7] and it's also accepted.
+
+	Example 2:
+	Input: root = [5,3,6,2,4,null,7], key = 0
+	Output: [5,3,6,2,4,null,7]
+	Explanation: The tree does not contain a node with value = 0.
+
+	Example 3:
+	Input: root = [], key = 0
+	Output: []
+
+	Constraints:
+	* The number of nodes in the tree is in the range [0, 10^4].
+	* -10^5 <= Node.val <= 10^5
+	* Each node has a unique value.
+	* root is a valid binary search tree.
+	* -10^5 <= key <= 10^5
+
+	Follow up: Could you solve it with time complexity O(height of tree)?*/
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root) 
+            if (root->val < key) root->right = deleteNode(root->right, key); 
+            else if (root->val > key) root->left = deleteNode(root->left, key); 
+            else {
+                if (!root->left || !root->right) return root->left ? root->left : root->right; 
+                TreeNode* node = root->left; 
+                for (; node->right; node = node->right); 
+                root->val = node->val; 
+                root->left = deleteNode(root->left, node->val); 
+            }
+        return root; 
+    }
+
+
     /*451. Sort Characters By Frequency (Medium)
 	Given a string s, sort it in decreasing order based on the frequency of the 
 	characters. The frequency of a character is the number of times it appears 
--- leetcode.py
+++ leetcode.py
@@ -11691,6 +11691,52 @@ class UnionFind:
         return ans 
 
 
+    """450. Delete Node in a BST (Medium)
+	Given a root node reference of a BST and a key, delete the node with the 
+	given key in the BST. Return the root node reference (possibly updated) of 
+	the BST. Basically, the deletion can be divided into two stages:
+	* Search for a node to remove.
+	* If the node is found, delete the node.
+
+	Example 1:
+	Input: root = [5,3,6,2,4,null,7], key = 3
+	Output: [5,4,6,2,null,null,7]
+	Explanation: Given key to delete is 3. So we find the node with value 3 and 
+	             delete it. One valid answer is [5,4,6,2,null,null,7], shown in 
+	             the above BST. Please notice that another valid answer is 
+	             [5,2,6,null,4,null,7] and it's also accepted.
+
+	Example 2:
+	Input: root = [5,3,6,2,4,null,7], key = 0
+	Output: [5,3,6,2,4,null,7]
+	Explanation: The tree does not contain a node with value = 0.
+
+	Example 3:
+	Input: root = [], key = 0
+	Output: []
+
+	Constraints:
+	* The number of nodes in the tree is in the range [0, 10^4].
+	* -10^5 <= Node.val <= 10^5
+	* Each node has a unique value.
+	* root is a valid binary search tree.
+	* -10^5 <= key <= 10^5
+
+	Follow up: Could you solve it with time complexity O(height of tree)?"""
+
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root: return 
+        if root.val < key: root.right = self.deleteNode(root.right, key)
+        elif root.val > key: root.left = self.deleteNode(root.left, key)
+        else: # found 
+            if not root.left or not root.right: return root.left or root.right 
+            node = root.left 
+            while node.right: node = node.right 
+            root.val = node.val 
+            root.left = self.deleteNode(root.left, node.val)
+        return root 
+
+
     """451. Sort Characters By Frequency (Medium)
 	Given a string s, sort it in decreasing order based on the frequency of the 
 	characters. The frequency of a character is the number of times it appears 
