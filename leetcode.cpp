fef3bf313c1ae11fc2b3bce2a38f70d1fb68b0fc
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-22 21:59:53 -0400

    October LeetCoding Challenge 2021-10-23 (154)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3471,6 +3471,47 @@ public:
     }
 
 
+    /*154. Find Minimum in Rotated Sorted Array II (Hard)
+	Suppose an array of length n sorted in ascending order is rotated between 1 
+	and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
+	* [4,5,6,7,0,1,4] if it was rotated 4 times.
+	* [0,1,4,4,5,6,7] if it was rotated 7 times.
+	Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time 
+	results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]]. Given the 
+	sorted rotated array nums that may contain duplicates, return the minimum 
+	element of this array. You must decrease the overall operation steps as 
+	much as possible.
+
+	Example 1:
+	Input: nums = [1,3,5]
+	Output: 1
+
+	Example 2:
+	Input: nums = [2,2,2,0,1]
+	Output: 0
+
+	Constraints:
+	* n == nums.length
+	* 1 <= n <= 5000
+	* -5000 <= nums[i] <= 5000
+	* nums is sorted and rotated between 1 and n times.
+
+	Follow up: This problem is similar to Find Minimum in Rotated Sorted Array, 
+	           but nums may contain duplicates. Would this affect the runtime 
+	           complexity? How and why?*/
+
+    int findMin(vector<int>& nums) {
+        int lo = 0, hi = nums.size()-1; 
+        while (lo < hi) {
+            int mid = lo + (hi - lo)/2; 
+            if (nums[mid] < nums[hi]) hi = mid; 
+            else if (nums[mid] == nums[hi]) hi -= 1; 
+            else lo = mid + 1; 
+        }
+        return nums[lo]; 
+    }
+
+
     /*162. Find Peak Element (Medium)
 	A peak element is an element that is strictly greater than its neighbors. 
 	Given an integer array nums, find a peak element, and return its index. If 
