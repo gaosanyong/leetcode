9c0de64714b37154c0d88a57783e115f16559d4c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-23 16:18:54 -0400

    solve'em all (516)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7238,6 +7238,39 @@ public:
     }
 
 
+    /*516. Longest Palindromic Subsequence (Medium)
+	Given a string s, find the longest palindromic subsequence's length in s. A 
+	subsequence is a sequence that can be derived from another sequence by 
+	deleting some or no elements without changing the order of the remaining 
+	elements.
+
+	Example 1:
+	Input: s = "bbbab"
+	Output: 4
+	Explanation: One possible longest palindromic subsequence is "bbbb".
+
+	Example 2:
+	Input: s = "cbbd"
+	Output: 2
+	Explanation: One possible longest palindromic subsequence is "bb".
+
+	Constraints:
+	* 1 <= s.length <= 1000
+	* s consists only of lowercase English letters.*/
+
+    int longestPalindromeSubseq(string s) {
+        int n = s.size(); 
+        vector<vector<int>> dp(n, vector<int>(n)); 
+        for (int i = n-1; i >= 0; --i) {
+            dp[i][i] = 1; 
+            for (int j = i+1; j < n; ++j) 
+                if (s[i] == s[j]) dp[i][j] = 2 + dp[i+1][j-1]; 
+                else dp[i][j] = max(dp[i+1][j], dp[i][j-1]); 
+        }
+        return dp[0][n-1]; 
+    }
+
+
     /*517. Super Washing Machines (Hard)
 	You have n super washing machines on a line. Initially, each washing 
 	machine has some dresses or is empty. For each move, you could choose any m 
