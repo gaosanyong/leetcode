767972a1328535e7a81400fd85a0dc94b2d530ec
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-23 13:32:44 -0400

    solve'em all (264)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4775,6 +4775,36 @@ public:
     }
 
 
+    /*264. Ugly Number II (Medium)
+	An ugly number is a positive integer whose prime factors are limited to 2, 
+	3, and 5. Given an integer n, return the nth ugly number.
+
+	Example 1:
+	Input: n = 10
+	Output: 12
+	Explanation: [1, 2, 3, 4, 5, 6, 8, 9, 10, 12] is the sequence of the first 
+	             10 ugly numbers.
+	
+	Example 2:
+	Input: n = 1
+	Output: 1
+	Explanation: 1 has no prime factors, therefore all of its prime factors are 
+	             limited to 2, 3, and 5.
+
+	Constraints: 1 <= n <= 1690*/
+
+    int nthUglyNumber(int n) {
+        vector<int> dp(1, 1); 
+        for (int i = 0, p2 = 0, p3 = 0, p5 = 0; i < n-1; ++i) {
+            dp.push_back(min({2*dp[p2], 3*dp[p3], 5*dp[p5]})); 
+            if (2*dp[p2] == dp.back()) ++p2; 
+            if (3*dp[p3] == dp.back()) ++p3; 
+            if (5*dp[p5] == dp.back()) ++p5; 
+        }
+        return dp.back(); 
+    }
+
+
     /*265. Paint House II (Hard)
 	There are a row of n houses, each house can be painted with one of the k 
 	colors. The cost of painting each house with a certain color is different. 
