a2fd47d4a4e8b7b6710e8292907664f1a971affb
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-23 13:54:37 -0400

    update 63

--- leetcode.cpp
+++ leetcode.cpp
@@ -1378,19 +1378,13 @@ public:
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size(), n = obstacleGrid[0].size(); 
-        vector<vector<int>> dp(m, vector<int>(n, 0)); 
-        if (obstacleGrid[0][0] == 0) {
-            dp[0][0] = 1; 
-            for (int i = 0; i < m; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    if (obstacleGrid[i][j] == 0) {
-                        if (i) dp[i][j] += dp[i-1][j];
-                        if (j) dp[i][j] += dp[i][j-1]; 
-                    }
-                }
-            }
-        }
-        return dp[m-1][n-1]; 
+        vector<int> dp(n); 
+        dp[0] = 1; 
+        for (int i = 0; i < m; ++i) 
+            for (int j = 0; j < n; ++j) 
+                if (obstacleGrid[i][j]) dp[j] = 0; 
+                else if (j) dp[j] += dp[j-1]; 
+        return dp.back(); 
     }
 
 
