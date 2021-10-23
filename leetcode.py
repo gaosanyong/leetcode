d44f26e6d678d5faed97dd19fd512b3077659494
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-22 22:14:15 -0400

    update 162

--- leetcode.py
+++ leetcode.py
@@ -5532,7 +5532,7 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         lo, hi = 0, len(nums)-1
         while lo < hi:
-            mid = (lo + hi)//2
+            mid = lo + hi >> 1
             if nums[mid] < nums[mid+1]: lo = mid + 1
             else: hi = mid
         return lo
