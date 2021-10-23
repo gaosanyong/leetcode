d96f6d94a9370da173e9a6b9efafc4af4206bb10
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-23 14:41:57 -0400

    solve'em all (64)

--- leetcode.cpp
+++ leetcode.cpp
@@ -1388,6 +1388,37 @@ public:
     }
 
 
+    /*64. Minimum Path Sum (Medium)
+	Given a m x n grid filled with non-negative numbers, find a path from top 
+	left to bottom right, which minimizes the sum of all numbers along its path.
+	Note: You can only move either down or right at any point in time.
+
+	Example 1:
+	Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+	Output: 7
+	Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+	Example 2:
+	Input: grid = [[1,2,3],[4,5,6]]
+	Output: 12
+
+	Constraints:
+	* m == grid.length
+	* n == grid[i].length
+	* 1 <= m, n <= 200
+	* 0 <= grid[i][j] <= 100*/
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(); 
+        for (int i = 0; i < m; ++i) 
+            for (int j = 0; j < n; ++j) 
+                if (i == 0 && j) grid[i][j] += grid[i][j-1]; 
+                else if (i && j == 0) grid[i][j] += grid[i-1][j]; 
+                else if (i && j) grid[i][j] += min(grid[i-1][j], grid[i][j-1]); 
+        return grid[m-1][n-1]; 
+    }
+
+
     /*73. Set Matrix Zeroes (Medium)
 	Given an m x n integer matrix matrix, if an element is 0, set its entire 
 	row and column to 0's, and return the matrix. You must do it in place.
