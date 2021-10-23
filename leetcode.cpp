f673acddca9c75c03becbb502c3b722e8a457326
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-23 14:33:29 -0400

    update 304

--- leetcode.cpp
+++ leetcode.cpp
@@ -31095,17 +31095,14 @@ Constraints:
 * At most 104 calls will be made to sumRegion.*/
 
 class NumMatrix {
-private: 
     vector<vector<int>> prefix; 
 public:
     NumMatrix(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        prefix = vector<vector<int>>(m+1, vector<int>(n+1, 0));
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                prefix[i+1][j+1] = matrix[i][j] + prefix[i+1][j] + prefix[i][j+1] - prefix[i][j]; 
-            }
-        }
+        int m = matrix.size(), n = matrix[0].size(); 
+        prefix = vector<vector<int>>(m+1, vector<int>(n+1)); 
+        for (int i = 0; i < m; ++i) 
+            for (int j = 0; j < n; ++j) 
+                prefix[i+1][j+1] = matrix[i][j] + prefix[i][j+1] + prefix[i+1][j] - prefix[i][j]; 
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
