882f2b2bc3e92cacfc82fddf08acf9ba028586af
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-22 23:23:57 -0400

    update 583

--- leetcode.cpp
+++ leetcode.cpp
@@ -8474,19 +8474,14 @@ public:
 	* word1 and word2 consist of only lowercase English letters.*/
 
     int minDistance(string word1, string word2) {
+        // longest common subsequence 
         int m = word1.size(), n = word2.size(); 
-        vector<vector<int>> dp (m+1, vector<int>(n+1, 0)); 
-        for (int i = 0; i < m; ++i) dp[i][n] = m - i; 
-        for (int j = 0; j < n; ++j) dp[m][j] = n - j; 
-        for (int i = m-1; i >= 0; --i) {
-            for (int j = n-1; j >= 0; --j) {
-                if (word1[i] == word2[j]) 
-                    dp[i][j] = dp[i+1][j+1]; 
-                else
-                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1]); 
-            }
-        }
-        return dp[0][0]; 
+        vector<vector<int>> dp(m+1, vector<int>(n+1)); 
+        for (int i = m-1; i >= 0; --i) 
+            for (int j = n-1; j >= 0; --j) 
+                if (word1[i] == word2[j]) dp[i][j] = 1 + dp[i+1][j+1]; 
+                else dp[i][j] = max(dp[i+1][j], dp[i][j+1]); 
+        return m + n - dp[0][0]*2; 
     }
 
 
