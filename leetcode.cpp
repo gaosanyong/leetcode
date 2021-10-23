f2c27416c3c61a523598011816a0e4a80107d2d5
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-22 23:05:58 -0400

    solve'em all (240)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4544,6 +4544,47 @@ public:
     }
 
 
+    /*240. Search a 2D Matrix II (Medium)
+	Write an efficient algorithm that searches for a target value in an m x n 
+	integer matrix. The matrix has the following properties:
+	* Integers in each row are sorted in ascending from left to right.
+	* Integers in each column are sorted in ascending from top to bottom.
+
+	Example 1:
+	Input: matrix = [[ 1 ,4, 7,11,15],
+	                 [ 2, 5, 8,12,19],
+	                 [ 3, 6, 9,16,22],
+	                 [10,13,14,17,24],
+	                 [18,21,23,26,30]], target = 5
+	Output: true
+	
+	Example 2:
+	Input: matrix = [[ 1, 4, 7,11,15],
+	                 [ 2, 5, 8,12,19],
+	                 [ 3, 6, 9,16,22],
+	                 [10,13,14,17,24],
+	                 [18,21,23,26,30]], target = 20
+	Output: false
+
+	Constraints:
+	* m == matrix.length
+	* n == matrix[i].length
+	* 1 <= n, m <= 300
+	* -10^9 <= matrix[i][j] <= 10^9
+	* All the integers in each row are sorted in ascending order.
+	* All the integers in each column are sorted in ascending order.
+	* -10^9 <= target <= 10^9*/
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size(); 
+        for (int i = 0, j = n-1; i < m && 0 <= j; ) 
+            if (matrix[i][j] < target) ++i; 
+            else if (matrix[i][j] == target) return true; 
+            else --j; 
+        return false; 
+    }
+
+
     /*242. Valid Anagram (Easy)
 	Given two strings s and t, return true if t is an anagram of s, and false 
 	otherwise.
