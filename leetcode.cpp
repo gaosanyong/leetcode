ba12d56fdddc2e7207d3fcb33772d6eb1b393bc2
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-24 20:51:32 -0400

    solve'em all (155)

--- leetcode.cpp
+++ leetcode.cpp
@@ -30916,6 +30916,59 @@ public:
 };
 
 
+/*155. Min Stack (Easy)
+Design a stack that supports push, pop, top, and retrieving the minimum element 
+in constant time. Implement the MinStack class:
+* MinStack() initializes the stack object.
+* void push(int val) pushes the element val onto the stack.
+* void pop() removes the element on the top of the stack.
+* int top() gets the top element of the stack.
+* int getMin() retrieves the minimum element in the stack.
+
+Example 1:
+Input: ["MinStack","push","push","push","getMin","pop","top","getMin"]
+       [[],[-2],[0],[-3],[],[],[],[]]
+Output: [null,null,null,null,-3,null,0,-2]
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+Constraints:
+* -2^31 <= val <= 2^31 - 1
+* Methods pop, top and getMin operations will always be called on non-empty 
+  stacks.
+* At most 3 * 104 calls will be made to push, pop, top, and getMin.*/
+
+class MinStack {
+    stack<pair<int, int>> stk; 
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        if (stk.size()) stk.emplace(val, min(val, stk.top().second)); 
+        else stk.emplace(val, val); 
+    }
+    
+    void pop() {
+        stk.pop(); 
+    }
+    
+    int top() {
+        return stk.top().first; 
+    }
+    
+    int getMin() {
+        return stk.top().second; 
+    }
+};
+
+
 /*208. Implement Trie (Prefix Tree) (Medium)
 A trie (pronounced as "try") or prefix tree is a tree data structure used to 
 efficiently store and retrieve keys in a dataset of strings. There are various 
