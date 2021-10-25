f2781f31c19bb0663094b258eec127cb721c967b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-25 12:05:45 -0400

    solve'em all (997)

--- leetcode.py
+++ leetcode.py
@@ -22373,6 +22373,53 @@ class UnionFind:
         return ans
 
 
+    """997. Find the Town Judge (Easy)
+	In a town, there are n people labeled from 1 to n. There is a rumor that 
+	one of these people is secretly the town judge. If the town judge exists, 
+	then:
+	* The town judge trusts nobody.
+	* Everybody (except for the town judge) trusts the town judge.
+	* There is exactly one person that satisfies properties 1 and 2.
+	You are given an array trust where trust[i] = [ai, bi] representing that 
+	the person labeled ai trusts the person labeled bi. Return the label of the 
+	town judge if the town judge exists and can be identified, or return -1 
+	otherwise.
+
+	Example 1:
+	Input: n = 2, trust = [[1,2]]
+	Output: 2
+
+	Example 2:
+	Input: n = 3, trust = [[1,3],[2,3]]
+	Output: 3
+
+	Example 3:
+	Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+	Output: -1
+
+	Example 4:
+	Input: n = 3, trust = [[1,2],[2,3]]
+	Output: -1
+
+	Example 5:
+	Input: n = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+	Output: 3
+
+	Constraints:
+	* 1 <= n <= 1000
+	* 0 <= trust.length <= 10^4
+	* trust[i].length == 2
+	* All the pairs of trust are unique.
+	* ai != bi"""
+
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        degree = [0]*n
+        for u, v in trust: 
+            degree[v-1] += 1
+            degree[u-1] -= 1
+        return next((i+1 for i, x in enumerate(degree) if x == n-1), -1)
+
+
     """1000. Minimum Cost to Merge Stones (Hard)
 	There are n piles of stones arranged in a row. The ith pile has stones[i] 
 	stones. A move consists of merging exactly k consecutive piles into one 
