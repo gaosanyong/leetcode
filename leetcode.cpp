93b42f4a0ecd1b6035bc1d1fd1b58729bca0efbe
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-25 14:50:34 -0400

    solve'em all (322)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5473,6 +5473,51 @@ public:
     }
 
 
+    /*322. Coin Change (Medium)
+	You are given an integer array coins representing coins of different 
+	denominations and an integer amount representing a total amount of money.
+	Return the fewest number of coins that you need to make up that amount. If 
+	that amount of money cannot be made up by any combination of the coins, 
+	return -1. You may assume that you have an infinite number of each kind of 
+	coin.
+
+	Example 1:
+	Input: coins = [1,2,5], amount = 11
+	Output: 3
+	Explanation: 11 = 5 + 5 + 1
+
+	Example 2:
+	Input: coins = [2], amount = 3
+	Output: -1
+
+	Example 3:
+	Input: coins = [1], amount = 0
+	Output: 0
+
+	Example 4:
+	Input: coins = [1], amount = 1
+	Output: 1
+
+	Example 5:
+	Input: coins = [1], amount = 2
+	Output: 2
+	 
+	Constraints:
+	* 1 <= coins.length <= 12
+	* 1 <= coins[i] <= 2^31 - 1
+	* 0 <= amount <= 10^4*/
+
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, amount+1); 
+        dp[0] = 0; 
+        for (int x = 1; x <= amount; ++x) 
+            for (auto& coin : coins) 
+                if (coin <= x) 
+                    dp[x] = min(dp[x], 1 + dp[x-coin]); 
+        return dp[amount] <= amount ? dp[amount] : -1; 
+    }
+
+
     /*329. Longest Increasing Path in a Matrix (Hard)
 	Given an m x n integers matrix, return the length of the longest increasing 
 	path in matrix. From each cell, you can either move in four directions: 
