e4cce2d4ae4f81b4af70aeb2e4d5c7ca0067b22e
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-25 15:07:54 -0400

    solve'em all (343)

--- leetcode.cpp
+++ leetcode.cpp
@@ -5735,6 +5735,32 @@ public:
     }
 
 
+    /*343. Integer Break (Medium)
+	Given an integer n, break it into the sum of k positive integers, where 
+	k >= 2, and maximize the product of those integers. Return the maximum 
+	product you can get.
+
+	Example 1:
+	Input: n = 2
+	Output: 1
+	Explanation: 2 = 1 + 1, 1 × 1 = 1.
+
+	Example 2:
+	Input: n = 10
+	Output: 36
+	Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+
+	Constraints: 2 <= n <= 58*/
+
+    int integerBreak(int n) {
+        if (n == 2) return 1; 
+        if (n == 3) return 2; 
+        if (n % 3 == 0) return pow(3, n/3); 
+        if (n % 3 == 1) return 4*pow(3, (n-4)/3); 
+        return 2*pow(3, (n-2)/3); 
+    }
+
+
     /*344. Reverse String (Easy)
 	Write a function that reverses a string. The input string is given as an 
 	array of characters s.
