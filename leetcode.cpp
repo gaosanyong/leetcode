2b3984f2f088b1aa682a962d1d7e4ff118726de7
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-25 15:34:55 -0400

    solve'em all (72)

--- leetcode.cpp
+++ leetcode.cpp
@@ -1419,6 +1419,48 @@ public:
     }
 
 
+    /*72. Edit Distance (Hard)
+	Given two strings word1 and word2, return the minimum number of operations 
+	required to convert word1 to word2. You have the following three operations 
+	permitted on a word:
+	* Insert a character
+	* Delete a character
+	* Replace a character
+
+	Example 1:
+	Input: word1 = "horse", word2 = "ros"
+	Output: 3
+	Explanation: horse -> rorse (replace 'h' with 'r')
+	             rorse -> rose (remove 'r')
+	             rose -> ros (remove 'e')
+	
+	Example 2:
+	Input: word1 = "intention", word2 = "execution"
+	Output: 5
+	Explanation: intention -> inention (remove 't')
+	             inention -> enention (replace 'i' with 'e')
+	             enention -> exention (replace 'n' with 'x')
+	             exention -> exection (replace 'n' with 'c')
+	             exection -> execution (insert 'u')
+
+	Constraints:
+	* 0 <= word1.length, word2.length <= 500
+	* word1 and word2 consist of lowercase English letters.*/
+
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size(); 
+        vector<vector<int>> dp(m+1, vector<int>(n+1)); 
+        for (int i = 0; i < m; ++i) dp[i][n] = m-i; 
+        for (int j = 0; j < n; ++j) dp[m][j] = n-j; 
+        
+        for (int i = m-1; 0 <= i; --i) 
+            for (int j = n-1; 0 <= j; --j) 
+                if (word1[i] == word2[j]) dp[i][j] = dp[i+1][j+1]; 
+                else dp[i][j] = 1 + min({dp[i+1][j], dp[i+1][j+1], dp[i][j+1]}); 
+        return dp[0][0]; 
+    }
+
+
     /*73. Set Matrix Zeroes (Medium)
 	Given an m x n integer matrix matrix, if an element is 0, set its entire 
 	row and column to 0's, and return the matrix. You must do it in place.
