996c39b7a011df2757562c3766a34362055a99bd
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-25 21:58:26 -0400

    solve'em all (518)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7553,6 +7553,48 @@ public:
     }
 
 
+    /*518. Coin Change 2 (Medium)
+	You are given an integer array coins representing coins of different 
+	denominations and an integer amount representing a total amount of money.
+	Return the number of combinations that make up that amount. If that amount 
+	of money cannot be made up by any combination of the coins, return 0. You 
+	may assume that you have an infinite number of each kind of coin. The 
+	answer is guaranteed to fit into a signed 32-bit integer.
+
+	Example 1:
+	Input: amount = 5, coins = [1,2,5]
+	Output: 4
+	Explanation: there are four ways to make up the amount:
+	             5=5
+	             5=2+2+1
+	             5=2+1+1+1
+	             5=1+1+1+1+1
+	
+	Example 2:
+	Input: amount = 3, coins = [2]
+	Output: 0
+	Explanation: the amount of 3 cannot be made up just with coins of 2.
+
+	Example 3:
+	Input: amount = 10, coins = [10]
+	Output: 1
+
+	Constraints:
+	* 1 <= coins.length <= 300
+	* 1 <= coins[i] <= 5000
+	* All the values of coins are unique.
+	* 0 <= amount <= 5000*/
+
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1, 0); 
+        dp[0] = 1; 
+        for (auto& coin : coins) 
+            for (int x = coin; x <= amount; ++x) 
+                dp[x] += dp[x-coin]; 
+        return dp[amount]; 
+    }
+
+
     /*522. Longest Uncommon Subsequence II (Medium)
 	Given an array of strings strs, return the length of the longest uncommon 
 	subsequence between them. If the longest uncommon subsequence does not 
