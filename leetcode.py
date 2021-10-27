ce1864a49444f6dca5fab5b78614cecfd9f305c5
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-26 21:58:34 -0400

    solve'em all (392)

--- leetcode.py
+++ leetcode.py
@@ -10796,6 +10796,35 @@ class UnionFind:
         return area == (X1-X0)*(Y1-Y0) and corner == {(X0, Y0), (X0, Y1), (X1, Y0), (X1, Y1)}
 
 
+    """392. Is Subsequence (Easy)
+	Given two strings s and t, return true if s is a subsequence of t, or false 
+	otherwise. A subsequence of a string is a new string that is formed from 
+	the original string by deleting some (can be none) of the characters 
+	without disturbing the relative positions of the remaining characters. (i.e., 
+	"ace" is a subsequence of "abcde" while "aec" is not).
+
+	Example 1:
+	Input: s = "abc", t = "ahbgdc"
+	Output: true
+
+	Example 2:
+	Input: s = "axc", t = "ahbgdc"
+	Output: false
+
+	Constraints:
+	* 0 <= s.length <= 100
+	* 0 <= t.length <= 10^4
+	* s and t consist only of lowercase English letters.
+
+	Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where 
+	           k >= 10^9, and you want to check one by one to see if t has its 
+	           subsequence. In this scenario, how would you change your code?"""
+
+    def isSubsequence(self, s: str, t: str) -> bool:
+        it = iter(t)
+        return all(ch in it for ch in s)
+
+
     """393. UTF-8 Validation (Medium)
 	A character in UTF8 can be from 1 to 4 bytes long, subjected to the 
 	following rules:
