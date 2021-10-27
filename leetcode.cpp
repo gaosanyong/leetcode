ac1b8f1502984471bf99b3869b6fc8d37ee616f2
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-26 22:51:47 -0400

    solve'em all (202)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4040,6 +4040,48 @@ public:
     }
 
 
+    /*202. Happy Number (Easy)
+	Write an algorithm to determine if a number n is happy. A happy number is a 
+	number defined by the following process:
+	* Starting with any positive integer, replace the number by the sum of the 
+	  squares of its digits.
+	* Repeat the process until the number equals 1 (where it will stay), or it 
+	  loops endlessly in a cycle which does not include 1.
+	* Those numbers for which this process ends in 1 are happy.
+	Return true if n is a happy number, and false if not.
+
+	Example 1:
+	Input: n = 19
+	Output: true
+	Explanation: 12 + 92 = 82
+	             82 + 22 = 68
+	             62 + 82 = 100
+	             12 + 02 + 02 = 1
+	
+	Example 2:
+	Input: n = 2
+	Output: false
+
+	Constraints: 1 <= n <= 2^31 - 1*/
+
+    bool isHappy(int n) {
+        
+        auto fn = [](int n) {
+            int ans = 0; 
+            for (; n; n /= 10)
+                ans += pow(n % 10, 2); 
+            return ans; 
+        }; 
+        
+        for (int fast = n, slow = n; fast != 1; ) {
+            fast = fn(fn(fast)); 
+            slow = fn(slow); 
+            if (fast == slow && fast != 1) return false; 
+        }
+        return true; 
+    }
+
+
     /*203. Remove Linked List Elements (Easy)
 	Given the head of a linked list and an integer val, remove all the nodes of 
 	the linked list that has Node.val == val, and return the new head.
