749d08ff46681ca79db27961eb862727a140e4db
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-27 12:22:18 -0400

    Combination Sum I,II,III,IV (39|40|216|377)

--- leetcode.cpp
+++ leetcode.cpp
@@ -889,6 +889,98 @@ public:
     }
 
 
+    /*39. Combination Sum (Medium)
+	Given an array of distinct integers candidates and a target integer target, 
+	return a list of all unique combinations of candidates where the chosen 
+	numbers sum to target. You may return the combinations in any order. The 
+	same number may be chosen from candidates an unlimited number of times. Two 
+	combinations are unique if the frequency of at least one of the chosen 
+	numbers is different. It is guaranteed that the number of unique 
+	combinations that sum up to target is less than 150 combinations for the 
+	given input.
+
+	Example 1:
+	Input: candidates = [2,3,6,7], target = 7
+	Output: [[2,2,3],[7]]
+	Explanation: 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be 
+	             used multiple times. 7 is a candidate, and 7 = 7. These are 
+	             the only two combinations.
+	
+	Example 2:
+	Input: candidates = [2,3,5], target = 8
+	Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+	Example 3:
+	Input: candidates = [2], target = 1
+	Output: []
+
+	Example 4:
+	Input: candidates = [1], target = 1
+	Output: [[1]]
+
+	Example 5:
+	Input: candidates = [1], target = 2
+	Output: [[1,1]]
+
+	Constraints:
+	* 1 <= candidates.length <= 30
+	* 1 <= candidates[i] <= 200
+	* All elements of candidates are distinct.
+	* 1 <= target <= 500*/
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<vector<int>>> dp(target+1); 
+        dp[0].push_back({}); 
+        for (auto& x : candidates) 
+            for (int i = 0; i < target; ++i) 
+                if (i + x <= target) 
+                    for (auto seq : dp[i]) {
+                        seq.push_back(x); 
+                        dp[i+x].push_back(seq); 
+                    }
+        return dp[target]; 
+    }
+
+
+    /*40. Combination Sum II (Medium)
+	Given a collection of candidate numbers (candidates) and a target number 
+	(target), find all unique combinations in candidates where the candidate 
+	numbers sum to target. Each number in candidates may only be used once in 
+	the combination. Note: The solution set must not contain duplicate 
+	combinations.
+
+	Example 1:
+	Input: candidates = [10,1,2,7,6,1,5], target = 8
+	Output: [[1,1,6],
+	         [1,2,5],
+	         [1,7],
+	         [2,6]]
+	
+	Example 2:
+	Input: candidates = [2,5,2,1,2], target = 5
+	Output: [[1,2,2],
+	         [5]]
+
+	Constraints:
+	* 1 <= candidates.length <= 100
+	* 1 <= candidates[i] <= 50
+	* 1 <= target <= 30*/
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); 
+        vector<set<vector<int>>> dp(target+1); 
+        dp[0].insert(vector<int>{}); 
+        for (auto& x : candidates) 
+            for (int i = target-1; i >= 0; --i) 
+                if (i + x <= target) 
+                    for (auto seq : dp[i]) {
+                        seq.push_back(x); 
+                        dp[i+x].insert(seq); 
+                    }
+        return vector<vector<int>>(dp[target].begin(), dp[target].end()); 
+    }
+
+
     /*42. Trapping Rain Water (Hard)
 	Given n non-negative integers representing an elevation map where the width 
 	of each bar is 1, compute how much water it can trap after raining.
@@ -4383,6 +4475,65 @@ public:
     }
 
 
+    /*216. Combination Sum III (Medium)
+	Find all valid combinations of k numbers that sum up to n such that the 
+	following conditions are true:
+	* Only numbers 1 through 9 are used.
+	* Each number is used at most once.
+	Return a list of all possible valid combinations. The list must not contain 
+	the same combination twice, and the combinations may be returned in any 
+	order.
+
+	Example 1:
+	Input: k = 3, n = 7
+	Output: [[1,2,4]]
+	Explanation: 1 + 2 + 4 = 7
+	             There are no other valid combinations.
+	
+	Example 2:
+	Input: k = 3, n = 9
+	Output: [[1,2,6],[1,3,5],[2,3,4]]
+	Explanation: 1 + 2 + 6 = 9
+	             1 + 3 + 5 = 9
+	             2 + 3 + 4 = 9
+	             There are no other valid combinations.
+	
+	Example 3:
+	Input: k = 4, n = 1
+	Output: []
+	Explanation: There are no valid combinations. Using 4 different numbers in 
+	             the range [1,9], the smallest sum we can get is 1+2+3+4 = 10 
+	             and since 10 > 1, there are no valid combination.
+	
+	Example 4:
+	Input: k = 3, n = 2
+	Output: []
+	Explanation: There are no valid combinations.
+
+	Example 5:
+	Input: k = 9, n = 45
+	Output: [[1,2,3,4,5,6,7,8,9]]
+	Explanation: 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = 45
+	             There are no other valid combinations.
+
+	Constraints:
+	* 2 <= k <= 9
+	* 1 <= n <= 60*/
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans; 
+        vector<int> stk; 
+        for (int x = 1; true; ) {
+            if (stk.size() == k && accumulate(stk.begin(), stk.end(), 0) == n) ans.push_back(stk); 
+            if (stk.size() == k || k - stk.size() > 9 - x + 1) {
+                if (stk.empty()) break; 
+                x = 1 + stk.back(); stk.pop_back(); 
+            } else stk.push_back(x++); 
+        }
+        return ans; 
+    }
+
+
     /*217. Contains Duplicate (Easy)
 	Given an integer array nums, return true if any value appears at least 
 	twice in the array, and return false if every element is distinct.
@@ -6169,7 +6320,7 @@ public:
 	           the question to allow negative numbers?*/
 
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned> dp(target+1, 0); 
+        vector<unsigned> dp(target+1); 
         dp[0] = 1; 
         for (int i = 0; i < target; ++i) 
             if (dp[i]) 
