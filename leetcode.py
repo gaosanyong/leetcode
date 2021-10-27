0f0b865e9e2c451fa14b9d4508a489d6fc411cfd
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-26 21:50:35 -0400

    solve'em all (973)

--- leetcode.py
+++ leetcode.py
@@ -21907,6 +21907,58 @@ class UnionFind:
         return ans 
 
 
+    """973. K Closest Points to Origin (Medium)
+	Given an array of points where points[i] = [xi, yi] represents a point on 
+	the X-Y plane and an integer k, return the k closest points to the origin 
+	(0, 0). The distance between two points on the X-Y plane is the Euclidean 
+	distance (i.e., √(x1 - x2)^2 + (y1 - y2)^2). You may return the answer in 
+	any order. The answer is guaranteed to be unique (except for the order that 
+	it is in).
+
+	Example 1:
+	Input: points = [[1,3],[-2,2]], k = 1
+	Output: [[-2,2]]
+	Explanation: The distance between (1, 3) and the origin is sqrt(10). The 
+	             distance between (-2, 2) and the origin is sqrt(8). Since 
+	             sqrt(8) < sqrt(10), (-2, 2) is closer to the origin. We only 
+	             want the closest k = 1 points from the origin, so the answer 
+	             is just [[-2,2]].
+	
+	Example 2:
+	Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+	Output: [[3,3],[-2,4]]
+	Explanation: The answer [[-2,4],[3,3]] would also be accepted.
+
+	Constraints:
+	* 1 <= k <= points.length <= 10^4
+	* -10^4 < xi, yi < 10^4"""
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        def partition(lo, hi): 
+            """Return partition of dist[lo:hi]."""
+            i, j = lo+1, hi-1
+            while i <= j: 
+                if dist[i] < dist[lo]: i += 1
+                elif dist[j] > dist[lo]: j -= 1
+                else: 
+                    dist[i], dist[j] = dist[j], dist[i]
+                    i += 1
+                    j -= 1
+            dist[lo], dist[j] = dist[j], dist[lo]
+            return j 
+        
+        dist = [x*x+y*y for x, y in points]
+        shuffle(dist)
+        lo, hi = 0, len(dist)
+        while lo < hi: 
+            mid = partition(lo, hi)
+            if mid + 1 < k: lo = mid + 1
+            elif mid + 1 == k: break 
+            else: hi = mid
+        return [[x, y] for x, y in points if x*x + y*y <= dist[mid]]
+
+
     """975. Odd Even Jump (Hard)
 	You are given an integer array arr. From some starting index, you can make 
 	a series of jumps. The (1st, 3rd, 5th, ...) jumps in the series are called 
