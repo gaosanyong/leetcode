7ad7c7493861b5eee5fb4f6b96c28a1451332c70
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-28 12:11:03 -0400

    solve'em all (297)

--- leetcode.py
+++ leetcode.py
@@ -50161,6 +50161,69 @@ class Codec:
         return ans 
 
 
+"""297. Serialize and Deserialize Binary Tree (Hard)
+Serialization is the process of converting a data structure or object into a 
+sequence of bits so that it can be stored in a file or memory buffer, or 
+transmitted across a network connection link to be reconstructed later in the 
+same or another computer environment. Design an algorithm to serialize and 
+deserialize a binary tree. There is no restriction on how your 
+serialization/deserialization algorithm should work. You just need to ensure 
+that a binary tree can be serialized to a string and this string can be 
+deserialized to the original tree structure. Clarification: The input/output 
+format is the same as how LeetCode serializes a binary tree. You do not 
+necessarily need to follow this format, so please be creative and come up with 
+different approaches yourself.
+
+Example 1:
+Input: root = [1,2,3,null,null,4,5]
+Output: [1,2,3,null,null,4,5]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+
+Example 4:
+Input: root = [1,2]
+Output: [1,2]
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 10^4].
+* -1000 <= Node.val <= 1000"""
+
+class Codec:
+
+    def serialize(self, root):
+        ans = []
+        queue = [root]
+        for node in queue: 
+            if node: ans.append(node.val)
+            else: ans.append('$')
+            if node: 
+                queue.append(node.left)
+                queue.append(node.right)
+        return ",".join(map(str, ans))
+
+    def deserialize(self, data):
+        root = parent = None 
+        queue = deque()
+        for x in data.split(','): 
+            if x == '$': node = None
+            else: node = TreeNode(int(x))
+            if not root: root = node 
+            elif parent: 
+                parent.right = node 
+                parent = None 
+            else: 
+                parent = queue.popleft()
+                parent.left = node 
+            if node: queue.append(node)
+        return root 
+
+
 """281. Zigzag Iterator (Medium)
 Given two 1d vectors, implement an iterator to return their elements alternately.
 
