f4bd538ec37ed444ceb797764a6d1b2011a6943b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-28 15:34:05 -0400

    solve'em all (891)

--- leetcode.cpp
+++ leetcode.cpp
@@ -14850,6 +14850,43 @@ public:
     }
 
 
+    /*891. Sum of Subsequence Widths (Hard)
+	The width of a sequence is the difference between the maximum and minimum 
+	elements in the sequence. Given an array of integers nums, return the sum 
+	of the widths of all the non-empty subsequences of nums. Since the answer 
+	may be very large, return it modulo 10^9 + 7. A subsequence is a sequence 
+	that can be derived from an array by deleting some or no elements without 
+	changing the order of the remaining elements. For example, [3,6,2,7] is a 
+	subsequence of the array [0,3,1,6,2,2,7].
+
+	Example 1:
+	Input: nums = [2,1,3]
+	Output: 6
+	Explanation: The subsequences are [1], [2], [3], [2,1], [2,3], [1,3], 
+	             [2,1,3]. The corresponding widths are 0, 0, 0, 1, 1, 2, 2.
+	             The sum of these widths is 6.
+	
+	Example 2:
+	Input: nums = [2]
+	Output: 0
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* 1 <= nums[i] <= 10^5*/
+
+    int sumSubseqWidths(vector<int>& nums) {
+        int n = nums.size(), MOD = 1'000'000'007; 
+        vector<long> p(n, 1); 
+        for (int i = 1; i < n; ++i) p[i] = 2*p[i-1]%MOD; 
+        
+        long ans = 0; 
+        sort(nums.begin(), nums.end()); 
+        for (int i = 0; i < n; ++i) 
+            ans = (ans + nums[i] * (p[i] - p[n-1-i])) % MOD; 
+        return ans; 
+    }
+
+
     /*892. Surface Area of 3D Shapes (Easy)
 	You are given an n x n grid where you have placed some 1 x 1 x 1 cubes. 
 	Each value v = grid[i][j] represents a tower of v cubes placed on top of 
--- leetcode.py
+++ leetcode.py
@@ -20389,6 +20389,38 @@ class UnionFind:
         return [word for word in words if len(set(zip(word, pattern))) == len(set(word)) == len(set(pattern))]
 
 
+    """891. Sum of Subsequence Widths (Hard)
+	The width of a sequence is the difference between the maximum and minimum 
+	elements in the sequence. Given an array of integers nums, return the sum 
+	of the widths of all the non-empty subsequences of nums. Since the answer 
+	may be very large, return it modulo 10^9 + 7. A subsequence is a sequence 
+	that can be derived from an array by deleting some or no elements without 
+	changing the order of the remaining elements. For example, [3,6,2,7] is a 
+	subsequence of the array [0,3,1,6,2,2,7].
+
+	Example 1:
+	Input: nums = [2,1,3]
+	Output: 6
+	Explanation: The subsequences are [1], [2], [3], [2,1], [2,3], [1,3], 
+	             [2,1,3]. The corresponding widths are 0, 0, 0, 1, 1, 2, 2.
+	             The sum of these widths is 6.
+	
+	Example 2:
+	Input: nums = [2]
+	Output: 0
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* 1 <= nums[i] <= 10^5"""
+
+    def sumSubseqWidths(self, nums: List[int]) -> int:
+        MOD = 1_000_000_007
+        ans = 0 
+        for i, x in enumerate(sorted(nums)): 
+            ans += x * (pow(2, i, MOD) - pow(2, len(nums)-i-1, MOD))
+        return ans % MOD
+        
+
     """894. All Possible Full Binary Trees (Medium)
 	A full binary tree is a binary tree where each node has exactly 0 or 2 
 	children. Return a list of all possible full binary trees with N nodes. 
