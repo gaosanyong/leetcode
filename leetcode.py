23e5d61144758485afb2afcfb90caf3addfe04f4
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-29 18:02:35 -0400

    reposition 952

--- leetcode.py
+++ leetcode.py
@@ -21131,69 +21131,6 @@ class UnionFind:
         return ans 
 
 
-    """952. Largest Component Size by Common Factor (Hard)
-	You are given an integer array of unique positive integers nums. Consider 
-	the following graph:
-	* There are nums.length nodes, labeled nums[0] to nums[nums.length - 1],
-	* There is an undirected edge between nums[i] and nums[j] if nums[i] and 
-	  nums[j] share a common factor greater than 1.
-	Return the size of the largest connected component in the graph.
-
-	Example 1:
-	Input: nums = [4,6,15,35]
-	Output: 4
-
-	Example 2:
-	Input: nums = [20,50,9,63]
-	Output: 2
-
-	Example 3:
-	Input: nums = [2,3,6,7,4,12,21,39]
-	Output: 8
-
-	Constraints:
-	* 1 <= nums.length <= 2 * 10^4
-	* 1 <= nums[i] <= 10^5
-	* All the values of nums are unique.
-
-class UnionFind: 
-    
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.rank = [1]*n
-        
-    def find(self, p): 
-        if p != self.parent[p]: 
-            self.parent[p] = self.find(self.parent[p])
-        return self.parent[p]
-    
-    def union(self, p, q): 
-        prt, qrt = self.find(p), self.find(q)
-        if prt == qrt: return False 
-        if self.rank[prt] > self.rank[qrt]: prt, qrt = qrt, prt
-        self.parent[prt] = qrt
-        self.rank[qrt] += self.rank[prt]
-        return True"""
-
-    def largestComponentSize(self, A: List[int]) -> int:
-        m = max(A)
-        uf = UnionFind(m+1)
-        seen = set(A)
-        # modified sieve of eratosthenes 
-        sieve = [1]*(m+1)
-        sieve[0] = sieve[1] = 0 
-        for k in range(m//2+1): 
-            if sieve[k]: 
-                ref = k if k in seen else 0
-                for x in range(2*k, m+1, k): 
-                    sieve[x] = 0
-                    if x in seen: 
-                        if ref: uf.union(ref, x)
-                        ref = x
-        freq = Counter(uf.find(i) for i in range(m+1))
-        return max(freq.values())
-
-
     """926. Flip String to Monotone Increasing (Medium)
 	A string of '0's and '1's is monotone increasing if it consists of some 
 	number of '0's (possibly 0), followed by some number of '1's (also possibly 
@@ -21821,6 +21758,69 @@ class UnionFind:
             return n1.val == n2.val and (fn(n1.left, n2.right) and fn(n1.right, n2.left) or fn(n1.left, n2.left) and fn(n1.right, n2.right))
         
         return fn(root1, root2)
+
+
+    """952. Largest Component Size by Common Factor (Hard)
+	You are given an integer array of unique positive integers nums. Consider 
+	the following graph:
+	* There are nums.length nodes, labeled nums[0] to nums[nums.length - 1],
+	* There is an undirected edge between nums[i] and nums[j] if nums[i] and 
+	  nums[j] share a common factor greater than 1.
+	Return the size of the largest connected component in the graph.
+
+	Example 1:
+	Input: nums = [4,6,15,35]
+	Output: 4
+
+	Example 2:
+	Input: nums = [20,50,9,63]
+	Output: 2
+
+	Example 3:
+	Input: nums = [2,3,6,7,4,12,21,39]
+	Output: 8
+
+	Constraints:
+	* 1 <= nums.length <= 2 * 10^4
+	* 1 <= nums[i] <= 10^5
+	* All the values of nums are unique.
+
+class UnionFind: 
+    
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1]*n
+        
+    def find(self, p): 
+        if p != self.parent[p]: 
+            self.parent[p] = self.find(self.parent[p])
+        return self.parent[p]
+    
+    def union(self, p, q): 
+        prt, qrt = self.find(p), self.find(q)
+        if prt == qrt: return False 
+        if self.rank[prt] > self.rank[qrt]: prt, qrt = qrt, prt
+        self.parent[prt] = qrt
+        self.rank[qrt] += self.rank[prt]
+        return True"""
+
+    def largestComponentSize(self, A: List[int]) -> int:
+        m = max(A)
+        uf = UnionFind(m+1)
+        seen = set(A)
+        # modified sieve of eratosthenes 
+        sieve = [1]*(m+1)
+        sieve[0] = sieve[1] = 0 
+        for k in range(m//2+1): 
+            if sieve[k]: 
+                ref = k if k in seen else 0
+                for x in range(2*k, m+1, k): 
+                    sieve[x] = 0
+                    if x in seen: 
+                        if ref: uf.union(ref, x)
+                        ref = x
+        freq = Counter(uf.find(i) for i in range(m+1))
+        return max(freq.values())
 
 
     """954. Array of Doubled Pairs (Medium)
