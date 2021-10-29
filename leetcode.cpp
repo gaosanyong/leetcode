d1e943c1b33f21f6df419e60f21da49b075979c0
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-28 23:02:10 -0400

    solve'em all (142)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3347,6 +3347,52 @@ public:
     }
 
 
+    /*142. Linked List Cycle II (Medium)
+	Given the head of a linked list, return the node where the cycle begins. If 
+	there is no cycle, return null. There is a cycle in a linked list if there 
+	is some node in the list that can be reached again by continuously 
+	following the next pointer. Internally, pos is used to denote the index of 
+	the node that tail's next pointer is connected to (0-indexed). It is -1 if 
+	there is no cycle. Note that pos is not passed as a parameter. Do not 
+	modify the linked list.
+
+	Example 1:
+	Input: head = [3,2,0,-4], pos = 1
+	Output: tail connects to node index 1
+	Explanation: There is a cycle in the linked list, where tail connects to 
+	             the second node.
+	
+	Example 2:
+	Input: head = [1,2], pos = 0
+	Output: tail connects to node index 0
+	Explanation: There is a cycle in the linked list, where tail connects to 
+	             the first node.
+	
+	Example 3:
+	Input: head = [1], pos = -1
+	Output: no cycle
+	Explanation: There is no cycle in the linked list.
+
+	Constraints:
+	* The number of the nodes in the list is in the range [0, 10^4].
+	* -10^5 <= Node.val <= 10^5
+	* pos is -1 or a valid index in the linked-list.
+
+	Follow up: Can you solve it using O(1) (i.e. constant) memory?*/
+
+    ListNode *detectCycle(ListNode *head) {
+        for (ListNode *fast = head, *slow = head; fast && fast->next; ) {
+            fast = fast->next->next; 
+            slow = slow->next; 
+            if (fast == slow) {
+                for (fast = head; fast != slow; fast = fast->next, slow = slow->next); 
+                return slow; 
+            }
+        }
+        return nullptr; 
+    }
+
+
     /*144. Binary Tree Preorder Traversal (Medium)
 	Given the root of a binary tree, return the preorder traversal of its 
 	nodes' values.
