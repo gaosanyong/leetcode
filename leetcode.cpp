15ac934feef83f67d354540a6e6a49bb75787ab2
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-30 22:17:02 -0400

    October LeetCoding Challenge 2021-10-31 (430)

--- leetcode.cpp
+++ leetcode.cpp
@@ -6859,6 +6859,71 @@ public:
     }
 
 
+    /*430. Flatten a Multilevel Doubly Linked List (Medium)
+	You are given a doubly linked list which in addition to the next and 
+	previous pointers, it could have a child pointer, which may or may not 
+	point to a separate doubly linked list. These child lists may have one or 
+	more children of their own, and so on, to produce a multilevel data 
+	structure, as shown in the example below. Flatten the list so that all the 
+	nodes appear in a single-level, doubly linked list. You are given the head 
+	of the first level of the list.
+
+	Example 1:
+	Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+	Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+
+	Example 2:
+	Input: head = [1,2,null,3]
+	Output: [1,3,2]
+	Explanation: The input multilevel linked list is as follows:
+	             1---2---NULL
+	             |
+	             3---NULL
+	
+	Example 3:
+	Input: head = []
+	Output: []
+
+	How multilevel linked list is represented in test case:
+	We use the multilevel linked list from Example 1 above:
+	 1---2---3---4---5---6--NULL
+	         |
+	         7---8---9---10--NULL
+	             |
+	             11--12--NULL
+	The serialization of each level is as follows:
+	[1,2,3,4,5,6,null]
+	[7,8,9,10,null]
+	[11,12,null]
+	To serialize all levels together we will add nulls in each level to signify 
+	no node connects to the upper node of the previous level. The serialization 
+	becomes:
+	[1,2,3,4,5,6,null]
+	[null,null,7,8,9,10,null]
+	[null,11,12,null]
+	Merging the serialization of each level and removing trailing nulls we obtain:
+	[1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+
+	Constraints:
+	* The number of Nodes will not exceed 1000.
+	* 1 <= Node.val <= 10^5*/
+
+    Node* flatten(Node* head) {
+        stack<Node*> stk; 
+        for (Node* node = head; node; node = node->next) 
+            if (node->child) {
+                if (node->next) stk.push(node->next); 
+                node->next = node->child; 
+                node->next->prev = node; 
+                node->child = nullptr; 
+            } else if (!node->next && stk.size()) {
+                node->next = stk.top(); stk.pop(); 
+                node->next->prev = node; 
+            }
+        return head; 
+    }
+
+
     /*435. Non-overlapping Intervals (Medium)
 	Given an array of intervals intervals where intervals[i] = [starti, endi], 
 	return the minimum number of intervals you need to remove to make the rest 
--- leetcode.py
+++ leetcode.py
@@ -11517,6 +11517,71 @@ class UnionFind:
         return ans 
 
 
+    """430. Flatten a Multilevel Doubly Linked List (Medium)
+	You are given a doubly linked list which in addition to the next and 
+	previous pointers, it could have a child pointer, which may or may not 
+	point to a separate doubly linked list. These child lists may have one or 
+	more children of their own, and so on, to produce a multilevel data 
+	structure, as shown in the example below. Flatten the list so that all the 
+	nodes appear in a single-level, doubly linked list. You are given the head 
+	of the first level of the list.
+
+	Example 1:
+	Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+	Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+
+	Example 2:
+	Input: head = [1,2,null,3]
+	Output: [1,3,2]
+	Explanation: The input multilevel linked list is as follows:
+	             1---2---NULL
+	             |
+	             3---NULL
+	
+	Example 3:
+	Input: head = []
+	Output: []
+
+	How multilevel linked list is represented in test case:
+	We use the multilevel linked list from Example 1 above:
+	 1---2---3---4---5---6--NULL
+	         |
+	         7---8---9---10--NULL
+	             |
+	             11--12--NULL
+	The serialization of each level is as follows:
+	[1,2,3,4,5,6,null]
+	[7,8,9,10,null]
+	[11,12,null]
+	To serialize all levels together we will add nulls in each level to signify 
+	no node connects to the upper node of the previous level. The serialization 
+	becomes:
+	[1,2,3,4,5,6,null]
+	[null,null,7,8,9,10,null]
+	[null,11,12,null]
+	Merging the serialization of each level and removing trailing nulls we obtain:
+	[1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+
+	Constraints:
+	* The number of Nodes will not exceed 1000.
+	* 1 <= Node.val <= 10^5"""
+
+    def flatten(self, head: 'Node') -> 'Node':
+        node = head
+        stack = []
+        while node: 
+            if node.child: 
+                if node.next: stack.append(node.next)
+                node.next = node.child
+                node.next.prev = node
+                node.child = None
+            elif not node.next and stack: 
+                node.next = stack.pop()
+                node.next.prev = node 
+            node = node.next 
+        return head 
+
+
     """435. Non-overlapping Intervals (Medium)
 	Given an array of intervals intervals where intervals[i] = [starti, endi], 
 	return the minimum number of intervals you need to remove to make the rest 
@@ -23678,6 +23743,48 @@ class UnionFind:
             return ans 
         
         return fn(len(arr))
+
+
+    """1044. Longest Duplicate Substring (Hard)
+	Given a string s, consider all duplicated substrings: (contiguous) 
+	substrings of s that occur 2 or more times. The occurrences may overlap.
+	Return any duplicated substring that has the longest possible length. If s 
+	does not have a duplicated substring, the answer is "".
+
+	Example 1:
+	Input: s = "banana"
+	Output: "ana"
+
+	Example 2:
+	Input: s = "abcd"
+	Output: ""
+
+	Constraints:
+	* 2 <= s.length <= 3 * 10^4
+	* s consists of lowercase English letters."""
+
+    def longestDupSubstring(self, s: str) -> str:
+        mod = 1_000_000_007
+        
+        def fn(k): 
+            """Return duplicated substring of length k."""
+            p = pow(26, k, mod)
+            hs = 0
+            seen = {}
+            for i, ch in enumerate(s): 
+                hs = (26*hs + ord(ch) - 97) % mod
+                if i >= k: hs = (hs - (ord(s[i-k])-97)*p) % mod # rolling hash 
+                if i+1 >= k:
+                    if hs in seen and s[i+1-k:i+1] in seen[hs]: return s[i+1-k:i+1] # resolve hash collision
+                    seen.setdefault(hs, set()).add(s[i+1-k:i+1])
+            return ""
+        
+        lo, hi = 0, len(s)-1
+        while lo < hi: 
+            mid = lo + hi + 1 >> 1
+            if fn(mid): lo = mid
+            else: hi = mid - 1
+        return fn(lo)
 
 
     """1047. Remove All Adjacent Duplicates In String (Easy)
