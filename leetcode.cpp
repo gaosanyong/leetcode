f1df821c4f9e7c7bfbfa0796318c1d9d24d1e40c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-30 22:24:25 -0400

    solve'em all (24)

--- leetcode.cpp
+++ leetcode.cpp
@@ -652,6 +652,40 @@ public:
     }
 
 
+    /*24. Swap Nodes in Pairs (Medium)
+	Given a linked list, swap every two adjacent nodes and return its head. You 
+	must solve the problem without modifying the values in the list's nodes 
+	(i.e., only nodes themselves may be changed.)
+
+	Example 1:
+	Input: head = [1,2,3,4]
+	Output: [2,1,4,3]
+
+	Example 2:
+	Input: head = []
+	Output: []
+
+	Example 3:
+	Input: head = [1]
+	Output: [1]
+
+	Constraints:
+	* The number of nodes in the list is in the range [0, 100].
+	* 0 <= Node.val <= 100*/
+
+    ListNode* swapPairs(ListNode* head) {
+        ListNode dummy(0, head), *node = &dummy; 
+        while (node->next && node->next->next) {
+            ListNode *temp = node->next->next; 
+            node->next->next = temp->next; 
+            temp->next = node->next; 
+            node->next = temp; 
+            node = node->next->next; 
+        }
+        return dummy.next; 
+    }
+
+
     /*26. Remove Duplicates from Sorted Array (Easy)
 	Given a sorted array nums, remove the duplicates in-place such that each 
 	element appears only once and returns the new length. Do not allocate extra 
