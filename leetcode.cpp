7d1ef9434e60ff8953c397cefe0730aee0b50aa3
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-31 23:04:12 -0400

    solve'em all (25)

--- leetcode.cpp
+++ leetcode.cpp
@@ -686,6 +686,58 @@ public:
     }
 
 
+    /*25. Reverse Nodes in k-Group (Hard)
+	Given a linked list, reverse the nodes of a linked list k at a time and 
+	return its modified list. k is a positive integer and is less than or equal 
+	to the length of the linked list. If the number of nodes is not a multiple 
+	of k then left-out nodes, in the end, should remain as it is. You may not 
+	alter the values in the list's nodes, only nodes themselves may be changed.
+
+	Example 1:
+	Input: head = [1,2,3,4,5], k = 2
+	Output: [2,1,4,3,5]
+
+	Example 2:
+	Input: head = [1,2,3,4,5], k = 3
+	Output: [3,2,1,4,5]
+
+	Example 3:
+	Input: head = [1,2,3,4,5], k = 1
+	Output: [1,2,3,4,5]
+
+	Example 4:
+	Input: head = [1], k = 1
+	Output: [1]
+
+	Constraints:
+	* The number of nodes in the list is in the range sz.
+	* 1 <= sz <= 5000
+	* 0 <= Node.val <= 1000
+	* 1 <= k <= sz
+
+	Follow-up: Can you solve the problem in O(1) extra memory space?*/
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        int sz = 0; 
+        for (ListNode* node = head; node; ++sz, node = node->next); 
+        ListNode dummy(0, head), *tail = &dummy, *node = head; 
+        for (sz /= k; sz; --sz) {
+            ListNode *prev = nullptr; 
+            for (int kk = k; kk; --kk) {
+                ListNode *temp = node->next; 
+                node->next = prev; 
+                prev = node; 
+                node = temp; 
+            }
+            tail->next = prev; 
+            head->next = node; 
+            tail = head; 
+            head = node; 
+        }
+        return dummy.next; 
+    }
+
+
     /*26. Remove Duplicates from Sorted Array (Easy)
 	Given a sorted array nums, remove the duplicates in-place such that each 
 	element appears only once and returns the new length. Do not allocate extra 
