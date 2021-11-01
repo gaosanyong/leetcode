92ca4a612aaed435cc7642e3d1dfc175155ebd6f
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-10-31 22:44:41 -0400

    solve'em all (143)

--- leetcode.cpp
+++ leetcode.cpp
@@ -3470,6 +3470,51 @@ public:
     }
 
 
+    /*143. Reorder List (Medium)
+	You are given the head of a singly linked-list. The list can be represented 
+	as:
+	L0 → L1 → … → Ln - 1 → Ln
+	Reorder the list to be on the following form:
+	L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+	You may not modify the values in the list's nodes. Only nodes themselves 
+	may be changed.
+
+	Example 1:
+	Input: head = [1,2,3,4]
+	Output: [1,4,2,3]
+
+	Example 2:
+	Input: head = [1,2,3,4,5]
+	Output: [1,5,2,4,3]
+
+	Constraints:
+	* The number of nodes in the list is in the range [1, 5 * 10^4].
+	* 1 <= Node.val <= 1000*/
+
+    void reorderList(ListNode* head) {
+        ListNode *fast = head, *slow = head; 
+        while (fast && fast->next) {
+            fast = fast->next->next; 
+            slow = slow->next; 
+        }
+        ListNode *prev = nullptr; 
+        while (slow) {
+            ListNode* temp = slow->next; 
+            slow->next = prev; 
+            prev = slow; 
+            slow = temp; 
+        }
+        ListNode *node = head; 
+        while (prev && prev->next) {
+            ListNode *temp = prev->next; 
+            prev->next = node->next; 
+            node->next = prev; 
+            prev = temp; 
+            node = node->next->next; 
+        }
+    }
+
+
     /*144. Binary Tree Preorder Traversal (Medium)
 	Given the root of a binary tree, return the preorder traversal of its 
 	nodes' values.
