72f60d447961841cc69265512214d114f844cde2
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-02 00:29:13 -0400

    November LeetCoding Challenge 2021-11-02 (980)

--- leetcode.cpp
+++ leetcode.cpp
@@ -16961,6 +16961,74 @@ public:
     }
 
 
+    /*980. Unique Paths III (Hard)
+	You are given an m x n integer array grid where grid[i][j] could be:
+	* 1 representing the starting square. There is exactly one starting square.
+	* 2 representing the ending square. There is exactly one ending square.
+	* 0 representing empty squares we can walk over.
+	* -1 representing obstacles that we cannot walk over.
+	Return the number of 4-directional walks from the starting square to the 
+	ending square, that walk over every non-obstacle square exactly once.
+
+	Example 1:
+	Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+	Output: 2
+	Explanation: We have the following two paths: 
+	             1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+	             2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+	
+	Example 2:
+	Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+	Output: 4
+	Explanation: We have the following four paths: 
+	             1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+	             2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+	             3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+	             4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+	
+	Example 3:
+	Input: grid = [[0,1],[2,0]]
+	Output: 0
+	Explanation: There is no path that walks over every empty square exactly 
+	             once. Note that the starting and ending square can be anywhere 
+	             in the grid.
+
+	Constraints:
+	* m == grid.length
+	* n == grid[i].length
+	* 1 <= m, n <= 20
+	* 1 <= m * n <= 20
+	* -1 <= grid[i][j] <= 2
+	* There is exactly one starting cell and one ending cell.*/
+
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(), si = 0, sj = 0, empty = 0;
+        for (int i = 0; i < m; ++i) 
+            for (int j = 0; j < n; ++j) 
+                if (grid[i][j] == 1) si = i, sj = j; 
+                else if (grid[i][j] == 0) ++empty; 
+        
+        int ans = 0, dir[5] = {-1, 0, 1, 0, -1}; 
+        
+        function<void(int, int, int)> fn = [&](int i, int j, int empty) {
+            if (grid[i][j] == 2) {
+                if (empty == -1) ++ans; 
+            } else {
+                grid[i][j] = -1; 
+                for (int k = 0; k < 4; ++k) {
+                    int ii = i+dir[k], jj = j+dir[k+1]; 
+                    if (0 <= ii && ii < m && 0 <= jj && jj < n && grid[ii][jj] != -1) 
+                        fn(ii, jj, empty-1); 
+                }
+                grid[i][j] = 0; 
+            }
+        }; 
+        
+        fn(si, sj, empty); 
+        return ans; 
+    }
+
+
     /*982. Triples with Bitwise AND Equal To Zero (Hard)
 	Given an array of integers nums, find the number of triples of indices 
 	(i, j, k) such that:
