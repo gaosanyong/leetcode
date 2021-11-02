107910d71f75589c70f4b63e51bd09f53f79c886
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-01 22:50:01 -0400

    solve'em all (1249)

--- leetcode.cpp
+++ leetcode.cpp
@@ -19689,6 +19689,59 @@ public:
     }
 
 
+    /*1249. Minimum Remove to Make Valid Parentheses (Medium)
+	Given a string s of '(' , ')' and lowercase English characters. Your task 
+	is to remove the minimum number of parentheses ( '(' or ')', in any 
+	positions ) so that the resulting parentheses string is valid and return 
+	any valid string. Formally, a parentheses string is valid if and only if:
+	* It is the empty string, contains only lowercase characters, or
+	* It can be written as AB (A concatenated with B), where A and B are valid 
+	  strings, or
+	* It can be written as (A), where A is a valid string.
+
+	Example 1:
+	Input: s = "lee(t(c)o)de)"
+	Output: "lee(t(c)o)de"
+	Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+	Example 2:
+	Input: s = "a)b(c)d"
+	Output: "ab(c)d"
+
+	Example 3:
+	Input: s = "))(("
+	Output: ""
+	Explanation: An empty string is also valid.
+
+	Example 4:
+	Input: s = "(a(b(c)d)"
+	Output: "a(b(c)d)"
+
+	Constraints:
+	* 1 <= s.length <= 10^5
+	* s[i] is either'(' , ')', or lowercase English letter.*/
+
+    string minRemoveToMakeValid(string s) {
+        int close = 0, diff = 0; 
+        for (auto& ch : s) 
+            if (ch == ')') ++close; 
+        
+        string ans; 
+        for (auto& ch : s) {
+            if (ch == '(') {
+                if (diff < close) ++diff; 
+                else continue; 
+            } else if (ch == ')') {
+                --close; 
+                if (diff > 0) --diff; 
+                else continue; 
+            }
+            ans.push_back(ch); 
+        }
+        return ans; 
+    }
+
+
     /*1275. Find Winner on a Tic Tac Toe Game (Easy)
 	Tic-tac-toe is played by two players A and B on a 3 x 3 grid. Here are the 
 	rules of Tic-Tac-Toe:
