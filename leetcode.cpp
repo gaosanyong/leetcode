28e1d40647aaf2b751e1aedc0663912e71768165
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-03 21:26:57 -0400

    solve'em all (199)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4464,6 +4464,46 @@ public:
     }
 
 
+    /*199. Binary Tree Right Side View (Medium)
+	Given the root of a binary tree, imagine yourself standing on the right 
+	side of it, return the values of the nodes you can see ordered from top to 
+	bottom.
+
+	Example 1:
+	Input: root = [1,2,3,null,5,null,4]
+	Output: [1,3,4]
+
+	Example 2:
+	Input: root = [1,null,3]
+	Output: [1,3]
+
+	Example 3:
+	Input: root = []
+	Output: []
+
+	Constraints:
+	* The number of nodes in the tree is in the range [0, 100].
+	* -100 <= Node.val <= 100*/
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans; 
+        if (root) {
+            queue<TreeNode*> q; 
+            q.push(root); 
+            while (q.size()) {
+                TreeNode* node = nullptr; 
+                for (int sz = q.size(); sz; --sz) {
+                    node = q.front(); q.pop(); 
+                    if (node->left) q.push(node->left); 
+                    if (node->right) q.push(node->right); 
+                }
+                ans.push_back(node->val); 
+            }
+        }
+        return ans; 
+    }
+
+
     /*200. Number of Islands (Medium)
 	Given an m x n 2D binary grid grid which represents a map of '1's (land) 
 	and '0's (water), return the number of islands. An island is surrounded by 
