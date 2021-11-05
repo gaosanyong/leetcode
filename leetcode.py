2958d70c7394f1f93e21aed6620dd52ed3fed86e
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-04 21:16:57 -0400

    November LeetCoding Challenge 2021-11-05 (441)

--- leetcode.py
+++ leetcode.py
@@ -11783,6 +11783,28 @@ class UnionFind:
         return stack[-1]
 
 
+    """441. Arranging Coins (Easy)
+	You have n coins and you want to build a staircase with these coins. The 
+	staircase consists of k rows where the ith row has exactly i coins. The 
+	last row of the staircase may be incomplete. Given the integer n, return 
+	the number of complete rows of the staircase you will build.
+
+	Example 1:
+	Input: n = 5
+	Output: 2
+	Explanation: Because the 3rd row is incomplete, we return 2.
+
+	Example 2:
+	Input: n = 8
+	Output: 3
+	Explanation: Because the 4th row is incomplete, we return 3.
+
+	Constraints: 1 <= n <= 2^31 - 1"""
+
+    def arrangeCoins(self, n: int) -> int:
+        return int((sqrt(8*n + 1) - 1)//2)
+
+
     """442. Find All Duplicates in an Array (Medium)
 	Given an integer array nums of length n where all the integers of nums are 
 	in the range [1, n] and each integer appears once or twice, return an array 
