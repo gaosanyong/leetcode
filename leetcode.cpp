154967ba21fa8362016bd6977f99d126b568aa3b
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-05 21:04:21 -0400

    solve'em all (956)

--- leetcode.cpp
+++ leetcode.cpp
@@ -16946,6 +16946,52 @@ public:
     }
 
 
+    /*956. Tallest Billboard (Hard)
+	You are installing a billboard and want it to have the largest height. The 
+	billboard will have two steel supports, one on each side. Each steel 
+	support must be an equal height. You are given a collection of rods that 
+	can be welded together. For example, if you have rods of lengths 1, 2, and 
+	3, you can weld them together to make a support of length 6. Return the 
+	largest possible height of your billboard installation. If you cannot 
+	support the billboard, return 0.
+
+	Example 1:
+	Input: rods = [1,2,3,6]
+	Output: 6
+	Explanation: We have two disjoint subsets {1,2,3} and {6}, which have the 
+	             same sum = 6.
+	
+	Example 2:
+	Input: rods = [1,2,3,4,5,6]
+	Output: 10
+	Explanation: We have two disjoint subsets {2,3,5} and {4,6}, which have the 
+	             same sum = 10.
+	
+	Example 3:
+	Input: rods = [1,2]
+	Output: 0
+	Explanation: The billboard cannot be supported, so we return 0.
+
+	Constraints:
+	* 1 <= rods.length <= 20
+	* 1 <= rods[i] <= 1000
+	* sum(rods[i]) <= 5000*/
+
+    int tallestBillboard(vector<int>& rods) {
+        unordered_map<int, int> dp; 
+        dp[0] = 0; 
+        for (auto& x : rods) {
+            unordered_map<int, int> temp = dp; 
+            for (auto& [k, v] : temp) {
+                dp[k+x] = max(dp[k+x], v); 
+                if (x <= k) dp[k-x] = max(dp[k-x], v+x); 
+                else dp[x-k] = max(dp[x-k], v+k); 
+            }
+        }
+        return dp[0]; 
+    }
+
+
     /*961. N-Repeated Element in Size 2N Array (Easy)
 	In a array nums of size 2 * n, there are n + 1 unique elements, and exactly 
 	one of these elements is repeated n times. Return the element repeated n 
