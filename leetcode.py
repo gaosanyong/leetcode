af8b41fa46a71846ce6f219c5b9eaf05f749389c
Harshit Gupta <harshitgupta1023@gmail.com>
2021-11-07 16:21:30 +0530

    Added question 69

--- leetcode.py
+++ leetcode.py
@@ -2330,6 +2330,34 @@ class Solution:
         ans.append(" ".join(line).ljust(maxWidth))
         return ans 
 
+	"""69 Given a non-negative integer x, compute and return the square root of x.
+	Since the return type is an integer, the decimal digits are truncated, and 
+	only the integer part of the result is returned.
+
+	Note: You are not allowed to use any built-in exponent function or operator,
+	such as pow(x, 0.5) or x ** 0.5.
+
+	Example 1:
+	Input: x = 4
+	Output: 2
+
+	Example 2:
+	Input: x = 8
+	Output: 2
+	Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+	"""
+	def mySqrt(self, x: int) -> int:
+	if x == 0 :return 0
+	i,j = 1,2**31        
+	while i<=j:
+		mid = (i+j)//2
+		temp = mid*mid
+		if temp == x:return mid
+		elif temp < x: i = mid+1
+		else:
+			if (mid-1)*(mid-1) < x:
+				return mid-1
+			j = mid-1
 
     """70. Climbing Stairs (Easy)
 	You are climbing a stair case. It takes n steps to reach to the top. Each 
