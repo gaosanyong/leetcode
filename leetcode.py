93c50eb92cd9f7a370974c3a83191804dea3eeb5
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-06 21:35:33 -0400

    update 583

--- leetcode.py
+++ leetcode.py
@@ -14163,16 +14163,15 @@ class Trie:
 	* word1 and word2 consist of only lowercase English letters."""
 
     def minDistance(self, word1: str, word2: str) -> int:
-        
-        @cache
-        def fn(i1, i2): 
-            """Return minimum steps to make word1 and word2 the same."""
-            if i1 == len(word1): return len(word2) - i2
-            if i2 == len(word2): return len(word1) - i1
-            if word1[i1] == word2[i2]: return fn(i1+1, i2+1)
-            return 1 + min(fn(i1+1,i2), fn(i1, i2+1))
-        
-        return fn(0, 0)
+        m, n = len(word1), len(word2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(m): dp[i][n] = m-i
+        for j in range(n): dp[m][j] = n-j
+        for i in range(m-1, -1, -1): 
+            for j in range(n-1, -1, -1): 
+                if word1[i] == word2[j]: dp[i][j] = dp[i+1][j+1]
+                else: dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1])
+        return dp[0][0]
 
 
     """587. Erect the Fence (Hard)
