50e9526184f539f4f77b5ca02103b668acaeb16a
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-06 21:23:11 -0400

    solve'em all (841)

--- leetcode.cpp
+++ leetcode.cpp
@@ -14121,6 +14121,57 @@ public:
     }
 
 
+    /*841. Keys and Rooms (Medium)
+	There are n rooms labeled from 0 to n - 1 and all the rooms are locked 
+	except for room 0. Your goal is to visit all the rooms. However, you cannot 
+	enter a locked room without having its key. When you visit a room, you may 
+	find a set of distinct keys in it. Each key has a number on it, denoting 
+	which room it unlocks, and you can take all of them with you to unlock the 
+	other rooms. Given an array rooms where rooms[i] is the set of keys that 
+	you can obtain if you visited room i, return true if you can visit all the 
+	rooms, or false otherwise.
+
+	Example 1:
+	Input: rooms = [[1],[2],[3],[]]
+	Output: true
+	Explanation: We visit room 0 and pick up key 1.
+	             We then visit room 1 and pick up key 2.
+	             We then visit room 2 and pick up key 3.
+	             We then visit room 3.
+	             Since we were able to visit every room, we return true.
+	
+	Example 2:
+	Input: rooms = [[1,3],[3,0,1],[2],[0]]
+	Output: false
+	Explanation: We can not enter room number 2 since the only key that unlocks 
+	             it is in that room.
+
+	Constraints:
+	* n == rooms.length
+	* 2 <= n <= 1000
+	* 0 <= rooms[i].length <= 1000
+	* 1 <= sum(rooms[i].length) <= 3000
+	* 0 <= rooms[i][j] < n
+	* All the values of rooms[i] are unique.*/
+
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size(); 
+        queue<int> q; q.push(0); 
+        vector<bool> visited(n); 
+        visited[0] = true; 
+        while (q.size()) 
+            for (int sz = q.size(); sz; --sz) {
+                int i = q.front(); q.pop(); 
+                for (auto& x : rooms[i]) 
+                    if (!visited[x]) {
+                        visited[x] = true; 
+                        q.push(x); 
+                    }
+            }
+        return all_of(visited.begin(), visited.end(), [&](bool x){ return x; }); 
+    }
+
+
     /*843. Guess the Word (Hard)
 	This is an interactive problem. You are given an array of unique strings 
 	wordlist where wordlist[i] is 6 letters long, and one word in this list is 
