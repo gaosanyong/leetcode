9c1550313ba676567fe29f85cc689107acdd5910
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-07 20:56:40 -0500

    solve'em all (215)

--- leetcode.cpp
+++ leetcode.cpp
@@ -4885,6 +4885,29 @@ public:
     }
 
 
+    /*215. Kth Largest Element in an Array (Medium)
+	Given an integer array nums and an integer k, return the kth largest 
+	element in the array. Note that it is the kth largest element in the 
+	sorted order, not the kth distinct element.
+
+	Example 1:
+	Input: nums = [3,2,1,5,6,4], k = 2
+	Output: 5
+
+	Example 2:
+	Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+	Output: 4
+
+	Constraints:
+	* 1 <= k <= nums.length <= 10^4
+	* -10^4 <= nums[i] <= 10^4*/
+
+    int findKthLargest(vector<int>& nums, int k) {
+        nth_element(nums.begin(), nums.end()-k, nums.end()); 
+        return nums[nums.size()-k]; 
+    }
+
+
     /*216. Combination Sum III (Medium)
 	Find all valid combinations of k numbers that sum up to n such that the 
 	following conditions are true:
