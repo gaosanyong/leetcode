945dbc74f2888d0efcfe9be99d8458f962d54c30
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-08 22:42:20 -0500

    solve'em all (973)

--- leetcode.cpp
+++ leetcode.cpp
@@ -17337,6 +17337,41 @@ public:
     }
 
 
+    /*973. K Closest Points to Origin (Medium)
+	Given an array of points where points[i] = [xi, yi] represents a point on 
+	the X-Y plane and an integer k, return the k closest points to the origin 
+	(0, 0). The distance between two points on the X-Y plane is the Euclidean 
+	distance (i.e., √(x1 - x2)^2 + (y1 - y2)^2). You may return the answer in 
+	any order. The answer is guaranteed to be unique (except for the order that 
+	it is in).
+
+	Example 1:
+	Input: points = [[1,3],[-2,2]], k = 1
+	Output: [[-2,2]]
+	Explanation: The distance between (1, 3) and the origin is sqrt(10). The 
+	             distance between (-2, 2) and the origin is sqrt(8). Since 
+	             sqrt(8) < sqrt(10), (-2, 2) is closer to the origin. We only 
+	             want the closest k = 1 points from the origin, so the answer 
+	             is just [[-2,2]].
+	
+	Example 2:
+	Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+	Output: [[3,3],[-2,4]]
+	Explanation: The answer [[-2,4],[3,3]] would also be accepted.
+
+	Constraints:
+	* 1 <= k <= points.length <= 10^4
+	* -10^4 < xi, yi < 10^4*/
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        nth_element(points.begin(), points.begin()+k, points.end(), [](auto& lhs, auto& rhs) {
+            return lhs[0]*lhs[0] + lhs[1]*lhs[1] < rhs[0]*rhs[0] + rhs[1]*rhs[1]; 
+        }); 
+        points.resize(k); 
+        return points; 
+    }
+
+
     /*975. Odd Even Jump (Hard)
 	You are given an integer array arr. From some starting index, you can make 
 	a series of jumps. The (1st, 3rd, 5th, ...) jumps in the series are called 
