970e28e30d3f86860705c19f2951bcdb9204e51c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-08 22:49:22 -0500

    update 201

--- leetcode.py
+++ leetcode.py
@@ -6229,20 +6229,26 @@ class Solution:
 
 
    """201. Bitwise AND of Numbers Range (Medium)
-	Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise 
-	AND of all numbers in this range, inclusive.
+	Given two integers left and right that represent the range [left, right], 
+	return the bitwise AND of all numbers in this range, inclusive.
 
 	Example 1:
-	Input: [5,7]
+	Input: left = 5, right = 7
 	Output: 4
 
 	Example 2:
-	Input: [0,1]
-	Output: 0"""
+	Input: left = 0, right = 0
+	Output: 0
 
-    def rangeBitwiseAnd(self, m: int, n: int) -> int:
-        while n > m: n &= n-1 # unset last set bit
-        return n 
+	Example 3:
+	Input: left = 1, right = 2147483647
+	Output: 0
+
+	Constraints: 0 <= left <= right <= 2^31 - 1"""
+
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        while left < right: right &= right - 1
+        return right 
 
 
     """202. Happy Number (Easy)
