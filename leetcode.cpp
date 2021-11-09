374fa391506c8fa613de1e9f0eaba1ef99c6cf9c
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-08 22:27:04 -0500

    solve'em all (1178)

--- leetcode.cpp
+++ leetcode.cpp
@@ -19860,6 +19860,63 @@ public:
     }
 
 
+    /*1178. Number of Valid Words for Each Puzzle (Hard)
+	With respect to a given puzzle string, a word is valid if both the 
+	following conditions are satisfied:
+	* word contains the first letter of puzzle.
+	* For each letter in word, that letter is in puzzle.
+	  + For example, if the puzzle is "abcdefg", then valid words are "faced", 
+	    "cabbage", and "baggage", while
+	  + invalid words are "beefed" (does not include 'a') and "based" (includes 
+	    's' which is not in the puzzle).
+	Return an array answer, where answer[i] is the number of words in the given 
+	word list words that is valid with respect to the puzzle puzzles[i].
+
+	Example 1:
+	Input: words = ["aaaa","asas","able","ability","actt","actor","access"], 
+	       puzzles = ["aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"]
+	Output: [1,1,3,2,4,0]
+	Explanation: 1 valid word for "aboveyz" : "aaaa" 
+	             1 valid word for "abrodyz" : "aaaa"
+	             3 valid words for "abslute" : "aaaa", "asas", "able"
+	             2 valid words for "absoryz" : "aaaa", "asas"
+	             4 valid words for "actresz" : "aaaa", "asas", "actt", "access"
+	             There are no valid words for "gaswxyz" cause none of the words 
+	             in the list contains letter 'g'.
+	
+	Example 2:
+	Input: words = ["apple","pleas","please"], 
+	       puzzles = ["aelwxyz","aelpxyz","aelpsxy","saelpxy","xaelpsy"]
+	Output: [0,1,3,2,0]
+
+	Constraints:
+	* 1 <= words.length <= 10^5
+	* 4 <= words[i].length <= 50
+	* 1 <= puzzles.length <= 10^4
+	* puzzles[i].length == 7
+	* words[i] and puzzles[i] consist of lowercase English letters.
+	* Each puzzles[i] does not contain repeated characters.*/
+
+    vector<int> findNumOfValidWords(vector<string>& words, vector<string>& puzzles) {
+        unordered_map<int, int> freq; 
+        for (auto& word : words) {
+            int mask = 0; 
+            for (auto& ch : word) mask |= 1 << (ch - 'a'); 
+            ++freq[mask]; 
+        }
+        
+        vector<int> ans; 
+        for (auto& puzzle : puzzles) {
+            int mask = 0, cnt = 0; 
+            for (auto& ch : puzzle) mask |= 1 << (ch - 'a'); 
+            for (int m = mask; m; m = mask & (m-1)) 
+                if (m & (1 << (puzzle[0]-'a'))) cnt += freq[m]; 
+            ans.push_back(cnt); 
+        }
+        return ans; 
+    }
+
+
     /*1181. Before and After Puzzle (Medium)
 	Given a list of phrases, generate a list of Before and After puzzles. A 
 	phrase is a string that consists of lowercase English letters and spaces 
