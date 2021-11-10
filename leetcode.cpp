f2e09e021654d101a292035e0de362c48982cd04
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-09 23:43:20 -0500

    solve'em all (757)

--- leetcode.cpp
+++ leetcode.cpp
@@ -12529,6 +12529,42 @@ public:
     }
 
 
+    /*757. Set Intersection Size At Least Two (Hard)
+	An integer interval [a, b] (for integers a < b) is a set of all consecutive 
+	integers from a to b, including a and b. Find the minimum size of a set S 
+	such that for every integer interval A in intervals, the intersection of S 
+	with A has a size of at least two.
+
+	Example 1:
+	Input: intervals = [[1,3],[1,4],[2,5],[3,5]]
+	Output: 3
+	Explanation: Consider the set S = {2, 3, 4}.  For each interval, there are 
+	             at least 2 elements from S in the interval. Also, there isn't 
+	             a smaller size set that fulfills the above condition. Thus, we 
+	             output the size of this set, which is 3.
+	
+	Example 2:
+	Input: intervals = [[1,2],[2,3],[2,4],[4,5]]
+	Output: 5
+	Explanation: An example of a minimum sized set is {1, 2, 3, 4, 5}.
+
+	Constraints:
+	* 1 <= intervals.length <= 3000
+	* intervals[i].length == 2
+	* 0 <= ai < bi <= 10^8*/
+
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& lhs, auto& rhs) {return lhs[1] < rhs[1] || (lhs[1] == rhs[1] && lhs[0] > rhs[0]);}); 
+        int ans = 0, y0 = -1, y1 = -1; 
+        for (auto& interval : intervals) {
+            int x = interval[0], y = interval[1]; 
+            if (y0 < x && x <= y1) ++ans, y0 = y1, y1 = y; 
+            else if (y1 < x) ans += 2, y0 = y-1, y1 = y; 
+        }
+        return ans; 
+    }
+
+
     /*762. Prime Number of Set Bits in Binary Representation (Easy)
 	Given two integers L and R, find the count of numbers in the range [L, R] 
 	(inclusive) having a prime number of set bits in their binary representation.
--- leetcode.py
+++ leetcode.py
@@ -17148,6 +17148,39 @@ class UnionFind:
         return fn(bottom)
 
 
+    """757. Set Intersection Size At Least Two (Hard)
+	An integer interval [a, b] (for integers a < b) is a set of all consecutive 
+	integers from a to b, including a and b. Find the minimum size of a set S 
+	such that for every integer interval A in intervals, the intersection of S 
+	with A has a size of at least two.
+
+	Example 1:
+	Input: intervals = [[1,3],[1,4],[2,5],[3,5]]
+	Output: 3
+	Explanation: Consider the set S = {2, 3, 4}.  For each interval, there are 
+	             at least 2 elements from S in the interval. Also, there isn't 
+	             a smaller size set that fulfills the above condition. Thus, we 
+	             output the size of this set, which is 3.
+	
+	Example 2:
+	Input: intervals = [[1,2],[2,3],[2,4],[4,5]]
+	Output: 5
+	Explanation: An example of a minimum sized set is {1, 2, 3, 4, 5}.
+
+	Constraints:
+	* 1 <= intervals.length <= 3000
+	* intervals[i].length == 2
+	* 0 <= ai < bi <= 10^8"""
+
+    def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
+        ans = []
+        for x, y in sorted(intervals, key=lambda x: (x[1], -x[0])): 
+            if not ans or ans[-2] < x: 
+                if ans and x <= ans[-1]: ans.append(y)
+                else: ans.extend([y-1, y])
+        return len(ans)
+
+
     """758. Bold Words in String (Easy)
 	Given a set of keywords words and a string S, make all appearances of all 
 	keywords in S bold. Any letters between <b> and </b> tags become bold. The 
