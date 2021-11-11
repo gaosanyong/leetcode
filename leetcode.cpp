a2d3b41efe899607ecd4157cf502bdde815d5dba
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-10 20:08:40 -0500

    solve'em all (1413)

--- leetcode.cpp
+++ leetcode.cpp
@@ -20770,6 +20770,48 @@ public:
     }
 
 
+    /*1413. Minimum Value to Get Positive Step by Step Sum (Easy)
+	Given an array of integers nums, you start with an initial positive value 
+	startValue. In each iteration, you calculate the step by step sum of 
+	startValue plus elements in nums (from left to right). Return the minimum 
+	positive value of startValue such that the step by step sum is never less 
+	than 1.
+
+	Example 1:
+	Input: nums = [-3,2,-3,4,2]
+	Output: 5
+	Explanation: If you choose startValue = 4, in the third iteration your step 
+	             by step sum is less than 1.
+	             startValue = 4 | startValue = 5 | nums
+	               (4 -3 ) = 1  | (5 -3 ) = 2    |  -3
+	               (1 +2 ) = 3  | (2 +2 ) = 4    |   2
+	               (3 -3 ) = 0  | (4 -3 ) = 1    |  -3
+	               (0 +4 ) = 4  | (1 +4 ) = 5    |   4
+	               (4 +2 ) = 6  | (5 +2 ) = 7    |   2
+	
+	Example 2:
+	Input: nums = [1,2]
+	Output: 1
+	Explanation: Minimum start value should be positive. 
+
+	Example 3:
+	Input: nums = [1,-2,-3]
+	Output: 5
+
+	Constraints:
+	* 1 <= nums.length <= 100
+	* -100 <= nums[i] <= 100*/
+
+    int minStartValue(vector<int>& nums) {
+        int ans = 0, prefix = 0; 
+        for (auto& x : nums) {
+            prefix += x; 
+            ans = min(ans, prefix); 
+        }
+        return 1 - ans; 
+    }
+
+
     /*1423. Maximum Points You Can Obtain from Cards (Medium)
 	There are several cards arranged in a row, and each card has an associated 
 	number of points The points are given in the integer array cardPoints. In 
