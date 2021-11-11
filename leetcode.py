d7ec94c1e365cfecbd75579b035f46e4c51d8681
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-10 20:27:39 -0500

    update 149

--- leetcode.py
+++ leetcode.py
@@ -5069,8 +5069,9 @@ class Solution:
 
 
     """149. Max Points on a Line (Hard)
-	Given n points on a 2D plane, find the maximum number of points that lie on 
-	the same straight line.
+	Given an array of points where points[i] = [xi, yi] represents a point on 
+	the X-Y plane, return the maximum number of points that lie on the same 
+	straight line.
 
 	Example 1:
 	Input: [[1,1],[2,2],[3,3]]
@@ -5097,25 +5098,28 @@ class Solution:
 	+------------------->
 	0  1  2  3  4  5  6
 
-	NOTE: input types have been changed on April 15, 2019. Please reset to 
-	default code definition to get new method signature."""
+	Constraints:
+    * 1 <= points.length <= 300
+    * points[i].length == 2
+    * -10^4 <= xi, yi <= 10^4
+    * All the points are unique."""
 
     def maxPoints(self, points: List[List[int]]) -> int:
         ans = 0
-        for i, (x0, y0) in enumerate(points): #reference 
-            dupe = 1 #count of duplicates
-            freq = dict() #frequency table 
-            for x, y in points[i+1:]:
-                if x0 == x and y0 == y: dupe += 1
-                elif x0 == x: freq[0, inf] = 1 + freq.get((0, inf), 0)
-                elif y0 == y: freq[inf, 0] = 1 + freq.get((inf, 0), 0)
+        for i, (x, y) in enumerate(points): 
+            freq = defaultdict(int)
+            for ii in range(i+1, len(points)): 
+                xx, yy = points[ii]
+                if x == xx: dx, dy = 0, 1
+                elif y == yy: dx, dy = 1, 0
                 else: 
-                    g = gcd(x-x0, y-y0)
-                    x, y = (x-x0)//g, (y-y0)//g
-                    if x < 0: x, y = -x, -y
-                    freq[x, y] = 1 + freq.get((x, y), 0)
-            ans = max(ans, dupe + max(freq.values(), default=0))
-        return ans 
+                    dx, dy = xx-x, yy-y
+                    g = gcd(dx, dy)
+                    if dx < 0: g *= -1
+                    dx, dy = dx//g, dy//g
+                freq[dx, dy] += 1
+            ans = max(ans, max(freq.values(), default=0))
+        return 1 + ans
 
 
     """150. Evaluate Reverse Polish Notation (Medium)
