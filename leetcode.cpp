a0c58457d97eff27a1b7c37fd9d1e888e57907a5
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-12 20:51:45 -0500

    November LeetCoding Challenge 2021-11-13 (739)

--- leetcode.cpp
+++ leetcode.cpp
@@ -12276,6 +12276,43 @@ public:
         return ans; 
     }
 
+    
+    /*739. Daily Temperatures (Medium)
+	Given an array of integers temperatures represents the daily temperatures, 
+	return an array answer such that answer[i] is the number of days you have 
+	to wait after the ith day to get a warmer temperature. If there is no 
+	future day for which this is possible, keep answer[i] == 0 instead.
+
+	Example 1:
+	Input: temperatures = [73,74,75,71,69,72,76,73]
+	Output: [1,1,4,2,1,1,0,0]
+
+	Example 2:
+	Input: temperatures = [30,40,50,60]
+	Output: [1,1,1,0]
+
+	Example 3:
+	Input: temperatures = [30,60,90]
+	Output: [1,1,0]
+
+	Constraints:
+	* 1 <= temperatures.length <= 10^5
+	* 30 <= temperatures[i] <= 100*/
+
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size(); 
+        vector<int> ans(n); 
+        stack<int> stk; 
+        for (int i = 0; i < n; ++i) {
+            while (stk.size() && temperatures[stk.top()] < temperatures[i]) {
+                int ii = stk.top(); stk.pop(); 
+                ans[ii] = i - ii; 
+            }
+            stk.push(i); 
+        }
+        return ans; 
+    }
+
 
     /*740. Delete and Earn (Medium)
 	You are given an integer array nums. You want to maximize the number of 
