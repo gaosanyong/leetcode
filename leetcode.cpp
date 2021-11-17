ab1c5fc9dc267554a26f5a31078507577ff7c5b6
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-16 22:24:11 -0500

    solve'em all (675)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10756,6 +10756,90 @@ public:
     }
 
 
+    /*675. Cut Off Trees for Golf Event (Hard)
+	You are asked to cut off all the trees in a forest for a golf event. The 
+	forest is represented as an m x n matrix. In this matrix:
+	* 0 means the cell cannot be walked through.
+	* 1 represents an empty cell that can be walked through.
+	* A number greater than 1 represents a tree in a cell that can be walked 
+	  through, and this number is the tree's height.
+	In one step, you can walk in any of the four directions: north, east, south, 
+	and west. If you are standing in a cell with a tree, you can choose whether 
+	to cut it off. You must cut off the trees in order from shortest to tallest. 
+	When you cut off a tree, the value at its cell becomes 1 (an empty cell).
+	Starting from the point (0, 0), return the minimum steps you need to walk 
+	to cut off all the trees. If you cannot cut off all the trees, return -1.
+	You are guaranteed that no two trees have the same height, and there is at 
+	least one tree needs to be cut off.
+
+	Example 1:
+	Input: forest = [[1,2,3],[0,0,4],[7,6,5]]
+	Output: 6
+	Explanation: Following the path above allows you to cut off the trees from 
+	             shortest to tallest in 6 steps.
+
+	Example 2:
+	Input: forest = [[1,2,3],[0,0,0],[7,6,5]]
+	Output: -1
+	Explanation: The trees in the bottom row cannot be accessed as the middle 
+	             row is blocked.
+	
+	Example 3:
+	Input: forest = [[2,3,4],[0,0,5],[8,7,6]]
+	Output: 6
+	Explanation: You can follow the same path as Example 1 to cut off all the 
+	             trees. Note that you can cut off the first tree at (0, 0) 
+	             before making any steps.
+
+	Constraints:
+	* m == forest.length
+	* n == forest[i].length
+	* 1 <= m, n <= 50
+	* 0 <= forest[i][j] <= 10^9*/
+
+    int cutOffTree(vector<vector<int>>& forest) {
+        int m = forest.size(), n = forest[0].size(), dir[5] = {-1, 0, 1, 0, -1}; 
+        bool seen[m][n]; 
+        
+        auto fn = [&](pair<int, int>& start, pair<int, int>& end) {
+            int ans = 0; 
+            memset(seen, false, sizeof(seen)); 
+            seen[start.first][start.second] = true; 
+            queue<pair<int, int>> q; q.push(start); 
+            for (; q.size(); ++ans) 
+                for (int sz = q.size(); sz; --sz) {
+                    auto [i, j] = q.front(); q.pop(); 
+                    if (i == end.first && j == end.second) return ans; 
+                    for (int k = 0; k < 4; ++k) {
+                        int ii = i + dir[k], jj = j + dir[k+1]; 
+                        if (0 <= ii && ii < m && 0 <= jj && jj < n && forest[ii][jj] != 0 && !seen[ii][jj]) {
+                            seen[ii][jj] = true; 
+                            q.emplace(ii, jj); 
+                        }
+                    }
+                }
+            return -1; 
+        }; 
+        
+        vector<tuple<int, int, int>> vals; 
+        for (int i = 0; i < m; ++i) 
+            for (int j = 0; j < n; ++j) 
+                if (forest[i][j] > 1) vals.emplace_back(forest[i][j], i, j); 
+        sort(vals.begin(), vals.end()); 
+        
+        int ans = 0; 
+        pair<int, int> start = {0, 0}; 
+        for (auto& [v, i, j] : vals) {
+            pair<int, int> end = {i, j}; 
+            int val = fn(start, end); 
+            if (val == -1) return -1; 
+            ans += val; 
+            start = end; 
+        }
+        return ans; 
+    }
+
+
     /*680. Valid Palindrome II (Easy)
 	Given a non-empty string s, you may delete at most one character. Judge 
 	whether you can make it a palindrome.
--- leetcode.py
+++ leetcode.py
@@ -15264,6 +15264,76 @@ class Trie:
         return lo
 
 
+    """675. Cut Off Trees for Golf Event (Hard)
+	You are asked to cut off all the trees in a forest for a golf event. The 
+	forest is represented as an m x n matrix. In this matrix:
+	* 0 means the cell cannot be walked through.
+	* 1 represents an empty cell that can be walked through.
+	* A number greater than 1 represents a tree in a cell that can be walked 
+	  through, and this number is the tree's height.
+	In one step, you can walk in any of the four directions: north, east, south, 
+	and west. If you are standing in a cell with a tree, you can choose whether 
+	to cut it off. You must cut off the trees in order from shortest to tallest. 
+	When you cut off a tree, the value at its cell becomes 1 (an empty cell).
+	Starting from the point (0, 0), return the minimum steps you need to walk 
+	to cut off all the trees. If you cannot cut off all the trees, return -1.
+	You are guaranteed that no two trees have the same height, and there is at 
+	least one tree needs to be cut off.
+
+	Example 1:
+	Input: forest = [[1,2,3],[0,0,4],[7,6,5]]
+	Output: 6
+	Explanation: Following the path above allows you to cut off the trees from 
+	             shortest to tallest in 6 steps.
+
+	Example 2:
+	Input: forest = [[1,2,3],[0,0,0],[7,6,5]]
+	Output: -1
+	Explanation: The trees in the bottom row cannot be accessed as the middle 
+	             row is blocked.
+	
+	Example 3:
+	Input: forest = [[2,3,4],[0,0,5],[8,7,6]]
+	Output: 6
+	Explanation: You can follow the same path as Example 1 to cut off all the 
+	             trees. Note that you can cut off the first tree at (0, 0) 
+	             before making any steps.
+
+	Constraints:
+	* m == forest.length
+	* n == forest[i].length
+	* 1 <= m, n <= 50
+	* 0 <= forest[i][j] <= 10^9"""
+
+    def cutOffTree(self, forest: List[List[int]]) -> int:
+        m, n = len(forest), len(forest[0])
+        
+        def fn(start, end): 
+            """Return min steps to move from start to end."""
+            ans = 0 
+            seen = {start}
+            queue = deque([start])
+            while queue: 
+                for _ in range(len(queue)): 
+                    i, j = queue.popleft()
+                    if (i, j) == end: return ans
+                    for ii, jj in (i-1, j), (i, j-1), (i, j+1), (i+1, j): 
+                        if 0 <= ii < m and 0 <= jj < n and forest[ii][jj] != 0 and (ii, jj) not in seen: 
+                            seen.add((ii, jj))
+                            queue.append((ii, jj))
+                ans += 1
+            return -1 
+        
+        ans = 0 
+        start = (0, 0)
+        for _, i, j in sorted((forest[i][j], i, j) for i in range(m) for j in range(n) if forest[i][j] > 1): 
+            val = fn(start, (i, j))
+            if val == -1: return -1 
+            ans += val 
+            start = (i, j)
+        return ans 
+
+
     """678. Valid Parenthesis String (Medium)
 	Given a string containing only three types of characters: '(', ')' and '*', 
 	write a function to check whether this string is valid. We define the 
