16ad4f1ff64d75b477c3e761cafbe02ecb9a6c09
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-18 20:26:35 -0500

    November LeetCoding Challenge 2021-11-19 (461)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7790,6 +7790,33 @@ public:
     }
 
 
+    /*461. Hamming Distance (Easy)
+	The Hamming distance between two integers is the number of positions at 
+	which the corresponding bits are different. Given two integers x and y, 
+	return the Hamming distance between them.
+
+	Example 1:
+	Input: x = 1, y = 4
+	Output: 2
+	Explanation: 1   (0 0 0 1)
+	             4   (0 1 0 0)
+	                    ↑   ↑
+	             The above arrows point to positions where the corresponding 
+	             bits are different.
+	
+	Example 2:
+	Input: x = 3, y = 1
+	Output: 1
+
+	Constraints: 0 <= x, y <= 2^31 - 1*/
+
+    int hammingDistance(int x, int y) {
+        int ans = 0; 
+        for (int diff = x^y; diff; diff &= diff-1) ++ans; 
+        return ans; 
+    }
+
+
     /*462. Minimum Moves to Equal Array Elements II (Medium)
 	Given an integer array nums of size n, return the minimum number of moves 
 	required to make all array elements equal. In one move, you can increment 
--- leetcode.py
+++ leetcode.py
@@ -12006,6 +12006,30 @@ class Solution:
         return sum(abs(x-nums[mid]) for x in nums)
 
 
+    """461. Hamming Distance (Easy)
+	The Hamming distance between two integers is the number of positions at 
+	which the corresponding bits are different. Given two integers x and y, 
+	return the Hamming distance between them.
+
+	Example 1:
+	Input: x = 1, y = 4
+	Output: 2
+	Explanation: 1   (0 0 0 1)
+	             4   (0 1 0 0)
+	                    ↑   ↑
+	             The above arrows point to positions where the corresponding 
+	             bits are different.
+	
+	Example 2:
+	Input: x = 3, y = 1
+	Output: 1
+
+	Constraints: 0 <= x, y <= 2^31 - 1"""
+
+    def hammingDistance(self, x: int, y: int) -> int:
+        return bin(x^y).count("1"); 
+
+
     """463. Island Perimeter (Easy)
 	You are given row x col grid representing a map where grid[i][j] = 1 
 	represents land and grid[i][j] = 0 represents water. Grid cells are 
