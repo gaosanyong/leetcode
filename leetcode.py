02dac4ff97ee937e6eefc5162256f4e2c156b497
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-19 21:38:27 -0500

    November LeetCoding Challenge 2021-11-20 (540)

--- leetcode.py
+++ leetcode.py
@@ -13356,6 +13356,33 @@ class Trie:
         return root 
 
 
+    """540. Single Element in a Sorted Array (Medium)
+	You are given a sorted array consisting of only integers where every 
+	element appears exactly twice, except for one element which appears exactly 
+	once. Return the single element that appears only once. Your solution must 
+	run in O(log n) time and O(1) space.
+
+	Example 1:
+	Input: nums = [1,1,2,3,3,4,4,8,8]
+	Output: 2
+
+	Example 2:
+	Input: nums = [3,3,7,7,10,11,11]
+	Output: 10
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* 0 <= nums[i] <= 10^5"""
+
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        lo, hi = 0, len(nums)
+        while lo < hi: 
+            mid = lo + hi >> 1
+            if mid^1 < len(nums) and nums[mid] == nums[mid^1]: lo = mid+1
+            else: hi = mid
+        return nums[lo] 
+
+
     """542. 01 Matrix (Medium)
 	Given an m x n binary matrix mat, return the distance of the nearest 0 for 
 	each cell. The distance between two adjacent cells is 1.
