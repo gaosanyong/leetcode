96eb606c36ad110884566842cfda47ab3e72dc13
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-25 20:47:43 -0500

    clean up

--- leetcode.py
+++ leetcode.py
@@ -1820,21 +1820,15 @@ class Solution:
 	of the matrix in spiral order.
 
 	Example 1:
-	Input:
-	[
-	 [ 1, 2, 3 ],
-	 [ 4, 5, 6 ],
-	 [ 7, 8, 9 ]
-	]
+	Input: [[ 1, 2, 3 ],
+	        [ 4, 5, 6 ],
+	        [ 7, 8, 9 ]]
 	Output: [1,2,3,6,9,8,7,4,5]
 
 	Example 2:
-	Input:
-	[
-	  [1, 2, 3, 4],
-	  [5, 6, 7, 8],
-	  [9,10,11,12]
-	]
+	Input: [[1, 2, 3, 4],
+	        [5, 6, 7, 8],
+	        [9,10,11,12]]
 	Output: [1,2,3,4,8,12,11,10,9,5,6,7]"""
 
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
