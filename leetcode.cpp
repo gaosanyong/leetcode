599fd2106f3451165a97cf90457695a405fdf563
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-30 13:41:34 -0500

    correct order (637)

--- leetcode.cpp
+++ leetcode.cpp
@@ -10147,48 +10147,6 @@ public:
     }
 
 
-    /*637. Average of Levels in Binary Tree (Easy)
-	Given the root of a binary tree, return the average value of the nodes on 
-	each level in the form of an array. Answers within 10-5 of the actual 
-	answer will be accepted.
-
-	Example 1:
-	Input: root = [3,9,20,null,15,7]
-	Output: [3.00000,14.50000,11.00000]
-	Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, 
-	             and on level 2 is 11. Hence return [3, 14.5, 11].
-
-	Example 2:
-	Input: root = [3,9,20,15,7]
-	Output: [3.00000,14.50000,11.00000]
-
-	Constraints:
-	* The number of nodes in the tree is in the range [1, 10^4].
-	* -2^31 <= Node.val <= 2^31 - 1*/
-
-    vector<double> averageOfLevels(TreeNode* root) {
-        vector<double> ans; 
-        
-        queue<TreeNode*> q; 
-        q.push(root); 
-        while (!q.empty()) {
-            double sm = 0, cnt = 0; 
-            for (int i = 0, n = q.size(); i < n; ++i) {
-                TreeNode* node = q.front(); 
-                q.pop(); 
-                if (node) {
-                    sm += node->val; 
-                    ++cnt; 
-                    q.push(node->left);
-                    q.push(node->right); 
-                }
-            }
-            if (cnt) ans.push_back(sm/cnt); 
-        }
-        return ans; 
-    }
-
-
     /*609. Find Duplicate File in System (Medium)
 	Given a list paths of directory info, including the directory path, and all 
 	the files with contents in this directory, return all the duplicate files 
@@ -10569,6 +10527,48 @@ public:
             f1 = f2; 
         }
         return f1; 
+    }
+
+
+    /*637. Average of Levels in Binary Tree (Easy)
+	Given the root of a binary tree, return the average value of the nodes on 
+	each level in the form of an array. Answers within 10-5 of the actual 
+	answer will be accepted.
+
+	Example 1:
+	Input: root = [3,9,20,null,15,7]
+	Output: [3.00000,14.50000,11.00000]
+	Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, 
+	             and on level 2 is 11. Hence return [3, 14.5, 11].
+
+	Example 2:
+	Input: root = [3,9,20,15,7]
+	Output: [3.00000,14.50000,11.00000]
+
+	Constraints:
+	* The number of nodes in the tree is in the range [1, 10^4].
+	* -2^31 <= Node.val <= 2^31 - 1*/
+
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans; 
+        
+        queue<TreeNode*> q; 
+        q.push(root); 
+        while (!q.empty()) {
+            double sm = 0, cnt = 0; 
+            for (int i = 0, n = q.size(); i < n; ++i) {
+                TreeNode* node = q.front(); 
+                q.pop(); 
+                if (node) {
+                    sm += node->val; 
+                    ++cnt; 
+                    q.push(node->left);
+                    q.push(node->right); 
+                }
+            }
+            if (cnt) ans.push_back(sm/cnt); 
+        }
+        return ans; 
     }
 
 
