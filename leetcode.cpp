b8fac6d0992070de9a0fc12edf911a95714b98a8
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-11-30 21:36:33 -0500

    solve'em all (483)

--- leetcode.cpp
+++ leetcode.cpp
@@ -8132,6 +8132,41 @@ public:
     }
 
 
+    /*483. Smallest Good Base (Hard)
+	Given an integer n represented as a string, return the smallest good base 
+	of n. We call k >= 2 a good base of n, if all digits of n base k are 1's.
+
+	Example 1:
+	Input: n = "13"
+	Output: "3"
+	Explanation: 13 base 3 is 111.
+
+	Example 2:
+	Input: n = "4681"
+	Output: "8"
+	Explanation: 4681 base 8 is 11111.
+
+	Example 3:
+	Input: n = "1000000000000000000"
+	Output: "999999999999999999"
+	Explanation: 1000000000000000000 base 999999999999999999 is 11.
+
+	Constraints:
+	* n is an integer in the range [3, 10^18].
+	* n does not contain any leading zeros.*/
+
+    string smallestGoodBase(string n) {
+        unsigned long x = stol(n); 
+        for (int p = log2(x); p > 1; --p) {
+            int k = pow(x, 1./p); 
+            unsigned long sm = 1, val = 1; 
+            for (int i = 1; i <= p; ++i, val *= k, sm += val); 
+            if (sm == x) return to_string(int(k)); 
+        }
+        return to_string(x-1); 
+    }
+
+
     /*484. Find Permutation (Medium)
 	A permutation perm of n integers of all the integers in the range [1, n] 
 	can be represented as a string s of length n - 1 where:
--- leetcode.py
+++ leetcode.py
@@ -12211,6 +12211,37 @@ class Trie:
         return S[:n].count(1)
 
 
+    """483. Smallest Good Base (Hard)
+	Given an integer n represented as a string, return the smallest good base 
+	of n. We call k >= 2 a good base of n, if all digits of n base k are 1's.
+
+	Example 1:
+	Input: n = "13"
+	Output: "3"
+	Explanation: 13 base 3 is 111.
+
+	Example 2:
+	Input: n = "4681"
+	Output: "8"
+	Explanation: 4681 base 8 is 11111.
+
+	Example 3:
+	Input: n = "1000000000000000000"
+	Output: "999999999999999999"
+	Explanation: 1000000000000000000 base 999999999999999999 is 11.
+
+	Constraints:
+	* n is an integer in the range [3, 10^18].
+	* n does not contain any leading zeros."""
+
+    def smallestGoodBase(self, n: str) -> str:
+        n = int(n)
+        for p in range(int(log2(n)), 1, -1): 
+            k = int(n**(1/p))
+            if (k**(p+1)-1)//(k-1) == n: return str(k)
+        return str(n-1)
+
+
     """484. Find Permutation (Medium)
 	A permutation perm of n integers of all the integers in the range [1, n] 
 	can be represented as a string s of length n - 1 where:
