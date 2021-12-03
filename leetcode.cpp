d93bd6fb9f26bdb800558f4bb5b89fc5464cfa61
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-02 20:48:35 -0500

    solve'em all (679)

--- leetcode.cpp
+++ leetcode.cpp
@@ -11179,6 +11179,65 @@ public:
     }
 
 
+    /*679. 24 Game (Hard)
+	You are given an integer array cards of length 4. You have four cards, each 
+	containing a number in the range [1, 9]. You should arrange the numbers on 
+	these cards in a mathematical expression using the operators 
+	['+', '-', '*', '/'] and the parentheses '(' and ')' to get the value 24.
+	You are restricted with the following rules:
+	* The division operator '/' represents real division, not integer division.
+	  + For example, 4 / (1 - 2 / 3) = 4 / (1 / 3) = 12.
+	* Every operation done is between two numbers. In particular, we cannot use 
+	  '-' as a unary operator.
+	  + For example, if cards = [1, 1, 1, 1], the expression "-1 - 1 - 1 - 1" 
+	    is not allowed.
+	* You cannot concatenate numbers together
+	  + For example, if cards = [1, 2, 1, 2], the expression "12 + 12" is not 
+	    valid.
+	Return true if you can get such expression that evaluates to 24, and false 
+	otherwise.
+
+	Example 1:
+	Input: cards = [4,1,8,7]
+	Output: true
+	Explanation: (8-4) * (7-1) = 24
+
+	Example 2:
+	Input: cards = [1,2,1,2]
+	Output: false
+
+	Constraints:
+	* cards.length == 4
+	* 1 <= cards[i] <= 9*/
+
+    bool judgePoint24(vector<int>& cards) {
+        
+        function<bool(vector<double>&)> fn = [&] (vector<double>& nums) {
+            if (nums.size() == 1) return abs(nums[0] - 24) < 1e-6; 
+            for (int i = 0; i < nums.size(); ++i) {
+                for (int j = i+1; j < nums.size(); ++j) {
+                    vector<double> cand; 
+                    for (int k = 0; k < nums.size(); ++k) 
+                        if (k != i && k != j) cand.push_back(nums[k]); 
+                    vector<double> vals = {nums[i]+nums[j], nums[i]-nums[j], nums[j]-nums[i], nums[i]*nums[j]}; 
+                    if (nums[j]) vals.push_back(nums[i]/nums[j]); 
+                    if (nums[i]) vals.push_back(nums[j]/nums[i]); 
+                    for (auto& x : vals) {
+                        cand.push_back(x); 
+                        if (fn(cand)) return true; 
+                        cand.pop_back(); 
+                    }
+                }
+            }
+            return false; 
+        }; 
+        
+        vector<double> nums(cards.begin(), cards.end()); 
+        return fn(nums); 
+        
+    }
+
+
     /*680. Valid Palindrome II (Easy)
 	Given a non-empty string s, you may delete at most one character. Judge 
 	whether you can make it a palindrome.
--- leetcode.py
+++ leetcode.py
@@ -3,6 +3,7 @@ Python3. No algorithm or reasoning is provided for the sake of saving spaces.
 For more details, the readers are suggested to explore on their own effort.
 """
 
+from fractions import Fraction
 from functools import cache, lru_cache, reduce
 from hashlib import sha256
 from heapq import heapify, heappop, heappush
@@ -15555,6 +15556,51 @@ class Trie:
             cl += 1 if s[~i] in ")*" else -1
             if op < 0 or cl < 0: return False 
         return True 
+
+
+    """679. 24 Game (Hard)
+	You are given an integer array cards of length 4. You have four cards, each 
+	containing a number in the range [1, 9]. You should arrange the numbers on 
+	these cards in a mathematical expression using the operators 
+	['+', '-', '*', '/'] and the parentheses '(' and ')' to get the value 24.
+	You are restricted with the following rules:
+	* The division operator '/' represents real division, not integer division.
+	  + For example, 4 / (1 - 2 / 3) = 4 / (1 / 3) = 12.
+	* Every operation done is between two numbers. In particular, we cannot use 
+	  '-' as a unary operator.
+	  + For example, if cards = [1, 1, 1, 1], the expression "-1 - 1 - 1 - 1" 
+	    is not allowed.
+	* You cannot concatenate numbers together
+	  + For example, if cards = [1, 2, 1, 2], the expression "12 + 12" is not 
+	    valid.
+	Return true if you can get such expression that evaluates to 24, and false 
+	otherwise.
+
+	Example 1:
+	Input: cards = [4,1,8,7]
+	Output: true
+	Explanation: (8-4) * (7-1) = 24
+
+	Example 2:
+	Input: cards = [1,2,1,2]
+	Output: false
+
+	Constraints:
+	* cards.length == 4
+	* 1 <= cards[i] <= 9"""
+
+    def judgePoint24(self, cards: List[int]) -> bool:
+        
+        @cache
+        def fn(*args): 
+            """Return True if existing arguments can be combined into 24."""
+            if len(args) == 1: return args[0] == 24
+            for x, y, *rem in permutations(args): 
+                for op in add, sub, mul, Fraction: 
+                    if (op != Fraction or y != 0) and fn(op(x, y), *rem): return True
+            return False 
+        
+        return fn(*cards)
 
 
     """681. Next Closest Time (Medium)
