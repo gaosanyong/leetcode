95df42e17b0e7c091222f0d83cdbfee4f02999f6
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-06 12:00:33 -0500

    solve'em all (1217)

--- leetcode.cpp
+++ leetcode.cpp
@@ -20833,6 +20833,44 @@ public:
     }
 
 
+    /*1217. Minimum Cost to Move Chips to The Same Position (Easy)
+	We have n chips, where the position of the ith chip is position[i]. We need 
+	to move all the chips to the same position. In one step, we can change the 
+	position of the ith chip from position[i] to:
+	* position[i] + 2 or position[i] - 2 with cost = 0.
+	* position[i] + 1 or position[i] - 1 with cost = 1.
+	Return the minimum cost needed to move all the chips to the same position.
+
+	Example 1:
+	Input: position = [1,2,3]
+	Output: 1
+	Explanation: First step: Move the chip at position 3 to position 1 with 
+	             cost = 0. Second step: Move the chip at position 2 to position 
+	             1 with cost = 1. Total cost is 1.
+	
+	Example 2:
+	Input: position = [2,2,2,3,3]
+	Output: 2
+	Explanation: We can move the two chips at position  3 to position 2. Each 
+	             move has cost = 1. The total cost = 2.
+	
+	Example 3:
+	Input: position = [1,1000000000]
+	Output: 1
+
+	Constraints:
+	* 1 <= position.length <= 100
+	* 1 <= position[i] <= 10^9*/
+
+    int minCostToMoveChips(vector<int>& position) {
+        int odd = 0, even = 0; 
+        for (auto& x : position) 
+            if (x & 1) ++odd; 
+            else ++even; 
+        return min(odd, even); 
+    }
+
+
     /*1229. Meeting Scheduler (Medium)
 	Given the availability time slots arrays slots1 and slots2 of two people 
 	and a meeting duration duration, return the earliest time slot that works 
