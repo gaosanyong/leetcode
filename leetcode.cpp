402fa2efbf493158b1265a64bd6bdb40f6f34c8f
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-06 20:44:03 -0500

    December LeetCoding Challenge 2021-12-07 (1290)

--- leetcode.cpp
+++ leetcode.cpp
@@ -21307,6 +21307,46 @@ public:
     }
 
 
+    /*1290. Convert Binary Number in a Linked List to Integer (Easy)
+	Given head which is a reference node to a singly-linked list. The value of 
+	each node in the linked list is either 0 or 1. The linked list holds the 
+	binary representation of a number. Return the decimal value of the number 
+	in the linked list.
+
+	Example 1:
+	Input: head = [1,0,1]
+	Output: 5
+	Explanation: (101) in base 2 = (5) in base 10
+
+	Example 2:
+	Input: head = [0]
+	Output: 0
+
+	Example 3:
+	Input: head = [1]
+	Output: 1
+
+	Example 4:
+	Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+	Output: 18880
+
+	Example 5:
+	Input: head = [0,0]
+	Output: 0
+
+	Constraints:
+	* The Linked List is not empty.
+	* Number of nodes will not exceed 30.
+	* Each node's value is either 0 or 1.*/
+
+    int getDecimalValue(ListNode* head) {
+        int ans = 0;
+        for (ListNode *node = head; node; node = node->next) 
+            ans = 2*ans + node->val; 
+        return ans; 
+    }
+
+
     /*1314. Matrix Block Sum (Medium)
 	Given a m x n matrix mat and an integer k, return a matrix answer where 
 	each answer[i][j] is the sum of all elements mat[r][c] for:
--- leetcode.py
+++ leetcode.py
@@ -29517,6 +29517,46 @@ class UnionFind:
         return "Pending" if len(moves) < 9 else "Draw"
 
 
+    """1290. Convert Binary Number in a Linked List to Integer (Easy)
+	Given head which is a reference node to a singly-linked list. The value of 
+	each node in the linked list is either 0 or 1. The linked list holds the 
+	binary representation of a number. Return the decimal value of the number 
+	in the linked list.
+
+	Example 1:
+	Input: head = [1,0,1]
+	Output: 5
+	Explanation: (101) in base 2 = (5) in base 10
+
+	Example 2:
+	Input: head = [0]
+	Output: 0
+
+	Example 3:
+	Input: head = [1]
+	Output: 1
+
+	Example 4:
+	Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+	Output: 18880
+
+	Example 5:
+	Input: head = [0,0]
+	Output: 0
+
+	Constraints:
+	* The Linked List is not empty.
+	* Number of nodes will not exceed 30.
+	* Each node's value is either 0 or 1."""
+
+    def getDecimalValue(self, head: ListNode) -> int:
+        ans = 0 
+        while head: 
+            ans = 2*ans + head.val 
+            head = head.next 
+        return ans 
+
+
     """1307. Verbal Arithmetic Puzzle (Hard)
 	Given an equation, represented by words on left side and the result on 
 	right side. You need to check if the equation is solvable under the 
