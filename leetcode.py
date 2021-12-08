bd26af84264050346dae235f9cdec21a6daf1dac
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-07 20:40:10 -0500

    December LeetCoding Challenge 2021-12-08 (563)

--- leetcode.py
+++ leetcode.py
@@ -13273,9 +13273,9 @@ class Trie:
 	Output: 2
 
 	Constraints:
-	* 1 <= nums.length <= 104
-	* -107 <= nums[i] <= 107
-	* 0 <= k <= 107"""
+	* 1 <= nums.length <= 10^4
+	* -10^7 <= nums[i] <= 10^7
+	* 0 <= k <= 10^7"""
 
     def findPairs(self, nums: List[int], k: int) -> int:
         ans, seen = set(), set()
@@ -14016,6 +14016,68 @@ class Trie:
                     else: rows[i] = cols[j] = diag[j-i+m-1] = anti[i+j] = 0
                     ans = max(ans, rows[i], cols[j], diag[j-i+m-1], anti[i+j])
         return ans 
+
+
+    """563. Binary Tree Tilt (Easy)
+	Given the root of a binary tree, return the sum of every tree node's tilt.
+	The tilt of a tree node is the absolute difference between the sum of all 
+	left subtree node values and all right subtree node values. If a node does 
+	not have a left child, then the sum of the left subtree node values is 
+	treated as 0. The rule is similar if there the node does not have a right 
+	child.
+
+	Example 1:
+	Input: root = [1,2,3]
+	Output: 1
+	Explanation: Tilt of node 2 : |0-0| = 0 (no children)
+	             Tilt of node 3 : |0-0| = 0 (no children)
+	             Tilt of node 1 : |2-3| = 1 (left subtree is just left child, 
+	             so sum is 2; right subtree is just right child, so sum is 3)
+	             Sum of every tilt : 0 + 0 + 1 = 1
+	
+	Example 2:
+	Input: root = [4,2,9,3,5,null,7]
+	Output: 15
+	Explanation: Tilt of node 3 : |0-0| = 0 (no children)
+	             Tilt of node 5 : |0-0| = 0 (no children)
+	             Tilt of node 7 : |0-0| = 0 (no children)
+	             Tilt of node 2 : |3-5| = 2 (left subtree is just left child, 
+	             so sum is 3; right subtree is just right child, so sum is 5)
+	             Tilt of node 9 : |0-7| = 7 (no left child, so sum is 0; right 
+	             subtree is just right child, so sum is 7)
+	             Tilt of node 4 : |(3+5+2)-(9+7)| = |10-16| = 6 (left subtree 
+	             values are 3, 5, and 2, which sums to 10; right subtree values 
+	             are 9 and 7, which sums to 16)
+	             Sum of every tilt : 0 + 0 + 0 + 2 + 7 + 6 = 15
+	
+	Example 3:
+	Input: root = [21,7,14,1,1,2,2,3,3]
+	Output: 9
+
+	Constraints:
+	* The number of nodes in the tree is in the range [0, 10^4].
+	* -1000 <= Node.val <= 1000"""
+
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        mp = {None: 0}
+        ans = 0 
+        prev = None
+        node = root
+        stack = []
+        while node or stack: 
+            if node: 
+                stack.append(node)
+                node = node.left
+            else: 
+                node = stack[-1]
+                if node.right and prev != node.right: node = node.right 
+                else: 
+                    mp[node] = node.val + mp[node.left] + mp[node.right]
+                    ans += abs(mp[node.left] - mp[node.right])
+                    stack.pop()
+                    prev = node 
+                    node = None 
+        return ans
 
 
     """565. Array Nesting (Medium)
