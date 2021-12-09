08d65ce1d3929f99f55b0674f9a3230e53e3c9c3
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-08 23:05:20 -0500

    December LeetCoding Challenge 2021-12-09 (1306)

--- leetcode.cpp
+++ leetcode.cpp
@@ -21471,6 +21471,51 @@ public:
     }
 
 
+    /*1306. Jump Game III (Medium)
+	Given an array of non-negative integers arr, you are initially positioned 
+	at start index of the array. When you are at index i, you can jump to 
+	i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+	Notice that you can not jump outside of the array at any time.
+
+	Example 1:
+	Input: arr = [4,2,3,0,3,1,2], start = 5
+	Output: true
+	Explanation: All possible ways to reach at index 3 with value 0 are: 
+	             index 5 -> index 4 -> index 1 -> index 3 
+	             index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+	
+	Example 2:
+	Input: arr = [4,2,3,0,3,1,2], start = 0
+	Output: true 
+	Explanation: One possible way to reach at index 3 with value 0 is: 
+	             index 0 -> index 4 -> index 1 -> index 3
+	
+	Example 3:
+	Input: arr = [3,0,2,1,2], start = 2
+	Output: false
+	Explanation: There is no way to reach at index 1 with value 0.
+
+	Constraints:
+	* 1 <= arr.length <= 5 * 10^4
+	* 0 <= arr[i] < arr.length
+	* 0 <= start < arr.length*/
+
+    bool canReach(vector<int>& arr, int start) {
+        stack<int> stk; stk.push(start); 
+        arr[start] *= -1; 
+        while (stk.size()) {
+            int i = stk.top(); stk.pop(); 
+            if (arr[i] == 0) return true; 
+            for (auto& ii : {i - arr[i], i + arr[i]}) 
+                if (0 <= ii && ii < arr.size() && arr[ii] >= 0) {
+                    stk.push(ii); 
+                    arr[ii] *= -1; 
+                }
+        }
+        return false; 
+    }
+
+
     /*1314. Matrix Block Sum (Medium)
 	Given a m x n matrix mat and an integer k, return a matrix answer where 
 	each answer[i][j] is the sum of all elements mat[r][c] for:
--- leetcode.py
+++ leetcode.py
@@ -18687,7 +18687,7 @@ class Trie:
                 x = mp[board[i][j]]
                 diff += x
                 freq[i] += x
-                freq[3+j] += x
+                freq[j+3] += x
                 if i == j: freq[6] += x
                 if i+j == 2: freq[7] += x
         xwin = 3 in freq 
@@ -29674,6 +29674,48 @@ class UnionFind:
             ans = 2*ans + head.val 
             head = head.next 
         return ans 
+
+
+    """1306. Jump Game III (Medium)
+	Given an array of non-negative integers arr, you are initially positioned 
+	at start index of the array. When you are at index i, you can jump to 
+	i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+	Notice that you can not jump outside of the array at any time.
+
+	Example 1:
+	Input: arr = [4,2,3,0,3,1,2], start = 5
+	Output: true
+	Explanation: All possible ways to reach at index 3 with value 0 are: 
+	             index 5 -> index 4 -> index 1 -> index 3 
+	             index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+	
+	Example 2:
+	Input: arr = [4,2,3,0,3,1,2], start = 0
+	Output: true 
+	Explanation: One possible way to reach at index 3 with value 0 is: 
+	             index 0 -> index 4 -> index 1 -> index 3
+	
+	Example 3:
+	Input: arr = [3,0,2,1,2], start = 2
+	Output: false
+	Explanation: There is no way to reach at index 1 with value 0.
+
+	Constraints:
+	* 1 <= arr.length <= 5 * 10^4
+	* 0 <= arr[i] < arr.length
+	* 0 <= start < arr.length"""
+
+    def canReach(self, arr: List[int], start: int) -> bool:
+        stack = [start]
+        arr[start] *= -1 # mark "visited"
+        while stack: 
+            i = stack.pop()
+            if arr[i] == 0: return True 
+            for ii in i - arr[i], i + arr[i]: 
+                if 0 <= ii < len(arr) and arr[ii] >= 0: 
+                    stack.append(ii)
+                    arr[ii] *= -1 
+        return False 
 
 
     """1307. Verbal Arithmetic Puzzle (Hard)
