c40cae608e37dfd204ae3559d5a4df85b716dddd
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-15 22:29:35 -0500

    December LeetCoding Challenge 2021-12-16 (310)

--- leetcode.cpp
+++ leetcode.cpp
@@ -6262,6 +6262,82 @@ public:
     }
 
 
+    /*310. Minimum Height Trees (Medium)
+	A tree is an undirected graph in which any two vertices are connected by 
+	exactly one path. In other words, any connected graph without simple cycles 
+	is a tree. Given a tree of n nodes labelled from 0 to n - 1, and an array 
+	of n - 1 edges where edges[i] = [ai, bi] indicates that there is an 
+	undirected edge between the two nodes ai and bi in the tree, you can choose 
+	any node of the tree as the root. When you select a node x as the root, the 
+	result tree has height h. Among all possible rooted trees, those with 
+	minimum height (i.e. min(h))  are called minimum height trees (MHTs). 
+	Return a list of all MHTs' root labels. You can return the answer in any 
+	order. The height of a rooted tree is the number of edges on the longest 
+	downward path between the root and a leaf.
+
+	Example 1:
+	Input: n = 4, edges = [[1,0],[1,2],[1,3]]
+	Output: [1]
+	Explanation: As shown, the height of the tree is 1 when the root is the 
+	             node with label 1 which is the only MHT.
+	
+	Example 2:
+	Input: n = 6, edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
+	Output: [3,4]
+
+	Example 3:
+	Input: n = 1, edges = []
+	Output: [0]
+
+	Example 4:
+	Input: n = 2, edges = [[0,1]]
+	Output: [0,1]
+
+	Constraints:
+	* 1 <= n <= 2 * 10^4
+	* edges.length == n - 1
+	* 0 <= ai, bi < n
+	* ai != bi
+	* All the pairs (ai, bi) are distinct.
+	* The given input is guaranteed to be a tree and there will be no repeated edges.*/
+
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> graph(n); 
+        for (auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]); 
+            graph[edge[1]].push_back(edge[0]); 
+        }
+        
+        auto fn = [&](int x) {
+            queue<int> q; q.push(x); 
+            vector<int> trace(n, -1); 
+            trace[x] = -2; 
+            while (q.size()) {
+                x = q.front(); q.pop(); 
+                for (auto& xx : graph[x]) 
+                    if (trace[xx] == -1) {
+                        q.push(xx); 
+                        trace[xx] = x; 
+                    }
+            }
+            return make_pair(x, trace); 
+        }; 
+        
+        auto [x, trace] = fn(0); 
+        tie(x, trace) = fn(x); 
+        
+        vector<int> vals; 
+        while (x >= 0) {
+            vals.push_back(x); 
+            x = trace[x]; 
+        }
+        
+        int k = vals.size(); 
+        auto it = vals.cbegin(); 
+        return vector<int>(it+(k-1)/2, it+k/2+1); 
+    }
+
+
     /*318. Maximum Product of Word Lengths (Medium)
 	Given a string array words, return the maximum value of 
 	length(word[i]) * length(word[j]) where the two words do not share common 
--- leetcode.py
+++ leetcode.py
@@ -8937,6 +8937,74 @@ class Solution:
         return sell
 
 
+    """310. Minimum Height Trees (Medium)
+	A tree is an undirected graph in which any two vertices are connected by 
+	exactly one path. In other words, any connected graph without simple cycles 
+	is a tree. Given a tree of n nodes labelled from 0 to n - 1, and an array 
+	of n - 1 edges where edges[i] = [ai, bi] indicates that there is an 
+	undirected edge between the two nodes ai and bi in the tree, you can choose 
+	any node of the tree as the root. When you select a node x as the root, the 
+	result tree has height h. Among all possible rooted trees, those with 
+	minimum height (i.e. min(h))  are called minimum height trees (MHTs). 
+	Return a list of all MHTs' root labels. You can return the answer in any 
+	order. The height of a rooted tree is the number of edges on the longest 
+	downward path between the root and a leaf.
+
+	Example 1:
+	Input: n = 4, edges = [[1,0],[1,2],[1,3]]
+	Output: [1]
+	Explanation: As shown, the height of the tree is 1 when the root is the 
+	             node with label 1 which is the only MHT.
+	
+	Example 2:
+	Input: n = 6, edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
+	Output: [3,4]
+
+	Example 3:
+	Input: n = 1, edges = []
+	Output: [0]
+
+	Example 4:
+	Input: n = 2, edges = [[0,1]]
+	Output: [0,1]
+
+	Constraints:
+	* 1 <= n <= 2 * 10^4
+	* edges.length == n - 1
+	* 0 <= ai, bi < n
+	* ai != bi
+	* All the pairs (ai, bi) are distinct.
+	* The given input is guaranteed to be a tree and there will be no repeated edges."""
+
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(n)]
+        for u, v in edges: 
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        def fn(x): 
+            """Return most distant node and trace."""
+            queue = deque([x])
+            trace = [-1] * n
+            trace[x] = -2 
+            while queue: 
+                u = queue.popleft()
+                for v in graph[u]: 
+                    if trace[v] == -1: 
+                        queue.append(v)
+                        trace[v] = u
+            return u, trace 
+        
+        x, _ = fn(0)
+        x, trace = fn(x)
+        vals = []
+        while x >= 0: 
+            vals.append(x)
+            x = trace[x]
+        k = len(vals)
+        return vals[(k-1)//2 : k//2+1]
+
+
     """311. Sparse Matrix Multiplication (Medium)
 	Given two sparse matrices A and B, return the result of AB. You may assume 
 	that A's column number is equal to B's row number.
