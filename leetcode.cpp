228c58d3ac62aff6c81ce0fda437403d302439c1
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-17 14:25:55 -0500

    solve'em all (1518)

--- leetcode.cpp
+++ leetcode.cpp
@@ -22595,6 +22595,48 @@ public:
     }
 
 
+    /*1518. Water Bottles (Easy)
+	Given numBottles full water bottles, you can exchange numExchange empty 
+	water bottles for one full water bottle. The operation of drinking a full 
+	water bottle turns it into an empty bottle. Return the maximum number of 
+	water bottles you can drink.
+
+	Example 1:
+	Input: numBottles = 9, numExchange = 3
+	Output: 13
+	Explanation: You can exchange 3 empty bottles to get 1 full water bottle.
+	             Number of water bottles you can drink: 9 + 3 + 1 = 13.
+	
+	Example 2:
+	Input: numBottles = 15, numExchange = 4
+	Output: 19
+	Explanation: You can exchange 4 empty bottles to get 1 full water bottle. 
+	             Number of water bottles you can drink: 15 + 3 + 1 = 19.
+	
+	Example 3:
+	Input: numBottles = 5, numExchange = 5
+	Output: 6
+
+	Example 4:
+	Input: numBottles = 2, numExchange = 3
+	Output: 2
+
+	Constraints:
+	* 1 <= numBottles <= 100
+	* 2 <= numExchange <= 100*/
+
+    int numWaterBottles(int numBottles, int numExchange) {
+        int ans = 0, empty = 0; 
+        while (numBottles) {
+            ans += numBottles; 
+            numBottles += empty; 
+            empty = numBottles % numExchange; 
+            numBottles = numBottles / numExchange; 
+        }
+        return ans; 
+    }
+
+
     /*1522. Diameter of N-Ary Tree (Medium)
 	Given a root of an N-ary tree, you need to compute the length of the 
 	diameter of the tree. The diameter of an N-ary tree is the length of the 
