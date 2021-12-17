82d6f0f9b23df07eea6608042d8291247ca87cda
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-17 14:18:35 -0500

    solve'em all (419)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7657,6 +7657,41 @@ public:
     }
 
 
+    /*419. Battleships in a Board (Medium)
+	Given an m x n matrix board where each cell is a battleship 'X' or empty 
+	'.', return the number of the battleships on board. Battleships can only be 
+	placed horizontally or vertically on board. In other words, they can only 
+	be made of the shape 1 x k (1 row, k columns) or k x 1 (k rows, 1 column), 
+	where k can be of any size. At least one horizontal or vertical cell 
+	separates between two battleships (i.e., there are no adjacent battleships).
+
+	Example 1:
+	Input: board = [["X",".",".","X"],[".",".",".","X"],[".",".",".","X"]]
+	Output: 2
+
+	Example 2:
+	Input: board = [["."]]
+	Output: 0
+
+	Constraints:
+	* m == board.length
+	* n == board[i].length
+	* 1 <= m, n <= 200
+	* board[i][j] is either '.' or 'X'.
+
+	Follow up: Could you do it in one-pass, using only O(1) extra memory and 
+	           without modifying the values board?*/
+
+    int countBattleships(vector<vector<char>>& board) {
+        int ans = 0, m = board.size(), n = board[0].size(); 
+        for (int i = 0; i < m; ++i) 
+            for (int j = 0; j < n; ++j) 
+                if (board[i][j] == 'X' && (i == 0 || board[i-1][j] == '.') && (j == 0 || board[i][j-1] == '.')) 
+                    ++ans; 
+        return ans; 
+    }
+
+
     /*429. N-ary Tree Level Order Traversal (Medium)
 	Given an n-ary tree, return the level order traversal of its nodes' values. 
 	Nary-Tree input serialization is represented in their level order traversal, 
--- leetcode.py
+++ leetcode.py
@@ -11520,6 +11520,41 @@ class Solution:
         return ans // len(ss)
 
 
+    """419. Battleships in a Board (Medium)
+	Given an m x n matrix board where each cell is a battleship 'X' or empty 
+	'.', return the number of the battleships on board. Battleships can only be 
+	placed horizontally or vertically on board. In other words, they can only 
+	be made of the shape 1 x k (1 row, k columns) or k x 1 (k rows, 1 column), 
+	where k can be of any size. At least one horizontal or vertical cell 
+	separates between two battleships (i.e., there are no adjacent battleships).
+
+	Example 1:
+	Input: board = [["X",".",".","X"],[".",".",".","X"],[".",".",".","X"]]
+	Output: 2
+
+	Example 2:
+	Input: board = [["."]]
+	Output: 0
+
+	Constraints:
+	* m == board.length
+	* n == board[i].length
+	* 1 <= m, n <= 200
+	* board[i][j] is either '.' or 'X'.
+
+	Follow up: Could you do it in one-pass, using only O(1) extra memory and 
+	           without modifying the values board?"""
+
+    def countBattleships(self, board: List[List[str]]) -> int:
+        m, n = len(board), len(board[0])
+        ans = 0 
+        for i in range(m): 
+            for j in range(n): 
+                if board[i][j] == 'X' and (i == 0 or board[i-1][j] == '.') and (j == 0 or board[i][j-1] == '.'): 
+                    ans += 1
+        return ans 
+
+
     """422. Valid Word Square (Easy)
 	Given a sequence of words, check whether it forms a valid word square. A 
 	sequence of words forms a valid word square if the kth row and column read 
