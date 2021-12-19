55c6a88797eef9ac745a3dbbff821a2aac735a70
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-19 13:07:13 -0500

    weekly 272 (2108|2109|2010|2011)

--- leetcode.cpp
+++ leetcode.cpp
@@ -36879,6 +36879,189 @@ public:
         }
         return ans; 
     }
+
+
+    /*2108. Find First Palindromic String in the Array (Easy)
+	Given an array of strings words, return the first palindromic string in the 
+	array. If there is no such string, return an empty string "". A string is 
+	palindromic if it reads the same forward and backward.
+
+	Example 1:
+	Input: words = ["abc","car","ada","racecar","cool"]
+	Output: "ada"
+	Explanation: The first string that is palindromic is "ada". Note that 
+	             "racecar" is also palindromic, but it is not the first.
+	
+	Example 2:
+	Input: words = ["notapalindrome","racecar"]
+	Output: "racecar"
+	Explanation: The first and only string that is palindromic is "racecar".
+
+	Example 3:
+	Input: words = ["def","ghi"]
+	Output: ""
+	Explanation: There are no palindromic strings, so the empty string is returned.
+
+	Constraints:
+	* 1 <= words.length <= 100
+	* 1 <= words[i].length <= 100
+	* words[i] consists only of lowercase English letters.*/
+
+    string firstPalindrome(vector<string>& words) {
+        for (auto& word : words) 
+            if (word == string(word.rbegin(), word.rend())) return word; 
+        return ""; 
+    }
+
+
+    /*2109. Adding Spaces to a String (Medium)
+	You are given a 0-indexed string s and a 0-indexed integer array spaces 
+	that describes the indices in the original string where spaces will be 
+	added. Each space should be inserted before the character at the given 
+	index. For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we 
+	place spaces before 'Y' and 'C', which are at indices 5 and 9 respectively. 
+	Thus, we obtain "Enjoy Your Coffee". Return the modified string after the 
+	spaces have been added.
+
+	Example 1:
+	Input: s = "LeetcodeHelpsMeLearn", spaces = [8,13,15]
+	Output: "Leetcode Helps Me Learn"
+	Explanation: The indices 8, 13, and 15 correspond to the underlined 
+	             characters in "LeetcodeHelpsMeLearn". We then place spaces 
+	             before those characters.
+	
+	Example 2:
+	Input: s = "icodeinpython", spaces = [1,5,7,9]
+	Output: "i code in py thon"
+	Explanation: The indices 1, 5, 7, and 9 correspond to the underlined 
+	             characters in "icodeinpython". We then place spaces before 
+	             those characters.
+	
+	Example 3:
+	Input: s = "spacing", spaces = [0,1,2,3,4,5,6]
+	Output: " s p a c i n g"
+	Explanation: We are also able to place spaces before the first character 
+	             of the string.
+
+	Constraints:
+	* 1 <= s.length <= 3 * 10^5
+	* s consists only of lowercase and uppercase English letters.
+	* 1 <= spaces.length <= 3 * 10^5
+	* 0 <= spaces[i] <= s.length - 1
+	* All the values of spaces are strictly increasing.*/
+
+    string addSpaces(string s, vector<int>& spaces) {
+        string ans; 
+        for (int i = 0, j = 0; i < s.size(); ++i) {
+            if (j < spaces.size() && i == spaces[j]) {
+                ans.push_back(' '); 
+                ++j; 
+            }
+            ans.push_back(s[i]); 
+        }
+        return ans; 
+    }
+
+
+    /*2110. Number of Smooth Descent Periods of a Stock (Medium)
+	You are given an integer array prices representing the daily price history 
+	of a stock, where prices[i] is the stock price on the ith day. A smooth 
+	descent period of a stock consists of one or more contiguous days such that 
+	the price on each day is lower than the price on the preceding day by 
+	exactly 1. The first day of the period is exempted from this rule. Return 
+	the number of smooth descent periods.
+
+	Example 1:
+	Input: prices = [3,2,1,4]
+	Output: 7
+	Explanation: There are 7 smooth descent periods: [3], [2], [1], [4], [3,2], 
+	             [2,1], and [3,2,1]. Note that a period with one day is a 
+	             smooth descent period by the definition.
+	
+	Example 2:
+	Input: prices = [8,6,7,7]
+	Output: 4
+	Explanation: There are 4 smooth descent periods: [8], [6], [7], and [7]. 
+	             Note that [8,6] is not a smooth descent period as 8 - 6 ≠ 1.
+	
+	Example 3:
+	Input: prices = [1]
+	Output: 1
+	Explanation: There is 1 smooth descent period: [1]
+
+	Constraints:
+	* 1 <= prices.length <= 10^5
+	* 1 <= prices[i] <= 10^5*/
+
+    long long getDescentPeriods(vector<int>& prices) {
+        long long ans = 0; 
+        for (int i = 0, cnt = 0; i < prices.size(); ++i) {
+            if (i && prices[i-1] != prices[i]+1) cnt = 0; 
+            ans += ++cnt; 
+        }
+        return ans; 
+    }
+
+
+    /*2111. Minimum Operations to Make the Array K-Increasing (Hard)
+	You are given a 0-indexed array arr consisting of n positive integers, and 
+	a positive integer k. The array arr is called K-increasing if 
+	arr[i-k] <= arr[i] holds for every index i, where k <= i <= n-1. 
+	* For example, arr = [4, 1, 5, 2, 6, 2] is K-increasing for k = 2 because:
+	  o arr[0] <= arr[2] (4 <= 5)
+	  o arr[1] <= arr[3] (1 <= 2)
+	  o arr[2] <= arr[4] (5 <= 6)
+	  o arr[3] <= arr[5] (2 <= 2)
+	* However, the same arr is not K-increasing for k = 1 (because 
+	  arr[0] > arr[1]) or k = 3 (because arr[0] > arr[3]).
+	In one operation, you can choose an index i and change arr[i] into any 
+	positive integer. Return the minimum number of operations required to make 
+	the array K-increasing for the given k.
+
+	Example 1:
+	Input: arr = [5,4,3,2,1], k = 1
+	Output: 4
+	Explanation: For k = 1, the resultant array has to be non-decreasing. Some 
+	             of the K-increasing arrays that can be formed are [5,6,7,8,9], 
+	             [1,1,1,1,1], [2,2,3,4,4]. All of them require 4 operations. It 
+	             is suboptimal to change the array to, for example, [6,7,8,9,10] 
+	             because it would take 5 operations. It can be shown that we 
+	             cannot make the array K-increasing in less than 4 operations.
+	
+	Example 2:
+	Input: arr = [4,1,5,2,6,2], k = 2
+	Output: 0
+	Explanation: This is the same example as the one in the problem description. 
+	             Here, for every index i where 2 <= i <= 5, arr[i-2] <= arr[i].
+	             Since the given array is already K-increasing, we do not need 
+	             to perform any operations.
+	
+	Example 3:
+	Input: arr = [4,1,5,2,6,2], k = 3
+	Output: 2
+	Explanation: Indices 3 and 5 are the only ones not satisfying 
+	             arr[i-3] <= arr[i] for 3 <= i <= 5. One of the ways we can 
+	             make the array K-increasing is by changing arr[3] to 4 and 
+	             arr[5] to 5. The array will now be [4,1,5,4,6,5]. Note that 
+	             there can be other ways to make the array K-increasing, but 
+	             none of them require less than 2 operations.
+
+	Constraints:
+	* 1 <= arr.length <= 10^5
+	* 1 <= arr[i], k <= arr.length*/
+
+    int kIncreasing(vector<int>& arr, int k) {
+        int ans = 0; 
+        for (int i = 0; i < k; ++i) {
+            vector<int> vals; 
+            for (int ii = i; ii < arr.size(); ii += k) {
+                if (vals.empty() || vals.back() <= arr[ii]) vals.push_back(arr[ii]); 
+                else *upper_bound(vals.begin(), vals.end(), arr[ii]) = arr[ii]; 
+            }
+            ans += vals.size(); 
+        }
+        return arr.size() - ans; 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -53441,6 +53441,179 @@ class Trie:
         return ans 
 
 
+    """2108. Find First Palindromic String in the Array (Easy)
+	Given an array of strings words, return the first palindromic string in the 
+	array. If there is no such string, return an empty string "". A string is 
+	palindromic if it reads the same forward and backward.
+
+	Example 1:
+	Input: words = ["abc","car","ada","racecar","cool"]
+	Output: "ada"
+	Explanation: The first string that is palindromic is "ada". Note that 
+	             "racecar" is also palindromic, but it is not the first.
+	
+	Example 2:
+	Input: words = ["notapalindrome","racecar"]
+	Output: "racecar"
+	Explanation: The first and only string that is palindromic is "racecar".
+
+	Example 3:
+	Input: words = ["def","ghi"]
+	Output: ""
+	Explanation: There are no palindromic strings, so the empty string is returned.
+
+	Constraints:
+	* 1 <= words.length <= 100
+	* 1 <= words[i].length <= 100
+	* words[i] consists only of lowercase English letters."""
+
+    def firstPalindrome(self, words: List[str]) -> str:
+        return next((word for word in words if word == word[::-1]), "")
+
+
+    """2109. Adding Spaces to a String (Medium)
+	You are given a 0-indexed string s and a 0-indexed integer array spaces 
+	that describes the indices in the original string where spaces will be 
+	added. Each space should be inserted before the character at the given 
+	index. For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we 
+	place spaces before 'Y' and 'C', which are at indices 5 and 9 respectively. 
+	Thus, we obtain "Enjoy Your Coffee". Return the modified string after the 
+	spaces have been added.
+
+	Example 1:
+	Input: s = "LeetcodeHelpsMeLearn", spaces = [8,13,15]
+	Output: "Leetcode Helps Me Learn"
+	Explanation: The indices 8, 13, and 15 correspond to the underlined 
+	             characters in "LeetcodeHelpsMeLearn". We then place spaces 
+	             before those characters.
+	
+	Example 2:
+	Input: s = "icodeinpython", spaces = [1,5,7,9]
+	Output: "i code in py thon"
+	Explanation: The indices 1, 5, 7, and 9 correspond to the underlined 
+	             characters in "icodeinpython". We then place spaces before 
+	             those characters.
+	
+	Example 3:
+	Input: s = "spacing", spaces = [0,1,2,3,4,5,6]
+	Output: " s p a c i n g"
+	Explanation: We are also able to place spaces before the first character 
+	             of the string.
+
+	Constraints:
+	* 1 <= s.length <= 3 * 10^5
+	* s consists only of lowercase and uppercase English letters.
+	* 1 <= spaces.length <= 3 * 10^5
+	* 0 <= spaces[i] <= s.length - 1
+	* All the values of spaces are strictly increasing."""
+
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        ans = []
+        for i in reversed(range(len(s))): 
+            ans.append(s[i])
+            if spaces and spaces[-1] == i: 
+                ans.append(' ')
+                spaces.pop()
+        return "".join(reversed(ans))
+
+
+    """2110. Number of Smooth Descent Periods of a Stock (Medium)
+	You are given an integer array prices representing the daily price history 
+	of a stock, where prices[i] is the stock price on the ith day. A smooth 
+	descent period of a stock consists of one or more contiguous days such that 
+	the price on each day is lower than the price on the preceding day by 
+	exactly 1. The first day of the period is exempted from this rule. Return 
+	the number of smooth descent periods.
+
+	Example 1:
+	Input: prices = [3,2,1,4]
+	Output: 7
+	Explanation: There are 7 smooth descent periods: [3], [2], [1], [4], [3,2], 
+	             [2,1], and [3,2,1]. Note that a period with one day is a 
+	             smooth descent period by the definition.
+	
+	Example 2:
+	Input: prices = [8,6,7,7]
+	Output: 4
+	Explanation: There are 4 smooth descent periods: [8], [6], [7], and [7]. 
+	             Note that [8,6] is not a smooth descent period as 8 - 6 ≠ 1.
+	
+	Example 3:
+	Input: prices = [1]
+	Output: 1
+	Explanation: There is 1 smooth descent period: [1]
+
+	Constraints:
+	* 1 <= prices.length <= 10^5
+	* 1 <= prices[i] <= 10^5"""
+
+    def getDescentPeriods(self, prices: List[int]) -> int:
+        ans = 0 
+        for i, x in enumerate(prices): 
+            if i == 0 or prices[i-1] != x + 1: cnt = 0
+            cnt += 1
+            ans += cnt 
+        return ans 
+
+
+    """2111. Minimum Operations to Make the Array K-Increasing (Hard)
+	You are given a 0-indexed array arr consisting of n positive integers, and 
+	a positive integer k. The array arr is called K-increasing if 
+	arr[i-k] <= arr[i] holds for every index i, where k <= i <= n-1. 
+	* For example, arr = [4, 1, 5, 2, 6, 2] is K-increasing for k = 2 because:
+	  o arr[0] <= arr[2] (4 <= 5)
+	  o arr[1] <= arr[3] (1 <= 2)
+	  o arr[2] <= arr[4] (5 <= 6)
+	  o arr[3] <= arr[5] (2 <= 2)
+	* However, the same arr is not K-increasing for k = 1 (because 
+	  arr[0] > arr[1]) or k = 3 (because arr[0] > arr[3]).
+	In one operation, you can choose an index i and change arr[i] into any 
+	positive integer. Return the minimum number of operations required to make 
+	the array K-increasing for the given k.
+
+	Example 1:
+	Input: arr = [5,4,3,2,1], k = 1
+	Output: 4
+	Explanation: For k = 1, the resultant array has to be non-decreasing. Some 
+	             of the K-increasing arrays that can be formed are [5,6,7,8,9], 
+	             [1,1,1,1,1], [2,2,3,4,4]. All of them require 4 operations. It 
+	             is suboptimal to change the array to, for example, [6,7,8,9,10] 
+	             because it would take 5 operations. It can be shown that we 
+	             cannot make the array K-increasing in less than 4 operations.
+	
+	Example 2:
+	Input: arr = [4,1,5,2,6,2], k = 2
+	Output: 0
+	Explanation: This is the same example as the one in the problem description. 
+	             Here, for every index i where 2 <= i <= 5, arr[i-2] <= arr[i].
+	             Since the given array is already K-increasing, we do not need 
+	             to perform any operations.
+	
+	Example 3:
+	Input: arr = [4,1,5,2,6,2], k = 3
+	Output: 2
+	Explanation: Indices 3 and 5 are the only ones not satisfying 
+	             arr[i-3] <= arr[i] for 3 <= i <= 5. One of the ways we can 
+	             make the array K-increasing is by changing arr[3] to 4 and 
+	             arr[5] to 5. The array will now be [4,1,5,4,6,5]. Note that 
+	             there can be other ways to make the array K-increasing, but 
+	             none of them require less than 2 operations.
+
+	Constraints:
+	* 1 <= arr.length <= 10^5
+	* 1 <= arr[i], k <= arr.length"""
+
+    def kIncreasing(self, arr: List[int], k: int) -> int:
+        ans = 0 
+        for _ in range(k): 
+            vals = []
+            for i in range(_, len(arr), k): 
+                if not vals or vals[-1] <= arr[i]: vals.append(arr[i])
+                else: vals[bisect_right(vals, arr[i])] = arr[i]
+            ans += len(vals)
+        return len(arr) - ans
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
