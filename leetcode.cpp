8556040179569077ce852c91db1c311f68801061
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-19 21:17:40 -0500

    solve'em all (475)

--- leetcode.cpp
+++ leetcode.cpp
@@ -8508,6 +8508,51 @@ public:
     }
 
 
+    /*475. Heaters (Medium)
+	Winter is coming! During the contest, your first job is to design a 
+	standard heater with a fixed warm radius to warm all the houses. Every 
+	house can be warmed, as long as the house is within the heater's warm 
+	radius range. Given the positions of houses and heaters on a horizontal 
+	line, return the minimum radius standard of heaters so that those heaters 
+	could cover all houses. Notice that all the heaters follow your radius 
+	standard, and the warm radius will the same.
+
+	Example 1:
+	Input: houses = [1,2,3], heaters = [2]
+	Output: 1
+	Explanation: The only heater was placed in the position 2, and if we use 
+	             the radius 1 standard, then all the houses can be warmed.
+	
+	Example 2:
+	Input: houses = [1,2,3,4], heaters = [1,4]
+	Output: 1
+	Explanation: The two heater was placed in the position 1 and 4. We need to 
+	             use radius 1 standard, then all the houses can be warmed.
+	
+	Example 3:
+	Input: houses = [1,5], heaters = [2]
+	Output: 3
+
+	Constraints:
+	* 1 <= houses.length, heaters.length <= 3 * 10^4
+	* 1 <= houses[i], heaters[i] <= 10^9*/
+
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(houses.begin(), houses.end()); 
+        sort(heaters.begin(), heaters.end()); 
+        int ans = 0; 
+        auto it = heaters.begin(); 
+        for (auto& x : houses) {
+            for (; it != heaters.end() && *it < x; ++it); 
+            int cand = INT_MAX; 
+            if (it != heaters.end()) cand = min(cand, *it - x); 
+            if (it != heaters.begin()) cand = min(cand, x - *prev(it)); 
+            ans = max(ans, cand); 
+        }
+        return ans; 
+    }
+
+
     /*483. Smallest Good Base (Hard)
 	Given an integer n represented as a string, return the smallest good base 
 	of n. We call k >= 2 a good base of n, if all digits of n base k are 1's.
--- leetcode.py
+++ leetcode.py
@@ -12367,6 +12367,47 @@ class Trie:
         return ans
 
 
+    """475. Heaters (Medium)
+	Winter is coming! During the contest, your first job is to design a 
+	standard heater with a fixed warm radius to warm all the houses. Every 
+	house can be warmed, as long as the house is within the heater's warm 
+	radius range. Given the positions of houses and heaters on a horizontal 
+	line, return the minimum radius standard of heaters so that those heaters 
+	could cover all houses. Notice that all the heaters follow your radius 
+	standard, and the warm radius will the same.
+
+	Example 1:
+	Input: houses = [1,2,3], heaters = [2]
+	Output: 1
+	Explanation: The only heater was placed in the position 2, and if we use 
+	             the radius 1 standard, then all the houses can be warmed.
+	
+	Example 2:
+	Input: houses = [1,2,3,4], heaters = [1,4]
+	Output: 1
+	Explanation: The two heater was placed in the position 1 and 4. We need to 
+	             use radius 1 standard, then all the houses can be warmed.
+	
+	Example 3:
+	Input: houses = [1,5], heaters = [2]
+	Output: 3
+
+	Constraints:
+	* 1 <= houses.length, heaters.length <= 3 * 10^4
+	* 1 <= houses[i], heaters[i] <= 10^9"""
+
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        heaters.sort()
+        ans = k = 0 
+        for x in sorted(houses):
+            while k < len(heaters) and heaters[k] < x: k += 1
+            cand = inf 
+            if k < len(heaters): cand = min(cand, heaters[k] - x)
+            if k: cand = min(cand, x - heaters[k-1])
+            ans = max(ans, cand)
+        return ans
+
+
     """481. Magical String (Medium)
 	A magical string S consists of only '1' and '2' and obeys the following 
 	rules:
