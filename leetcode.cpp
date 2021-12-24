d6e48cd1f1ccbc9aeee4ed010f7872de00eab205
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-24 12:04:59 -0500

    solve'em all (1609)

--- leetcode.cpp
+++ leetcode.cpp
@@ -23255,6 +23255,63 @@ public:
     }
 
 
+    /*1609. Even Odd Tree (Medium)
+	A binary tree is named Even-Odd if it meets the following conditions:
+	* The root of the binary tree is at level index 0, its children are at 
+	  level index 1, their children are at level index 2, etc.
+	* For every even-indexed level, all nodes at the level have odd integer 
+	  values in strictly increasing order (from left to right).
+	* For every odd-indexed level, all nodes at the level have even integer 
+	  values in strictly decreasing order (from left to right).
+	Given the root of a binary tree, return true if the binary tree is Even-Odd, 
+	otherwise return false.
+
+	Example 1:
+	Input: root = [1,10,4,3,null,7,9,12,8,6,null,null,2]
+	Output: true
+	Explanation: The node values on each level are:
+	             Level 0: [1]
+	             Level 1: [10,4]
+	             Level 2: [3,7,9]
+	             Level 3: [12,8,6,2]
+	             Since levels 0 and 2 are all odd and increasing and levels 1 
+	             and 3 are all even and decreasing, the tree is Even-Odd.
+	
+	Example 2:
+	Input: root = [5,4,2,3,3,7]
+	Output: false
+	Explanation: The node values on each level are:
+	             Level 0: [5]
+	             Level 1: [4,2]
+	             Level 2: [3,3,7]
+	             Node values in level 2 must be in strictly increasing order, 
+	             so the tree is not Even-Odd.
+	
+	Example 3:
+	Input: root = [5,9,1,3,5,7]
+	Output: false
+	Explanation: Node values in the level 1 should be even integers.
+
+	Constraints:
+	* The number of nodes in the tree is in the range [1, 10^5].
+	* 1 <= Node.val <= 10^6*/
+
+    bool isEvenOddTree(TreeNode* root) {
+        deque<TreeNode*> q; q.push_back(root); 
+        for (int even = 1; q.size(); even ^= 1) {
+            int prev = even ? INT_MIN : INT_MAX; 
+            for (int sz = q.size(); sz; --sz) {
+                TreeNode* node = q.front(); q.pop_front(); 
+                if ((even && ((node->val&1) == 0 || prev >= node->val)) || (!even && (node->val&1 || prev <= node->val))) return false; 
+                prev = node->val; 
+                if (node->left) q.push_back(node->left); 
+                if (node->right) q.push_back(node->right); 
+            }
+        }
+        return true; 
+    }
+
+
     /*1612. Check If Two Expression Trees are Equivalent (Medium)
 	A binary expression tree is a kind of binary tree used to represent 
 	arithmetic expressions. Each node of a binary expression tree has either 
--- leetcode.py
+++ leetcode.py
@@ -34724,19 +34724,18 @@ class UnionFind:
 	* 1 <= Node.val <= 10^6"""
 
     def isEvenOddTree(self, root: TreeNode) -> bool:
-        queue = [root]
-        even = True # True for even level
+        even = 1 # even level 
+        queue = deque([root])
         while queue: 
             newq = []
             prev = -inf if even else inf
-            for node in queue: 
-                if not even and (node.val & 1 or prev <= node.val): return False 
-                elif even and (not (node.val & 1) or prev >= node.val): return False 
+            for _ in range(len(queue)): 
+                node = queue.popleft()
+                if even and (node.val&1 == 0 or prev >= node.val) or not even and (node.val&1 or prev <= node.val): return False 
                 prev = node.val 
-                if node.left: newq.append(node.left)
-                if node.right: newq.append(node.right)
-            queue = newq 
-            even = not even
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            even ^= 1
         return True 
 
 
