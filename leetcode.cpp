c463d554aadd81056701cef973bb1b0ae47cb900
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-26 19:13:34 -0500

    December LeetCoding Challenge 2021-12-27 (476)

--- leetcode.cpp
+++ leetcode.cpp
@@ -8815,6 +8815,36 @@ public:
     }
 
 
+    /*476. Number Complement (Easy)
+	The complement of an integer is the integer you get when you flip all the 
+	0's to 1's and all the 1's to 0's in its binary representation. For example, 
+	The integer 5 is "101" in binary and its complement is "010" which is the 
+	integer 2. Given an integer num, return its complement.
+
+	Example 1:
+	Input: num = 5
+	Output: 2
+	Explanation: The binary representation of 5 is 101 (no leading zero bits), 
+	             and its complement is 010. So you need to output 2.
+	
+	Example 2:
+	Input: num = 1
+	Output: 0
+	Explanation: The binary representation of 1 is 1 (no leading zero bits), 
+	             and its complement is 0. So you need to output 0.
+
+	Constraints: 1 <= num < 2^31
+
+	Note: This question is the same as 1009: 
+	      https://leetcode.com/problems/complement-of-base-10-integer/*/
+
+    int findComplement(int num) {
+        int mask = 0; 
+        for (; mask < num; mask = mask << 1 | 1); 
+        return num ^ mask; 
+    }
+
+
     /*483. Smallest Good Base (Hard)
 	Given an integer n represented as a string, return the smallest good base 
 	of n. We call k >= 2 a good base of n, if all digits of n base k are 1's.
--- leetcode.py
+++ leetcode.py
@@ -12397,6 +12397,33 @@ class Trie:
         return ans
 
 
+    """476. Number Complement (Easy)
+	The complement of an integer is the integer you get when you flip all the 
+	0's to 1's and all the 1's to 0's in its binary representation. For example, 
+	The integer 5 is "101" in binary and its complement is "010" which is the 
+	integer 2. Given an integer num, return its complement.
+
+	Example 1:
+	Input: num = 5
+	Output: 2
+	Explanation: The binary representation of 5 is 101 (no leading zero bits), 
+	             and its complement is 010. So you need to output 2.
+	
+	Example 2:
+	Input: num = 1
+	Output: 0
+	Explanation: The binary representation of 1 is 1 (no leading zero bits), 
+	             and its complement is 0. So you need to output 0.
+
+	Constraints: 1 <= num < 2^31
+
+	Note: This question is the same as 1009: 
+	      https://leetcode.com/problems/complement-of-base-10-integer/"""
+
+    def findComplement(self, num: int) -> int:
+        return num ^ (1 << num.bit_length()) - 1
+
+
     """481. Magical String (Medium)
 	A magical string S consists of only '1' and '2' and obeys the following 
 	rules:
