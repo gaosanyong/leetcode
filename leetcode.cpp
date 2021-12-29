d6f7ebf4be4833fba5283c2da965131d81e30b52
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-29 11:27:56 -0500

    updated 116

--- leetcode.cpp
+++ leetcode.cpp
@@ -3125,11 +3125,9 @@ public:
     Node* connect(Node* root) {
         Node* head = root; 
         while (head && head->left) {
-            Node* node = head; 
-            while (node) {
+            for (Node* node = head; node; node = node->next) {
                 node->left->next = node->right; 
                 if (node->next) node->right->next = node->next->left; 
-                node = node->next; 
             }
             head = head->left; 
         }
