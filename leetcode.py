eb8e000af43615b1d53df64048d21e404d0457a8
Ye Gao <ye_gao@mfe.berkeley.edu>
2021-12-29 12:19:52 -0500

    solve'em all (846)

--- leetcode.py
+++ leetcode.py
@@ -20290,6 +20290,41 @@ class Solution:
         return ans 
 
 
+    """846. Hand of Straights (Medium)
+	Alice has some number of cards and she wants to rearrange the cards into 
+	groups so that each group is of size groupSize, and consists of groupSize 
+	consecutive cards. Given an integer array hand where hand[i] is the value 
+	written on the ith card and an integer groupSize, return true if she can 
+	rearrange the cards, or false otherwise.
+
+	Example 1:
+	Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+	Output: true
+	Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
+
+	Example 2:
+	Input: hand = [1,2,3,4,5], groupSize = 4
+	Output: false
+	Explanation: Alice's hand can not be rearranged into groups of 4.
+
+	Constraints:
+	* 1 <= hand.length <= 10^4
+	* 0 <= hand[i] <= 10^9
+	* 1 <= groupSize <= hand.length
+
+	Note: This question is the same as 1296: 
+	      https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/"""
+
+    def isNStraightHand(self, hand: List[int], W: int) -> bool:
+        freq = Counter(hand)
+        for x in sorted(freq): 
+            if freq[x]: 
+                for dx in range(1, W): 
+                    if freq[x+dx] < freq[x]: return False 
+                    freq[x+dx] -= freq[x]
+        return True 
+
+
     """847. Shortest Path Visiting All Nodes (Hard)
 	You have an undirected, connected graph of n nodes labeled from 0 to n - 1. 
 	You are given an array graph where graph[i] is a list of all the nodes 
