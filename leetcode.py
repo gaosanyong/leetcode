18f88320ffb2c2a06e86fad46d82ebd17dadad64
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-01-01 23:37:20 -0500

    weekly 274 (2124|2125|2126|2127)

--- leetcode.py
+++ leetcode.py
@@ -54332,6 +54332,225 @@ class Trie:
                 else: return ans 
 
 
+    """2124. Check if All A's Appears Before All B's (Easy)
+	Given a string s consisting of only the characters 'a' and 'b', return true 
+	if every 'a' appears before every 'b' in the string. Otherwise, return 
+	false.
+
+	Example 1:
+	Input: s = "aaabbb"
+	Output: true
+	Explanation: The 'a's are at indices 0, 1, and 2, while the 'b's are at 
+	             indices 3, 4, and 5. Hence, every 'a' appears before every 'b' 
+	             and we return true.
+	
+	Example 2:
+	Input: s = "abab"
+	Output: false
+	Explanation: There is an 'a' at index 2 and a 'b' at index 1. Hence, not 
+	             every 'a' appears before every 'b' and we return false.
+	
+	Example 3:
+	Input: s = "bbb"
+	Output: true
+	Explanation: There are no 'a's, hence, every 'a' appears before every 'b' 
+	             and we return true.
+
+	Constraints:
+	* 1 <= s.length <= 100
+	* s[i] is either 'a' or 'b'."""
+
+    def checkString(self, s: str) -> bool:
+        return "ba" not in s
+
+
+    """2125. Number of Laser Beams in a Bank (Medium)
+	Anti-theft security devices are activated inside a bank. You are given a 0-
+	indexed binary string array bank representing the floor plan of the bank, 
+	which is an m x n 2D matrix. bank[i] represents the ith row, consisting of 
+	'0's and '1's. '0' means the cell is empty, while'1' means the cell has a 
+	security device. There is one laser beam between any two security devices 
+	if both conditions are met:
+	* The two devices are located on two different rows: r1 and r2, where 
+	  r1 < r2.
+	* For each row i where r1 < i < r2, there are no security devices in the 
+	  ith row.
+	Laser beams are independent, i.e., one beam does not interfere nor join 
+	with another. Return the total number of laser beams in the bank.
+
+	Example 1:
+	Input: bank = ["011001","000000","010100","001000"]
+	Output: 8
+	Explanation: Between each of the following device pairs, there is one beam. 
+	             In total, there are 8 beams:
+	              * bank[0][1] -- bank[2][1]
+	              * bank[0][1] -- bank[2][3]
+	              * bank[0][2] -- bank[2][1]
+	              * bank[0][2] -- bank[2][3]
+	              * bank[0][5] -- bank[2][1]
+	              * bank[0][5] -- bank[2][3]
+	              * bank[2][1] -- bank[3][2]
+	              * bank[2][3] -- bank[3][2]
+	             Note that there is no beam between any device on the 0th row 
+	             with any on the 3rd row. This is because the 2nd row contains 
+	             security devices, which breaks the second condition.
+	
+	Example 2:
+	Input: bank = ["000","111","000"]
+	Output: 0
+	Explanation: There does not exist two devices located on two different rows.
+
+	Constraints:
+	* m == bank.length
+	* n == bank[i].length
+	* 1 <= m, n <= 500
+	* bank[i][j] is either '0' or '1'."""
+
+    def numberOfBeams(self, bank: List[str]) -> int:
+        ans = prev = 0 
+        for row in bank: 
+            curr = row.count('1')
+            if curr: 
+                ans += prev * curr
+                prev = curr 
+        return ans 
+
+
+    """2126. Destroying Asteroids (Medium)
+	You are given an integer mass, which represents the original mass of a 
+	planet. You are further given an integer array asteroids, where asteroids[i] 
+	is the mass of the ith asteroid. You can arrange for the planet to collide 
+	with the asteroids in any arbitrary order. If the mass of the planet is 
+	greater than or equal to the mass of the asteroid, the asteroid is 
+	destroyed and the planet gains the mass of the asteroid. Otherwise, the 
+	planet is destroyed. Return true if all asteroids can be destroyed. 
+	Otherwise, return false.
+
+	Example 1:
+	Input: mass = 10, asteroids = [3,9,19,5,21]
+	Output: true
+	Explanation: One way to order the asteroids is [9,19,5,3,21]:
+	- The planet collides with the asteroid with a mass of 9. New planet mass: 10 + 9 = 19
+	- The planet collides with the asteroid with a mass of 19. New planet mass: 19 + 19 = 38
+	- The planet collides with the asteroid with a mass of 5. New planet mass: 38 + 5 = 43
+	- The planet collides with the asteroid with a mass of 3. New planet mass: 43 + 3 = 46
+	- The planet collides with the asteroid with a mass of 21. New planet mass: 46 + 21 = 67
+	All asteroids are destroyed.
+
+	Example 2:
+	Input: mass = 5, asteroids = [4,9,23,4]
+	Output: false
+	Explanation: The planet cannot ever gain enough mass to destroy the 
+	             asteroid with a mass of 23. After the planet destroys the 
+	             other asteroids, it will have a mass of 5 + 4 + 9 + 4 = 22.
+	             This is less than 23, so a collision would not destroy the 
+	             last asteroid.
+
+	Constraints:
+	* 1 <= mass <= 10^5
+	* 1 <= asteroids.length <= 10^5
+	* 1 <= asteroids[i] <= 10^5"""
+
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        for x in sorted(asteroids): 
+            if mass < x: return False 
+            mass += x
+        return True 
+
+
+    """2127. Maximum Employees to Be Invited to a Meeting (Hard)
+	A company is organizing a meeting and has a list of n employees, waiting 
+	to be invited. They have arranged for a large circular table, capable of 
+	seating any number of employees. The employees are numbered from 0 to n - 1. 
+	Each employee has a favorite person and they will attend the meeting only 
+	if they can sit next to their favorite person at the table. The favorite 
+	person of an employee is not themself. Given a 0-indexed integer array 
+	favorite, where favorite[i] denotes the favorite person of the ith employee, 
+	return the maximum number of employees that can be invited to the meeting.
+
+	Example 1:
+	Input: favorite = [2,2,1,2]
+	Output: 3
+	Explanation: The above figure shows how the company can invite employees 0, 
+	             1, and 2, and seat them at the round table. All employees 
+	             cannot be invited because employee 2 cannot sit beside 
+	             employees 0, 1, and 3, simultaneously. Note that the company 
+	             can also invite employees 1, 2, and 3, and give them their 
+	             desired seats. The maximum number of employees that can be 
+	             invited to the meeting is 3. 
+	
+	Example 2:
+	Input: favorite = [1,2,0]
+	Output: 3
+	Explanation: Each employee is the favorite person of at least one other 
+	             employee, and the only way the company can invite them is if 
+	             they invite every employee. The seating arrangement will be 
+	             the same as that in the figure given in example 1:
+	             - Employee 0 will sit between employees 2 and 1.
+	             - Employee 1 will sit between employees 0 and 2.
+	             - Employee 2 will sit between employees 1 and 0.
+	             The maximum number of employees that can be invited to the 
+	             meeting is 3.
+	
+	Example 3:
+	Input: favorite = [3,0,1,4,1]
+	Output: 4
+	Explanation: The above figure shows how the company will invite employees 
+	             0, 1, 3, and 4, and seat them at the round table. Employee 2 
+	             cannot be invited because the two spots next to their favorite 
+	             employee 0 are taken. So the company leaves them out of the 
+	             meeting. The maximum number of employees that can be invited 
+	             to the meeting is 4.
+
+	Constraints:
+	* n == favorite.length
+	* 2 <= n <= 10^5
+	* 0 <= favorite[i] <= n - 1
+	* favorite[i] != i"""
+
+    def maximumInvitations(self, favorite: List[int]) -> int:
+        n = len(favorite)
+        graph = [[] for _ in range(n)]
+        for i, x in enumerate(favorite): graph[x].append(i)
+        
+        def bfs(x, seen): 
+            """Return longest arm of x."""
+            ans = 0 
+            queue = deque([x])
+            while queue: 
+                for _ in range(len(queue)): 
+                    u = queue.popleft()
+                    for v in graph[u]: 
+                        if v not in seen: 
+                            seen.add(v)
+                            queue.append(v)
+                ans += 1
+            return ans 
+        
+        ans = 0 
+        seen = [False]*n
+        for i, x in enumerate(favorite): 
+            if favorite[x] == i and not seen[i]: 
+                seen[i] = seen[x] = True 
+                ans += bfs(i, {i, x}) + bfs(x, {i, x})
+                
+        dp = [0]*n
+        for i, x in enumerate(favorite): 
+            if dp[i] == 0: 
+                ii, val = i, 0
+                memo = {}
+                while ii not in memo: 
+                    if dp[ii]: 
+                        cycle = dp[ii]
+                        break
+                    memo[ii] = val
+                    val += 1
+                    ii = favorite[ii]
+                else: cycle = val - memo[ii]
+                for k in memo: dp[k] = cycle
+        return max(ans, max(dp))
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
