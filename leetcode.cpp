47e3eb9cdd631e40c017ef9fd9be14e6943f6b72
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-01-01 20:20:43 -0500

    January LeetCoding Challenge 2022-01-02 (1010)

--- leetcode.cpp
+++ leetcode.cpp
@@ -20244,6 +20244,41 @@ public:
     }
 
 
+    /*1010. Pairs of Songs With Total Durations Divisible by 60 (Medium)
+	You are given a list of songs where the ith song has a duration of time[i] 
+	seconds. Return the number of pairs of songs for which their total duration 
+	in seconds is divisible by 60. Formally, we want the number of indices i, j 
+	such that i < j with (time[i] + time[j]) % 60 == 0.
+
+	Example 1:
+	Input: time = [30,20,150,100,40]
+	Output: 3
+	Explanation: Three pairs have a total duration divisible by 60:
+	             (time[0] = 30, time[2] = 150): total duration 180
+	             (time[1] = 20, time[3] = 100): total duration 120
+	             (time[1] = 20, time[4] = 40): total duration 60
+	
+	Example 2:
+	Input: time = [60,60,60]
+	Output: 3
+	Explanation: All three pairs have a total duration of 120, which is divisible by 60.
+
+	Constraints:
+	* 1 <= time.length <= 6 * 10^4
+	* 1 <= time[i] <= 500*/
+
+    int numPairsDivisibleBy60(vector<int>& time) {
+        int ans = 0; 
+        unordered_map<int, int> freq; 
+        for (auto& x : time) {
+            x %= 60; 
+            ans += x == 0 ? freq[x] : freq[60-x]; 
+            ++freq[x]; 
+        }
+        return ans; 
+    }
+
+
     /*1014. Best Sightseeing Pair (Medium)
 	You are given an integer array values where values[i] represents the value 
 	of the ith sightseeing spot. Two sightseeing spots i and j have a distance 
--- leetcode.py
+++ leetcode.py
@@ -24052,6 +24052,38 @@ class Solution:
         return root
 
 
+    """1010. Pairs of Songs With Total Durations Divisible by 60 (Medium)
+	You are given a list of songs where the ith song has a duration of time[i] 
+	seconds. Return the number of pairs of songs for which their total duration 
+	in seconds is divisible by 60. Formally, we want the number of indices i, j 
+	such that i < j with (time[i] + time[j]) % 60 == 0.
+
+	Example 1:
+	Input: time = [30,20,150,100,40]
+	Output: 3
+	Explanation: Three pairs have a total duration divisible by 60:
+	             (time[0] = 30, time[2] = 150): total duration 180
+	             (time[1] = 20, time[3] = 100): total duration 120
+	             (time[1] = 20, time[4] = 40): total duration 60
+	
+	Example 2:
+	Input: time = [60,60,60]
+	Output: 3
+	Explanation: All three pairs have a total duration of 120, which is divisible by 60.
+
+	Constraints:
+	* 1 <= time.length <= 6 * 10^4
+	* 1 <= time[i] <= 500"""
+
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        ans = 0 
+        freq = Counter()
+        for x in time: 
+            ans += freq[-x%60]
+            freq[x%60] += 1
+        return ans 
+
+
     """1014. Best Sightseeing Pair (Medium)
 	Given an array A of positive integers, A[i] represents the value of the 
 	i-th sightseeing spot, and two sightseeing spots i and j have distance 
