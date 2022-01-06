9ece9b8df092105c66e7b17d02d79de1b5bb113d
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-01-05 21:06:01 -0500

    solve'em all (71)

--- leetcode.cpp
+++ leetcode.cpp
@@ -1731,6 +1731,61 @@ public:
     }
 
 
+    /*71. Simplify Path (Medium)
+	Given a string path, which is an absolute path (starting with a slash '/') 
+	to a file or directory in a Unix-style file system, convert it to the 
+	simplified canonical path. In a Unix-style file system, a period '.' refers 
+	to the current directory, a double period '..' refers to the directory up a 
+	level, and any multiple consecutive slashes (i.e. '//') are treated as a 
+	single slash '/'. For this problem, any other format of periods such as 
+	'...' are treated as file/directory names. The canonical path should have 
+	the following format:
+	* The path starts with a single slash '/'.
+	* Any two directories are separated by a single slash '/'.
+	* The path does not end with a trailing '/'.
+	* The path only contains the directories on the path from the root 
+	  directory to the target file or directory (i.e., no period '.' or double 
+	  period '..')
+	Return the simplified canonical path.
+
+	Example 1:
+	Input: path = "/home/"
+	Output: "/home"
+	Explanation: Note that there is no trailing slash after the last directory 
+	             name.
+
+	Example 2:
+	Input: path = "/../"
+	Output: "/"
+	Explanation: Going one level up from the root directory is a no-op, as the 
+	             root level is the highest level you can go.
+	
+	Example 3:
+	Input: path = "/home//foo/"
+	Output: "/home/foo"
+	Explanation: In the canonical path, multiple consecutive slashes are 
+	             replaced by a single one.
+
+	Constraints:
+	* 1 <= path.length <= 3000
+	* path consists of English letters, digits, period '.', slash '/' or '_'.
+	* path is a valid absolute Unix path.*/
+
+    string simplifyPath(string path) {
+        vector<string> stk; 
+        istringstream iss(path); 
+        string buf; 
+        while (getline(iss, buf, '/')) 
+            if (buf == "..") {
+                if (stk.size()) stk.pop_back(); 
+            } else if (buf.size() && buf != ".") 
+                stk.push_back(buf); 
+        string ans; 
+        for (auto& x : stk) ans += "/" + x; 
+        return ans.size() ? ans : "/"; 
+    }
+
+
     /*72. Edit Distance (Hard)
 	Given two strings word1 and word2, return the minimum number of operations 
 	required to convert word1 to word2. You have the following three operations 
--- leetcode.py
+++ leetcode.py
@@ -2366,17 +2366,20 @@ class Solution:
 	Example 1:
 	Input: "/home/"
 	Output: "/home"
-	Explanation: Note that there is no trailing slash after the last directory name.
+	Explanation: Note that there is no trailing slash after the last directory 
+	             name.
 
 	Example 2:
 	Input: "/../"
 	Output: "/"
-	Explanation: Going one level up from the root directory is a no-op, as the root level is the highest level you can go.
+	Explanation: Going one level up from the root directory is a no-op, as the 
+	             root level is the highest level you can go.
 
 	Example 3:
 	Input: "/home//foo/"
 	Output: "/home/foo"
-	Explanation: In the canonical path, multiple consecutive slashes are replaced by a single one.
+	Explanation: In the canonical path, multiple consecutive slashes are 
+	             replaced by a single one.
 
 	Example 4:
 	Input: "/a/./b/../../c/"
@@ -2392,10 +2395,11 @@ class Solution:
 
     def simplifyPath(self, path: str) -> str:
         stack = []
-        for x in path.split("/"):
-                if x == ".." and stack: stack.pop()
-                if x not in (".", "..", ""): stack.append(x)
-        return "/" + "/".join(stack)
+        for x in path.split('/'): 
+            if x == '..': 
+                if stack: stack.pop()
+            elif x and x != '.': stack.append(x)
+        return "/" + '/'.join(stack)
 
 
     """72. Edit Distance (Hard)
