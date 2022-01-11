eb5c38d9f441234a07ab02fe628328bd4e1097a8
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-01-10 21:02:03 -0500

    January LeetCoding Challenge 2022-01-11 (1022)

--- leetcode.cpp
+++ leetcode.cpp
@@ -20463,6 +20463,44 @@ public:
     }
 
 
+    /*1022. Sum of Root To Leaf Binary Numbers (Easy)
+	You are given the root of a binary tree where each node has a value 0 or 1. 
+	Each root-to-leaf path represents a binary number starting with the most 
+	significant bit.
+	* For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could 
+	  represent 01101 in binary, which is 13.
+	For all leaves in the tree, consider the numbers represented by the path 
+	from the root to that leaf. Return the sum of these numbers. The test cases 
+	are generated so that the answer fits in a 32-bits integer.
+
+	Example 1:
+	Input: root = [1,0,1,0,1,0,1]
+	Output: 22
+	Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+
+	Example 2:
+	Input: root = [0]
+	Output: 0
+
+	Constraints:
+	* The number of nodes in the tree is in the range [1, 1000].
+	* Node.val is 0 or 1.*/
+
+    int sumRootToLeaf(TreeNode* root) {
+        int ans = 0; 
+        stack<pair<TreeNode*, int>> stk; 
+        stk.emplace(root, 0); 
+        while (stk.size()) {
+            auto [node, val] = stk.top(); stk.pop(); 
+            val = val * 2 + node->val; 
+            if (!node->left && !node->right) ans += val; 
+            if (node->left) stk.emplace(node->left, val); 
+            if (node->right) stk.emplace(node->right, val); 
+        }
+        return ans; 
+    }
+
+
     /*1026. Maximum Difference Between Node and Ancestor (Medium)
 	Given the root of a binary tree, find the maximum value v for which there 
 	exist different nodes a and b where v = |a.val - b.val| and a is an 
--- leetcode.py
+++ leetcode.py
@@ -24297,6 +24297,41 @@ class Solution:
         return sum(map(sum, A))
 
 
+    """1022. Sum of Root To Leaf Binary Numbers (Easy)
+	You are given the root of a binary tree where each node has a value 0 or 1. 
+	Each root-to-leaf path represents a binary number starting with the most 
+	significant bit.
+	* For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could 
+	  represent 01101 in binary, which is 13.
+	For all leaves in the tree, consider the numbers represented by the path 
+	from the root to that leaf. Return the sum of these numbers. The test cases 
+	are generated so that the answer fits in a 32-bits integer.
+
+	Example 1:
+	Input: root = [1,0,1,0,1,0,1]
+	Output: 22
+	Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+
+	Example 2:
+	Input: root = [0]
+	Output: 0
+
+	Constraints:
+	* The number of nodes in the tree is in the range [1, 1000].
+	* Node.val is 0 or 1."""
+
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        ans = 0 
+        stack = [(root, 0)]
+        while stack: 
+            node, val = stack.pop()
+            val = 2*val + node.val 
+            if node.left is node.right: ans += val 
+            if node.left: stack.append((node.left, val))
+            if node.right: stack.append((node.right, val))
+        return ans 
+
+
     """1023. Camelcase Matching (Medium)
 	A query word matches a given pattern if we can insert lowercase letters to 
 	the pattern word so that it equals the query. (We may insert each character 
