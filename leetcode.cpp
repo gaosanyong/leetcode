571093f19f48eee9afbb0af5d24c6ea44f5d892b
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-01-19 20:27:11 -0500

    January LeetCoding Challenge 2022-01-20 (875)

--- leetcode.cpp
+++ leetcode.cpp
@@ -17265,6 +17265,45 @@ public:
     }
 
 
+    /*875. Koko Eating Bananas (Medium)
+	Koko loves to eat bananas. There are n piles of bananas, the ith pile has 
+	piles[i] bananas. The guards have gone and will come back in h hours. Koko 
+	can decide her bananas-per-hour eating speed of k. Each hour, she chooses 
+	some pile of bananas and eats k bananas from that pile. If the pile has 
+	less than k bananas, she eats all of them instead and will not eat any more 
+	bananas during this hour. Koko likes to eat slowly but still wants to 
+	finish eating all the bananas before the guards return. Return the minimum 
+	integer k such that she can eat all the bananas within h hours.
+
+	Example 1:
+	Input: piles = [3,6,7,11], h = 8
+	Output: 4
+
+	Example 2:
+	Input: piles = [30,11,23,4,20], h = 5
+	Output: 30
+
+	Example 3:
+	Input: piles = [30,11,23,4,20], h = 6
+	Output: 23
+
+	Constraints:
+	* 1 <= piles.length <= 10^4
+	* piles.length <= h <= 10^9
+	* 1 <= piles[i] <= 10^9*/
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int lo = 1, hi = *max_element(piles.begin(), piles.end()); 
+        while (lo < hi) {
+            int mid = lo + (hi - lo)/2, val = 0; 
+            for (auto& x : piles) val += (x+mid-1)/mid; 
+            if (val <= h) hi = mid; 
+            else lo = mid+1; 
+        }
+        return lo; 
+    }
+
+
     /*876. Middle of the Linked List (Easy)
 	Given a non-empty, singly linked list with head node head, return a middle 
 	node of linked list. If there are two middle nodes, return the second 
--- leetcode.py
+++ leetcode.py
@@ -21057,6 +21057,42 @@ class Solution:
         return ans 
 
 
+    """875. Koko Eating Bananas (Medium)
+	Koko loves to eat bananas. There are n piles of bananas, the ith pile has 
+	piles[i] bananas. The guards have gone and will come back in h hours. Koko 
+	can decide her bananas-per-hour eating speed of k. Each hour, she chooses 
+	some pile of bananas and eats k bananas from that pile. If the pile has 
+	less than k bananas, she eats all of them instead and will not eat any more 
+	bananas during this hour. Koko likes to eat slowly but still wants to 
+	finish eating all the bananas before the guards return. Return the minimum 
+	integer k such that she can eat all the bananas within h hours.
+
+	Example 1:
+	Input: piles = [3,6,7,11], h = 8
+	Output: 4
+
+	Example 2:
+	Input: piles = [30,11,23,4,20], h = 5
+	Output: 30
+
+	Example 3:
+	Input: piles = [30,11,23,4,20], h = 6
+	Output: 23
+
+	Constraints:
+	* 1 <= piles.length <= 10^4
+	* piles.length <= h <= 10^9
+	* 1 <= piles[i] <= 10^9"""
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        lo, hi = 1, max(piles)
+        while lo < hi: 
+            mid = lo + hi >> 1
+            if sum(ceil(x/mid) for x in piles) <= h: hi = mid 
+            else: lo = mid + 1
+        return lo 
+
+
     """876. Middle of the Linked List (Easy)
 	Given the head of a singly linked list, return the middle node of the 
 	linked list. If there are two middle nodes, return the second middle node.
