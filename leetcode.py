ec29d17d40e043b99828b56d35d371dd791db7bb
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-01-20 20:34:21 -0500

    January LeetCoding Challenge 2022-01-21 (134)

--- leetcode.py
+++ leetcode.py
@@ -4573,49 +4573,45 @@ class Solution:
 	the clockwise direction, otherwise return -1.
 
 	Note:
-	If there exists a solution, it is guaranteed to be unique.
-	Both input arrays are non-empty and have the same length.
-	Each element in the input arrays is a non-negative integer.
+	* If there exists a solution, it is guaranteed to be unique.
+	* Both input arrays are non-empty and have the same length.
+	* Each element in the input arrays is a non-negative integer.
 
 	Example 1:
-	Input: 
-	gas  = [1,2,3,4,5]
-	cost = [3,4,5,1,2]
-
+	Input: gas  = [1,2,3,4,5]
+	       cost = [3,4,5,1,2]
 	Output: 3
-
-	Explanation:
-	Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
-	Travel to station 4. Your tank = 4 - 1 + 5 = 8
-	Travel to station 0. Your tank = 8 - 2 + 1 = 7
-	Travel to station 1. Your tank = 7 - 3 + 2 = 6
-	Travel to station 2. Your tank = 6 - 4 + 3 = 5
-	Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
-	Therefore, return 3 as the starting index.
+	Explanation: Start at station 3 (index 3) and fill up with 4 unit of gas. 
+	             Your tank = 0 + 4 = 4
+	             Travel to station 4. Your tank = 4 - 1 + 5 = 8
+	             Travel to station 0. Your tank = 8 - 2 + 1 = 7
+	             Travel to station 1. Your tank = 7 - 3 + 2 = 6
+	             Travel to station 2. Your tank = 6 - 4 + 3 = 5
+	             Travel to station 3. The cost is 5. Your gas is just enough to 
+	             travel back to station 3. Therefore, return 3 as the starting 
+	             index.
 
 	Example 2:
-	Input: 
-	gas  = [2,3,4]
-	cost = [3,4,3]
-
+	Input: gas  = [2,3,4]
+	       cost = [3,4,3]
 	Output: -1
-
-	Explanation:
-	You can't start at station 0 or 1, as there is not enough gas to travel to the next station. 
-	Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
-	Travel to station 0. Your tank = 4 - 3 + 2 = 3
-	Travel to station 1. Your tank = 3 - 3 + 3 = 3
-	You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
-	Therefore, you can't travel around the circuit once no matter where you start."""
+	Explanation: You can't start at station 0 or 1, as there is not enough gas 
+	             to travel to the next station. Let's start at station 2 and 
+	             fill up with 4 unit of gas. Your tank = 0 + 4 = 4. Travel to 
+	             station 0. Your tank = 4 - 3 + 2 = 3. Travel to station 1. 
+	             Your tank = 3 - 3 + 3 = 3. You cannot travel back to station 2, 
+	             as it requires 4 unit of gas but you only have 3. Therefore, 
+	             you can't travel around the circuit once no matter where you 
+	             start."""
 
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        ans = prefix = lowest = 0
-        for n, (g, c) in enumerate(zip(gas, cost), 1): 
-            prefix += g - c
-            if prefix < lowest: 
-                lowest = prefix
-                ans = n
-        return -1 if prefix < 0 else ans%n
+        ans = prefix = least = 0 
+        for i, (g, c) in enumerate(zip(gas, cost)): 
+            prefix += g-c
+            if prefix < least:
+                ans = i+1
+                least = prefix
+        return ans % len(gas) if prefix >= 0 else -1 
 
 
     """135. Candy (Hard)
