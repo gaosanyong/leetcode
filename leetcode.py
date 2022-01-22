8fc8469e823a229654fc858172dfa9383d805e39
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-01-21 20:44:07 -0500

    January LeetCoding Challenge 2022-01-22 (1510)

--- leetcode.py
+++ leetcode.py
@@ -33552,6 +33552,44 @@ class UnionFind:
         return next(node for node in tree if node.val == val)
 
 
+    """1510. Stone Game IV (Hard)
+	Alice and Bob take turns playing a game, with Alice starting first. 
+	Initially, there are n stones in a pile. On each player's turn, that player 
+	makes a move consisting of removing any non-zero square number of stones in 
+	the pile. Also, if a player cannot make a move, he/she loses the game. 
+	Given a positive integer n, return true if and only if Alice wins the game 
+	otherwise return false, assuming both players play optimally.
+
+	Example 1:
+	Input: n = 1
+	Output: true
+	Explanation: Alice can remove 1 stone winning the game because Bob doesn't 
+	             have any moves.
+	
+	Example 2:
+	Input: n = 2
+	Output: false
+	Explanation: Alice can only remove 1 stone, after that Bob removes the last 
+	             one winning the game (2 -> 1 -> 0).
+	
+	Example 3:
+	Input: n = 4
+	Output: true
+	Explanation: n is already a perfect square, Alice can win with one move, 
+	             removing 4 stones (4 -> 0).
+
+	Constraints: 1 <= n <= 10^5"""
+
+    def winnerSquareGame(self, n: int) -> bool:
+        dp = [False] * (n+1)
+        for x in range(1, n+1): 
+            for k in range(1, int(sqrt(x))+1):
+                if not dp[x-k*k]: 
+                    dp[x] = True
+                    break 
+        return dp[-1]
+
+
     """1518. Water Bottles (Easy)
 	Given numBottles full water bottles, you can exchange numExchange empty 
 	water bottles for one full water bottle. The operation of drinking a full 
