9e175e69c9b1fe23681c0c0dbbf23538f8f4c114
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-01-23 20:17:32 -0500

    January LeetCoding Challenge 2022-01-24 (520)

--- leetcode.cpp
+++ leetcode.cpp
@@ -9798,6 +9798,35 @@ public:
     }
 
 
+    /*520. Detect Capital (Easy)
+	We define the usage of capitals in a word to be right when one of the 
+	following cases holds:
+	* All letters in this word are capitals, like "USA".
+	* All letters in this word are not capitals, like "leetcode".
+	* Only the first letter in this word is capital, like "Google".
+	Given a string word, return true if the usage of capitals in it is right.
+
+	Example 1:
+	Input: word = "USA"
+	Output: true
+
+	Example 2:
+	Input: word = "FlaG"
+	Output: false
+
+	Constraints:
+	* 1 <= word.length <= 100
+	* word consists of lowercase and uppercase English letters.*/
+
+    bool detectCapitalUse(string word) {
+        for (int i = 1; i < word.size(); ++i) {
+            if (islower(word[0]) && isupper(word[i])) return false; 
+            if (islower(word[1]) != islower(word[i])) return false; 
+        }
+        return true; 
+    }
+
+
     /*522. Longest Uncommon Subsequence II (Medium)
 	Given an array of strings strs, return the length of the longest uncommon 
 	subsequence between them. If the longest uncommon subsequence does not 
--- leetcode.py
+++ leetcode.py
@@ -13215,6 +13215,30 @@ class Solution:
         return dp[-1]
 
 
+    """520. Detect Capital (Easy)
+	We define the usage of capitals in a word to be right when one of the 
+	following cases holds:
+	* All letters in this word are capitals, like "USA".
+	* All letters in this word are not capitals, like "leetcode".
+	* Only the first letter in this word is capital, like "Google".
+	Given a string word, return true if the usage of capitals in it is right.
+
+	Example 1:
+	Input: word = "USA"
+	Output: true
+
+	Example 2:
+	Input: word = "FlaG"
+	Output: false
+
+	Constraints:
+	* 1 <= word.length <= 100
+	* word consists of lowercase and uppercase English letters."""
+
+    def detectCapitalUse(self, word: str) -> bool:
+        return word.isupper() or word.islower() or word.istitle()
+
+
     """522. Longest Uncommon Subsequence II (Medium)
 	Given a list of strings, you need to find the longest uncommon subsequence 
 	among them. The longest uncommon subsequence is defined as the longest 
