06dc6ae2c98088788c59bace79f40bea4b306ef4
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-02-01 18:01:52 -0500

    solve'em all (1763)

--- leetcode.cpp
+++ leetcode.cpp
@@ -25115,6 +25115,49 @@ public:
     }
 
 
+    /*1763. Longest Nice Substring (Easy)
+	A string s is nice if, for every letter of the alphabet that s contains, it 
+	appears both in uppercase and lowercase. For example, "abABB" is nice 
+	because 'A' and 'a' appear, and 'B' and 'b' appear. However, "abA" is not 
+	because 'b' appears, but 'B' does not. Given a string s, return the longest 
+	substring of s that is nice. If there are multiple, return the substring of 
+	the earliest occurrence. If there are none, return an empty string.
+
+	Example 1:
+	Input: s = "YazaAay"
+	Output: "aAa"
+	Explanation: "aAa" is a nice string because 'A/a' is the only letter of the 
+	             alphabet in s, and both 'A' and 'a' appear. "aAa" is the 
+	             longest nice substring.
+	
+	Example 2:
+	Input: s = "Bb"
+	Output: "Bb"
+	Explanation: "Bb" is a nice string because both 'B' and 'b' appear. The 
+	             whole string is a substring.
+	
+	Example 3:
+	Input: s = "c"
+	Output: ""
+	Explanation: There are no nice substrings.
+
+	Constraints:
+	* 1 <= s.length <= 100
+	* s consists of uppercase and lowercase English letters.*/
+
+    string longestNiceSubstring(string s) {
+        if (s.size()) {
+            unordered_set<char> ss(s.begin(), s.end()); 
+            for (int i = 0; i < s.size(); ++i) 
+                if (!ss.count(s[i]^32)) {
+                    string cand0 = longestNiceSubstring(s.substr(0, i)), cand1 = longestNiceSubstring(s.substr(i+1)); 
+                    return cand0.size() >= cand1.size() ? cand0 : cand1; 
+                }
+        }
+        return s; 
+    }
+
+
     /*1772. Sort Features by Popularity (Medium)
 	You are given a string array features where features[i] is a single word 
 	that represents the name of a feature of the latest product you are working 
--- leetcode.py
+++ leetcode.py
@@ -40555,13 +40555,13 @@ class Fenwick:
 	* s consists of uppercase and lowercase English letters."""
 
     def longestNiceSubstring(self, s: str) -> str:
-        if not s: return "" # boundary condition 
-        ss = set(s)
-        for i, c in enumerate(s):
-            if c.swapcase() not in ss: 
-                s0 = self.longestNiceSubstring(s[:i])
-                s1 = self.longestNiceSubstring(s[i+1:])
-                return max(s0, s1, key=len)
+        if s: 
+	        ss = set(s)
+	        for i, ch in enumerate(s):
+	            if ch.swapcase() not in ss: 
+	                s0 = self.longestNiceSubstring(s[:i])
+	                s1 = self.longestNiceSubstring(s[i+1:])
+	                return max(s0, s1, key=len)
         return s
 
 
