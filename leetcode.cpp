7c7e9478dceff76e0c62460eeb464871156d9b67
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-02-02 21:39:11 -0500

    February LeetCoding Challenge 2022-02-03 (454)

--- leetcode.cpp
+++ leetcode.cpp
@@ -8890,6 +8890,44 @@ public:
     }
 
 
+    /*454. 4Sum II (Medium)
+	Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, 
+	return the number of tuples (i, j, k, l) such that:
+	* 0 <= i, j, k, l < n
+	* nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+
+	Example 1:
+	Input: nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+	Output: 2
+	Explanation: The two tuples are:
+	             1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+	             2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+	
+	Example 2:
+	Input: nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+	Output: 1
+
+	Constraints:
+	* n == nums1.length
+	* n == nums2.length
+	* n == nums3.length
+	* n == nums4.length
+	* 1 <= n <= 200
+	* -2^28 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 2^28*/
+
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int, int> freq; 
+        for (auto& x : nums1) 
+            for (auto& y : nums2)
+                ++freq[x+y]; 
+        int ans = 0; 
+        for (auto& x : nums3) 
+            for (auto& y : nums4) 
+                ans += freq[-x-y]; 
+        return ans; 
+    }
+
+
     /*456. 132 Pattern (Medium)
 	Given an array of n integers nums, a 132 pattern is a subsequence of three 
 	integers nums[i], nums[j] and nums[k] such that i < j < k and 
--- leetcode.py
+++ leetcode.py
@@ -12178,6 +12178,36 @@ class Solution:
         return "".join(ch*x for ch, x in Counter(s).most_common())
 
 
+    """454. 4Sum II (Medium)
+	Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, 
+	return the number of tuples (i, j, k, l) such that:
+	* 0 <= i, j, k, l < n
+	* nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+
+	Example 1:
+	Input: nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+	Output: 2
+	Explanation: The two tuples are:
+	             1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+	             2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+	
+	Example 2:
+	Input: nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+	Output: 1
+
+	Constraints:
+	* n == nums1.length
+	* n == nums2.length
+	* n == nums3.length
+	* n == nums4.length
+	* 1 <= n <= 200
+	* -2^28 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 2^28"""
+
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        freq = Counter(x+y for x in nums1 for y in nums2)
+        return sum(freq[-x-y] for x in nums3 for y in nums4)
+
+
     """456. 132 Pattern (Medium)
 	Given an array of n integers nums, a 132 pattern is a subsequence of three 
 	integers nums[i], nums[j] and nums[k] such that i < j < k and 
