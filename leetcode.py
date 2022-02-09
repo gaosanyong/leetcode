c7c78fd5da75a85942e24e176675f2a9083f646b
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-02-09 15:39:45 -0500

    solve'em all (1447)

--- leetcode.py
+++ leetcode.py
@@ -32843,8 +32843,8 @@ class UnionFind:
     def simplifiedFractions(self, n: int) -> List[str]:
         ans = []
         for d in range(2, n+1): 
-            for n in range(1, d):
-                if gcd(d, n) == 1: ans.append(str(n) + "/" + str(d))
+            for x in range(1, d):
+                if gcd(x, d) == 1: ans.append(f"{x}/{d}")
         return ans 
 
 
