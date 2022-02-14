fe226f2667dccd82b3baa7e16df8d3ead2ed2dbf
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-02-14 18:20:07 -0500

    solve'em all (1380)

--- leetcode.cpp
+++ leetcode.cpp
@@ -24171,6 +24171,51 @@ public:
     }
 
 
+    /*1380. Lucky Numbers in a Matrix (Easy)
+	Given an m x n matrix of distinct numbers, return all lucky numbers in the 
+	matrix in any order. A lucky number is an element of the matrix such that 
+	it is the minimum element in its row and maximum in its column.
+
+	Example 1:
+	Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
+	Output: [15]
+	Explanation: 15 is the only lucky number since it is the minimum in its row 
+	             and the maximum in its column.
+	
+	Example 2:
+	Input: matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
+	Output: [12]
+	Explanation: 12 is the only lucky number since it is the minimum in its row 
+	             and the maximum in its column.
+	
+	Example 3:
+	Input: matrix = [[7,8],[1,2]]
+	Output: [7]
+	Explanation: 7 is the only lucky number since it is the minimum in its row 
+	             and the maximum in its column.
+
+	Constraints:
+	* m == mat.length
+	* n == mat[i].length
+	* 1 <= n, m <= 50
+	* 1 <= matrix[i][j] <= 10^5.
+	* All elements in the matrix are distinct.*/
+
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size(); 
+        unordered_set<int> seen; 
+        for (int i = 0; i < m; ++i) 
+            seen.insert(*min_element(matrix[i].begin(), matrix[i].end())); 
+        vector<int> ans; 
+        for (int j = 0; j < n; ++j) {
+            int cand = 0; 
+            for (int i = 0; i < m; ++i) cand = max(cand, matrix[i][j]); 
+            if (seen.count(cand)) ans.push_back(cand); 
+        }
+        return ans; 
+    }
+
+
     /*1405. Longest Happy String (Medium)
 	A string s is called happy if it satisfies the following conditions:
 	* s only contains the letters 'a', 'b', and 'c'.
--- leetcode.py
+++ leetcode.py
@@ -31360,6 +31360,40 @@ class UnionFind:
         return fn(root)[-1]
 
 
+    """1380. Lucky Numbers in a Matrix (Easy)
+	Given an m x n matrix of distinct numbers, return all lucky numbers in the 
+	matrix in any order. A lucky number is an element of the matrix such that 
+	it is the minimum element in its row and maximum in its column.
+
+	Example 1:
+	Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
+	Output: [15]
+	Explanation: 15 is the only lucky number since it is the minimum in its row 
+	             and the maximum in its column.
+	
+	Example 2:
+	Input: matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
+	Output: [12]
+	Explanation: 12 is the only lucky number since it is the minimum in its row 
+	             and the maximum in its column.
+	
+	Example 3:
+	Input: matrix = [[7,8],[1,2]]
+	Output: [7]
+	Explanation: 7 is the only lucky number since it is the minimum in its row 
+	             and the maximum in its column.
+
+	Constraints:
+	* m == mat.length
+	* n == mat[i].length
+	* 1 <= n, m <= 50
+	* 1 <= matrix[i][j] <= 10^5.
+	* All elements in the matrix are distinct."""
+
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        return set(map(min, matrix)) & set(map(max, zip(*matrix)))
+
+
     """1383. Maximum Performance of a Team (Hard)
 	You are given two integers n and k and two integer arrays speed and 
 	efficiency both of length n. There are n engineers numbered from 1 to n. 
