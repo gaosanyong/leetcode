47baa9ce5e9e3eaa8705adb1631f28f1c52f63dd
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-02-17 20:35:41 -0500

    February LeetCoding Challenge 2022-02-18 (402)

--- leetcode.cpp
+++ leetcode.cpp
@@ -8183,6 +8183,47 @@ public:
     }
 
 
+    /*402. Remove K Digits (Medium)
+	Given string num representing a non-negative integer num, and an integer k, 
+	return the smallest possible integer after removing k digits from num.
+
+	Example 1:
+	Input: num = "1432219", k = 3
+	Output: "1219"
+	Explanation: Remove the three digits 4, 3, and 2 to form the new number 
+	             1219 which is the smallest.
+	
+	Example 2:
+	Input: num = "10200", k = 1
+	Output: "200"
+	Explanation: Remove the leading 1 and the number is 200. Note that the 
+	             output must not contain leading zeroes.
+	
+	Example 3:
+	Input: num = "10", k = 2
+	Output: "0"
+	Explanation: Remove all the digits from the number and it is left with 
+	             nothing which is 0.
+
+	Constraints:
+	* 1 <= k <= num.length <= 10^5
+	* num consists of only digits.
+	* num does not have any leading zeros except for the zero itself.*/
+
+    string removeKdigits(string num, int k) {
+        string ans;
+        for (auto& ch : num) {
+            while (k && ans.size() && ans.back() > ch) {
+                --k; 
+                ans.pop_back(); 
+            }
+            if (ans.size() || ch != '0') ans.push_back(ch); 
+        }
+        while (k-- && ans.size()) ans.pop_back(); 
+        return ans.size() ? ans : "0"; 
+    }
+
+
     /*403. Frog Jump (Hard)
 	A frog is crossing a river. The river is divided into some number of units, 
 	and at each unit, there may or may not exist a stone. The frog can jump on 
