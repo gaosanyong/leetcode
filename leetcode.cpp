5627f0e89a7204f1b0d19fbf21230a222e73b473
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-02-19 20:44:40 -0500

    February LeetCoding Challenge 2022-02-20 (1288)

--- leetcode.cpp
+++ leetcode.cpp
@@ -23749,6 +23749,42 @@ public:
     }
 
 
+    /*1288. Remove Covered Intervals (Medium)
+	Given an array intervals where intervals[i] = [li, ri] represent the 
+	interval [li, ri), remove all intervals that are covered by another 
+	interval in the list. The interval [a, b) is covered by the interval 
+	[c, d) if and only if c <= a and b <= d. Return the number of remaining 
+	intervals.
+
+	Example 1:
+	Input: intervals = [[1,4],[3,6],[2,8]]
+	Output: 2
+	Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+
+	Example 2:
+	Input: intervals = [[1,4],[2,3]]
+	Output: 1
+
+	Constraints:
+	* 1 <= intervals.length <= 1000
+	* intervals[i].length == 2
+	* 0 <= li <= ri <= 10^5
+	* All the given intervals are unique.*/
+
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [&](auto& lhs, auto& rhs) { 
+            return lhs[0] < rhs[0] || (lhs[0] == rhs[0] && lhs[1] > rhs[1]); 
+        }); 
+        int ans = 0, prev = INT_MIN; 
+        for (auto& interval : intervals) 
+            if (interval[1] > prev) {
+                ++ans; 
+                prev = interval[1]; 
+            }
+        return ans; 
+    }
+
+
     /*1290. Convert Binary Number in a Linked List to Integer (Easy)
 	Given head which is a reference node to a singly-linked list. The value of 
 	each node in the linked list is either 0 or 1. The linked list holds the 
--- leetcode.py
+++ leetcode.py
@@ -30463,6 +30463,38 @@ class UnionFind:
         return "Pending" if len(moves) < 9 else "Draw"
 
 
+    """1288. Remove Covered Intervals (Medium)
+	Given an array intervals where intervals[i] = [li, ri] represent the 
+	interval [li, ri), remove all intervals that are covered by another 
+	interval in the list. The interval [a, b) is covered by the interval 
+	[c, d) if and only if c <= a and b <= d. Return the number of remaining 
+	intervals.
+
+	Example 1:
+	Input: intervals = [[1,4],[3,6],[2,8]]
+	Output: 2
+	Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+
+	Example 2:
+	Input: intervals = [[1,4],[2,3]]
+	Output: 1
+
+	Constraints:
+	* 1 <= intervals.length <= 1000
+	* intervals[i].length == 2
+	* 0 <= li <= ri <= 10^5
+	* All the given intervals are unique."""
+
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        ans = 0
+        prev = -inf
+        for _, y in sorted(intervals, key=lambda x: (x[0], -x[1])): 
+            if y > prev: 
+                ans += 1
+                prev = y 
+        return ans 
+
+
     """1290. Convert Binary Number in a Linked List to Integer (Easy)
 	Given head which is a reference node to a singly-linked list. The value of 
 	each node in the linked list is either 0 or 1. The linked list holds the 
