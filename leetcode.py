2355cdfb5e6432393dccddab2abb97e9e2518e9d
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-03-27 21:40:23 -0400

    March LeetCoding Challenge 2022-03-27 (1337)

--- leetcode.py
+++ leetcode.py
@@ -31039,6 +31039,67 @@ class UnionFind:
         return mat
 
 
+    """1337. The K Weakest Rows in a Matrix (Easy)
+	You are given an m x n binary matrix mat of 1's (representing soldiers) and 
+	0's (representing civilians). The soldiers are positioned in front of the 
+	civilians. That is, all the 1's will appear to the left of all the 0's in 
+	each row. A row i is weaker than a row j if one of the following is true:
+	* The number of soldiers in row i is less than the number of soldiers in 
+	  row j.
+	* Both rows have the same number of soldiers and i < j.
+	Return the indices of the k weakest rows in the matrix ordered from weakest 
+	to strongest.
+
+	Example 1:
+	Input: mat = [[1,1,0,0,0],
+	              [1,1,1,1,0],
+	              [1,0,0,0,0],
+	              [1,1,0,0,0],
+	              [1,1,1,1,1]], 
+	       k = 3
+	Output: [2,0,3]
+	Explanation: The number of soldiers in each row is: 
+	             - Row 0: 2 
+	             - Row 1: 4 
+	             - Row 2: 1 
+	             - Row 3: 2 
+	             - Row 4: 5 
+	             The rows ordered from weakest to strongest are [2,0,3,1,4].
+	
+	Example 2:
+	Input: mat = [[1,0,0,0],
+	              [1,1,1,1],
+	              [1,0,0,0],
+	              [1,0,0,0]], 
+	       k = 2
+	Output: [0,2]
+	Explanation: The number of soldiers in each row is: 
+	             - Row 0: 1 
+	             - Row 1: 4 
+	             - Row 2: 1 
+	             - Row 3: 1 
+	             The rows ordered from weakest to strongest are [0,2,3,1].
+
+	Constraints:
+	* m == mat.length
+	* n == mat[i].length
+	* 2 <= n, m <= 100
+	* 1 <= k <= m
+	* matrix[i][j] is either 0 or 1."""
+
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        m, n = len(mat), len(mat[0])
+        vals = []
+        for i in range(m): 
+            lo, hi = 0, n
+            while lo < hi: 
+                mid = lo + hi >> 1
+                if mat[i][mid]: lo = mid + 1
+                else: hi = mid 
+            vals.append((lo, i))
+        return [i for _, i in sorted(vals)[:k]]
+
+
     """1338. Reduce Array Size to The Half (Medium)
 	Given an array arr.  You can choose a set of integers and remove all the 
 	occurrences of these integers in the array. Return the minimum size of the 
