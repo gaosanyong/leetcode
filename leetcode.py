5c7dc0464a7496e964f7f5f0c141cf5fb3164d20
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-06-11 00:55:05 -0400

    solved 2235|2236

--- leetcode.py
+++ leetcode.py
@@ -58136,6 +58136,55 @@ class Trie:
         return lo 
 
 
+    """2235. Add Two Integers (Easy)
+	Given two integers num1 and num2, return the sum of the two integers.
+
+	Example 1:
+	Input: num1 = 12, num2 = 5
+	Output: 17
+	Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is 
+	             returned.
+	
+	Example 2:
+	Input: num1 = -10, num2 = 4
+	Output: -6
+	Explanation: num1 + num2 = -6, so -6 is returned.
+
+	Constraints:
+	* -100 <= num1, num2 <= 100"""
+
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+
+
+    """2236. Root Equals Sum of Children (Easy)
+	You are given the root of a binary tree that consists of exactly 3 nodes: 
+	the root, its left child, and its right child. Return true if the value of 
+	the root is equal to the sum of the values of its two children, or false 
+	otherwise.
+
+	Example 1:
+	Input: root = [10,4,6]
+	Output: true
+	Explanation: The values of the root, its left child, and its right child 
+	             are 10, 4, and 6, respectively. 10 is equal to 4 + 6, so we 
+	             return true.
+	
+	Example 2:
+	Input: root = [5,3,1]
+	Output: false
+	Explanation: The values of the root, its left child, and its right child 
+	             are 5, 3, and 1, respectively. 5 is not equal to 3 + 1, so we 
+	             return false.
+
+	Constraints:
+	* The tree consists only of the root, its left child, and its right child.
+	* -100 <= Node.val <= 100"""
+
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        return root.val == root.left.val + root.right.val
+
+
     """2243. Calculate Digit Sum of a String (Easy)
 	You are given a string s consisting of digits and an integer k. A round can 
 	be completed if the length of s is greater than k. In one round, do the 
