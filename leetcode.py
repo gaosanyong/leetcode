bba73444e60b36e84c0dd1f183328c8575982718
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-07-21 16:45:20 -0400

    updated 92

--- leetcode.py
+++ leetcode.py
@@ -3075,22 +3075,33 @@ class Solution:
 
 
     """92. Reverse Linked List II (Medium)
-	Reverse a linked list from position m to n. Do it in one-pass. Note: 
-	1 ≤ m ≤ n ≤ length of list.
+	Given the head of a singly linked list and two integers left and right 
+	where left <= right, reverse the nodes of the list from position left to 
+	position right, and return the reversed list.
 
-	Example:
-	Input: 1->2->3->4->5->NULL, m = 2, n = 4
-	Output: 1->4->3->2->5->NULL"""
+	Example 1:
+	Input: head = [1,2,3,4,5], left = 2, right = 4
+	Output: [1,4,3,2,5]
+
+	Example 2:
+	Input: head = [5], left = 1, right = 1
+	Output: [5]
+
+	Constraints:
+	* The number of nodes in the list is n.
+	* 1 <= n <= 500
+	* -500 <= Node.val <= 500
+	* 1 <= left <= right <= n
+
+	Follow up: Could you do it in one pass?"""
 
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
         dummy = node = ListNode(next=head)
         prev = None
         for _ in range(left): node, prev = node.next, node 
         pp, nn = prev, node 
-        
         for _ in range(left, right+1): node.next, node, prev = prev, node.next, node 
         pp.next, nn.next = prev, node
-        
         return dummy.next 
 
 
