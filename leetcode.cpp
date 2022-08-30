a468fa7e6961a9dfd2f41f21063b4a9e25f5d2c7
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-08-30 00:33:13 -0400

    solve'em all (715)

--- leetcode.cpp
+++ leetcode.cpp
@@ -44553,6 +44553,84 @@ public:
 };
 
 
+/*715. Range Module (Hard)
+A Range Module is a module that tracks ranges of numbers. Design a data 
+structure to track the ranges represented as half-open intervals and query 
+about them. A half-open interval [left, right) denotes all the real numbers x 
+where left <= x < right. Implement the RangeModule class:
+* RangeModule() Initializes the object of the data structure.
+* void addRange(int left, int right) Adds the half-open interval [left, right), 
+  tracking every real number in that interval. Adding an interval that 
+  partially overlaps with currently tracked numbers should add any numbers in 
+  the interval [left, right) that are not already tracked.
+* boolean queryRange(int left, int right) Returns true if every real number in 
+  the interval [left, right) is currently being tracked, and false otherwise.
+* void removeRange(int left, int right) Stops tracking every real number 
+  currently being tracked in the half-open interval [left, right).
+
+Example 1:
+Input: ["RangeModule", "addRange", "removeRange", "queryRange", "queryRange", "queryRange"]
+       [[], [10, 20], [14, 16], [10, 14], [13, 15], [16, 17]]
+Output: [null, null, null, true, false, true]
+Explanation
+RangeModule rangeModule = new RangeModule();
+rangeModule.addRange(10, 20);
+rangeModule.removeRange(14, 16);
+rangeModule.queryRange(10, 14); // return True,(Every number in [10, 14) is 
+                                // being tracked)
+rangeModule.queryRange(13, 15); // return False,(Numbers like 14, 14.03, 14.17 
+                                // in [13, 15) are not being tracked)
+rangeModule.queryRange(16, 17); // return True, (The number 16 in [16, 17) is 
+                                // still being tracked, despite the remove 
+                                // operation)
+ 
+Constraints:
+* 1 <= left < right <= 10^9
+* At most 104 calls will be made to addRange, queryRange, and removeRange.*/
+
+class RangeModule {
+    map<int, int> intervals; 
+public:
+    RangeModule() {}
+    
+    void addRange(int left, int right) {
+        auto it = intervals.lower_bound(left); 
+        while (it != intervals.end() && it->first <= right) {
+            right = max(right, it->second); 
+            intervals.erase(prev(++it)); 
+        }
+        if (intervals.begin() != it && left <= prev(it)->second) {
+            left = min(left, prev(it)->first); 
+            right = max(right, prev(it)->second); 
+            intervals.erase(prev(it)); 
+        }
+        intervals[left] = right; 
+    }
+    
+    bool queryRange(int left, int right) {
+        auto it = intervals.lower_bound(left); 
+        return (intervals.begin() != it && prev(it)->first <= left && right <= prev(it)->second) || (it != intervals.end() && it->first <= left && right <= it->second); 
+    }
+    
+    void removeRange(int left, int right) {
+        auto it = intervals.lower_bound(left); 
+        while (it != intervals.end() && it->first <= right) {
+            if (right < it->second) {
+                intervals[right] = it->second; 
+                intervals.erase(prev(++it)); 
+                break; 
+            }            
+            intervals.erase(prev(++it)); 
+        }
+        if (intervals.begin() != it && left < prev(it)->second) {
+            auto [lo, hi] = *prev(it); 
+            prev(it)->second = left; 
+            if (right < hi) intervals[right] = hi; 
+        }
+    }
+};
+
+
 /*729. My Calendar I (Medium)
 Implement a MyCalendar class to store your events. A new event can be added if 
 adding the event will not cause a double booking. Your class will have the 
