2914616aee31cfe58176ce5f05b7bd3e639e079d
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-08-29 20:14:18 -0400

    solve'em all (460)

--- leetcode.cpp
+++ leetcode.cpp
@@ -44139,6 +44139,124 @@ public:
 };
 
 
+/*460. LFU Cache (Hard)
+Design and implement a data structure for a Least Frequently Used (LFU) cache.
+Implement the LFUCache class:
+* LFUCache(int capacity) Initializes the object with the capacity of the data 
+  structure.
+* int get(int key) Gets the value of the key if the key exists in the cache. 
+  Otherwise, returns -1.
+* void put(int key, int value) Update the value of the key if present, or 
+  inserts the key if not already present. When the cache reaches its capacity, 
+  it should invalidate and remove the least frequently used key before 
+  inserting a new item. For this problem, when there is a tie (i.e., two or 
+  more keys with the same frequency), the least recently used key would be 
+  invalidated.
+To determine the least frequently used key, a use counter is maintained for 
+each key in the cache. The key with the smallest use counter is the least 
+frequently used key. When a key is first inserted into the cache, its use 
+counter is set to 1 (due to the put operation). The use counter for a key in 
+the cache is incremented either a get or put operation is called on it. The 
+functions get and put must each run in O(1) average time complexity.
+
+Example 1:
+Input: ["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
+       [[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
+Output: [null, null, null, 1, null, -1, 3, null, -1, 3, 4]
+Explanation
+// cnt(x) = the use counter for key x
+// cache=[] will show the last used order for tiebreakers (leftmost element is  most recent)
+LFUCache lfu = new LFUCache(2);
+lfu.put(1, 1);   // cache=[1,_], cnt(1)=1
+lfu.put(2, 2);   // cache=[2,1], cnt(2)=1, cnt(1)=1
+lfu.get(1);      // return 1
+                 // cache=[1,2], cnt(2)=1, cnt(1)=2
+lfu.put(3, 3);   // 2 is the LFU key because cnt(2)=1 is the smallest, invalidate 2.
+                 // cache=[3,1], cnt(3)=1, cnt(1)=2
+lfu.get(2);      // return -1 (not found)
+lfu.get(3);      // return 3
+                 // cache=[3,1], cnt(3)=2, cnt(1)=2
+lfu.put(4, 4);   // Both 1 and 3 have the same cnt, but 1 is LRU, invalidate 1.
+                 // cache=[4,3], cnt(4)=1, cnt(3)=2
+lfu.get(1);      // return -1 (not found)
+lfu.get(3);      // return 3
+                 // cache=[3,4], cnt(4)=1, cnt(3)=3
+lfu.get(4);      // return 4
+                 // cache=[4,3], cnt(4)=2, cnt(3)=3
+
+Constraints:
+* 0 <= capacity <= 10^4
+* 0 <= key <= 10^5
+* 0 <= value <= 10^9
+* At most 2 * 10^5 calls will be made to get and put.*/
+
+struct Node {
+    int key, val, freq; 
+    Node *prev, *next; 
+    Node(int key=0, int val=0, int freq=0, Node* prev=nullptr, Node* next=nullptr) : key(key), val(val), freq(freq), prev(prev), next(next) {}
+}; 
+
+class LFUCache {
+    int capacity, lo; 
+    unordered_map<int, Node*> mp; 
+    vector<pair<Node*, Node*>> freq; 
+    
+    void remove(Node* node) {
+        if (lo == node->freq && freq[node->freq] == make_pair(node->prev, node->next)) ++lo; 
+        node->next->prev = node->prev; 
+        node->prev->next = node->next; 
+        mp.erase(node->key); 
+    }
+    
+    void insert(Node* node) {
+        if (node->freq == freq.size()) {
+            Node *head = new Node(), *tail = new Node(); 
+            head->next = tail; 
+            tail->prev = head; 
+            freq.emplace_back(head, tail); 
+        }
+        Node* head = freq[node->freq].first; 
+        node->next = head->next; 
+        node->prev = head; 
+        head->next = head->next->prev = node; 
+        mp[node->key] = node; 
+    }
+public:
+    LFUCache(int capacity) {
+        this->capacity = capacity; 
+        lo = 0; 
+    }
+    
+    int get(int key) {
+        if (!mp.count(key)) return -1; 
+        Node* node = mp[key]; 
+        remove(node); 
+        ++node->freq;
+        insert(node); 
+        return node->val; 
+    }
+    
+    void put(int key, int value) {
+        if (get(key) == -1) {
+            if (capacity == 0 && freq.size()) {
+                ++capacity; 
+                Node* node = freq[lo].second->prev; 
+                remove(node); 
+            }
+            if (capacity) {
+                --capacity; 
+                Node* node = new Node(key, value); 
+                insert(node); 
+                lo = 0; 
+            }
+        } else {
+            Node* node = mp[key]; 
+            node->val = value; 
+        }
+    }
+};
+
+
 /*635. Design Log Storage System (Medium)
 You are given several logs, where each log contains a unique ID and timestamp. 
 Timestamp is a string that has the following format: 
--- leetcode.py
+++ leetcode.py
@@ -65139,6 +65139,118 @@ class AllOne:
         return ans 
 
 
+"""460. LFU Cache (Hard)
+Design and implement a data structure for a Least Frequently Used (LFU) cache.
+Implement the LFUCache class:
+* LFUCache(int capacity) Initializes the object with the capacity of the data 
+  structure.
+* int get(int key) Gets the value of the key if the key exists in the cache. 
+  Otherwise, returns -1.
+* void put(int key, int value) Update the value of the key if present, or 
+  inserts the key if not already present. When the cache reaches its capacity, 
+  it should invalidate and remove the least frequently used key before 
+  inserting a new item. For this problem, when there is a tie (i.e., two or 
+  more keys with the same frequency), the least recently used key would be 
+  invalidated.
+To determine the least frequently used key, a use counter is maintained for 
+each key in the cache. The key with the smallest use counter is the least 
+frequently used key. When a key is first inserted into the cache, its use 
+counter is set to 1 (due to the put operation). The use counter for a key in 
+the cache is incremented either a get or put operation is called on it. The 
+functions get and put must each run in O(1) average time complexity.
+
+Example 1:
+Input: ["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
+       [[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
+Output: [null, null, null, 1, null, -1, 3, null, -1, 3, 4]
+Explanation
+// cnt(x) = the use counter for key x
+// cache=[] will show the last used order for tiebreakers (leftmost element is  most recent)
+LFUCache lfu = new LFUCache(2);
+lfu.put(1, 1);   // cache=[1,_], cnt(1)=1
+lfu.put(2, 2);   // cache=[2,1], cnt(2)=1, cnt(1)=1
+lfu.get(1);      // return 1
+                 // cache=[1,2], cnt(2)=1, cnt(1)=2
+lfu.put(3, 3);   // 2 is the LFU key because cnt(2)=1 is the smallest, invalidate 2.
+                 // cache=[3,1], cnt(3)=1, cnt(1)=2
+lfu.get(2);      // return -1 (not found)
+lfu.get(3);      // return 3
+                 // cache=[3,1], cnt(3)=2, cnt(1)=2
+lfu.put(4, 4);   // Both 1 and 3 have the same cnt, but 1 is LRU, invalidate 1.
+                 // cache=[4,3], cnt(4)=1, cnt(3)=2
+lfu.get(1);      // return -1 (not found)
+lfu.get(3);      // return 3
+                 // cache=[3,4], cnt(4)=1, cnt(3)=3
+lfu.get(4);      // return 4
+                 // cache=[4,3], cnt(4)=2, cnt(3)=3
+
+Constraints:
+* 0 <= capacity <= 10^4
+* 0 <= key <= 10^5
+* 0 <= value <= 10^9
+* At most 2 * 10^5 calls will be made to get and put."""
+
+class ListNode: 
+    
+    def __init__(self, key=0, val=0, freq = 0, prev=None, next=None): 
+        self.key = key
+        self.val = val
+        self.freq = freq
+        self.prev = prev
+        self.next = next 
+        
+
+class LFUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity 
+        self.lo = 0
+        self.mp = {} 
+        self.freq = []
+        
+    def _remove(self, node): 
+        if self.lo == node.freq and self.freq[node.freq] == (node.prev, node.next): self.lo += 1
+        node.next.prev = node.prev
+        node.prev.next = node.next
+        self.mp.pop(node.key)
+        
+    def _insert(self, node): 
+        if node.freq == len(self.freq): 
+            head = ListNode()
+            tail = ListNode()
+            head.next = tail
+            tail.prev = head 
+            self.freq.append((head, tail))
+        head = self.freq[node.freq][0]
+        node.next = head.next
+        node.prev = head
+        head.next.prev = head.next = node 
+        self.mp[node.key] = node 
+        
+    def get(self, key: int) -> int:
+        if key not in self.mp: return -1 
+        node = self.mp[key] 
+        self._remove(node)
+        node.freq += 1
+        self._insert(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if self.get(key) == -1: 
+            if self.cap == 0 and self.freq: 
+                self.cap += 1
+                node = self.freq[self.lo][1].prev 
+                self._remove(node)
+            if self.cap: 
+                self.cap -= 1
+                node = ListNode(key, value)
+                self._insert(node)
+                self.lo = 0  
+        else: 
+            node = self.mp[key]
+            node.val = value
+
+
 """519. Random Flip Matrix (Medium)
 You are given the number of rows n_rows and number of columns n_cols of a 2D 
 binary matrix where all values are initially 0. Write a function flip which 
