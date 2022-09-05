291bf702c73835df55bf312f5fa3bc03aa39709d
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-09-05 01:03:47 -0400

    solve'em all (488)

--- leetcode.cpp
+++ leetcode.cpp
@@ -9905,6 +9905,96 @@ public:
     }
 
 
+    /*488. Zuma Game (Hard)
+	You are playing a variation of the game Zuma. In this variation of Zuma, 
+	there is a single row of colored balls on a board, where each ball can be 
+	colored red 'R', yellow 'Y', blue 'B', green 'G', or white 'W'. You also 
+	have several colored balls in your hand. Your goal is to clear all of the 
+	balls from the board. On each turn:
+	* Pick any ball from your hand and insert it in between two balls in the 
+	  row or on either end of the row.
+	* If there is a group of three or more consecutive balls of the same color, 
+	  remove the group of balls from the board.
+      + If this removal causes more groups of three or more of the same color 
+        to form, then continue removing each group until there are none left.
+	* If there are no more balls on the board, then you win the game.
+	* Repeat this process until you either win or do not have any more balls in 
+	  your hand.
+	Given a string board, representing the row of balls on the board, and a 
+	string hand, representing the balls in your hand, return the minimum number 
+	of balls you have to insert to clear all the balls from the board. If you 
+	cannot clear all the balls from the board using the balls in your hand, 
+	return -1.
+
+	Example 1:
+	Input: board = "WRRBBW", hand = "RB"
+	Output: -1
+	Explanation: It is impossible to clear all the balls. The best you can do 
+	             is:
+	             - Insert 'R' so the board becomes WRRRBBW. WRRRBBW -> WBBW.
+	             - Insert 'B' so the board becomes WBBBW. WBBBW -> WW.
+	             There are still balls remaining on the board, and you are out 
+	             of balls to insert.
+	
+	Example 2:
+	Input: board = "WWRRBBWW", hand = "WRBRW"
+	Output: 2
+	Explanation: To make the board empty:
+	             - Insert 'R' so the board becomes WWRRRBBWW. WWRRRBBWW -> 
+	               WWBBWW.
+	             - Insert 'B' so the board becomes WWBBBWW. WWBBBWW -> WWWW -> 
+	               empty.
+	             2 balls from your hand were needed to clear the board.
+	
+	Example 3:
+	Input: board = "G", hand = "GGGGG"
+	Output: 2
+	Explanation: To make the board empty:
+	             - Insert 'G' so the board becomes GG.
+	             - Insert 'G' so the board becomes GGG. GGG -> empty.
+	             2 balls from your hand were needed to clear the board.
+
+	Constraints:
+	* 1 <= board.length <= 16
+	* 1 <= hand.length <= 5
+	* board and hand consist of the characters 'R', 'Y', 'B', 'G', and 'W'.
+	* The initial row of balls on the board will not have any groups of three 
+	  or more consecutive balls of the same color.*/
+
+    int findMinStep(string board, string hand) {
+        sort(hand.begin(), hand.end()); 
+        unordered_set<string> seen = {board + "#" + hand}; 
+        queue<pair<string, string>> q; 
+        q.emplace(board, hand); 
+        while (q.size()) {
+            auto [b, h] = q.front(); q.pop(); 
+            if (b.empty()) return hand.size() - h.size(); 
+            for (int i = 0; i < h.size(); ++i) 
+                if (i == 0 || h[i-1] != h[i]) {
+                    string hh = h.substr(0, i) + h.substr(i+1); 
+                    for (int j = 0; j < b.size(); ++j) 
+                        if ((h[i] == b[j] && (j == 0 || b[j-1] != b[j])) || (j && h[i] != b[j-1] && b[j-1] == b[j])) {
+                            string bb = "", nn = b.substr(0, j) + string(1, h[i]) + b.substr(j); 
+                            while (bb != nn) {
+                                bb = nn; 
+                                nn = ""; 
+                                for (int k = 0, kk = 0; k <= bb.size(); ++k) 
+                                    if (k == bb.size() || (k && bb[k-1] != bb[k])) {
+                                        if (k-kk < 3) nn += bb.substr(kk, k-kk); 
+                                        kk = k; 
+                                    }
+                            }
+                            if (!seen.count(bb + "#" + hh)) {
+                                seen.insert(bb + "#" + hh); 
+                                q.emplace(bb, hh); 
+                            }
+                        }
+                }
+        }
+        return -1; 
+    }
+
+
     /*490. The Maze (Medium)
 	There is a ball in a maze with empty spaces (represented as 0) and walls 
 	(represented as 1). The ball can go through the empty spaces by rolling up, 
--- leetcode.py
+++ leetcode.py
@@ -12697,6 +12697,88 @@ class Solution:
         return ans 
 
 
+    """488. Zuma Game (Hard)
+	You are playing a variation of the game Zuma. In this variation of Zuma, 
+	there is a single row of colored balls on a board, where each ball can be 
+	colored red 'R', yellow 'Y', blue 'B', green 'G', or white 'W'. You also 
+	have several colored balls in your hand. Your goal is to clear all of the 
+	balls from the board. On each turn:
+	* Pick any ball from your hand and insert it in between two balls in the 
+	  row or on either end of the row.
+	* If there is a group of three or more consecutive balls of the same color, 
+	  remove the group of balls from the board.
+      + If this removal causes more groups of three or more of the same color 
+        to form, then continue removing each group until there are none left.
+	* If there are no more balls on the board, then you win the game.
+	* Repeat this process until you either win or do not have any more balls in 
+	  your hand.
+	Given a string board, representing the row of balls on the board, and a 
+	string hand, representing the balls in your hand, return the minimum number 
+	of balls you have to insert to clear all the balls from the board. If you 
+	cannot clear all the balls from the board using the balls in your hand, 
+	return -1.
+
+	Example 1:
+	Input: board = "WRRBBW", hand = "RB"
+	Output: -1
+	Explanation: It is impossible to clear all the balls. The best you can do 
+	             is:
+	             - Insert 'R' so the board becomes WRRRBBW. WRRRBBW -> WBBW.
+	             - Insert 'B' so the board becomes WBBBW. WBBBW -> WW.
+	             There are still balls remaining on the board, and you are out 
+	             of balls to insert.
+	
+	Example 2:
+	Input: board = "WWRRBBWW", hand = "WRBRW"
+	Output: 2
+	Explanation: To make the board empty:
+	             - Insert 'R' so the board becomes WWRRRBBWW. WWRRRBBWW -> 
+	               WWBBWW.
+	             - Insert 'B' so the board becomes WWBBBWW. WWBBBWW -> WWWW -> 
+	               empty.
+	             2 balls from your hand were needed to clear the board.
+	
+	Example 3:
+	Input: board = "G", hand = "GGGGG"
+	Output: 2
+	Explanation: To make the board empty:
+	             - Insert 'G' so the board becomes GG.
+	             - Insert 'G' so the board becomes GGG. GGG -> empty.
+	             2 balls from your hand were needed to clear the board.
+
+	Constraints:
+	* 1 <= board.length <= 16
+	* 1 <= hand.length <= 5
+	* board and hand consist of the characters 'R', 'Y', 'B', 'G', and 'W'.
+	* The initial row of balls on the board will not have any groups of three 
+	  or more consecutive balls of the same color."""
+
+    def findMinStep(self, board: str, hand: str) -> int:
+        hand = ''.join(sorted(hand))
+        
+        @cache
+        def fn(board, hand):
+            """Return min number of balls to insert."""
+            if not board: return 0
+            if not hand: return inf 
+            ans = inf 
+            for i, ch in enumerate(hand): 
+                if i == 0 or hand[i-1] != ch: # pruning 1
+                    hh = hand[:i] + hand[i+1:]
+                    for j in range(0, len(board)): 
+                        if ch == board[j] or j and board[j-1] == board[j]: # pruning 2
+                            bb, nn = "", board[:j] + ch + board[j:]
+                            while bb != nn:
+                                bb, nn = nn, ""
+                                for k, grp in groupby(bb): 
+                                    x = len(list(grp))
+                                    if x < 3: nn += k*x
+                            ans = min(ans, 1 + fn(bb, hh))
+            return ans 
+        
+        return (lambda x: x if x < inf else -1)(fn(board, hand))
+
+
     """490. The Maze (Medium)
 	There is a ball in a maze with empty spaces (represented as 0) and walls 
 	(represented as 1). The ball can go through the empty spaces by rolling up, 
