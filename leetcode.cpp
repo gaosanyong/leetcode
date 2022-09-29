38147a7b2e1a57223491765a529c3f2c9cfb9c22
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-09-28 22:19:56 -0400

    solve'em all (2229)

--- leetcode.cpp
+++ leetcode.cpp
@@ -43632,6 +43632,49 @@ public:
     }
 
 
+    /*2229. Check if an Array Is Consecutive (Easy)
+	Given an integer array nums, return true if nums is consecutive, otherwise 
+	return false. An array is consecutive if it contains every number in the 
+	range [x, x + n - 1] (inclusive), where x is the minimum number in the 
+	array and n is the length of the array.
+
+	Example 1:
+	Input: nums = [1,3,4,2]
+	Output: true
+	Explanation: The minimum value is 1 and the length of nums is 4. All of the 
+	             values in the range [x, x + n - 1] = [1, 1 + 4 - 1] = [1, 4] 
+	             = (1, 2, 3, 4) occur in nums. Therefore, nums is consecutive.
+	
+	Example 2:
+	Input: nums = [1,3]
+	Output: false
+	Explanation: The minimum value is 1 and the length of nums is 2. The value 
+	             2 in the range [x, x + n - 1] = [1, 1 + 2 - 1], = [1, 2] 
+	             = (1, 2) does not occur in nums. Therefore, nums is not 
+	             consecutive.
+	
+	Example 3:
+	Input: nums = [3,5,4]
+	Output: true
+	Explanation: The minimum value is 3 and the length of nums is 3. All of the 
+	             values in the range [x, x + n - 1] = [3, 3 + 3 - 1] = [3, 5] 
+	             = (3, 4, 5) occur in nums. Therefore, nums is consecutive.
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* 0 <= nums[i] <= 10^5*/
+
+    bool isConsecutive(vector<int>& nums) {
+        int m = *min_element(nums.begin(), nums.end()), n = nums.size(); 
+        vector<bool> seen(n); 
+        for (auto& x : nums) {
+            if (x >= m + n || seen[x-m]) return false; 
+            seen[x-m] = true; 
+        }
+        return true; 
+    }
+
+
     /*2235. Add Two Integers (Easy)
 	Given two integers num1 and num2, return the sum of the two integers.
 
--- leetcode.py
+++ leetcode.py
@@ -59095,6 +59095,42 @@ class Trie:
         return lo 
 
 
+    """2229. Check if an Array Is Consecutive (Easy)
+	Given an integer array nums, return true if nums is consecutive, otherwise 
+	return false. An array is consecutive if it contains every number in the 
+	range [x, x + n - 1] (inclusive), where x is the minimum number in the 
+	array and n is the length of the array.
+
+	Example 1:
+	Input: nums = [1,3,4,2]
+	Output: true
+	Explanation: The minimum value is 1 and the length of nums is 4. All of the 
+	             values in the range [x, x + n - 1] = [1, 1 + 4 - 1] = [1, 4] 
+	             = (1, 2, 3, 4) occur in nums. Therefore, nums is consecutive.
+	
+	Example 2:
+	Input: nums = [1,3]
+	Output: false
+	Explanation: The minimum value is 1 and the length of nums is 2. The value 
+	             2 in the range [x, x + n - 1] = [1, 1 + 2 - 1], = [1, 2] 
+	             = (1, 2) does not occur in nums. Therefore, nums is not 
+	             consecutive.
+	
+	Example 3:
+	Input: nums = [3,5,4]
+	Output: true
+	Explanation: The minimum value is 3 and the length of nums is 3. All of the 
+	             values in the range [x, x + n - 1] = [3, 3 + 3 - 1] = [3, 5] 
+	             = (3, 4, 5) occur in nums. Therefore, nums is consecutive.
+
+	Constraints:
+	* 1 <= nums.length <= 10^5
+	* 0 <= nums[i] <= 10^5"""
+
+    def isConsecutive(self, nums: List[int]) -> bool:
+        return max(nums)-min(nums)+1 == len(nums) == len(set(nums))
+
+
     """2231. Largest Number After Digit Swaps by Parity (Easy)
 	You are given a positive integer num. You may swap any two digits of num 
 	that have the same parity (i.e. both odd digits or both even digits). 
