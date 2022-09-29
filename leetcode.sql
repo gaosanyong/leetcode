410bb2a573392f269d73bee22cbe595d6ce0586f
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-09-28 23:24:43 -0400

    solve'em all (2339)

--- leetcode.sql
+++ leetcode.sql
@@ -4308,3 +4308,45 @@ SELECT user_id, SUM(price * quantity) AS spending
 FROM Sales LEFT JOIN Product USING (product_id)
 GROUP BY user_id
 ORDER BY 2 DESC, 1 ASC
+
+
+/*2339. All the Matches of the League (Easy)
+SQL Schema
+Table: Teams
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| team_name   | varchar |
++-------------+---------+
+team_name is the primary key of this table. Each row of this table shows the 
+name of a team.
+
+Write an SQL query that reports all the possible matches of the league. Note 
+that every two teams play two matches with each other, with one team being the 
+home_team once and the other time being the away_team. Return the result table 
+in any order. The query result format is in the following example.
+
+Example 1:
+Input:  Teams table:
+        +-------------+
+        | team_name   |
+        +-------------+
+        | Leetcode FC |
+        | Ahly SC     |
+        | Real Madrid |
+        +-------------+
+Output: +-------------+-------------+
+        | home_team   | away_team   |
+        +-------------+-------------+
+        | Real Madrid | Leetcode FC |
+        | Real Madrid | Ahly SC     |
+        | Leetcode FC | Real Madrid |
+        | Leetcode FC | Ahly SC     |
+        | Ahly SC     | Real Madrid |
+        | Ahly SC     | Leetcode FC |
+        +-------------+-------------+
+Explanation: All the matches of the league are shown in the table.*/
+
+SELECT a.team_name AS home_team, b.team_name AS away_team
+FROM Teams AS a CROSS JOIN Teams AS b
+WHERE a.team_name != b.team_name
