bb423627fcbddb9eca91952ef60e8385d16ff71f
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-09-28 23:17:55 -0400

    solve'em all (2329)

--- leetcode.sql
+++ leetcode.sql
@@ -4249,3 +4249,62 @@ BEGIN
     WHERE startDate <= time_stamp AND time_stamp <= endDate AND amount >= minAmount
     ORDER BY 1;
 END
+
+
+/*2329. Product Sales Analysis V (Easy)
+SQL Schema
+Table: Sales
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| user_id     | int   |
+| quantity    | int   |
++-------------+-------+
+sale_id is the primary key of this table. product_id is a foreign key to 
+Product table. Each row of this table shows the ID of the product and the 
+quantity purchased by a user.
+
+Table: Product
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| product_id  | int  |
+| price       | int  |
++-------------+------+
+product_id is the primary key of this table. Each row of this table indicates 
+the price of each product.
+
+Write an SQL query that reports the spending of each user. Return the resulting 
+table ordered by spending in descending order. In case of a tie, order them by 
+user_id in ascending order. The query result format is in the following example.
+ 
+Example 1:
+Input:  Sales table:                                   Product table:
+        +---------+------------+---------+----------+  +------------+-------+
+        | sale_id | product_id | user_id | quantity |  | product_id | price |
+        +---------+------------+---------+----------+  +------------+-------+
+        | 1       | 1          | 101     | 10       |  | 1          | 10    |
+        | 2       | 2          | 101     | 1        |  | 2          | 25    |
+        | 3       | 3          | 102     | 3        |  | 3          | 15    |
+        | 4       | 3          | 102     | 2        |  +------------+-------+
+        | 5       | 2          | 103     | 3        |
+        +---------+------------+---------+----------+
+Output: +---------+----------+
+        | user_id | spending |
+        +---------+----------+
+        | 101     | 125      |
+        | 102     | 75       |
+        | 103     | 75       |
+        +---------+----------+
+Explanation: User 101 spent 10 * 10 + 1 * 25 = 125.
+             User 102 spent 3 * 15 + 2 * 15 = 75.
+             User 103 spent 3 * 25 = 75.
+             Users 102 and 103 spent the same amount and we break the tie by 
+             their ID while user 101 is on the top.*/
+
+SELECT user_id, SUM(price * quantity) AS spending
+FROM Sales LEFT JOIN Product USING (product_id)
+GROUP BY user_id
+ORDER BY 2 DESC, 1 ASC
