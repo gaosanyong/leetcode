fd9b44be510d0275af1728131f0fb30f8dbafcfa
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-09-28 22:43:01 -0400

    solve'em all (2072)

--- leetcode.sql
+++ leetcode.sql
@@ -3977,3 +3977,123 @@ SELECT problem_id
 FROM Problems
 WHERE likes/(likes + dislikes) < 0.6
 ORDER BY 1
+
+
+/*2072. The Winner University (Easy)
+SQL Schema
+Table: NewYork
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| student_id  | int  |
+| score       | int  |
++-------------+------+
+student_id is the primary key for this table. Each row contains information 
+about the score of one student from New York University in an exam.
+
+Table: California
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| student_id  | int  |
+| score       | int  |
++-------------+------+
+student_id is the primary key for this table. Each row contains information 
+about the score of one student from California University in an exam. There is 
+a competition between New York University and California University. The 
+competition is held between the same number of students from both universities. 
+The university that has more excellent students wins the competition. If the 
+two universities have the same number of excellent students, the competition 
+ends in a draw. An excellent student is a student that scored 90% or more in 
+the exam. Write an SQL query to report:
+"New York University" if New York University wins the competition.
+"California University" if California University wins the competition.
+"No Winner" if the competition ends in a draw.
+The query result format is in the following example.
+
+Example 1:
+Input: 
+NewYork table:
++------------+-------+
+| student_id | score |
++------------+-------+
+| 1          | 90    |
+| 2          | 87    |
++------------+-------+
+California table:
++------------+-------+
+| student_id | score |
++------------+-------+
+| 2          | 89    |
+| 3          | 88    |
++------------+-------+
+Output: 
++---------------------+
+| winner              |
++---------------------+
+| New York University |
++---------------------+
+Explanation: New York University has 1 excellent student, and California 
+             University has 0 excellent students.
+
+Example 2:
+Input: 
+NewYork table:
++------------+-------+
+| student_id | score |
++------------+-------+
+| 1          | 89    |
+| 2          | 88    |
++------------+-------+
+California table:
++------------+-------+
+| student_id | score |
++------------+-------+
+| 2          | 90    |
+| 3          | 87    |
++------------+-------+
+Output: 
++-----------------------+
+| winner                |
++-----------------------+
+| California University |
++-----------------------+
+Explanation: New York University has 0 excellent students, and California 
+             University has 1 excellent student.
+
+Example 3:
+Input: 
+NewYork table:
++------------+-------+
+| student_id | score |
++------------+-------+
+| 1          | 89    |
+| 2          | 90    |
++------------+-------+
+California table:
++------------+-------+
+| student_id | score |
++------------+-------+
+| 2          | 87    |
+| 3          | 99    |
++------------+-------+
+Output: 
++-----------+
+| winner    |
++-----------+
+| No Winner |
++-----------+
+Explanation: Both New York University and California University have 1 
+             excellent student.*/
+
+WITH cte AS (
+    SELECT 
+        (SELECT COUNT(*) FROM NewYork WHERE score >= 90) AS NY,
+        (SELECT COUNT(*) FROM California WHERE score >= 90) AS CA
+)
+SELECT CASE
+        WHEN NY > CA THEN 'New York University'
+        WHEN NY < CA THEN 'California University'
+    ELSE 'No Winner'
+    END AS winner
+FROM cte
