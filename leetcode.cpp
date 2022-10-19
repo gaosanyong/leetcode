f38901b92f2e29f13537ea6884e8a1c3d393b4fa
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-19 13:02:20 -0700

    solve'em all (2098)

--- leetcode.cpp
+++ leetcode.cpp
@@ -40280,6 +40280,49 @@ public:
     }
 
 
+    /*2098. Subsequence of Size K With the Largest Even Sum (Medium)
+    You are given an integer array nums and an integer k. Find the largest even 
+    sum of any subsequence of nums that has a length of k. Return this sum, or 
+    -1 if such a sum does not exist. A subsequence is an array that can be 
+    derived from another array by deleting some or no elements without changing 
+    the order of the remaining elements.
+
+    Example 1:
+    Input: nums = [4,1,5,3,1], k = 3
+    Output: 12
+    Explanation: The subsequence with the largest possible even sum is [4,5,3]. 
+                 It has a sum of 4 + 5 + 3 = 12.
+    
+    Example 2:
+    Input: nums = [4,6,2], k = 3
+    Output: 12
+    Explanation: The subsequence with the largest possible even sum is [4,6,2]. 
+                 It has a sum of 4 + 6 + 2 = 12.
+    
+    Example 3:
+    Input: nums = [1,3,5], k = 1
+    Output: -1
+    Explanation: No subsequence of nums with length 1 has an even sum.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 0 <= nums[i] <= 10^5
+    * 1 <= k <= nums.length*/
+
+    long long largestEvenSum(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end(), greater<>()); 
+        long long ans = -1, prefix = 0; 
+        vector<int> least(2, INT_MAX); 
+        for (int i = 0; i < nums.size(); ++i) 
+            if (i < k) {
+                prefix += nums[i]; 
+                least[nums[i]&1] = min(least[nums[i]&1], nums[i]); 
+                if (i == k-1 && !(prefix&1)) return prefix; 
+            } else ans = max(ans, prefix - least[!(nums[i]&1)] + nums[i]); 
+        return ans; 
+    }
+
+
     /*2099. Find Subsequence of Length K With the Largest Sum (Easy)
     You are given an integer array nums and an integer k. You want to find a 
     subsequence of nums of length k that has the largest sum. Return any such 
--- leetcode.py
+++ leetcode.py
@@ -54669,6 +54669,48 @@ class Trie:
         return [[ans[i], ans[i+1]] for i in range(len(ans)-1)]
 
 
+    """2098. Subsequence of Size K With the Largest Even Sum (Medium)
+    You are given an integer array nums and an integer k. Find the largest even 
+    sum of any subsequence of nums that has a length of k. Return this sum, or 
+    -1 if such a sum does not exist. A subsequence is an array that can be 
+    derived from another array by deleting some or no elements without changing 
+    the order of the remaining elements.
+
+    Example 1:
+    Input: nums = [4,1,5,3,1], k = 3
+    Output: 12
+    Explanation: The subsequence with the largest possible even sum is [4,5,3]. 
+                 It has a sum of 4 + 5 + 3 = 12.
+    
+    Example 2:
+    Input: nums = [4,6,2], k = 3
+    Output: 12
+    Explanation: The subsequence with the largest possible even sum is [4,6,2]. 
+                 It has a sum of 4 + 6 + 2 = 12.
+    
+    Example 3:
+    Input: nums = [1,3,5], k = 1
+    Output: -1
+    Explanation: No subsequence of nums with length 1 has an even sum.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 0 <= nums[i] <= 10^5
+    * 1 <= k <= nums.length"""
+
+    def largestEvenSum(self, nums: List[int], k: int) -> int:
+        ans = -1 
+        least = [inf, inf]
+        prefix = 0 
+        for i, x in enumerate(sorted(nums, reverse=True)): 
+            if i < k: 
+                prefix += x
+                least[x&1] = min(least[x&1], x)
+                if i == k-1 and not prefix&1: return prefix 
+            else: ans = max(ans, prefix - least[not x&1] + x)
+        return ans 
+
+
     """2099. Find Subsequence of Length K With the Largest Sum (Easy)
     You are given an integer array nums and an integer k. You want to find a 
     subsequence of nums of length k that has the largest sum. Return any such 
