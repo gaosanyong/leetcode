fb61ccf245be7c35236a9abc598e316abc4d588f
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-19 12:50:48 -0700

    solve'em all (2128)

--- leetcode.cpp
+++ leetcode.cpp
@@ -41614,6 +41614,45 @@ public:
     }
 
 
+    /*2128. Remove All Ones With Row and Column Flips (Medium)
+    You are given an m x n binary matrix grid. In one operation, you can choose 
+    any row or column and flip each value in that row or column (i.e., changing 
+    all 0's to 1's, and all 1's to 0's). Return true if it is possible to 
+    remove all 1's from grid using any number of operations or false otherwise.
+
+    Example 1:
+    Input: grid = [[0,1,0],[1,0,1],[0,1,0]]
+    Output: true
+    Explanation: One possible way to remove all 1's from grid is to:
+                 - Flip the middle row
+                 - Flip the middle column
+    
+    Example 2:
+    Input: grid = [[1,1,0],[0,0,0],[0,0,0]]
+    Output: false
+    Explanation: It is impossible to remove all 1's from grid.
+
+    Example 3:
+    Input: grid = [[0]]
+    Output: true
+    Explanation: There are no 1's in grid.
+
+    Constraints:
+    * m == grid.length
+    * n == grid[i].length
+    * 1 <= m, n <= 300
+    * grid[i][j] is either 0 or 1.*/
+
+    bool removeOnes(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(); 
+        vector<int> flip(n); 
+        for (int j = 0; j < n; ++j) flip[j] = 1^grid[0][j]; 
+        for (int i = 1; i < m; ++i) 
+            if (grid[i] != grid[0] && grid[i] != flip) return false; 
+        return true; 
+    }
+
+
     /*2133. Check if Every Row and Column Contains All Numbers (Easy)
     An n x n matrix is valid if every row and every column contains all the 
     integers from 1 to n (inclusive). Given an n x n integer matrix matrix, 
--- leetcode.py
+++ leetcode.py
@@ -55957,6 +55957,43 @@ class Trie:
         return max(ans, max(dp))
 
 
+    """2128. Remove All Ones With Row and Column Flips (Medium)
+    You are given an m x n binary matrix grid. In one operation, you can choose 
+    any row or column and flip each value in that row or column (i.e., changing 
+    all 0's to 1's, and all 1's to 0's). Return true if it is possible to 
+    remove all 1's from grid using any number of operations or false otherwise.
+
+    Example 1:
+    Input: grid = [[0,1,0],[1,0,1],[0,1,0]]
+    Output: true
+    Explanation: One possible way to remove all 1's from grid is to:
+                 - Flip the middle row
+                 - Flip the middle column
+    
+    Example 2:
+    Input: grid = [[1,1,0],[0,0,0],[0,0,0]]
+    Output: false
+    Explanation: It is impossible to remove all 1's from grid.
+
+    Example 3:
+    Input: grid = [[0]]
+    Output: true
+    Explanation: There are no 1's in grid.
+
+    Constraints:
+    * m == grid.length
+    * n == grid[i].length
+    * 1 <= m, n <= 300
+    * grid[i][j] is either 0 or 1."""
+
+    def removeOnes(self, grid: List[List[int]]) -> bool:
+        m, n = len(grid), len(grid[0])
+        flip = [x^1 for x in grid[0]]
+        for i in range(1, m): 
+            if grid[i] != grid[0] and grid[i] != flip: return False 
+        return True 
+
+
     """2133. Check if Every Row and Column Contains All Numbers (Easy)
     An n x n matrix is valid if every row and every column contains all the 
     integers from 1 to n (inclusive). Given an n x n integer matrix matrix, 
