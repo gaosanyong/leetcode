e6be63bf9c88575fad335dc127dc257542cc7aba
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-19 16:39:20 -0700

    solve'em all (2219)

--- leetcode.cpp
+++ leetcode.cpp
@@ -44349,6 +44349,45 @@ public:
     }
 
 
+    /*2219. Maximum Sum Score of Array (Medium)
+    You are given a 0-indexed integer array nums of length n. The sum score of 
+    nums at an index i where 0 <= i < n is the maximum of:
+    * The sum of the first i + 1 elements of nums.
+    * The sum of the last n - i elements of nums.
+    Return the maximum sum score of nums at any index.
+
+    Example 1:
+    Input: nums = [4,3,-2,5]
+    Output: 10
+    Explanation: The sum score at index 0 is max(4, 4 + 3 + -2 + 5) = max(4, 10) = 10.
+                 The sum score at index 1 is max(4 + 3, 3 + -2 + 5) = max(7, 6) = 7.
+                 The sum score at index 2 is max(4 + 3 + -2, -2 + 5) = max(5, 3) = 5.
+                 The sum score at index 3 is max(4 + 3 + -2 + 5, 5) = max(10, 5) = 10.
+                 The maximum sum score of nums is 10.
+    
+    Example 2:
+    Input: nums = [-3,-5]
+    Output: -3
+    Explanation: The sum score at index 0 is max(-3, -3 + -5) = max(-3, -8) = -3.
+                 The sum score at index 1 is max(-3 + -5, -5) = max(-8, -5) = -5.
+                 The maximum sum score of nums is -3.
+
+    Constraints:
+    * n == nums.length
+    * 1 <= n <= 10^5
+    * -10^5 <= nums[i] <= 10^5*/
+
+    long long maximumSumScore(vector<int>& nums) {
+        long long ans = LONG_MIN, prefix = 0, suffix = accumulate(nums.begin(), nums.end(), 0ll); 
+        for (auto& x : nums) {
+            prefix += x; 
+            ans = max({ans, prefix, suffix}); 
+            suffix -= x; 
+        }
+        return ans; 
+    }
+
+
     /*2229. Check if an Array Is Consecutive (Easy)
     Given an integer array nums, return true if nums is consecutive, otherwise 
     return false. An array is consecutive if it contains every number in the 
--- leetcode.py
+++ leetcode.py
@@ -59481,6 +59481,45 @@ class Trie:
         return fn(0, k)
 
 
+    """2219. Maximum Sum Score of Array (Medium)
+    You are given a 0-indexed integer array nums of length n. The sum score of 
+    nums at an index i where 0 <= i < n is the maximum of:
+    * The sum of the first i + 1 elements of nums.
+    * The sum of the last n - i elements of nums.
+    Return the maximum sum score of nums at any index.
+
+    Example 1:
+    Input: nums = [4,3,-2,5]
+    Output: 10
+    Explanation: The sum score at index 0 is max(4, 4 + 3 + -2 + 5) = max(4, 10) = 10.
+                 The sum score at index 1 is max(4 + 3, 3 + -2 + 5) = max(7, 6) = 7.
+                 The sum score at index 2 is max(4 + 3 + -2, -2 + 5) = max(5, 3) = 5.
+                 The sum score at index 3 is max(4 + 3 + -2 + 5, 5) = max(10, 5) = 10.
+                 The maximum sum score of nums is 10.
+    
+    Example 2:
+    Input: nums = [-3,-5]
+    Output: -3
+    Explanation: The sum score at index 0 is max(-3, -3 + -5) = max(-3, -8) = -3.
+                 The sum score at index 1 is max(-3 + -5, -5) = max(-8, -5) = -5.
+                 The maximum sum score of nums is -3.
+
+    Constraints:
+    * n == nums.length
+    * 1 <= n <= 10^5
+    * -10^5 <= nums[i] <= 10^5"""
+
+    def maximumSumScore(self, nums: List[int]) -> int:
+        prefix = 0
+        suffix = sum(nums)
+        ans = -inf 
+        for i, x in enumerate(nums):
+            prefix += x
+            ans = max(ans, prefix, suffix)
+            suffix -= x
+        return ans 
+
+
     """2220. Minimum Bit Flips to Convert Number (Easy)
     A bit flip of a number x is choosing a bit in the binary representation of 
     x and flipping it from either 0 to 1 or 1 to 0. 
