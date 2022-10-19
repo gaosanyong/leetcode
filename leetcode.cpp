1eea5651b720501c563a6d57ffa2ec1506e0d27d
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-19 12:47:33 -0700

    solve'em all (2137)

--- leetcode.cpp
+++ leetcode.cpp
@@ -41840,6 +41840,58 @@ public:
     }
 
 
+    /*2137. Pour Water Between Buckets to Make Water Levels Equal (Medium)
+    You have n buckets each containing some gallons of water in it, represented 
+    by a 0-indexed integer array buckets, where the ith bucket contains 
+    buckets[i] gallons of water. You are also given an integer loss. You want 
+    to make the amount of water in each bucket equal. You can pour any amount 
+    of water from one bucket to another bucket (not necessarily an integer). 
+    However, every time you pour k gallons of water, you spill loss percent of 
+    k. Return the maximum amount of water in each bucket after making the 
+    amount of water equal. Answers within 10-5 of the actual answer will be 
+    accepted.
+
+    Example 1:
+    Input: buckets = [1,2,7], loss = 80
+    Output: 2.00000
+    Explanation: Pour 5 gallons of water from buckets[2] to buckets[0]. 
+                 5 * 80% = 4 gallons are spilled and buckets[0] only receives 
+                 5 - 4 = 1 gallon of water. All buckets have 2 gallons of water 
+                 in them so return 2.
+    
+    Example 2:
+    Input: buckets = [2,4,6], loss = 50
+    Output: 3.50000
+    Explanation: Pour 0.5 gallons of water from buckets[1] to buckets[0].
+                 0.5 * 50% = 0.25 gallons are spilled and buckets[0] only 
+                 receives 0.5 - 0.25 = 0.25 gallons of water. Now, 
+                 buckets = [2.25, 3.5, 6]. Pour 2.5 gallons of water from 
+                 buckets[2] to buckets[0]. 2.5 * 50% = 1.25 gallons are spilled 
+                 and buckets[0] only receives 2.5 - 1.25 = 1.25 gallons of 
+                 water. All buckets have 3.5 gallons of water in them so return 
+                 3.5.
+    
+    Example 3:
+    Input: buckets = [3,3,3,3], loss = 40
+    Output: 3.00000
+    Explanation: All buckets already have the same amount of water in them.
+
+    Constraints:
+    * 1 <= buckets.length <= 10^5
+    * 0 <= buckets[i] <= 10^5
+    * 0 <= loss <= 99*/
+
+    double equalizeWater(vector<int>& buckets, int loss) {
+        sort(buckets.begin(), buckets.end()); 
+        double ans = 0, prefix = 0, total = accumulate(buckets.begin(), buckets.end(), 0.);
+        for (int i = 0, n = buckets.size(); i < n; prefix += buckets[i++]) {
+            double cand = (prefix + (1-loss/100.)*(total-prefix))/(i + (1-loss/100.)*(n-i)); 
+            if ((i == 0 || buckets[i-1] <= cand) && cand <= buckets[i]) ans = max(ans, cand); 
+        }
+        return ans; 
+    }
+
+
     /*2138. Divide a String Into Groups of Size k (Easy)
     A string s can be partitioned into groups of size k using the following 
     procedure:
--- leetcode.py
+++ leetcode.py
@@ -56167,6 +56167,59 @@ class Trie:
         return ans 
 
 
+    """2137. Pour Water Between Buckets to Make Water Levels Equal (Medium)
+    You have n buckets each containing some gallons of water in it, represented 
+    by a 0-indexed integer array buckets, where the ith bucket contains 
+    buckets[i] gallons of water. You are also given an integer loss. You want 
+    to make the amount of water in each bucket equal. You can pour any amount 
+    of water from one bucket to another bucket (not necessarily an integer). 
+    However, every time you pour k gallons of water, you spill loss percent of 
+    k. Return the maximum amount of water in each bucket after making the 
+    amount of water equal. Answers within 10-5 of the actual answer will be 
+    accepted.
+
+    Example 1:
+    Input: buckets = [1,2,7], loss = 80
+    Output: 2.00000
+    Explanation: Pour 5 gallons of water from buckets[2] to buckets[0]. 
+                 5 * 80% = 4 gallons are spilled and buckets[0] only receives 
+                 5 - 4 = 1 gallon of water. All buckets have 2 gallons of water 
+                 in them so return 2.
+    
+    Example 2:
+    Input: buckets = [2,4,6], loss = 50
+    Output: 3.50000
+    Explanation: Pour 0.5 gallons of water from buckets[1] to buckets[0].
+                 0.5 * 50% = 0.25 gallons are spilled and buckets[0] only 
+                 receives 0.5 - 0.25 = 0.25 gallons of water. Now, 
+                 buckets = [2.25, 3.5, 6]. Pour 2.5 gallons of water from 
+                 buckets[2] to buckets[0]. 2.5 * 50% = 1.25 gallons are spilled 
+                 and buckets[0] only receives 2.5 - 1.25 = 1.25 gallons of 
+                 water. All buckets have 3.5 gallons of water in them so return 
+                 3.5.
+    
+    Example 3:
+    Input: buckets = [3,3,3,3], loss = 40
+    Output: 3.00000
+    Explanation: All buckets already have the same amount of water in them.
+
+    Constraints:
+    * 1 <= buckets.length <= 10^5
+    * 0 <= buckets[i] <= 10^5
+    * 0 <= loss <= 99"""
+
+    def equalizeWater(self, buckets: List[int], loss: int) -> float:
+        buckets.sort()
+        ans = prefix = 0 
+        loss /= 100
+        total = sum(buckets)
+        for i, x in enumerate(buckets): 
+            cand = (prefix + (1-loss)*(total - prefix))/(i + (1-loss)*(len(buckets)-i))
+            if (i == 0 or buckets[i-1] <= cand) and cand <= x: ans = max(ans, cand)
+            prefix += x 
+        return ans 
+
+
     """2138. Divide a String Into Groups of Size k (Easy)
     A string s can be partitioned into groups of size k using the following 
     procedure:
