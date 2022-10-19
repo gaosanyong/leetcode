d998ed91a393020d623a747cecfcb960546e6ad0
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-19 13:12:45 -0700

    solve'em all (2083)

--- leetcode.py
+++ leetcode.py
@@ -53987,6 +53987,44 @@ class Trie:
         return ans 
 
 
+    """2083. Substrings That Begin and End With the Same Letter (Medium)
+    You are given a 0-indexed string s consisting of only lowercase English 
+    letters. Return the number of substrings in s that begin and end with the 
+    same character. A substring is a contiguous non-empty sequence of 
+    characters within a string.
+
+    Example 1:
+    Input: s = "abcba"
+    Output: 7
+    Explanation: The substrings of length 1 that start and end with the same 
+                 letter are: "a", "b", "c", "b", and "a". The substring of 
+                 length 3 that starts and ends with the same letter is: "bcb".
+                 The substring of length 5 that starts and ends with the same 
+                 letter is: "abcba".
+    
+    Example 2:
+    Input: s = "abacad"
+    Output: 9
+    Explanation: The substrings of length 1 that start and end with the same 
+                 letter are: "a", "b", "a", "c", "a", and "d". The substrings 
+                 of length 3 that start and end with the same letter are: "aba" 
+                 and "aca". The substring of length 5 that starts and ends with 
+                 the same letter is: "abaca".
+    
+    Example 3:
+    Input: s = "a"
+    Output: 1
+    Explanation: The substring of length 1 that starts and ends with the same 
+                 letter is: "a".
+
+    Constraints:
+    * 1 <= s.length <= 10^5
+    * s consists only of lowercase English letters."""
+
+    def numberOfSubstrings(self, s: str) -> int:
+        return sum(v*(v+1)//2 for v in Counter(s).values())
+
+
     """2085. Count Common Words With One Occurrence (Easy)
     Given two string arrays words1 and words2, return the number of strings 
     that appear exactly once in each of the two arrays.
