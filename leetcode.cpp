b4e2a26f4ad7263a823c697d79253d246d61aacd
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-19 19:01:25 -0700

    solve'em all (2268)

--- leetcode.cpp
+++ leetcode.cpp
@@ -44553,6 +44553,58 @@ public:
     }
 
 
+    /*2268. Minimum Number of Keypresses (Medium)
+    You have a keypad with 9 buttons, numbered from 1 to 9, each mapped to 
+    lowercase English letters. You can choose which characters each button is 
+    matched to as long as:
+    * All 26 lowercase English letters are mapped to.
+    * Each character is mapped to by exactly 1 button.
+    * Each button maps to at most 3 characters.
+    To type the first character matched to a button, you press the button once. 
+    To type the second character, you press the button twice, and so on. Given 
+    a string s, return the minimum number of keypresses needed to type s using 
+    your keypad. Note that the characters mapped to by each button, and the 
+    order they are mapped in cannot be changed.
+
+    Example 1:
+    Input: s = "apple"
+    Output: 5
+    Explanation: One optimal way to setup your keypad is shown above.
+                 Type 'a' by pressing button 1 once.
+                 Type 'p' by pressing button 6 once.
+                 Type 'p' by pressing button 6 once.
+                 Type 'l' by pressing button 5 once.
+                 Type 'e' by pressing button 3 once.
+                 A total of 5 button presses are needed, so return 5.
+    
+    Example 2:
+    Input: s = "abcdefghijkl"
+    Output: 15
+    Explanation: One optimal way to setup your keypad is shown above.
+                 The letters 'a' to 'i' can each be typed by pressing a button 
+                 once.
+                 Type 'j' by pressing button 1 twice.
+                 Type 'k' by pressing button 2 twice.
+                 Type 'l' by pressing button 3 twice.
+                 A total of 15 button presses are needed, so return 15.
+
+    Constraints:
+    * 1 <= s.length <= 10^5
+    * s consists of lowercase English letters.*/
+
+    int minimumKeypresses(string s) {
+        vector<int> freq(26); 
+        for (auto& ch : s) ++freq[ch - 'a']; 
+        sort(freq.begin(), freq.end(), greater<>()); 
+        int ans = 0; 
+        for (int i = 0, press = 0; i < 26; ++i) {
+            if (i % 9 == 0) ++press; 
+            ans += press * freq[i]; 
+        }
+        return ans; 
+    }
+
+
     /*2347. Best Poker Hand (Easy)
     You are given an integer array ranks and a character array suits. You have 
     5 cards where the ith card has a rank of ranks[i] and a suit of suits[i].
--- leetcode.py
+++ leetcode.py
@@ -60764,6 +60764,54 @@ class Trie:
         return fn(0, 0, 1)
 
 
+    """2268. Minimum Number of Keypresses (Medium)
+    You have a keypad with 9 buttons, numbered from 1 to 9, each mapped to 
+    lowercase English letters. You can choose which characters each button is 
+    matched to as long as:
+    * All 26 lowercase English letters are mapped to.
+    * Each character is mapped to by exactly 1 button.
+    * Each button maps to at most 3 characters.
+    To type the first character matched to a button, you press the button once. 
+    To type the second character, you press the button twice, and so on. Given 
+    a string s, return the minimum number of keypresses needed to type s using 
+    your keypad. Note that the characters mapped to by each button, and the 
+    order they are mapped in cannot be changed.
+
+    Example 1:
+    Input: s = "apple"
+    Output: 5
+    Explanation: One optimal way to setup your keypad is shown above.
+                 Type 'a' by pressing button 1 once.
+                 Type 'p' by pressing button 6 once.
+                 Type 'p' by pressing button 6 once.
+                 Type 'l' by pressing button 5 once.
+                 Type 'e' by pressing button 3 once.
+                 A total of 5 button presses are needed, so return 5.
+    
+    Example 2:
+    Input: s = "abcdefghijkl"
+    Output: 15
+    Explanation: One optimal way to setup your keypad is shown above.
+                 The letters 'a' to 'i' can each be typed by pressing a button 
+                 once.
+                 Type 'j' by pressing button 1 twice.
+                 Type 'k' by pressing button 2 twice.
+                 Type 'l' by pressing button 3 twice.
+                 A total of 15 button presses are needed, so return 15.
+
+    Constraints:
+    * 1 <= s.length <= 10^5
+    * s consists of lowercase English letters."""
+
+    def minimumKeypresses(self, s: str) -> int:
+        ans = press = 0 
+        freq = Counter(s)
+        for i, v in enumerate(sorted(freq.values(), reverse=True)): 
+            if i % 9 == 0: press += 1
+            ans += press * v
+        return ans 
+
+
     """2269. Find the K-Beauty of a Number (Easy)
     The k-beauty of an integer num is defined as the number of substrings of 
     num when it is read as a string that meet the following conditions:
