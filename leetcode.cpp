499a55bc466be73f2ea1a9ce198634ba029aa57b
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-20 15:38:11 -0400

    solve'em all (2345)

--- leetcode.cpp
+++ leetcode.cpp
@@ -44863,6 +44863,54 @@ public:
     }
 
 
+    /*2345. Finding the Number of Visible Mountains (Medium)
+    You are given a 0-indexed 2D integer array peaks where peaks[i] = [xi, yi] 
+    states that mountain i has a peak at coordinates (xi, yi). A mountain can 
+    be described as a right-angled isosceles triangle, with its base along the 
+    x-axis and a right angle at its peak. More formally, the gradients of 
+    ascending and descending the mountain are 1 and -1 respectively. A mountain 
+    is considered visible if its peak does not lie within another mountain 
+    (including the border of other mountains). Return the number of visible 
+    mountains.
+
+    Example 1:
+    Input: peaks = [[2,2],[6,3],[5,4]]
+    Output: 2
+    Explanation: The diagram above shows the mountains.
+                 - Mountain 0 is visible since its peak does not lie within 
+                   another mountain or its sides.
+                 - Mountain 1 is not visible since its peak lies within the 
+                   side of mountain 2.
+                 - Mountain 2 is visible since its peak does not lie within 
+                   another mountain or its sides.
+                 There are 2 mountains that are visible.
+    
+    Example 2:
+    Input: peaks = [[1,3],[1,3]]
+    Output: 0
+    Explanation: The diagram above shows the mountains (they completely overlap).
+                 Both mountains are not visible since their peaks lie within 
+                 each other.
+
+    Constraints:
+    * 1 <= peaks.length <= 10^5
+    * peaks[i].length == 2
+    * 1 <= xi, yi <= 10^5*/
+
+    int visibleMountains(vector<vector<int>>& peaks) {
+        sort(peaks.begin(), peaks.end(), [&](auto& lhs, auto& rhs) {
+            return lhs[0]-lhs[1] < rhs[0]-rhs[1] || (lhs[0]-lhs[1] == rhs[0]-rhs[1] && lhs[0]+lhs[1] > rhs[0]+rhs[1]); 
+        }); 
+        int ans = 0, right = INT_MIN; 
+        for (int i = 0, n = peaks.size(); i < n; ++i) {
+            int hi = peaks[i][0] + peaks[i][1]; 
+            if (right < hi && (i+1 == n || peaks[i] != peaks[i+1])) ++ans; 
+            right = max(right, hi); 
+        }
+        return ans; 
+    }
+
+
     /*2347. Best Poker Hand (Easy)
     You are given an integer array ranks and a character array suits. You have 
     5 cards where the ith card has a rank of ranks[i] and a suit of suits[i].
--- leetcode.py
+++ leetcode.py
@@ -63482,6 +63482,51 @@ class Trie:
         return next((i for i, x in enumerate(sorted(nums)) if g % x == 0), -1)
 
 
+    """2345. Finding the Number of Visible Mountains (Medium)
+    You are given a 0-indexed 2D integer array peaks where peaks[i] = [xi, yi] 
+    states that mountain i has a peak at coordinates (xi, yi). A mountain can 
+    be described as a right-angled isosceles triangle, with its base along the 
+    x-axis and a right angle at its peak. More formally, the gradients of 
+    ascending and descending the mountain are 1 and -1 respectively. A mountain 
+    is considered visible if its peak does not lie within another mountain 
+    (including the border of other mountains). Return the number of visible 
+    mountains.
+
+    Example 1:
+    Input: peaks = [[2,2],[6,3],[5,4]]
+    Output: 2
+    Explanation: The diagram above shows the mountains.
+                 - Mountain 0 is visible since its peak does not lie within 
+                   another mountain or its sides.
+                 - Mountain 1 is not visible since its peak lies within the 
+                   side of mountain 2.
+                 - Mountain 2 is visible since its peak does not lie within 
+                   another mountain or its sides.
+                 There are 2 mountains that are visible.
+    
+    Example 2:
+    Input: peaks = [[1,3],[1,3]]
+    Output: 0
+    Explanation: The diagram above shows the mountains (they completely overlap).
+                 Both mountains are not visible since their peaks lie within 
+                 each other.
+
+    Constraints:
+    * 1 <= peaks.length <= 10^5
+    * peaks[i].length == 2
+    * 1 <= xi, yi <= 10^5"""
+
+    def visibleMountains(self, peaks: List[List[int]]) -> int:
+        ans = 0
+        right = -inf 
+        peaks.sort(key=lambda x: (x[0]-x[1], -x[0]-x[1]))
+        for i, (p, s) in enumerate(peaks): 
+            hi = p+s
+            if right < hi and (i+1 == len(peaks) or peaks[i] != peaks[i+1]): ans += 1
+            right = max(right, hi)
+        return ans 
+
+
     """2347. Best Poker Hand (Easy)
     You are given an integer array ranks and a character array suits. You have 
     5 cards where the ith card has a rank of ranks[i] and a suit of suits[i].
