2159178ca65bc6e992079f4f635e1debab2e2a86
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-19 23:36:41 -0400

    solve'em all (2330)

--- leetcode.cpp
+++ leetcode.cpp
@@ -44774,6 +44774,47 @@ public:
     }
 
 
+    /*2330. Valid Palindrome IV (Medium)
+    You are given a 0-indexed string s consisting of only lowercase English 
+    letters. In one operation, you can change any character of s to any other 
+    character. Return true if you can make s a palindrome after performing 
+    exactly one or two operations, or return false otherwise.
+
+    Example 1:
+    Input: s = "abcdba"
+    Output: true
+    Explanation: One way to make s a palindrome using 1 operation is:
+                 - Change s[2] to 'd'. Now, s = "abddba".
+                 One operation could be performed to make s a palindrome so 
+                 return true.
+    
+    Example 2:
+    Input: s = "aa"
+    Output: true
+    Explanation: One way to make s a palindrome using 2 operations is:
+                 - Change s[0] to 'b'. Now, s = "ba".
+                 - Change s[1] to 'b'. Now, s = "bb".
+                 Two operations could be performed to make s a palindrome so 
+                 return true.
+    
+    Example 3:
+    Input: s = "abcdef"
+    Output: false
+    Explanation: It is not possible to make s a palindrome using one or two 
+                 operations so return false.
+
+    Constraints:
+    * 1 <= s.length <= 10^5
+    * s consists only of lowercase English letters.*/
+
+    bool makePalindrome(string s) {
+        int count = 0; 
+        for (int i = 0, n = s.size(); i < n/2; ++i) 
+            if (s[i] != s[n-1-i]) ++count; 
+        return count <= 2; 
+    }
+
+
     /*2347. Best Poker Hand (Easy)
     You are given an integer array ranks and a character array suits. You have 
     5 cards where the ith card has a rank of ranks[i] and a suit of suits[i].
--- leetcode.py
+++ leetcode.py
@@ -62856,6 +62856,43 @@ class Trie:
         return sum(fn(i, j) for i in range(m) for j in range(n)) % 1_000_000_007
 
 
+    """2330. Valid Palindrome IV (Medium)
+    You are given a 0-indexed string s consisting of only lowercase English 
+    letters. In one operation, you can change any character of s to any other 
+    character. Return true if you can make s a palindrome after performing 
+    exactly one or two operations, or return false otherwise.
+
+    Example 1:
+    Input: s = "abcdba"
+    Output: true
+    Explanation: One way to make s a palindrome using 1 operation is:
+                 - Change s[2] to 'd'. Now, s = "abddba".
+                 One operation could be performed to make s a palindrome so 
+                 return true.
+    
+    Example 2:
+    Input: s = "aa"
+    Output: true
+    Explanation: One way to make s a palindrome using 2 operations is:
+                 - Change s[0] to 'b'. Now, s = "ba".
+                 - Change s[1] to 'b'. Now, s = "bb".
+                 Two operations could be performed to make s a palindrome so 
+                 return true.
+    
+    Example 3:
+    Input: s = "abcdef"
+    Output: false
+    Explanation: It is not possible to make s a palindrome using one or two 
+                 operations so return false.
+
+    Constraints:
+    * 1 <= s.length <= 10^5
+    * s consists only of lowercase English letters."""
+
+    def makePalindrome(self, s: str) -> bool:
+        return sum(s[i] != s[~i] for i in range(len(s)//2)) <= 2
+
+
     """2331. Evaluate Boolean Binary Tree (Easy)
     You are given the root of a full binary tree with the following properties:
     * Leaf nodes have either the value 0 or 1, where 0 represents False and 1 
