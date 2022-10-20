f94024666dcb4f734730665c9edc6097acbb4bd1
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-19 23:36:09 -0400

    solve'em all (2323)

--- leetcode.cpp
+++ leetcode.cpp
@@ -44724,6 +44724,56 @@ public:
     }
 
 
+    /*2323. Find Minimum Time to Finish All Jobs II (Medium)
+    You are given two 0-indexed integer arrays jobs and workers of equal length, 
+    where jobs[i] is the amount of time needed to complete the ith job, and 
+    workers[j] is the amount of time the jth worker can work each day. Each job 
+    should be assigned to exactly one worker, such that each worker completes 
+    exactly one job. Return the minimum number of days needed to complete all 
+    the jobs after assignment.
+
+    Example 1:
+    Input: jobs = [5,2,4], workers = [1,7,5]
+    Output: 2
+    Explanation: - Assign the 2nd worker to the 0th job. It takes them 1 day to 
+                   finish the job.
+                 - Assign the 0th worker to the 1st job. It takes them 2 days 
+                   to finish the job.
+                 - Assign the 1st worker to the 2nd job. It takes them 1 day to 
+                   finish the job.
+                 It takes 2 days for all the jobs to be completed, so return 2.
+                 It can be proven that 2 days is the minimum number of days needed.
+    
+    Example 2:
+    Input: jobs = [3,18,15,9], workers = [6,5,1,3]
+    Output: 3
+    Explanation: - Assign the 2nd worker to the 0th job. It takes them 3 days 
+                   to finish the job.
+                 - Assign the 0th worker to the 1st job. It takes them 3 days 
+                   to finish the job.
+                 - Assign the 1st worker to the 2nd job. It takes them 3 days 
+                   to finish the job.
+                 - Assign the 3rd worker to the 3rd job. It takes them 3 days 
+                   to finish the job.
+                 It takes 3 days for all the jobs to be completed, so return 3.
+                 It can be proven that 3 days is the minimum number of days 
+                 needed.
+
+    Constraints:
+    * n == jobs.length == workers.length
+    * 1 <= n <= 10^5
+    * 1 <= jobs[i], workers[i] <= 10^5*/
+
+    int minimumTime(vector<int>& jobs, vector<int>& workers) {
+        sort(jobs.begin(), jobs.end()); 
+        sort(workers.begin(), workers.end()); 
+        int ans = 0; 
+        for (int i = 0; i < jobs.size(); ++i) 
+            ans = max(ans, (jobs[i] + workers[i] - 1)/workers[i]); 
+        return ans; 
+    }
+
+
     /*2347. Best Poker Hand (Easy)
     You are given an integer array ranks and a character array suits. You have 
     5 cards where the ith card has a rank of ranks[i] and a suit of suits[i].
--- leetcode.py
+++ leetcode.py
@@ -62639,6 +62639,50 @@ class Trie:
         return ans  
 
 
+    """2323. Find Minimum Time to Finish All Jobs II (Medium)
+    You are given two 0-indexed integer arrays jobs and workers of equal length, 
+    where jobs[i] is the amount of time needed to complete the ith job, and 
+    workers[j] is the amount of time the jth worker can work each day. Each job 
+    should be assigned to exactly one worker, such that each worker completes 
+    exactly one job. Return the minimum number of days needed to complete all 
+    the jobs after assignment.
+
+    Example 1:
+    Input: jobs = [5,2,4], workers = [1,7,5]
+    Output: 2
+    Explanation: - Assign the 2nd worker to the 0th job. It takes them 1 day to 
+                   finish the job.
+                 - Assign the 0th worker to the 1st job. It takes them 2 days 
+                   to finish the job.
+                 - Assign the 1st worker to the 2nd job. It takes them 1 day to 
+                   finish the job.
+                 It takes 2 days for all the jobs to be completed, so return 2.
+                 It can be proven that 2 days is the minimum number of days needed.
+    
+    Example 2:
+    Input: jobs = [3,18,15,9], workers = [6,5,1,3]
+    Output: 3
+    Explanation: - Assign the 2nd worker to the 0th job. It takes them 3 days 
+                   to finish the job.
+                 - Assign the 0th worker to the 1st job. It takes them 3 days 
+                   to finish the job.
+                 - Assign the 1st worker to the 2nd job. It takes them 3 days 
+                   to finish the job.
+                 - Assign the 3rd worker to the 3rd job. It takes them 3 days 
+                   to finish the job.
+                 It takes 3 days for all the jobs to be completed, so return 3.
+                 It can be proven that 3 days is the minimum number of days 
+                 needed.
+
+    Constraints:
+    * n == jobs.length == workers.length
+    * 1 <= n <= 10^5
+    * 1 <= jobs[i], workers[i] <= 10^5"""
+
+    def minimumTime(self, jobs: List[int], workers: List[int]) -> int:
+        return max((j+w-1)//w for j, w in zip(sorted(jobs), sorted(workers)))
+
+
     """2325. Decode the Message (Easy)
     You are given the strings key and message, which represent a cipher key and 
     a secret message, respectively. The steps to decode message are as follows:
