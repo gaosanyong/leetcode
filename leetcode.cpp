a4e63ac0933dabe9d134f59934193dfc8aa1eb44
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-21 18:38:00 -0400

    solve'em all (2431)

--- leetcode.cpp
+++ leetcode.cpp
@@ -45513,6 +45513,68 @@ public:
         
         return fn(nums1, copy, 0, nums1.size()); 
     }
+
+
+    /*2431. Maximize Total Tastiness of Purchased Fruits (Medium)
+    You are given two non-negative integer arrays price and tastiness, both 
+    arrays have the same length n. You are also given two non-negative integers 
+    maxAmount and maxCoupons.
+
+    For every integer i in range [0, n - 1]:
+    * price[i] describes the price of ith fruit.
+    * tastiness[i] describes the tastiness of ith fruit.
+    You want to purchase some fruits such that total tastiness is maximized and 
+    the total price does not exceed maxAmount. Additionally, you can use a 
+    coupon to purchase fruit for half of its price (rounded down to the closest 
+    integer). You can use at most maxCoupons of such coupons. Return the 
+    maximum total tastiness that can be purchased.
+
+    Note that:
+    * You can purchase each fruit at most once.
+    * You can use coupons on some fruit at most once.
+
+    Example 1:
+    Input: price = [10,20,20], tastiness = [5,8,8], maxAmount = 20, maxCoupons = 1
+    Output: 13
+    Explanation: It is possible to make total tastiness 13 in following way:
+                 - Buy first fruit without coupon, so that total price = 0 + 10 
+                   and total tastiness = 0 + 5.
+                 - Buy second fruit with coupon, so that total price = 10 + 10 
+                   and total tastiness = 5 + 8.
+                 - Do not buy third fruit, so that total price = 20 and total 
+                   tastiness = 13.
+                 It can be proven that 13 is the maximum total tastiness that 
+                 can be obtained.
+    
+    Example 2:
+    Input: price = [10,15,7], tastiness = [5,8,20], maxAmount = 10, maxCoupons = 2
+    Output: 28
+    Explanation: It is possible to make total tastiness 20 in following way:
+                 - Do not buy first fruit, so that total price = 0 and total 
+                   tastiness = 0.
+                 - Buy second fruit with coupon, so that total price = 0 + 7 
+                   and total tastiness = 0 + 8.
+                 - Buy third fruit with coupon, so that total price = 7 + 3 and 
+                   total tastiness = 8 + 20.
+                 It can be proven that 28 is the maximum total tastiness that 
+                 can be obtained.
+
+    Constraints:
+    * n == price.length == tastiness.length
+    * 1 <= n <= 100
+    * 0 <= price[i], tastiness[i], maxAmount <= 1000
+    * 0 <= maxCoupons <= 5*/
+
+    int maxTastiness(vector<int>& price, vector<int>& tastiness, int maxAmount, int maxCoupons) {
+        vector<vector<int>> dp(maxAmount+1, vector<int>(maxCoupons+1)); 
+        for (int i = 0; i < price.size(); ++i)
+            for (int j = maxAmount; j >= price[i]/2; --j) 
+                for (int k = maxCoupons; k >= 0; --k) {
+                    if (price[i] <= j) dp[j][k] = max(dp[j][k], tastiness[i] + dp[j-price[i]][k]); 
+                    if (k) dp[j][k] = max(dp[j][k], tastiness[i] + dp[j-price[i]/2][k-1]); 
+                }
+        return dp.back().back(); 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -66537,6 +66537,66 @@ class SegTree:
         return dp[0]
 
 
+    """2431. Maximize Total Tastiness of Purchased Fruits (Medium)
+    You are given two non-negative integer arrays price and tastiness, both 
+    arrays have the same length n. You are also given two non-negative integers 
+    maxAmount and maxCoupons.
+
+    For every integer i in range [0, n - 1]:
+    * price[i] describes the price of ith fruit.
+    * tastiness[i] describes the tastiness of ith fruit.
+    You want to purchase some fruits such that total tastiness is maximized and 
+    the total price does not exceed maxAmount. Additionally, you can use a 
+    coupon to purchase fruit for half of its price (rounded down to the closest 
+    integer). You can use at most maxCoupons of such coupons. Return the 
+    maximum total tastiness that can be purchased.
+
+    Note that:
+    * You can purchase each fruit at most once.
+    * You can use coupons on some fruit at most once.
+
+    Example 1:
+    Input: price = [10,20,20], tastiness = [5,8,8], maxAmount = 20, maxCoupons = 1
+    Output: 13
+    Explanation: It is possible to make total tastiness 13 in following way:
+                 - Buy first fruit without coupon, so that total price = 0 + 10 
+                   and total tastiness = 0 + 5.
+                 - Buy second fruit with coupon, so that total price = 10 + 10 
+                   and total tastiness = 5 + 8.
+                 - Do not buy third fruit, so that total price = 20 and total 
+                   tastiness = 13.
+                 It can be proven that 13 is the maximum total tastiness that 
+                 can be obtained.
+    
+    Example 2:
+    Input: price = [10,15,7], tastiness = [5,8,20], maxAmount = 10, maxCoupons = 2
+    Output: 28
+    Explanation: It is possible to make total tastiness 20 in following way:
+                 - Do not buy first fruit, so that total price = 0 and total 
+                   tastiness = 0.
+                 - Buy second fruit with coupon, so that total price = 0 + 7 
+                   and total tastiness = 0 + 8.
+                 - Buy third fruit with coupon, so that total price = 7 + 3 and 
+                   total tastiness = 8 + 20.
+                 It can be proven that 28 is the maximum total tastiness that 
+                 can be obtained.
+
+    Constraints:
+    * n == price.length == tastiness.length
+    * 1 <= n <= 100
+    * 0 <= price[i], tastiness[i], maxAmount <= 1000
+    * 0 <= maxCoupons <= 5"""
+
+    def maxTastiness(self, price: List[int], tastiness: List[int], maxAmount: int, maxCoupons: int) -> int:
+        dp = [[0] * (maxCoupons+1) for _ in range(maxAmount+1)]
+        for p, t in zip(price, tastiness): 
+            for i in range(maxAmount, p//2-1, -1): 
+                for j in range(maxCoupons, -1, -1): 
+                    if p <= i: dp[i][j] = max(dp[i][j], t + dp[i-p][j])
+                    if j: dp[i][j] = max(dp[i][j], t + dp[i-p//2][j-1])
+        return dp[-1][-1]
+
+
     """2432. The Employee That Worked on the Longest Task (Easy)
     There are n employees, each with a unique id from 0 to n - 1. You are given 
     a 2D integer array logs where logs[i] = [idi, leaveTimei] where:
