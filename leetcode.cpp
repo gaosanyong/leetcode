1e45804745712b8c30d253267468e042eaab55bc
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-21 18:11:19 -0400

    solve'em all (2422)

--- leetcode.cpp
+++ leetcode.cpp
@@ -45340,6 +45340,51 @@ public:
     }
 
 
+    /*2422. Merge Operations to Turn Array Into a Palindrome (Medium)
+    You are given an array nums consisting of positive integers. You can 
+    perform the following operation on the array any number of times:
+    * Choose any two adjacent elements and replace them with their sum.
+      + For example, if nums = [1,2,3,1], you can apply one operation to make 
+        it [1,5,1].
+    Return the minimum number of operations needed to turn the array into a 
+    palindrome.
+
+    Example 1:
+    Input: nums = [4,3,2,1,2,3,1]
+    Output: 2
+    Explanation: We can turn the array into a palindrome in 2 operations as 
+                 follows:
+                 - Apply the operation on the fourth and fifth element of the 
+                   array, nums becomes equal to [4,3,2,3,3,1].
+                 - Apply the operation on the fifth and sixth element of the 
+                   array, nums becomes equal to [4,3,2,3,4].
+                 The array [4,3,2,3,4] is a palindrome. It can be shown that 2 
+                 is the minimum number of operations needed.
+    
+    Example 2:
+    Input: nums = [1,2,3,4]
+    Output: 3
+    Explanation: We do the operation 3 times in any position, we obtain the 
+                 array [10] at the end which is a palindrome.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 1 <= nums[i] <= 10^6*/
+
+    int minimumOperations(vector<int>& nums) {
+        int ans = 0; 
+        for (int lo = -1, hi = nums.size(), diff = 0; lo < hi; ) 
+            if (diff > 0) {
+                ++ans; 
+                diff -= nums[--hi]; 
+            } else if (diff < 0) {
+                ++ans; 
+                diff += nums[++lo]; 
+            } else diff = nums[++lo] - nums[--hi]; 
+        return ans; 
+    }
+
+
     /*2423. Remove Letter To Equalize Frequency (Easy)
     You are given a 0-indexed string word, consisting of lowercase English 
     letters. You need to select one index and remove the letter at that index 
