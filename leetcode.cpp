62a9a3d369ab70e2d506d04a4ef85a259d15b6c6
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-21 15:30:24 -0400

    solve'em all (2393)

--- leetcode.cpp
+++ leetcode.cpp
@@ -45106,6 +45106,40 @@ public:
     }
 
 
+    /*2393. Count Strictly Increasing Subarrays (Medium)
+    You are given an array nums consisting of positive integers. Return the 
+    number of subarrays of nums that are in strictly increasing order. A 
+    subarray is a contiguous part of an array.
+
+    Example 1:
+    Input: nums = [1,3,5,4,4,6]
+    Output: 10
+    Explanation: The strictly increasing subarrays are the following:
+                 - Subarrays of length 1: [1], [3], [5], [4], [4], [6].
+                 - Subarrays of length 2: [1,3], [3,5], [4,6].
+                 - Subarrays of length 3: [1,3,5].
+                 The total number of subarrays is 6 + 3 + 1 = 10.
+    
+    Example 2:
+    Input: nums = [1,2,3,4,5]
+    Output: 15
+    Explanation: Every subarray is strictly increasing. There are 15 possible 
+                 subarrays that we can take.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 1 <= nums[i] <= 10^6*/
+
+    long long countSubarrays(vector<int>& nums) {
+        long long ans = 0; 
+        for (int i = 0, cnt = 0; i < nums.size(); ++i) {
+            if (i && nums[i-1] >= nums[i]) cnt = 0; 
+            ans += ++cnt; 
+        }
+        return ans; 
+    }
+
+
     /*2418. Sort the People (Easy)
     You are given an array of strings names, and an array heights that consists 
     of distinct positive integers. Both arrays are of length n. For each index 
--- leetcode.py
+++ leetcode.py
@@ -65154,6 +65154,39 @@ class Trie:
         return ans 
 
 
+    """2393. Count Strictly Increasing Subarrays (Medium)
+    You are given an array nums consisting of positive integers. Return the 
+    number of subarrays of nums that are in strictly increasing order. A 
+    subarray is a contiguous part of an array.
+
+    Example 1:
+    Input: nums = [1,3,5,4,4,6]
+    Output: 10
+    Explanation: The strictly increasing subarrays are the following:
+                 - Subarrays of length 1: [1], [3], [5], [4], [4], [6].
+                 - Subarrays of length 2: [1,3], [3,5], [4,6].
+                 - Subarrays of length 3: [1,3,5].
+                 The total number of subarrays is 6 + 3 + 1 = 10.
+    
+    Example 2:
+    Input: nums = [1,2,3,4,5]
+    Output: 15
+    Explanation: Every subarray is strictly increasing. There are 15 possible 
+                 subarrays that we can take.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 1 <= nums[i] <= 10^6"""
+
+    def countSubarrays(self, nums: List[int]) -> int:
+        ans = cnt = 0 
+        for i, x in enumerate(nums): 
+            if i and nums[i-1] >= nums[i]: cnt = 0
+            cnt += 1
+            ans += cnt 
+        return ans 
+
+
     """2395. Find Subarrays With Equal Sum (Easy)
     Given a 0-indexed integer array nums, determine whether there exist two 
     subarrays of length 2 with equal sum. Note that the two subarrays must 
