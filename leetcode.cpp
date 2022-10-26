06ea7170ecabeeff00a8d87a6983c9df04a01537
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-26 15:18:45 -0400

    solve'em all (2371)

--- leetcode.cpp
+++ leetcode.cpp
@@ -45001,6 +45001,57 @@ public:
     }
 
 
+    /*2371. Minimize Maximum Value in a Grid (Hard)
+    You are given an m x n integer matrix grid containing distinct positive 
+    integers. You have to replace each integer in the matrix with a positive 
+    integer satisfying the following conditions:
+    * The relative order of every two elements that are in the same row or 
+      column should stay the same after the replacements.
+    * The maximum number in the matrix after the replacements should be as 
+      small as possible.
+    The relative order stays the same if for all pairs of elements in the 
+    original matrix such that grid[r1][c1] > grid[r2][c2] where either r1 == r2 
+    or c1 == c2, then it must be true that grid[r1][c1] > grid[r2][c2] after 
+    the replacements. For example, if grid = [[2, 4, 5], [7, 3, 9]] then a good 
+    replacement could be either grid = [[1, 2, 3], [2, 1, 4]] or 
+    grid = [[1, 2, 3], [3, 1, 4]]. Return the resulting matrix. If there are 
+    multiple answers, return any of them.
+
+    Example 1:
+    Input: grid = [[3,1],[2,5]]
+    Output: [[2,1],[1,2]]
+    Explanation: The above diagram shows a valid replacement. The maximum 
+                 number in the matrix is 2. It can be shown that no smaller 
+                 value can be obtained.
+    
+    Example 2:
+    Input: grid = [[10]]
+    Output: [[1]]
+    Explanation: We replace the only number in the matrix with 1.
+
+    Constraints:
+    * m == grid.length
+    * n == grid[i].length
+    * 1 <= m, n <= 1000
+    * 1 <= m * n <= 10^5
+    * 1 <= grid[i][j] <= 10^9
+    * grid consists of distinct integers.*/
+
+    vector<vector<int>> minScore(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(); 
+        vector<tuple<int, int, int>> vals; 
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                vals.emplace_back(grid[i][j], i, j); 
+        sort(vals.begin(), vals.end()); 
+        vector<int> row(m), col(n); 
+        vector<vector<int>> ans(m, vector<int>(n)); 
+        for (auto& [_, i, j] : vals) 
+            ans[i][j] = row[i] = col[j] = max(row[i], col[j]) + 1; 
+        return ans; 
+    }
+
+
     /*2378. Choose Edges to Maximize Score in a Tree (Medium)
     You are given a weighted tree consisting of n nodes numbered from 0 to 
     n - 1. The tree is rooted at node 0 and represented with a 2D array edges 
--- leetcode.py
+++ leetcode.py
@@ -64275,6 +64275,52 @@ class Trie:
         return max(dp)
 
 
+    """2371. Minimize Maximum Value in a Grid (Hard)
+    You are given an m x n integer matrix grid containing distinct positive 
+    integers. You have to replace each integer in the matrix with a positive 
+    integer satisfying the following conditions:
+    * The relative order of every two elements that are in the same row or 
+      column should stay the same after the replacements.
+    * The maximum number in the matrix after the replacements should be as 
+      small as possible.
+    The relative order stays the same if for all pairs of elements in the 
+    original matrix such that grid[r1][c1] > grid[r2][c2] where either r1 == r2 
+    or c1 == c2, then it must be true that grid[r1][c1] > grid[r2][c2] after 
+    the replacements. For example, if grid = [[2, 4, 5], [7, 3, 9]] then a good 
+    replacement could be either grid = [[1, 2, 3], [2, 1, 4]] or 
+    grid = [[1, 2, 3], [3, 1, 4]]. Return the resulting matrix. If there are 
+    multiple answers, return any of them.
+
+    Example 1:
+    Input: grid = [[3,1],[2,5]]
+    Output: [[2,1],[1,2]]
+    Explanation: The above diagram shows a valid replacement. The maximum 
+                 number in the matrix is 2. It can be shown that no smaller 
+                 value can be obtained.
+    
+    Example 2:
+    Input: grid = [[10]]
+    Output: [[1]]
+    Explanation: We replace the only number in the matrix with 1.
+
+    Constraints:
+    * m == grid.length
+    * n == grid[i].length
+    * 1 <= m, n <= 1000
+    * 1 <= m * n <= 10^5
+    * 1 <= grid[i][j] <= 10^9
+    * grid consists of distinct integers."""
+
+    def minScore(self, grid: List[List[int]]) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
+        ans = [[0] * n for _ in range(m)]
+        row = [0] * m
+        col = [0] * n
+        for _, i, j in sorted((grid[i][j], i, j) for i in range(m) for j in range(n)): 
+            ans[i][j] = row[i] = col[j] = max(row[i], col[j]) + 1
+        return ans 
+
+
     """2373. Largest Local Values in a Matrix (Easy)
     You are given an n x n integer matrix grid. Generate an integer matrix 
     maxLocal of size (n - 2) x (n - 2) such that:
