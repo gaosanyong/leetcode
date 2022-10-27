606a93654f663c940379d65d2d42ce8ed8d114cc
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-10-27 15:34:31 -0400

    solve'em all (2362)

--- leetcode.sql
+++ leetcode.sql
@@ -4403,6 +4403,85 @@ FROM Teacher
 GROUP BY teacher_id
 
 
+/*2362. Generate the Invoice (Hard)
+SQL Schema
+Table: Products
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| product_id  | int  |
+| price       | int  |
++-------------+------+
+product_id is the primary key for this table. Each row in this table shows the 
+ID of a product and the price of one unit.
+
+Table: Purchases
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| invoice_id  | int  |
+| product_id  | int  |
+| quantity    | int  |
++-------------+------+
+(invoice_id, product_id) is the primary key for this table. Each row in this 
+table shows the quantity ordered from one product in an invoice. 
+
+Write an SQL query to show the details of the invoice with the highest price. 
+If two or more invoices have the same price, return the details of the one with 
+the smallest invoice_id. Return the result table in any order. The query result 
+format is shown in the following example.
+
+Example 1:
+Input: 
+Products table:
++------------+-------+
+| product_id | price |
++------------+-------+
+| 1          | 100   |
+| 2          | 200   |
++------------+-------+
+Purchases table:
++------------+------------+----------+
+| invoice_id | product_id | quantity |
++------------+------------+----------+
+| 1          | 1          | 2        |
+| 3          | 2          | 1        |
+| 2          | 2          | 3        |
+| 2          | 1          | 4        |
+| 4          | 1          | 10       |
++------------+------------+----------+
+Output: 
++------------+----------+-------+
+| product_id | quantity | price |
++------------+----------+-------+
+| 2          | 3        | 600   |
+| 1          | 4        | 400   |
++------------+----------+-------+
+Explanation: Invoice 1: price = (2 * 100) = $200
+             Invoice 2: price = (4 * 100) + (3 * 200) = $1000
+             Invoice 3: price = (1 * 200) = $200
+             Invoice 4: price = (10 * 100) = $1000
+The highest price is $1000, and the invoices with the highest prices are 2 and 
+4. We return the details of the one with the smallest ID, which is invoice 2.*/
+
+WITH Cte AS (
+    SELECT 
+        invoice_id, 
+        product_id, 
+        quantity, 
+        price
+    FROM Products JOIN Purchases USING (product_id)
+)
+SELECT 
+    product_id, 
+    quantity, 
+    price*quantity AS price
+FROM Cte JOIN (SELECT invoice_id, SUM(price * quantity) AS total 
+               FROM Cte 
+               GROUP BY invoice_id 
+               ORDER BY 2 DESC, 1 ASC LIMIT 1) a USING (invoice_id)
+
+
 /*2372. Calculate the Influence of Each Salesperson (Medium)
 SQL Schema
 Table: Salesperson
