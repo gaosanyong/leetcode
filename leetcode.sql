071a0f0945cee0b8f1a28fa67c34683e59c7eef4
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-01 21:49:45 -0400

    solve'em all (2308)

--- leetcode.sql
+++ leetcode.sql
@@ -4251,6 +4251,63 @@ BEGIN
 END
 
 
+/*2308. Arrange Table by Gender (Medium)
+SQL Schema
+Table: Genders
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| user_id     | int     |
+| gender      | varchar |
++-------------+---------+
+user_id is the primary key for this table. gender is ENUM of type 'female', 
+'male', or 'other'. Each row in this table contains the ID of a user and their 
+gender. The table has an equal number of 'female', 'male', and 'other'. 
+
+Write an SQL query to rearrange the Genders table such that the rows alternate 
+between 'female', 'other', and 'male' in order. The table should be rearranged 
+such that the IDs of each gender are sorted in ascending order. Return the 
+result table in the mentioned order. The query result format is shown in the 
+following example.
+
+Example 1:
+Input: 
+Genders table: +---------+--------+
+               | user_id | gender |
+               +---------+--------+
+               | 4       | male   |
+               | 7       | female |
+               | 2       | other  |
+               | 5       | male   |
+               | 3       | female |
+               | 8       | male   |
+               | 6       | other  |
+               | 1       | other  |
+               | 9       | female |
+               +---------+--------+
+Output: +---------+--------+
+        | user_id | gender |
+        +---------+--------+
+        | 3       | female |
+        | 1       | other  |
+        | 4       | male   |
+        | 7       | female |
+        | 2       | other  |
+        | 5       | male   |
+        | 9       | female |
+        | 6       | other  |
+        | 8       | male   |
+        +---------+--------+
+Explanation: Female gender: IDs 3, 7, and 9. Other gender: IDs 1, 2, and 6. 
+             Male gender: IDs 4, 5, and 8. We arrange the table alternating 
+             between 'female', 'other', and 'male'. Note that the IDs of each 
+             gender are sorted in ascending order.*/
+
+SELECT user_id, gender
+FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY gender ORDER BY user_id) AS rnk FROM Genders) a
+ORDER BY rnk, gender
+
+
 /*2314. The First Day of the Maximum Recorded Degree in Each City (Medium)
 SQL Schema
 Table: Weather
