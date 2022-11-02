ad405be4811a300dcc8b3d251efafd2a111ba299
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-02 18:00:48 -0400

    solve'em all (2292)

--- leetcode.sql
+++ leetcode.sql
@@ -4251,6 +4251,60 @@ BEGIN
 END
 
 
+/*2292. Products With Three or More Orders in Two Consecutive Years (Medium)
+SQL Schema
+Table: Orders
++---------------+------+
+| Column Name   | Type |
++---------------+------+
+| order_id      | int  |
+| product_id    | int  |
+| quantity      | int  |
+| purchase_date | date |
++---------------+------+
+order_id is the primary key for this table. Each row in this table contains the 
+ID of an order, the id of the product purchased, the quantity, and the purchase 
+date.
+
+Write an SQL query to report the IDs of all the products that were ordered 
+three or more times in two consecutive years. Return the result table in any 
+order. The query result format is shown in the following example.
+
+Example 1:
+Input: 
+Orders table: +----------+------------+----------+---------------+
+              | order_id | product_id | quantity | purchase_date |
+              +----------+------------+----------+---------------+
+              | 1        | 1          | 7        | 2020-03-16    |
+              | 2        | 1          | 4        | 2020-12-02    |
+              | 3        | 1          | 7        | 2020-05-10    |
+              | 4        | 1          | 6        | 2021-12-23    |
+              | 5        | 1          | 5        | 2021-05-21    |
+              | 6        | 1          | 6        | 2021-10-11    |
+              | 7        | 2          | 6        | 2022-10-11    |
+              +----------+------------+----------+---------------+
+Output: +------------+
+        | product_id |
+        +------------+
+        | 1          |
+        +------------+
+Explanation: Product 1 was ordered in 2020 three times and in 2021 three times. 
+             Since it was ordered three times in two consecutive years, we 
+             include it in the answer. Product 2 was ordered one time in 2022. 
+             We do not include it in the answer.*/
+
+SELECT 
+    DISTINCT product_id 
+FROM (SELECT 
+        product_id, 
+        YEAR(purchase_date) AS curr_year, 
+        LEAD(YEAR(purchase_date)) OVER(PARTITION BY product_id ORDER BY YEAR(purchase_date)) AS next_year 
+      FROM orders 
+      GROUP BY 1, 2 
+      HAVING COUNT(order_id) >= 3) a
+WHERE next_year = curr_year+1
+
+
 /*2298. Tasks Count in the Weekend (Medium)
 SQL Schema
 Table: Tasks
