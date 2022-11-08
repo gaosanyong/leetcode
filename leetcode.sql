add0fe2e78829504de664c50c59a6bbb92deaaf3
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-08 13:45:11 -0500

    solve'em all (2238)

--- leetcode.sql
+++ leetcode.sql
@@ -4251,6 +4251,55 @@ BEGIN
 END
 
 
+/*2238. Number of Times a Driver Was a Passenger (Medium)
+SQL Schema
+Table: Rides
++--------------+------+
+| Column Name  | Type |
++--------------+------+
+| ride_id      | int  |
+| driver_id    | int  |
+| passenger_id | int  |
++--------------+------+
+ride_id is the primary key for this table. Each row of this table contains the 
+ID of the driver and the ID of the passenger that rode in ride_id. Note that 
+driver_id != passenger_id.
+
+Write an SQL query to report the ID of each driver and the number of times they 
+were a passenger. Return the result table in any order. The query result format 
+is in the following example.
+
+Example 1:
+Input: 
+Rides table: +---------+-----------+--------------+
+             | ride_id | driver_id | passenger_id |
+             +---------+-----------+--------------+
+             | 1       | 7         | 1            |
+             | 2       | 7         | 2            |
+             | 3       | 11        | 1            |
+             | 4       | 11        | 7            |
+             | 5       | 11        | 7            |
+             | 6       | 11        | 3            |
+             +---------+-----------+--------------+
+Output: +-----------+-----+
+        | driver_id | cnt |
+        +-----------+-----+
+        | 7         | 2   |
+        | 11        | 0   |
+        +-----------+-----+
+Explanation: There are two drivers in all the given rides: 7 and 11. The driver 
+             with ID = 7 was a passenger two times. The driver with ID = 11 was 
+             never a passenger.*/
+
+SELECT 
+    a.driver_id, 
+    COUNT(Rides.passenger_id) AS cnt
+FROM 
+    (SELECT DISTINCT driver_id FROM Rides) a 
+    LEFT JOIN Rides ON a.driver_id = Rides.passenger_id
+GROUP BY a.driver_id
+
+
 /*2292. Products With Three or More Orders in Two Consecutive Years (Medium)
 SQL Schema
 Table: Orders
