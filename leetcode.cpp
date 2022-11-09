cf3e9a7e4fc5fd8a5d8f15da3ad2aa454752086b
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 13:46:30 -0500

    solve'em all (1999)

--- leetcode.cpp
+++ leetcode.cpp
@@ -36064,6 +36064,50 @@ public:
     }
 
 
+    /*1999. Smallest Greater Multiple Made of Two Digits (Medium)
+    Given three integers, k, digit1, and digit2, you want to find the smallest 
+    integer that is:
+    * Larger than k,
+    * A multiple of k, and
+    * Comprised of only the digits digit1 and/or digit2.
+    Return the smallest such integer. If no such integer exists or the integer 
+    exceeds the limit of a signed 32-bit integer (2^31 - 1), return -1.
+
+    Example 1:
+    Input: k = 2, digit1 = 0, digit2 = 2
+    Output: 20
+    Explanation: 20 is the first integer larger than 2, a multiple of 2, and 
+                 comprised of only the digits 0 and/or 2.
+    
+    Example 2:
+    Input: k = 3, digit1 = 4, digit2 = 2
+    Output: 24
+    Explanation: 24 is the first integer larger than 3, a multiple of 3, and 
+                 comprised of only the digits 4 and/or 2.
+    
+    Example 3:
+    Input: k = 2, digit1 = 0, digit2 = 0
+    Output: -1
+    Explanation: No integer meets the requirements so return -1.
+
+    Constraints:
+    * 1 <= k <= 1000
+    * 0 <= digit1 <= 9
+    * 0 <= digit2 <= 9*/
+
+    int findInteger(int k, int digit1, int digit2) {
+        if (digit1 > digit2) swap(digit1, digit2); 
+        queue<long> q; q.push(0); 
+        while (q.size()) {
+            long val = q.front(); q.pop(); 
+            if (k < val && val % k == 0) return val; 
+            if (val+digit1 && 10*val+digit1 < INT_MAX) q.push(10*val+digit1); 
+            if (digit1 < digit2 && 10*val+digit2 < INT_MAX) q.push(10*val+digit2); 
+        }
+        return -1; 
+    }
+
+
     /*2000. Reverse Prefix of Word (Easy)
     Given a 0-indexed string word and a character ch, reverse the segment of 
     word that starts at index 0 and ends at the index of the first occurrence 
--- leetcode.py
+++ leetcode.py
@@ -50490,6 +50490,48 @@ class Trie:
         return all(uf.find(x) == uf.find(y) for x, y in zip(nums, sorted(nums)))
 
 
+    """1999. Smallest Greater Multiple Made of Two Digits (Medium)
+    Given three integers, k, digit1, and digit2, you want to find the smallest 
+    integer that is:
+    * Larger than k,
+    * A multiple of k, and
+    * Comprised of only the digits digit1 and/or digit2.
+    Return the smallest such integer. If no such integer exists or the integer 
+    exceeds the limit of a signed 32-bit integer (2^31 - 1), return -1.
+
+    Example 1:
+    Input: k = 2, digit1 = 0, digit2 = 2
+    Output: 20
+    Explanation: 20 is the first integer larger than 2, a multiple of 2, and 
+                 comprised of only the digits 0 and/or 2.
+    
+    Example 2:
+    Input: k = 3, digit1 = 4, digit2 = 2
+    Output: 24
+    Explanation: 24 is the first integer larger than 3, a multiple of 3, and 
+                 comprised of only the digits 4 and/or 2.
+    
+    Example 3:
+    Input: k = 2, digit1 = 0, digit2 = 0
+    Output: -1
+    Explanation: No integer meets the requirements so return -1.
+
+    Constraints:
+    * 1 <= k <= 1000
+    * 0 <= digit1 <= 9
+    * 0 <= digit2 <= 9"""
+
+    def findInteger(self, k: int, digit1: int, digit2: int) -> int:
+        if digit1 > digit2: digit1, digit2 = digit2, digit1 
+        queue = deque([0])
+        while queue: 
+            val = queue.popleft()
+            if k < val and val % k == 0: return val 
+            if val+digit1 and 10*val+digit1 < 2**31: queue.append(10*val+digit1)
+            if digit1 < digit2 and 10*val+digit2 < 2**31: queue.append(10*val+digit2)
+        return -1 
+
+
     """2000. Reverse Prefix of Word (Easy)
     Given a 0-indexed string word and a character ch, reverse the segment of 
     word that starts at index 0 and ends at the index of the first occurrence 
