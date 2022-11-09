0c502cc52356da302660fc485c3c47a6146eb6d0
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-08 22:48:41 -0500

    solve'em all (2021)

--- leetcode.cpp
+++ leetcode.cpp
@@ -36912,6 +36912,67 @@ public:
     }
 
 
+    /*2021. Brightest Position on Street (Medium)
+    A perfectly straight street is represented by a number line. The street has 
+    street lamp(s) on it and is represented by a 2D integer array lights. Each 
+    lights[i] = [positioni, rangei] indicates that there is a street lamp at 
+    position positioni that lights up the area from 
+    [positioni - rangei, positioni + rangei] (inclusive). The brightness of a 
+    position p is defined as the number of street lamp that light up the 
+    position p. Given lights, return the brightest position on the street. If 
+    there are multiple brightest positions, return the smallest one.
+
+    Example 1:
+    Input: lights = [[-3,2],[1,2],[3,3]]
+    Output: -1
+    Explanation: The first street lamp lights up the area from 
+                 [(-3) - 2, (-3) + 2] = [-5, -1]. The second street lamp lights 
+                 up the area from [1 - 2, 1 + 2] = [-1, 3]. The third street 
+                 lamp lights up the area from [3 - 3, 3 + 3] = [0, 6]. Position 
+                 -1 has a brightness of 2, illuminated by the first and second 
+                 street light. Positions 0, 1, 2, and 3 have a brightness of 2, 
+                 illuminated by the second and third street light. Out of all 
+                 these positions, -1 is the smallest, so return it.
+    
+    Example 2:
+    Input: lights = [[1,0],[0,1]]
+    Output: 1
+    Explanation: The first street lamp lights up the area from [1 - 0, 1 + 0] = 
+                 [1, 1]. The second street lamp lights up the area from 
+                 [0 - 1, 0 + 1] = [-1, 1]. Position 1 has a brightness of 2, 
+                 illuminated by the first and second street light. Return 1 
+                 because it is the brightest position on the street.
+    
+    Example 3:
+    Input: lights = [[1,2]]
+    Output: -1
+    Explanation: The first street lamp lights up the area from [1 - 2, 1 + 2] = 
+                 [-1, 3]. Positions -1, 0, 1, 2, and 3 have a brightness of 1, 
+                 illuminated by the first street light. Out of all these 
+                 positions, -1 is the smallest, so return it.
+
+    Constraints:
+    * 1 <= lights.length <= 10^5
+    * lights[i].length == 2
+    * -10^8 <= positioni <= 10^8
+    * 0 <= rangei <= 10^8*/
+
+    int brightestPosition(vector<vector<int>>& lights) {
+        vector<pair<int, int>> line; 
+        for (auto& light : lights) {
+            line.emplace_back(light[0] - light[1], 1); 
+            line.emplace_back(light[0] + light[1] + 1, -1);
+        }
+        sort(line.begin(), line.end()); 
+        int ans = 0, most = 0, prefix = 0; 
+        for (auto& [x, v] : line) {
+            prefix += v; 
+            if (prefix > most) tie(ans, most) = make_pair(x, prefix); 
+        }
+        return ans; 
+    }
+
+
     /*2022. Convert 1D Array Into 2D Array (Easy)
     You are given a 0-indexed 1-dimensional (1D) integer array original, and 
     two integers, m and n. You are tasked with creating a 2-dimensional (2D) 
--- leetcode.py
+++ leetcode.py
@@ -51271,6 +51271,65 @@ class Trie:
         return ans 
 
 
+    """2021. Brightest Position on Street (Medium)
+    A perfectly straight street is represented by a number line. The street has 
+    street lamp(s) on it and is represented by a 2D integer array lights. Each 
+    lights[i] = [positioni, rangei] indicates that there is a street lamp at 
+    position positioni that lights up the area from 
+    [positioni - rangei, positioni + rangei] (inclusive). The brightness of a 
+    position p is defined as the number of street lamp that light up the 
+    position p. Given lights, return the brightest position on the street. If 
+    there are multiple brightest positions, return the smallest one.
+
+    Example 1:
+    Input: lights = [[-3,2],[1,2],[3,3]]
+    Output: -1
+    Explanation: The first street lamp lights up the area from 
+                 [(-3) - 2, (-3) + 2] = [-5, -1]. The second street lamp lights 
+                 up the area from [1 - 2, 1 + 2] = [-1, 3]. The third street 
+                 lamp lights up the area from [3 - 3, 3 + 3] = [0, 6]. Position 
+                 -1 has a brightness of 2, illuminated by the first and second 
+                 street light. Positions 0, 1, 2, and 3 have a brightness of 2, 
+                 illuminated by the second and third street light. Out of all 
+                 these positions, -1 is the smallest, so return it.
+    
+    Example 2:
+    Input: lights = [[1,0],[0,1]]
+    Output: 1
+    Explanation: The first street lamp lights up the area from [1 - 0, 1 + 0] = 
+                 [1, 1]. The second street lamp lights up the area from 
+                 [0 - 1, 0 + 1] = [-1, 1]. Position 1 has a brightness of 2, 
+                 illuminated by the first and second street light. Return 1 
+                 because it is the brightest position on the street.
+    
+    Example 3:
+    Input: lights = [[1,2]]
+    Output: -1
+    Explanation: The first street lamp lights up the area from [1 - 2, 1 + 2] = 
+                 [-1, 3]. Positions -1, 0, 1, 2, and 3 have a brightness of 1, 
+                 illuminated by the first street light. Out of all these 
+                 positions, -1 is the smallest, so return it.
+
+    Constraints:
+    * 1 <= lights.length <= 10^5
+    * lights[i].length == 2
+    * -10^8 <= positioni <= 10^8
+    * 0 <= rangei <= 10^8"""
+
+    def brightestPosition(self, lights: List[List[int]]) -> int:
+        line = []
+        for pos, rng in lights: 
+            line.append((pos-rng, 1))
+            line.append((pos+rng+1, -1))
+        ans = most = prefix = 0 
+        for x, v in sorted(line):
+            prefix += v
+            if prefix > most: 
+                ans = x 
+                most = prefix 
+        return ans 
+
+
     """2022. Convert 1D Array Into 2D Array (Easy)
     You are given a 0-indexed 1-dimensional (1D) integer array original, and 
     two integers, m and n. You are tasked with creating a 2-dimensional (2D) 
