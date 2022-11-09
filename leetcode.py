7fc6223bee1ee54e63756d86d7b62a1d5e84ca52
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 14:10:19 -0500

    solve'em all (1538)

--- leetcode.py
+++ leetcode.py
@@ -35253,6 +35253,70 @@ class UnionFind:
                 else: lo = mid+1
 
 
+    """1538. Guess the Majority in a Hidden Array (Medium)
+    We have an integer array nums, where all the integers in nums are 0 or 1. 
+    You will not be given direct access to the array, instead, you will have an 
+    API ArrayReader which have the following functions:
+    * int query(int a, int b, int c, int d): where 
+      0 <= a < b < c < d < ArrayReader.length(). The function returns the 
+      distribution of the value of the 4 elements and returns:
+      + 4 : if the values of the 4 elements are the same (0 or 1).
+      + 2 : if three elements have a value equal to 0 and one element has value 
+            equal to 1 or vice versa.
+      + 0 : if two element have a value equal to 0 and two elements have a 
+            value equal to 1.
+    * int length(): Returns the size of the array.
+    You are allowed to call query() 2 * n times at most where n is equal to 
+    ArrayReader.length(). Return any index of the most frequent value in nums, 
+    in case of tie, return -1.
+
+    Example 1:
+    Input: nums = [0,0,1,0,1,1,1,1]
+    Output: 5
+    Explanation: The following calls to the API
+                 reader.length() // returns 8 because there are 8 elements in 
+                                 // the hidden array.
+                 reader.query(0,1,2,3) // returns 2 this is a query that 
+                                       // compares the elements nums[0], 
+                                       // nums[1], nums[2], nums[3]
+                                       // Three elements have a value equal to 0 
+                                       // and one element has value equal to 1 
+                                       // or viceversa.
+                 reader.query(4,5,6,7) // returns 4 because nums[4], nums[5], 
+                                       // nums[6], nums[7] have the same value.
+                 we can infer that the most frequent value is found in the last 
+                 4 elements. Index 2, 4, 6, 7 is also a correct answer.
+    
+    Example 2:
+    Input: nums = [0,0,1,1,0]
+    Output: 0
+
+    Example 3:
+    Input: nums = [1,0,1,0,1,0,1,0]
+    Output: -1
+
+    Constraints:
+    * 5 <= nums.length <= 10^5
+    * 0 <= nums[i] <= 1
+
+    Follow up: What is the minimum number of calls needed to find the majority 
+               element?"""
+
+    def guessMajority(self, reader: 'ArrayReader') -> int:
+        freq = [0, 0]
+        idx = [-1, -1]
+        for i in range(3, reader.length()):
+            k = reader.query(0, 1, 2, i) == reader.query(0, 1, 2, 3)
+            freq[k] += 1
+            idx[k] = i
+        for i in range(3): 
+            args = [x for x in range(5) if x != i]
+            k = reader.query(*args) == reader.query(0, 1, 2, 4)
+            freq[k] += 1
+            idx[k] = i 
+        return idx[0] if freq[0] > freq[1] else (idx[1] if freq[0] < freq[1] else -1)
+
+
     """1554. Strings Differ by One Character (Medium)
     Given a list of strings dict where all the strings are of the same length.
     Return True if there are 2 strings that only differ by 1 character in the 
