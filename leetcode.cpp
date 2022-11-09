63891cbbea3d62f43ab64103fc429675ebdba1fa
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 14:25:04 -0500

    solve'em all (1810)

--- leetcode.cpp
+++ leetcode.cpp
@@ -28188,6 +28188,127 @@ public:
     }
 
 
+    /*1810. Minimum Path Cost in a Hidden Grid (Medium)
+    This is an interactive problem. There is a robot in a hidden grid, and you 
+    are trying to get it from its starting cell to the target cell in this grid. 
+    The grid is of size m x n, and each cell in the grid is either empty or 
+    blocked. It is guaranteed that the starting cell and the target cell are 
+    different, and neither of them is blocked. Each cell has a cost that you 
+    need to pay each time you move to the cell. The starting cell's cost is not 
+    applied before the robot moves. You want to find the minimum total cost to 
+    move the robot to the target cell. However, you do not know the grid's 
+    dimensions, the starting cell, nor the target cell. You are only allowed to 
+    ask queries to the GridMaster object. The GridMaster class has the 
+    following functions:
+    * boolean canMove(char direction) Returns true if the robot can move in 
+      that direction. Otherwise, it returns false.
+    * int move(char direction) Moves the robot in that direction and returns 
+      the cost of moving to that cell. If this move would move the robot to a 
+      blocked cell or off the grid, the move will be ignored, the robot will 
+      remain in the same position, and the function will return -1.
+    * boolean isTarget() Returns true if the robot is currently on the target 
+      cell. Otherwise, it returns false.
+    Note that direction in the above functions should be a character from 
+    {'U','D','L','R'}, representing the directions up, down, left, and right, 
+    respectively. Return the minimum total cost to get the robot from its 
+    initial starting cell to the target cell. If there is no valid path between 
+    the cells, return -1.
+
+    Custom testing:
+    The test input is read as a 2D matrix grid of size m x n and four integers 
+    r1, c1, r2, and c2 where:
+    * grid[i][j] == 0 indicates that the cell (i, j) is blocked.
+    * grid[i][j] >= 1 indicates that the cell (i, j) is empty and grid[i][j] is 
+      the cost to move to that cell.
+    * (r1, c1) is the starting cell of the robot.
+    * (r2, c2) is the target cell of the robot.
+    Remember that you will not have this information in your code.
+
+    Example 1:
+    Input: grid = [[2,3],[1,1]], r1 = 0, c1 = 1, r2 = 1, c2 = 0
+    Output: 2
+    Explanation: One possible interaction is described below:
+                 The robot is initially standing on cell (0, 1), denoted by the 
+                 3.
+                 - master.canMove('U') returns false.
+                 - master.canMove('D') returns true.
+                 - master.canMove('L') returns true.
+                 - master.canMove('R') returns false.
+                 - master.move('L') moves the robot to the cell (0, 0) and 
+                   returns 2.
+                 - master.isTarget() returns false.
+                 - master.canMove('U') returns false.
+                 - master.canMove('D') returns true.
+                 - master.canMove('L') returns false.
+                 - master.canMove('R') returns true.
+                 - master.move('D') moves the robot to the cell (1, 0) and 
+                   returns 1.
+                 - master.isTarget() returns true.
+                 - master.move('L') doesn't move the robot and returns -1.
+                 - master.move('R') moves the robot to the cell (1, 1) and 
+                   returns 1.
+                 We now know that the target is the cell (1, 0), and the 
+                 minimum total cost to reach it is 2. 
+    
+    Example 2:
+    Input: grid = [[0,3,1],[3,4,2],[1,2,0]], r1 = 2, c1 = 0, r2 = 0, c2 = 2
+    Output: 9
+    Explanation: The minimum cost path is (2,0) -> (2,1) -> (1,1) -> (1,2) -> 
+                 (0,2).
+
+    Example 3:
+    Input: grid = [[1,0],[0,1]], r1 = 0, c1 = 0, r2 = 1, c2 = 1
+    Output: -1
+    Explanation: There is no path from the robot to the target cell.
+
+    Constraints:
+    * 1 <= n, m <= 100
+    * m == grid.length
+    * n == grid[i].length
+    * 0 <= grid[i][j] <= 100*/
+
+    int findShortestPath(GridMaster &master) {
+        int sz = 202, i0 = sz/2, j0 = sz/2, dir[5] = {-1, 0, 1, 0, -1};
+        vector<vector<int>> cost(sz, vector<int>(sz, -1)); 
+        string s = "LURD"; 
+        vector<int> target = {-100, -100}; 
+        
+        function<void(int, int)> dfs = [&](int i, int j) {
+            if (master.isTarget()) target = {i, j}; 
+            for (int k = 0; k < 4; ++k) {
+                if (master.canMove(s[k])) {
+                    int di = dir[k], dj = dir[k+1], ii = i+di, jj = j+dj; 
+                    if (cost[ii][jj] == -1) {
+                        cost[ii][jj] = master.move(s[k]); 
+                        dfs(ii, jj); 
+                        master.move(s[(k+2) % 4]); 
+                    }
+                }
+            }
+        }; 
+        
+        dfs(i0, j0); 
+        
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq; pq.emplace(0, i0, j0); 
+        vector<vector<int>> dist(sz, vector<int>(sz, -1)); 
+        while (pq.size()) {
+            auto [v, i, j] = pq.top(); pq.pop(); 
+            if (target[0] == i && target[1] == j) return v; 
+            for (int k = 0; k < 4; ++k) {
+                int ii = i+dir[k], jj = j+dir[k+1]; 
+                if (cost[ii][jj] != -1) {
+                    int vv = v + cost[ii][jj]; 
+                    if (dist[ii][jj] == -1 || vv < dist[ii][jj]) {
+                        dist[ii][jj] = vv; 
+                        pq.emplace(vv, ii, jj); 
+                    }
+                }
+            }
+        }
+        return -1; 
+    }
+
+
     /*1812. Determine Color of a Chessboard Square (Easy)
     You are given coordinates, a string that represents the coordinates of a 
     square of the chessboard. Below is a chessboard for your reference. Return
--- leetcode.py
+++ leetcode.py
@@ -43230,6 +43230,119 @@ class Fenwick:
         return "".join(ans)
 
 
+    """1810. Minimum Path Cost in a Hidden Grid (Medium)
+    This is an interactive problem. There is a robot in a hidden grid, and you 
+    are trying to get it from its starting cell to the target cell in this grid. 
+    The grid is of size m x n, and each cell in the grid is either empty or 
+    blocked. It is guaranteed that the starting cell and the target cell are 
+    different, and neither of them is blocked. Each cell has a cost that you 
+    need to pay each time you move to the cell. The starting cell's cost is not 
+    applied before the robot moves. You want to find the minimum total cost to 
+    move the robot to the target cell. However, you do not know the grid's 
+    dimensions, the starting cell, nor the target cell. You are only allowed to 
+    ask queries to the GridMaster object. The GridMaster class has the 
+    following functions:
+    * boolean canMove(char direction) Returns true if the robot can move in 
+      that direction. Otherwise, it returns false.
+    * int move(char direction) Moves the robot in that direction and returns 
+      the cost of moving to that cell. If this move would move the robot to a 
+      blocked cell or off the grid, the move will be ignored, the robot will 
+      remain in the same position, and the function will return -1.
+    * boolean isTarget() Returns true if the robot is currently on the target 
+      cell. Otherwise, it returns false.
+    Note that direction in the above functions should be a character from 
+    {'U','D','L','R'}, representing the directions up, down, left, and right, 
+    respectively. Return the minimum total cost to get the robot from its 
+    initial starting cell to the target cell. If there is no valid path between 
+    the cells, return -1.
+
+    Custom testing:
+    The test input is read as a 2D matrix grid of size m x n and four integers 
+    r1, c1, r2, and c2 where:
+    * grid[i][j] == 0 indicates that the cell (i, j) is blocked.
+    * grid[i][j] >= 1 indicates that the cell (i, j) is empty and grid[i][j] is 
+      the cost to move to that cell.
+    * (r1, c1) is the starting cell of the robot.
+    * (r2, c2) is the target cell of the robot.
+    Remember that you will not have this information in your code.
+
+    Example 1:
+    Input: grid = [[2,3],[1,1]], r1 = 0, c1 = 1, r2 = 1, c2 = 0
+    Output: 2
+    Explanation: One possible interaction is described below:
+                 The robot is initially standing on cell (0, 1), denoted by the 
+                 3.
+                 - master.canMove('U') returns false.
+                 - master.canMove('D') returns true.
+                 - master.canMove('L') returns true.
+                 - master.canMove('R') returns false.
+                 - master.move('L') moves the robot to the cell (0, 0) and 
+                   returns 2.
+                 - master.isTarget() returns false.
+                 - master.canMove('U') returns false.
+                 - master.canMove('D') returns true.
+                 - master.canMove('L') returns false.
+                 - master.canMove('R') returns true.
+                 - master.move('D') moves the robot to the cell (1, 0) and 
+                   returns 1.
+                 - master.isTarget() returns true.
+                 - master.move('L') doesn't move the robot and returns -1.
+                 - master.move('R') moves the robot to the cell (1, 1) and 
+                   returns 1.
+                 We now know that the target is the cell (1, 0), and the 
+                 minimum total cost to reach it is 2. 
+    
+    Example 2:
+    Input: grid = [[0,3,1],[3,4,2],[1,2,0]], r1 = 2, c1 = 0, r2 = 0, c2 = 2
+    Output: 9
+    Explanation: The minimum cost path is (2,0) -> (2,1) -> (1,1) -> (1,2) -> 
+                 (0,2).
+
+    Example 3:
+    Input: grid = [[1,0],[0,1]], r1 = 0, c1 = 0, r2 = 1, c2 = 1
+    Output: -1
+    Explanation: There is no path from the robot to the target cell.
+
+    Constraints:
+    * 1 <= n, m <= 100
+    * m == grid.length
+    * n == grid[i].length
+    * 0 <= grid[i][j] <= 100"""
+
+    def findShortestPath(self, master: 'GridMaster') -> int:
+        graph = {(0, 0) : 0}
+        oppo = {'U' : 'D', 'D' : 'U', 'L' : 'R', 'R' : 'L'}
+        step = {'U' : (0, 1), 'D' : (0, -1), 'L' : (-1, 0), 'R' : (1, 0)}
+        target = None
+        
+        def dfs(i, j): 
+            """Explore the graph"""
+            nonlocal target
+            if master.isTarget(): target = (i, j)
+            for ch in "UDLR": 
+                if master.canMove(ch): 
+                    di, dj = step[ch]
+                    ii, jj = i+di, j+dj
+                    if (ii, jj) not in graph:
+                        graph[ii, jj] = master.move(ch)
+                        dfs(ii, jj)
+                        master.move(oppo[ch])
+        
+        dfs(0, 0)
+        pq = [(0, 0, 0)]
+        dist = defaultdict(lambda : inf, {(0, 0) : 0})
+        while pq: 
+            v, i, j = heappop(pq)
+            if (i, j) == target: return v 
+            for ii, jj in (i-1, j), (i, j-1), (i, j+1), (i+1, j): 
+                if (ii, jj) in graph: 
+                    vv = v + graph[ii, jj]
+                    if vv < dist[ii, jj]: 
+                        dist[ii, jj] = vv
+                        heappush(pq, (vv, ii, jj))
+        return -1 
+
+
     """1812. Determine Color of a Chessboard Square (Easy)
     You are given coordinates, a string that represents the coordinates of a 
     square of the chessboard. Below is a chessboard for your reference. Return 
