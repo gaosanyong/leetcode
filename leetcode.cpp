a1a955575901b1361934543f8825433d681ac973
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 14:19:02 -0500

    solve'em all (1778)

--- leetcode.cpp
+++ leetcode.cpp
@@ -27276,6 +27276,120 @@ public:
     }
 
 
+    /*1778. Shortest Path in a Hidden Grid (Medium)
+    This is an interactive problem. There is a robot in a hidden grid, and you 
+    are trying to get it from its starting cell to the target cell in this grid. 
+    The grid is of size m x n, and each cell in the grid is either empty or 
+    blocked. It is guaranteed that the starting cell and the target cell are 
+    different, and neither of them is blocked. You want to find the minimum 
+    distance to the target cell. However, you do not know the grid's dimensions, 
+    the starting cell, nor the target cell. You are only allowed to ask queries 
+    to the GridMaster object. Thr GridMaster class has the following functions:
+    * boolean canMove(char direction) Returns true if the robot can move in 
+      that direction. Otherwise, it returns false.
+    * void move(char direction) Moves the robot in that direction. If this move 
+      would move the robot to a blocked cell or off the grid, the move will be 
+      ignored, and the robot will remain in the same position.
+    * boolean isTarget() Returns true if the robot is currently on the target 
+      cell. Otherwise, it returns false.
+    Note that direction in the above functions should be a character from 
+    {'U','D','L','R'}, representing the directions up, down, left, and right, 
+    respectively. Return the minimum distance between the robot's initial 
+    starting cell and the target cell. If there is no valid path between the 
+    cells, return -1.
+
+    Custom testing:
+    The test input is read as a 2D matrix grid of size m x n where:
+    * grid[i][j] == -1 indicates that the robot is in cell (i, j) (the starting 
+      cell).
+    * grid[i][j] == 0 indicates that the cell (i, j) is blocked.
+    * grid[i][j] == 1 indicates that the cell (i, j) is empty.
+    * grid[i][j] == 2 indicates that the cell (i, j) is the target cell.
+    There is exactly one -1 and 2 in grid. Remember that you will not have this 
+    information in your code.
+
+    Example 1:
+    Input: grid = [[1,2],[-1,0]]
+    Output: 2
+    Explanation: One possible interaction is described below:
+                 The robot is initially standing on cell (1, 0), denoted by the 
+                 -1.
+                 - master.canMove('U') returns true.
+                 - master.canMove('D') returns false.
+                 - master.canMove('L') returns false.
+                 - master.canMove('R') returns false.
+                 - master.move('U') moves the robot to the cell (0, 0).
+                 - master.isTarget() returns false.
+                 - master.canMove('U') returns false.
+                 - master.canMove('D') returns true.
+                 - master.canMove('L') returns false.
+                 - master.canMove('R') returns true.
+                 - master.move('R') moves the robot to the cell (0, 1).
+                 - master.isTarget() returns true. 
+                 We now know that the target is the cell (0, 1), and the 
+                 shortest path to the target cell is 2.
+    
+    Example 2:
+    Input: grid = [[0,0,-1],[1,1,1],[2,0,0]]
+    Output: 4
+    Explanation: The minimum distance between the robot and the target cell is 
+                 4.
+
+    Example 3:
+    Input: grid = [[-1,0],[0,2]]
+    Output: -1
+    Explanation: There is no path from the robot to the target cell.
+
+    Constraints:
+    * 1 <= n, m <= 500
+    * m == grid.length
+    * n == grid[i].length
+    * grid[i][j] is either -1, 0, 1, or 2.
+    * There is exactly one -1 in grid.
+    * There is exactly one 2 in grid.*/
+
+    int findShortestPath(GridMaster &master) {
+        int sz = 1002, i0 = sz/2, j0 = sz/2, dir[5] = {-1, 0, 1, 0, -1}; 
+        string s = "LURD"; 
+        vector<vector<int>> grid(sz, vector<int>(sz, 0)); 
+
+        function<void(int, int)> dfs = [&](int i, int j) {
+            grid[i][j] = 1; 
+            if (master.isTarget()) grid[i][j] = 2; 
+            for (int k = 0; k < 4; ++k) {
+                if (master.canMove(s[k])) {
+                    int di = dir[k], dj = dir[k+1], ii = i + di, jj = j + dj; 
+                    if (!grid[ii][jj]) {
+                        master.move(s[k]); 
+                        dfs(ii, jj); 
+                        master.move(s[(k+2) % 4]); 
+                    }
+                }
+            }
+        }; 
+
+        dfs(i0, j0); 
+        int ans = 0; 
+        vector<vector<bool>> seen(sz, vector<bool>(sz)); 
+        queue<pair<int, int>> q; q.emplace(i0, j0); 
+        while (q.size()) {
+            for (int sz = q.size(); sz; --sz) {
+                auto [i, j] = q.front(); q.pop(); 
+                if (grid[i][j] == 2) return ans; 
+                for (int k = 0; k < 4; ++k) {
+                    int ii = i + dir[k], jj = j + dir[k+1]; 
+                    if (grid[ii][jj] && !seen[ii][jj]) {
+                        seen[ii][jj] = true; 
+                        q.emplace(ii, jj); 
+                    }
+                }
+            }
+            ++ans; 
+        }
+        return -1; 
+    }
+
+
     /*1790. Check if One String Swap Can Make Strings Equal (Easy)
     You are given two strings s1 and s2 of equal length. A string swap is an 
     operation where you choose two indices in a string (not necessarily 
--- leetcode.py
+++ leetcode.py
@@ -42123,6 +42123,110 @@ class Fenwick:
         return ans 
 
 
+    """1778. Shortest Path in a Hidden Grid (Medium)
+    This is an interactive problem. There is a robot in a hidden grid, and you 
+    are trying to get it from its starting cell to the target cell in this grid. 
+    The grid is of size m x n, and each cell in the grid is either empty or 
+    blocked. It is guaranteed that the starting cell and the target cell are 
+    different, and neither of them is blocked. You want to find the minimum 
+    distance to the target cell. However, you do not know the grid's dimensions, 
+    the starting cell, nor the target cell. You are only allowed to ask queries 
+    to the GridMaster object. Thr GridMaster class has the following functions:
+    * boolean canMove(char direction) Returns true if the robot can move in 
+      that direction. Otherwise, it returns false.
+    * void move(char direction) Moves the robot in that direction. If this move 
+      would move the robot to a blocked cell or off the grid, the move will be 
+      ignored, and the robot will remain in the same position.
+    * boolean isTarget() Returns true if the robot is currently on the target 
+      cell. Otherwise, it returns false.
+    Note that direction in the above functions should be a character from 
+    {'U','D','L','R'}, representing the directions up, down, left, and right, 
+    respectively. Return the minimum distance between the robot's initial 
+    starting cell and the target cell. If there is no valid path between the 
+    cells, return -1.
+
+    Custom testing:
+    The test input is read as a 2D matrix grid of size m x n where:
+    * grid[i][j] == -1 indicates that the robot is in cell (i, j) (the starting 
+      cell).
+    * grid[i][j] == 0 indicates that the cell (i, j) is blocked.
+    * grid[i][j] == 1 indicates that the cell (i, j) is empty.
+    * grid[i][j] == 2 indicates that the cell (i, j) is the target cell.
+    There is exactly one -1 and 2 in grid. Remember that you will not have this 
+    information in your code.
+
+    Example 1:
+    Input: grid = [[1,2],[-1,0]]
+    Output: 2
+    Explanation: One possible interaction is described below:
+                 The robot is initially standing on cell (1, 0), denoted by the 
+                 -1.
+                 - master.canMove('U') returns true.
+                 - master.canMove('D') returns false.
+                 - master.canMove('L') returns false.
+                 - master.canMove('R') returns false.
+                 - master.move('U') moves the robot to the cell (0, 0).
+                 - master.isTarget() returns false.
+                 - master.canMove('U') returns false.
+                 - master.canMove('D') returns true.
+                 - master.canMove('L') returns false.
+                 - master.canMove('R') returns true.
+                 - master.move('R') moves the robot to the cell (0, 1).
+                 - master.isTarget() returns true. 
+                 We now know that the target is the cell (0, 1), and the 
+                 shortest path to the target cell is 2.
+    
+    Example 2:
+    Input: grid = [[0,0,-1],[1,1,1],[2,0,0]]
+    Output: 4
+    Explanation: The minimum distance between the robot and the target cell is 
+                 4.
+
+    Example 3:
+    Input: grid = [[-1,0],[0,2]]
+    Output: -1
+    Explanation: There is no path from the robot to the target cell.
+
+    Constraints:
+    * 1 <= n, m <= 500
+    * m == grid.length
+    * n == grid[i].length
+    * grid[i][j] is either -1, 0, 1, or 2.
+    * There is exactly one -1 in grid.
+    * There is exactly one 2 in grid."""
+
+    def findShortestPath(self, master: 'GridMaster') -> int:
+        diff = {'U' : (0, 1), 'D' : (0, -1), 'L' : (-1, 0), 'R' : (1, 0)}
+        oppo = {'U' : 'D', 'D' : 'U', 'L' : 'R', 'R' : 'L'}
+        graph = {}
+
+        def dfs(x, y): 
+            graph[x, y] = master.isTarget()
+            for ch in "UDLR": 
+                if master.canMove(ch):
+                    dx, dy = diff[ch] 
+                    xx, yy = x+dx, y+dy
+                    if (xx, yy) not in graph: 
+                        master.move(ch)
+                        dfs(xx, yy)
+                        master.move(oppo[ch])
+        
+        dfs(0, 0)
+        ans = 0 
+        seen = {(0, 0)}
+        queue = deque([(0, 0)])
+        while queue: 
+            for _ in range(len(queue)): 
+                x, y = queue.popleft()
+                if graph[x, y]: return ans 
+                for xx, yy in (x-1, y), (x, y-1), (x, y+1), (x+1, y): 
+                    if (xx, yy) in graph and (xx, yy) not in seen: 
+                        seen.add((xx, yy))
+                        queue.append((xx, yy))
+            ans += 1
+        return -1 
+
+
     """1779. Find Nearest Point That Has the Same X or Y Coordinate (Easy)
     You are given two integers, x and y, which represent your current location 
     on a Cartesian grid: (x, y). You are also given an array points where each 
