7eeb5ffb5ff7728e276843b2dd4ed0c66d603131
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 15:25:30 -0500

    solve'em all (1788)

--- leetcode.py
+++ leetcode.py
@@ -42547,6 +42547,57 @@ class Fenwick:
         return len(nums) - dp[0]
 
 
+    """1788. Maximize the Beauty of the Garden (Hard)
+    There is a garden of n flowers, and each flower has an integer beauty value. 
+    The flowers are arranged in a line. You are given an integer array flowers 
+    of size n and each flowers[i] represents the beauty of the ith flower. A 
+    garden is valid if it meets these conditions:
+    * The garden has at least two flowers.
+    * The first and the last flower of the garden have the same beauty value.
+    As the appointed gardener, you have the ability to remove any (possibly 
+    none) flowers from the garden. You want to remove flowers in a way that 
+    makes the remaining garden valid. The beauty of the garden is the sum of 
+    the beauty of all the remaining flowers. Return the maximum possible beauty 
+    of some valid garden after you have removed any (possibly none) flowers.
+
+    Example 1:
+    Input: flowers = [1,2,3,1,2]
+    Output: 8
+    Explanation: You can produce the valid garden [2,3,1,2] to have a total 
+                 beauty of 2 + 3 + 1 + 2 = 8.
+    
+    Example 2:
+    Input: flowers = [100,1,1,-3,1]
+    Output: 3
+    Explanation: You can produce the valid garden [1,1,1] to have a total 
+                 beauty of 1 + 1 + 1 = 3.
+    
+    Example 3:
+    Input: flowers = [-1,-2,0,-1]
+    Output: -2
+    Explanation: You can produce the valid garden [-1,-1] to have a total 
+                 beauty of -1 + -1 = -2.
+
+    Constraints:
+    * 2 <= flowers.length <= 10^5
+    * -10^4 <= flowers[i] <= 10^4
+    * It is possible to create a valid garden by removing some (possibly none) 
+      flowers."""
+
+    def maximumBeauty(self, flowers: List[int]) -> int:
+        ans = -inf 
+        seen = {}
+        prefix = 0
+        for x in flowers: 
+            prefix += max(0, x)
+            if x in seen: 
+                cand = prefix - seen[x]
+                if x < 0: cand += 2*x
+                ans = max(ans, cand)
+            else: seen[x] = prefix - max(0, x)
+        return ans 
+
+
     """1790. Check if One String Swap Can Make Strings Equal (Easy)
     You are given two strings s1 and s2 of equal length. A string swap is an 
     operation where you choose two indices in a string (not necessarily 
