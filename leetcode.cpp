9087b1f2a41d5f65905b3bcceff826e1363083cd
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 20:21:06 -0500

    solve'em all (1692)

--- leetcode.cpp
+++ leetcode.cpp
@@ -26908,6 +26908,59 @@ public:
     }
 
 
+    /*1692. Count Ways to Distribute Candies (Hard)
+    There are n unique candies (labeled 1 through n) and k bags. You are asked 
+    to distribute all the candies into the bags such that every bag has at 
+    least one candy. There can be multiple ways to distribute the candies. Two 
+    ways are considered different if the candies in one bag in the first way 
+    are not all in the same bag in the second way. The order of the bags and 
+    the order of the candies within each bag do not matter. For example, (1), 
+    (2,3) and (2), (1,3) are considered different because candies 2 and 3 in 
+    the bag (2,3) in the first way are not in the same bag in the second way 
+    (they are split between the bags (2) and (1,3)). However, (1), (2,3) and 
+    (3,2), (1) are considered the same because the candies in each bag are all 
+    in the same bags in both ways. Given two integers, n and k, return the 
+    number of different ways to distribute the candies. As the answer may be 
+    too large, return it modulo 10^9 + 7.
+
+    Example 1:
+    Input: n = 3, k = 2
+    Output: 3
+    Explanation: You can distribute 3 candies into 2 bags in 3 ways:
+                 (1), (2,3)
+                 (1,2), (3)
+                 (1,3), (2)
+    
+    Example 2:
+    Input: n = 4, k = 2
+    Output: 7
+    Explanation: You can distribute 4 candies into 2 bags in 7 ways:
+                 (1), (2,3,4)
+                 (1,2), (3,4)
+                 (1,3), (2,4)
+                 (1,4), (2,3)
+                 (1,2,3), (4)
+                 (1,2,4), (3)
+                 (1,3,4), (2)
+    
+    Example 3:
+    Input: n = 20, k = 5
+    Output: 206085257
+    Explanation: You can distribute 20 candies into 5 bags in 1881780996 ways. 
+                 1881780996 modulo 10^9 + 7 = 206085257.
+
+    Constraints: 1 <= k <= n <= 1000*/
+
+    int waysToDistribute(int n, int k) {
+        vector<vector<long>> dp(n+1, vector<long>(k+2)); 
+        dp[n][k] = 1; 
+        for (int i = n-1; i >= 0; --i) 
+            for (int j = k; j >= 0 && j >= k-n+i; --j) 
+                dp[i][j] = (j * dp[i+1][j] + dp[i+1][j+1]) % 1'000'000'007; 
+        return dp[0][0]; 
+    }
+
+
     /*1695. Maximum Erasure Value (Medium)
     You are given an array of positive integers nums and want to erase a 
     subarray containing unique elements. The score you get by erasing the 
--- leetcode.py
+++ leetcode.py
@@ -39216,6 +39216,58 @@ class Fenwick:
         return fn(0, inf, inf, inf)
 
 
+    """1692. Count Ways to Distribute Candies (Hard)
+    There are n unique candies (labeled 1 through n) and k bags. You are asked 
+    to distribute all the candies into the bags such that every bag has at 
+    least one candy. There can be multiple ways to distribute the candies. Two 
+    ways are considered different if the candies in one bag in the first way 
+    are not all in the same bag in the second way. The order of the bags and 
+    the order of the candies within each bag do not matter. For example, (1), 
+    (2,3) and (2), (1,3) are considered different because candies 2 and 3 in 
+    the bag (2,3) in the first way are not in the same bag in the second way 
+    (they are split between the bags (2) and (1,3)). However, (1), (2,3) and 
+    (3,2), (1) are considered the same because the candies in each bag are all 
+    in the same bags in both ways. Given two integers, n and k, return the 
+    number of different ways to distribute the candies. As the answer may be 
+    too large, return it modulo 10^9 + 7.
+
+    Example 1:
+    Input: n = 3, k = 2
+    Output: 3
+    Explanation: You can distribute 3 candies into 2 bags in 3 ways:
+                 (1), (2,3)
+                 (1,2), (3)
+                 (1,3), (2)
+    
+    Example 2:
+    Input: n = 4, k = 2
+    Output: 7
+    Explanation: You can distribute 4 candies into 2 bags in 7 ways:
+                 (1), (2,3,4)
+                 (1,2), (3,4)
+                 (1,3), (2,4)
+                 (1,4), (2,3)
+                 (1,2,3), (4)
+                 (1,2,4), (3)
+                 (1,3,4), (2)
+    
+    Example 3:
+    Input: n = 20, k = 5
+    Output: 206085257
+    Explanation: You can distribute 20 candies into 5 bags in 1881780996 ways. 
+                 1881780996 modulo 10^9 + 7 = 206085257.
+
+    Constraints: 1 <= k <= n <= 1000"""
+
+    def waysToDistribute(self, n: int, k: int) -> int:
+        dp = [[0]*(k+2) for _ in range(n+1)]
+        dp[n][k] = 1
+        for i in range(n-1, -1, -1): 
+            for j in range(k, max(0, k-n+i)-1, -1): 
+                dp[i][j] = (j*dp[i+1][j] + dp[i+1][j+1]) % 1_000_000_007
+        return dp[0][0]
+
+
     """1694. Reformat Phone Number (Easy)
     You are given a phone number as a string number. number consists of digits, 
     spaces ' ', and/or dashes '-'. You would like to reformat the phone number 
