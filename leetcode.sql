41ec9d29e4b5f2f5f941cd98aa0f3fd5cef03533
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-10 13:46:15 -0500

    solve'em all (1459)

--- leetcode.sql
+++ leetcode.sql
@@ -3047,6 +3047,63 @@ HAVING DATEDIFF(MAX(login_date), MIN(login_date)) >= 4
 ORDER BY 1
 
 
+/*1459. Rectangles Area (Medium)
+SQL Schema
+Table: Points
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| x_value       | int     |
+| y_value       | int     |
++---------------+---------+
+id is the primary key for this table. Each point is represented as a 2D 
+coordinate (x_value, y_value).
+
+Write an SQL query to report all possible axis-aligned rectangles with a non-
+zero area that can be formed by any two points from the Points table. Each row 
+in the result should contain three columns (p1, p2, area) where:
+* p1 and p2 are the id's of the two points that determine the opposite corners 
+  of a rectangle.
+* area is the area of the rectangle and must be non-zero.
+Return the result table ordered by area in descending order. If there is a tie, 
+order them by p1 in ascending order. If there is still a tie, order them by p2 
+in ascending order. The query result format is in the following table.
+
+Example 1:
+Input: 
+Points table:
++----------+-------------+-------------+
+| id       | x_value     | y_value     |
++----------+-------------+-------------+
+| 1        | 2           | 7           |
+| 2        | 4           | 8           |
+| 3        | 2           | 10          |
++----------+-------------+-------------+
+Output: 
++----------+-------------+-------------+
+| p1       | p2          | area        |
++----------+-------------+-------------+
+| 2        | 3           | 4           |
+| 1        | 2           | 2           |
++----------+-------------+-------------+
+Explanation: The rectangle formed by p1 = 2 and p2 = 3 has an area equal to 
+             |4-2| * |8-10| = 4. The rectangle formed by p1 = 1 and p2 = 2 has 
+             an area equal to |2-4| * |7-8| = 2. Note that the rectangle formed 
+             by p1 = 1 and p2 = 3 is invalid because the area is 0.*/
+
+SELECT 
+    a.id AS p1, 
+    b.id AS p2, 
+    ABS(a.x_value - b.x_value) * ABS(a.y_value - b.y_value) AS area
+FROM 
+    Points a JOIN Points b ON 
+        a.id < b.id AND 
+        a.x_value <> b.x_value AND 
+        a.y_value <> b.y_value
+ORDER BY 3 DESC, 1, 2
+
+
 /*1468. Calculate Salaries (Medium)
 SQL Schema
 Table Salaries:
