5729a46be1ef194c8a574756b03f613dac51c15e
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 23:09:01 -0500

    solve'em all (1270)

--- leetcode.sql
+++ leetcode.sql
@@ -1943,6 +1943,65 @@ AND
     page_id NOT IN (SELECT page_id FROM Likes WHERE user_id=1)
 
 
+/*1270. All People Report to the Given Manager (Medium)
+SQL Schema
+Table: Employees
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| employee_id   | int     |
+| employee_name | varchar |
+| manager_id    | int     |
++---------------+---------+
+employee_id is the primary key for this table. Each row of this table indicates 
+that the employee with ID employee_id and name employee_name reports his work 
+to his/her direct manager with manager_id. The head of the company is the 
+employee with employee_id = 1.
+
+Write an SQL query to find employee_id of all employees that directly or 
+indirectly report their work to the head of the company. The indirect relation 
+between managers will not exceed three managers as the company is small. Return 
+the result table in any order. The query result format is in the following 
+example.
+
+Example 1:
+Input: 
+Employees table: +-------------+---------------+------------+
+                 | employee_id | employee_name | manager_id |
+                 +-------------+---------------+------------+
+                 | 1           | Boss          | 1          |
+                 | 3           | Alice         | 3          |
+                 | 2           | Bob           | 1          |
+                 | 4           | Daniel        | 2          |
+                 | 7           | Luis          | 4          |
+                 | 8           | Jhon          | 3          |
+                 | 9           | Angela        | 8          |
+                 | 77          | Robert        | 1          |
+                 +-------------+---------------+------------+
+Output:          +-------------+
+                 | employee_id |
+                 +-------------+
+                 | 2           |
+                 | 77          |
+                 | 4           |
+                 | 7           |
+                 +-------------+
+Explanation: The head of the company is the employee with employee_id 1. The 
+             employees with employee_id 2 and 77 report their work directly to 
+             the head of the company. The employee with employee_id 4 reports 
+             their work indirectly to the head of the company 4 --> 2 --> 1. 
+             The employee with employee_id 7 reports their work indirectly to 
+             the head of the company 7 --> 4 --> 2 --> 1. The employees with 
+             employee_id 3, 8, and 9 do not report their work to the head of 
+             the company directly or indirectly. */
+
+SELECT DISTINCT t1.employee_id AS employee_id
+FROM Employees AS t1
+JOIN Employees AS t2 ON t1.manager_id = t2.employee_id
+JOIN Employees AS t3 ON t2.manager_id = t3.employee_id
+WHERE t1.employee_id <>1 AND t3.manager_id = 1
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
