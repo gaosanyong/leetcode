e378a9289312bd2e9ab2c1b57c791f07077b5970
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 21:48:49 -0500

    solve'em all (180)

--- leetcode.sql
+++ leetcode.sql
@@ -156,6 +156,49 @@ FROM Scores, (SELECT @i:=0, @prev:=-1) init
 ORDER BY Score DESC; 
 
 
+/*180. Consecutive Numbers (Medium)
+SQL Schema
+Table: Logs
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table. id is an autoincrement column.
+
+Write an SQL query to find all numbers that appear at least three times 
+consecutively. Return the result table in any order. The query result format is 
+in the following example.
+
+Example 1:
+Input: 
+Logs table:    +----+-----+
+               | id | num |
+               +----+-----+
+               | 1  | 1   |
+               | 2  | 1   |
+               | 3  | 1   |
+               | 4  | 2   |
+               | 5  | 1   |
+               | 6  | 2   |
+               | 7  | 2   |
+               +----+-----+
+Output: +-----------------+
+        | ConsecutiveNums |
+        +-----------------+
+        | 1               |
+        +-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three 
+             times.*/
+
+SELECT DISTINCT Num AS ConsecutiveNums
+FROM 
+(SELECT Num, @i:=@j AS i, @j:=@k AS j, @k:=CAST(Num AS CHAR) AS k
+FROM Logs, (SELECT @i:=NULL, @j:=NULL, @k:=NULL) a) b
+WHERE i = j AND j = k; 
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
