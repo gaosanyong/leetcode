931a486f411e2872aa42e9d9896fa560398deb70
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 22:45:17 -0500

    solve'em all (1126)

--- leetcode.sql
+++ leetcode.sql
@@ -1226,6 +1226,61 @@ GROUP BY 1, 3
 ORDER BY 1
 
 
+/*1126. Active Businesses (Medium)
+SQL Schema
+Table: Events
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| business_id   | int     |
+| event_type    | varchar |
+| occurences    | int     | 
++---------------+---------+
+(business_id, event_type) is the primary key of this table. Each row in the 
+table logs the info that an event of some type occurred at some business for a 
+number of times. The average activity for a particular event_type is the 
+average occurences across all companies that have this event. An active 
+business is a business that has more than one event_type such that their 
+occurences is strictly greater than the average activity for that event.
+
+Write an SQL query to find all active businesses. Return the result table in 
+any order. The query result format is in the following example.
+
+Example 1:
+Input: 
+Events table: +-------------+------------+------------+
+              | business_id | event_type | occurences |
+              +-------------+------------+------------+
+              | 1           | reviews    | 7          |
+              | 3           | reviews    | 3          |
+              | 1           | ads        | 11         |
+              | 2           | ads        | 7          |
+              | 3           | ads        | 6          |
+              | 1           | page views | 3          |
+              | 2           | page views | 12         |
+              +-------------+------------+------------+
+Output:                                 +-------------+
+                                        | business_id |
+                                        +-------------+
+                                        | 1           |
+                                        +-------------+
+Explanation: The average activity for each event can be calculated as follows:
+             - 'reviews': (7+3)/2 = 5
+             - 'ads': (11+7+6)/3 = 8
+             - 'page views': (3+12)/2 = 7.5
+             The business with id=1 has 7 'reviews' events (more than 5) and 11 
+             'ads' events (more than 8), so it is an active business.*/
+
+SELECT business_id
+FROM 
+    Events a
+    JOIN (SELECT event_type, AVG(occurences) as occurences
+          FROM Events 
+          GROUP BY event_type) b ON a.event_type = b.event_type AND a.occurences > b.occurences
+GROUP BY business_id
+HAVING COUNT(a.event_type) > 1
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
