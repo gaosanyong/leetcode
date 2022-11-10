9da500ebbf93de6b4b1ba5a15388a33a15160946
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 21:51:19 -0500

    solve'em all (184)

--- leetcode.sql
+++ leetcode.sql
@@ -199,6 +199,69 @@ FROM Logs, (SELECT @i:=NULL, @j:=NULL, @k:=NULL) a) b
 WHERE i = j AND j = k; 
 
 
+/*184. Department Highest Salary (Medium)
+SQL Schema
+Table: Employee
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key column for this table. departmentId is a foreign key of 
+the ID from the Department table. Each row of this table indicates the ID, name, 
+and salary of an employee. It also contains the ID of their department.
+
+Table: Department 
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table. Each row of this table indicates 
+the ID of a department and its name.
+
+Write an SQL query to find employees who have the highest salary in each of the 
+departments. Return the result table in any order. The query result format is 
+in the following example.
+
+Example 1:
+Input: 
+Employee table: +----+-------+--------+--------------+
+                | id | name  | salary | departmentId |
+                +----+-------+--------+--------------+
+                | 1  | Joe   | 70000  | 1            |
+                | 2  | Jim   | 90000  | 1            |
+                | 3  | Henry | 80000  | 2            |
+                | 4  | Sam   | 60000  | 2            |
+                | 5  | Max   | 90000  | 1            |
+                +----+-------+--------+--------------+
+Department table:                       +----+-------+
+                                        | id | name  |
+                                        +----+-------+
+                                        | 1  | IT    |
+                                        | 2  | Sales |
+                                        +----+-------+
+Output:             +------------+----------+--------+
+                    | Department | Employee | Salary |
+                    +------------+----------+--------+
+                    | IT         | Jim      | 90000  |
+                    | Sales      | Henry    | 80000  |
+                    | IT         | Max      | 90000  |
+                    +------------+----------+--------+
+Explanation: Max and Jim both have the highest salary in the IT department and 
+             Henry has the highest salary in the Sales department.*/
+
+SELECT a.Name AS Department, b.Name AS Employee, Salary
+FROM 
+    Department a, 
+    Employee b
+WHERE a.Id = b.DepartmentId AND Salary = (SELECT MAX(Salary) FROM Employee c WHERE b.DepartmentId = c.DepartmentId); 
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
