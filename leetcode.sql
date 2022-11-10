777ea494b1dd73c5337750a96983b37e0f7d3dbe
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 21:44:59 -0500

    solve'em all (177)

--- leetcode.sql
+++ leetcode.sql
@@ -51,6 +51,62 @@ SELECT
     NULL) AS SecondHighestSalary
 
 
+/*177. Nth Highest Salary (Medium)
+SQL Schema
+Table: Employee
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table. Each row of this table contains 
+information about the salary of an employee.
+
+Write an SQL query to report the nth highest salary from the Employee table. If 
+there is no nth highest salary, the query should report null. The query result 
+format is in the following example.
+
+Example 1:
+Input: 
+Employee table:    +----+--------+
+                   | id | salary |
+                   +----+--------+
+                   | 1  | 100    |
+                   | 2  | 200    |
+                   | 3  | 300    |
+                   +----+--------+
+n = 2
+Output: +------------------------+
+        | getNthHighestSalary(2) |
+        +------------------------+
+        | 200                    |
+        +------------------------+
+
+Example 2:
+Input: 
+Employee table:    +----+--------+
+                   | id | salary |
+                   +----+--------+
+                   | 1  | 100    |
+                   +----+--------+
+n = 2
+Output: +------------------------+
+        | getNthHighestSalary(2) |
+        +------------------------+
+        | null                   |
+        +------------------------+*/
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+    SELECT DISTINCT Salary
+    FROM Employee e1
+    WHERE N-1 = (SELECT COUNT(DISTINCT Salary) FROM Employee e2 WHERE e1.Salary < e2.Salary)
+  );
+END
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
