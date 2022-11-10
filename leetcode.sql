d19916fa847a888edd9c9a3fcb40a0ec4fe6131e
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 22:47:39 -0500

    solve'em all (1132)

--- leetcode.sql
+++ leetcode.sql
@@ -1281,6 +1281,82 @@ GROUP BY business_id
 HAVING COUNT(a.event_type) > 1
 
 
+/*1132. Reported Posts II (Medium)
+SQL Schema
+Table: Actions
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| post_id       | int     |
+| action_date   | date    | 
+| action        | enum    |
+| extra         | varchar |
++---------------+---------+
+There is no primary key for this table, it may have duplicate rows. The action 
+column is an ENUM type of ('view', 'like', 'reaction', 'comment', 'report', 
+'share'). The extra column has optional information about the action, such as a 
+reason for the report or a type of reaction.
+
+Table: Removals
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| post_id       | int     |
+| remove_date   | date    | 
++---------------+---------+
+post_id is the primary key of this table. Each row in this table indicates that 
+some post was removed due to being reported or as a result of an admin review.
+
+Write an SQL query to find the average daily percentage of posts that got 
+removed after being reported as spam, rounded to 2 decimal places. The query 
+result format is in the following example.
+
+Example 1:
+Input: 
+Actions table: +---------+---------+-------------+--------+--------+
+               | user_id | post_id | action_date | action | extra  |
+               +---------+---------+-------------+--------+--------+
+               | 1       | 1       | 2019-07-01  | view   | null   |
+               | 1       | 1       | 2019-07-01  | like   | null   |
+               | 1       | 1       | 2019-07-01  | share  | null   |
+               | 2       | 2       | 2019-07-04  | view   | null   |
+               | 2       | 2       | 2019-07-04  | report | spam   |
+               | 3       | 4       | 2019-07-04  | view   | null   |
+               | 3       | 4       | 2019-07-04  | report | spam   |
+               | 4       | 3       | 2019-07-02  | view   | null   |
+               | 4       | 3       | 2019-07-02  | report | spam   |
+               | 5       | 2       | 2019-07-03  | view   | null   |
+               | 5       | 2       | 2019-07-03  | report | racism |
+               | 5       | 5       | 2019-07-03  | view   | null   |
+               | 5       | 5       | 2019-07-03  | report | racism |
+               +---------+---------+-------------+--------+--------+
+Removals table:                            +---------+-------------+
+                                           | post_id | remove_date |
+                                           +---------+-------------+
+                                           | 2       | 2019-07-20  |
+                                           | 3       | 2019-07-18  |
+                                           +---------+-------------+
+Output:                                    +-----------------------+
+                                           | average_daily_percent |
+                                           +-----------------------+
+                                           | 75.00                 |
+                                           +-----------------------+
+Explanation: The percentage for 2019-07-04 is 50% because only one post of two 
+             spam reported posts were removed. The percentage for 2019-07-02 is 
+             100% because one post was reported as spam and it was removed. The 
+             other days had no spam reports so the average is 
+             (50 + 100) / 2 = 75%. Note that the output is only one number and 
+             that we do not care about the remove dates.*/
+
+SELECT ROUND(AVG(result), 2) AS average_daily_percent
+FROM (SELECT 100*COUNT(DISTINCT b.post_id)/COUNT(DISTINCT a.post_id) AS result 
+      FROM Actions a
+      LEFT JOIN Removals b ON a.post_id = b.post_id 
+      WHERE a.action = "report" AND a.extra = "spam" 
+      GROUP BY action_date) c
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
