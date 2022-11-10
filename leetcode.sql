58630c6e34fd5eeb3a1032f475db152561499f52
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 22:43:16 -0500

    solve'em all (1112)

--- leetcode.sql
+++ leetcode.sql
@@ -1177,6 +1177,55 @@ FROM (SELECT user_id, MIN(activity_date) AS login_date
 GROUP BY login_date
 
 
+/*1112. Highest Grade For Each Student (Medium)
+SQL Schema
+Table: Enrollments
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| course_id     | int     |
+| grade         | int     |
++---------------+---------+
+(student_id, course_id) is the primary key of this table.
+
+Write a SQL query to find the highest grade with its corresponding course for 
+each student. In case of a tie, you should find the course with the smallest 
+course_id. Return the result table ordered by student_id in ascending order. 
+The query result format is in the following example.
+
+Example 1:
+Input: 
+Enrollments table: +------------+-------------------+
+                   | student_id | course_id | grade |
+                   +------------+-----------+-------+
+                   | 2          | 2         | 95    |
+                   | 2          | 3         | 95    |
+                   | 1          | 1         | 90    |
+                   | 1          | 2         | 99    |
+                   | 3          | 1         | 80    |
+                   | 3          | 2         | 75    |
+                   | 3          | 3         | 82    |
+                   +------------+-----------+-------+
+Output:            +------------+-------------------+
+                   | student_id | course_id | grade |
+                   +------------+-----------+-------+
+                   | 1          | 2         | 99    |
+                   | 2          | 2         | 95    |
+                   | 3          | 3         | 82    |
+                   +------------+-----------+-------+*/
+
+SELECT student_id, MIN(course_id) AS course_id, grade
+FROM 
+    Enrollments
+    JOIN (SELECT student_id, MAX(grade) AS max_grade 
+          FROM Enrollments 
+          GROUP BY student_id) a USING (student_id)
+WHERE grade = max_grade
+GROUP BY 1, 3
+ORDER BY 1
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
