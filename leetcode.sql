2f01dfd61b9f2aa02d81a49e78ad074b22eb63b9
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 23:01:31 -0500

    solve'em all (1205)

--- leetcode.sql
+++ leetcode.sql
@@ -1712,6 +1712,79 @@ ORDER BY SUM(b.weight) DESC
 LIMIT 1
 
 
+/*1205. Monthly Transactions II (Medium)
+SQL Schema
+Table: Transactions
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| id             | int     |
+| country        | varchar |
+| state          | enum    |
+| amount         | int     |
+| trans_date     | date    |
++----------------+---------+
+id is the primary key of this table. The table has information about incoming 
+transactions. The state column is an enum of type ["approved", "declined"].
+Table: Chargebacks
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| trans_id       | int     |
+| trans_date     | date    |
++----------------+---------+
+Chargebacks contains basic information regarding incoming chargebacks from some 
+transactions placed in Transactions table. trans_id is a foreign key to the id 
+column of Transactions table. Each chargeback corresponds to a transaction made 
+previously even if they were not approved.
+
+Write an SQL query to find for each month and country: the number of approved 
+transactions and their total amount, the number of chargebacks, and their total 
+amount. Note: In your query, given the month and country, ignore rows with all 
+zeros. Return the result table in any order. The query result format is in the 
+following example.
+
+Example 1:
+Input: 
+Transactions table: +-----+---------+----------+--------+------------+
+                    | id  | country | state    | amount | trans_date |
+                    +-----+---------+----------+--------+------------+
+                    | 101 | US      | approved | 1000   | 2019-05-18 |
+                    | 102 | US      | declined | 2000   | 2019-05-19 |
+                    | 103 | US      | approved | 3000   | 2019-06-10 |
+                    | 104 | US      | declined | 4000   | 2019-06-13 |
+                    | 105 | US      | approved | 5000   | 2019-06-15 |
+                    +-----+---------+----------+--------+------------+
+Chargebacks table:                           +----------+------------+
+                                             | trans_id | trans_date |
+                                             +----------+------------+
+                                             | 102      | 2019-05-29 |
+                                             | 101      | 2019-06-30 |
+                                             | 105      | 2019-09-18 |
+                                             +----------+------------+
+Output: +---------+---------+----------------+-----------------+------------------+-------------------+
+        | month   | country | approved_count | approved_amount | chargeback_count | chargeback_amount |
+        +---------+---------+----------------+-----------------+------------------+-------------------+
+        | 2019-05 | US      | 1              | 1000            | 1                | 2000              |
+        | 2019-06 | US      | 2              | 8000            | 1                | 1000              |
+        | 2019-09 | US      | 0              | 0               | 1                | 5000              |
+        +---------+---------+----------------+-----------------+------------------+-------------------+*/
+
+SELECT 
+    LEFT(trans_date, 7) AS month, 
+    country, 
+    SUM(state = "approved") AS approved_count, 
+    SUM(IF(state = "approved", amount, 0)) AS approved_amount, 
+    SUM(state = "chargebacks") AS chargeback_count, 
+    SUM(IF(state = "chargebacks", amount, 0)) AS chargeback_amount
+FROM ((SELECT * FROM Transactions WHERE state = "approved") 
+      UNION ALL
+      (SELECT a.id, country, "chargebacks" AS state, amount, b.trans_date 
+       FROM Transactions a 
+       RIGHT JOIN Chargebacks b ON a.id = b.trans_id)) c
+GROUP BY LEFT(trans_date, 7), country
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
