111e27a19c25ebeabe81e6581f0fd1bce67050de
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 21:58:37 -0500

    solve'em all (570)

--- leetcode.sql
+++ leetcode.sql
@@ -368,6 +368,53 @@ WHERE
     CONCAT(player_id, event_date) IN (SELECT CONCAT(player_id, ADDDATE(MIN(event_date), 1)) FROM Activity GROUP BY player_id);
 
 
+/*570. Managers with at Least 5 Direct Reports (Medium)
+SQL Schema
+Table: Employee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key column for this table. Each row of this table indicates 
+the name of an employee, their department, and the id of their manager. If 
+managerId is null, then the employee does not have a manager. No employee will 
+be the manager of themself.
+
+Write an SQL query to report the managers with at least five direct reports. 
+Return the result table in any order. The query result format is in the 
+following example.
+
+Example 1:
+Input: 
+Employee table: +-----+-------+------------+-----------+
+                | id  | name  | department | managerId |
+                +-----+-------+------------+-----------+
+                | 101 | John  | A          | None      |
+                | 102 | Dan   | A          | 101       |
+                | 103 | James | A          | 101       |
+                | 104 | Amy   | A          | 101       |
+                | 105 | Anne  | A          | 101       |
+                | 106 | Ron   | B          | 101       |
+                +-----+-------+------------+-----------+
+Output:                                         +------+
+                                                | name |
+                                                +------+
+                                                | John |
+                                                +------+*/
+
+SELECT Name
+FROM Employee 
+WHERE Id IN
+    (SELECT ManagerId 
+     FROM Employee
+     GROUP BY ManagerId
+     HAVING COUNT(ManagerId) >= 5)
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
