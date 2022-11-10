cbfb06e46a7ea503b133946fe597640419683449
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 23:10:44 -0500

    solve'em all (1285)

--- leetcode.sql
+++ leetcode.sql
@@ -2002,6 +2002,54 @@ JOIN Employees AS t3 ON t2.manager_id = t3.employee_id
 WHERE t1.employee_id <>1 AND t3.manager_id = 1
 
 
+/*1285. Find the Start and End Number of Continuous Ranges (Medium)
+SQL Schema
+Table: Logs
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| log_id        | int     |
++---------------+---------+
+log_id is the primary key for this table. Each row of this table contains the 
+ID in a log Table.
+
+Write an SQL query to find the start and end number of continuous ranges in the 
+table Logs. Return the result table ordered by start_id. The query result 
+format is in the following example.
+
+Example 1:
+Input: 
+Logs table: +------------+
+            | log_id     |
+            +------------+
+            | 1          |
+            | 2          |
+            | 3          |
+            | 7          |
+            | 8          |
+            | 10         |
+            +------------+
+Output:     +------------+--------------+
+            | start_id   | end_id       |
+            +------------+--------------+
+            | 1          | 3            |
+            | 7          | 8            |
+            | 10         | 10           |
+            +------------+--------------+
+Explanation: The result table should contain all ranges in table Logs. From 1 
+             to 3 is contained in the table. From 4 to 6 is missing in the 
+             table From 7 to 8 is contained in the table. Number 9 is missing 
+             from the table. Number 10 is contained in the table.*/
+
+SELECT 
+    MIN(log_id) AS start_id, 
+    MAX(log_id) AS end_id
+FROM 
+    (SELECT *, @id:=@id+1 AS id 
+     FROM Logs, (SELECT @id:=0) init) a
+GROUP BY log_id - id
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
