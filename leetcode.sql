c579e971a4125a1c64902f704bbff84d6da32893
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 22:30:43 -0500

    solve'em all (626)

--- leetcode.sql
+++ leetcode.sql
@@ -809,6 +809,55 @@ GROUP BY followee
 ORDER BY followee
 
 
+/*626. Exchange Seats (Medium)
+SQL Schema
+Table: Seat
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key column for this table. Each row of this table indicates 
+the name and the ID of a student. id is a continuous increment.
+
+Write an SQL query to swap the seat id of every two consecutive students. If 
+the number of students is odd, the id of the last student is not swapped. 
+Return the result table ordered by id in ascending order. The query result 
+format is in the following example.
+
+Example 1:
+Input: 
+Seat table: +----+---------+
+            | id | student |
+            +----+---------+
+            | 1  | Abbot   |
+            | 2  | Doris   |
+            | 3  | Emerson |
+            | 4  | Green   |
+            | 5  | Jeames  |
+            +----+---------+
+Output:     +----+---------+
+            | id | student |
+            +----+---------+
+            | 1  | Doris   |
+            | 2  | Abbot   |
+            | 3  | Green   |
+            | 4  | Emerson |
+            | 5  | Jeames  |
+            +----+---------+
+Explanation: Note that if the number of students is odd, there is no need to 
+             change the last one's seat.*/
+
+SELECT 
+    CASE WHEN MOD(id, 2) AND id = (SELECT COUNT(*) FROM seat) THEN id
+         ELSE (id+1)^1-1
+    END AS id, student
+FROM 
+    seat
+ORDER BY id
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
