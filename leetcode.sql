d76ad7f0d184ab6481bd7c7dfb4900b5e411c8bd
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 22:56:54 -0500

    solve'em all (1193)

--- leetcode.sql
+++ leetcode.sql
@@ -1597,6 +1597,57 @@ FROM
     JOIN (SELECT customer_id, MIN(order_date) AS order_date FROM Delivery GROUP BY customer_id) a USING (customer_id, order_date)
 
 
+/*1193. Monthly Transactions I (Medium)
+SQL Schema
+Table: Transactions
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| country       | varchar |
+| state         | enum    |
+| amount        | int     |
+| trans_date    | date    |
++---------------+---------+
+id is the primary key of this table. The table has information about incoming 
+transactions. The state column is an enum of type ["approved", "declined"].
+
+Write an SQL query to find for each month and country, the number of 
+transactions and their total amount, the number of approved transactions and 
+their total amount. Return the result table in any order. The query result 
+format is in the following example.
+
+Example 1:
+Input: 
+Transactions table: +------+---------+----------+--------+------------+
+                    | id   | country | state    | amount | trans_date |
+                    +------+---------+----------+--------+------------+
+                    | 121  | US      | approved | 1000   | 2018-12-18 |
+                    | 122  | US      | declined | 2000   | 2018-12-19 |
+                    | 123  | US      | approved | 2000   | 2019-01-01 |
+                    | 124  | DE      | approved | 2000   | 2019-01-07 |
+                    +------+---------+----------+--------+------------+
+Output: +----------+---------+-------------+----------------+--------------------+-----------------------+
+        | month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
+        +----------+---------+-------------+----------------+--------------------+-----------------------+
+        | 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+        | 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+        | 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
+        +----------+---------+-------------+----------------+--------------------+-----------------------+*/
+
+SELECT 
+    LEFT(trans_date, 7) AS month, 
+    country, 
+    COUNT(*) AS trans_count, 
+    COUNT(IF(state = "approved", 1, NULL)) AS approved_count, 
+    SUM(amount) AS trans_total_amount, 
+    SUM(IF(state = "approved", amount, 0)) AS approved_total_amount
+FROM 
+    Transactions
+GROUP BY 
+    LEFT(trans_date, 7), country
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
