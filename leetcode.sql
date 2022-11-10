457e621596d1f4d4fc24cd3f31d6d050eb973c6f
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 22:49:26 -0500

    solve'em all (1149)

--- leetcode.sql
+++ leetcode.sql
@@ -1357,6 +1357,53 @@ FROM (SELECT 100*COUNT(DISTINCT b.post_id)/COUNT(DISTINCT a.post_id) AS result
       GROUP BY action_date) c
 
 
+/*1149. Article Views II (Medium)
+SQL Schema
+Table: Views
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key for this table, it may have duplicate rows. Each row of 
+this table indicates that some viewer viewed an article (written by some author) 
+on some date.  Note that equal author_id and viewer_id indicate the same person.
+
+Write an SQL query to find all the people who viewed more than one article on 
+the same date. Return the result table sorted by id in ascending order. The 
+query result format is in the following example.
+
+Example 1:
+Input: 
+Views table: +------------+-----------+-----------+------------+
+             | article_id | author_id | viewer_id | view_date  |
+             +------------+-----------+-----------+------------+
+             | 1          | 3         | 5         | 2019-08-01 |
+             | 3          | 4         | 5         | 2019-08-01 |
+             | 1          | 3         | 6         | 2019-08-02 |
+             | 2          | 7         | 7         | 2019-08-01 |
+             | 2          | 7         | 6         | 2019-08-02 |
+             | 4          | 7         | 1         | 2019-07-22 |
+             | 3          | 4         | 4         | 2019-07-21 |
+             | 3          | 4         | 4         | 2019-07-21 |
+             +------------+-----------+-----------+------------+
+Output:                                                 +------+
+                                                        | id   |
+                                                        +------+
+                                                        | 5    |
+                                                        | 6    |
+                                                        +------+*/
+
+SELECT DISTINCT viewer_id AS id
+FROM Views
+GROUP BY view_date, viewer_id
+HAVING COUNT(DISTINCT article_id) > 1
+ORDER BY viewer_id
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
