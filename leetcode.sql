2029651bf62933d9fa44f27331811bbf577b7512
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-10 13:41:35 -0500

    solve'em all (1454)

--- leetcode.sql
+++ leetcode.sql
@@ -2969,6 +2969,84 @@ FROM Sales
 GROUP BY 1
 
 
+/*1454. Active Users (Medium)
+SQL Schema
+Table: Accounts
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key for this table. This table contains the account id and 
+the user name of each account.
+
+Table: Logins
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| login_date    | date    |
++---------------+---------+
+There is no primary key for this table, it may contain duplicates. This table 
+contains the account id of the user who logged in and the login date. A user 
+may log in multiple times in the day. Active users are those who logged in to 
+their accounts for five or more consecutive days.
+
+Write an SQL query to find the id and the name of active users. Return the 
+result table ordered by id. The query result format is in the following example.
+
+Example 1:
+Input: 
+Accounts table:
++----+----------+
+| id | name     |
++----+----------+
+| 1  | Winston  |
+| 7  | Jonathan |
++----+----------+
+Logins table:
++----+------------+
+| id | login_date |
++----+------------+
+| 7  | 2020-05-30 |
+| 1  | 2020-05-30 |
+| 7  | 2020-05-31 |
+| 7  | 2020-06-01 |
+| 7  | 2020-06-02 |
+| 7  | 2020-06-02 |
+| 7  | 2020-06-03 |
+| 1  | 2020-06-07 |
+| 7  | 2020-06-10 |
++----+------------+
+Output: 
++----+----------+
+| id | name     |
++----+----------+
+| 7  | Jonathan |
++----+----------+
+Explanation: User Winston with id = 1 logged in 2 times only in 2 different 
+             days, so, Winston is not an active user. User Jonathan with id = 7 
+             logged in 7 times in 6 different days, five of them were 
+             consecutive days, so, Jonathan is an active user.
+
+Follow up: Could you write a general solution if the active users are those who 
+           logged in to their accounts for n or more consecutive days?*/
+
+WITH Cte AS (
+    SELECT  
+        id,
+        login_date,
+        DATE_ADD(login_date, INTERVAL -DENSE_RANK() OVER(PARTITION BY id ORDER BY login_date) DAY) AS grp
+    FROM Logins
+)
+SELECT DISTINCT id, name -- "Gaps & Islands" problem
+FROM Cte JOIN Accounts USING (id)
+GROUP BY id, grp
+HAVING DATEDIFF(MAX(login_date), MIN(login_date)) >= 4
+ORDER BY 1
+
+
 /*1468. Calculate Salaries (Medium)
 SQL Schema
 Table Salaries:
