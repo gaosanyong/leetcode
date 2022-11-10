0744a5fa090f89c5dc9fd377309055c2233185fd
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-09 22:40:06 -0500

    solve'em all (1098)

--- leetcode.sql
+++ leetcode.sql
@@ -1047,6 +1047,77 @@ WHERE
     experience_years = (SELECT MAX(experience_years) FROM Project JOIN Employee USING (employee_id) WHERE project_id = a.project_id);
 
 
+/*1098. Unpopular Books (Medium)
+SQL Schema
+Table: Books
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| book_id        | int     |
+| name           | varchar |
+| available_from | date    |
++----------------+---------+
+book_id is the primary key of this table.
+
+Table: Orders
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| order_id       | int     |
+| book_id        | int     |
+| quantity       | int     |
+| dispatch_date  | date    |
++----------------+---------+
+order_id is the primary key of this table. book_id is a foreign key to the 
+Books table.
+ 
+Write an SQL query that reports the books that have sold less than 10 copies in 
+the last year, excluding books that have been available for less than one month 
+from today. Assume today is 2019-06-23. Return the result table in any order.
+The query result format is in the following example.
+
+Example 1:
+Input: 
+Books table:  +---------+--------------------+----------------+
+              | book_id | name               | available_from |
+              +---------+--------------------+----------------+
+              | 1       | "Kalila And Demna" | 2010-01-01     |
+              | 2       | "28 Letters"       | 2012-05-12     |
+              | 3       | "The Hobbit"       | 2019-06-10     |
+              | 4       | "13 Reasons Why"   | 2019-06-01     |
+              | 5       | "The Hunger Games" | 2008-09-21     |
+              +---------+--------------------+----------------+
+Orders table: +----------+---------+----------+---------------+
+              | order_id | book_id | quantity | dispatch_date |
+              +----------+---------+----------+---------------+
+              | 1        | 1       | 2        | 2018-07-26    |
+              | 2        | 1       | 1        | 2018-11-05    |
+              | 3        | 3       | 8        | 2019-06-11    |
+              | 4        | 4       | 6        | 2019-06-05    |
+              | 5        | 4       | 5        | 2019-06-20    |
+              | 6        | 5       | 9        | 2009-02-02    |
+              | 7        | 5       | 8        | 2010-04-13    |
+              +----------+---------+----------+---------------+
+Output:                      +-----------+--------------------+
+                             | book_id   | name               |
+                             +-----------+--------------------+
+                             | 1         | "Kalila And Demna" |
+                             | 2         | "28 Letters"       |
+                             | 5         | "The Hunger Games" |
+                             +-----------+--------------------+*/
+
+SELECT book_id, name 
+FROM Books
+WHERE 
+    available_from < ADDDATE("2019-06-23", INTERVAL -1 MONTH) #excluding books available less than one month
+    AND 
+    book_id NOT IN (SELECT book_id #popular books
+                    FROM Orders 
+                    WHERE dispatch_date > ADDDATE(DATE("2019-06-23"), INTERVAL -1 YEAR) 
+                    GROUP BY book_id 
+                    HAVING SUM(quantity) >= 10)
+
+
 /*1364. Number of Trusted Contacts of a Customer (Medium)
 SQL Schema
 Table: Customers
