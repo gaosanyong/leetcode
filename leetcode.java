2504f195160e3ab39204aec1954842e0303e45af
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-15 18:18:30 -0500

    java (9)

--- leetcode.java
+++ leetcode.java
@@ -29,7 +29,7 @@ class Solution {
 
 	Follow-up: Can you come up with an algorithm that is less than O(n2) time 
 	           complexity?*/
-	           
+
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> seen = new HashMap<>(); 
         for (int i = 0; i < nums.length; ++i) {
@@ -38,5 +38,37 @@ class Solution {
             seen.put(nums[i], i); 
         }
         return null; 
+    }
+
+
+    /*9. Palindrome Number (Easy)
+	Given an integer x, return true if x is a palindrome, and false otherwise.
+
+	Example 1:
+	Input: x = 121
+	Output: true
+	Explanation: 121 reads as 121 from left to right and from right to left.
+
+	Example 2:
+	Input: x = -121
+	Output: false
+	Explanation: From left to right, it reads -121. From right to left, it 
+	             becomes 121-. Therefore it is not a palindrome.
+
+	Example 3:
+	Input: x = 10
+	Output: false
+	Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+ 	Constraints: -2^31 <= x <= 2^31 - 1
+
+	Follow up: Could you solve it without converting the integer to a string?*/
+
+    public boolean isPalindrome(int x) {
+        if (x % 10 == 0 && x != 0 || x < 0) return false; 
+        int rev = 0; 
+        for (; x > rev; x /= 10) 
+            rev = 10*rev + x%10; 
+        return x == rev || x == rev/10; 
     }
 }
