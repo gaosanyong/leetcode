3bb586612db7990ae143ad7b4095f52caf82ea68
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-16 21:02:57 -0500

    java (58)

--- leetcode.java
+++ leetcode.java
@@ -377,4 +377,35 @@ class Solution {
         int k = Arrays.binarySearch(nums, target); 
         return k >= 0 ? k : -k-1; 
     }
+
+
+    /*58. Length of Last Word (Easy)
+	Given a string s consisting of words and spaces, return the length of the 
+	last word in the string. A word is a maximal substring consisting of non-
+	space characters only.
+
+	Example 1:
+	Input: s = "Hello World"
+	Output: 5
+	Explanation: The last word is "World" with length 5.
+
+	Example 2:
+	Input: s = "   fly me   to   the moon  "
+	Output: 4
+	Explanation: The last word is "moon" with length 4.
+
+	Example 3:
+	Input: s = "luffy is still joyboy"
+	Output: 6
+	Explanation: The last word is "joyboy" with length 6.
+
+	Constraints:
+	* 1 <= s.length <= 10^4
+	* s consists of only English letters and spaces ' '.
+	* There will be at least one word in s.*/
+
+    public int lengthOfLastWord(String s) {
+        String[] words = s.split(" "); 
+        return words[words.length-1].length(); 
+    }
 }
