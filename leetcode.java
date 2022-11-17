6a41acb7961d4ac9b18c204e04661e635ac7ed35
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-16 21:36:36 -0500

    java (66)

--- leetcode.java
+++ leetcode.java
@@ -408,4 +408,47 @@ class Solution {
         String[] words = s.split(" "); 
         return words[words.length-1].length(); 
     }
+
+
+    /*66. Plus One (Easy)
+	You are given a large integer represented as an integer array digits, where 
+	each digits[i] is the ith digit of the integer. The digits are ordered from 
+	most significant to least significant in left-to-right order. The large 
+	integer does not contain any leading 0's. Increment the large integer by 
+	one and return the resulting array of digits.
+
+	Example 1:
+	Input: digits = [1,2,3]
+	Output: [1,2,4]
+	Explanation: The array represents the integer 123. Incrementing by one 
+	             gives 123 + 1 = 124. Thus, the result should be [1,2,4].
+	
+	Example 2:
+	Input: digits = [4,3,2,1]
+	Output: [4,3,2,2]
+	Explanation: The array represents the integer 4321. Incrementing by one 
+	             gives 4321 + 1 = 4322. Thus, the result should be [4,3,2,2].
+	
+	Example 3:
+	Input: digits = [9]
+	Output: [1,0]
+	Explanation: The array represents the integer 9. Incrementing by one gives 
+	             9 + 1 = 10. Thus, the result should be [1,0].
+
+	Constraints:
+	* 1 <= digits.length <= 100
+	* 0 <= digits[i] <= 9
+	* digits does not contain any leading 0's.*/
+
+    public int[] plusOne(int[] digits) {
+        int carry = 1; 
+        for (int i = digits.length-1; i >= 0; --i, carry /= 10) {
+            carry += digits[i]; 
+            digits[i] = carry % 10; 
+        }
+        if (carry == 0) return digits; 
+        int[] ans = new int[digits.length+1]; 
+        ans[0] = 1; 
+        return ans; 
+    }
 }
