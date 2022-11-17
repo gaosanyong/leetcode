ef4559ea07957420f0fd52c19f2cfd8d0f2693ad
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-16 20:56:06 -0500

    java (35)

--- leetcode.java
+++ leetcode.java
@@ -347,4 +347,34 @@ class Solution {
             if (x != val) nums[k++] = x; 
         return k; 
     }
+
+
+    /*35. Search Insert Position (Easy)
+	Given a sorted array of distinct integers and a target value, return the 
+	index if the target is found. If not, return the index where it would be if 
+	it were inserted in order. You must write an algorithm with O(log n) 
+	runtime complexity.
+
+	Example 1:
+	Input: nums = [1,3,5,6], target = 5
+	Output: 2
+
+	Example 2:
+	Input: nums = [1,3,5,6], target = 2
+	Output: 1
+
+	Example 3:
+	Input: nums = [1,3,5,6], target = 7
+	Output: 4
+
+	Constraints:
+	* 1 <= nums.length <= 10^4
+	* -10^4 <= nums[i] <= 10^4
+	* nums contains distinct values sorted in ascending order.
+	* -10^4 <= target <= 10^4*/
+
+    public int searchInsert(int[] nums, int target) {
+        int k = Arrays.binarySearch(nums, target); 
+        return k >= 0 ? k : -k-1; 
+    }
 }
