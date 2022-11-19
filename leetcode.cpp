5640694b59c0e89fbf80a733859bb71cc4c58d2b
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-18 22:25:03 -0500

    solve'em all (2473)

--- leetcode.cpp
+++ leetcode.cpp
@@ -48017,6 +48017,81 @@ public:
         }
         return dp[0][0]; 
     }
+
+
+    /*2473. Minimum Cost to Buy Apples (Medium)
+    You are given a positive integer n representing n cities numbered from 1 to 
+    n. You are also given a 2D array roads, where roads[i] = [ai, bi, costi] 
+    indicates that there is a bidirectional road between cities ai and bi with 
+    a cost of traveling equal to costi. You can buy apples in any city you want, 
+    but some cities have different costs to buy apples. You are given the array 
+    appleCost where appleCost[i] is the cost of buying one apple from city i.
+    You start at some city, traverse through various roads, and eventually buy 
+    exactly one apple from any city. After you buy that apple, you have to 
+    return back to the city you started at, but now the cost of all the roads 
+    will be multiplied by a given factor k. Given the integer k, return an 
+    array answer of size n where answer[i] is the minimum total cost to buy an 
+    apple if you start at city i.
+
+    Example 1:
+    Input: n = 4, roads = [[1,2,4],[2,3,2],[2,4,5],[3,4,1],[1,3,4]], 
+           appleCost = [56,42,102,301], k = 2
+    Output: [54,42,48,51]
+    Explanation: The minimum cost for each starting city is the following:
+                 - Starting at city 1: You take the path 1 -> 2, buy an apple 
+                   at city 2, and finally take the path 2 -> 1. The total 
+                   cost is 4 + 42 + 4 * 2 = 54.
+                 - Starting at city 2: You directly buy an apple at city 2. 
+                   The total cost is 42.
+                 - Starting at city 3: You take the path 3 -> 2, buy an apple 
+                   at city 2, and finally take the path 2 -> 3. The total cost 
+                   is 2 + 42 + 2 * 2 = 48.
+                 - Starting at city 4: You take the path 4 -> 3 -> 2 then you 
+                   buy at city 2, and finally take the path 2 -> 3 -> 4. The 
+                   total cost is 1 + 2 + 42 + 1 * 2 + 2 * 2 = 51.
+    
+    Example 2:
+    Input: n = 3, roads = [[1,2,5],[2,3,1],[3,1,2]], appleCost = [2,3,1], k = 3
+    Output: [2,3,1]
+    Explanation: It is always optimal to buy the apple in the starting city.
+
+    Constraints:
+    * 2 <= n <= 1000
+    * 1 <= roads.length <= 1000
+    * 1 <= ai, bi <= n
+    * ai != bi
+    * 1 <= costi <= 105
+    * appleCost.length == n
+    * 1 <= appleCost[i] <= 105
+    * 1 <= k <= 100
+    * There are no repeated edges.*/
+
+    vector<long long> minCost(int n, vector<vector<int>>& roads, vector<int>& appleCost, int k) {
+        vector<vector<pair<int, int>>> graph(n); 
+        for (auto& r : roads) {
+            graph[r[0]-1].emplace_back(r[1]-1, r[2]); 
+            graph[r[1]-1].emplace_back(r[0]-1, r[2]); 
+        }
+        vector<long long> ans(n, LONG_MAX); 
+        for (int i = 0; i < n; ++i) {
+            vector<long long> dist(n, LONG_MAX); 
+            dist[i] = 0; 
+            priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq; 
+            pq.emplace(0, i); 
+            while (pq.size()) {
+                auto [x, u] = pq.top(); pq.pop(); 
+                ans[i] = min(ans[i], appleCost[u] + (1+k)*x); 
+                for (auto& [v, w] : graph[u]) {
+                    long long xx = x + w; 
+                    if (xx < dist[v]) {
+                        dist[v] = xx; 
+                        pq.emplace(xx, v); 
+                    }
+                }
+            }
+        }
+        return ans; 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -69010,6 +69010,74 @@ class SegTree:
         return dp[0][0]
 
 
+    """2473. Minimum Cost to Buy Apples (Medium)
+    You are given a positive integer n representing n cities numbered from 1 to 
+    n. You are also given a 2D array roads, where roads[i] = [ai, bi, costi] 
+    indicates that there is a bidirectional road between cities ai and bi with 
+    a cost of traveling equal to costi. You can buy apples in any city you want, 
+    but some cities have different costs to buy apples. You are given the array 
+    appleCost where appleCost[i] is the cost of buying one apple from city i.
+    You start at some city, traverse through various roads, and eventually buy 
+    exactly one apple from any city. After you buy that apple, you have to 
+    return back to the city you started at, but now the cost of all the roads 
+    will be multiplied by a given factor k. Given the integer k, return an 
+    array answer of size n where answer[i] is the minimum total cost to buy an 
+    apple if you start at city i.
+
+    Example 1:
+    Input: n = 4, roads = [[1,2,4],[2,3,2],[2,4,5],[3,4,1],[1,3,4]], 
+           appleCost = [56,42,102,301], k = 2
+    Output: [54,42,48,51]
+    Explanation: The minimum cost for each starting city is the following:
+                 - Starting at city 1: You take the path 1 -> 2, buy an apple 
+                   at city 2, and finally take the path 2 -> 1. The total 
+                   cost is 4 + 42 + 4 * 2 = 54.
+                 - Starting at city 2: You directly buy an apple at city 2. 
+                   The total cost is 42.
+                 - Starting at city 3: You take the path 3 -> 2, buy an apple 
+                   at city 2, and finally take the path 2 -> 3. The total cost 
+                   is 2 + 42 + 2 * 2 = 48.
+                 - Starting at city 4: You take the path 4 -> 3 -> 2 then you 
+                   buy at city 2, and finally take the path 2 -> 3 -> 4. The 
+                   total cost is 1 + 2 + 42 + 1 * 2 + 2 * 2 = 51.
+    
+    Example 2:
+    Input: n = 3, roads = [[1,2,5],[2,3,1],[3,1,2]], appleCost = [2,3,1], k = 3
+    Output: [2,3,1]
+    Explanation: It is always optimal to buy the apple in the starting city.
+
+    Constraints:
+    * 2 <= n <= 1000
+    * 1 <= roads.length <= 1000
+    * 1 <= ai, bi <= n
+    * ai != bi
+    * 1 <= costi <= 105
+    * appleCost.length == n
+    * 1 <= appleCost[i] <= 105
+    * 1 <= k <= 100
+    * There are no repeated edges."""
+
+    def minCost(self, n: int, roads: List[List[int]], appleCost: List[int], k: int) -> List[int]:
+        graph = [[] for _ in range(n)]
+        for u, v, w in roads: 
+            graph[u-1].append((v-1, w))
+            graph[v-1].append((u-1, w))
+        ans = [inf]*n
+        for i in range(n): 
+            dist = [inf]*n
+            dist[i] = 0 
+            pq = [(0, i)]
+            while pq: 
+                x, u = heappop(pq)
+                ans[i] = min(ans[i], appleCost[u]+(1+k)*x)
+                for v, w in graph[u]: 
+                    xx = x + w
+                    if xx < dist[v]: 
+                        dist[v] = xx
+                        heappush(pq, (xx, v))
+        return ans 
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
