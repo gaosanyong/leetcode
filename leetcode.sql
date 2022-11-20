f1d13d75e45bd7e0a1336bd58505c03692544f2f
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-20 18:28:18 -0500

    solve'em all (2474)

--- leetcode.sql
+++ leetcode.sql
@@ -8129,3 +8129,85 @@ GROUP BY
     1, 
     needed_hours
 HAVING SUM(CEIL(IFNULL(TIMESTAMPDIFF(SECOND, in_time, out_time), 0)/60)/60) < needed_hours
+
+
+/*2474. Customers With Strictly Increasing Purchases (Hard)
+SQL Schema
+Table: Orders
++--------------+------+
+| Column Name  | Type |
++--------------+------+
+| order_id     | int  |
+| customer_id  | int  |
+| order_date   | date |
+| price        | int  |
++--------------+------+
+order_id is the primary key for this table. Each row contains the id of an 
+order, the id of customer that ordered it, the date of the order, and its price.
+
+Write an SQL query to report the IDs of the customers with the total purchases 
+strictly increasing yearly. The total purchases of a customer in one year is 
+the sum of the prices of their orders in that year. If for some year the 
+customer did not make any order, we consider the total purchases 0. The first 
+year to consider for each customer is the year of their first order. The last 
+year to consider for each customer is the year of their last order. Return the 
+result table in any order. The query result format is in the following example.
+
+Example 1:
+Input: 
+Orders table:
++----------+-------------+------------+-------+
+| order_id | customer_id | order_date | price |
++----------+-------------+------------+-------+
+| 1        | 1           | 2019-07-01 | 1100  |
+| 2        | 1           | 2019-11-01 | 1200  |
+| 3        | 1           | 2020-05-26 | 3000  |
+| 4        | 1           | 2021-08-31 | 3100  |
+| 5        | 1           | 2022-12-07 | 4700  |
+| 6        | 2           | 2015-01-01 | 700   |
+| 7        | 2           | 2017-11-07 | 1000  |
+| 8        | 3           | 2017-01-01 | 900   |
+| 9        | 3           | 2018-11-07 | 900   |
++----------+-------------+------------+-------+
+Output: 
++-------------+
+| customer_id |
++-------------+
+| 1           |
++-------------+
+Explanation: Customer 1: The first year is 2019 and the last year is 2022
+               - 2019: 1100 + 1200 = 2300
+               - 2020: 3000
+               - 2021: 3100
+               - 2022: 4700
+               We can see that the total purchases are strictly increasing 
+               yearly, so we include customer 1 in the answer.
+         
+             Customer 2: The first year is 2015 and the last year is 2017
+               - 2015: 700
+               - 2016: 0
+               - 2017: 1000
+               We do not include customer 2 in the answer because the total 
+               purchases are not strictly increasing. Note that customer 2 did 
+               not make any purchases in 2016.
+         
+             Customer 3: The first year is 2017, and the last year is 2018
+               - 2017: 900
+               - 2018: 900
+               We can see that the total purchases are strictly increasing 
+               yearly, so we include customer 1 in the answer.*/
+
+WITH Yearly AS (
+    SELECT 
+        customer_id, 
+        YEAR(order_date) AS yr, 
+        SUM(price) AS total 
+    FROM Orders
+    GROUP BY 1, 2
+)
+SELECT a.customer_id
+FROM 
+    Yearly a 
+    LEFT JOIN Yearly b ON a.customer_id = b.customer_id AND a.yr+1 = b.yr
+GROUP BY a.customer_id 
+HAVING SUM(a.total >= IFNULL(b.total, 0)) = 1
