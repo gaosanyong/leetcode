c5c6072c23a2a36531a94088614c5fff37d77439
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-26 11:58:01 -0500

    update 2484

--- leetcode.py
+++ leetcode.py
@@ -69955,14 +69955,14 @@ class SegTree:
         ans = 0 
         for x in range(10): 
             for y in range(10): 
-                pattern = str(x) + str(y) + "|" + str(y) + str(x) 
+                pattern = f"{x}{y}|{y}{x}" 
                 dp = [0]*6
                 dp[-1] = 1 
                 for i in range(len(s)): 
                     for j in range(5): 
                         if s[i] == pattern[j] or j == 2: dp[j] += dp[j+1]
                 ans = (ans + dp[0]) % 1_000_000_007
-        return ans 
+        return ans  
 
 
 """146. LRU Cache (Medium)
