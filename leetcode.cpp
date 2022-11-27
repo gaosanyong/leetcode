b21666ba56bca87905e7a968d529057d382d7df1
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-11-27 15:32:06 -0500

    solve'em all (2297)

--- leetcode.cpp
+++ leetcode.cpp
@@ -45482,6 +45482,59 @@ public:
     }
 
 
+    /*2297. Jump Game VIII (Medium)
+    You are given a 0-indexed integer array nums of length n. You are initially 
+    standing at index 0. You can jump from index i to index j where i < j if:
+    * nums[i] <= nums[j] and nums[k] < nums[i] for all indexes k in the range 
+      i < k < j, or
+    * nums[i] > nums[j] and nums[k] >= nums[i] for all indexes k in the range 
+      i < k < j.
+    You are also given an integer array costs of length n where costs[i] 
+    denotes the cost of jumping to index i. Return the minimum cost to jump to 
+    the index n - 1.
+
+    Example 1:
+    Input: nums = [3,2,4,4,1], costs = [3,7,6,4,2]
+    Output: 8
+    Explanation: You start at index 0.
+                 - Jump to index 2 with a cost of costs[2] = 6.
+                 - Jump to index 4 with a cost of costs[4] = 2.
+                 The total cost is 8. It can be proven that 8 is the minimum 
+                 cost needed. Two other possible paths are from index 0 -> 1 -> 
+                 4 and index 0 -> 2 -> 3 -> 4. These have a total cost of 9 and 
+                 12, respectively.
+    
+    Example 2:
+    Input: nums = [0,1,2], costs = [1,1,1]
+    Output: 2
+    Explanation: Start at index 0.
+                 - Jump to index 1 with a cost of costs[1] = 1.
+                 - Jump to index 2 with a cost of costs[2] = 1.
+                 The total cost is 2. Note that you cannot jump directly from 
+                 index 0 to index 2 because nums[0] <= nums[1].
+
+    Constraints:
+    * n == nums.length == costs.length
+    * 1 <= n <= 10^5
+    * 0 <= nums[i], costs[i] <= 10^5*/
+
+    long long minCost(vector<int>& nums, vector<int>& costs) {
+        vector<vector<int>> jump(nums.size());
+        stack<int> s0, s1; 
+        for (int i = 0; i < nums.size(); ++i) {
+            while (s0.size() && nums[s0.top()] <= nums[i]) jump[i].push_back(s0.top()), s0.pop(); 
+            while (s1.size() && nums[s1.top()] > nums[i]) jump[i].push_back(s1.top()), s1.pop(); 
+            s0.push(i); 
+            s1.push(i); 
+        }
+        vector<long long> dp(nums.size(), LONG_MAX); 
+        dp[0] = 0; 
+        for (int i = 0; i < costs.size(); ++i) 
+            for (auto& k : jump[i]) dp[i] = min(dp[i], dp[k] + costs[i]); 
+        return dp.back(); 
+    }
+
+
     /*2313. Minimum Flips in Binary Tree to Get Result (Hard)
     You are given the root of a binary tree with the following properties:
     * Leaf nodes have either the value 0 or 1, representing false and true 
--- leetcode.py
+++ leetcode.py
@@ -62490,6 +62490,57 @@ class Trie:
         return [mp.get(x, x) for x in nums]
 
 
+    """2297. Jump Game VIII (Medium)
+    You are given a 0-indexed integer array nums of length n. You are initially 
+    standing at index 0. You can jump from index i to index j where i < j if:
+    * nums[i] <= nums[j] and nums[k] < nums[i] for all indexes k in the range 
+      i < k < j, or
+    * nums[i] > nums[j] and nums[k] >= nums[i] for all indexes k in the range 
+      i < k < j.
+    You are also given an integer array costs of length n where costs[i] 
+    denotes the cost of jumping to index i. Return the minimum cost to jump to 
+    the index n - 1.
+
+    Example 1:
+    Input: nums = [3,2,4,4,1], costs = [3,7,6,4,2]
+    Output: 8
+    Explanation: You start at index 0.
+                 - Jump to index 2 with a cost of costs[2] = 6.
+                 - Jump to index 4 with a cost of costs[4] = 2.
+                 The total cost is 8. It can be proven that 8 is the minimum 
+                 cost needed. Two other possible paths are from index 0 -> 1 -> 
+                 4 and index 0 -> 2 -> 3 -> 4. These have a total cost of 9 and 
+                 12, respectively.
+    
+    Example 2:
+    Input: nums = [0,1,2], costs = [1,1,1]
+    Output: 2
+    Explanation: Start at index 0.
+                 - Jump to index 1 with a cost of costs[1] = 1.
+                 - Jump to index 2 with a cost of costs[2] = 1.
+                 The total cost is 2. Note that you cannot jump directly from 
+                 index 0 to index 2 because nums[0] <= nums[1].
+
+    Constraints:
+    * n == nums.length == costs.length
+    * 1 <= n <= 10^5
+    * 0 <= nums[i], costs[i] <= 10^5"""
+
+    def minCost(self, nums: List[int], costs: List[int]) -> int:
+        jump = [[] for _ in range(len(nums))] 
+        s0, s1 = [], []
+        for i, x in enumerate(nums): 
+            while s0 and s0[-1][1] <= x: jump[i].append(s0.pop()[0])
+            while s1 and s1[-1][1] > x: jump[i].append(s1.pop()[0])
+            s0.append((i, x))
+            s1.append((i, x))
+        dp = [inf] * len(nums)
+        dp[0] = 0
+        for i, x in enumerate(costs): 
+            for k in jump[i]: dp[i] = min(dp[i], dp[k] + x)
+        return dp[-1]
+
+
     """2303. Calculate Amount Paid in Taxes (Easy)
     You are given a 0-indexed 2D integer array brackets where 
     brackets[i] = [upperi, percenti] means that the ith tax bracket has an 
