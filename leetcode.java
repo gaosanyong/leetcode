cc34915d7951b70012078cccda90f7f261e890a2
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-12-06 10:01:30 -0500

    solve'em all (2046)

--- leetcode.java
+++ leetcode.java
@@ -480,6 +480,53 @@ class Solution {
     }
 
 
+    /*2046. Sort Linked List Already Sorted Using Absolute Values (Medium)
+    Given the head of a singly linked list that is sorted in non-decreasing 
+    order using the absolute values of its nodes, return the list sorted in 
+    non-decreasing order using the actual values of its nodes.
+
+    Example 1:
+    Input: head = [0,2,-5,5,10,-10]
+    Output: [-10,-5,0,2,5,10]
+    Explanation: The list sorted in non-descending order using the absolute 
+                 values of the nodes is [0,2,-5,5,10,-10]. The list sorted in 
+                 non-descending order using the actual values is 
+                 [-10,-5,0,2,5,10].
+    
+    Example 2:
+    Input: head = [0,1,2]
+    Output: [0,1,2]
+    Explanation: The linked list is already sorted in non-decreasing order.
+    
+    Example 3:
+    Input: head = [1]
+    Output: [1]
+    Explanation: The linked list is already sorted in non-decreasing order.
+
+    Constraints:
+    * The number of nodes in the list is the range [1, 10^5].
+    * -5000 <= Node.val <= 5000
+    * head is sorted in non-decreasing order using the absolute value of its 
+      nodes.
+
+    Follow up: Can you think of a solution with O(n) time complexity?*/
+
+    public ListNode sortLinkedList(ListNode head) {
+        ListNode prev = head, node = head.next; 
+        while (node != null) 
+            if (node.val < 0) {
+                prev.next = node.next; 
+                node.next = head; 
+                head = node; 
+                node = prev.next; 
+            } else {
+                prev = node; 
+                node = node.next; 
+            }
+        return head; 
+    }
+
+
     /*2198. Number of Single Divisor Triplets (Medium)
     You are given a 0-indexed array of positive integers nums. A triplet of 
     three distinct indices (i, j, k) is called a single divisor triplet of nums 
--- leetcode.py
+++ leetcode.py
@@ -52917,6 +52917,52 @@ class Trie:
                     queue.append((t, v))
 
 
+    """2046. Sort Linked List Already Sorted Using Absolute Values (Medium)
+    Given the head of a singly linked list that is sorted in non-decreasing 
+    order using the absolute values of its nodes, return the list sorted in 
+    non-decreasing order using the actual values of its nodes.
+
+    Example 1:
+    Input: head = [0,2,-5,5,10,-10]
+    Output: [-10,-5,0,2,5,10]
+    Explanation: The list sorted in non-descending order using the absolute 
+                 values of the nodes is [0,2,-5,5,10,-10]. The list sorted in 
+                 non-descending order using the actual values is 
+                 [-10,-5,0,2,5,10].
+    
+    Example 2:
+    Input: head = [0,1,2]
+    Output: [0,1,2]
+    Explanation: The linked list is already sorted in non-decreasing order.
+    
+    Example 3:
+    Input: head = [1]
+    Output: [1]
+    Explanation: The linked list is already sorted in non-decreasing order.
+
+    Constraints:
+    * The number of nodes in the list is the range [1, 10^5].
+    * -5000 <= Node.val <= 5000
+    * head is sorted in non-decreasing order using the absolute value of its 
+      nodes.
+
+    Follow up: Can you think of a solution with O(n) time complexity?"""
+
+    def sortLinkedList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = head
+        node = head.next
+        while node: 
+            if node.val < 0: 
+                prev.next = node.next 
+                node.next = head
+                head = node 
+                node = prev.next 
+            else: 
+                prev = node 
+                node = node.next 
+        return head 
+
+
     """2047. Number of Valid Words in a Sentence (Easy)
     A sentence consists of lowercase letters ('a' to 'z'), digits ('0' to '9'), 
     hyphens ('-'), punctuation marks ('!', '.', and ','), and spaces (' ') only. 
