3d963bec91e36f2f020130716e3266b67fb64c90
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-12-06 17:42:55 -0500

    solve'em all (2061)

--- leetcode.java
+++ leetcode.java
@@ -599,6 +599,65 @@ class Solution {
     }
 
 
+    /*2061. Number of Spaces Cleaning Robot Cleaned (Medium)
+    A room is represented by a 0-indexed 2D binary matrix room where a 0 
+    represents an empty space and a 1 represents a space with an object. The 
+    top left corner of the room will be empty in all test cases. A cleaning 
+    robot starts at the top left corner of the room and is facing right. The 
+    robot will continue heading straight until it reaches the edge of the room 
+    or it hits an object, after which it will turn 90 degrees clockwise and 
+    repeat this process. The starting space and all spaces that the robot 
+    visits are cleaned by it. Return the number of clean spaces in the room if 
+    the robot runs indefinetely.
+
+    Example 1:
+    Input: room = [[0,0,0],[1,1,0],[0,0,0]]
+    Output: 7
+    Explanation: The robot cleans the spaces at (0, 0), (0, 1), and (0, 2). The 
+                 robot is at the edge of the room, so it turns 90 degrees 
+                 clockwise and now faces down. The robot cleans the spaces at 
+                 (1, 2), and (2, 2). The robot is at the edge of the room, so 
+                 it turns 90 degrees clockwise and now faces left. The robot 
+                 cleans the spaces at (2, 1), and (2, 0). The robot has cleaned 
+                 all 7 empty spaces, so return 7.
+    
+    Example 2:
+    Input: room = [[0,1,0],[1,0,0],[0,0,0]]
+    Output: 1
+    Explanation: The robot cleans the space at (0, 0). The robot hits an object, 
+                 so it turns 90 degrees clockwise and now faces down. The robot 
+                 hits an object, so it turns 90 degrees clockwise and now faces 
+                 left. The robot is at the edge of the room, so it turns 90 
+                 degrees clockwise and now faces up. The robot is at the edge 
+                 of the room, so it turns 90 degrees clockwise and now faces 
+                 right. The robot is back at its starting position. The robot 
+                 has cleaned 1 space, so return 1.
+
+    Constraints:
+    * m == room.length
+    * n == room[r].length
+    * 1 <= m, n <= 300
+    * room[r][c] is either 0 or 1.
+    * room[0][0] == 0*/
+
+    public int numberOfCleanRooms(int[][] room) {
+        int m = room.length, n = room[0].length, ans = 0; 
+        int[] dir = new int[]{0, 1, 0, -1, 0}; 
+        int[][] seen = new int[m][n]; 
+        for (int i = 0, j = 0, k = 0; (seen[i][j] & 1<<k) == 0; ) {
+            if (room[i][j] == 0) {
+                ++ans; 
+                room[i][j] = -1; 
+            }
+            seen[i][j] |= 1<<k; 
+            int ii = i + dir[k], jj = j + dir[k+1]; 
+            if (0 <= ii && ii < m && 0 <= jj && jj < n && room[ii][jj] != 1) { i = ii; j = jj; }
+            else k = (k+1) % 4; 
+        }
+        return ans; 
+    }
+
+
     /*2198. Number of Single Divisor Triplets (Medium)
     You are given a 0-indexed array of positive integers nums. A triplet of 
     three distinct indices (i, j, k) is called a single divisor triplet of nums 
--- leetcode.py
+++ leetcode.py
@@ -53820,6 +53820,64 @@ class Trie:
         return fn(0, 0, 0)
 
 
+    """2061. Number of Spaces Cleaning Robot Cleaned (Medium)
+    A room is represented by a 0-indexed 2D binary matrix room where a 0 
+    represents an empty space and a 1 represents a space with an object. The 
+    top left corner of the room will be empty in all test cases. A cleaning 
+    robot starts at the top left corner of the room and is facing right. The 
+    robot will continue heading straight until it reaches the edge of the room 
+    or it hits an object, after which it will turn 90 degrees clockwise and 
+    repeat this process. The starting space and all spaces that the robot 
+    visits are cleaned by it. Return the number of clean spaces in the room if 
+    the robot runs indefinetely.
+
+    Example 1:
+    Input: room = [[0,0,0],[1,1,0],[0,0,0]]
+    Output: 7
+    Explanation: The robot cleans the spaces at (0, 0), (0, 1), and (0, 2). The 
+                 robot is at the edge of the room, so it turns 90 degrees 
+                 clockwise and now faces down. The robot cleans the spaces at 
+                 (1, 2), and (2, 2). The robot is at the edge of the room, so 
+                 it turns 90 degrees clockwise and now faces left. The robot 
+                 cleans the spaces at (2, 1), and (2, 0). The robot has cleaned 
+                 all 7 empty spaces, so return 7.
+    
+    Example 2:
+    Input: room = [[0,1,0],[1,0,0],[0,0,0]]
+    Output: 1
+    Explanation: The robot cleans the space at (0, 0). The robot hits an object, 
+                 so it turns 90 degrees clockwise and now faces down. The robot 
+                 hits an object, so it turns 90 degrees clockwise and now faces 
+                 left. The robot is at the edge of the room, so it turns 90 
+                 degrees clockwise and now faces up. The robot is at the edge 
+                 of the room, so it turns 90 degrees clockwise and now faces 
+                 right. The robot is back at its starting position. The robot 
+                 has cleaned 1 space, so return 1.
+
+    Constraints:
+    * m == room.length
+    * n == room[r].length
+    * 1 <= m, n <= 300
+    * room[r][c] is either 0 or 1.
+    * room[0][0] == 0"""
+
+    def numberOfCleanRooms(self, room: List[List[int]]) -> int:
+        m, n = len(room), len(room[0])
+        ans = i = j = k = 0 
+        dirs = (0, 1, 0, -1, 0)
+        seen = defaultdict(int)
+        while not seen[i, j] & 1<<k: 
+            if room[i][j] == 0: 
+                ans += 1
+                room[i][j] = -1 
+            seen[i, j] |= 1<<k
+            ii = i + dirs[k]
+            jj = j + dirs[k+1]
+            if 0 <= ii < m and 0 <= jj < n and room[ii][jj] != 1: i, j = ii, jj 
+            else: k = (k+1)%4
+        return ans 
+
+
     """2062. Count Vowel Substrings of a String (Easy)
     A substring is a contiguous (non-empty) sequence of characters within a 
     string. A vowel substring is a substring that only consists of vowels 
