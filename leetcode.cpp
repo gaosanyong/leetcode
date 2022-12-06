940fe920d329e8fb9e18198df89ac4ef3425dbfe
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-12-06 09:18:46 -0500

    solve'em all (2495)

--- leetcode.cpp
+++ leetcode.cpp
@@ -49316,6 +49316,40 @@ public:
         }
         return ans; 
     }
+
+
+    /*2495. Number of Subarrays Having Even Product (Medium)
+    Given a 0-indexed integer array nums, return the number of subarrays of 
+    nums having an even product.
+
+    Example 1:
+    Input: nums = [9,6,7,13]
+    Output: 6
+    Explanation: There are 6 subarrays with an even product:
+                 - nums[0..1] = 9 * 6 = 54.
+                 - nums[0..2] = 9 * 6 * 7 = 378.
+                 - nums[0..3] = 9 * 6 * 7 * 13 = 4914.
+                 - nums[1..1] = 6.
+                 - nums[1..2] = 6 * 7 = 42.
+                 - nums[1..3] = 6 * 7 * 13 = 546.
+    
+    Example 2:
+    Input: nums = [7,3,5]
+    Output: 0
+    Explanation: There are no subarrays with an even product.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 1 <= nums[i] <= 10^5*/
+
+    long long evenProduct(vector<int>& nums) {
+        long long ans = 0; 
+        for (int i = 0, val = 0; i < nums.size(); ++i) {
+            if (nums[i] % 2 == 0) val = i+1; 
+            ans += val; 
+        }
+        return ans; 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -70263,6 +70263,38 @@ class SegTree:
         return ans 
 
 
+    """2495. Number of Subarrays Having Even Product (Medium)
+    Given a 0-indexed integer array nums, return the number of subarrays of 
+    nums having an even product.
+
+    Example 1:
+    Input: nums = [9,6,7,13]
+    Output: 6
+    Explanation: There are 6 subarrays with an even product:
+                 - nums[0..1] = 9 * 6 = 54.
+                 - nums[0..2] = 9 * 6 * 7 = 378.
+                 - nums[0..3] = 9 * 6 * 7 * 13 = 4914.
+                 - nums[1..1] = 6.
+                 - nums[1..2] = 6 * 7 = 42.
+                 - nums[1..3] = 6 * 7 * 13 = 546.
+    
+    Example 2:
+    Input: nums = [7,3,5]
+    Output: 0
+    Explanation: There are no subarrays with an even product.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 1 <= nums[i] <= 10^5"""
+
+    def evenProduct(self, nums: List[int]) -> int:
+        ans = val = 0
+        for i, x in enumerate(nums): 
+            if not x&1: val = i+1
+            ans += val 
+        return ans 
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
