f28c0f3dd3da5249b6049bc7dffd03adebb2d4cd
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-12-06 21:50:22 -0500

    java (2107)

--- leetcode.java
+++ leetcode.java
@@ -711,6 +711,55 @@ class Solution {
     }
 
 
+    /*2107. Number of Unique Flavors After Sharing K Candies (Medium)
+    You are given a 0-indexed integer array candies, where candies[i] 
+    represents the flavor of the ith candy. Your mom wants you to share these 
+    candies with your little sister by giving her k consecutive candies, but 
+    you want to keep as many flavors of candies as possible. Return the maximum 
+    number of unique flavors of candy you can keep after sharing with your 
+    sister.
+
+    Example 1:
+    Input: candies = [1,2,2,3,4,3], k = 3
+    Output: 3
+    Explanation: Give the candies in the range [1, 3] (inclusive) with flavors 
+                 [2,2,3]. You can eat candies with flavors [1,4,3]. There are 3 
+                 unique flavors, so return 3.
+    
+    Example 2:
+    Input: candies = [2,2,2,2,3,3], k = 2
+    Output: 2
+    Explanation: Give the candies in the range [3, 4] (inclusive) with flavors 
+                 [2,3]. You can eat candies with flavors [2,2,2,3]. There are 2 
+                 unique flavors, so return 2. Note that you can also share the 
+                 candies with flavors [2,2] and eat the candies with flavors 
+                 [2,2,3,3].
+    
+    Example 3:
+    Input: candies = [2,4,5], k = 0
+    Output: 3
+    Explanation: You do not have to give any candies. You can eat the candies 
+                 with flavors [2,4,5]. There are 3 unique flavors, so return 3.
+
+    Constraints:
+    * 1 <= candies.length <= 10^5
+    * 1 <= candies[i] <= 10^5
+    * 0 <= k <= candies.length*/
+
+    public int shareCandies(int[] candies, int k) {
+        int ans = 0; 
+        HashMap<Integer, Integer> freq = new HashMap(); 
+        for (int x : candies) freq.merge(x, 1, Integer::sum); 
+        for (int i = 0; i < candies.length; ++i) {
+            freq.merge(candies[i], -1, Integer::sum); 
+            if (freq.get(candies[i]) == 0) freq.remove(candies[i]); 
+            if (i >= k) freq.merge(candies[i-k], 1, Integer::sum); 
+            if (i >= k-1) ans = Math.max(ans, freq.size()); 
+        }
+        return ans; 
+    }
+
+
     /*2113. Elements in Array After Removing and Replacing Elements (Medium)
     You are given a 0-indexed integer array nums. Initially on minute 0, the 
     array is unchanged. Every minute, the leftmost element in nums is removed 
