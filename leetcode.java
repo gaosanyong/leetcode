9f7f5e0e5dc7ca4a060ec9b9057297c9a2d1b1be
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-12-07 15:34:29 -0500

    java (2077)

--- leetcode.java
+++ leetcode.java
@@ -711,6 +711,57 @@ class Solution {
     }
 
 
+    /*2077. Paths in Maze That Lead to Same Room (Medium)
+    A maze consists of n rooms numbered from 1 to n, and some rooms are 
+    connected by corridors. You are given a 2D integer array corridors where 
+    corridors[i] = [room1i, room2i] indicates that there is a corridor 
+    connecting room1i and room2i, allowing a person in the maze to go from 
+    room1i to room2i and vice versa. The designer of the maze wants to know 
+    how confusing the maze is. The confusion score of the maze is the number 
+    of different cycles of length 3.
+    * For example, 1 → 2 → 3 → 1 is a cycle of length 3, but 1 → 2 → 3 → 4 and 
+      1 → 2 → 3 → 2 → 1 are not.
+    Two cycles are considered to be different if one or more of the rooms 
+    visited in the first cycle is not in the second cycle. Return the confusion 
+    score of the maze.
+
+    Example 1:
+    Input: n = 5, corridors = [[1,2],[5,2],[4,1],[2,4],[3,1],[3,4]]
+    Output: 2
+    Explanation: One cycle of length 3 is 4 → 1 → 3 → 4, denoted in red. Note 
+                 that this is the same cycle as 3 → 4 → 1 → 3 or 1 → 3 → 4 → 1 
+                 because the rooms are the same. Another cycle of length 3 is 
+                 1 → 2 → 4 → 1, denoted in blue. Thus, there are two different 
+                 cycles of length 3.
+    
+    Example 2:
+    Input: n = 4, corridors = [[1,2],[3,4]]
+    Output: 0
+    Explanation: There are no cycles of length 3.
+
+    Constraints:
+    * 2 <= n <= 1000
+    * 1 <= corridors.length <= 5 * 10^4
+    * corridors[i].length == 2
+    * 1 <= room1i, room2i <= n
+    * room1i != room2i
+    * There are no duplicate corridors.*/
+
+    public int numberOfPaths(int n, int[][] corridors) {
+        HashSet<Integer>[] graph = new HashSet[n]; 
+        for (int i = 0; i < n; ++i) graph[i] = new HashSet(); 
+        for (int[] c : corridors) {
+            graph[c[0]-1].add(c[1]-1); 
+            graph[c[1]-1].add(c[0]-1); 
+        }
+        int ans = 0; 
+        for (int[] c : corridors) 
+            for (int u : graph[c[0]-1]) 
+                if (graph[c[1]-1].contains(u)) ++ans; 
+        return ans/3; 
+    }
+
+
     /*2083. Substrings That Begin and End With the Same Letter (Medium)
     You are given a 0-indexed string s consisting of only lowercase English 
     letters. Return the number of substrings in s that begin and end with the 
