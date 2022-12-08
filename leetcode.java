d39ce321c8e5baa676851e3a520243377149c901
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-12-08 16:50:02 -0500

    solve'em all (2189)

--- leetcode.java
+++ leetcode.java
@@ -1024,6 +1024,54 @@ class Solution {
     }
 
 
+    /*2189. Number of Ways to Build House of Cards (Medium)
+    You are given an integer n representing the number of playing cards you 
+    have. A house of cards meets the following conditions:
+    * A house of cards consists of one or more rows of triangles and horizontal 
+      cards.
+    * Triangles are created by leaning two cards against each other.
+    * One card must be placed horizontally between all adjacent triangles in a 
+      row.
+    * Any triangle on a row higher than the first must be placed on a 
+      horizontal card from the previous row.
+    * Each triangle is placed in the leftmost available spot in the row.
+    Return the number of distinct house of cards you can build using all n 
+    cards. Two houses of cards are considered distinct if there exists a row 
+    where the two houses contain a different number of cards.
+
+    Example 1:
+    Input: n = 16
+    Output: 2
+    Explanation: The two valid houses of cards are shown. The third house of 
+                 cards in the diagram is not valid because the rightmost 
+                 triangle on the top row is not placed on top of a horizontal 
+                 card.
+    
+    Example 2:
+    Input: n = 2
+    Output: 1
+    Explanation: The one valid house of cards is shown.
+
+    Example 3:
+    Input: n = 4
+    Output: 0
+    Explanation: The three houses of cards in the diagram are not valid. The 
+                 first house of cards needs a horizontal card placed between 
+                 the two triangles. The second house of cards uses 5 cards. The 
+                 third house of cards uses 2 cards.
+
+    Constraints: 1 <= n <= 500*/
+
+    public int houseOfCards(int n) {
+        int[] dp = new int[n+1]; 
+        dp[0] = 1; 
+        for (int x = 2; x <= n; x += 3) 
+            for (int i = n; i >= x; --i)
+                dp[i] += dp[i-x]; 
+        return dp[n]; 
+    }
+
+
     /*2198. Number of Single Divisor Triplets (Medium)
     You are given a 0-indexed array of positive integers nums. A triplet of 
     three distinct indices (i, j, k) is called a single divisor triplet of nums 
--- leetcode.py
+++ leetcode.py
@@ -59310,6 +59310,53 @@ class Trie:
         return fn(numLaps)
 
 
+    """2189. Number of Ways to Build House of Cards (Medium)
+    You are given an integer n representing the number of playing cards you 
+    have. A house of cards meets the following conditions:
+    * A house of cards consists of one or more rows of triangles and horizontal 
+      cards.
+    * Triangles are created by leaning two cards against each other.
+    * One card must be placed horizontally between all adjacent triangles in a 
+      row.
+    * Any triangle on a row higher than the first must be placed on a 
+      horizontal card from the previous row.
+    * Each triangle is placed in the leftmost available spot in the row.
+    Return the number of distinct house of cards you can build using all n 
+    cards. Two houses of cards are considered distinct if there exists a row 
+    where the two houses contain a different number of cards.
+
+    Example 1:
+    Input: n = 16
+    Output: 2
+    Explanation: The two valid houses of cards are shown. The third house of 
+                 cards in the diagram is not valid because the rightmost 
+                 triangle on the top row is not placed on top of a horizontal 
+                 card.
+    
+    Example 2:
+    Input: n = 2
+    Output: 1
+    Explanation: The one valid house of cards is shown.
+
+    Example 3:
+    Input: n = 4
+    Output: 0
+    Explanation: The three houses of cards in the diagram are not valid. The 
+                 first house of cards needs a horizontal card placed between 
+                 the two triangles. The second house of cards uses 5 cards. The 
+                 third house of cards uses 2 cards.
+
+    Constraints: 1 <= n <= 500"""
+
+    def houseOfCards(self, n: int) -> int:
+        dp = [0]*(n+1)
+        dp[0] = 1
+        for x in range(2, n+1, 3): 
+            for i in range(n, x-1, -1): 
+                dp[i] += dp[i-x]
+        return dp[-1]
+
+
     """2190. Most Frequent Number Following Key In an Array (Easy)
     You are given a 0-indexed integer array nums. You are also given an integer 
     key, which is present in nums. For every unique integer target in nums, 
