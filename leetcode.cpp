a4e82b4e0a320a5b0b46a19471b0d1182badea42
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-12-27 11:26:45 -0500

    complete 2279

--- leetcode.cpp
+++ leetcode.cpp
@@ -45807,6 +45807,52 @@ public:
     }
 
 
+    /*2279. Maximum Bags With Full Capacity of Rocks (Medium)
+    You have n bags numbered from 0 to n - 1. You are given two 0-indexed 
+    integer arrays capacity and rocks. The ith bag can hold a maximum of 
+    capacity[i] rocks and currently contains rocks[i] rocks. You are also given 
+    an integer additionalRocks, the number of additional rocks you can place in 
+    any of the bags. Return the maximum number of bags that could have full 
+    capacity after placing the additional rocks in some bags.
+
+    Example 1:
+    Input: capacity = [2,3,4,5], rocks = [1,2,4,4], additionalRocks = 2
+    Output: 3
+    Explanation: Place 1 rock in bag 0 and 1 rock in bag 1. The number of rocks 
+                 in each bag are now [2,3,4,4]. Bags 0, 1, and 2 have full 
+                 capacity. There are 3 bags at full capacity, so we return 3. 
+                 It can be shown that it is not possible to have more than 3 
+                 bags at full capacity. Note that there may be other ways of 
+                 placing the rocks that result in an answer of 3.
+    
+    Example 2:
+    Input: capacity = [10,2,2], rocks = [2,2,0], additionalRocks = 100
+    Output: 3
+    Explanation: Place 8 rocks in bag 0 and 2 rocks in bag 2. The number of 
+                 rocks in each bag are now [10,2,2]. Bags 0, 1, and 2 have full 
+                 capacity. There are 3 bags at full capacity, so we return 3. 
+                 It can be shown that it is not possible to have more than 3 
+                 bags at full capacity. Note that we did not use all of the 
+                 additional rocks.
+
+    Constraints:
+    * n == capacity.length == rocks.length
+    * 1 <= n <= 5 * 10^4
+    * 1 <= capacity[i] <= 10^9
+    * 0 <= rocks[i] <= capacity[i]
+    * 1 <= additionalRocks <= 10^9*/
+
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        vector<int> diff; 
+        for (int i = 0; i < capacity.size(); ++i) diff.push_back(capacity[i] - rocks[i]); 
+        sort(diff.begin(), diff.end()); 
+        int ans = 0; 
+        for (auto& x : diff) 
+            if (x <= additionalRocks) ++ans, additionalRocks -= x; 
+        return ans; 
+    }
+
+
     /*2282. Number of People That Can Be Seen in a Grid (Medium)
     You are given an m x n 0-indexed 2D array of positive integers heights 
     where heights[i][j] is the height of the person standing at position (i, j).
--- leetcode.java
+++ leetcode.java
@@ -1237,6 +1237,55 @@ class Solution {
     }
 
 
+    /*2279. Maximum Bags With Full Capacity of Rocks (Medium)
+    You have n bags numbered from 0 to n - 1. You are given two 0-indexed 
+    integer arrays capacity and rocks. The ith bag can hold a maximum of 
+    capacity[i] rocks and currently contains rocks[i] rocks. You are also given 
+    an integer additionalRocks, the number of additional rocks you can place in 
+    any of the bags. Return the maximum number of bags that could have full 
+    capacity after placing the additional rocks in some bags.
+
+    Example 1:
+    Input: capacity = [2,3,4,5], rocks = [1,2,4,4], additionalRocks = 2
+    Output: 3
+    Explanation: Place 1 rock in bag 0 and 1 rock in bag 1. The number of rocks 
+                 in each bag are now [2,3,4,4]. Bags 0, 1, and 2 have full 
+                 capacity. There are 3 bags at full capacity, so we return 3. 
+                 It can be shown that it is not possible to have more than 3 
+                 bags at full capacity. Note that there may be other ways of 
+                 placing the rocks that result in an answer of 3.
+    
+    Example 2:
+    Input: capacity = [10,2,2], rocks = [2,2,0], additionalRocks = 100
+    Output: 3
+    Explanation: Place 8 rocks in bag 0 and 2 rocks in bag 2. The number of 
+                 rocks in each bag are now [10,2,2]. Bags 0, 1, and 2 have full 
+                 capacity. There are 3 bags at full capacity, so we return 3. 
+                 It can be shown that it is not possible to have more than 3 
+                 bags at full capacity. Note that we did not use all of the 
+                 additional rocks.
+
+    Constraints:
+    * n == capacity.length == rocks.length
+    * 1 <= n <= 5 * 10^4
+    * 1 <= capacity[i] <= 10^9
+    * 0 <= rocks[i] <= capacity[i]
+    * 1 <= additionalRocks <= 10^9*/
+
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        int[] diff = new int[capacity.length]; 
+        for (int i = 0; i < capacity.length; ++i) diff[i] = capacity[i] - rocks[i]; 
+        Arrays.sort(diff);
+        int ans = 0; 
+        for (var x : diff) 
+            if (x <= additionalRocks) {
+                ++ans; 
+                additionalRocks -= x; 
+            }
+        return ans; 
+    }
+
+
     /*2481. Minimum Cuts to Divide a Circle (Easy)
     A valid cut in a circle can be:
     * A cut that is represented by a straight line that touches two points on 
