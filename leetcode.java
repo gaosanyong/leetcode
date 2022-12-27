8ea809a8189b48998e243ed72f1f8a2ec3e25026
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-12-27 11:27:40 -0500

    complete 55

--- leetcode.java
+++ leetcode.java
@@ -377,6 +377,37 @@ class Solution {
         int k = Arrays.binarySearch(nums, target); 
         return k >= 0 ? k : -k-1; 
     }
+    
+
+    /*55. Jump Game (Medium)
+    You are given an integer array nums. You are initially positioned at the 
+    array's first index, and each element in the array represents your maximum 
+    jump length at that position. Return true if you can reach the last index, 
+    or false otherwise.
+
+    Example 1:
+    Input: nums = [2,3,1,1,4]
+    Output: true
+    Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+    Example 2:
+    Input: nums = [3,2,1,0,4]
+    Output: false
+    Explanation: You will always arrive at index 3 no matter what. Its maximum 
+                 jump length is 0, which makes it impossible to reach the last 
+                 index.
+
+    Constraints:
+    * 1 <= nums.length <= 10^4
+    * 0 <= nums[i] <= 10^5*/
+
+    public boolean canJump(int[] nums) {
+        for (int i = 0, ii = 0; i < nums.length; ++i) {
+            if (ii < i) return false; 
+            ii = Math.max(ii, i+nums[i]); 
+        }
+        return true; 
+    }
 
 
     /*58. Length of Last Word (Easy)
