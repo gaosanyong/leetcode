76dca64fae9c02d334f37777dbf38283d9ec8778
Ye Gao <ye_gao@mfe.berkeley.edu>
2022-12-27 20:40:01 -0500

    complete 1962

--- leetcode.java
+++ leetcode.java
@@ -617,6 +617,57 @@ class Solution {
     }
 
 
+    /*1962. Remove Stones to Minimize the Total (Medium)
+    You are given a 0-indexed integer array piles, where piles[i] represents 
+    the number of stones in the ith pile, and an integer k. You should apply 
+    the following operation exactly k times:
+    * Choose any piles[i] and remove floor(piles[i] / 2) stones from it.
+    Notice that you can apply the operation on the same pile more than once.
+    Return the minimum possible total number of stones remaining after applying 
+    the k operations. floor(x) is the greatest integer that is smaller than or 
+    equal to x (i.e., rounds x down).
+
+    Example 1:
+    Input: piles = [5,4,9], k = 2
+    Output: 12
+    Explanation: Steps of a possible scenario are:
+                 - Apply the operation on pile 2. The resulting piles are 
+                   [5,4,5].
+                 - Apply the operation on pile 0. The resulting piles are 
+                   [3,4,5].
+                 The total number of stones in [3,4,5] is 12.
+    
+    Example 2:
+    Input: piles = [4,3,6,7], k = 3
+    Output: 12
+    Explanation: Steps of a possible scenario are:
+                 - Apply the operation on pile 2. The resulting piles are 
+                   [4,3,3,7].
+                 - Apply the operation on pile 3. The resulting piles are 
+                   [4,3,3,4].
+                 - Apply the operation on pile 0. The resulting piles are 
+                   [2,3,3,4].
+                 The total number of stones in [2,3,3,4] is 12.
+
+    Constraints:
+    * 1 <= piles.length <= 10^5
+    * 1 <= piles[i] <= 10^4
+    * 1 <= k <= 10^5*/
+
+    public int minStoneSum(int[] piles, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder()); 
+        for (var x : piles) pq.add(x); 
+        while (k-- > 0) {
+            var x = pq.poll(); 
+            x -= x/2; 
+            pq.add(x); 
+        }
+        int ans = 0; 
+        while (!pq.isEmpty()) ans += pq.poll(); 
+        return ans; 
+    }
+
+
     /*2046. Sort Linked List Already Sorted Using Absolute Values (Medium)
     Given the head of a singly linked list that is sorted in non-decreasing 
     order using the absolute values of its nodes, return the list sorted in 
