269820c855553c8b9ee89949aabcf6ee670813a3
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-05 11:06:15 -0500

    solve'em all (452)

--- leetcode.java
+++ leetcode.java
@@ -511,6 +511,61 @@ class Solution {
     }
 
 
+    /*452. Minimum Number of Arrows to Burst Balloons (Medium)
+    There are some spherical balloons taped onto a flat wall that represents 
+    the XY-plane. The balloons are represented as a 2D integer array points 
+    where points[i] = [xstart, xend] denotes a balloon whose horizontal 
+    diameter stretches between xstart and xend. You do not know the exact 
+    y-coordinates of the balloons. Arrows can be shot up directly vertically 
+    (in the positive y-direction) from different points along the x-axis. A 
+    balloon with xstart and xend is burst by an arrow shot at x if 
+    xstart <= x <= xend. There is no limit to the number of arrows that can be 
+    shot. A shot arrow keeps traveling up infinitely, bursting any balloons in 
+    its path. Given the array points, return the minimum number of arrows that 
+    must be shot to burst all balloons.
+
+    Example 1:
+    Input: points = [[10,16],[2,8],[1,6],[7,12]]
+    Output: 2
+    Explanation: The balloons can be burst by 2 arrows:
+                 - Shoot an arrow at x = 6, bursting the balloons [2,8] and 
+                   [1,6].
+                 - Shoot an arrow at x = 11, bursting the balloons [10,16] and 
+                   [7,12].
+    
+    Example 2:
+    Input: points = [[1,2],[3,4],[5,6],[7,8]]
+    Output: 4
+    Explanation: One arrow needs to be shot for each balloon for a total of 4 
+                 arrows.
+    
+    Example 3:
+    Input: points = [[1,2],[2,3],[3,4],[4,5]]
+    Output: 2
+    Explanation: The balloons can be burst by 2 arrows:
+                 - Shoot an arrow at x = 2, bursting the balloons [1,2] and 
+                   [2,3].
+                 - Shoot an arrow at x = 4, bursting the balloons [3,4] and 
+                   [4,5].
+
+    Constraints:
+    * 1 <= points.length <= 10^5
+    * points[i].length == 2
+    * -231 <= xstart < xend <= 2^31 - 1*/
+
+    public int findMinArrowShots(int[][] points) {
+        int ans = 0; 
+        long prev = Long.MIN_VALUE; 
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1])); 
+        for (var p : points) 
+            if (prev < p[0]) {
+                ++ans; 
+                prev = p[1]; 
+            }
+        return ans; 
+    }
+
+
     /*616. Add Bold Tag in String (Medium)
     You are given a string s and an array of strings words. You should add a 
     closed pair of bold tag <b> and </b> to wrap the substrings in s that exist 
--- leetcode.py
+++ leetcode.py
@@ -12232,6 +12232,58 @@ class Solution:
         return "".join(ch*x for ch, x in Counter(s).most_common())
 
 
+    """452. Minimum Number of Arrows to Burst Balloons (Medium)
+    There are some spherical balloons taped onto a flat wall that represents 
+    the XY-plane. The balloons are represented as a 2D integer array points 
+    where points[i] = [xstart, xend] denotes a balloon whose horizontal 
+    diameter stretches between xstart and xend. You do not know the exact 
+    y-coordinates of the balloons. Arrows can be shot up directly vertically 
+    (in the positive y-direction) from different points along the x-axis. A 
+    balloon with xstart and xend is burst by an arrow shot at x if 
+    xstart <= x <= xend. There is no limit to the number of arrows that can be 
+    shot. A shot arrow keeps traveling up infinitely, bursting any balloons in 
+    its path. Given the array points, return the minimum number of arrows that 
+    must be shot to burst all balloons.
+
+    Example 1:
+    Input: points = [[10,16],[2,8],[1,6],[7,12]]
+    Output: 2
+    Explanation: The balloons can be burst by 2 arrows:
+                 - Shoot an arrow at x = 6, bursting the balloons [2,8] and 
+                   [1,6].
+                 - Shoot an arrow at x = 11, bursting the balloons [10,16] and 
+                   [7,12].
+    
+    Example 2:
+    Input: points = [[1,2],[3,4],[5,6],[7,8]]
+    Output: 4
+    Explanation: One arrow needs to be shot for each balloon for a total of 4 
+                 arrows.
+    
+    Example 3:
+    Input: points = [[1,2],[2,3],[3,4],[4,5]]
+    Output: 2
+    Explanation: The balloons can be burst by 2 arrows:
+                 - Shoot an arrow at x = 2, bursting the balloons [1,2] and 
+                   [2,3].
+                 - Shoot an arrow at x = 4, bursting the balloons [3,4] and 
+                   [4,5].
+
+    Constraints:
+    * 1 <= points.length <= 10^5
+    * points[i].length == 2
+    * -231 <= xstart < xend <= 2^31 - 1"""
+
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        ans = 0 
+        prev = -inf 
+        for x, y in sorted(points, key=lambda x: x[1]): 
+            if prev < x: 
+                ans += 1
+                prev = y 
+        return ans 
+
+
     """454. 4Sum II (Medium)
     Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, 
     return the number of tuples (i, j, k, l) such that:
