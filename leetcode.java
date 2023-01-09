ad88cb125a20d78246f793b3b1536f944567c178
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-09 17:21:45 -0500

    solve'em all (69)

--- leetcode.java
+++ leetcode.java
@@ -511,6 +511,33 @@ class Solution {
     }
 
 
+    /*69. Sqrt(x) (Easy)
+    Given a non-negative integer x, return the square root of x rounded down to 
+    the nearest integer. The returned integer should be non-negative as well.
+    You must not use any built-in exponent function or operator. For example, 
+    do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+    Example 1:
+    Input: x = 4
+    Output: 2
+    Explanation: The square root of 4 is 2, so we return 2.
+
+    Example 2:
+    Input: x = 8
+    Output: 2
+    Explanation: The square root of 8 is 2.82842..., and since we round it down 
+                 to the nearest integer, 2 is returned.
+
+    Constraints: 0 <= x <= 2^31 - 1*/
+
+    public int mySqrt(int x) {
+        long ans = x; 
+        while (ans * ans > x) 
+            ans = (ans + x/ans)/2; 
+        return (int) ans;
+    }
+
+
     /*144. Binary Tree Preorder Traversal (Easy)
     Given the root of a binary tree, return the preorder traversal of its nodes' 
     values.
--- leetcode.py
+++ leetcode.py
@@ -2318,6 +2318,32 @@ class Solution:
         return ans 
 
 
+    """69. Sqrt(x) (Easy)
+    Given a non-negative integer x, return the square root of x rounded down to 
+    the nearest integer. The returned integer should be non-negative as well.
+    You must not use any built-in exponent function or operator. For example, 
+    do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+    Example 1:
+    Input: x = 4
+    Output: 2
+    Explanation: The square root of 4 is 2, so we return 2.
+
+    Example 2:
+    Input: x = 8
+    Output: 2
+    Explanation: The square root of 8 is 2.82842..., and since we round it down 
+                 to the nearest integer, 2 is returned.
+
+    Constraints: 0 <= x <= 2^31 - 1"""
+
+    def mySqrt(self, x: int) -> int:
+        ans = x 
+        while ans*ans > x: 
+            ans = (ans + x//ans)//2
+        return ans 
+
+
     """70. Climbing Stairs (Easy)
     You are climbing a stair case. It takes n steps to reach to the top. Each 
     time you can either climb 1 or 2 steps. In how many distinct ways can you 
