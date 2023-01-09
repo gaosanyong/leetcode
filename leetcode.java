93fd436e430f99bf1fe5c3b4dfb9c641d8195b7a
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-09 17:49:30 -0500

    java 94

--- leetcode.java
+++ leetcode.java
@@ -644,6 +644,45 @@ class Solution {
     }
 
 
+    /*94. Binary Tree Inorder Traversal (Easy)
+    Given the root of a binary tree, return the inorder traversal of its nodes' 
+    values.
+
+    Example 1:
+    Input: root = [1,null,2,3]
+    Output: [1,3,2]
+
+    Example 2:
+    Input: root = []
+    Output: []
+
+    Example 3:
+    Input: root = [1]
+    Output: [1]
+
+    Constraints:
+    * The number of nodes in the tree is in the range [0, 100].
+    * -100 <= Node.val <= 100
+
+    Follow up: Recursive solution is trivial, could you do it iteratively?*/
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList(); 
+        TreeNode node = root; 
+        Stack<TreeNode> stack = new Stack(); 
+        while (node != null || !stack.isEmpty()) 
+            if (node != null) {
+                stack.push(node); 
+                node = node.left; 
+            } else {
+                node = stack.pop();
+                ans.add(node.val); 
+                node = node.right; 
+            }
+        return ans; 
+    }
+
+
     /*144. Binary Tree Preorder Traversal (Easy)
     Given the root of a binary tree, return the preorder traversal of its nodes' 
     values.
