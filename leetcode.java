fd8ec3a86ef285d0217b13a9002a4ba0d1c37a7f
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-09 17:29:57 -0500

    java 83

--- leetcode.java
+++ leetcode.java
@@ -571,6 +571,32 @@ class Solution {
     }
 
 
+    /*83. Remove Duplicates from Sorted List (Easy)
+    Given the head of a sorted linked list, delete all duplicates such that 
+    each element appears only once. Return the linked list sorted as well.
+
+    Example 1:
+    Input: head = [1,1,2]
+    Output: [1,2]
+
+    Example 2:
+    Input: head = [1,1,2,3,3]
+    Output: [1,2,3]
+
+    Constraints:
+    * The number of nodes in the list is in the range [0, 300].
+    * -100 <= Node.val <= 100
+    * The list is guaranteed to be sorted in ascending order.*/
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head; 
+        while (node != null)
+            if (node.next != null && node.val == node.next.val) node.next = node.next.next; 
+            else node = node.next; 
+        return head; 
+    }
+
+
     /*144. Binary Tree Preorder Traversal (Easy)
     Given the root of a binary tree, return the preorder traversal of its nodes' 
     values.
