e3ece2c1022c307f13eb41068d1e5e7c8e2d2bb2
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-10 17:44:26 -0500

    solve'em all (1231)

--- leetcode.java
+++ leetcode.java
@@ -1314,6 +1314,52 @@ class Solution {
     }
 
 
+    /*1231. Divide Chocolate (Hard)
+    You have one chocolate bar that consists of some chunks. Each chunk has its 
+    own sweetness given by the array sweetness. You want to share the chocolate 
+    with your k friends so you start cutting the chocolate bar into k + 1 
+    pieces using k cuts, each piece consists of some consecutive chunks. Being 
+    generous, you will eat the piece with the minimum total sweetness and give 
+    the other pieces to your friends. Find the maximum total sweetness of the 
+    piece you can get by cutting the chocolate bar optimally.
+
+    Example 1:
+    Input: sweetness = [1,2,3,4,5,6,7,8,9], k = 5
+    Output: 6
+    Explanation: You can divide the chocolate to [1,2,3], [4,5], [6], [7], [8], [9]
+
+    Example 2:
+    Input: sweetness = [5,6,7,8,9,1,2,3,4], k = 8
+    Output: 1
+    Explanation: There is only one way to cut the bar into 9 pieces.
+
+    Example 3:
+    Input: sweetness = [1,2,2,1,2,2,1,2,2], k = 2
+    Output: 5
+    Explanation: You can divide the chocolate to [1,2,2], [1,2,2], [1,2,2]
+
+    Constraints:
+    * 0 <= k < sweetness.length <= 10^4
+    * 1 <= sweetness[i] <= 10^5*/
+
+    public int maximizeSweetness(int[] sweetness, int k) {
+        int lo = 0, hi = 1_000_000_000;
+        while (lo < hi) {
+            int mid = lo + (hi-lo+1)/2, cnt = 0, prefix = 0; 
+            for (var x : sweetness) {
+                prefix += x; 
+                if (prefix >= mid) {
+                    ++cnt; 
+                    prefix = 0; 
+                }
+            }
+            if (cnt < k+1) hi = mid-1; 
+            else lo = mid; 
+        }
+        return lo; 
+    }
+
+
     /*1962. Remove Stones to Minimize the Total (Medium)
     You are given a 0-indexed integer array piles, where piles[i] represents 
     the number of stones in the ith pile, and an integer k. You should apply 
--- leetcode.py
+++ leetcode.py
@@ -30663,6 +30663,49 @@ class UnionFind:
         return dp[0][-1]
 
 
+    """1231. Divide Chocolate (Hard)
+    You have one chocolate bar that consists of some chunks. Each chunk has its 
+    own sweetness given by the array sweetness. You want to share the chocolate 
+    with your k friends so you start cutting the chocolate bar into k + 1 
+    pieces using k cuts, each piece consists of some consecutive chunks. Being 
+    generous, you will eat the piece with the minimum total sweetness and give 
+    the other pieces to your friends. Find the maximum total sweetness of the 
+    piece you can get by cutting the chocolate bar optimally.
+
+    Example 1:
+    Input: sweetness = [1,2,3,4,5,6,7,8,9], k = 5
+    Output: 6
+    Explanation: You can divide the chocolate to [1,2,3], [4,5], [6], [7], [8], [9]
+
+    Example 2:
+    Input: sweetness = [5,6,7,8,9,1,2,3,4], k = 8
+    Output: 1
+    Explanation: There is only one way to cut the bar into 9 pieces.
+
+    Example 3:
+    Input: sweetness = [1,2,2,1,2,2,1,2,2], k = 2
+    Output: 5
+    Explanation: You can divide the chocolate to [1,2,2], [1,2,2], [1,2,2]
+
+    Constraints:
+    * 0 <= k < sweetness.length <= 10^4
+    * 1 <= sweetness[i] <= 10^5"""
+
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
+        lo, hi = 0, 1_000_000_000
+        while lo < hi: 
+            mid = lo + hi + 1 >> 1
+            cnt = prefix = 0 
+            for x in sweetness: 
+                prefix += x 
+                if prefix >= mid: 
+                    cnt += 1
+                    prefix = 0 
+            if cnt < k+1: hi = mid - 1
+            else: lo = mid
+        return lo 
+
+
     """1232. Check If It Is a Straight Line (Easy)
     You are given an array coordinates, coordinates[i] = [x, y], where [x, y] 
     represents the coordinate of a point. Check if these points make a straight 
