29e21a230c01982a33d2a58c07eaa09d6a2f2177
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-12 10:57:13 -0500

    solve'em all (1519)

--- leetcode.py
+++ leetcode.py
@@ -35150,6 +35150,77 @@ class UnionFind:
         return ans 
 
 
+    """1519. Number of Nodes in the Sub-Tree With the Same Label (Medium)
+    You are given a tree (i.e. a connected, undirected graph that has no cycles) 
+    consisting of n nodes numbered from 0 to n - 1 and exactly n - 1 edges. The 
+    root of the tree is the node 0, and each node of the tree has a label which 
+    is a lower-case character given in the string labels (i.e. The node with 
+    the number i has the label labels[i]). The edges array is given on the form 
+    edges[i] = [ai, bi], which means there is an edge between nodes ai and bi 
+    in the tree. Return an array of size n where ans[i] is the number of nodes 
+    in the subtree of the ith node which have the same label as node i. A 
+    subtree of a tree T is the tree consisting of a node in T and all of its 
+    descendant nodes.
+
+    Example 1:
+    Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], labels = "abaedcd"
+    Output: [2,1,1,1,1,1,1]
+    Explanation: Node 0 has label 'a' and its sub-tree has node 2 with label 
+                 'a' as well, thus the answer is 2. Notice that any node is 
+                 part of its sub-tree. Node 1 has a label 'b'. The sub-tree of 
+                 node 1 contains nodes 1,4 and 5, as nodes 4 and 5 have 
+                 different labels than node 1, the answer is just 1 (the node 
+                 itself).
+    
+    Example 2:
+    Input: n = 4, edges = [[0,1],[1,2],[0,3]], labels = "bbbb"
+    Output: [4,2,1,1]
+    Explanation: The sub-tree of node 2 contains only node 2, so the answer is 
+                 1. The sub-tree of node 3 contains only node 3, so the answer 
+                 is 1. The sub-tree of node 1 contains nodes 1 and 2, both have 
+                 label 'b', thus the answer is 2. The sub-tree of node 0 
+                 contains nodes 0, 1, 2 and 3, all with label 'b', thus the 
+                 answer is 4.
+    
+    Example 3:
+    Input: n = 5, edges = [[0,1],[0,2],[1,3],[0,4]], labels = "aabab"
+    Output: [3,2,1,1,1]
+
+    Constraints:
+    * 1 <= n <= 10^5
+    * edges.length == n - 1
+    * edges[i].length == 2
+    * 0 <= ai, bi < n
+    * ai != bi
+    * labels.length == n
+    * labels is consisting of only of lowercase English letters."""
+
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
+        tree = [[] for _ in range(n)]
+        for u, v in edges:
+            tree[u].append(v)
+            tree[v].append(u)
+        ans = [0]*n 
+        freq = [[0]*26 for _ in range(n)]
+        seen = [False]*n 
+        stack = [(0, -1)]
+        while stack: 
+            u, p = stack[-1]
+            if seen[u]: 
+                freq[u][ord(labels[u]) - 97] += 1
+                for v in tree[u]: 
+                    if v != p: 
+                        for i, x in enumerate(freq[v]): 
+                            freq[u][i] += x
+                ans[u] = freq[u][ord(labels[u]) - 97]
+                stack.pop()
+            else: 
+                for v in tree[u]: 
+                    if v != p: stack.append((v, u))
+                seen[u] = True 
+        return ans 
+
+
     """1520. Maximum Number of Non-Overlapping Substrings (Hard)
     Given a string s of lowercase letters, you need to find the maximum number 
     of non-empty substrings of s that meet the following conditions:
