7c1819e30f351a2cef499b56552ca129bfed82a6
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-13 14:44:55 -0500

    complete 190

--- leetcode.cpp
+++ leetcode.cpp
@@ -5416,9 +5416,9 @@ public:
     Follow up: If this function is called many times, how would you optimize it?*/
 
     uint32_t reverseBits(uint32_t n) {
-        int ans = 0; 
-        for (int i = 0; i < 32; ++i) 
-            if (n & 1 << i) ans ^= 1 << 31-i; 
+        uint32_t ans = 0; 
+        for (int i = 0; i < 32; ++i, n >>= 1)
+            ans = (ans<<1) + (n&1); 
         return ans; 
     }
 
