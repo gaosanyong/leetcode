67e2a3e0f6fddb75ba9ff7331699dfb1d6bdbee7
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-12 21:44:27 -0500

    solve'em all (2533)

--- leetcode.cpp
+++ leetcode.cpp
@@ -52362,6 +52362,49 @@ public:
         }
         return ans; 
     }
+
+
+    /*2533. Number of Good Binary Strings (Medium)
+    You are given four integers minLenght, maxLength, oneGroup and zeroGroup. A 
+    binary string is good if it satisfies the following conditions:
+    * The length of the string is in the range [minLength, maxLength].
+    * The size of each block of consecutive 1's is a multiple of oneGroup.
+      + For example in a binary string 00110111100 sizes of each block of 
+        consecutive ones are [2,4].
+    * The size of each block of consecutive 0's is a multiple of zeroGroup.
+      + For example, in a binary string 00110111100 sizes of each block of 
+        consecutive ones are [2,1,2].
+    Return the number of good binary strings. Since the answer may be too large, 
+    return it modulo 10^9 + 7. Note that 0 is considered a multiple of all the 
+    numbers.
+
+    Example 1:
+    Input: minLength = 2, maxLength = 3, oneGroup = 1, zeroGroup = 2
+    Output: 5
+    Explanation: There are 5 good binary strings in this example: "00", "11", 
+                 "001", "100", and "111". It can be proven that there are only 
+                 5 good strings satisfying all conditions.
+    
+    Example 2:
+    Input: minLength = 4, maxLength = 4, oneGroup = 4, zeroGroup = 3
+    Output: 1
+    Explanation: There is only 1 good binary string in this example: "1111". It 
+                 can be proven that there is only 1 good string satisfying all 
+                 conditions.
+
+    Constraints:
+    * 1 <= minLength <= maxLength <= 10^5
+    * 1 <= oneGroup, zeroGroup <= maxLength*/
+
+    int goodBinaryStrings(int minLength, int maxLength, int oneGroup, int zeroGroup) {
+        vector<long> dp(maxLength+1); 
+        for (int i = maxLength; i >= 0; --i) {
+            if (minLength <= i) ++dp[i]; 
+            if (i+oneGroup <= maxLength) dp[i] = (dp[i] + dp[i+oneGroup]) % 1'000'000'007; 
+            if (i+zeroGroup <= maxLength) dp[i] = (dp[i] + dp[i+zeroGroup]) % 1'000'000'007; 
+        }
+        return dp[0]; 
+    }
 };
 
 
--- leetcode.py
+++ leetcode.py
@@ -72916,6 +72916,48 @@ class SegTree:
         return ans 
 
 
+    """2533. Number of Good Binary Strings (Medium)
+    You are given four integers minLenght, maxLength, oneGroup and zeroGroup. A 
+    binary string is good if it satisfies the following conditions:
+    * The length of the string is in the range [minLength, maxLength].
+    * The size of each block of consecutive 1's is a multiple of oneGroup.
+      + For example in a binary string 00110111100 sizes of each block of 
+        consecutive ones are [2,4].
+    * The size of each block of consecutive 0's is a multiple of zeroGroup.
+      + For example, in a binary string 00110111100 sizes of each block of 
+        consecutive ones are [2,1,2].
+    Return the number of good binary strings. Since the answer may be too large, 
+    return it modulo 10^9 + 7. Note that 0 is considered a multiple of all the 
+    numbers.
+
+    Example 1:
+    Input: minLength = 2, maxLength = 3, oneGroup = 1, zeroGroup = 2
+    Output: 5
+    Explanation: There are 5 good binary strings in this example: "00", "11", 
+                 "001", "100", and "111". It can be proven that there are only 
+                 5 good strings satisfying all conditions.
+    
+    Example 2:
+    Input: minLength = 4, maxLength = 4, oneGroup = 4, zeroGroup = 3
+    Output: 1
+    Explanation: There is only 1 good binary string in this example: "1111". It 
+                 can be proven that there is only 1 good string satisfying all 
+                 conditions.
+
+    Constraints:
+    * 1 <= minLength <= maxLength <= 10^5
+    * 1 <= oneGroup, zeroGroup <= maxLength"""
+
+    def goodBinaryStrings(self, minLength: int, maxLength: int, oneGroup: int, zeroGroup: int) -> int:
+        dp = [0] * (maxLength+1)
+        for i in range(maxLength, -1, -1): 
+            if minLength <= i: dp[i] += 1
+            if i+oneGroup <= maxLength: dp[i] += dp[i+oneGroup]
+            if i+zeroGroup <= maxLength: dp[i] += dp[i+zeroGroup]
+            dp[i] %= 1_000_000_007 
+        return dp[0]
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
