0fc61849eccae29d46bb776d850923565631709b
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-13 14:37:10 -0500

    complete 171

--- leetcode.cpp
+++ leetcode.cpp
@@ -5226,7 +5226,7 @@ public:
     int titleToNumber(string columnTitle) {
         int ans = 0; 
         for (auto& ch : columnTitle) 
-            ans = 26*ans + (ch - '@'); 
+            ans = 26*ans + (ch - 'A' + 1); 
         return ans; 
     }
 
--- leetcode.java
+++ leetcode.java
@@ -1184,6 +1184,43 @@ class Solution {
     }
 
 
+    /*171. Excel Sheet Column Number (Easy)
+    Given a string columnTitle that represents the column title as appears in 
+    an Excel sheet, return its corresponding column number. For example: 
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+
+    Example 1:
+    Input: columnTitle = "A"
+    Output: 1
+
+    Example 2:
+    Input: columnTitle = "AB"
+    Output: 28
+
+    Example 3:
+    Input: columnTitle = "ZY"
+    Output: 701
+
+    Constraints:
+    * 1 <= columnTitle.length <= 7
+    * columnTitle consists only of uppercase English letters.
+    * columnTitle is in the range ["A", "FXSHRXW"].*/
+
+    public int titleToNumber(String columnTitle) {
+        int ans = 0; 
+        for (var ch : columnTitle.toCharArray()) 
+            ans = 26*ans + (ch - 'A' + 1); 
+        return ans; 
+    }
+
+
     /*452. Minimum Number of Arrows to Burst Balloons (Medium)
     There are some spherical balloons taped onto a flat wall that represents 
     the XY-plane. The balloons are represented as a 2D integer array points 
