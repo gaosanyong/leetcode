6f1ce1d4f2dcabf44e067f793c7fd303294966af
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-13 14:29:36 -0500

    java 169

--- leetcode.java
+++ leetcode.java
@@ -1152,6 +1152,38 @@ class Solution {
     }
 
 
+    /*169. Majority Element (Easy)
+    Given an array nums of size n, return the majority element. The majority 
+    element is the element that appears more than ⌊n / 2⌋ times. You may assume 
+    that the majority element always exists in the array.
+
+    Example 1:
+    Input: nums = [3,2,3]
+    Output: 3
+
+    Example 2:
+    Input: nums = [2,2,1,1,1,2,2]
+    Output: 2
+
+    Constraints:
+    * n == nums.length
+    * 1 <= n <= 5 * 10^4
+    * -10^9 <= nums[i] <= 10^9
+
+    Follow-up: Could you solve the problem in linear time and in O(1) space?*/
+
+    public int majorityElement(int[] nums) {
+        /*Boyer-Moore majority vote algo*/
+        int ans = 0, vote = 0; 
+        for (var x : nums) {
+            if (vote == 0) ans = x; 
+            if (x == ans) ++vote; 
+            else --vote; 
+        }
+        return ans; 
+    }
+
+
     /*452. Minimum Number of Arrows to Burst Balloons (Medium)
     There are some spherical balloons taped onto a flat wall that represents 
     the XY-plane. The balloons are represented as a 2D integer array points 
