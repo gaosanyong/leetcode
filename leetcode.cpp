adf9211886c348b7b29d9157b95da754ad7382cd
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-17 21:13:21 -0500

    solve'em all (918)

--- leetcode.cpp
+++ leetcode.cpp
@@ -20129,6 +20129,49 @@ public:
     }
 
 
+    /*918. Maximum Sum Circular Subarray (Medium)
+    Given a circular integer array nums of length n, return the maximum 
+    possible sum of a non-empty subarray of nums. A circular array means the 
+    end of the array connects to the beginning of the array. Formally, the 
+    next element of nums[i] is nums[(i + 1) % n] and the previous element of 
+    nums[i] is nums[(i - 1 + n) % n]. A subarray may only include each element 
+    of the fixed buffer nums at most once. Formally, for a subarray nums[i], 
+    nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with 
+    k1 % n == k2 % n.
+
+    Example 1:
+    Input: nums = [1,-2,3,-2]
+    Output: 3
+    Explanation: Subarray [3] has maximum sum 3.
+
+    Example 2:
+    Input: nums = [5,-3,5]
+    Output: 10
+    Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10.
+
+    Example 3:
+    Input: nums = [-3,-2,-3]
+    Output: -2
+    Explanation: Subarray [-2] has maximum sum -2.
+
+    Constraints:
+    * n == nums.length
+    * 1 <= n <= 3 * 10^4
+    * -3 * 10^4 <= nums[i] <= 3 * 10^4*/
+
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int large = 0, small = 0, total = 0, most = INT_MIN, least = INT_MIN; 
+        for (auto& x : nums) {
+            total += x; 
+            large = max(0, large) + x; 
+            most  = max(most, large); 
+            small = max(0, small - x); 
+            least = max(least, small); 
+        }
+        return most >= 0 ? max(most, total + least) : most; 
+    }
+
+
     /*920. Number of Music Playlists (Hard)
     Your music player contains n different songs and she wants to listen to 
     goal (not necessarily different) songs during your trip.  You create a 
--- leetcode.java
+++ leetcode.java
@@ -1667,6 +1667,49 @@ class Solution {
     }
 
 
+    /*918. Maximum Sum Circular Subarray (Medium)
+    Given a circular integer array nums of length n, return the maximum 
+    possible sum of a non-empty subarray of nums. A circular array means the 
+    end of the array connects to the beginning of the array. Formally, the 
+    next element of nums[i] is nums[(i + 1) % n] and the previous element of 
+    nums[i] is nums[(i - 1 + n) % n]. A subarray may only include each element 
+    of the fixed buffer nums at most once. Formally, for a subarray nums[i], 
+    nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with 
+    k1 % n == k2 % n.
+
+    Example 1:
+    Input: nums = [1,-2,3,-2]
+    Output: 3
+    Explanation: Subarray [3] has maximum sum 3.
+
+    Example 2:
+    Input: nums = [5,-3,5]
+    Output: 10
+    Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10.
+
+    Example 3:
+    Input: nums = [-3,-2,-3]
+    Output: -2
+    Explanation: Subarray [-2] has maximum sum -2.
+
+    Constraints:
+    * n == nums.length
+    * 1 <= n <= 3 * 10^4
+    * -3 * 10^4 <= nums[i] <= 3 * 10^4*/
+
+    public int maxSubarraySumCircular(int[] nums) {
+        int large = 0, small = 0, total = 0, most = Integer.MIN_VALUE, least = Integer.MIN_VALUE; 
+        for (var x : nums) {
+            total += x; 
+            large = Math.max(0, large) + x; 
+            most  = Math.max(most, large); 
+            small = Math.max(0, small - x); 
+            least = Math.max(least, small); 
+        }
+        return most >= 0 ? Math.max(most, total + least) : most; 
+    }
+
+
     /*926. Flip String to Monotone Increasing (Medium)
     A binary string is monotone increasing if it consists of some number of 0's 
     (possibly none), followed by some number of 1's (also possibly none). You 
