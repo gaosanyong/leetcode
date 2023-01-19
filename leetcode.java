03010b3e474e2b28e50a23933b5203a653b25727
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-18 22:29:40 -0500

    solve'em all (974)

--- leetcode.java
+++ leetcode.java
@@ -1746,6 +1746,40 @@ class Solution {
     }
 
 
+    /*974. Subarray Sums Divisible by K (Medium)
+    Given an integer array nums and an integer k, return the number of non-
+    empty subarrays that have a sum divisible by k. A subarray is a contiguous 
+    part of an array.
+
+    Example 1:
+    Input: nums = [4,5,0,-2,-3,1], k = 5
+    Output: 7
+    Explanation: There are 7 subarrays with a sum divisible by k = 5:
+                 [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], 
+                 [0, -2, -3], [-2, -3]
+    
+    Example 2:
+    Input: nums = [5], k = 9
+    Output: 0
+
+    Constraints:
+    * 1 <= nums.length <= 3 * 10^4
+    * -10^4 <= nums[i] <= 10^4
+    * 2 <= k <= 10^4*/
+
+    public int subarraysDivByK(int[] nums, int k) {
+        int ans = 0, prefix = 0; 
+        Map<Integer, Integer> freq = new HashMap(); 
+        freq.put(0, 1); 
+        for (var x : nums) {
+            prefix = (prefix + x%k + k) % k; 
+            ans += freq.getOrDefault(prefix, 0); 
+            freq.merge(prefix, 1, Integer::sum); 
+        }
+        return ans; 
+    }
+
+
     /*1063. Number of Valid Subarrays (Hard)
     Given an integer array nums, return the number of non-empty subarrays with 
     the leftmost element of the subarray not larger than other elements in the 
--- leetcode.py
+++ leetcode.py
@@ -24344,6 +24344,37 @@ class Solution:
         return [[x, y] for x, y in points if x*x + y*y <= dist[mid]]
 
 
+    """974. Subarray Sums Divisible by K (Medium)
+    Given an integer array nums and an integer k, return the number of non-
+    empty subarrays that have a sum divisible by k. A subarray is a contiguous 
+    part of an array.
+
+    Example 1:
+    Input: nums = [4,5,0,-2,-3,1], k = 5
+    Output: 7
+    Explanation: There are 7 subarrays with a sum divisible by k = 5:
+                 [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], 
+                 [0, -2, -3], [-2, -3]
+    
+    Example 2:
+    Input: nums = [5], k = 9
+    Output: 0
+
+    Constraints:
+    * 1 <= nums.length <= 3 * 10^4
+    * -10^4 <= nums[i] <= 10^4
+    * 2 <= k <= 10^4"""
+
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        ans = prefix = 0
+        freq = Counter({0: 1})
+        for x in nums: 
+            prefix = (prefix + x) % k 
+            ans += freq[prefix]
+            freq[prefix] += 1
+        return ans 
+
+
     """975. Odd Even Jump (Hard)
     You are given an integer array arr. From some starting index, you can make 
     a series of jumps. The (1st, 3rd, 5th, ...) jumps in the series are called 
