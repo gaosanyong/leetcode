923f58aff738b6528942b0b11de6f17452dcaba2
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-28 12:08:47 -0500

    solve'em all (2548)

--- leetcode.java
+++ leetcode.java
@@ -6488,6 +6488,56 @@ class Solution {
         }
         return dp[0]; 
     }
+
+
+    /*2548. Maximum Price to Fill a Bag (Medium)
+    You are given a 2D integer array items where items[i] = [pricei, weighti] 
+    denotes the price and weight of the ith item, respectively. You are also 
+    given a positive integer capacity. Each item can be divided into two items 
+    with ratios part1 and part2, where part1 + part2 == 1.
+    * The weight of the first item is weighti * part1 and the price of the 
+      first item is pricei * part1.
+    * Similarly, the weight of the second item is weighti * part2 and the price 
+      of the second item is pricei * part2.
+    Return the maximum total price to fill a bag of capacity capacity with 
+    given items. If it is impossible to fill a bag return -1. Answers within 
+    10^-5 of the actual answer will be considered accepted.
+
+    Example 1:
+    Input: items = [[50,1],[10,8]], capacity = 5
+    Output: 55.00000
+    Explanation: We divide the 2nd item into two parts with part1 = 0.5 and 
+                 part2 = 0.5. The price and weight of the 1st item are 5, 4. 
+                 And similarly, the price and the weight of the 2nd item are 
+                 5, 4. The array items after operation becomes 
+                 [[50,1],[5,4],[5,4]]. To fill a bag with capacity 5 we take 
+                 the 1st element with a price of 50 and the 2nd element with a 
+                 price of 5. It can be proved that 55.0 is the maximum total 
+                 price that we can achieve.
+    
+    Example 2:
+    Input: items = [[100,30]], capacity = 50
+    Output: -1.00000
+    Explanation: It is impossible to fill a bag with the given item.
+
+    Constraints:
+    * 1 <= items.length <= 10^5
+    * items[i].length == 2
+    * 1 <= pricei, weighti <= 10^4
+    * 1 <= capacity <= 10^9*/
+
+    public double maxPrice(int[][] items, int capacity) {
+        double ans = 0; 
+        Arrays.sort(items, (a, b) -> Double.compare((double) b[0]/b[1], (double) a[0]/a[1])); 
+        for (var item : items) {
+            double p = item[0]; 
+            if (item[1] > capacity) p *= (double) capacity/item[1]; 
+            ans += p; 
+            capacity -= Math.min(item[1], capacity); 
+            if (capacity == 0) break; 
+        }
+        return capacity == 0 ? ans : -1; 
+    }
 }
 
 
--- leetcode.py
+++ leetcode.py
@@ -73953,6 +73953,52 @@ class SegTree:
         return dp[0]
 
 
+    """2548. Maximum Price to Fill a Bag (Medium)
+    You are given a 2D integer array items where items[i] = [pricei, weighti] 
+    denotes the price and weight of the ith item, respectively. You are also 
+    given a positive integer capacity. Each item can be divided into two items 
+    with ratios part1 and part2, where part1 + part2 == 1.
+    * The weight of the first item is weighti * part1 and the price of the 
+      first item is pricei * part1.
+    * Similarly, the weight of the second item is weighti * part2 and the price 
+      of the second item is pricei * part2.
+    Return the maximum total price to fill a bag of capacity capacity with 
+    given items. If it is impossible to fill a bag return -1. Answers within 
+    10^-5 of the actual answer will be considered accepted.
+
+    Example 1:
+    Input: items = [[50,1],[10,8]], capacity = 5
+    Output: 55.00000
+    Explanation: We divide the 2nd item into two parts with part1 = 0.5 and 
+                 part2 = 0.5. The price and weight of the 1st item are 5, 4. 
+                 And similarly, the price and the weight of the 2nd item are 
+                 5, 4. The array items after operation becomes 
+                 [[50,1],[5,4],[5,4]]. To fill a bag with capacity 5 we take 
+                 the 1st element with a price of 50 and the 2nd element with a 
+                 price of 5. It can be proved that 55.0 is the maximum total 
+                 price that we can achieve.
+    
+    Example 2:
+    Input: items = [[100,30]], capacity = 50
+    Output: -1.00000
+    Explanation: It is impossible to fill a bag with the given item.
+
+    Constraints:
+    * 1 <= items.length <= 10^5
+    * items[i].length == 2
+    * 1 <= pricei, weighti <= 10^4
+    * 1 <= capacity <= 10^9"""
+
+    def maxPrice(self, items: List[List[int]], capacity: int) -> float:
+        ans = 0 
+        for p, w in sorted(items, key=lambda x: x[0]/x[1], reverse=True): 
+            if w > capacity: p *= capacity / w
+            ans += p
+            capacity -= min(w, capacity)
+            if not capacity: break 
+        return ans if not capacity else -1 
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It 
 should support the following operations: get and put. 
