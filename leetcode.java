b6c81daf1078bd3bfcd80b12bc60494766d4451a
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-29 21:18:30 -0500

    java 1137

--- leetcode.java
+++ leetcode.java
@@ -2156,6 +2156,34 @@ class Solution {
     }
 
 
+    /*1137. N-th Tribonacci Number (Easy)
+    The Tribonacci sequence Tn is defined as follows: 
+    T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+    Given n, return the value of Tn.
+
+    Example 1:
+    Input: n = 4
+    Output: 4
+    Explanation: T_3 = 0 + 1 + 1 = 2
+                 T_4 = 1 + 1 + 2 = 4
+    
+    Example 2:
+    Input: n = 25
+    Output: 1389537
+
+    Constraints:
+    * 0 <= n <= 37
+    * The answer is guaranteed to fit within a 32-bit integer, ie. 
+      answer <= 2^31 - 1.*/
+
+    public int tribonacci(int n) {
+        int[] dp = new int[] {0, 1, 1}; 
+        for (int i = 0; i < n; ++i) 
+            dp[i%3] = dp[0] + dp[1] + dp[2]; 
+        return dp[n%3]; 
+    }
+
+
     /*1183. Maximum Number of Ones (Hard)
     Consider a matrix M with dimensions width * height, such that every cell 
     has value 0 or 1, and any square sub-matrix of M of size 
