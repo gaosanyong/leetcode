e4f8f9abd073f969ae3c66336d2ab2b9b38dfff8
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-01-31 00:19:41 -0500

    solve'em all (2143)

--- leetcode.cpp
+++ leetcode.cpp
@@ -44504,6 +44504,80 @@ public:
     }
 
 
+    /*2143. Choose Numbers From Two Arrays in Range (Hard)
+    You are given two 0-indexed integer arrays nums1 and nums2 of length n. A 
+    range [l, r] (inclusive) where 0 <= l <= r < n is balanced if:
+    * For every i in the range [l, r], you pick either nums1[i] or nums2[i].
+    * The sum of the numbers you pick from nums1 equals to the sum of the 
+      numbers you pick from nums2 (the sum is considered to be 0 if you pick no 
+      numbers from an array).
+    Two balanced ranges from [l1, r1] and [l2, r2] are considered to be 
+    different if at least one of the following is true:
+    * l1 != l2
+    * r1 != r2
+    * nums1[i] is picked in the first range, and nums2[i] is picked in the 
+      second range or vice versa for at least one i.
+    Return the number of different ranges that are balanced. Since the answer 
+    may be very large, return it modulo 10^9 + 7.
+
+    Example 1:
+    Input: nums1 = [1,2,5], nums2 = [2,6,3]
+    Output: 3
+    Explanation: The balanced ranges are:
+                 - [0, 1] where we choose nums2[0], and nums1[1].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 2 = 2.
+                 - [0, 2] where we choose nums1[0], nums2[1], and nums1[2].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 1 + 5 = 6.
+                 - [0, 2] where we choose nums1[0], nums1[1], and nums2[2].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 1 + 2 = 3.
+                 Note that the second and third balanced ranges are different.
+                 In the second balanced range, we choose nums2[1] and in the 
+                 third balanced range, we choose nums1[1].
+    
+    Example 2:
+    Input: nums1 = [0,1], nums2 = [1,0]
+    Output: 4
+    Explanation: The balanced ranges are:
+                 - [0, 0] where we choose nums1[0].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 0 = 0.
+                 - [1, 1] where we choose nums2[1].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 0 = 0.
+                 - [0, 1] where we choose nums1[0] and nums2[1].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 0 = 0.
+                 - [0, 1] where we choose nums2[0] and nums1[1].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 1 = 1.
+
+    Constraints:
+    * n == nums1.length == nums2.length
+    * 1 <= n <= 100
+    * 0 <= nums1[i], nums2[i] <= 100*/
+
+    int countSubranges(vector<int>& nums1, vector<int>& nums2) {
+        const int mod = 1'000'000'007; 
+        long ans = 0; 
+        unordered_map<int, long> freq; 
+        for (int i = 0; i < nums1.size(); ++i) {
+            unordered_map<int, long> ff; 
+            ++ff[nums1[i]];
+            ++ff[-nums2[i]];
+            for (auto& [k, v] : freq) {
+                ff[k+nums1[i]] = (ff[k+nums1[i]] + v) % mod; 
+                ff[k-nums2[i]] = (ff[k-nums2[i]] + v) % mod; 
+            }
+            freq = ff; 
+            ans = (ans + freq[0]) % mod; 
+        }
+        return ans; 
+    }
+
+
     /*2144. Minimum Cost of Buying Candies With Discount (Easy)
     A shop is selling candies at a discount. For every two candies sold, the 
     shop gives a third candy for free. The customer can choose any candy to 
--- leetcode.java
+++ leetcode.java
@@ -3030,6 +3030,82 @@ class Solution {
     }
 
 
+    /*2143. Choose Numbers From Two Arrays in Range (Hard)
+    You are given two 0-indexed integer arrays nums1 and nums2 of length n. A 
+    range [l, r] (inclusive) where 0 <= l <= r < n is balanced if:
+    * For every i in the range [l, r], you pick either nums1[i] or nums2[i].
+    * The sum of the numbers you pick from nums1 equals to the sum of the 
+      numbers you pick from nums2 (the sum is considered to be 0 if you pick no 
+      numbers from an array).
+    Two balanced ranges from [l1, r1] and [l2, r2] are considered to be 
+    different if at least one of the following is true:
+    * l1 != l2
+    * r1 != r2
+    * nums1[i] is picked in the first range, and nums2[i] is picked in the 
+      second range or vice versa for at least one i.
+    Return the number of different ranges that are balanced. Since the answer 
+    may be very large, return it modulo 10^9 + 7.
+
+    Example 1:
+    Input: nums1 = [1,2,5], nums2 = [2,6,3]
+    Output: 3
+    Explanation: The balanced ranges are:
+                 - [0, 1] where we choose nums2[0], and nums1[1].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 2 = 2.
+                 - [0, 2] where we choose nums1[0], nums2[1], and nums1[2].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 1 + 5 = 6.
+                 - [0, 2] where we choose nums1[0], nums1[1], and nums2[2].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 1 + 2 = 3.
+                 Note that the second and third balanced ranges are different.
+                 In the second balanced range, we choose nums2[1] and in the 
+                 third balanced range, we choose nums1[1].
+    
+    Example 2:
+    Input: nums1 = [0,1], nums2 = [1,0]
+    Output: 4
+    Explanation: The balanced ranges are:
+                 - [0, 0] where we choose nums1[0].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 0 = 0.
+                 - [1, 1] where we choose nums2[1].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 0 = 0.
+                 - [0, 1] where we choose nums1[0] and nums2[1].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 0 = 0.
+                 - [0, 1] where we choose nums2[0] and nums1[1].
+                   The sum of the numbers chosen from nums1 equals the sum of 
+                   the numbers chosen from nums2: 1 = 1.
+
+    Constraints:
+    * n == nums1.length == nums2.length
+    * 1 <= n <= 100
+    * 0 <= nums1[i], nums2[i] <= 100*/
+
+    public int countSubranges(int[] nums1, int[] nums2) {
+        final int mod = 1_000_000_007; 
+        long ans = 0; 
+        Map<Integer, Long> freq = new HashMap(); 
+        for (int i = 0; i < nums1.length; ++i) {
+            Map<Integer, Long> ff = new HashMap(); 
+            ff.merge(nums1[i], 1l, Long::sum); 
+            ff.merge(-nums2[i], 1l, Long::sum); 
+            for (var elem : freq.entrySet()) {
+                int k = elem.getKey(); 
+                long v = elem.getValue(); 
+                ff.put(k+nums1[i], (ff.getOrDefault(k+nums1[i], 0l) + v) % mod);
+                ff.put(k-nums2[i], (ff.getOrDefault(k-nums2[i], 0l) + v) % mod); 
+            }
+            freq = ff; 
+            ans = (ans + freq.getOrDefault(0, 0l)) % mod; 
+        }
+        return (int) ans; 
+    }
+
+
     /*2152. Minimum Number of Lines to Cover Points (Medium)
     You are given an array points where points[i] = [xi, yi] represents a point 
     on an X-Y plane. Straight lines are going to be added to the X-Y plane, 
