676f14d54fcd80ce1f60237906b48030a474756e
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-02-01 15:06:10 -0500

    solve'em all (1714)

--- leetcode.cpp
+++ leetcode.cpp
@@ -28384,6 +28384,60 @@ public:
     }
 
 
+    /*1714. Sum Of Special Evenly-Spaced Elements In Array (Hard)
+    You are given a 0-indexed integer array nums consisting of n non-negative 
+    integers. You are also given an array queries, where queries[i] = [xi, yi]. 
+    The answer to the ith query is the sum of all nums[j] where xi <= j < n 
+    and (j - xi) is divisible by yi. Return an array answer where 
+    answer.length == queries.length and answer[i] is the answer to the ith 
+    query modulo 10^9 + 7.
+
+    Example 1:
+    Input: nums = [0,1,2,3,4,5,6,7], queries = [[0,3],[5,1],[4,2]]
+    Output: [9,18,10]
+    Explanation: The answers of the queries are as follows:
+                 1) The j indices that satisfy this query are 0, 3, and 6. 
+                    nums[0] + nums[3] + nums[6] = 9
+                 2) The j indices that satisfy this query are 5, 6, and 7. 
+                    nums[5] + nums[6] + nums[7] = 18
+                 3) The j indices that satisfy this query are 4 and 6. 
+                    nums[4] + nums[6] = 10
+    
+    Example 2:
+    Input: nums = [100,200,101,201,102,202,103,203], queries = [[0,7]]
+    Output: [303]
+
+    Constraints:
+    * n == nums.length
+    * 1 <= n <= 5 * 10^4
+    * 0 <= nums[i] <= 10^9
+    * 1 <= queries.length <= 1.5 * 10^5
+    * 0 <= xi < n
+    * 1 <= yi <= 5 * 10^4*/
+
+    vector<int> solve(vector<int>& nums, vector<vector<int>>& queries) {
+        const int mod = 1'000'000'007; 
+        int n = nums.size(), r = sqrt(n); 
+        vector<vector<long>> dp(n, vector<long>(r)); 
+        for (int i = n-1; i >= 0; --i) 
+            for (int j = 0; j < r; ++j) {
+                dp[i][j] = nums[i]; 
+                if (i+j < n) dp[i][j] = (dp[i][j] + dp[i+j][j]) % mod; 
+            }
+        vector<int> ans; 
+        for (auto& q : queries) {
+            int x = q[0], y = q[1]; 
+            if (y < r) ans.push_back(dp[x][y]); 
+            else {
+                long val = 0; 
+                for (int j = x; j < n; j += y) val = (val + nums[j]) % mod; 
+                ans.push_back(val); 
+            }
+        }
+        return ans; 
+    }
+
+
     /*1735. Count Ways to Make Array With Product (Hard)
     You are given a 2D integer array, queries. For each queries[i], where 
     queries[i] = [ni, ki], find the number of different ways you can place 
