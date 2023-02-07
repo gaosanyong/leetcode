b5d02cf21b82a65af0360446aea352cb391644a0
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-02-06 20:45:04 -0500

    solve'em all (904)

--- leetcode.cpp
+++ leetcode.cpp
@@ -20219,6 +20219,58 @@ public:
     }
 
 
+    /*904. Fruit Into Baskets (Medium)
+    You are visiting a farm that has a single row of fruit trees arranged from 
+    left to right. The trees are represented by an integer array fruits where 
+    fruits[i] is the type of fruit the ith tree produces. You want to collect 
+    as much fruit as possible. However, the owner has some strict rules that 
+    you must follow:
+    * You only have two baskets, and each basket can only hold a single type of 
+      fruit. There is no limit on the amount of fruit each basket can hold.
+    * Starting from any tree of your choice, you must pick exactly one fruit 
+      from every tree (including the start tree) while moving to the right. The 
+      picked fruits must fit in one of your baskets.
+    * Once you reach a tree with fruit that cannot fit in your baskets, you 
+      must stop.
+    Given the integer array fruits, return the maximum number of fruits you can 
+    pick.
+
+    Example 1:
+    Input: fruits = [1,2,1]
+    Output: 3
+    Explanation: We can pick from all 3 trees.
+
+    Example 2:
+    Input: fruits = [0,1,2,2]
+    Output: 3
+    Explanation: We can pick from trees [1,2,2]. If we had started at the first 
+                 tree, we would only pick from trees [0,1].
+    
+    Example 3:
+    Input: fruits = [1,2,3,2,2]
+    Output: 4
+    Explanation: We can pick from trees [2,3,2,2]. If we had started at the 
+                 first tree, we would only pick from trees [1,2].
+
+    Constraints:
+    * 1 <= fruits.length <= 10^5
+    * 0 <= fruits[i] < fruits.length*/
+
+    int totalFruit(vector<int>& fruits) {
+        int ans = 0; 
+        unordered_map<int, int> freq; 
+        for (int i = 0, ii = 0; i < fruits.size(); ++i) {
+            ++freq[fruits[i]]; 
+            while (freq.size() > 2) {
+                if (--freq[fruits[ii]] == 0) freq.erase(fruits[ii]); 
+                ++ii; 
+            }
+            ans = max(ans, i - ii + 1); 
+        }
+        return ans; 
+    }
+
+
     /*905. Sort Array By Parity (Easy)
     Given an array A of non-negative integers, return an array consisting of 
     all the even elements of A, followed by all the odd elements of A. You may 
--- leetcode.java
+++ leetcode.java
@@ -2031,6 +2031,59 @@ class Solution {
     }
 
 
+    /*904. Fruit Into Baskets (Medium)
+    You are visiting a farm that has a single row of fruit trees arranged from 
+    left to right. The trees are represented by an integer array fruits where 
+    fruits[i] is the type of fruit the ith tree produces. You want to collect 
+    as much fruit as possible. However, the owner has some strict rules that 
+    you must follow:
+    * You only have two baskets, and each basket can only hold a single type of 
+      fruit. There is no limit on the amount of fruit each basket can hold.
+    * Starting from any tree of your choice, you must pick exactly one fruit 
+      from every tree (including the start tree) while moving to the right. The 
+      picked fruits must fit in one of your baskets.
+    * Once you reach a tree with fruit that cannot fit in your baskets, you 
+      must stop.
+    Given the integer array fruits, return the maximum number of fruits you can 
+    pick.
+
+    Example 1:
+    Input: fruits = [1,2,1]
+    Output: 3
+    Explanation: We can pick from all 3 trees.
+
+    Example 2:
+    Input: fruits = [0,1,2,2]
+    Output: 3
+    Explanation: We can pick from trees [1,2,2]. If we had started at the first 
+                 tree, we would only pick from trees [0,1].
+    
+    Example 3:
+    Input: fruits = [1,2,3,2,2]
+    Output: 4
+    Explanation: We can pick from trees [2,3,2,2]. If we had started at the 
+                 first tree, we would only pick from trees [1,2].
+
+    Constraints:
+    * 1 <= fruits.length <= 10^5
+    * 0 <= fruits[i] < fruits.length*/
+
+    public int totalFruit(int[] fruits) {
+        int ans = 0; 
+        Map<Integer, Integer> freq = new HashMap(); 
+        for (int i = 0, ii = 0; i < fruits.length; ++i) {
+            freq.merge(fruits[i], 1, Integer::sum); 
+            while (freq.size() > 2) {
+                freq.merge(fruits[ii], -1, Integer::sum); 
+                if (freq.get(fruits[ii]) == 0) freq.remove(fruits[ii]); 
+                ++ii; 
+            }
+            ans = Math.max(ans, i - ii + 1); 
+        }
+        return ans; 
+    }
+
+
     /*909. Snakes and Ladders (Medium)
     You are given an n x n integer matrix board where the cells are labeled 
     from 1 to n2 in a Boustrophedon style starting from the bottom left of the 
