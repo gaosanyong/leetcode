18d1b76c6ac920f62be31a6c56ec484b98e43528
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-02-06 20:32:02 -0500

    solve'em all (2254)

--- leetcode.cpp
+++ leetcode.cpp
@@ -57820,6 +57820,138 @@ public:
 };
 
 
+/*2254. Design Video Sharing Platform (Hard)
+You have a video sharing platform where users can upload and delete videos. 
+Each video is a string of digits, where the ith digit of the string represents 
+the content of the video at minute i. For example, the first digit represents 
+the content at minute 0 in the video, the second digit represents the content 
+at minute 1 in the video, and so on. Viewers of videos can also like and 
+dislike videos. Internally, the platform keeps track of the number of views, 
+likes, and dislikes on each video. When a video is uploaded, it is associated 
+with the smallest available integer videoId starting from 0. Once a video is 
+deleted, the videoId associated with that video can be reused for another 
+video. Implement the VideoSharingPlatform class:
+* VideoSharingPlatform() Initializes the object.
+* int upload(String video) The user uploads a video. Return the videoId 
+  associated with the video.
+* void remove(int videoId) If there is a video associated with videoId, remove 
+  the video.
+* String watch(int videoId, int startMinute, int endMinute) If there is a video 
+  associated with videoId, increase the number of views on the video by 1 and 
+  return the substring of the video string starting at startMinute and ending 
+  at min(endMinute, video.length - 1) (inclusive). Otherwise, return "-1".
+* void like(int videoId) Increases the number of likes on the video associated 
+  with videoId by 1 if there is a video associated with videoId.
+* void dislike(int videoId) Increases the number of dislikes on the video 
+  associated with videoId by 1 if there is a video associated with videoId.
+* int[] getLikesAndDislikes(int videoId) Return a 0-indexed integer array 
+  values of length 2 where values[0] is the number of likes and values[1] is 
+  the number of dislikes on the video associated with videoId. If there is no 
+  video associated with videoId, return [-1].
+* int getViews(int videoId) Return the number of views on the video associated 
+  with videoId, if there is no video associated with videoId, return -1.
+
+Example 1:
+Input: ["VideoSharingPlatform", "upload", "upload", "remove", "remove", "upload", "watch", "watch", "like", "dislike", "dislike", "getLikesAndDislikes", "getViews"]
+       [[], ["123"], ["456"], [4], [0], ["789"], [1, 0, 5], [1, 0, 1], [1], [1], [1], [1], [1]]
+Output: [null, 0, 1, null, null, 0, "456", "45", null, null, null, [1, 2], 2]
+Explanation
+VideoSharingPlatform videoSharingPlatform = new VideoSharingPlatform();
+videoSharingPlatform.upload("123");          // The smallest available videoId is 0, so return 0.
+videoSharingPlatform.upload("456");          // The smallest available videoId is 1, so return 1.
+videoSharingPlatform.remove(4);              // There is no video associated with videoId 4, so do nothing.
+videoSharingPlatform.remove(0);              // Remove the video associated with videoId 0.
+videoSharingPlatform.upload("789");          // Since the video associated with videoId 0 was deleted,
+                                             // 0 is the smallest available videoId, so return 0.
+videoSharingPlatform.watch(1, 0, 5);         // The video associated with videoId 1 is "456".
+                                             // The video from minute 0 to min(5, 3 - 1) = 2 is "456", so return "453".
+videoSharingPlatform.watch(1, 0, 1);         // The video associated with videoId 1 is "456".
+                                             // The video from minute 0 to min(1, 3 - 1) = 1 is "45", so return "45".
+videoSharingPlatform.like(1);                // Increase the number of likes on the video associated with videoId 1.
+videoSharingPlatform.dislike(1);             // Increase the number of dislikes on the video associated with videoId 1.
+videoSharingPlatform.dislike(1);             // Increase the number of dislikes on the video associated with videoId 1.
+videoSharingPlatform.getLikesAndDislikes(1); // There is 1 like and 2 dislikes on the video associated with videoId 1, so return [1, 2].
+videoSharingPlatform.getViews(1);            // The video associated with videoId 1 has 2 views, so return 2.
+
+Example 2:
+Input: ["VideoSharingPlatform", "remove", "watch", "like", "dislike", "getLikesAndDislikes", "getViews"]
+       [[], [0], [0, 0, 1], [0], [0], [0], [0]]
+Output: [null, null, "-1", null, null, [-1], -1]
+Explanation
+VideoSharingPlatform videoSharingPlatform = new VideoSharingPlatform();
+videoSharingPlatform.remove(0);              // There is no video associated with videoId 0, so do nothing.
+videoSharingPlatform.watch(0, 0, 1);         // There is no video associated with videoId 0, so return "-1".
+videoSharingPlatform.like(0);                // There is no video associated with videoId 0, so do nothing.
+videoSharingPlatform.dislike(0);             // There is no video associated with videoId 0, so do nothing.
+videoSharingPlatform.getLikesAndDislikes(0); // There is no video associated with videoId 0, so return [-1].
+videoSharingPlatform.getViews(0);            // There is no video associated with videoId 0, so return -1.
+
+Constraints:
+* 1 <= video.length <= 10^5
+* The sum of video.length over all calls to upload does not exceed 10^5
+* video consists of digits.
+* 0 <= videoId <= 10^5
+* 0 <= startMinute < endMinute < 10^5
+* startMinute < video.length
+* The sum of endMinute - startMinute over all calls to watch does not exceed 10^5.
+* At most 10^5 calls in total will be made to all functions.*/
+
+class VideoSharingPlatform {
+    priority_queue<int, vector<int>, greater<>> pq; 
+    vector<tuple<string, int, int, int>> videos; 
+    
+public:
+    VideoSharingPlatform() {}
+    
+    int upload(string video) {
+        int videoId = videos.size(); 
+        if (pq.size()) {
+            videoId = pq.top(); pq.pop(); 
+            videos[videoId] = make_tuple(video, 0, 0, 0); 
+        } else 
+            videos.emplace_back(video, 0, 0, 0); 
+        return videoId; 
+    }
+    
+    void remove(int videoId) {
+        if (videoId < videos.size() && get<0>(videos[videoId]).size()) {
+            pq.push(videoId); 
+            videos[videoId] = make_tuple("", 0, 0, 0); 
+        }
+    }
+    
+    string watch(int videoId, int startMinute, int endMinute) {
+        if (videoId < videos.size() && get<0>(videos[videoId]).size()) {
+            ++get<1>(videos[videoId]); 
+            return get<0>(videos[videoId]).substr(startMinute, endMinute - startMinute + 1); 
+        }
+        return "-1"; 
+    }
+    
+    void like(int videoId) {
+        if (videoId < videos.size() && get<0>(videos[videoId]).size())
+            ++get<2>(videos[videoId]); 
+    }
+    
+    void dislike(int videoId) {
+        if (videoId < videos.size() && get<0>(videos[videoId]).size())
+            ++get<3>(videos[videoId]); 
+    }
+    
+    vector<int> getLikesAndDislikes(int videoId) {
+        if (videoId < videos.size() && get<0>(videos[videoId]).size())
+            return {get<2>(videos[videoId]), get<3>(videos[videoId])}; 
+        return {-1}; 
+    }
+    
+    int getViews(int videoId) {
+        if (videoId < videos.size() && get<0>(videos[videoId]).size())
+            return get<1>(videos[videoId]); 
+        return -1; 
+    }
+};
+
+
 /*2408. Design SQL (Medium)
 You are given n tables represented with two arrays names and columns, where 
 names[i] is the name of the ith table and columns[i] is the number of columns 
--- leetcode.java
+++ leetcode.java
@@ -7867,6 +7867,146 @@ class NumMatrix {
 }
 
 
+/*2254. Design Video Sharing Platform (Hard)
+You have a video sharing platform where users can upload and delete videos. 
+Each video is a string of digits, where the ith digit of the string represents 
+the content of the video at minute i. For example, the first digit represents 
+the content at minute 0 in the video, the second digit represents the content 
+at minute 1 in the video, and so on. Viewers of videos can also like and 
+dislike videos. Internally, the platform keeps track of the number of views, 
+likes, and dislikes on each video. When a video is uploaded, it is associated 
+with the smallest available integer videoId starting from 0. Once a video is 
+deleted, the videoId associated with that video can be reused for another 
+video. Implement the VideoSharingPlatform class:
+* VideoSharingPlatform() Initializes the object.
+* int upload(String video) The user uploads a video. Return the videoId 
+  associated with the video.
+* void remove(int videoId) If there is a video associated with videoId, remove 
+  the video.
+* String watch(int videoId, int startMinute, int endMinute) If there is a video 
+  associated with videoId, increase the number of views on the video by 1 and 
+  return the substring of the video string starting at startMinute and ending 
+  at min(endMinute, video.length - 1) (inclusive). Otherwise, return "-1".
+* void like(int videoId) Increases the number of likes on the video associated 
+  with videoId by 1 if there is a video associated with videoId.
+* void dislike(int videoId) Increases the number of dislikes on the video 
+  associated with videoId by 1 if there is a video associated with videoId.
+* int[] getLikesAndDislikes(int videoId) Return a 0-indexed integer array 
+  values of length 2 where values[0] is the number of likes and values[1] is 
+  the number of dislikes on the video associated with videoId. If there is no 
+  video associated with videoId, return [-1].
+* int getViews(int videoId) Return the number of views on the video associated 
+  with videoId, if there is no video associated with videoId, return -1.
+
+Example 1:
+Input: ["VideoSharingPlatform", "upload", "upload", "remove", "remove", "upload", "watch", "watch", "like", "dislike", "dislike", "getLikesAndDislikes", "getViews"]
+       [[], ["123"], ["456"], [4], [0], ["789"], [1, 0, 5], [1, 0, 1], [1], [1], [1], [1], [1]]
+Output: [null, 0, 1, null, null, 0, "456", "45", null, null, null, [1, 2], 2]
+Explanation
+VideoSharingPlatform videoSharingPlatform = new VideoSharingPlatform();
+videoSharingPlatform.upload("123");          // The smallest available videoId is 0, so return 0.
+videoSharingPlatform.upload("456");          // The smallest available videoId is 1, so return 1.
+videoSharingPlatform.remove(4);              // There is no video associated with videoId 4, so do nothing.
+videoSharingPlatform.remove(0);              // Remove the video associated with videoId 0.
+videoSharingPlatform.upload("789");          // Since the video associated with videoId 0 was deleted,
+                                             // 0 is the smallest available videoId, so return 0.
+videoSharingPlatform.watch(1, 0, 5);         // The video associated with videoId 1 is "456".
+                                             // The video from minute 0 to min(5, 3 - 1) = 2 is "456", so return "453".
+videoSharingPlatform.watch(1, 0, 1);         // The video associated with videoId 1 is "456".
+                                             // The video from minute 0 to min(1, 3 - 1) = 1 is "45", so return "45".
+videoSharingPlatform.like(1);                // Increase the number of likes on the video associated with videoId 1.
+videoSharingPlatform.dislike(1);             // Increase the number of dislikes on the video associated with videoId 1.
+videoSharingPlatform.dislike(1);             // Increase the number of dislikes on the video associated with videoId 1.
+videoSharingPlatform.getLikesAndDislikes(1); // There is 1 like and 2 dislikes on the video associated with videoId 1, so return [1, 2].
+videoSharingPlatform.getViews(1);            // The video associated with videoId 1 has 2 views, so return 2.
+
+Example 2:
+Input: ["VideoSharingPlatform", "remove", "watch", "like", "dislike", "getLikesAndDislikes", "getViews"]
+       [[], [0], [0, 0, 1], [0], [0], [0], [0]]
+Output: [null, null, "-1", null, null, [-1], -1]
+Explanation
+VideoSharingPlatform videoSharingPlatform = new VideoSharingPlatform();
+videoSharingPlatform.remove(0);              // There is no video associated with videoId 0, so do nothing.
+videoSharingPlatform.watch(0, 0, 1);         // There is no video associated with videoId 0, so return "-1".
+videoSharingPlatform.like(0);                // There is no video associated with videoId 0, so do nothing.
+videoSharingPlatform.dislike(0);             // There is no video associated with videoId 0, so do nothing.
+videoSharingPlatform.getLikesAndDislikes(0); // There is no video associated with videoId 0, so return [-1].
+videoSharingPlatform.getViews(0);            // There is no video associated with videoId 0, so return -1.
+
+Constraints:
+* 1 <= video.length <= 10^5
+* The sum of video.length over all calls to upload does not exceed 10^5
+* video consists of digits.
+* 0 <= videoId <= 10^5
+* 0 <= startMinute < endMinute < 10^5
+* startMinute < video.length
+* The sum of endMinute - startMinute over all calls to watch does not exceed 10^5.
+* At most 10^5 calls in total will be made to all functions.*/
+
+class VideoSharingPlatform {
+    private Queue<Integer> pq; 
+    private List<int[]> stats; 
+    private List<String> videos; 
+    
+    public VideoSharingPlatform() {
+        pq = new PriorityQueue(); 
+        stats = new ArrayList(); 
+        videos = new ArrayList(); 
+    }
+    
+    public int upload(String video) {
+        int videoId = videos.size(); 
+        if (pq.size() > 0) {
+            videoId = pq.poll(); 
+            videos.set(videoId, video); 
+        } else {
+            stats.add(new int[]{0, 0, 0}); 
+            videos.add(video); 
+        }
+        return videoId; 
+    }
+    
+    public void remove(int videoId) {
+        if (videoId < videos.size() && !videos.get(videoId).isEmpty()) {
+            pq.add(videoId); 
+            stats.set(videoId, new int[]{0, 0, 0}); 
+            videos.set(videoId, ""); 
+        }
+    }
+    
+    public String watch(int videoId, int startMinute, int endMinute) {
+        if (videoId < videos.size() && !videos.get(videoId).isEmpty()) {
+            ++stats.get(videoId)[0]; 
+            String video = videos.get(videoId); 
+            return video.substring(startMinute, Math.min(video.length(), endMinute+1)); 
+        }
+        return "-1"; 
+    }
+    
+    public void like(int videoId) {
+        if (videoId < videos.size() && !videos.get(videoId).isEmpty()) 
+            ++stats.get(videoId)[1]; 
+    }
+    
+    public void dislike(int videoId) {
+        if (videoId < videos.size() && !videos.get(videoId).isEmpty()) 
+            ++stats.get(videoId)[2]; 
+    }
+    
+    public int[] getLikesAndDislikes(int videoId) {
+        if (videoId < videos.size() && !videos.get(videoId).isEmpty()) 
+            return new int[]{stats.get(videoId)[1], stats.get(videoId)[2]}; 
+        return new int[]{-1}; 
+    }
+    
+    public int getViews(int videoId) {
+        if (videoId < videos.size() && !videos.get(videoId).isEmpty()) 
+            return stats.get(videoId)[0]; 
+        return -1; 
+    }
+}
+
+
 /*2502. Design Memory Allocator (Medium)
 You are given an integer n representing the size of a 0-indexed memory array. 
 All memory units are initially free. You have a memory allocator with the 
