ae892ccda4ec420249c607ce2c9f0eef62a94656
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-02-07 19:21:04 -0500

    solve'em all (280)

--- leetcode.cpp
+++ leetcode.cpp
@@ -7055,6 +7055,34 @@ public:
     }
 
 
+    /*280. Wiggle Sort (Medium)
+    Given an integer array nums, reorder it such that 
+    nums[0] <= nums[1] >= nums[2] <= nums[3].... You may assume the input array 
+    always has a valid answer.
+
+    Example 1:
+    Input: nums = [3,5,2,1,6,4]
+    Output: [3,5,1,6,2,4]
+    Explanation: [1,6,2,5,3,4] is also accepted.
+
+    Example 2:
+    Input: nums = [6,6,5,6,3,8]
+    Output: [6,6,5,6,3,8]
+
+    Constraints:
+    * 1 <= nums.length <= 5 * 10^4
+    * 0 <= nums[i] <= 10^4
+    * It is guaranteed that there will be an answer for the given input nums.
+
+    Follow up: Could you solve the problem in O(n) time complexity?*/
+
+    void wiggleSort(vector<int>& nums) {
+        for (int i = 0; i < nums.size()-1; ++i) 
+            if (i&1 && nums[i] < nums[i+1] || !(i&1) && nums[i] > nums[i+1]) 
+                swap(nums[i], nums[i+1]); 
+    }
+
+
     /*282. Expression Add Operators (Hard)
     Given a string num that contains only digits and an integer target, return 
     all possibilities to add the binary operators '+', '-', or '*' between the 
--- leetcode.py
+++ leetcode.py
@@ -8336,7 +8336,7 @@ class Solution:
 
     def wiggleSort(self, nums: List[int]) -> None:
         for i in range(len(nums)-1):
-            if i & 1 and nums[i] < nums[i+1] or not i&1 and nums[i] > nums[i+1]: 
+            if i&1 and nums[i] < nums[i+1] or not i&1 and nums[i] > nums[i+1]: 
                 nums[i], nums[i+1] = nums[i+1], nums[i]
 
 
