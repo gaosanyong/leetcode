3c14ab6ad56e12d4317ca3c56094972d09bc5938
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-02-12 21:02:39 -0500

    complete 1523

--- leetcode.cpp
+++ leetcode.cpp
@@ -27541,6 +27541,27 @@ public:
     }
 
 
+    /*1523. Count Odd Numbers in an Interval Range (Easy)
+    Given two non-negative integers low and high. Return the count of odd 
+    numbers between low and high (inclusive).
+
+    Example 1:
+    Input: low = 3, high = 7
+    Output: 3
+    Explanation: The odd numbers between 3 and 7 are [3,5,7].
+
+    Example 2:
+    Input: low = 8, high = 10
+    Output: 1
+    Explanation: The odd numbers between 8 and 10 are [9].
+
+    Constraints: 0 <= low <= high <= 10^9*/
+
+    int countOdds(int low, int high) {
+        return (high+1)/2 - low/2; 
+    }
+
+
     /*1533. Find the Index of the Large Integer (Medium)
     We have an integer array arr, where all the integers in arr are equal 
     except for one integer which is larger than the rest of the integers. You 
--- leetcode.java
+++ leetcode.java
@@ -2924,6 +2924,27 @@ class Solution {
     }
 
 
+    /*1523. Count Odd Numbers in an Interval Range (Easy)
+    Given two non-negative integers low and high. Return the count of odd 
+    numbers between low and high (inclusive).
+
+    Example 1:
+    Input: low = 3, high = 7
+    Output: 3
+    Explanation: The odd numbers between 3 and 7 are [3,5,7].
+
+    Example 2:
+    Input: low = 8, high = 10
+    Output: 1
+    Explanation: The odd numbers between 8 and 10 are [9].
+
+    Constraints: 0 <= low <= high <= 10^9*/
+
+    public int countOdds(int low, int high) {
+        return (high+1)/2 - low/2; 
+    }
+
+
     /*1714. Sum Of Special Evenly-Spaced Elements In Array (Hard)
     You are given a 0-indexed integer array nums consisting of n non-negative 
     integers. You are also given an array queries, where queries[i] = [xi, yi]. 
