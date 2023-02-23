d751133b0ff72ac9271307132af5d18ad18c7d39
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-02-22 21:50:24 -0500

    java 502

--- leetcode.java
+++ leetcode.java
@@ -1583,6 +1583,61 @@ class Solution {
     }
 
 
+    /*502. IPO (Hard)
+    Suppose LeetCode will start its IPO soon. In order to sell a good price of 
+    its shares to Venture Capital, LeetCode would like to work on some projects 
+    to increase its capital before the IPO. Since it has limited resources, it 
+    can only finish at most k distinct projects before the IPO. Help LeetCode 
+    design the best way to maximize its total capital after finishing at most k 
+    distinct projects. You are given n projects where the ith project has a 
+    pure profit profits[i] and a minimum capital of capital[i] is needed to 
+    start it. Initially, you have w capital. When you finish a project, you 
+    will obtain its pure profit and the profit will be added to your total 
+    capital. Pick a list of at most k distinct projects from given projects to 
+    maximize your final capital, and return the final maximized capital. The 
+    answer is guaranteed to fit in a 32-bit signed integer.
+
+    Example 1:
+    Input: k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
+    Output: 4
+    Explanation: Since your initial capital is 0, you can only start the 
+                 project indexed 0. After finishing it you will obtain profit 1 
+                 and your capital becomes 1. With capital 1, you can either 
+                 start the project indexed 1 or the project indexed 2. Since 
+                 you can choose at most 2 projects, you need to finish the 
+                 project indexed 2 to get the maximum capital. Therefore, 
+                 output the final maximized capital, which is 0 + 1 + 3 = 4.
+    
+    Example 2:
+    Input: k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]
+    Output: 6
+
+    Constraints:
+    * 1 <= k <= 10^5
+    * 0 <= w <= 10^9
+    * n == profits.length
+    * n == capital.length
+    * 1 <= n <= 10^5
+    * 0 <= profits[i] <= 10^4
+    * 0 <= capital[i] <= 10^9*/
+
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length; 
+        int[][] aug = new int[n][2]; 
+        for (int i = 0; i < n; ++i) {
+            aug[i][0] = capital[i]; 
+            aug[i][1] = profits[i]; 
+        }
+        Arrays.sort(aug, (a, b) -> Integer.compare(a[0], b[0])); 
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(b, a)); 
+        for (int i = 0; k > 0; --k) {
+            for (; i < n && aug[i][0] <= w; ++i) pq.add(aug[i][1]); 
+            if (!pq.isEmpty()) w += pq.poll(); 
+        }
+        return w; 
+    }
+
+
     /*540. Single Element in a Sorted Array (Medium)
     You are given a sorted array consisting of only integers where every 
     element appears exactly twice, except for one element which appears exactly 
