58125a88d0e55682f7b60080d42a028b6d2e7dc6
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-02-23 20:56:44 -0500

    java 1675

--- leetcode.java
+++ leetcode.java
@@ -3244,6 +3244,59 @@ class Solution {
     }
 
 
+    /*1675. Minimize Deviation in Array (Hard)
+    You are given an array nums of n positive integers. You can perform two 
+    types of operations on any element of the array any number of times:
+    * If the element is even, divide it by 2.
+      + For example, if the array is [1,2,3,4], then you can do this operation 
+        on the last element, and the array will be [1,2,3,2].
+    * If the element is odd, multiply it by 2.
+      + For example, if the array is [1,2,3,4], then you can do this operation 
+        on the first element, and the array will be [2,2,3,4].
+    The deviation of the array is the maximum difference between any two 
+    elements in the array. Return the minimum deviation the array can have 
+    after performing some number of operations.
+
+    Example 1:
+    Input: nums = [1,2,3,4]
+    Output: 1
+    Explanation: You can transform the array to [1,2,3,2], then to [2,2,3,2], 
+                 then the deviation will be 3 - 2 = 1.
+    
+    Example 2:
+    Input: nums = [4,1,5,20,3]
+    Output: 3
+    Explanation: You can transform the array after two operations to 
+                 [4,2,5,5,3], then the deviation will be 5 - 2 = 3.
+    
+    Example 3:
+    Input: nums = [2,10,8]
+    Output: 3
+
+    Constraints:
+    * n == nums.length
+    * 2 <= n <= 5 * 10^4
+    * 1 <= nums[i] <= 10^9*/
+
+    public int minimumDeviation(int[] nums) {
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(b, a)); 
+        int lo = Integer.MAX_VALUE; 
+        for (var x : nums) {
+            if (x % 2 == 1) x *= 2; 
+            pq.add(x); 
+            lo = Math.min(lo, x); 
+        }
+        int ans = pq.peek() - lo; 
+        while (pq.peek() % 2 == 0) {
+            int val = pq.poll() / 2; 
+            pq.add(val); 
+            lo = Math.min(lo, val); 
+            ans = Math.min(ans, pq.peek() - lo); 
+        }
+        return ans; 
+    }
+
+
     /*1697. Checking Existence of Edge Length Limited Paths (Hard)
     An undirected graph of n nodes is defined by edgeList, where 
     edgeList[i] = [ui, vi, disi] denotes an edge between nodes ui and vi with 
