9ad0e02c1e52edb34065d7efa518847200f648a0
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-02-25 19:21:55 -0500

    java 72

--- leetcode.java
+++ leetcode.java
@@ -609,6 +609,47 @@ class Solution {
     }
 
 
+    /*72. Edit Distance (Hard)
+    Given two strings word1 and word2, return the minimum number of operations 
+    required to convert word1 to word2. You have the following three operations 
+    permitted on a word:
+    * Insert a character
+    * Delete a character
+    * Replace a character
+
+    Example 1:
+    Input: word1 = "horse", word2 = "ros"
+    Output: 3
+    Explanation: horse -> rorse (replace 'h' with 'r')
+                 rorse -> rose (remove 'r')
+                 rose -> ros (remove 'e')
+    
+    Example 2:
+    Input: word1 = "intention", word2 = "execution"
+    Output: 5
+    Explanation: intention -> inention (remove 't')
+                 inention -> enention (replace 'i' with 'e')
+                 enention -> exention (replace 'n' with 'x')
+                 exention -> exection (replace 'n' with 'c')
+                 exection -> execution (insert 'u')
+
+    Constraints:
+    * 0 <= word1.length, word2.length <= 500
+    * word1 and word2 consist of lowercase English letters.*/
+
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m+1][n+1]; 
+        for (int i = 0; i < m; ++i) dp[i][n] = m-i; 
+        for (int j = 0; j < n; ++j) dp[m][j] = n-j; 
+        for (int i  = m-1; i >= 0; --i) 
+            for (int j = n-1; j >= 0; --j)
+                if (word1.charAt(i) == word2.charAt(j)) dp[i][j] = dp[i+1][j+1]; 
+                else dp[i][j] = 1 + Math.min(dp[i+1][j], Math.min(dp[i+1][j+1], dp[i][j+1])); 
+        return dp[0][0]; 
+    }
+
+
     /*83. Remove Duplicates from Sorted List (Easy)
     Given the head of a sorted linked list, delete all duplicates such that 
     each element appears only once. Return the linked list sorted as well.
