135a50a676decc7909be1383955930a2aa0089eb
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-02-27 10:54:35 -0500

    solve'em all (427)

--- leetcode.java
+++ leetcode.java
@@ -1602,6 +1602,85 @@ class Solution {
     }
 
 
+    /*427. Construct Quad Tree (Medium)
+    Given a n * n matrix grid of 0's and 1's only. We want to represent the 
+    grid with a Quad-Tree. Return the root of the Quad-Tree representing the 
+    grid. Notice that you can assign the value of a node to True or False when 
+    isLeaf is False, and both are accepted in the answer. A Quad-Tree is a tree 
+    data structure in which each internal node has exactly four children. 
+    Besides, each node has two attributes:
+    * val: True if the node represents a grid of 1's or False if the node 
+      represents a grid of 0's.
+    * isLeaf: True if the node is leaf node on the tree or False if the node 
+      has the four children.
+    class Node {
+        public boolean val;
+        public boolean isLeaf;
+        public Node topLeft;
+        public Node topRight;
+        public Node bottomLeft;
+        public Node bottomRight;
+    }
+    We can construct a Quad-Tree from a two-dimensional area using the 
+    following steps:
+    * If the current grid has the same value (i.e all 1's or all 0's) set 
+      isLeaf True and set val to the value of the grid and set the four 
+      children to Null and stop.
+    * If the current grid has different values, set isLeaf to False and set val 
+      to any value and divide the current grid into four sub-grids as shown in 
+      the photo.
+    * Recurse for each of the children with the proper sub-grid.
+    If you want to know more about the Quad-Tree, you can refer to the wiki. 
+    Quad-Tree format:
+    * The output represents the serialized format of a Quad-Tree using level 
+      order traversal, where null signifies a path terminator where no node 
+      exists below.
+    * It is very similar to the serialization of the binary tree. The only 
+      difference is that the node is represented as a list [isLeaf, val].
+    * If the value of isLeaf or val is True we represent it as 1 in the list 
+      [isLeaf, val] and if the value of isLeaf or val is False we represent it 
+      as 0.
+
+    Example 1:
+    Input: grid = [[0,1],[1,0]]
+    Output: [[0,1],[1,0],[1,1],[1,1],[1,0]]
+    Explanation: The explanation of this example is shown below: Notice that 0 
+                 represnts False and 1 represents True in the photo 
+                 representing the Quad-Tree.
+
+    Example 2:
+    Input: grid = [[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0]]
+    Output: [[0,1],[1,1],[0,1],[1,1],[1,0],null,null,null,null,[1,0],[1,0],[1,1],[1,1]]
+    Explanation: All values in the grid are not the same. We divide the grid 
+                 into four sub-grids. The topLeft, bottomLeft and bottomRight 
+                 each has the same value. The topRight have different values so 
+                 we divide it into 4 sub-grids where each has the same value. 
+                 Explanation is shown in the photo below:
+
+    Constraints:
+    * n == grid.length == grid[i].length
+    * n == 2x where 0 <= x <= 6*/
+
+    private Node fn(int i, int ii, int j, int jj, int[][] prefix, int[][] grid) {
+        int diff = prefix[ii][jj] - prefix[i][jj] - prefix[ii][j] + prefix[i][j]; 
+        if (diff == 0 || diff == (ii-i)*(jj-j)) return new Node(grid[i][j] == 1 ? true : false, true); 
+        Node tl = fn(i, (i+ii)/2, j, (j+jj)/2, prefix, grid); 
+        Node tr = fn(i, (i+ii)/2, (j+jj)/2, jj, prefix, grid); 
+        Node bl = fn((i+ii)/2, ii, j, (j+jj)/2, prefix, grid); 
+        Node br = fn((i+ii)/2, ii, (j+jj)/2, jj, prefix, grid); 
+        return new Node(false, false, tl, tr, bl, br); 
+    }
+    
+    public Node construct(int[][] grid) {
+        int n = grid.length; 
+        int[][] prefix = new int[n+1][n+1]; 
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                prefix[i+1][j+1] = prefix[i][j+1] + prefix[i+1][j] - prefix[i][j] + grid[i][j]; 
+        return fn(0, n, 0, n, prefix, grid); 
+    }
+
+
     /*452. Minimum Number of Arrows to Burst Balloons (Medium)
     There are some spherical balloons taped onto a flat wall that represents 
     the XY-plane. The balloons are represented as a 2D integer array points 
@@ -1642,7 +1721,7 @@ class Solution {
     Constraints:
     * 1 <= points.length <= 10^5
     * points[i].length == 2
-    * -231 <= xstart < xend <= 2^31 - 1*/
+    * -2^31 <= xstart < xend <= 2^31 - 1*/
 
     public int findMinArrowShots(int[][] points) {
         int ans = 0; 
--- leetcode.py
+++ leetcode.py
@@ -11897,6 +11897,85 @@ class Solution:
         return head 
 
 
+    """427. Construct Quad Tree (Medium)
+    Given a n * n matrix grid of 0's and 1's only. We want to represent the 
+    grid with a Quad-Tree. Return the root of the Quad-Tree representing the 
+    grid. Notice that you can assign the value of a node to True or False when 
+    isLeaf is False, and both are accepted in the answer. A Quad-Tree is a tree 
+    data structure in which each internal node has exactly four children. 
+    Besides, each node has two attributes:
+    * val: True if the node represents a grid of 1's or False if the node 
+      represents a grid of 0's.
+    * isLeaf: True if the node is leaf node on the tree or False if the node 
+      has the four children.
+    class Node {
+        public boolean val;
+        public boolean isLeaf;
+        public Node topLeft;
+        public Node topRight;
+        public Node bottomLeft;
+        public Node bottomRight;
+    }
+    We can construct a Quad-Tree from a two-dimensional area using the 
+    following steps:
+    * If the current grid has the same value (i.e all 1's or all 0's) set 
+      isLeaf True and set val to the value of the grid and set the four 
+      children to Null and stop.
+    * If the current grid has different values, set isLeaf to False and set val 
+      to any value and divide the current grid into four sub-grids as shown in 
+      the photo.
+    * Recurse for each of the children with the proper sub-grid.
+    If you want to know more about the Quad-Tree, you can refer to the wiki. 
+    Quad-Tree format:
+    * The output represents the serialized format of a Quad-Tree using level 
+      order traversal, where null signifies a path terminator where no node 
+      exists below.
+    * It is very similar to the serialization of the binary tree. The only 
+      difference is that the node is represented as a list [isLeaf, val].
+    * If the value of isLeaf or val is True we represent it as 1 in the list 
+      [isLeaf, val] and if the value of isLeaf or val is False we represent it 
+      as 0.
+
+    Example 1:
+    Input: grid = [[0,1],[1,0]]
+    Output: [[0,1],[1,0],[1,1],[1,1],[1,0]]
+    Explanation: The explanation of this example is shown below: Notice that 0 
+                 represnts False and 1 represents True in the photo 
+                 representing the Quad-Tree.
+
+    Example 2:
+    Input: grid = [[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0]]
+    Output: [[0,1],[1,1],[0,1],[1,1],[1,0],null,null,null,null,[1,0],[1,0],[1,1],[1,1]]
+    Explanation: All values in the grid are not the same. We divide the grid 
+                 into four sub-grids. The topLeft, bottomLeft and bottomRight 
+                 each has the same value. The topRight have different values so 
+                 we divide it into 4 sub-grids where each has the same value. 
+                 Explanation is shown in the photo below:
+
+    Constraints:
+    * n == grid.length == grid[i].length
+    * n == 2x where 0 <= x <= 6"""
+
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        n = len(grid)
+        prefix = [[0]*(n+1) for _ in range(n+1)]
+        for i in range(n): 
+            for j in range(n): 
+                prefix[i+1][j+1] = prefix[i][j+1] + prefix[i+1][j] - prefix[i][j] + grid[i][j]
+        
+        def fn(i, ii, j, jj): 
+            """Return QuadTree subtree."""
+            diff = prefix[ii][jj] - prefix[i][jj] - prefix[ii][j] + prefix[i][j]
+            if diff in (0, (ii-i)*(jj-j)): return Node(grid[i][j], True)
+            tl = fn(i, (i+ii)//2, j, (j+jj)//2)
+            tr = fn(i, (i+ii)//2, (j+jj)//2, jj)
+            bl = fn((i+ii)//2, ii, j, (j+jj)//2)
+            br = fn((i+ii)//2, ii, (j+jj)//2, jj)
+            return Node(False, False, tl, tr, bl, br)
+        
+        return fn(0, n, 0, n)
+
+
     """429. N-ary Tree Level Order Traversal (Medium)
     Given an n-ary tree, return the level order traversal of its nodes' values. 
     Nary-Tree input serialization is represented in their level order traversal, 
