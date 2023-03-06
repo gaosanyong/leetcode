ad701291cb5075073fb3aef45ef746021d85c9fa
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-03-05 19:46:33 -0500

    solve'em all (1539)

--- leetcode.cpp
+++ leetcode.cpp
@@ -28094,6 +28094,42 @@ public:
     }
 
 
+    /*1539. Kth Missing Positive Number (Easy)
+    Given an array arr of positive integers sorted in a strictly increasing 
+    order, and an integer k. Return the kth positive integer that is missing 
+    from this array.
+
+    Example 1:
+    Input: arr = [2,3,4,7,11], k = 5
+    Output: 9
+    Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. 
+                 The 5th missing positive integer is 9.
+    
+    Example 2:
+    Input: arr = [1,2,3,4], k = 2
+    Output: 6
+    Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing 
+                 positive integer is 6.
+
+    Constraints:
+    * 1 <= arr.length <= 1000
+    * 1 <= arr[i] <= 1000
+    * 1 <= k <= 1000
+    * arr[i] < arr[j] for 1 <= i < j <= arr.length
+
+    Follow up: Could you solve this problem in less than O(n) complexity?*/
+
+    int findKthPositive(vector<int>& arr, int k) {
+        int lo = 0, hi = arr.size(); 
+        while (lo < hi) {
+            int mid = lo + (hi-lo)/2; 
+            if (arr[mid] - (mid+1) < k) lo = mid+1; 
+            else hi = mid; 
+        }
+        return lo+k; 
+    }
+
+
     /*1548. The Most Similar Path in a Graph (Hard)
     We have n cities and m bi-directional roads where roads[i] = [ai, bi] 
     connects city ai with city bi. Each city has a name consisting of exactly 
--- leetcode.py
+++ leetcode.py
@@ -36761,6 +36761,40 @@ class UnionFind:
         return idx[0] if freq[0] > freq[1] else (idx[1] if freq[0] < freq[1] else -1)
 
 
+    """1539. Kth Missing Positive Number (Easy)
+    Given an array arr of positive integers sorted in a strictly increasing 
+    order, and an integer k. Return the kth positive integer that is missing 
+    from this array.
+
+    Example 1:
+    Input: arr = [2,3,4,7,11], k = 5
+    Output: 9
+    Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. 
+                 The 5th missing positive integer is 9.
+    
+    Example 2:
+    Input: arr = [1,2,3,4], k = 2
+    Output: 6
+    Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing 
+                 positive integer is 6.
+
+    Constraints:
+    * 1 <= arr.length <= 1000
+    * 1 <= arr[i] <= 1000
+    * 1 <= k <= 1000
+    * arr[i] < arr[j] for 1 <= i < j <= arr.length
+
+    Follow up: Could you solve this problem in less than O(n) complexity?"""
+
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        lo, hi = 0, len(arr)
+        while lo < hi:
+            mid = (lo + hi)//2
+            if arr[mid] - (mid + 1) < k: lo = mid + 1
+            else: hi = mid
+        return lo + k
+
+
     """1548. The Most Similar Path in a Graph (Hard)
     We have n cities and m bi-directional roads where roads[i] = [ai, bi] 
     connects city ai with city bi. Each city has a name consisting of exactly 
