f7fddab8d0c6573a168b87816f951a0cf51db42a
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-03-08 18:59:00 -0500

    java 1055

--- leetcode.java
+++ leetcode.java
@@ -2849,6 +2849,57 @@ class Solution {
     }
 
 
+    /*1055. Shortest Way to Form String (Medium)
+    A subsequence of a string is a new string that is formed from the original 
+    string by deleting some (can be none) of the characters without disturbing 
+    the relative positions of the remaining characters. (i.e., "ace" is a 
+    subsequence of "abcde" while "aec" is not). Given two strings source and 
+    target, return the minimum number of subsequences of source such that their 
+    concatenation equals target. If the task is impossible, return -1.
+
+    Example 1:
+    Input: source = "abc", target = "abcbc"
+    Output: 2
+    Explanation: The target "abcbc" can be formed by "abc" and "bc", which are 
+                 subsequences of source "abc".
+    
+    Example 2:
+    Input: source = "abc", target = "acdbc"
+    Output: -1
+    Explanation: The target string cannot be constructed from the subsequences 
+                 of source string due to the character "d" in target string.
+    
+    Example 3:
+    Input: source = "xyz", target = "xzyxz"
+    Output: 3
+    Explanation: The target string can be constructed as follows 
+                 "xz" + "y" + "xz".
+
+    Constraints:
+    * 1 <= source.length, target.length <= 1000
+    * source and target consist of lowercase English letters.*/
+
+    public int shortestWay(String source, String target) {
+        Map<Character, List<Integer>> locs = new HashMap(); 
+        for (int i = 0; i < source.length(); ++i) {
+            locs.putIfAbsent(source.charAt(i), new ArrayList()); 
+            locs.get(source.charAt(i)).add(i); 
+        }
+        int ans = 0, i = 0; 
+        for (var x : target.toCharArray()) {
+            if (!locs.containsKey(x)) return -1; 
+            int k = Collections.binarySearch(locs.get(x), i); 
+            if (k < 0) k = -k-1; 
+            if (k == locs.get(x).size()) {
+                k = 0; 
+                ++ans; 
+            }
+            i = locs.get(x).get(k) + 1; 
+        }
+        return ++ans; 
+    }
+
+
     /*1063. Number of Valid Subarrays (Hard)
     Given an integer array nums, return the number of non-empty subarrays with 
     the leftmost element of the subarray not larger than other elements in the 
