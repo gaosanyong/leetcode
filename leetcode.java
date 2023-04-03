a236e2e39031e78b0fe1c55d1cfe182d9d7458e6
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-04-02 22:26:14 -0400

    java 881

--- leetcode.java
+++ leetcode.java
@@ -2500,6 +2500,44 @@ class Solution {
     }
 
 
+    /*881. Boats to Save People (Medium)
+    You are given an array people where people[i] is the weight of the ith 
+    person, and an infinite number of boats where each boat can carry a maximum 
+    weight of limit. Each boat carries at most two people at the same time, 
+    provided the sum of the weight of those people is at most limit. Return the 
+    minimum number of boats to carry every given person.
+
+    Example 1:
+    Input: people = [1,2], limit = 3
+    Output: 1
+    Explanation: 1 boat (1, 2)
+
+    Example 2:
+    Input: people = [3,2,2,1], limit = 3
+    Output: 3
+    Explanation: 3 boats (1, 2), (2) and (3)
+
+    Example 3:
+    Input: people = [3,5,3,4], limit = 5
+    Output: 4
+    Explanation: 4 boats (3), (3), (4), (5)
+
+    Constraints:
+    * 1 <= people.length <= 5 * 10^4
+    * 1 <= people[i] <= limit <= 3 * 10^4*/
+
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people); 
+        int ans = 0; 
+        for (int lo = 0, hi = people.length-1; lo <= hi; ) {
+            if (lo < hi && people[lo] + people[hi] <= limit) ++lo; 
+            --hi; 
+            ++ans; 
+        }
+        return ans; 
+    }
+
+
     /*904. Fruit Into Baskets (Medium)
     You are visiting a farm that has a single row of fruit trees arranged from 
     left to right. The trees are represented by an integer array fruits where 
