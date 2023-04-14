4593472bcf6df4be3d95cd9ec3297f6b06f4075d
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-04-13 20:05:30 -0400

    java 516

--- leetcode.java
+++ leetcode.java
@@ -1914,6 +1914,39 @@ class Solution {
     }
 
 
+    /*516. Longest Palindromic Subsequence (Medium)
+    Given a string s, find the longest palindromic subsequence's length in s. A 
+    subsequence is a sequence that can be derived from another sequence by 
+    deleting some or no elements without changing the order of the remaining 
+    elements.
+
+    Example 1:
+    Input: s = "bbbab"
+    Output: 4
+    Explanation: One possible longest palindromic subsequence is "bbbb".
+
+    Example 2:
+    Input: s = "cbbd"
+    Output: 2
+    Explanation: One possible longest palindromic subsequence is "bb".
+
+    Constraints:
+    * 1 <= s.length <= 1000
+    * s consists only of lowercase English letters.*/
+
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length(); 
+        int[][] dp = new int[n][n]; 
+        for (int i = n-1; i >= 0; --i) {
+            dp[i][i] = 1; 
+            for (int j = i+1; j < n; ++j) 
+                if (s.charAt(i) == s.charAt(j)) dp[i][j] = 2 + dp[i+1][j-1]; 
+                else dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]); 
+        }
+        return dp[0][n-1]; 
+    }
+
+
     /*540. Single Element in a Sorted Array (Medium)
     You are given a sorted array consisting of only integers where every 
     element appears exactly twice, except for one element which appears exactly 
