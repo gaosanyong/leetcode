c0bc667720455bdfffdcfc1116475557ccd56d9a
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-04-26 19:19:08 -0400

    solve'em all (2647)

--- leetcode.cpp
+++ leetcode.cpp
@@ -59175,6 +59175,70 @@ public:
     }
 
 
+    /*2647. Color the Triangle Red (Hard)
+    You are given an integer n. Consider an equilateral triangle of side length 
+    n, broken up into n2 unit equilateral triangles. The triangle has n 
+    1-indexed rows where the ith row has 2i - 1 unit equilateral triangles.
+    The triangles in the ith row are also 1-indexed with coordinates from (i, 1) 
+    to (i, 2i - 1). The following image shows a triangle of side length 4 with 
+    the indexing of its triangle. Two triangles are neighbors if they share a 
+    side. For example:
+    * Triangles (1,1) and (2,2) are neighbors
+    * Triangles (3,2) and (3,3) are neighbors.
+    * Triangles (2,2) and (3,3) are not neighbors because they do not share any 
+      side.
+    Initially, all the unit triangles are white. You want to choose k triangles 
+    and color them red. We will then run the following algorithm:
+    * Choose a white triangle that has at least two red neighbors.
+      - If there is no such triangle, stop the algorithm.
+    * Color that triangle red.
+    * Go to step 1.
+    Choose the minimum k possible and set k triangles red before running this 
+    algorithm such that after the algorithm stops, all unit triangles are 
+    colored red. Return a 2D list of the coordinates of the triangles that you 
+    will color red initially. The answer has to be of the smallest size 
+    possible. If there are multiple valid solutions, return any.
+
+    Example 1:
+    Input: n = 3
+    Output: [[1,1],[2,1],[2,3],[3,1],[3,5]]
+    Explanation: Initially, we choose the shown 5 triangles to be red. Then, we 
+                 run the algorithm:
+                 - Choose (2,2) that has three red neighbors and color it red.
+                 - Choose (3,2) that has two red neighbors and color it red.
+                 - Choose (3,4) that has three red neighbors and color it red.
+                 - Choose (3,3) that has three red neighbors and color it red.
+                 It can be shown that choosing any 4 triangles and running the 
+                 algorithm will not make all triangles red.
+    
+    Example 2:
+    Input: n = 2
+    Output: [[1,1],[2,1],[2,3]]
+    Explanation: Initially, we choose the shown 3 triangles to be red. Then, we 
+                 run the algorithm:
+                 - Choose (2,2) that has three red neighbors and color it red.
+                 It can be shown that choosing any 2 triangles and running the 
+                 algorithm will not make all triangles red.
+
+    Constraints: 1 <= n <= 1000*/
+
+    vector<vector<int>> colorRed(int n) {
+        vector<vector<int>> ans; 
+        int p = 0; 
+        for (int i = n; i >= 1; --i, p = (p+1)%4) 
+            if (p == 0) 
+                for (int j = 2*i-1; j > 0; j -= 2) ans.push_back({i, j}); 
+            else if (p == 1) {
+                if (2 <= i) ans.push_back({i, 2}); 
+            } else if (p == 2) 
+                for (int j = 2*i-1; j >= 3; j -= 2) ans.push_back({i, j}); 
+            else ans.push_back({i, 1}); 
+        if (p == 2 || p == 3) ans.push_back({1, 1}); 
+        reverse(ans.begin(), ans.end()); 
+        return ans; 
+    }
+
+
     /*2655. Find Maximal Uncovered Ranges (Medium)
     You are given an integer n which is the length of a 0-indexed array nums, 
     and a 0-indexed 2D-array ranges, which is a list of sub-ranges of nums 
--- leetcode.py
+++ leetcode.py
@@ -78959,6 +78959,68 @@ class SegTreeLazy:
         return min(dfs(0, -1))
 
 
+    """2647. Color the Triangle Red (Hard)
+    You are given an integer n. Consider an equilateral triangle of side length 
+    n, broken up into n2 unit equilateral triangles. The triangle has n 
+    1-indexed rows where the ith row has 2i - 1 unit equilateral triangles.
+    The triangles in the ith row are also 1-indexed with coordinates from (i, 1) 
+    to (i, 2i - 1). The following image shows a triangle of side length 4 with 
+    the indexing of its triangle. Two triangles are neighbors if they share a 
+    side. For example:
+    * Triangles (1,1) and (2,2) are neighbors
+    * Triangles (3,2) and (3,3) are neighbors.
+    * Triangles (2,2) and (3,3) are not neighbors because they do not share any 
+      side.
+    Initially, all the unit triangles are white. You want to choose k triangles 
+    and color them red. We will then run the following algorithm:
+    * Choose a white triangle that has at least two red neighbors.
+      - If there is no such triangle, stop the algorithm.
+    * Color that triangle red.
+    * Go to step 1.
+    Choose the minimum k possible and set k triangles red before running this 
+    algorithm such that after the algorithm stops, all unit triangles are 
+    colored red. Return a 2D list of the coordinates of the triangles that you 
+    will color red initially. The answer has to be of the smallest size 
+    possible. If there are multiple valid solutions, return any.
+
+    Example 1:
+    Input: n = 3
+    Output: [[1,1],[2,1],[2,3],[3,1],[3,5]]
+    Explanation: Initially, we choose the shown 5 triangles to be red. Then, we 
+                 run the algorithm:
+                 - Choose (2,2) that has three red neighbors and color it red.
+                 - Choose (3,2) that has two red neighbors and color it red.
+                 - Choose (3,4) that has three red neighbors and color it red.
+                 - Choose (3,3) that has three red neighbors and color it red.
+                 It can be shown that choosing any 4 triangles and running the 
+                 algorithm will not make all triangles red.
+    
+    Example 2:
+    Input: n = 2
+    Output: [[1,1],[2,1],[2,3]]
+    Explanation: Initially, we choose the shown 3 triangles to be red. Then, we 
+                 run the algorithm:
+                 - Choose (2,2) that has three red neighbors and color it red.
+                 It can be shown that choosing any 2 triangles and running the 
+                 algorithm will not make all triangles red.
+
+    Constraints: 1 <= n <= 1000"""
+
+    def colorRed(self, n: int) -> List[List[int]]:
+        ans = []
+        for i in range(n, 0, -1): 
+            p = (n-i)%4
+            if p == 0: 
+                for j in range(2*i-1, 0, -2): ans.append([i, j])
+            elif p == 1: 
+                if 2 <= i: ans.append([i, 2])
+            elif p == 2: 
+                for j in range(2*i-1, 2, -2): ans.append([i, j])
+            else: ans.append([i, 1])
+        if not ans or ans[-1] != [1, 1]: ans.append([1, 1])
+        return ans[::-1]
+
+
     """2655. Find Maximal Uncovered Ranges (Medium)
     You are given an integer n which is the length of a 0-indexed array nums, 
     and a 0-indexed 2D-array ranges, which is a list of sub-ranges of nums 
