b91387858ce99d58cb7f4af15c480674d1d148fb
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-12-09 23:11:48 -0500

    weekly 375 (2960|2961|2962|2963)

--- leetcode.java
+++ leetcode.java
@@ -17746,6 +17746,198 @@ class SegTreeLazy {
             seen[i % n].merge(diff, 1, Integer::sum); 
         }
         return ans; 
+    }
+
+
+    /*2960. Count Tested Devices After Test Operations (Easy)
+    You are given a 0-indexed integer array batteryPercentages having length n,
+    denoting the battery percentages of n 0-indexed devices. Your task is to
+    test each device i in order from 0 to n - 1, by performing the following
+    test operations:
+    * If batteryPercentages[i] is greater than 0:
+      + Increment the count of tested devices.
+      + Decrease the battery percentage of all devices with indices j in the
+        range [i + 1, n - 1] by 1, ensuring their battery percentage never goes
+        below 0, i.e, batteryPercentages[j] = max(0, batteryPercentages[j] - 1).
+      + Move to the next device.
+    * Otherwise, move to the next device without performing any test.
+    Return an integer denoting the number of devices that will be tested after
+    performing the test operations in order.
+
+    Example 1:
+    Input: batteryPercentages = [1,1,2,1,3]
+    Output: 3
+    Explanation: Performing the test operations in order starting from device 0:
+                 - At device 1, batteryPercentages [1] == 0, so we move to the
+                   next device without testing.
+                 - At device 2, batteryPercentages[2] > 0, so there are now 2
+                   tested devices, and batteryPercentages becomes [1,0,1,0,1].
+                 - At device 3, batteryPercentages [3] == 0, so we move to the
+                   next device without testing.
+                 - At device 0, batteryPercentages[0] > 0, so there is now 1
+                   tested device, and batteryPercentages becomes [1,0,1,0,2].
+                 - At device 4, batteryPercentages[4] > 0, so there are now 3
+                   tested devices, and batteryPercentages stays the same.
+                 So, the answer is 3.
+
+    Example 2:
+    Input: batteryPercentages = [0,1,2]
+    Output: 2
+    Explanation: Performing the test operations in order starting from device 0:
+                 - At device 0, batteryPercentages[0] == 0, so we move to the
+                   next device without testing.
+                 - At device 1, batteryPercentages[1] > 0, so there is now 1
+                   tested device, and batteryPercentages becomes [0,1,1].
+                 - At device 2, batteryPercentages [2] > 0, so there are now 2
+                   tested devices, and batteryPercentages stays the same.
+                 So, the answer is 2.
+
+    Constraints:
+    * 1 <= n == batteryPercentages.length <= 100
+    * 0 <= batteryPercentages[i] <= 100*/
+
+    public int countTestedDevices(int[] batteryPercentages) {
+        int ans = 0;
+        for (var x : batteryPercentages)
+            if (ans < x) ++ans;
+        return ans;
+    }
+
+
+    /*2961. Double Modular Exponentiation (Medium)
+    You are given a 0-indexed 2D array variables where
+    variables[i] = [ai, bi, ci, mi], and an integer target. An index i is good
+    if the following formula holds:
+    * 0 <= i < variables.length
+    * ((aibi % 10)ci) % mi == target
+    Return an array consisting of good indices in any order.
+
+    Example 1:
+    Input: variables = [[2,3,3,10],[3,3,3,1],[6,1,1,4]], target = 2
+    Output: [0,2]
+    Explanation: For each index i in the variables array:
+                 1) For the index 0, variables[0] = [2,3,3,10],
+                    (23 % 10)3 % 10 = 2.
+                 2) For the index 1, variables[1] = [3,3,3,1],
+                    (33 % 10)3 % 1 = 0.
+                 3) For the index 2, variables[2] = [6,1,1,4],
+                    (61 % 10)1 % 4 = 2.
+                 Therefore we return [0,2] as the answer.
+
+    Example 2:
+    Input: variables = [[39,3,1000,1000]], target = 17
+    Output: []
+    Explanation: For each index i in the variables array:
+                 1) For the index 0, variables[0] = [39,3,1000,1000],
+                    (393 % 10)1000 % 1000 = 1.
+                 Therefore we return [] as the answer.
+
+    Constraints:
+    * 1 <= variables.length <= 100
+    * variables[i] == [ai, bi, ci, mi]
+    * 1 <= ai, bi, ci, mi <= 10^3
+    * 0 <= target <= 10^3*/
+
+    private int pow(long x, int p, int m) {
+        long ans = 1;
+        for (; p > 0; p >>= 1) {
+            if ((p & 1) > 0) ans = ans * x % m;
+            x = x * x % m;
+        }
+        return (int) ans;
+    }
+
+    public List<Integer> getGoodIndices(int[][] variables, int target) {
+        List<Integer> ans = new ArrayList();
+        for (int i = 0; i < variables.length; ++i) {
+            int a = variables[i][0], b = variables[i][1], c = variables[i][2], m = variables[i][3];
+            if (pow(pow(a, b, 10), c, m) == target) ans.add(i);
+        }
+        return ans;
+    }
+
+
+    /*2962. Count Subarrays Where Max Element Appears at Least K Times (Meidum)
+    You are given an integer array nums and a positive integer k. Return the
+    number of subarrays where the maximum element of nums appears at least k
+    times in that subarray. A subarray is a contiguous sequence of elements
+    within an array.
+
+    Example 1:
+    Input: nums = [1,3,2,3,3], k = 2
+    Output: 6
+    Explanation: The subarrays that contain the element 3 at least 2 times are:
+                 [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+
+    Example 2:
+    Input: nums = [1,4,2,1], k = 3
+    Output: 0
+    Explanation: No subarray contains the element 4 at least 3 times.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 1 <= nums[i] <= 10^6
+    * 1 <= k <= 10^5*/
+
+    public long countSubarrays(int[] nums, int k) {
+        long ans = 0;
+        Map<Integer, Integer> freq = new HashMap();
+        int m = Arrays.stream(nums).max().getAsInt(), ii = 0;
+        for (var x : nums) {
+            freq.merge(x, 1, Integer::sum);
+            while (freq.getOrDefault(m, 0) >= k)
+                freq.merge(nums[ii++], -1, Integer::sum);
+            ans += ii;
+        }
+        return ans;
+    }
+
+
+    /*2963. Count the Number of Good Partitions (Hard)
+    You are given a 0-indexed array nums consisting of positive integers. A
+    partition of an array into one or more contiguous subarrays is called good
+    if no two subarrays contain the same number. Return the total number of good
+    partitions of nums. Since the answer may be large, return it modulo
+    10^9 + 7.
+
+    Example 1:
+    Input: nums = [1,2,3,4]
+    Output: 8
+    Explanation: The 8 possible good partitions are: ([1], [2], [3], [4]),
+                 ([1], [2], [3,4]), ([1], [2,3], [4]), ([1], [2,3,4]),
+                 ([1,2], [3], [4]), ([1,2], [3,4]), ([1,2,3], [4]), and
+                 ([1,2,3,4]).
+
+    Example 2:
+    Input: nums = [1,1,1,1]
+    Output: 1
+    Explanation: The only possible good partition is: ([1,1,1,1]).
+
+    Example 3:
+    Input: nums = [1,2,1,3]
+    Output: 2
+    Explanation: The 2 possible good partitions are: ([1,2,1], [3]) and
+                 ([1,2,1,3]).
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 1 <= nums[i] <= 10^9*/
+
+    public int numberOfGoodPartitions(int[] nums) {
+        Map<Integer, Integer> last = new HashMap();
+        for (int i = 0; i < nums.length; ++i) last.put(nums[i], i);
+        int p = -1;
+        for (int i = 0, ii = -1; i < nums.length; ++i) {
+            if (ii < i) ++p;
+            ii = Math.max(ii, last.get(nums[i]));
+        }
+        long ans = 1, x = 2;
+        int mod = 1_000_000_007;
+        for (; p > 0; p >>= 1) {
+            if ((p & 1) > 0) ans = ans * x % mod;
+            x = x * x % mod;
+        }
+        return (int) ans;
     }    
 }
 
