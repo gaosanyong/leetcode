a95131defbbebb86a97116948a22a1f852dc5bee
Ye Gao <ye_gao@mfe.berkeley.edu>
2023-12-19 21:51:31 -0500

    solve'em all (2744)

--- leetcode.cpp
+++ leetcode.cpp
@@ -60068,6 +60068,60 @@ public:
     }
 
 
+    /*2744. Find Maximum Number of String Pairs (Easy)
+    You are given a 0-indexed array words consisting of distinct strings. The
+    string words[i] can be paired with the string words[j] if:
+    * The string words[i] is equal to the reversed string of words[j].
+    * 0 <= i < j < words.length.
+    Return the maximum number of pairs that can be formed from the array words.
+    Note that each string can belong in at most one pair.
+
+    Example 1:
+    Input: words = ["cd","ac","dc","ca","zz"]
+    Output: 2
+    Explanation: In this example, we can form 2 pair of strings in the following
+                 way:
+                 - We pair the 0th string with the 2nd string, as the reversed
+                   string of word[0] is "dc" and is equal to words[2].
+                 - We pair the 1st string with the 3rd string, as the reversed
+                   string of word[1] is "ca" and is equal to words[3].
+                 It can be proven that 2 is the maximum number of pairs that can
+                 be formed.
+
+    Example 2:
+    Input: words = ["ab","ba","cc"]
+    Output: 1
+    Explanation: In this example, we can form 1 pair of strings in the following
+                 way:
+                 - We pair the 0th string with the 1st string, as the reversed
+                   string of words[1] is "ab" and is equal to words[0].
+                 It can be proven that 1 is the maximum number of pairs that can
+                 be formed.
+
+    Example 3:
+    Input: words = ["aa","ab"]
+    Output: 0
+    Explanation: In this example, we are unable to form any pair of strings.
+
+    Constraints:
+    * 1 <= words.length <= 50
+    * words[i].length == 2
+    * words consists of distinct strings.
+    * words[i] contains only lowercase English letters.*/
+
+    int maximumNumberOfStringPairs(vector<string>& words) {
+        int ans = 0;
+        unordered_set<string> seen;
+        for (auto& w : words) {
+            string ww = w;
+            reverse(w.begin(), w.end());
+            if (seen.contains(w)) ++ans;
+            seen.insert(ww);
+        }
+        return ans;
+    }
+
+
     /*2769. Find the Maximum Achievable Number (Easy)
     You are given two integers, num and t. An integer x is called achievable if
     it can become equal to num after applying the following operation no more
--- leetcode.py
+++ leetcode.py
@@ -79974,6 +79974,56 @@ class SegTreeLazy:
         return sorted(nums[:3])[1]
 
 
+    """2744. Find Maximum Number of String Pairs (Easy)
+    You are given a 0-indexed array words consisting of distinct strings. The
+    string words[i] can be paired with the string words[j] if:
+    * The string words[i] is equal to the reversed string of words[j].
+    * 0 <= i < j < words.length.
+    Return the maximum number of pairs that can be formed from the array words.
+    Note that each string can belong in at most one pair.
+
+    Example 1:
+    Input: words = ["cd","ac","dc","ca","zz"]
+    Output: 2
+    Explanation: In this example, we can form 2 pair of strings in the following
+                 way:
+                 - We pair the 0th string with the 2nd string, as the reversed
+                   string of word[0] is "dc" and is equal to words[2].
+                 - We pair the 1st string with the 3rd string, as the reversed
+                   string of word[1] is "ca" and is equal to words[3].
+                 It can be proven that 2 is the maximum number of pairs that can
+                 be formed.
+
+    Example 2:
+    Input: words = ["ab","ba","cc"]
+    Output: 1
+    Explanation: In this example, we can form 1 pair of strings in the following
+                 way:
+                 - We pair the 0th string with the 1st string, as the reversed
+                   string of words[1] is "ab" and is equal to words[0].
+                 It can be proven that 1 is the maximum number of pairs that can
+                 be formed.
+
+    Example 3:
+    Input: words = ["aa","ab"]
+    Output: 0
+    Explanation: In this example, we are unable to form any pair of strings.
+
+    Constraints:
+    * 1 <= words.length <= 50
+    * words[i].length == 2
+    * words consists of distinct strings.
+    * words[i] contains only lowercase English letters."""
+
+    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
+        ans = 0
+        seen = set()
+        for word in words:
+            if word[::-1] in seen: ans += 1
+            seen.add(word)
+        return ans
+
+
     """2778. Sum of Squares of Special Elements (Easy)
     You are given a 1-indexed integer array nums of length n. An element nums[i]
     of nums is called special if i divides n, i.e. n % i == 0. Return the sum
