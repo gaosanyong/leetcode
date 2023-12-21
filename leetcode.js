bd0942c152ab6c943458247f841b729770a0bcee
Ye Gao <gaoyescm@amazon.com>
2023-12-21 11:53:36 -0500

    solve'em all (2913)

--- leetcode.js
+++ leetcode.js
@@ -246,6 +246,55 @@ var minimumRightShifts = function(nums) {
 };
 
 
+/*2913. Subarrays Distinct Element Sum of Squares I (Easy)
+You are given a 0-indexed integer array nums. The distinct count of a
+subarray of nums is defined as:
+* Let nums[i..j] be a subarray of nums consisting of all the indices from i
+  to j such that 0 <= i <= j < nums.length. Then the number of distinct
+  values in nums[i..j] is called the distinct count of nums[i..j].
+Return the sum of the squares of distinct counts of all subarrays of nums. A
+subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+Input: nums = [1,2,1]
+Output: 15
+Explanation: Six possible subarrays are:
+             [1]: 1 distinct value
+             [2]: 1 distinct value
+             [1]: 1 distinct value
+             [1,2]: 2 distinct values
+             [2,1]: 2 distinct values
+             [1,2,1]: 2 distinct values
+             The sum of the squares of the distinct counts in all subarrays
+             is equal to 12 + 12 + 12 + 22 + 22 + 22 = 15.
+
+Example 2:
+Input: nums = [1,1]
+Output: 3
+Explanation: Three possible subarrays are:
+             [1]: 1 distinct value
+             [1]: 1 distinct value
+             [1,1]: 1 distinct value
+             The sum of the squares of the distinct counts in all subarrays
+             is equal to 12 + 12 + 12 = 3.
+
+Constraints:
+* 1 <= nums.length <= 100
+* 1 <= nums[i] <= 100*/
+
+var sumCounts = function(nums) {
+    let ans = 0;
+    for (let i = 0, n = nums.length; i < n; ++i) {
+        const seen = new Set();
+        for (let j = i; j < n; ++j) {
+            seen.add(nums[j]);
+            ans += Math.pow(seen.size, 2);
+        }
+    }
+    return ans;
+};
+
+
 /*2928. Distribute Candies Among Children I (Easy)
 You are given two positive integers n and limit. Return the total number of 
 ways to distribute n candies among 3 children such that no child gets more 
--- leetcode.ts
+++ leetcode.ts
@@ -211,3 +211,52 @@ function minimumRightShifts(nums: number[]): number {
     if (k === 0) return 0;
     return nums[n-1] < nums[0] ? n - k : -1;
 };
+
+
+/*2913. Subarrays Distinct Element Sum of Squares I (Easy)
+You are given a 0-indexed integer array nums. The distinct count of a
+subarray of nums is defined as:
+* Let nums[i..j] be a subarray of nums consisting of all the indices from i
+  to j such that 0 <= i <= j < nums.length. Then the number of distinct
+  values in nums[i..j] is called the distinct count of nums[i..j].
+Return the sum of the squares of distinct counts of all subarrays of nums. A
+subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+Input: nums = [1,2,1]
+Output: 15
+Explanation: Six possible subarrays are:
+             [1]: 1 distinct value
+             [2]: 1 distinct value
+             [1]: 1 distinct value
+             [1,2]: 2 distinct values
+             [2,1]: 2 distinct values
+             [1,2,1]: 2 distinct values
+             The sum of the squares of the distinct counts in all subarrays
+             is equal to 12 + 12 + 12 + 22 + 22 + 22 = 15.
+
+Example 2:
+Input: nums = [1,1]
+Output: 3
+Explanation: Three possible subarrays are:
+             [1]: 1 distinct value
+             [1]: 1 distinct value
+             [1,1]: 1 distinct value
+             The sum of the squares of the distinct counts in all subarrays
+             is equal to 12 + 12 + 12 = 3.
+
+Constraints:
+* 1 <= nums.length <= 100
+* 1 <= nums[i] <= 100*/
+
+function sumCounts(nums: number[]): number {
+    let ans = 0;
+    for (let i = 0, n = nums.length; i < n; ++i) {
+        const seen = new Set();
+        for (let j = i; j < n; ++j) {
+            seen.add(nums[j]);
+            ans += Math.pow(seen.size, 2);
+        }
+    }
+    return ans;
+};
