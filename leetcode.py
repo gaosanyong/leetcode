6fac473e6b2e0291ccbeee7c1d985acf8c1ac394
Ye Gao <gaoyescm@amazon.com>
2024-01-30 12:28:41 -0500

    weekly 382 (3019|3020|3021|3022)

--- leetcode.py
+++ leetcode.py
@@ -84593,6 +84593,188 @@ class SegTreeLazy:
         return ans
 
 
+    """3019. Number of Changing Keys (Easy)
+    You are given a 0-indexed string s typed by a user. Changing a key is
+    defined as using a key different from the last used key. For example,
+    s = "ab" has a change of a key while s = "bBBb" does not have any. Return
+    the number of times the user had to change the key. Note: Modifiers like
+    shift or caps lock won't be counted in changing the key that is if a user
+    typed the letter 'a' and then the letter 'A' then it will not be considered
+    as a changing of key.
+
+    Example 1:
+    Input: s = "aAbBcC"
+    Output: 2
+    Explanation: - From s[0] = 'a' to s[1] = 'A', there is no change of key as
+                   caps lock or shift is not counted.
+                 - From s[1] = 'A' to s[2] = 'b', there is a change of key.
+                 - From s[2] = 'b' to s[3] = 'B', there is no change of key as
+                   caps lock or shift is not counted.
+                 - From s[3] = 'B' to s[4] = 'c', there is a change of key.
+                 - From s[4] = 'c' to s[5] = 'C', there is no change of key as
+                   caps lock or shift is not counted.
+
+    Example 2:
+    Input: s = "AaAaAaaA"
+    Output: 0
+    Explanation: There is no change of key since only the letters 'a' and 'A'
+                 are pressed which does not require change of key.
+
+    Constraints:
+    * 1 <= s.length <= 100
+    * s consists of only upper case and lower case English letters."""
+
+    def countKeyChanges(self, s: str) -> int:
+        return sum(s[i-1].lower() != s[i].lower() for i in range(1, len(s)))
+
+
+    """3020. Find the Maximum Number of Elements in Subset (Medium)
+    You are given an array of positive integers nums. You need to select a
+    subset of nums which satisfies the following condition:
+    * You can place the selected elements in a 0-indexed array such that it
+      follows the pattern: [x, x2, x4, ..., xk/2, xk, xk/2, ..., x4, x2, x]
+      (Note that k can be be any non-negative power of 2). For example,
+      [2, 4, 16, 4, 2] and [3, 9, 3] follow the pattern while [2, 4, 8, 4, 2]
+      does not.
+    Return the maximum number of elements in a subset that satisfies these
+    conditions.
+
+    Example 1:
+    Input: nums = [5,4,1,2,2]
+    Output: 3
+    Explanation: We can select the subset {4,2,2}, which can be placed in the
+                 array as [2,4,2] which follows the pattern and 22 == 4. Hence
+                 the answer is 3.
+
+    Example 2:
+    Input: nums = [1,3,2,4]
+    Output: 1
+    Explanation: We can select the subset {1}, which can be placed in the array
+                 as [1] which follows the pattern. Hence the answer is 1. Note
+                 that we could have also selected the subsets {2}, {4}, or {3},
+                 there may be multiple subsets which provide the same answer.
+
+    Constraints:
+    * 2 <= nums.length <= 10^5
+    * 1 <= nums[i] <= 10^9"""
+
+    def maximumLength(self, nums: List[int]) -> int:
+        ans = 0
+        freq = Counter(nums)
+        for k, v in freq.items():
+            if k == 1: cand = v
+            else:
+                cand = 0
+                while k in freq:
+                    cand += 2
+                    if freq[k] == 1: break
+                    k *= k
+            if not cand&1: cand -= 1
+            ans = max(ans, cand)
+        return ans
+
+
+    """3021. Alice and Bob Playing Flower Game (Medium)
+    Alice and Bob are playing a turn-based game on a circular field surrounded
+    by flowers. The circle represents the field, and there are x flowers in the
+    clockwise direction between Alice and Bob, and y flowers in the anti-
+    clockwise direction between them. The game proceeds as follows:
+    * Alice takes the first turn.
+    * In each turn, a player must choose either the clockwise or anti-clockwise
+      direction and pick one flower from that side.
+    * At the end of the turn, if there are no flowers left at all, the current
+      player captures their opponent and wins the game.
+    Given two integers, n and m, the task is to compute the number of possible
+    pairs (x, y) that satisfy the conditions:
+    * Alice must win the game according to the described rules.
+    * The number of flowers x in the clockwise direction must be in the range
+      [1,n].
+    * The number of flowers y in the anti-clockwise direction must be in the
+      range [1,m].
+    Return the number of possible pairs (x, y) that satisfy the conditions
+    mentioned in the statement.
+
+    Example 1:
+    Input: n = 3, m = 2
+    Output: 3
+    Explanation: The following pairs satisfy conditions described in the
+                 statement: (1,2), (3,2), (2,1).
+
+    Example 2:
+    Input: n = 1, m = 1
+    Output: 0
+    Explanation: No pairs satisfy the conditions described in the statement.
+
+    Constraints: 1 <= n, m <= 10^5"""
+
+    def flowerGame(self, n: int, m: int) -> int:
+        return m*n//2
+
+
+    """3022. Minimize OR of Remaining Elements Using Operations (Hard)
+    You are given a 0-indexed integer array nums and an integer k. In one
+    operation, you can pick any index i of nums such that
+    0 <= i < nums.length - 1 and replace nums[i] and nums[i + 1] with a single
+    occurrence of nums[i] & nums[i + 1], where & represents the bitwise AND
+    operator. Return the minimum possible value of the bitwise OR of the
+    remaining elements of nums after applying at most k operations.
+
+    Example 1:
+    Input: nums = [3,5,3,2,7], k = 2
+    Output: 3
+    Explanation: Let's do the following operations:
+                 1. Replace nums[0] and nums[1] with (nums[0] & nums[1]) so that
+                    nums becomes equal to [1,3,2,7].
+                 2. Replace nums[2] and nums[3] with (nums[2] & nums[3]) so that
+                    nums becomes equal to [1,3,2].
+                 The bitwise-or of the final array is 3. It can be shown that 3
+                 is the minimum possible value of the bitwise OR of the
+                 remaining elements of nums after applying at most k operations.
+
+    Example 2:
+    Input: nums = [7,3,15,14,2,8], k = 4
+    Output: 2
+    Explanation: Let's do the following operations:
+                 1. Replace nums[0] and nums[1] with (nums[0] & nums[1]) so that
+                    nums becomes equal to [3,15,14,2,8].
+                 2. Replace nums[0] and nums[1] with (nums[0] & nums[1]) so that
+                    nums becomes equal to [3,14,2,8].
+                 3. Replace nums[0] and nums[1] with (nums[0] & nums[1]) so that
+                    nums becomes equal to [2,2,8].
+                 4. Replace nums[1] and nums[2] with (nums[1] & nums[2]) so that
+                    nums becomes equal to [2,0].
+                 The bitwise-or of the final array is 2. It can be shown that 2
+                 is the minimum possible value of the bitwise OR of the
+                 remaining elements of nums after applying at most k operations.
+
+    Example 3:
+    Input: nums = [10,7,10,3,9,14,9,4], k = 1
+    Output: 15
+    Explanation: Without applying any operations, the bitwise-or of nums is 15.
+                 It can be shown that 15 is the minimum possible value of the
+                 bitwise OR of the remaining elements of nums after applying at
+                 most k operations.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 0 <= nums[i] < 2^30
+    * 0 <= k < nums.length"""
+
+    def minOrAfterOperations(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        ans = 0
+        for bit in range(30, -1, -1):
+            cnt = 0
+            mask = (1<<30)-1
+            target = ans | (1<<bit)-1
+            for x in nums:
+                mask &= x
+                if mask | target > target: cnt += 1
+                else: mask = (1<<30)-1
+            if cnt > k: ans |= 1<<bit
+        return ans
+
+
 """146. LRU Cache (Medium)
 Design and implement a data structure for Least Recently Used (LRU) cache. It
 should support the following operations: get and put.
--- leetcode.ts
+++ leetcode.ts
@@ -1076,3 +1076,194 @@ function beautifulIndices(s: string, a: string, b: string, k: number): number[] 
     }
     return ans;
 };
+
+
+/*3019. Number of Changing Keys (Easy)
+You are given a 0-indexed string s typed by a user. Changing a key is
+defined as using a key different from the last used key. For example,
+s = "ab" has a change of a key while s = "bBBb" does not have any. Return
+the number of times the user had to change the key. Note: Modifiers like
+shift or caps lock won't be counted in changing the key that is if a user
+typed the letter 'a' and then the letter 'A' then it will not be considered
+as a changing of key.
+
+Example 1:
+Input: s = "aAbBcC"
+Output: 2
+Explanation: - From s[0] = 'a' to s[1] = 'A', there is no change of key as
+               caps lock or shift is not counted.
+             - From s[1] = 'A' to s[2] = 'b', there is a change of key.
+             - From s[2] = 'b' to s[3] = 'B', there is no change of key as
+               caps lock or shift is not counted.
+             - From s[3] = 'B' to s[4] = 'c', there is a change of key.
+             - From s[4] = 'c' to s[5] = 'C', there is no change of key as
+               caps lock or shift is not counted.
+
+Example 2:
+Input: s = "AaAaAaaA"
+Output: 0
+Explanation: There is no change of key since only the letters 'a' and 'A'
+             are pressed which does not require change of key.
+
+Constraints:
+* 1 <= s.length <= 100
+* s consists of only upper case and lower case English letters.*/
+
+function countKeyChanges(s: string): number {
+    let ans = 0;
+    for (let i = 1; i < s.length; ++i)
+        if (s.charAt(i-1).toLowerCase() != s.charAt(i).toLowerCase()) ++ans;
+    return ans;
+};
+
+
+/*3020. Find the Maximum Number of Elements in Subset (Medium)
+You are given an array of positive integers nums. You need to select a
+subset of nums which satisfies the following condition:
+* You can place the selected elements in a 0-indexed array such that it
+  follows the pattern: [x, x2, x4, ..., xk/2, xk, xk/2, ..., x4, x2, x]
+  (Note that k can be be any non-negative power of 2). For example,
+  [2, 4, 16, 4, 2] and [3, 9, 3] follow the pattern while [2, 4, 8, 4, 2]
+  does not.
+Return the maximum number of elements in a subset that satisfies these
+conditions.
+
+Example 1:
+Input: nums = [5,4,1,2,2]
+Output: 3
+Explanation: We can select the subset {4,2,2}, which can be placed in the
+             array as [2,4,2] which follows the pattern and 22 == 4. Hence
+             the answer is 3.
+
+Example 2:
+Input: nums = [1,3,2,4]
+Output: 1
+Explanation: We can select the subset {1}, which can be placed in the array
+             as [1] which follows the pattern. Hence the answer is 1. Note
+             that we could have also selected the subsets {2}, {4}, or {3},
+             there may be multiple subsets which provide the same answer.
+
+Constraints:
+* 2 <= nums.length <= 10^5
+* 1 <= nums[i] <= 10^9*/
+
+function maximumLength(nums: number[]): number {
+    const freq = new Map();
+    for (var x of nums)
+        freq.set(x, (freq.get(x) ?? 0) + 1);
+    let ans = 0;
+    for (let [k, v] of freq) {
+        if (k != 1) {
+            v = 0;
+            for (; freq.has(k); k *= k) {
+                v += 2;
+                if (freq.get(k) == 1) break;
+            }
+        }
+        if (v % 2 == 0) --v;
+        ans = Math.max(ans, v);
+    }
+    return ans;
+};
+
+
+/*3021. Alice and Bob Playing Flower Game (Medium)
+Alice and Bob are playing a turn-based game on a circular field surrounded
+by flowers. The circle represents the field, and there are x flowers in the
+clockwise direction between Alice and Bob, and y flowers in the anti-
+clockwise direction between them. The game proceeds as follows:
+* Alice takes the first turn.
+* In each turn, a player must choose either the clockwise or anti-clockwise
+  direction and pick one flower from that side.
+* At the end of the turn, if there are no flowers left at all, the current
+  player captures their opponent and wins the game.
+Given two integers, n and m, the task is to compute the number of possible
+pairs (x, y) that satisfy the conditions:
+* Alice must win the game according to the described rules.
+* The number of flowers x in the clockwise direction must be in the range
+  [1,n].
+* The number of flowers y in the anti-clockwise direction must be in the
+  range [1,m].
+Return the number of possible pairs (x, y) that satisfy the conditions
+mentioned in the statement.
+
+Example 1:
+Input: n = 3, m = 2
+Output: 3
+Explanation: The following pairs satisfy conditions described in the
+             statement: (1,2), (3,2), (2,1).
+
+Example 2:
+Input: n = 1, m = 1
+Output: 0
+Explanation: No pairs satisfy the conditions described in the statement.
+
+Constraints: 1 <= n, m <= 10^5*/
+
+function flowerGame(n: number, m: number): number {
+    return Math.floor(m*n/2);
+};
+
+
+/*3022. Minimize OR of Remaining Elements Using Operations (Hard)
+You are given a 0-indexed integer array nums and an integer k. In one
+operation, you can pick any index i of nums such that
+0 <= i < nums.length - 1 and replace nums[i] and nums[i + 1] with a single
+occurrence of nums[i] & nums[i + 1], where & represents the bitwise AND
+operator. Return the minimum possible value of the bitwise OR of the
+remaining elements of nums after applying at most k operations.
+
+Example 1:
+Input: nums = [3,5,3,2,7], k = 2
+Output: 3
+Explanation: Let's do the following operations:
+             1. Replace nums[0] and nums[1] with (nums[0] & nums[1]) so that
+                nums becomes equal to [1,3,2,7].
+             2. Replace nums[2] and nums[3] with (nums[2] & nums[3]) so that
+                nums becomes equal to [1,3,2].
+             The bitwise-or of the final array is 3. It can be shown that 3
+             is the minimum possible value of the bitwise OR of the
+             remaining elements of nums after applying at most k operations.
+
+Example 2:
+Input: nums = [7,3,15,14,2,8], k = 4
+Output: 2
+Explanation: Let's do the following operations:
+             1. Replace nums[0] and nums[1] with (nums[0] & nums[1]) so that
+                nums becomes equal to [3,15,14,2,8].
+             2. Replace nums[0] and nums[1] with (nums[0] & nums[1]) so that
+                nums becomes equal to [3,14,2,8].
+             3. Replace nums[0] and nums[1] with (nums[0] & nums[1]) so that
+                nums becomes equal to [2,2,8].
+             4. Replace nums[1] and nums[2] with (nums[1] & nums[2]) so that
+                nums becomes equal to [2,0].
+             The bitwise-or of the final array is 2. It can be shown that 2
+             is the minimum possible value of the bitwise OR of the
+             remaining elements of nums after applying at most k operations.
+
+Example 3:
+Input: nums = [10,7,10,3,9,14,9,4], k = 1
+Output: 15
+Explanation: Without applying any operations, the bitwise-or of nums is 15.
+             It can be shown that 15 is the minimum possible value of the
+             bitwise OR of the remaining elements of nums after applying at
+             most k operations.
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* 0 <= nums[i] < 2^30
+* 0 <= k < nums.length*/
+
+function minOrAfterOperations(nums: number[], k: number): number {
+    let ans = 0, n = nums.length;
+    for (let bit = 30; bit >= 0; --bit) {
+        let cnt = 0, mask = (1<<30)-1, target = ans | (1<<bit)-1;
+        for (const x of nums) {
+            mask &= x;
+            if ((mask | target) > target) ++cnt;
+            else mask = (1<<30)-1;
+        }
+        if (cnt > k) ans |= 1<<bit;
+    }
+    return ans;
+};
