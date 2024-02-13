657ded53a6bcec14a6a91a2ce3140425f16c28f7
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-02-13 16:27:42 -0500

    solve'em all (2728)

--- leetcode.java
+++ leetcode.java
@@ -14240,6 +14240,53 @@ class SegTreeLazy {
     }
 
 
+    /*2728. Count Houses in a Circular Street (Easy)
+    You are given an object street of class Street that represents a circular
+    street and a positive integer k which represents a maximum bound for the
+    number of houses in that street (in other words, the number of houses is
+    less than or equal to k). Houses' doors could be open or closed initially.
+    Initially, you are standing in front of a door to a house on this street.
+    Your task is to count the number of houses in the street. The class Street
+    contains the following functions which may help you:
+    * void openDoor(): Open the door of the house you are in front of.
+    * void closeDoor(): Close the door of the house you are in front of.
+    * boolean isDoorOpen(): Returns true if the door of the current house is
+      open and false otherwise.
+    * void moveRight(): Move to the right house.
+    * void moveLeft(): Move to the left house.
+    Return ans which represents the number of houses on this street.
+
+    Example 1:
+    Input: street = [0,0,0,0], k = 10
+    Output: 4
+    Explanation: There are 4 houses, and all their doors are closed. The number
+                 of houses is less than k, which is 10.
+
+    Example 2:
+    Input: street = [1,0,1,1,0], k = 5
+    Output: 5
+    Explanation: There are 5 houses, and the doors of the 1st, 3rd, and 4th
+                 house (moving in the right direction) are open, and the rest
+                 are closed. The number of houses is equal to k, which is 5.
+
+    Constraints:
+    * n == number of houses
+    * 1 <= n <= k <= 10^3*/
+
+    public int houseCount(Street street, int k) {
+        for (; k > 0; --k) {
+            street.openDoor();
+            street.moveRight();
+        }
+        int ans = 0;
+        for (; street.isDoorOpen(); ++ans) {
+            street.closeDoor();
+            street.moveRight();
+        }
+        return ans;
+    }
+
+
     /*2733. Neither Minimum nor Maximum (Easy)
     Given an integer array nums containing distinct positive integers, find and
     return any number from the array that is neither the minimum nor the maximum
--- leetcode.py
+++ leetcode.py
@@ -79820,6 +79820,51 @@ class SegTreeLazy:
         return node.val[k-1]
 
 
+    """2728. Count Houses in a Circular Street (Easy)
+    You are given an object street of class Street that represents a circular
+    street and a positive integer k which represents a maximum bound for the
+    number of houses in that street (in other words, the number of houses is
+    less than or equal to k). Houses' doors could be open or closed initially.
+    Initially, you are standing in front of a door to a house on this street.
+    Your task is to count the number of houses in the street. The class Street
+    contains the following functions which may help you:
+    * void openDoor(): Open the door of the house you are in front of.
+    * void closeDoor(): Close the door of the house you are in front of.
+    * boolean isDoorOpen(): Returns true if the door of the current house is
+      open and false otherwise.
+    * void moveRight(): Move to the right house.
+    * void moveLeft(): Move to the left house.
+    Return ans which represents the number of houses on this street.
+
+    Example 1:
+    Input: street = [0,0,0,0], k = 10
+    Output: 4
+    Explanation: There are 4 houses, and all their doors are closed. The number
+                 of houses is less than k, which is 10.
+
+    Example 2:
+    Input: street = [1,0,1,1,0], k = 5
+    Output: 5
+    Explanation: There are 5 houses, and the doors of the 1st, 3rd, and 4th
+                 house (moving in the right direction) are open, and the rest
+                 are closed. The number of houses is equal to k, which is 5.
+
+    Constraints:
+    * n == number of houses
+    * 1 <= n <= k <= 10^3"""
+
+    def houseCount(self, street: Optional['Street'], k: int) -> int:
+        for _ in range(k):
+            street.openDoor()
+            street.moveRight()
+        ans = 0
+        while street.isDoorOpen():
+            ans += 1
+            street.closeDoor()
+            street.moveRight()
+        return ans
+
+
     """2769. Find the Maximum Achievable Number (Easy)
     You are given two integers, num and t. An integer x is called achievable if
     it can become equal to num after applying the following operation no more
