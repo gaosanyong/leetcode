5b3143fbdb4f09fae599b8b7453e1e7efcb6d35c
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-02-13 11:22:44 -0500

    solve'em all (2108)

--- leetcode.js
+++ leetcode.js
@@ -120,6 +120,42 @@ var firstUniqChar = function(s) {
 };
 
 
+/*2108. Find First Palindromic String in the Array (Easy)
+Given an array of strings words, return the first palindromic string in the
+array. If there is no such string, return an empty string "". A string is
+palindromic if it reads the same forward and backward.
+
+Example 1:
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada". Note that
+             "racecar" is also palindromic, but it is not the first.
+
+Example 2:
+Input: words = ["notapalindrome","racecar"]
+Output: "racecar"
+Explanation: The first and only string that is palindromic is "racecar".
+
+Example 3:
+Input: words = ["def","ghi"]
+Output: ""
+Explanation: There are no palindromic strings, so the empty string is
+             returned.
+
+Constraints:
+* 1 <= words.length <= 100
+* 1 <= words[i].length <= 100
+* words[i] consists only of lowercase English letters.*/
+
+var firstPalindrome = function(words) {
+    for (const w of words) {
+        const ww = w.split("").reverse().join("");
+        if (w === ww) return w;
+    }
+    return "";
+};
+
+
 /*2648. Generate Fibonacci Sequence (Easy)
 Write a generator function that returns a generator object which yields the 
 fibonacci sequence. The fibonacci sequence is defined by the relation 
--- leetcode.ts
+++ leetcode.ts
@@ -120,6 +120,42 @@ function firstUniqChar(s: string): number {
 };
 
 
+/*2108. Find First Palindromic String in the Array (Easy)
+Given an array of strings words, return the first palindromic string in the
+array. If there is no such string, return an empty string "". A string is
+palindromic if it reads the same forward and backward.
+
+Example 1:
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada". Note that
+             "racecar" is also palindromic, but it is not the first.
+
+Example 2:
+Input: words = ["notapalindrome","racecar"]
+Output: "racecar"
+Explanation: The first and only string that is palindromic is "racecar".
+
+Example 3:
+Input: words = ["def","ghi"]
+Output: ""
+Explanation: There are no palindromic strings, so the empty string is
+             returned.
+
+Constraints:
+* 1 <= words.length <= 100
+* 1 <= words[i].length <= 100
+* words[i] consists only of lowercase English letters.*/
+
+function firstPalindrome(words: string[]): string {
+    for (const w of words) {
+        const ww = w.split("").reverse().join("");
+        if (w === ww) return w;
+    }
+    return "";
+};
+
+
 /*2733. Neither Minimum nor Maximum (Easy)
 Given an integer array nums containing distinct positive integers, find and
 return any number from the array that is neither the minimum nor the maximum
