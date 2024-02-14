6247a7ba8e6049f73f80fed9068b30326a12201b
Ye Gao <gaoyescm@amazon.com>
2024-02-14 16:44:55 -0500

    solve'em all (2797)

--- leetcode.ts
+++ leetcode.ts
@@ -474,6 +474,63 @@ String.prototype.replicate = function(times): string {
 }
 
 
+/*2797. Partial Function with Placeholders (Easy)
+Given a function fn and an array args, return a function partialFn. Placeholders
+"_" in the args should be replaced with values from restArgs starting from index
+0. Any remaining values in the restArgs should be added at the end of the args.
+partialFn should return a result of fn. fn should be called with the elements of
+the modified args passed as separate arguments.
+
+Example 1:
+Input: fn = (...args) => args, args = [2,4,6], restArgs = [8,10]
+Output: [2,4,6,8,10]
+Explanation: const partialFn = partial(fn, args)
+             const result = partialFn(...restArgs)
+             console.log(result) // [2,4,6,8,10]
+             There are no placeholders "_" in args therefore restArgs is just
+             added at the end of args. Then the elements of the args are passed
+             as separate arguments to fn, which returns passed arguments as an
+             array.
+
+Example 2:
+Input: fn = (...args) => args, args = [1,2,"_",4,"_",6], restArgs = [3,5]
+Output: [1,2,3,4,5,6]
+Explanation: const partialFn = partial(fn, args)
+             const result = partialFn(...restArgs)
+             console.log(result) // [1,2,3,4,5,6]
+             Placeholders "_" are replaced with values from the restArgs. Then
+             the elements of the args are passed as separate arguments to fn,
+             which returns passed arguments as an array.
+
+Example 3:
+Input: fn = (a, b, c) => b + a - c, args = ["_", 5], restArgs = [5, 20]
+Output: -10
+Explanation: const partialFn = partial(fn, args)
+             const result = partialFn(...restArgs)
+             console.log(result) // -10
+             Placeholder "_" is replaced with 5 and 20 is added at the end of
+             args. Then the elements of the args are passed as separate
+             arguments to fn, which returns -10 (5 + 5 - 20).
+
+Constraints:
+* fn is a function
+* args and restArgs are valid JSON arrays
+* 1 <= args.length <= 5 * 10^4
+* 1 <= restArgs.length <= 5 * 10^4
+* 0 <= number of placeholders <= restArgs.length*/
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => JSONValue
+
+function partial(fn: Fn, args: JSONValue[]): Fn {
+
+    return function(...restArgs) {
+        args = args.map(x => x === '_' ? restArgs.shift() : x);
+        return fn(...args.concat(restArgs));
+    }
+};
+
+
 /*2824. Count Pairs Whose Sum is Less than Target (Easy)
 Given a 0-indexed integer array nums of length n and an integer target,
 return the number of pairs (i, j) where 0 <= i < j < n and
