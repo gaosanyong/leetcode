522ec86d3921d03f0428401c3eac9f6d1085e6bd
Ye Gao <gaoyescm@amazon.com>
2024-02-14 16:09:17 -0500

    solve'em all (2774)

--- leetcode.ts
+++ leetcode.ts
@@ -364,6 +364,45 @@ Date.prototype.nextDay = function(): string {
 }
 
 
+/*2774. Array Upper Bound (Easy)
+Write code that enhances all arrays such that you can call the upperBound()
+method on any array and it will return the last index of a given target number.
+nums is a sorted ascending array of numbers that may contain duplicates. If the
+target number is not found in the array, return -1.
+
+Example 1:
+Input: nums = [3,4,5], target = 5
+Output: 2
+Explanation: Last index of target value is 2
+
+Example 2:
+Input: nums = [1,4,5], target = 2
+Output: -1
+Explanation: Because there is no digit 2 in the array, return -1.
+
+Example 3:
+Input: nums = [3,4,6,6,6,6,7], target = 6
+Output: 5
+Explanation: Last index of target value is 5
+
+Constraints:
+* 1 <= nums.length <= 10^4
+* -10^4 <= nums[i], target <= 10^4
+* nums is sorted in ascending order.
+
+Follow up: Can you write an algorithm with O(log n) runtime complexity?*/
+
+Array.prototype.upperBound = function(target): number {
+    let lo = 0, hi = this.length-1;
+    while (lo < hi) {
+        const mid = Math.floor((lo + hi + 1)/2);
+        if (this[mid] <= target) lo = mid;
+        else hi = mid-1;
+    }
+    return this[lo] == target ? lo : -1;
+};
+
+
 /*2824. Count Pairs Whose Sum is Less than Target (Easy)
 Given a 0-indexed integer array nums of length n and an integer target,
 return the number of pairs (i, j) where 0 <= i < j < n and
