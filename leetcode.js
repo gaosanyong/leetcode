b7bfc0fef8ba0c441271ce13bbc161d1cd6f4e2e
Ye Gao <gaoyescm@amazon.com>
2024-02-14 16:37:17 -0500

    solve'em all (2796)

--- leetcode.js
+++ leetcode.js
@@ -544,6 +544,36 @@ var createObject = function(keysArr, valuesArr) {
 };
 
 
+/*2796. Repeat String (Easy)
+Write code that enhances all strings such that you can call the
+string.replicate(x) method on any string and it will return repeated string x
+times. Try to implement it without using the built-in method string.repeat.
+
+Example 1:
+Input: str = "hello", times = 2
+Output: "hellohello"
+Explanation: "hello" is repeated 2 times
+
+Example 2:
+Input: str = "code", times = 3
+Output: "codecodecode"
+Explanation: "code" is repeated 3 times
+
+Example 3:
+Input: str = "js", times = 1
+Output: "js"
+Explanation: "js" is repeated 1 time
+
+Constraints: 1 <= str.length, times <= 10^5*/
+
+String.prototype.replicate = function(times) {
+    const vals = [];
+    while (times--)
+        vals.push(this);
+    return vals.join('');
+}
+
+
 /*2824. Count Pairs Whose Sum is Less than Target (Easy)
 Given a 0-indexed integer array nums of length n and an integer target,
 return the number of pairs (i, j) where 0 <= i < j < n and
