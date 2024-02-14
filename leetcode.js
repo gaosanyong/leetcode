1d0efde76aad6252c6cbb0ad303a7e090ad3cff9
Ye Gao <gaoyescm@amazon.com>
2024-02-14 15:53:57 -0500

    solve'em all (2758)

--- leetcode.js
+++ leetcode.js
@@ -441,6 +441,31 @@ var maximumNumberOfStringPairs = function(words) {
 };
 
 
+/*2758. Next Day (Easy)
+Write code that enhances all date objects such that you can call the
+date.nextDay() method on any date object and it will return the next day in the
+format YYYY-MM-DD as a string.
+
+Example 1:
+Input: date = "2014-06-20"
+Output: "2014-06-21"
+Explanation: const date = new Date("2014-06-20");
+             date.nextDay(); // "2014-06-21"
+
+Example 2:
+Input: date = "2017-10-31"
+Output: "2017-11-01"
+Explanation: The day after 2017-10-31 is 2017-11-01.
+
+Constraints: new Date(date) is a valid date object*/
+
+Date.prototype.nextDay = function() {
+    const date = new Date(this);
+    date.setDate(date.getDate() + 1);
+    return date.toISOString().split('T')[0];
+}
+
+
 /*2824. Count Pairs Whose Sum is Less than Target (Easy)
 Given a 0-indexed integer array nums of length n and an integer target,
 return the number of pairs (i, j) where 0 <= i < j < n and
