2e0ef741724eedfad56a27426efad8d253dae626
Ye Gao <gaoyescm@amazon.com>
2024-02-15 10:22:48 -0500

    solve'em all (2804)

--- leetcode.ts
+++ leetcode.ts
@@ -572,6 +572,62 @@ function* factorial(n: number): Generator<number> {
 };
 
 
+/*2804. Array Prototype ForEach (Easy)
+Write your version of method forEach that enhances all arrays such that you can
+call the array.forEach(callback, context) method on any array and it will
+execute callback on each element of the array. Method forEach should not return
+anything. callback accepts the following arguments:
+* currentValue - represents the current element being processed in the array. It
+  is the value of the element in the current iteration.
+* index - represents the index of the current element being processed in the
+  array.
+* array - represents the array itself, allowing access to the entire array
+  within the callback function.
+The context is the object that should be passed as the function context
+parameter to the callback function, ensuring that the this keyword within the
+callback function refers to this context object. Try to implement it without
+using the built-in array methods.
+
+Example 1:
+Input: arr = [1,2,3],
+       callback = (val, i, arr) => arr[i] = val * 2,
+       context = {"context":true}
+Output: [2,4,6]
+Explanation: arr.forEach(callback, context)
+             console.log(arr) // [2,4,6]
+             The callback is executed on each element of the array.
+
+Example 2:
+Input: arr = [true, true, false, false],
+       callback = (val, i, arr) => arr[i] = this,
+       context = {"context": false}
+Output: [{"context":false},{"context":false},{"context":false},{"context":false}]
+Explanation: arr.forEach(callback, context)
+             console.log(arr) // [{"context":false},{"context":false},{"context":false},{"context":false}]
+             The callback is executed on each element of the array with the right context.
+
+Example 3:
+Input: arr = [true, true, false, false],
+       callback = (val, i, arr) => arr[i] = !val,
+       context = {"context": 5}
+Output: [false,false,true,true]
+
+Constraints:
+* context is a valid JSON object
+* fn is a function
+* 0 <= arr.length <= 10^5*/
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Callback = (currentValue: JSONValue, index: number, array: JSONValue[]) => any
+type Context = Record<string, JSONValue>
+
+Array.prototype.forEach = function(callback: Callback, context: Context): void {
+    for (const [i, x] of this.entries()) {
+        callback.call(context, x, i, this)
+    }
+}
+
+
 /*2824. Count Pairs Whose Sum is Less than Target (Easy)
 Given a 0-indexed integer array nums of length n and an integer target,
 return the number of pairs (i, j) where 0 <= i < j < n and
