875cab2e64ae727ff9b08f81765761afbb36e9e6
Ye Gao <gaoyescm@amazon.com>
2024-02-15 13:54:41 -0500

    solve'em all (2690)

--- leetcode.js
+++ leetcode.js
@@ -306,6 +306,35 @@ var getKthCharacter = function(root, k) {
 };
 
 
+/*2690. Infinite Method Object (Easy)
+Write a function that returns an infinite-method object. An infinite-method
+object is defined as an object that allows you to call any method and it will
+always return the name of the method. For example, if you execute obj.abc123(),
+it will return "abc123".
+
+Example 1:
+Input: method = "abc123"
+Output: "abc123"
+Explanation: const obj = createInfiniteObject();
+             obj['abc123'](); // "abc123"
+             The returned string should always match the method name.
+
+Example 2:
+Input: method = ".-qw73n|^2It"
+Output: ".-qw73n|^2It"
+Explanation: The returned string should always match the method name.
+
+Constraints: 0 <= method.length <= 1000*/
+
+var createInfiniteObject = function() {
+    return new Proxy({}, {
+        get(_, prop) {
+            return () => prop;
+        }
+    });
+};
+
+
 /*2728. Count Houses in a Circular Street (Easy)
 You are given an object street of class Street that represents a circular
 street and a positive integer k which represents a maximum bound for the
--- leetcode.ts
+++ leetcode.ts
@@ -204,6 +204,35 @@ function rearrangeArray(nums: number[]): number[] {
 };
 
 
+/*2690. Infinite Method Object (Easy)
+Write a function that returns an infinite-method object. An infinite-method
+object is defined as an object that allows you to call any method and it will
+always return the name of the method. For example, if you execute obj.abc123(),
+it will return "abc123".
+
+Example 1:
+Input: method = "abc123"
+Output: "abc123"
+Explanation: const obj = createInfiniteObject();
+             obj['abc123'](); // "abc123"
+             The returned string should always match the method name.
+
+Example 2:
+Input: method = ".-qw73n|^2It"
+Output: ".-qw73n|^2It"
+Explanation: The returned string should always match the method name.
+
+Constraints: 0 <= method.length <= 1000*/
+
+function createInfiniteObject(): Record<string, () => string> {
+    return new Proxy({}, {
+        get(_, prop) {
+            return () => prop;
+        }
+    });
+};
+
+
 /*2728. Count Houses in a Circular Street (Easy)
 You are given an object street of class Street that represents a circular
 street and a positive integer k which represents a maximum bound for the
