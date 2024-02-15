5f48e3b999762b191a1090b1dadfcd228095ae77
Ye Gao <gaoyescm@amazon.com>
2024-02-15 12:11:11 -0500

    solve'em all (2853)

--- leetcode-pandas.py
+++ leetcode-pandas.py
@@ -1922,3 +1922,60 @@ def get_total_distance(users: pd.DataFrame, rides: pd.DataFrame) -> pd.DataFrame
     ).rename(
         columns={"traveled_distance" : "traveled distance"}
     )[["user_id", "name", "traveled distance"]]
+
+
+"""2853. Highest Salaries Difference (Easy)
+SQL Schema
+Table: Salaries
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| emp_name    | varchar |
+| department  | varchar |
+| salary      | int     |
++-------------+---------+
+(emp_name, department) is the primary key (combination of unique values) for
+this table. Each row of this table contains emp_name, department and salary.
+There will be at least one entry for the engineering and marketing departments.
+Write a solution to calculate the difference between the highest salaries in the
+marketing and engineering department. Output the absolute difference in salaries.
+Return the result table. The result format is in the following example.
+
+Example 1:
+Input:
+Salaries table:
++----------+-------------+--------+
+| emp_name | department  | salary |
++----------+-------------+--------+
+| Kathy    | Engineering | 50000  |
+| Roy      | Marketing   | 30000  |
+| Charles  | Engineering | 45000  |
+| Jack     | Engineering | 85000  |
+| Benjamin | Marketing   | 34000  |
+| Anthony  | Marketing   | 42000  |
+| Edward   | Engineering | 102000 |
+| Terry    | Engineering | 44000  |
+| Evelyn   | Marketing   | 53000  |
+| Arthur   | Engineering | 32000  |
++----------+-------------+--------+
+Output:
++-------------------+
+| salary_difference |
++-------------------+
+| 49000             |
++-------------------+
+Explanation:
+- The Engineering and Marketing departments have the highest salaries of 102,000
+  and 53,000, respectively. Resulting in an absolute difference of 49,000."""
+
+def salaries_difference(salaries: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame(
+        data=[
+            abs(
+                np.max(salaries.query('department == "Marketing"')["salary"])
+                -
+                np.max(salaries.query('department == "Engineering"')["salary"])
+            )
+        ],
+        columns=["salary_difference"]
+    )
--- leetcode.sql
+++ leetcode.sql
@@ -9350,3 +9350,54 @@ FROM
     Rides USING (user_id)
 GROUP BY 1
 ORDER BY 1
+
+
+/*2853. Highest Salaries Difference (Easy)
+SQL Schema
+Table: Salaries
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| emp_name    | varchar |
+| department  | varchar |
+| salary      | int     |
++-------------+---------+
+(emp_name, department) is the primary key (combination of unique values) for
+this table. Each row of this table contains emp_name, department and salary.
+There will be at least one entry for the engineering and marketing departments.
+Write a solution to calculate the difference between the highest salaries in the
+marketing and engineering department. Output the absolute difference in salaries.
+Return the result table. The result format is in the following example.
+
+Example 1:
+Input:
+Salaries table:
++----------+-------------+--------+
+| emp_name | department  | salary |
++----------+-------------+--------+
+| Kathy    | Engineering | 50000  |
+| Roy      | Marketing   | 30000  |
+| Charles  | Engineering | 45000  |
+| Jack     | Engineering | 85000  |
+| Benjamin | Marketing   | 34000  |
+| Anthony  | Marketing   | 42000  |
+| Edward   | Engineering | 102000 |
+| Terry    | Engineering | 44000  |
+| Evelyn   | Marketing   | 53000  |
+| Arthur   | Engineering | 32000  |
++----------+-------------+--------+
+Output:
++-------------------+
+| salary_difference |
++-------------------+
+| 49000             |
++-------------------+
+Explanation:
+- The Engineering and Marketing departments have the highest salaries of 102,000
+  and 53,000, respectively. Resulting in an absolute difference of 49,000.*/
+
+SELECT
+    ABS((SELECT MAX(salary) FROM Salaries WHERE department = "Marketing")
+        -
+        (SELECT MAX(salary) FROM Salaries WHERE department = "Engineering"))
+AS salary_difference
