4179336df52512213c94d0fa0bf8c550d78e6f82
Ye Gao <gaoyescm@amazon.com>
2024-02-15 10:59:58 -0500

    solve'em all (2822)

--- leetcode.js
+++ leetcode.js
@@ -721,6 +721,49 @@ Array.prototype.forEach = function(callback, context) {
 }
 
 
+/*2822. Inversion of Object (Easy)
+Given an object or an array obj, return an inverted object or array invertedObj.
+The invertedObj should have the keys of obj as values and the values of obj as
+keys. The indices of array should be treated as keys. It is guaranteed that the
+values in obj are only strings. The function should handle duplicates, meaning
+that if there are multiple keys in obj with the same value, the invertedObj
+should map the value to an array containing all corresponding keys.
+
+Example 1:
+Input: obj = {"a": "1", "b": "2", "c": "3", "d": "4"}
+Output: invertedObj = {"1": "a", "2": "b", "3": "c", "4": "d"}
+Explanation: The keys from obj become the values in invertedObj, and the values
+             from obj become the keys in invertedObj.
+
+Example 2:
+Input: obj = {"a": "1", "b": "2", "c": "2", "d": "4"}
+Output: invertedObj = {"1": "a", "2": ["b", "c"], "4": "d"}
+Explanation: There are two keys in obj with the same value, the invertedObj
+             mapped the value to an array containing all corresponding keys.
+
+Example 3:
+Input: obj = ["1", "2", "3", "4"]
+Output: invertedObj = {"1": "0", "2": "1", "3": "2", "4": "3"}
+Explanation: Arrays are also objects therefore array has changed to an object
+             and the keys (indices) from obj become the values in invertedObj,
+             and the values from obj become the keys in invertedObj.
+
+Constraints:
+* obj is a valid JSON object or array
+* typeof obj[key] === "string"
+* 2 <= JSON.stringify(obj).length <= 10^5*/
+
+var invertObject = function(obj) {
+    return Object.entries(obj).reduce((acc , [k, v]) => {
+        if (acc[v]) {
+            if (!Array.isArray(acc[v])) acc[v] = [acc[v]];
+            acc[v].push(k);
+        } else acc[v] = k;
+        return acc;
+    }, {})
+};
+
+
 /*2824. Count Pairs Whose Sum is Less than Target (Easy)
 Given a 0-indexed integer array nums of length n and an integer target,
 return the number of pairs (i, j) where 0 <= i < j < n and
