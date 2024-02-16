2422603de60b8843bba43c5ba7c1d63af8bbceb4
Ye Gao <gaoyescm@amazon.com>
2024-02-16 15:46:39 -0500

    solve'em all (2674)

--- leetcode.cpp
+++ leetcode.cpp
@@ -60031,6 +60031,52 @@ public:
     }
 
 
+    /*2674. Split a Circular Linked Listn (Medium)
+    Given a circular linked list list of positive integers, your task is to
+    split it into 2 circular linked lists so that the first one contains the
+    first half of the nodes in list (exactly ceil(list.length / 2) nodes) in the
+    same order they appeared in list, and the second one contains the rest of
+    the nodes in list in the same order they appeared in list. Return an array
+    answer of length 2 in which the first element is a circular linked list
+    representing the first half and the second element is a circular linked list
+    representing the second half. A circular linked list is a normal linked list
+    with the only difference being that the last node's next node, is the first
+    node.
+
+    Example 1:
+    Input: nums = [1,5,7]
+    Output: [[1,5],[7]]
+    Explanation: The initial list has 3 nodes so the first half would be the
+                 first 2 elements since ceil(3 / 2) = 2 and the rest which is 1
+                 node is in the second half.
+
+    Example 2:
+    Input: nums = [2,6,1,5]
+    Output: [[2,6],[1,5]]
+    Explanation: The initial list has 4 nodes so the first half would be the
+                 first 2 elements since ceil(4 / 2) = 2 and the rest which is 2
+                 nodes are in the second half.
+
+    Constraints:
+    * The number of nodes in list is in the range [2, 10^5]
+    * 0 <= Node.val <= 10^9
+    * LastNode.next = FirstNode where LastNode is the last node of the list and
+      FirstNode is the first one*/
+
+    vector<ListNode*> splitCircularLinkedList(ListNode* list) {
+        ListNode *fast = list, *slow = list;
+        while (fast->next != list && fast->next->next != list) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        if (fast->next != list) fast = fast->next;
+        ListNode *head = slow->next;
+        slow->next = list;
+        fast->next = head;
+        return {list, head};
+    }
+
+
     /*2689. Extract Kth Character From The Rope Tree (Easy)
     You are given the root of a binary tree and an integer k. Besides the left
     and right children, every node of this tree has two other properties, a
--- leetcode.ts
+++ leetcode.ts
@@ -258,6 +258,52 @@ function areDeeplyEqual(o1: JSONValue, o2: JSONValue): boolean {
 };
 
 
+/*2674. Split a Circular Linked Listn (Medium)
+Given a circular linked list list of positive integers, your task is to
+split it into 2 circular linked lists so that the first one contains the
+first half of the nodes in list (exactly ceil(list.length / 2) nodes) in the
+same order they appeared in list, and the second one contains the rest of
+the nodes in list in the same order they appeared in list. Return an array
+answer of length 2 in which the first element is a circular linked list
+representing the first half and the second element is a circular linked list
+representing the second half. A circular linked list is a normal linked list
+with the only difference being that the last node's next node, is the first
+node.
+
+Example 1:
+Input: nums = [1,5,7]
+Output: [[1,5],[7]]
+Explanation: The initial list has 3 nodes so the first half would be the
+             first 2 elements since ceil(3 / 2) = 2 and the rest which is 1
+             node is in the second half.
+
+Example 2:
+Input: nums = [2,6,1,5]
+Output: [[2,6],[1,5]]
+Explanation: The initial list has 4 nodes so the first half would be the
+             first 2 elements since ceil(4 / 2) = 2 and the rest which is 2
+             nodes are in the second half.
+
+Constraints:
+* The number of nodes in list is in the range [2, 10^5]
+* 0 <= Node.val <= 10^9
+* LastNode.next = FirstNode where LastNode is the last node of the list and
+  FirstNode is the first one*/
+
+function splitCircularLinkedList(list: ListNode | null): Array<ListNode | null> {
+    let fast = list, slow = list;
+    while (fast.next != list && fast.next.next != list) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    if (fast.next != list) fast = fast.next;
+    let head = slow.next;
+    slow.next = list;
+    fast.next = head;
+    return [list, head];
+};
+
+
 /*2690. Infinite Method Object (Easy)
 Write a function that returns an infinite-method object. An infinite-method
 object is defined as an object that allows you to call any method and it will
