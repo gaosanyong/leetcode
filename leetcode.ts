89366ced0751dd8e5fb32a0e2bc7d786daae5660
Ye Gao <gaoyescm@amazon.com>
2024-02-16 15:04:00 -0500

    solve'em all (2628)

--- leetcode.ts
+++ leetcode.ts
@@ -204,6 +204,60 @@ function rearrangeArray(nums: number[]): number[] {
 };
 
 
+/*2628. JSON Deep Equal (Medium)
+Given two values o1 and o2, return a boolean value indicating whether two
+values, o1 and o2, are deeply equal. For two values to be deeply equal, the
+following conditions must be met:
+* If both values are primitive types, they are deeply equal if they pass the
+  === equality check.
+* If both values are arrays, they are deeply equal if they have the same
+  elements in the same order, and each element is also deeply equal according to
+  these conditions.
+* If both values are objects, they are deeply equal if they have the same keys,
+  and the associated values for each key are also deeply equal according to
+  these conditions.
+You may assume both values are the output of JSON.parse. In other words, they
+are valid JSON. Please solve it without using lodash's _.isEqual() function
+
+Example 1:
+Input: o1 = {"x":1,"y":2}, o2 = {"x":1,"y":2}
+Output: true
+Explanation: The keys and values match exactly.
+
+Example 2:
+Input: o1 = {"y":2,"x":1}, o2 = {"x":1,"y":2}
+Output: true
+Explanation: Although the keys are in a different order, they still match exactly.
+
+Example 3:
+Input: o1 = {"x":null,"L":[1,2,3]}, o2 = {"x":null,"L":["1","2","3"]}
+Output: false
+Explanation: The array of numbers is different from the array of strings.
+
+Example 4:
+Input: o1 = true, o2 = false
+Output: false
+Explanation: true !== false
+
+Constraints:
+* 1 <= JSON.stringify(o1).length <= 10^5
+* 1 <= JSON.stringify(o2).length <= 105
+* maxNestingDepth <= 1000*/
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function areDeeplyEqual(o1: JSONValue, o2: JSONValue): boolean {
+    if (o1 === o2) return true;
+    if (typeof o1 != 'object' || typeof o2 != 'object') return false;
+    if (o1 === null || o2 === null) return false;
+    if (Array.isArray(o1) !== Array.isArray(o2)) return false;
+    if (Object.keys(o1).length != Object.keys(o2).length) return false;
+    for (const k in o1)
+        if (!areDeeplyEqual(o1[k], o2[k])) return false;
+    return true;
+};
+
+
 /*2690. Infinite Method Object (Easy)
 Write a function that returns an infinite-method object. An infinite-method
 object is defined as an object that allows you to call any method and it will
