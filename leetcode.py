ebf0c08fd602dc0327dd6383d0bda98b5573758f
Ye Gao <gaoyescm@amazon.com>
2024-02-23 15:06:43 -0500

    solve'em all (2852)

--- leetcode.py
+++ leetcode.py
@@ -82283,6 +82283,89 @@ class SegTreeLazy:
         return ''.join(map(str, reversed(ans)))
 
 
+    """2852. Sum of Remoteness of All Cells (Medium)
+    You are given a 0-indexed matrix grid of order n * n. Each cell in this
+    matrix has a value grid[i][j], which is either a positive integer or -1
+    representing a blocked cell. You can move from a non-blocked cell to any
+    non-blocked cell that shares an edge. For any cell (i, j), we represent its
+    remoteness as R[i][j] which is defined as the following:
+    * If the cell (i, j) is a non-blocked cell, R[i][j] is the sum of the values
+      grid[x][y] such that there is no path from the non-blocked cell (x, y) to
+      the cell (i, j).
+    * For blocked cells, R[i][j] == 0.
+    Return the sum of R[i][j] over all cells.
+
+    Example 1:
+    Input: grid = [[-1,1,-1],[5,-1,4],[-1,3,-1]]
+    Output: 39
+    Explanation: In the picture above, there are four grids. The top-left grid
+                 contains the initial values in the grid. Blocked cells are
+                 colored black, and other cells get their values as it is in the
+                 input. In the top-right grid, you can see the value of R[i][j]
+                 for all cells. So the answer would be the sum of them. That is:
+                 0 + 12 + 0 + 8 + 0 + 9 + 0 + 10 + 0 = 39. Let's jump on the
+                 bottom-left grid in the above picture and calculate R[0][1]
+                 (the target cell is colored green). We should sum up the value
+                 of cells that can't be reached by the cell (0, 1). These cells
+                 are colored yellow in this grid. So R[0][1] = 5 + 4 + 3 = 12.
+                 Now let's jump on the bottom-right grid in the above picture
+                 and calculate R[1][2] (the target cell is colored green). We
+                 should sum up the value of cells that can't be reached by the
+                 cell (1, 2). These cells are colored yellow in this grid. So
+                 R[1][2] = 1 + 5 + 3 = 9.
+
+    Example 2:
+    Input: grid = [[-1,3,4],[-1,-1,-1],[3,-1,-1]]
+    Output: 13
+    Explanation: In the picture above, there are four grids. The top-left grid
+                 contains the initial values in the grid. Blocked cells are
+                 colored black, and other cells get their values as it is in the
+                 input. In the top-right grid, you can see the value of R[i][j]
+                 for all cells. So the answer would be the sum of them. That is:
+                 3 + 3 + 0 + 0 + 0 + 0 + 7 + 0 + 0 = 13. Let's jump on the
+                 bottom-left grid in the above picture and calculate R[0][2]
+                 (the target cell is colored green). We should sum up the value
+                 of cells that can't be reached by the cell (0, 2). This cell is
+                 colored yellow in this grid. So R[0][2] = 3. Now let's jump on
+                 the bottom-right grid in the above picture and calculate
+                 R[2][0] (the target cell is colored green). We should sum up
+                 the value of cells that can't be reached by the cell (2, 0).
+                 These cells are colored yellow in this grid. So
+                 R[2][0] = 3 + 4 = 7.
+
+    Example 3:
+    Input: grid = [[1]]
+    Output: 0
+    Explanation: Since there are no other cells than (0, 0), R[0][0] is equal to
+                 0. So the sum of R[i][j] over all cells would be 0.
+
+    Constraints:
+    * 1 <= n <= 300
+    * 1 <= grid[i][j] <= 10^6 or grid[i][j] == -1"""
+
+    def sumRemoteness(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        ans = value = count = 0
+        for r in range(n):
+            for c in range(n):
+                if grid[r][c] != -1:
+                    val, cnt = grid[r][c], 1
+                    grid[r][c] = -1
+                    stack = [(r, c)]
+                    while stack:
+                        i, j = stack.pop()
+                        for ii, jj in (i-1, j), (i, j-1), (i, j+1), (i+1, j):
+                            if 0 <= ii < n and 0 <= jj < n and grid[ii][jj] != -1:
+                                val += grid[ii][jj]
+                                cnt += 1
+                                grid[ii][jj] = -1
+                                stack.append((ii, jj))
+                    ans -= val*cnt
+                    value += val
+                    count += cnt
+        return ans + value*count
+
+
     """2855. Minimum Right Shifts to Sort the Array (Easy)
     You are given a 0-indexed array nums of length n containing distinct
     positive integers. Return the minimum number of right shifts required to
