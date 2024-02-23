591f21efd0bf78fc36a43362457ca15c8d000272
Ye Gao <gaoyescm@amazon.com>
2024-02-23 16:03:00 -0500

    solve'em all (2863)

--- leetcode.java
+++ leetcode.java
@@ -17355,6 +17355,54 @@ class SegTreeLazy {
     }
 
 
+    /*2863. Maximum Length of Semi-Decreasing Subarrays (Medium)
+    You are given an integer array nums. Return the length of the longest semi-
+    decreasing subarray of nums, and 0 if there are no such subarrays. A
+    subarray is a contiguous non-empty sequence of elements within an array. A
+    non-empty array is semi-decreasing if its first element is strictly greater
+    than its last element.
+
+    Example 1:
+    Input: nums = [7,6,5,4,3,2,1,6,10,11]
+    Output: 8
+    Explanation: Take the subarray [7,6,5,4,3,2,1,6]. The first element is 7 and
+                 the last one is 6 so the condition is met. Hence, the answer
+                 would be the length of the subarray or 8. It can be shown that
+                 there aren't any subarrays with the given condition with a
+                 length greater than 8.
+
+    Example 2:
+    Input: nums = [57,55,50,60,61,58,63,59,64,60,63]
+    Output: 6
+    Explanation: Take the subarray [61,58,63,59,64,60]. The first element is 61
+                 and the last one is 60 so the condition is met. Hence, the
+                 answer would be the length of the subarray or 6. It can be
+                 shown that there aren't any subarrays with the given condition
+                 with a length greater than 6.
+
+    Example 3:
+    Input: nums = [1,2,3,4]
+    Output: 0
+    Explanation: Since there are no semi-decreasing subarrays in the given
+                 array, the answer is 0.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * -10^9 <= nums[i] <= 10^9*/
+
+    public int maxSubarrayLength(int[] nums) {
+        Stack<Integer> stk = new Stack();
+        for (int i = 0; i < nums.length; ++i)
+            if (stk.isEmpty() || nums[stk.peek()] < nums[i])
+                stk.push(i);
+        int ans = 0;
+        for (int i = nums.length-1; i >= 0; --i)
+            while (!stk.isEmpty() && nums[stk.peek()] > nums[i])
+                ans = Math.max(ans, i - stk.pop() + 1);
+        return ans;
+    }
+
+
     /*2864. Maximum Odd Binary Number (Easy)
     You are given a binary string s that contains at least one '1'. You have to 
     rearrange the bits in such a way that the resulting binary number is the 
--- leetcode.js
+++ leetcode.js
@@ -1606,6 +1606,56 @@ var minimumRightShifts = function(nums) {
 };
 
 
+/*2863. Maximum Length of Semi-Decreasing Subarrays (Medium)
+You are given an integer array nums. Return the length of the longest semi-
+decreasing subarray of nums, and 0 if there are no such subarrays. A
+subarray is a contiguous non-empty sequence of elements within an array. A
+non-empty array is semi-decreasing if its first element is strictly greater
+than its last element.
+
+Example 1:
+Input: nums = [7,6,5,4,3,2,1,6,10,11]
+Output: 8
+Explanation: Take the subarray [7,6,5,4,3,2,1,6]. The first element is 7 and
+             the last one is 6 so the condition is met. Hence, the answer
+             would be the length of the subarray or 8. It can be shown that
+             there aren't any subarrays with the given condition with a
+             length greater than 8.
+
+Example 2:
+Input: nums = [57,55,50,60,61,58,63,59,64,60,63]
+Output: 6
+Explanation: Take the subarray [61,58,63,59,64,60]. The first element is 61
+             and the last one is 60 so the condition is met. Hence, the
+             answer would be the length of the subarray or 6. It can be
+             shown that there aren't any subarrays with the given condition
+             with a length greater than 6.
+
+Example 3:
+Input: nums = [1,2,3,4]
+Output: 0
+Explanation: Since there are no semi-decreasing subarrays in the given
+             array, the answer is 0.
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* -10^9 <= nums[i] <= 10^9*/
+
+var maxSubarrayLength = function(nums) {
+    const stk = [];
+    for (const [i, x] of nums.entries())
+        if (stk.length == 0 || stk[stk.length-1][1] < x)
+            stk.push([i, x]);
+    let ans = 0;
+    for (let i = nums.length-1; i >= 0; --i)
+        while (stk.length && stk[stk.length-1][1] > nums[i]) {
+            const [ii, _] = stk.pop();
+            ans = Math.max(ans, i-ii+1);
+        }
+    return ans;
+};
+
+
 /*2899. Last Visited Integers (Easy)
 Given a 0-indexed array of strings words where words[i] is either a positive
 integer represented as a string or the string "prev". Start iterating from
