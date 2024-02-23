84332ed431fdfb3897cb864a5f0781e6be3e459b
Ye Gao <gaoyescm@amazon.com>
2024-02-23 14:03:41 -0500

    solve'em all (2847)

--- leetcode.cpp
+++ leetcode.cpp
@@ -62693,6 +62693,46 @@ public:
     }
 
 
+    /*2847. Smallest Number With Given Digit Product (Medium)
+    Given a positive integer n, return a string representing the smallest
+    positive integer such that the product of its digits is equal to n, or "-1"
+    if no such number exists.
+
+    Example 1:
+    Input: n = 105
+    Output: "357"
+    Explanation: 3 * 5 * 7 = 105. It can be shown that 357 is the smallest
+                 number with a product of digits equal to 105. So the answer
+                 would be "105".
+
+    Example 2:
+    Input: n = 7
+    Output: "7"
+    Explanation: Since 7 has only one digit, its product of digits would be 7.
+                 We will show that 7 is the smallest number with a product of
+                 digits equal to 7. Since the product of numbers 1 to 6 is 1 to
+                 6 respectively, so "7" would be the answer.
+
+    Example 3:
+    Input: n = 44
+    Output: "-1"
+    Explanation: It can be shown that there is no number such that its product
+                 of digits is equal to 44. So the answer would be "-1".
+
+    Constraints: 1 <= n <= 10^18*/
+
+    string smallestNumber(long long n) {
+        if (n == 1) return "1";
+        string ans;
+        for (int d = 9; d >= 2; --d)
+            for (; n && n % d == 0; n /= d)
+                ans.push_back(d+'0');
+        if (n > 1) return "-1";
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+
     /*2855. Minimum Right Shifts to Sort the Array (Easy)
     You are given a 0-indexed array nums of length n containing distinct
     positive integers. Return the minimum number of right shifts required to
--- leetcode.py
+++ leetcode.py
@@ -82244,6 +82244,45 @@ class SegTreeLazy:
         return ans
 
 
+    """2847. Smallest Number With Given Digit Product (Medium)
+    Given a positive integer n, return a string representing the smallest
+    positive integer such that the product of its digits is equal to n, or "-1"
+    if no such number exists.
+
+    Example 1:
+    Input: n = 105
+    Output: "357"
+    Explanation: 3 * 5 * 7 = 105. It can be shown that 357 is the smallest
+                 number with a product of digits equal to 105. So the answer
+                 would be "105".
+
+    Example 2:
+    Input: n = 7
+    Output: "7"
+    Explanation: Since 7 has only one digit, its product of digits would be 7.
+                 We will show that 7 is the smallest number with a product of
+                 digits equal to 7. Since the product of numbers 1 to 6 is 1 to
+                 6 respectively, so "7" would be the answer.
+
+    Example 3:
+    Input: n = 44
+    Output: "-1"
+    Explanation: It can be shown that there is no number such that its product
+                 of digits is equal to 44. So the answer would be "-1".
+
+    Constraints: 1 <= n <= 10^18"""
+
+    def smallestNumber(self, n: int) -> str:
+        if n == 1: return "1"
+        ans = []
+        for d in range(9, 1, -1):
+            while n and n % d == 0:
+                ans.append(d)
+                n //= d
+        if n > 1: return "-1"
+        return ''.join(map(str, reversed(ans)))
+
+
     """2855. Minimum Right Shifts to Sort the Array (Easy)
     You are given a 0-indexed array nums of length n containing distinct
     positive integers. Return the minimum number of right shifts required to
