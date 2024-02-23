6362fecf3aa393d096aadff7b50b8a5916c119aa
Ye Gao <gaoyescm@amazon.com>
2024-02-23 11:40:09 -0500

    solve'em all (2838)

--- leetcode.java
+++ leetcode.java
@@ -16400,6 +16400,85 @@ class SegTreeLazy {
     }
 
 
+    /*2838. Maximum Coins Heroes Can Collect (Medium)
+    There is a battle and n heroes are trying to defeat m monsters. You are
+    given two 1-indexed arrays of positive integers heroes and monsters of
+    length n and m, respectively. heroes[i] is the power of ith hero, and
+    monsters[i] is the power of ith monster. The ith hero can defeat the jth
+    monster if monsters[j] <= heroes[i]. You are also given a 1-indexed array
+    coins of length m consisting of positive integers. coins[i] is the number of
+    coins that each hero earns after defeating the ith monster. Return an array
+    ans of length n where ans[i] is the maximum number of coins that the ith
+    hero can collect from this battle.
+
+    Notes
+    * The health of a hero doesn't get reduced after defeating a monster.
+    * Multiple heroes can defeat a monster, but each monster can be defeated by
+      a given hero only once.
+
+    Example 1:
+    Input: heroes = [1,4,2], monsters = [1,1,5,2,3], coins = [2,3,4,5,6]
+    Output: [5,16,10]
+    Explanation: For each hero, we list the index of all the monsters he can
+                 defeat:
+                 - 1st hero: [1,2] since the power of this hero is 1 and
+                             monsters[1], monsters[2] <= 1. So this hero
+                             collects coins[1] + coins[2] = 5 coins.
+                 - 2nd hero: [1,2,4,5] since the power of this hero is 4 and
+                             monsters[1], monsters[2], monsters[4], monsters[5]
+                             <= 4. So this hero collects
+                             coins[1] + coins[2] + coins[4] + coins[5] = 16 coins.
+                 - 3rd hero: [1,2,4] since the power of this hero is 2 and
+                             monsters[1], monsters[2], monsters[4] <= 2. So this
+                             hero collects coins[1] + coins[2] + coins[4] = 10
+                             coins.
+                 - So the answer would be [5,16,10].
+
+    Example 2:
+    Input: heroes = [5], monsters = [2,3,1,2], coins = [10,6,5,2]
+    Output: [23]
+    Explanation: This hero can defeat all the monsters since monsters[i] <= 5.
+                 So he collects all of the coins:
+                 coins[1] + coins[2] + coins[3] + coins[4] = 23, and the answer
+                 would be [23].
+
+    Example 3:
+    Input: heroes = [4,4], monsters = [5,7,8], coins = [1,1,1]
+    Output: [0,0]
+    Explanation: In this example, no hero can defeat a monster. So the answer
+                 would be [0,0],
+
+    Constraints:
+    * 1 <= n == heroes.length <= 10^5
+    * 1 <= m == monsters.length <= 10^5
+    * coins.length == m
+    * 1 <= heroes[i], monsters[i], coins[i] <= 10^9*/
+
+    public long[] maximumCoins(int[] heroes, int[] monsters, int[] coins) {
+        int m = monsters.length, n = heroes.length;
+        int[][] mc = new int[m][2];
+        for (int i = 0; i < m; ++i) {
+            mc[i][0] = monsters[i];
+            mc[i][1] = coins[i];
+        }
+        Arrays.sort(mc, (a, b) -> Integer.compare(a[0], b[0]));
+        int[][] hi = new int[n][2];
+        for (int i = 0; i < n; ++i) {
+            hi[i][0] = heroes[i];
+            hi[i][1] = i;
+        }
+        Arrays.sort(hi, (a, b) -> Integer.compare(a[0], b[0]));
+        long[] ans = new long[n];
+        long prefix = 0;
+        for (int i = 0, j = 0; i < n; ++i) {
+            for (; j < m && mc[j][0] <= hi[i][0]; ++j)
+                prefix += mc[j][1];
+            ans[hi[i][1]] = prefix;
+        }
+        return ans;
+    }
+
+
     /*2839. Check if Strings Can be Made Equal With Operations I (Easy)
     You are given two strings s1 and s2, both of length 4, consisting of 
     lowercase English letters. You can apply the following operation on any of 
--- leetcode.js
+++ leetcode.js
@@ -1348,6 +1348,80 @@ var maximumLengthOfRanges = function(nums) {
 };
 
 
+/*2838. Maximum Coins Heroes Can Collect (Medium)
+There is a battle and n heroes are trying to defeat m monsters. You are
+given two 1-indexed arrays of positive integers heroes and monsters of
+length n and m, respectively. heroes[i] is the power of ith hero, and
+monsters[i] is the power of ith monster. The ith hero can defeat the jth
+monster if monsters[j] <= heroes[i]. You are also given a 1-indexed array
+coins of length m consisting of positive integers. coins[i] is the number of
+coins that each hero earns after defeating the ith monster. Return an array
+ans of length n where ans[i] is the maximum number of coins that the ith
+hero can collect from this battle.
+
+Notes
+* The health of a hero doesn't get reduced after defeating a monster.
+* Multiple heroes can defeat a monster, but each monster can be defeated by
+  a given hero only once.
+
+Example 1:
+Input: heroes = [1,4,2], monsters = [1,1,5,2,3], coins = [2,3,4,5,6]
+Output: [5,16,10]
+Explanation: For each hero, we list the index of all the monsters he can
+             defeat:
+             - 1st hero: [1,2] since the power of this hero is 1 and
+                         monsters[1], monsters[2] <= 1. So this hero
+                         collects coins[1] + coins[2] = 5 coins.
+             - 2nd hero: [1,2,4,5] since the power of this hero is 4 and
+                         monsters[1], monsters[2], monsters[4], monsters[5]
+                         <= 4. So this hero collects
+                         coins[1] + coins[2] + coins[4] + coins[5] = 16 coins.
+             - 3rd hero: [1,2,4] since the power of this hero is 2 and
+                         monsters[1], monsters[2], monsters[4] <= 2. So this
+                         hero collects coins[1] + coins[2] + coins[4] = 10
+                         coins.
+             - So the answer would be [5,16,10].
+
+Example 2:
+Input: heroes = [5], monsters = [2,3,1,2], coins = [10,6,5,2]
+Output: [23]
+Explanation: This hero can defeat all the monsters since monsters[i] <= 5.
+             So he collects all of the coins:
+             coins[1] + coins[2] + coins[3] + coins[4] = 23, and the answer
+             would be [23].
+
+Example 3:
+Input: heroes = [4,4], monsters = [5,7,8], coins = [1,1,1]
+Output: [0,0]
+Explanation: In this example, no hero can defeat a monster. So the answer
+             would be [0,0],
+
+Constraints:
+* 1 <= n == heroes.length <= 10^5
+* 1 <= m == monsters.length <= 10^5
+* coins.length == m
+* 1 <= heroes[i], monsters[i], coins[i] <= 10^9*/
+
+var maximumCoins = function(heroes, monsters, coins) {
+    const mc = [];
+    for (const i in monsters)
+        mc.push([monsters[i], coins[i]]);
+    mc.sort((a, b) => (a[0] - b[0]));
+    const hi = [];
+    for (const [i, x] of heroes.entries())
+        hi.push([x, i]);
+    hi.sort((a, b) => (a[0] - b[0]));
+    const ans = Array(heroes.length);
+    let j = 0, prefix = 0;
+    for (const [x, i] of hi) {
+        for(; j < mc.length && mc[j][0] <= x; ++j)
+            prefix += mc[j][1];
+        ans[i] = prefix;
+    }
+    return ans;
+};
+
+
 /*2855. Minimum Right Shifts to Sort the Array (Easy)
 You are given a 0-indexed array nums of length n containing distinct
 positive integers. Return the minimum number of right shifts required to
