27332e6ad211061de12160dc18ca025e61ee4acc
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-02-24 11:03:09 -0500

    solve'em all (2936)

--- leetcode.ts
+++ leetcode.ts
@@ -1828,6 +1828,65 @@ function maximumStrongPairXor(nums: number[]): number {
 };
 
 
+/*2936. Number of Equal Numbers Blocks (Medium)
+You are given a 0-indexed array of integers, nums. The following property
+holds for nums:
+* All occurrences of a value are adjacent. In other words, if there are two
+  indices i < j such that nums[i] == nums[j], then for every index k that
+  i < k < j, nums[k] == nums[i].
+Since nums is a very large array, you are given an instance of the class
+BigArray which has the following functions:
+* int at(long long index): Returns the value of nums[i].
+* void size(): Returns nums.length.
+Let's partition the array into maximal blocks such that each block contains
+equal values. Return the number of these blocks. Note that if you want to
+test your solution using a custom test, behavior for tests with
+nums.length > 10 is undefined.
+
+Example 1:
+Input: nums = [3,3,3,3,3]
+Output: 1
+Explanation: There is only one block here which is the whole array (because
+             all numbers are equal) and that is: [3,3,3,3,3]. So the answer
+             would be 1.
+
+Example 2:
+Input: nums = [1,1,1,3,9,9,9,2,10,10]
+Output: 5
+Explanation: There are 5 blocks here:
+Block number 1: [1,1,1,3,9,9,9,2,10,10]
+Block number 2: [1,1,1,3,9,9,9,2,10,10]
+Block number 3: [1,1,1,3,9,9,9,2,10,10]
+Block number 4: [1,1,1,3,9,9,9,2,10,10]
+Block number 5: [1,1,1,3,9,9,9,2,10,10]
+So the answer would be 5.
+
+Example 3:
+Input: nums = [1,2,3,4,5,6,7]
+Output: 7
+Explanation: Since all numbers are distinct, there are 7 blocks here and
+             each element representing one block. So the answer would be 7.
+
+Constraints:
+* 1 <= nums.length <= 10^15
+* 1 <= nums[i] <= 10^9
+* The input is generated such that all equal values are adjacent.
+* The sum of the elements of nums is at most 10^15.*/
+
+function countBlocks(nums: BigArray | null): number {
+
+    function fn(lo, hi) {
+        if (lo == hi || nums.at(lo) == nums.at(hi)) return 1;
+        const mid = lo + Math.floor((hi - lo)/2);
+        let ans = fn(lo, mid) + fn(mid+1, hi);
+        if (nums.at(mid) == nums.at(mid+1)) --ans;
+        return ans;
+    }
+
+    return fn(0, nums.size()-1);
+};
+
+
 /*2942. Find Words Containing Character (Easy)
 You are given a 0-indexed array of strings words and a character x. Return
 an array of indices representing the words that contain the character x.
