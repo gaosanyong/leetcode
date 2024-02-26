8a907f3a5f514f4b75930e6eb7cb6cdd6e8b032d
Ye Gao <gaoyescm@amazon.com>
2024-02-26 12:40:13 -0500

    solve'em all (2892)

--- leetcode.java
+++ leetcode.java
@@ -18081,6 +18081,50 @@ class SegTreeLazy {
     }
 
 
+    /*2892. Minimizing Array After Replacing Pairs With Their Product (Medium)
+    Given an integer array nums and an integer k, you can perform the following
+    operation on the array any number of times:
+    * Select two adjacent elements of the array like x and y, such that
+      x * y <= k, and replace both of them with a single element with value
+      x * y (e.g. in one operation the array [1, 2, 2, 3] with k = 5 can become
+      [1, 4, 3] or [2, 2, 3], but can't become [1, 2, 6]).
+    Return the minimum possible length of nums after any number of operations.
+
+    Example 1:
+    Input: nums = [2,3,3,7,3,5], k = 20
+    Output: 3
+    Explanation: We perform these operations:
+                 1. [2,3,3,7,3,5] -> [6,3,7,3,5]
+                 2. [6,3,7,3,5] -> [18,7,3,5]
+                 3. [18,7,3,5] -> [18,7,15]
+                 It can be shown that 3 is the minimum length possible to
+                 achieve with the given operation.
+
+    Example 2:
+    Input: nums = [3,3,3,3], k = 6
+    Output: 4
+    Explanation: We can't perform any operations since the product of every two
+                 adjacent elements is greater than 6. Hence, the answer is 4.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 0 <= nums[i] <= 10^9
+    * 1 <= k <= 10^9*/
+
+    public int minArrayLength(int[] nums, int k) {
+        int ans = 0;
+        long prefix = 0;
+        for (var x : nums)
+            if (x == 0) return 1;
+            else if (prefix > 0 && prefix*x <= k) prefix *= x;
+            else {
+                ++ans;
+                prefix = x;
+            }
+        return ans;
+    }
+
+
     /*2894. Divisible and Non-divisible Sums Difference (Easy)
     You are given positive integers n and m. Define two integers, num1 and num2, 
     as follows:
--- leetcode.ts
+++ leetcode.ts
@@ -1598,6 +1598,49 @@ function maxSubarrayLength(nums: number[]): number {
 };
 
 
+/*2892. Minimizing Array After Replacing Pairs With Their Product (Medium)
+Given an integer array nums and an integer k, you can perform the following
+operation on the array any number of times:
+* Select two adjacent elements of the array like x and y, such that
+  x * y <= k, and replace both of them with a single element with value
+  x * y (e.g. in one operation the array [1, 2, 2, 3] with k = 5 can become
+  [1, 4, 3] or [2, 2, 3], but can't become [1, 2, 6]).
+Return the minimum possible length of nums after any number of operations.
+
+Example 1:
+Input: nums = [2,3,3,7,3,5], k = 20
+Output: 3
+Explanation: We perform these operations:
+             1. [2,3,3,7,3,5] -> [6,3,7,3,5]
+             2. [6,3,7,3,5] -> [18,7,3,5]
+             3. [18,7,3,5] -> [18,7,15]
+             It can be shown that 3 is the minimum length possible to
+             achieve with the given operation.
+
+Example 2:
+Input: nums = [3,3,3,3], k = 6
+Output: 4
+Explanation: We can't perform any operations since the product of every two
+             adjacent elements is greater than 6. Hence, the answer is 4.
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* 0 <= nums[i] <= 10^9
+* 1 <= k <= 10^9*/
+
+function minArrayLength(nums: number[], k: number): number {
+    let ans = 0, prefix = 0;
+    for (const x of nums)
+        if (x == 0) return 1;
+        else if (prefix && prefix*x <= k) prefix *= x;
+        else {
+            ++ans;
+            prefix = x;
+        }
+    return ans;
+};
+
+
 /*2898. Maximum Linear Stock Score (Medium)
 Given a 1-indexed integer array prices, where prices[i] is the price of a
 particular stock on the ith day, your task is to select some of the elements
