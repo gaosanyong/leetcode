bf21e67c590a4e662b03c2d5820c420676d4f2e0
Ye Gao <gaoyescm@amazon.com>
2024-02-26 22:19:54 -0500

    solve'em all (2964)

--- leetcode.cpp
+++ leetcode.cpp
@@ -65545,6 +65545,48 @@ public:
     }
 
 
+    /*2964. Number of Divisible Triplet Sums (Medium)
+    Given a 0-indexed integer array nums and an integer d, return the number of
+    triplets (i, j, k) such that i < j < k and
+    (nums[i] + nums[j] + nums[k]) % d == 0.
+
+    Example 1:
+    Input: nums = [3,3,4,7,8], d = 5
+    Output: 3
+    Explanation: The triplets which are divisible by 5 are: (0, 1, 2),
+                 (0, 2, 4), (1, 2, 4). It can be shown that no other triplet is
+                 divisible by 5. Hence, the answer is 3.
+
+    Example 2:
+    Input: nums = [3,3,3,3], d = 3
+    Output: 4
+    Explanation: Any triplet chosen here has a sum of 9, which is divisible by
+                 3. Hence, the answer is the total number of triplets which is 4.
+
+    Example 3:
+    Input: nums = [3,3,3,3], d = 6
+    Output: 0
+    Explanation: Any triplet chosen here has a sum of 9, which is not divisible
+                 by 6. Hence, the answer is 0.
+
+    Constraints:
+    * 1 <= nums.length <= 1000
+    * 1 <= nums[i] <= 10^9
+    * 1 <= d <= 10^9*/
+
+    int divisibleTripletCount(vector<int>& nums, int d) {
+        int ans = 0;
+        for (int i = 0, n = nums.size(); i < n; ++i) {
+            unordered_map<int, int> freq;
+            for (int ii = i+1; ii < n; ++ii) {
+                ans += freq[(d - nums[ii]%d) % d];
+                ++freq[(nums[i] + nums[ii]) % d];
+            }
+        }
+        return ans;
+    }
+
+
     /*2965. Find Missing and Repeated Values (Easy)
     You are given a 0-indexed 2D integer matrix grid of size n * n with values
     in the range [1, n2]. Each integer appears exactly once except a which
