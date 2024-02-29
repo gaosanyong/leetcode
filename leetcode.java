6cf804bd4d3455cbc12abe062bb6321deaa0c204
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-02-28 23:05:40 -0500

    solve'em all (3064)

--- leetcode.java
+++ leetcode.java
@@ -21468,6 +21468,40 @@ class SegTreeLazy {
         }
         return ans;
     }
+
+
+    /*3064. Guess the Number Using Bitwise Questions I (Medium)
+    There is a number n that you have to find. There is also a pre-defined API
+    int commonSetBits(int num), which returns the number of bits where both n
+    and num are 1 in that position of their binary representation. In other
+    words, it returns the number of set bits in n & num, where & is the bitwise
+    AND operator. Return the number n.
+
+    Example 1:
+    Input: n = 31
+    Output: 31
+    Explanation: It can be proven that it's possible to find 31 using the
+                 provided API.
+
+    Example 2:
+    Input: n = 33
+    Output: 33
+    Explanation: It can be proven that it's possible to find 33 using the
+                 provided API.
+
+    Constraints:
+    * 1 <= n <= 2^30 - 1
+    * 0 <= num <= 2^30 - 1
+    * If you ask for some num out of the given range, the output wouldn't be
+      reliable.*/
+
+    public int findNumber() {
+        int ans = 0;
+        for (int i = 0; i < 30; ++i)
+            if (commonSetBits(1<<i) > 0)
+                ans ^= 1<<i;
+        return ans;
+    }
 }
 
 
