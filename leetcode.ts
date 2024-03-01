59e4fe909397f0e63e8db52dc296122a5545fabe
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-03-01 14:35:51 -0500

    solve'em all (2864)

--- leetcode.ts
+++ leetcode.ts
@@ -1697,6 +1697,38 @@ function maxSubarrayLength(nums: number[]): number {
 };
 
 
+/*2864. Maximum Odd Binary Number (Easy)
+You are given a binary string s that contains at least one '1'. You have to
+rearrange the bits in such a way that the resulting binary number is the
+maximum odd binary number that can be created from this combination. Return
+a string representing the maximum odd binary number that can be created
+from the given combination. Note that the resulting string can have leading
+zeros.
+
+Example 1:
+Input: s = "010"
+Output: "001"
+Explanation: Because there is just one '1', it must be in the last position.
+             So the answer is "001".
+
+Example 2:
+Input: s = "0101"
+Output: "1001"
+Explanation: One of the '1's must be in the last position. The maximum
+             number that can be made with the remaining digits is "100". So
+             the answer is "1001".
+
+Constraints:
+* 1 <= s.length <= 100
+* s consists only of '0' and '1'.
+* s contains at least one '1'.*/
+
+function maximumOddBinaryNumber(s: string): string {
+    const ones = s.split('').filter(ch => ch == '1').length;
+    return '1'.repeat(ones-1) + '0'.repeat(s.length - ones) + '1';
+};
+
+
 /*2892. Minimizing Array After Replacing Pairs With Their Product (Medium)
 Given an integer array nums and an integer k, you can perform the following
 operation on the array any number of times:
