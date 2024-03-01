b37fb05e27ad973e7b229c46d1bba07b8220621a
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-02-29 23:06:16 -0500

    solve'em all (2979)

--- leetcode.cpp
+++ leetcode.cpp
@@ -65890,6 +65890,39 @@ public:
     }
 
 
+    /*2979. Most Expensive Item That Can Not Be Bought (Medium)
+    You are given two distinct prime numbers primeOne and primeTwo. Alice and
+    Bob are visiting a market. The market has an infinite number of items, for
+    any positive integer x there exists an item whose price is x. Alice wants to
+    buy some items from the market to gift to Bob. She has an infinite number of
+    coins in the denomination primeOne and primeTwo. She wants to know the most
+    expensive item she can not buy to gift to Bob. Return the price of the most
+    expensive item which Alice can not gift to Bob.
+
+    Example 1:
+    Input: primeOne = 2, primeTwo = 5
+    Output: 3
+    Explanation: The prices of items which cannot be bought are [1,3]. It can be
+                 shown that all items with a price greater than 3 can be bought
+                 using a combination of coins of denominations 2 and 5.
+
+    Example 2:
+    Input: primeOne = 5, primeTwo = 7
+    Output: 23
+    Explanation: The prices of items which cannot be bought are
+                 [1,2,3,4,6,8,9,11,13,16,18,23]. It can be shown that all items
+                 with a price greater than 23 can be bought.
+
+    Constraints:
+    * 1 < primeOne, primeTwo < 10^4
+    * primeOne, primeTwo are prime numbers.
+    * primeOne * primeTwo < 10^5*/
+
+    int mostExpensiveItem(int primeOne, int primeTwo) {
+        return primeOne * primeTwo - primeOne - primeTwo;
+    }
+
+
     /*2992. Number of Self-Divisible Permutations (Medium)
     Given an integer n, return the number of permutations of the 1-indexed array
     nums = [1, 2, ..., n], such that it's self-divisible. A 1-indexed array a of
