6ef4653d6b7c43e71010f5a6ff25e9bef7c890e7
Ye Gao <gaoyescm@amazon.com>
2024-03-04 23:10:55 -0500

    solve'em all (3050)

--- leetcode-pandas.py
+++ leetcode-pandas.py
@@ -2457,3 +2457,76 @@ def friday_purchases(purchases: pd.DataFrame) -> pd.DataFrame:
     ).sort_values(
         by = 'week_of_month'
     )
+
+
+"""3050. Pizza Toppings Cost Analysis (Medium)
+SQL Schema
+Table: Toppings
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| topping_name | varchar |
+| cost         | decimal |
++--------------+---------+
+topping_name is the primary key for this table. Each row of this table contains
+topping name and the cost of the topping. Write a solution to calculate the
+total cost of all possible 3-topping pizza combinations from a given list of
+toppings. The total cost of toppings must be rounded to 2 decimal places.
+
+Note:
+* Do not include the pizzas where a topping is repeated. For example,
+  ‘Pepperoni, Pepperoni, Onion Pizza’.
+* Toppings must be listed in alphabetical order. For example,
+  'Chicken, Onions, Sausage'. 'Onion, Sausage, Chicken' is not acceptable.
+Return the result table ordered by total cost in descending order and
+combination of toppings in ascending order. The result format is in the
+following example.
+
+Example 1:
+Input:
+Toppings table:
++--------------+------+
+| topping_name | cost |
++--------------+------+
+| Pepperoni    | 0.50 |
+| Sausage      | 0.70 |
+| Chicken      | 0.55 |
+| Extra Cheese | 0.40 |
++--------------+------+
+Output:
++--------------------------------+------------+
+| pizza                          | total_cost |
++--------------------------------+------------+
+| Chicken,Pepperoni,Sausage      | 1.75       |
+| Chicken,Extra Cheese,Sausage   | 1.65       |
+| Extra Cheese,Pepperoni,Sausage | 1.60       |
+| Chicken,Extra Cheese,Pepperoni | 1.45       |
++--------------------------------+------------+
+Explanation:
+There are only four different combinations possible with the three topings:
+- Chicken, Pepperoni, Sausage: Total cost is $1.75 (Chicken $0.55, Pepperoni
+  $0.50, Sausage $0.70).
+- Chicken, Extra Cheese, Sausage: Total cost is $1.65 (Chicken $0.55, Extra
+  Cheese $0.40, Sausage $0.70).
+- Extra Cheese, Pepperoni, Sausage: Total cost is $1.60 (Extra Cheese $0.40,
+  Pepperoni $0.50, Sausage $0.70).
+- Chicken, Extra Cheese, Pepperoni: Total cost is $1.45 (Chicken $0.55, Extra
+  Cheese $0.40, Pepperoni $0.50).
+Output table is ordered by the total cost in descending order."""
+
+def cost_analysis(toppings: pd.DataFrame) -> pd.DataFrame:
+    return toppings.merge(
+        right = toppings,
+        how = 'cross'
+    ).merge(
+        right = toppings,
+        how = 'cross'
+    ).query(
+        expr = 'topping_name_x < topping_name_y & topping_name_y < topping_name'
+    ).assign(
+        pizza = lambda x : x['topping_name_x'] + ',' + x['topping_name_y'] + ',' + x['topping_name'],
+        total_cost = lambda x : round(x['cost_x'] + x['cost_y'] + x['cost'], 2)
+    ).sort_values(
+        by = ['total_cost', 'pizza'],
+        ascending = [False, True]
+    )[['pizza', 'total_cost']]
--- leetcode.sql
+++ leetcode.sql
@@ -9875,3 +9875,69 @@ FROM
 WHERE WEEKDAY(purchase_date) = 4
 GROUP BY 1
 ORDER BY 1
+
+
+/*3050. Pizza Toppings Cost Analysis (Medium)
+SQL Schema
+Table: Toppings
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| topping_name | varchar |
+| cost         | decimal |
++--------------+---------+
+topping_name is the primary key for this table. Each row of this table contains
+topping name and the cost of the topping. Write a solution to calculate the
+total cost of all possible 3-topping pizza combinations from a given list of
+toppings. The total cost of toppings must be rounded to 2 decimal places.
+
+Note:
+* Do not include the pizzas where a topping is repeated. For example,
+  ‘Pepperoni, Pepperoni, Onion Pizza’.
+* Toppings must be listed in alphabetical order. For example,
+  'Chicken, Onions, Sausage'. 'Onion, Sausage, Chicken' is not acceptable.
+Return the result table ordered by total cost in descending order and
+combination of toppings in ascending order. The result format is in the
+following example.
+
+Example 1:
+Input:
+Toppings table:
++--------------+------+
+| topping_name | cost |
++--------------+------+
+| Pepperoni    | 0.50 |
+| Sausage      | 0.70 |
+| Chicken      | 0.55 |
+| Extra Cheese | 0.40 |
++--------------+------+
+Output:
++--------------------------------+------------+
+| pizza                          | total_cost |
++--------------------------------+------------+
+| Chicken,Pepperoni,Sausage      | 1.75       |
+| Chicken,Extra Cheese,Sausage   | 1.65       |
+| Extra Cheese,Pepperoni,Sausage | 1.60       |
+| Chicken,Extra Cheese,Pepperoni | 1.45       |
++--------------------------------+------------+
+Explanation:
+There are only four different combinations possible with the three topings:
+- Chicken, Pepperoni, Sausage: Total cost is $1.75 (Chicken $0.55, Pepperoni
+  $0.50, Sausage $0.70).
+- Chicken, Extra Cheese, Sausage: Total cost is $1.65 (Chicken $0.55, Extra
+  Cheese $0.40, Sausage $0.70).
+- Extra Cheese, Pepperoni, Sausage: Total cost is $1.60 (Extra Cheese $0.40,
+  Pepperoni $0.50, Sausage $0.70).
+- Chicken, Extra Cheese, Pepperoni: Total cost is $1.45 (Chicken $0.55, Extra
+  Cheese $0.40, Pepperoni $0.50).
+Output table is ordered by the total cost in descending order.*/
+
+SELECT
+    CONCAT(a.topping_name, ',', b.topping_name, ',', c.topping_name) AS pizza,
+    ROUND(a.cost + b.cost + c.cost, 2) AS total_cost
+FROM
+    Toppings a,
+    Toppings b,
+    Toppings c
+WHERE a.topping_name < b.topping_name AND b.topping_name < c.topping_name
+ORDER by 2 DESC, 1 ASC
