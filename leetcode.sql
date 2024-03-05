7c7c278110e9c2327877512766605990865dd391
Ye Gao <gaoyescm@amazon.com>
2024-03-05 18:35:08 -0500

    solve'em all (3054)

--- leetcode.sql
+++ leetcode.sql
@@ -9941,3 +9941,64 @@ FROM
     Toppings c
 WHERE a.topping_name < b.topping_name AND b.topping_name < c.topping_name
 ORDER by 2 DESC, 1 ASC
+
+
+/*3054. Binary Tree Nodes (Medium)
+SQL Schema
+Table: Tree
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| N           | int  |
+| P           | int  |
++-------------+------+
+N is the column of unique values for this table. Each row includes N and P,
+where N represents the value of a node in Binary Tree, and P is the parent of N.
+Write a solution to find the node type of the Binary Tree. Output one of the
+following for each node:
+* Root: if the node is the root node.
+* Leaf: if the node is the leaf node.
+* Inner: if the node is neither root nor leaf node.
+Return the result table ordered by node value in ascending order. The result
+format is in the following example.
+
+Example 1:
+Input:
+Tree table:
++---+------+
+| N | P    |
++---+------+
+| 1 | 2    |
+| 3 | 2    |
+| 6 | 8    |
+| 9 | 8    |
+| 2 | 5    |
+| 8 | 5    |
+| 5 | null |
++---+------+
+Output:
++---+-------+
+| N | Type  |
++---+-------+
+| 1 | Leaf  |
+| 2 | Inner |
+| 3 | Leaf  |
+| 5 | Root  |
+| 6 | Leaf  |
+| 8 | Inner |
+| 9 | Leaf  |
++---+-------+
+Explanation:
+- Node 5 is the root node since it has no parent node.
+- Nodes 1, 3, 6, and 8 are leaf nodes because they don't have any child nodes.
+- Nodes 2, 4, and 7 are inner nodes as they serve as parents to some of the
+  nodes in the structure.*/
+
+SELECT
+    N,
+    CASE
+        WHEN P IS NULL THEN "Root"
+        WHEN N IN (SELECT DISTINCT P FROM Tree) THEN "Inner"
+        ELSE "Leaf" END AS Type
+FROM Tree
+ORDER BY 1
