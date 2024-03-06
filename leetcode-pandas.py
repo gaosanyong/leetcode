1dff78ed6c3d33762806c7668be0f1ba71a508d6
Ye Gao <gaoyescm@amazon.com>
2024-03-06 11:29:40 -0500

    solve'em all (2820)

--- leetcode-pandas.py
+++ leetcode-pandas.py
@@ -1954,6 +1954,82 @@ def count_occurrences(files: pd.DataFrame) -> pd.DataFrame:
     return pd.DataFrame({"word" : ["bull", "bear"], "count" : [bulls, bears]})
 
 
+"""2820. Election Results (Medium)
+SQL Schema
+Table: Votes
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| voter       | varchar |
+| candidate   | varchar |
++-------------+---------+
+(voter, candidate) is the primary key (combination of unique values) for this
+table. Each row of this table contains name of the voter and their candidate.
+The election is conducted in a city where everyone can vote for one or more
+candidates or choose not to vote. Each person has 1 vote so if they vote for
+multiple candidates, their vote gets equally split across them. For example, if
+a person votes for 2 candidates, these candidates receive an equivalent of 0.5
+votes each. Write a solution to find candidate who got the most votes and won
+the election. Output the name of the candidate or If multiple candidates have an
+equal number of votes, display the names of all of them. Return the result table
+ordered by candidate in ascending order. The result format is in the following
+example.
+
+Example 1:
+Input:
+Votes table:
++----------+-----------+
+| voter    | candidate |
++----------+-----------+
+| Kathy    | null      |
+| Charles  | Ryan      |
+| Charles  | Christine |
+| Charles  | Kathy     |
+| Benjamin | Christine |
+| Anthony  | Ryan      |
+| Edward   | Ryan      |
+| Terry    | null      |
+| Evelyn   | Kathy     |
+| Arthur   | Christine |
++----------+-----------+
+Output:
++-----------+
+| candidate |
++-----------+
+| Christine |
+| Ryan      |
++-----------+
+Explanation:
+- Kathy and Terry opted not to participate in voting, resulting in their votes
+  being recorded as 0. Charles distributed his vote among three candidates,
+  equating to 0.33 for each candidate. On the other hand, Benjamin, Arthur,
+  Anthony, Edward, and Evelyn each cast their votes for a single candidate.
+- Collectively, Candidate Ryan and Christine amassed a total of 2.33 votes,
+  while Kathy received a combined total of 1.33 votes.
+Since Ryan and Christine received an equal number of votes, we will display
+their names in ascending order."""
+
+def get_election_results(votes: pd.DataFrame) -> pd.DataFrame:
+    return votes.merge(
+        right = votes.dropna().groupby(
+            by = "voter",
+            as_index = False
+        ).agg(
+            vote = ("voter", lambda x : 1/x.count())
+        ),
+        on = "voter"
+    ).groupby(
+        by = "candidate",
+        as_index = False
+    ).agg(
+        vote = ("vote", "sum")
+    ).query(
+        expr = 'vote == vote.max()'
+    ).sort_values(
+        by = 'candidate'
+    )[['candidate']]
+
+
 """2837. Total Traveled Distance (Easy)
 SQL Schema
 Table: Users
--- leetcode.sql
+++ leetcode.sql
@@ -9391,6 +9391,80 @@ FROM Files
 WHERE content LIKE '% bear %'
 
 
+/*2820. Election Results (Medium)
+SQL Schema
+Table: Votes
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| voter       | varchar |
+| candidate   | varchar |
++-------------+---------+
+(voter, candidate) is the primary key (combination of unique values) for this
+table. Each row of this table contains name of the voter and their candidate.
+The election is conducted in a city where everyone can vote for one or more
+candidates or choose not to vote. Each person has 1 vote so if they vote for
+multiple candidates, their vote gets equally split across them. For example, if
+a person votes for 2 candidates, these candidates receive an equivalent of 0.5
+votes each. Write a solution to find candidate who got the most votes and won
+the election. Output the name of the candidate or If multiple candidates have an
+equal number of votes, display the names of all of them. Return the result table
+ordered by candidate in ascending order. The result format is in the following
+example.
+
+Example 1:
+Input:
+Votes table:
++----------+-----------+
+| voter    | candidate |
++----------+-----------+
+| Kathy    | null      |
+| Charles  | Ryan      |
+| Charles  | Christine |
+| Charles  | Kathy     |
+| Benjamin | Christine |
+| Anthony  | Ryan      |
+| Edward   | Ryan      |
+| Terry    | null      |
+| Evelyn   | Kathy     |
+| Arthur   | Christine |
++----------+-----------+
+Output:
++-----------+
+| candidate |
++-----------+
+| Christine |
+| Ryan      |
++-----------+
+Explanation:
+- Kathy and Terry opted not to participate in voting, resulting in their votes
+  being recorded as 0. Charles distributed his vote among three candidates,
+  equating to 0.33 for each candidate. On the other hand, Benjamin, Arthur,
+  Anthony, Edward, and Evelyn each cast their votes for a single candidate.
+- Collectively, Candidate Ryan and Christine amassed a total of 2.33 votes,
+  while Kathy received a combined total of 1.33 votes.
+Since Ryan and Christine received an equal number of votes, we will display
+their names in ascending order.*/
+
+WITH Cte0 AS (
+    SELECT
+        candidate,
+        1/COUNT(candidate) OVER (PARTITION BY voter) AS vote
+    FROM Votes
+    WHERE candidate IS NOT NULL
+), Cte1 AS (
+    SELECT
+        candidate,
+        RANK() OVER (ORDER BY SUM(vote) DESC) AS rnk
+    FROM Cte0
+    GROUP BY 1
+)
+SELECT candidate
+FROM Cte1
+WHERE rnk = 1
+ORDER BY 1
+
+
 /*2837. Total Traveled Distance (Easy)
 SQL Schema
 Table: Users
