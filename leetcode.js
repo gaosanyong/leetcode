92748c1833662ec92b23505d77a1d42fecc50482
Ye Gao <gaoyescm@amazon.com>
2024-03-06 15:26:28 -0500

    solve'em all (141)

--- leetcode.js
+++ leetcode.js
@@ -59,6 +59,49 @@ var isSameTree = function(p, q) {
 };
 
 
+/*141. Linked List Cycle (Easy)
+Given head, the head of a linked list, determine if the linked list has a
+cycle in it. There is a cycle in a linked list if there is some node in the
+list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next
+pointer is connected to. Note that pos is not passed as a parameter. Return
+true if there is a cycle in the linked list. Otherwise, return false.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to
+             the 1st node (0-indexed).
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to
+             the 0th node.
+
+Example 3:
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+Constraints:
+* The number of the nodes in the list is in the range [0, 10^4].
+* -10^5 <= Node.val <= 10^5
+* pos is -1 or a valid index in the linked-list.
+
+Follow up: Can you solve it using O(1) (i.e. constant) memory?*/
+
+var hasCycle = function(head) {
+    let fast = head, slow = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast == slow) return true;
+    }
+    return false;
+};
+
+
 /*169. Majority Element (Easy)
 Given an array nums of size n, return the majority element. The majority
 element is the element that appears more than ⌊n / 2⌋ times. You may assume
