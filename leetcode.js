176a4ca1b13fbcda7ce867c406e08c8c0a76f470
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-03-09 16:07:23 -0500

    solve'em all (2540)

--- leetcode.js
+++ leetcode.js
@@ -571,6 +571,40 @@ var rearrangeArray = function(nums) {
 };
 
 
+/*2540. Minimum Common Value (Easy)
+Given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+return the minimum integer common to both arrays. If there is no common
+integer amongst nums1 and nums2, return -1. Note that an integer is said
+to be common to nums1 and nums2 if both arrays have at least one
+occurrence of that integer.
+
+Example 1:
+Input: nums1 = [1,2,3], nums2 = [2,4]
+Output: 2
+Explanation: The smallest element common to both arrays is 2, so we return
+             2.
+
+Example 2:
+Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
+Output: 2
+Explanation: There are two common elements in the array 2 and 3 out of
+             which 2 is the smallest, so 2 is returned.
+
+Constraints:
+* 1 <= nums1.length, nums2.length <= 10^5
+* 1 <= nums1[i], nums2[j] <= 10^9
+* Both nums1 and nums2 are sorted in non-decreasing order.*/
+
+var getCommon = function(nums1, nums2) {
+    for (let i = 0, ii = 0; i < nums1.length && ii < nums2.length; ) {
+        if (nums1[i] < nums2[ii]) ++i;
+        else if (nums1[i] == nums2[ii]) return nums1[i];
+        else ++ii;
+    }
+    return -1;
+};
+
+
 /*2628. JSON Deep Equal (Medium)
 Given two values o1 and o2, return a boolean value indicating whether two
 values, o1 and o2, are deeply equal. For two values to be deeply equal, the
