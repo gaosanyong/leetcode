6e819e35b701fc97e15381b93e5a58f58bf2e339
Ye Gao <gaoyescm@amazon.com>
2024-03-12 18:39:16 -0400

    solve'em all (3053)

--- leetcode-pandas.py
+++ leetcode-pandas.py
@@ -2742,6 +2742,61 @@ def find_candidates(candidates: pd.DataFrame) -> pd.DataFrame:
     )[['candidate_id']]
 
 
+"""3053. Classifying Triangles by Lengths (Easy)
+SQL Schema
+Table: Triangles
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| A           | int  |
+| B           | int  |
+| C           | int  |
++-------------+------+
+(A, B, C) is the primary key for this table. Each row include the lengths of
+each of a triangle's three sides. Write a query to find the type of triangle.
+Output one of the following for each row:
+* Equilateral: It's a triangle with 3 sides of equal length.
+* Isosceles: It's a triangle with 2 sides of equal length.
+* Scalene: It's a triangle with 3 sides of differing lengths.
+* Not A Triangle: The given values of A, B, and C don't form a triangle.
+Return the result table in any order. The result format is in the following
+example.
+
+Example 1:
+Input:
+Triangles table:
++----+----+----+
+| A  | B  | C  |
++----+----+----+
+| 20 | 20 | 23 |
+| 20 | 20 | 20 |
+| 20 | 21 | 22 |
+| 13 | 14 | 30 |
++----+----+----+
+Output:
++----------------+
+| triangle_type  |
++----------------+
+| Isosceles      |
+| Equilateral    |
+| Scalene        |
+| Not A Triangle |
++----------------+
+Explanation:
+- Values in the first row from an Isosceles triangle, because A = B.
+- Values in the second row from an Equilateral triangle, because A = B = C.
+- Values in the third row from an Scalene triangle, because A != B != C.
+- Values in the fourth row cannot form a triangle, because the combined value of
+  sides A and B is not larger than that of side C."""
+
+def type_of_triangle(triangles: pd.DataFrame) -> pd.DataFrame:
+    return triangles.assign(
+        triangle_type = lambda x: np.where((x['A'] == x['B']) & (x['B'] == x['C']), 'Equilateral',
+                                  np.where((x['A']+x['B'] <= x['C']) | (x['B']+x['C'] <= x['A']) | (x['C']+x['A'] <= x['B']), 'Not A Triangle',
+                                  np.where((x['A'] == x['B']) | (x['B'] == x['C']) | (x['C'] == x['A']), 'Isosceles', 'Scalene')))
+    )[['triangle_type']]
+
+
 """3054. Binary Tree Nodes (Medium)
 SQL Schema
 Table: Tree
--- leetcode.sql
+++ leetcode.sql
@@ -10200,6 +10200,62 @@ HAVING COUNT(skill) = 3
 ORDER BY 1
 
 
+/*3053. Classifying Triangles by Lengths (Easy)
+SQL Schema
+Table: Triangles
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| A           | int  |
+| B           | int  |
+| C           | int  |
++-------------+------+
+(A, B, C) is the primary key for this table. Each row include the lengths of
+each of a triangle's three sides. Write a query to find the type of triangle.
+Output one of the following for each row:
+* Equilateral: It's a triangle with 3 sides of equal length.
+* Isosceles: It's a triangle with 2 sides of equal length.
+* Scalene: It's a triangle with 3 sides of differing lengths.
+* Not A Triangle: The given values of A, B, and C don't form a triangle.
+Return the result table in any order. The result format is in the following
+example.
+
+Example 1:
+Input:
+Triangles table:
++----+----+----+
+| A  | B  | C  |
++----+----+----+
+| 20 | 20 | 23 |
+| 20 | 20 | 20 |
+| 20 | 21 | 22 |
+| 13 | 14 | 30 |
++----+----+----+
+Output:
++----------------+
+| triangle_type  |
++----------------+
+| Isosceles      |
+| Equilateral    |
+| Scalene        |
+| Not A Triangle |
++----------------+
+Explanation:
+- Values in the first row from an Isosceles triangle, because A = B.
+- Values in the second row from an Equilateral triangle, because A = B = C.
+- Values in the third row from an Scalene triangle, because A != B != C.
+- Values in the fourth row cannot form a triangle, because the combined value of
+  sides A and B is not larger than that of side C.*/
+
+SELECT
+    CASE WHEN A = B AND B = C THEN 'Equilateral'
+         WHEN A+B <= C OR B+C <= A OR C+A <= B THEN 'Not A Triangle'
+         WHEN A = B OR B = C OR C = A THEN 'Isosceles'
+         ELSE 'Scalene'
+    END AS triangle_type
+FROM Triangles
+
+
 /*3054. Binary Tree Nodes (Medium)
 SQL Schema
 Table: Tree
