d011735eea02e7c139474515b74e83eda4ed303f
Ye Gao <gaoyescm@amazon.com>
2024-03-12 17:47:29 -0400

    solve'em all (3051)

--- leetcode-pandas.py
+++ leetcode-pandas.py
@@ -2676,6 +2676,72 @@ def cost_analysis(toppings: pd.DataFrame) -> pd.DataFrame:
     )[['pizza', 'total_cost']]
 
 
+"""3051. Find Candidates for Data Scientist Position (Easy)
+SQL Schema
+Table: Candidates
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| candidate_id | int     |
+| skill        | varchar |
++--------------+---------+
+(candidate_id, skill) is the primary key (columns with unique values) for this
+table. Each row includes candidate_id and skill. Write a query to find the
+candidates best suited for a Data Scientist position. The candidate must be
+proficient in Python, Tableau, and PostgreSQL. Return the result table ordered
+by candidate_id in ascending order. The result format is in the following
+example.
+
+Example 1:
+Input:
+Candidates table:
++---------------+--------------+
+| candidate_id  | skill        |
++---------------+--------------+
+| 123           | Python       |
+| 234           | R            |
+| 123           | Tableau      |
+| 123           | PostgreSQL   |
+| 234           | PowerBI      |
+| 234           | SQL Server   |
+| 147           | Python       |
+| 147           | Tableau      |
+| 147           | Java         |
+| 147           | PostgreSQL   |
+| 256           | Tableau      |
+| 102           | DataAnalysis |
++---------------+--------------+
+Output:
++--------------+
+| candidate_id |
++--------------+
+| 123          |
+| 147          |
++--------------+
+Explanation:
+- Candidates 123 and 147 possess the necessary skills in Python, Tableau, and
+  PostgreSQL for the data scientist position.
+- Candidates 234 and 102 do not possess any of the required skills for this
+  position.
+- Candidate 256 has proficiency in Tableau but is missing skills in Python and
+  PostgreSQL.
+The output table is sorted by candidate_id in ascending order."""
+
+def find_candidates(candidates: pd.DataFrame) -> pd.DataFrame:
+    return candidates.query(
+        expr = 'skill in ["Python", "Tableau", "PostgreSQL"]'
+    ).groupby(
+        by = 'candidate_id',
+        as_index = False
+    ).agg(
+        cnt = ('skill', 'count')
+    ).query(
+        expr = 'cnt == 3'
+    ).sort_values(
+        by = 'candidate_id'
+    )[['candidate_id']]
+
+
 """3054. Binary Tree Nodes (Medium)
 SQL Schema
 Table: Tree
--- leetcode.sql
+++ leetcode.sql
@@ -10141,6 +10141,65 @@ WHERE a.topping_name < b.topping_name AND b.topping_name < c.topping_name
 ORDER by 2 DESC, 1 ASC
 
 
+/*3051. Find Candidates for Data Scientist Position (Easy)
+SQL Schema
+Table: Candidates
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| candidate_id | int     |
+| skill        | varchar |
++--------------+---------+
+(candidate_id, skill) is the primary key (columns with unique values) for this
+table. Each row includes candidate_id and skill. Write a query to find the
+candidates best suited for a Data Scientist position. The candidate must be
+proficient in Python, Tableau, and PostgreSQL. Return the result table ordered
+by candidate_id in ascending order. The result format is in the following
+example.
+
+Example 1:
+Input:
+Candidates table:
++---------------+--------------+
+| candidate_id  | skill        |
++---------------+--------------+
+| 123           | Python       |
+| 234           | R            |
+| 123           | Tableau      |
+| 123           | PostgreSQL   |
+| 234           | PowerBI      |
+| 234           | SQL Server   |
+| 147           | Python       |
+| 147           | Tableau      |
+| 147           | Java         |
+| 147           | PostgreSQL   |
+| 256           | Tableau      |
+| 102           | DataAnalysis |
++---------------+--------------+
+Output:
++--------------+
+| candidate_id |
++--------------+
+| 123          |
+| 147          |
++--------------+
+Explanation:
+- Candidates 123 and 147 possess the necessary skills in Python, Tableau, and
+  PostgreSQL for the data scientist position.
+- Candidates 234 and 102 do not possess any of the required skills for this
+  position.
+- Candidate 256 has proficiency in Tableau but is missing skills in Python and
+  PostgreSQL.
+The output table is sorted by candidate_id in ascending order.*/
+
+SELECT candidate_id
+FROM Candidates
+WHERE skill in ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY 1
+HAVING COUNT(skill) = 3
+ORDER BY 1
+
+
 /*3054. Binary Tree Nodes (Medium)
 SQL Schema
 Table: Tree
