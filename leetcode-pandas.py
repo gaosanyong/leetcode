bde2e1885e1687cd0408cb7ed90fc3a0ef18afa0
Ye Gao <gaoyescm@amazon.com>
2024-03-13 16:03:35 -0400

    solve'em all (3058)

--- leetcode-pandas.py
+++ leetcode-pandas.py
@@ -3090,6 +3090,81 @@ def snap_analysis(activities: pd.DataFrame, age: pd.DataFrame) -> pd.DataFrame:
     )[['age_bucket', 'send_perc', 'open_perc']]
 
 
+"""3058. Friends With No Mutual Friends (Medium)
+SQL Schema
+Table: Friends
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| user_id1    | int  |
+| user_id2    | int  |
++-------------+------+
+(user_id1, user_id2) is the primary key (combination of columns with unique
+values) for this table. Each row contains user id1, user id2, both of whom are
+friends with each other. Write a solution to find all pairs of users who are
+friends with each other and have no mutual friends. Return the result table
+ordered by user_id1, user_id2 in ascending order. The result format is in the
+following example.
+
+Example 1:
+Input:
+Friends table:
++----------+----------+
+| user_id1 | user_id2 |
++----------+----------+
+| 1        | 2        |
+| 2        | 3        |
+| 2        | 4        |
+| 1        | 5        |
+| 6        | 7        |
+| 3        | 4        |
+| 2        | 5        |
+| 8        | 9        |
++----------+----------+
+Output:
++----------+----------+
+| user_id1 | user_id2 |
++----------+----------+
+| 6        | 7        |
+| 8        | 9        |
++----------+----------+
+Explanation:
+- Users 1 and 2 are friends with each other, but they share a mutual friend with
+  user ID 5, so this pair is not included.
+- Users 2 and 3 are friends, they both share a mutual friend with user ID 4,
+  resulting in exclusion, similarly for users 2 and 4 who share a mutual friend
+  with user ID 3, hence not included.
+- Users 1 and 5 are friends with each other, but they share a mutual friend with
+  user ID 2, so this pair is not included.
+- Users 6 and 7, as well as users 8 and 9, are friends with each other, and they
+  don't have any mutual friends, hence included.
+- Users 3 and 4 are friends with each other, but their mutual connection with
+  user ID 2 means they are not included, similarly for users 2 and 5 are friends
+  but are excluded due to their mutual connection with user ID 1.
+Output table is ordered by user_id1 in ascending order."""
+
+def friends_with_no_mutual_friends(friends: pd.DataFrame) -> pd.DataFrame:
+    union = pd.concat([friends, friends.rename(columns = {'user_id1' : 'user_id2', 'user_id2' : 'user_id1'})])
+    union = union.merge(
+        right = union,
+        left_on = 'user_id2',
+        right_on = 'user_id1'
+    ).rename(
+        columns = {'user_id1_x' : 'user_id1', 'user_id2_y' : 'user_id2'}
+    )[['user_id1', 'user_id2']]
+    return friends.merge(
+        right = union,
+        how = 'left',
+        indicator = True
+    ).query(
+        expr = '_merge == "left_only"'
+    ).drop_duplicates(
+        subset = None
+    ).sort_values(
+        by = ['user_id1', 'user_id2']
+    )[['user_id1', 'user_id2']]
+
+
 """3059. Find All Unique Email Domains (Easy)
 SQL Schema
 Table: Emails
--- leetcode.sql
+++ leetcode.sql
@@ -10534,6 +10534,74 @@ FROM Activities JOIN Age USING (user_id)
 GROUP BY 1
 
 
+/*3058. Friends With No Mutual Friends (Medium)
+SQL Schema
+Table: Friends
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| user_id1    | int  |
+| user_id2    | int  |
++-------------+------+
+(user_id1, user_id2) is the primary key (combination of columns with unique
+values) for this table. Each row contains user id1, user id2, both of whom are
+friends with each other. Write a solution to find all pairs of users who are
+friends with each other and have no mutual friends. Return the result table
+ordered by user_id1, user_id2 in ascending order. The result format is in the
+following example.
+
+Example 1:
+Input:
+Friends table:
++----------+----------+
+| user_id1 | user_id2 |
++----------+----------+
+| 1        | 2        |
+| 2        | 3        |
+| 2        | 4        |
+| 1        | 5        |
+| 6        | 7        |
+| 3        | 4        |
+| 2        | 5        |
+| 8        | 9        |
++----------+----------+
+Output:
++----------+----------+
+| user_id1 | user_id2 |
++----------+----------+
+| 6        | 7        |
+| 8        | 9        |
++----------+----------+
+Explanation:
+- Users 1 and 2 are friends with each other, but they share a mutual friend with
+  user ID 5, so this pair is not included.
+- Users 2 and 3 are friends, they both share a mutual friend with user ID 4,
+  resulting in exclusion, similarly for users 2 and 4 who share a mutual friend
+  with user ID 3, hence not included.
+- Users 1 and 5 are friends with each other, but they share a mutual friend with
+  user ID 2, so this pair is not included.
+- Users 6 and 7, as well as users 8 and 9, are friends with each other, and they
+  don't have any mutual friends, hence included.
+- Users 3 and 4 are friends with each other, but their mutual connection with
+  user ID 2 means they are not included, similarly for users 2 and 5 are friends
+  but are excluded due to their mutual connection with user ID 1.
+Output table is ordered by user_id1 in ascending order.*/
+
+WITH Cte AS (
+    SELECT * FROM Friends
+    UNION
+    SELECT user_id2 AS user_id1, user_id1 AS user_id2 FROM Friends
+), Cte1 AS (
+    SELECT a.user_id1, a.user_id2 AS common, b.user_id2
+    FROM Cte a,
+         Cte b
+    WHERE a.user_id2 = b.user_id1 AND a.user_id1 <> b.user_id2
+)
+SELECT user_id1, user_id2 FROM Friends
+WHERE (user_id1, user_id2) NOT IN (SELECT user_id1, user_id2 FROM Cte1)
+ORDER BY 1, 2
+
+
 /*3059. Find All Unique Email Domains (Easy)
 SQL Schema
 Table: Emails
