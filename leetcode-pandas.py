53249b78d184cd45aa6eb68dc464d6f96bac511a
Ye Gao <gaoyescm@amazon.com>
2024-03-13 18:15:32 -0400

    solve'em all (2783)

--- leetcode-pandas.py
+++ leetcode-pandas.py
@@ -2015,6 +2015,96 @@ def count_occurrences(files: pd.DataFrame) -> pd.DataFrame:
     return pd.DataFrame({"word" : ["bull", "bear"], "count" : [bulls, bears]})
 
 
+"""2783. Flight Occupancy and Waitlist Analysis (Medium)
+SQL Schema
+Table: Flights
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| flight_id   | int  |
+| capacity    | int  |
++-------------+------+
+flight_id is the column with unique values for this table. Each row of this
+table contains flight id and its capacity.
+
+Table: Passengers
++--------------+------+
+| Column Name  | Type |
++--------------+------+
+| passenger_id | int  |
+| flight_id    | int  |
++--------------+------+
+passenger_id is the column with unique values for this table. Each row of this
+table contains passenger id and flight id. Passengers book tickets for flights
+in advance. If a passenger books a ticket for a flight and there are still empty
+seats available on the flight, the passenger ticket will be confirmed. However,
+the passenger will be on a waitlist if the flight is already at full capacity.
+Write a solution to report the number of passengers who successfully booked a
+flight (got a seat) and the number of passengers who are on the waitlist for
+each flight. Return the result table ordered by flight_id in ascending order.
+The result format is in the following example.
+
+Example 1:
+Input:
+Flights table:
++-----------+----------+
+| flight_id | capacity |
++-----------+----------+
+| 1         | 2        |
+| 2         | 2        |
+| 3         | 1        |
++-----------+----------+
+Passengers table:
++--------------+-----------+
+| passenger_id | flight_id |
++--------------+-----------+
+| 101          | 1         |
+| 102          | 1         |
+| 103          | 1         |
+| 104          | 2         |
+| 105          | 2         |
+| 106          | 3         |
+| 107          | 3         |
++--------------+-----------+
+Output:
++-----------+------------+--------------+
+| flight_id | booked_cnt | waitlist_cnt |
++-----------+------------+--------------+
+| 1         | 2          | 1            |
+| 2         | 2          | 0            |
+| 3         | 1          | 1            |
++-----------+------------+--------------+
+Explanation:
+- Flight 1 has a capacity of 2. As there are 3 passengers who have booked
+  tickets, only 2 passengers can get a seat. Therefore, 2 passengers are
+  successfully booked, and 1 passenger is on the waitlist.
+- Flight 2 has a capacity of 2. Since there are exactly 2 passengers who booked
+  tickets, everyone can secure a seat. As a result, 2 passengers successfully
+  booked their seats and there are no passengers on the waitlist.
+- Flight 3 has a capacity of 1. As there are 2 passengers who have booked
+  tickets, only 1 passenger can get a seat. Therefore, 1 passenger is
+successfully booked, and 1 passenger is on the waitlist."""
+
+def waitlist_analysis(flights: pd.DataFrame, passengers: pd.DataFrame) -> pd.DataFrame:
+    return passengers.groupby(
+        by = 'flight_id',
+        as_index = False
+    ).agg(
+        total = ('passenger_id', 'count')
+    ).merge(
+        right = flights,
+        how = 'right',
+        on = 'flight_id'
+    ).fillna(
+        value = 0
+    ).assign(
+        booked_cnt = lambda x: np.minimum(x['total'], x['capacity']),
+        waitlist_cnt = lambda x: np.maximum(0, x['total'] - x['capacity'])
+    ).sort_values(
+        by = 'flight_id'
+    )[['flight_id', 'booked_cnt', 'waitlist_cnt']]
+
+
 """2820. Election Results (Medium)
 SQL Schema
 Table: Votes
--- leetcode.sql
+++ leetcode.sql
@@ -9450,6 +9450,86 @@ FROM Files
 WHERE content LIKE '% bear %'
 
 
+/*2783. Flight Occupancy and Waitlist Analysis (Medium)
+SQL Schema
+Table: Flights
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| flight_id   | int  |
+| capacity    | int  |
++-------------+------+
+flight_id is the column with unique values for this table. Each row of this
+table contains flight id and its capacity.
+
+Table: Passengers
++--------------+------+
+| Column Name  | Type |
++--------------+------+
+| passenger_id | int  |
+| flight_id    | int  |
++--------------+------+
+passenger_id is the column with unique values for this table. Each row of this
+table contains passenger id and flight id. Passengers book tickets for flights
+in advance. If a passenger books a ticket for a flight and there are still empty
+seats available on the flight, the passenger ticket will be confirmed. However,
+the passenger will be on a waitlist if the flight is already at full capacity.
+Write a solution to report the number of passengers who successfully booked a
+flight (got a seat) and the number of passengers who are on the waitlist for
+each flight. Return the result table ordered by flight_id in ascending order.
+The result format is in the following example.
+
+Example 1:
+Input:
+Flights table:
++-----------+----------+
+| flight_id | capacity |
++-----------+----------+
+| 1         | 2        |
+| 2         | 2        |
+| 3         | 1        |
++-----------+----------+
+Passengers table:
++--------------+-----------+
+| passenger_id | flight_id |
++--------------+-----------+
+| 101          | 1         |
+| 102          | 1         |
+| 103          | 1         |
+| 104          | 2         |
+| 105          | 2         |
+| 106          | 3         |
+| 107          | 3         |
++--------------+-----------+
+Output:
++-----------+------------+--------------+
+| flight_id | booked_cnt | waitlist_cnt |
++-----------+------------+--------------+
+| 1         | 2          | 1            |
+| 2         | 2          | 0            |
+| 3         | 1          | 1            |
++-----------+------------+--------------+
+Explanation:
+- Flight 1 has a capacity of 2. As there are 3 passengers who have booked
+  tickets, only 2 passengers can get a seat. Therefore, 2 passengers are
+  successfully booked, and 1 passenger is on the waitlist.
+- Flight 2 has a capacity of 2. Since there are exactly 2 passengers who booked
+  tickets, everyone can secure a seat. As a result, 2 passengers successfully
+  booked their seats and there are no passengers on the waitlist.
+- Flight 3 has a capacity of 1. As there are 2 passengers who have booked
+  tickets, only 1 passenger can get a seat. Therefore, 1 passenger is
+successfully booked, and 1 passenger is on the waitlist.*/
+
+SELECT
+    flight_id,
+    IFNULL(LEAST(total, capacity), 0) AS booked_cnt,
+    IFNULL(GREATEST(0, total - capacity), 0) AS waitlist_cnt
+FROM
+    (SELECT flight_id, COUNT(*) AS total FROM Passengers GROUP BY 1) a
+    RIGHT JOIN Flights USING (flight_id)
+ORDER BY 1
+
+
 /*2820. Election Results (Medium)
 SQL Schema
 Table: Votes
