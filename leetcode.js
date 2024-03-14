5fac5b343a0d372a3a43489433f0c6ab70680be6
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-03-14 00:38:59 -0400

    solve'em all (2633)

--- leetcode.js
+++ leetcode.js
@@ -688,6 +688,49 @@ var areDeeplyEqual = function(o1, o2) {
 };
 
 
+/*2633. Convert Object to JSON String (Medium)
+Given a value, return a valid JSON string of that value. The value can be a
+string, number, array, object, boolean, or null. The returned string should not
+include extra spaces. The order of keys should be the same as the order returned
+by Object.keys(). Please solve it without using the built-in JSON.stringify
+method.
+
+Example 1:
+Input: object = {"y":1,"x":2}
+Output: {"y":1,"x":2}
+Explanation: Return the JSON representation. Note that the order of keys should
+             be the same as the order returned by Object.keys().
+
+Example 2:
+Input: object = {"a":"str","b":-12,"c":true,"d":null}
+Output: {"a":"str","b":-12,"c":true,"d":null}
+Explanation: The primitives of JSON are strings, numbers, booleans, and null.
+
+Example 3:
+Input: object = {"key":{"a":1,"b":[{},null,"Hello"]}}
+Output: {"key":{"a":1,"b":[{},null,"Hello"]}}
+Explanation: Objects and arrays can include other objects and arrays.
+
+Example 4:
+Input: object = true
+Output: true
+Explanation: Primitive types are valid inputs.
+
+Constraints:
+* value is a valid JSON value
+* 1 <= JSON.stringify(object).length <= 10^5
+* maxNestingLevel <= 1000
+* all strings contain only alphanumeric characters*/
+
+var jsonStringify = function(object) {
+    if (object == null) return 'null';
+    if (typeof object === 'string') return `"${object}"`;
+    if (typeof object == 'number' || typeof object == 'boolean') return String(object);
+    if (Array.isArray(object)) return `[${object.map(x => jsonStringify(x)).join(',')}]`;
+    return `{${Object.entries(object).map(([k, v]) => jsonStringify(k) + ':' + jsonStringify(v)).join(',')}}`;
+};
+
+
 /*2648. Generate Fibonacci Sequence (Easy)
 Write a generator function that returns a generator object which yields the 
 fibonacci sequence. The fibonacci sequence is defined by the relation 
--- leetcode.ts
+++ leetcode.ts
@@ -690,6 +690,51 @@ function areDeeplyEqual(o1: JSONValue, o2: JSONValue): boolean {
 };
 
 
+/*2633. Convert Object to JSON String (Medium)
+Given a value, return a valid JSON string of that value. The value can be a
+string, number, array, object, boolean, or null. The returned string should not
+include extra spaces. The order of keys should be the same as the order returned
+by Object.keys(). Please solve it without using the built-in JSON.stringify
+method.
+
+Example 1:
+Input: object = {"y":1,"x":2}
+Output: {"y":1,"x":2}
+Explanation: Return the JSON representation. Note that the order of keys should
+             be the same as the order returned by Object.keys().
+
+Example 2:
+Input: object = {"a":"str","b":-12,"c":true,"d":null}
+Output: {"a":"str","b":-12,"c":true,"d":null}
+Explanation: The primitives of JSON are strings, numbers, booleans, and null.
+
+Example 3:
+Input: object = {"key":{"a":1,"b":[{},null,"Hello"]}}
+Output: {"key":{"a":1,"b":[{},null,"Hello"]}}
+Explanation: Objects and arrays can include other objects and arrays.
+
+Example 4:
+Input: object = true
+Output: true
+Explanation: Primitive types are valid inputs.
+
+Constraints:
+* value is a valid JSON value
+* 1 <= JSON.stringify(object).length <= 10^5
+* maxNestingLevel <= 1000
+* all strings contain only alphanumeric characters*/
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function jsonStringify(object: JSONValue): string {
+    if (object == null) return 'null';
+    if (typeof object === 'string') return `"${object}"`;
+    if (typeof object == 'number' || typeof object == 'boolean') return String(object);
+    if (Array.isArray(object)) return `[${object.map(x => jsonStringify(x)).join(',')}]`;
+    return `{${Object.entries(object).map(([k, v]) => jsonStringify(k) + ':' + jsonStringify(v)).join(',')}}`;
+};
+
+
 /*2674. Split a Circular Linked Listn (Medium)
 Given a circular linked list list of positive integers, your task is to
 split it into 2 circular linked lists so that the first one contains the
