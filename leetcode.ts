d201b01505a50d7fa5c2ece48767f3c7f066d12a
Ye Gao <gaoyescm@amazon.com>
2024-03-15 12:46:08 -0400

    solve'em all (2900)

--- leetcode.ts
+++ leetcode.ts
@@ -2148,6 +2148,55 @@ function lastVisitedIntegers(words: string[]): number[] {
 };
 
 
+/*2900. Longest Unequal Adjacent Groups Subsequence I (Easy)
+You are given a string array words and a binary array groups both of length
+n, where words[i] is associated with groups[i]. Your task is to select the
+longest alternating subsequence from words. A subsequence of words is
+alternating if for any two consecutive strings in the sequence, their
+corresponding elements in the binary array groups differ. Essentially, you
+are to choose strings such that adjacent elements have non-matching
+corresponding bits in the groups array. Formally, you need to find the
+longest subsequence of an array of indices [0, 1, ..., n - 1] denoted as
+[i0, i1, ..., ik-1], such that groups[ij] != groups[ij+1] for each
+0 <= j < k - 1 and then find the words corresponding to these indices.
+Return the selected subsequence. If there are multiple answers, return any
+of them. Note: The elements in words are distinct.
+
+Example 1:
+Input: words = ["e","a","b"], groups = [0,0,1]
+Output: ["e","b"]
+Explanation: A subsequence that can be selected is ["e","b"] because
+             groups[0] != groups[2]. Another subsequence that can be
+             selected is ["a","b"] because groups[1] != groups[2]. It can be
+             demonstrated that the length of the longest subsequence of
+             indices that satisfies the condition is 2.
+
+Example 2:
+Input: words = ["a","b","c","d"], groups = [1,0,1,1]
+Output: ["a","b","c"]
+Explanation: A subsequence that can be selected is ["a","b","c"] because
+             groups[0] != groups[1] and groups[1] != groups[2]. Another
+             subsequence that can be selected is ["a","b","d"] because
+             groups[0] != groups[1] and groups[1] != groups[3]. It can be
+             shown that the length of the longest subsequence of indices
+             that satisfies the condition is 3.
+
+Constraints:
+* 1 <= n == words.length == groups.length <= 100
+* 1 <= words[i].length <= 10
+* groups[i] is either 0 or 1.
+* words consists of distinct strings.
+* words[i] consists of lowercase English letters.*/
+
+function getLongestSubsequence(words: string[], groups: number[]): string[] {
+    const ans = [];
+    for (let i = 0; i < words.length; ++i)
+        if (i == 0 || groups[i-1] != groups[i])
+            ans.push(words[i]);
+    return ans;
+};
+
+
 /*2913. Subarrays Distinct Element Sum of Squares I (Easy)
 You are given a 0-indexed integer array nums. The distinct count of a
 subarray of nums is defined as:
