76ba8701b17c232cfcaeb56f5475a4dc68a4e827
Ye Gao <gaoyescm@amazon.com>
2024-03-20 15:19:14 -0400

    biweekly 126 (3079|3080|3081|3082)

--- leetcode.js
+++ leetcode.js
@@ -5930,3 +5930,232 @@ var maximumStrength = function(nums, k) {
         }
     return dp[k][0];
 };
+
+
+/*3079. Find the Sum of Encrypted Integers (Easy)
+You are given an integer array nums containing positive integers. We define
+a function encrypt such that encrypt(x) replaces every digit in x with the
+largest digit in x. For example, encrypt(523) = 555 and encrypt(213) = 333.
+Return the sum of encrypted elements.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: 6
+Explanation: The encrypted elements are [1,2,3]. The sum of encrypted
+             elements is 1 + 2 + 3 == 6.
+
+Example 2:
+Input: nums = [10,21,31]
+Output: 66
+Explanation: The encrypted elements are [11,22,33]. The sum of encrypted
+             elements is 11 + 22 + 33 == 66.
+
+Constraints:
+* 1 <= nums.length <= 50
+* 1 <= nums[i] <= 1000*/
+
+var sumOfEncryptedInt = function(nums) {
+    let ans = 0;
+    for (let x of nums) {
+        let most = 0, mul = 0;
+        for (; x; x = Math.floor(x/10)) {
+            most = Math.max(most, x % 10);
+            mul = 10*mul + 1;
+        }
+        ans += most * mul;
+    }
+    return ans;
+};
+
+
+/*3080. Mark Elements on Array by Performing Queries (Medium)
+You are given a 0-indexed array nums of size n consisting of positive
+integers. You are also given a 2D array queries of size m where
+queries[i] = [indexi, ki]. Initially all elements of the array are unmarked.
+You need to apply m queries on the array in order, where on the ith query
+you do the following:
+* Mark the element at index indexi if it is not already marked.
+* Then mark ki unmarked elements in the array with the smallest values. If
+  multiple such elements exist, mark the ones with the smallest indices. And
+  if less than ki unmarked elements exist, then mark all of them.
+Return an array answer of size m where answer[i] is the sum of unmarked
+elements in the array after the ith query.
+
+Example 1:
+Input: nums = [1,2,2,1,2,3,1], queries = [[1,2],[3,3],[4,2]]
+Output: [8,3,0]
+Explanation: We do the following queries on the array:
+             - Mark the element at index 1, and 2 of the smallest unmarked
+               elements with the smallest indices if they exist, the marked
+               elements now are nums = [1,2,2,1,2,3,1]. The sum of unmarked
+               elements is 2 + 2 + 3 + 1 = 8.
+             - Mark the element at index 3, since it is already marked we
+               skip it. Then we mark 3 of the smallest unmarked elements
+               with the smallest indices, the marked elements now are
+               nums = [1,2,2,1,2,3,1]. The sum of unmarked elements is 3.
+             - Mark the element at index 4, since it is already marked we
+               skip it. Then we mark 2 of the smallest unmarked elements
+               with the smallest indices if they exist, the marked elements
+               now are nums = [1,2,2,1,2,3,1]. The sum of unmarked elements
+               is 0.
+
+Example 2:
+Input: nums = [1,4,2,3], queries = [[0,1]]
+Output: [7]
+Explanation: We do one query which is mark the element at index 0 and mark
+             the smallest element among unmarked elements. The marked
+             elements will be nums = [1,4,2,3], and the sum of unmarked
+             elements is 4 + 3 = 7.
+
+Constraints:
+* n == nums.length
+* m == queries.length
+* 1 <= m <= n <= 10^5
+* 1 <= nums[i] <= 10^5
+* queries[i].length == 2
+* 0 <= indexi, ki <= n - 1*/
+
+var unmarkedSumArray = function(nums, queries) {
+    const pq = new PriorityQueue({ compare : (x, y) => x[0] != y[0] ? x[0] - y[0] : x[1] - y[1]});
+    for (const [i, x] of nums.entries())
+        pq.enqueue([x, i]);
+    let suffix = nums.reduce((x, y) => x + y);
+    const ans = [];
+    for (let [i, k] of queries) {
+        if (nums[i]) {
+            suffix -= nums[i];
+            nums[i] = 0;
+        }
+        while (k && pq.size()) {
+            const [_, j] = pq.dequeue();
+            if (nums[j]) {
+                --k;
+                suffix -= nums[j];
+                nums[j] = 0;
+            }
+        }
+        ans.push(suffix);
+    }
+    return ans;
+};
+
+
+/*3081. Replace Question Marks in String to Minimize Its Value (Medium)
+You are given a string s. s[i] is either a lowercase English letter or '?'.
+For a string t having length m containing only lowercase English letters, we
+define the function cost(i) for an index i as the number of characters equal
+to t[i] that appeared before it, i.e. in the range [0, i - 1]. The value of
+t is the sum of cost(i) for all indices i. For example, for the string
+t = "aab":
+* cost(0) = 0
+* cost(1) = 1
+* cost(2) = 0
+* Hence, the value of "aab" is 0 + 1 + 0 = 1.
+Your task is to replace all occurrences of '?' in s with any lowercase
+English letter so that the value of s is minimized. Return a string denoting
+the modified string with replaced occurrences of '?'. If there are multiple
+strings resulting in the minimum value, return the lexicographically
+smallest one.
+
+Example 1:
+Input:  s = "???"
+Output:  "abc"
+Explanation: In this example, we can replace the occurrences of '?' to make
+             s equal to "abc". For "abc", cost(0) = 0, cost(1) = 0, and
+             cost(2) = 0. The value of "abc" is 0. Some other modifications
+             of s that have a value of 0 are "cba", "abz", and, "hey". Among
+             all of them, we choose the lexicographically smallest.
+
+Example 2:
+Input: s = "a?a?"
+Output: "abac"
+Explanation: In this example, the occurrences of '?' can be replaced to make
+             s equal to "abac". For "abac", cost(0) = 0, cost(1) = 0,
+             cost(2) = 1, and cost(3) = 0. The value of "abac" is 1.
+
+Constraints:
+* 1 <= s.length <= 10^5
+* s[i] is either a lowercase English letter or '?'.*/
+
+var minimizeStringValue = function(s) {
+    const ans = s.split(''), loc = [], freq = Array(26).fill(0);
+    for (const [i, ch] of ans.entries())
+        if (ch == '?') loc.push(i);
+        else ++freq[ch.charCodeAt(0) - 97];
+    const pq = new PriorityQueue({ compare : (x, y) => x[0] != y[0] ? x[0] - y[0] : x[1].localeCompare(y[1]) });
+    for (const [i, x] of freq.entries())
+        pq.enqueue([x, String.fromCharCode(97 + i)]);
+    const vals = [];
+    for (const _ of loc) {
+        const [x, c] = pq.dequeue();
+        pq.enqueue([x+1, c]);
+        vals.push(c);
+    }
+    vals.sort();
+    for (let i = 0; i < loc.length; ++i)
+        ans[loc[i]] = vals[i];
+    return ans.join('');
+};
+
+
+/*3082. Find the Sum of the Power of All Subsequences (Hard)
+You are given an integer array nums of length n and a positive integer k.
+The power of an array of integers is defined as the number of subsequences
+with their sum equal to k. Return the sum of power of all subsequences of
+nums. Since the answer may be very large, return it modulo 10^9 + 7.
+
+Example 1:
+Input:  nums = [1,2,3], k = 3
+Output:  6
+Explanation: There are 5 subsequences of nums with non-zero power:
+             - The subsequence [1,2,3] has 2 subsequences with sum == 3:
+               [1,2,3] and [1,2,3].
+             - The subsequence [1,2,3] has 1 subsequence with sum == 3:
+               [1,2,3].
+             - The subsequence [1,2,3] has 1 subsequence with sum == 3:
+               [1,2,3].
+             - The subsequence [1,2,3] has 1 subsequence with sum == 3:
+               [1,2,3].
+             - The subsequence [1,2,3] has 1 subsequence with sum == 3:
+               [1,2,3].
+             Hence the answer is 2 + 1 + 1 + 1 + 1 = 6.
+
+Example 2:
+Input:  nums = [2,3,3], k = 5
+Output:  4
+Explanation: There are 3 subsequences of nums with non-zero power:
+             - The subsequence [2,3,3] has 2 subsequences with sum == 5:
+               [2,3,3] and [2,3,3].
+             - The subsequence [2,3,3] has 1 subsequence with sum == 5:
+               [2,3,3].
+             - The subsequence [2,3,3] has 1 subsequence with sum == 5:
+               [2,3,3].
+             Hence the answer is 2 + 1 + 1 = 4.
+
+Example 3:
+Input:  nums = [1,2,3], k = 7
+Output:  0
+Explanation: There exists no subsequence with sum 7. Hence all subsequences
+             of nums have power = 0.
+
+Constraints:
+* 1 <= n <= 100
+* 1 <= nums[i] <= 10^4
+* 1 <= k <= 100*/
+
+var sumOfPower = function(nums, k) {
+    const n = nums.length, mod = 1_000_000_007n;
+    const dp = Array(n+1).fill(0).map(() => Array(k+1).fill(0n));
+    dp[0][0] = 1n;
+    const p2 = Array(n+1).fill(1n);
+    for (let i = 1; i <= n; ++i)
+        p2[i] = p2[i-1] * 2n % mod;
+    for (const x of nums)
+        for (let i = n; i > 0; --i)
+            for (let j = k; j >= x; --j)
+                dp[i][j] = (dp[i][j] + dp[i-1][j-x]) % mod;
+    let ans = 0n;
+    for (let i = 0; i <= n; ++i)
+        ans = (ans + dp[i][k] * p2[n-i]) % mod;
+    return ans;
+};
