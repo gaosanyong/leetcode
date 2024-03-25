4ff2587eca03a4bfcc21f0fcda4ac16489f76c9d
Ye Gao <gaoyescm@amazon.com>
2024-03-25 11:36:27 -0400

    solve'em all (442)

--- leetcode.js
+++ leetcode.js
@@ -256,6 +256,41 @@ var firstUniqChar = function(s) {
 };
 
 
+/*442. Find All Duplicates in an Array (Medium)
+Given an integer array nums of length n where all the integers of nums are
+in the range [1, n] and each integer appears once or twice, return an array
+of all the integers that appears twice. You must write an algorithm that
+runs in O(n) time and uses only constant extra space.
+
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [2,3]
+
+Example 2:
+Input: nums = [1,1,2]
+Output: [1]
+
+Example 3:
+Input: nums = [1]
+Output: []
+
+Constraints:
+* n == nums.length
+* 1 <= n <= 10^5
+* 1 <= nums[i] <= n
+* Each element in nums appears once or twice.*/
+
+var findDuplicates = function(nums) {
+    const ans = [];
+    for (let x of nums) {
+        x = Math.abs(x);
+        if (nums[x-1] < 0) ans.push(x);
+        nums[x-1] *= -1;
+    }
+    return ans;
+};
+
+
 /*452. Minimum Number of Arrows to Burst Balloons (Medium)
 There are some spherical balloons taped onto a flat wall that represents
 the XY-plane. The balloons are represented as a 2D integer array points
