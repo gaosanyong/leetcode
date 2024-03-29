c8d1c95c9057230de5b331f6fa1d9c82c3cad153
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-03-29 08:53:45 -0400

    solve'em all (2962)

--- leetcode.ts
+++ leetcode.ts
@@ -2794,6 +2794,41 @@ function maxSubarrayLength(nums: number[], k: number): number {
 };
 
 
+/*2962. Count Subarrays Where Max Element Appears at Least K Times (Meidum)
+You are given an integer array nums and a positive integer k. Return the
+number of subarrays where the maximum element of nums appears at least k
+times in that subarray. A subarray is a contiguous sequence of elements
+within an array.
+
+Example 1:
+Input: nums = [1,3,2,3,3], k = 2
+Output: 6
+Explanation: The subarrays that contain the element 3 at least 2 times are:
+             [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+
+Example 2:
+Input: nums = [1,4,2,1], k = 3
+Output: 0
+Explanation: No subarray contains the element 4 at least 3 times.
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* 1 <= nums[i] <= 10^6
+* 1 <= k <= 10^5*/
+
+function countSubarrays(nums: number[], k: number): number {
+    let m = Math.max(...nums);
+    let ans = 0, ii = 0, freq = 0;
+    for (let x of nums) {
+        if (x == m) ++freq;
+        while (freq == k)
+            if (nums[ii++] == m) --freq;
+        ans += ii;
+    }
+    return ans;
+};
+
+
 /*2964. Number of Divisible Triplet Sums (Medium)
 Given a 0-indexed integer array nums and an integer d, return the number of
 triplets (i, j, k) such that i < j < k and
