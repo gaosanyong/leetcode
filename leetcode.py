da9a32ce67cdd7fff9125a91b0fb0e0c24d25dfa
Ye Gao <gaoyescm@amazon.com>
2024-04-09 23:59:42 -0400

    solve'em all (950)

--- leetcode.py
+++ leetcode.py
@@ -24414,44 +24414,56 @@ class Solution:
 
 
     """950. Reveal Cards In Increasing Order (Medium)
-    In a deck of cards, every card has a unique integer. You can order the deck
-    in any order you want. Initially, all the cards start face down (unrevealed)
-    in one deck. Now, you do the following steps repeatedly, until all cards are
-    revealed:
-    1) Take the top card of the deck, reveal it, and take it out of the deck.
-    2) If there are still cards in the deck, put the next top card of the deck
-       at the bottom of the deck.
-    3) If there are still unrevealed cards, go back to step 1.  Otherwise, stop.
-    Return an ordering of the deck that would reveal the cards in increasing order.
-    The first entry in the answer is considered to be the top of the deck.
+    You are given an integer array deck. There is a deck of cards where every
+    card has a unique integer. The integer on the ith card is deck[i]. You can
+    order the deck in any order you want. Initially, all the cards start face
+    down (unrevealed) in one deck. You will do the following steps repeatedly
+    until all cards are revealed:
+    * Take the top card of the deck, reveal it, and take it out of the deck.
+    * If there are still cards in the deck then put the next top card of the
+      deck at the bottom of the deck.
+    * If there are still unrevealed cards, go back to step 1. Otherwise, stop.
+    Return an ordering of the deck that would reveal the cards in increasing
+    order. Note that the first entry in the answer is considered to be the top
+    of the deck.
 
     Example 1:
-    Input: [17,13,11,2,3,5,7]
+    Input: deck = [17,13,11,2,3,5,7]
     Output: [2,13,3,11,5,17,7]
-    Explanation:
-    We get the deck in the order [17,13,11,2,3,5,7] (this order doesn't matter),
-    and reorder it. After reordering, the deck starts as [2,13,3,11,5,17,7],
-    where 2 is the top of the deck.
-    We reveal  2, and move 13 to the bottom.  The deck is now [3,11,5,17,7,13].
-    We reveal  3, and move 11 to the bottom.  The deck is now [5,17,7,13,11].
-    We reveal  5, and move 17 to the bottom.  The deck is now [7,13,11,17].
-    We reveal  7, and move 13 to the bottom.  The deck is now [11,17,13].
-    We reveal 11, and move 17 to the bottom.  The deck is now [13,17].
-    We reveal 13, and move 17 to the bottom.  The deck is now [17].
-    We reveal 17.
-    Since all the cards revealed are in increasing order, the answer is correct.
+    Explanation: We get the deck in the order [17,13,11,2,3,5,7] (this order
+                 does not matter), and reorder it.
+                 After reordering, the deck starts as [2,13,3,11,5,17,7], where
+                 2 is the top of the deck.
+                 - We reveal 2, and move 13 to the bottom.  The deck is now
+                   [3,11,5,17,7,13].
+                 - We reveal 3, and move 11 to the bottom.  The deck is now
+                   [5,17,7,13,11].
+                 - We reveal 5, and move 17 to the bottom.  The deck is now
+                   [7,13,11,17].
+                 - We reveal 7, and move 13 to the bottom.  The deck is now
+                   [11,17,13].
+                 - We reveal 11, and move 17 to the bottom.  The deck is now
+                   [13,17].
+                 - We reveal 13, and move 17 to the bottom.  The deck is now
+                   [17].
+                 - We reveal 17.
+                 Since all the cards revealed are in increasing order, the
+                 answer is correct.
 
-    Note:
-    * 1 <= A.length <= 1000
-    * 1 <= A[i] <= 10^6
-    * A[i] != A[j] for all i != j"""
+    Example 2:
+    Input: deck = [1,1000]
+    Output: [1,1000]
+
+    Constraints:
+    * 1 <= deck.length <= 1000
+    * 1 <= deck[i] <= 10^6
+    * All the values of deck are unique."""
 
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        ans = [0]*len(deck)
-        idx = deque(range(len(deck)))
-        for x in sorted(deck):
-            ans[idx.popleft()] = x
-            if idx: idx.append(idx.popleft())
+        ans = deque()
+        for x in sorted(deck, reverse=True):
+            ans.rotate()
+            ans.appendleft(x)
         return ans
 
 
