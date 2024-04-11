ac42311777f8f2821eb2e33846301f9d861af1a7
Ye Gao <gaoyescm@amazon.com>
2024-04-11 12:32:35 -0400

    solve'em all (402)

--- leetcode.java
+++ leetcode.java
@@ -2022,6 +2022,48 @@ class Solution {
     }
 
 
+    /*402. Remove K Digits (Medium)
+    Given string num representing a non-negative integer num, and an integer k,
+    return the smallest possible integer after removing k digits from num.
+
+    Example 1:
+    Input: num = "1432219", k = 3
+    Output: "1219"
+    Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219
+                 which is the smallest.
+
+    Example 2:
+    Input: num = "10200", k = 1
+    Output: "200"
+    Explanation: Remove the leading 1 and the number is 200. Note that the
+                 output must not contain leading zeroes.
+
+    Example 3:
+    Input: num = "10", k = 2
+    Output: "0"
+    Explanation: Remove all the digits from the number and it is left with
+                 nothing which is 0.
+
+    Constraints:
+    * 1 <= k <= num.length <= 10^5
+    * num consists of only digits.
+    * num does not have any leading zeros except for the zero itself.*/
+
+    public String removeKdigits(String num, int k) {
+        List<Character> ans = new ArrayList();
+        for (var ch : num.toCharArray()) {
+            while (k > 0 && ans.size() > 0 && ans.get(ans.size()-1) > ch) {
+                --k;
+                ans.remove(ans.size()-1);
+            }
+            if (ans.size() > 0 || ch != '0') ans.add(ch);
+        }
+        while (k-- > 0 && ans.size() > 0)
+            ans.remove(ans.size()-1);
+        return ans.size() > 0 ? ans.stream().map(String::valueOf).collect(Collectors.joining()) : "0";
+    }
+
+
     /*427. Construct Quad Tree (Medium)
     Given a n * n matrix grid of 0's and 1's only. We want to represent the 
     grid with a Quad-Tree. Return the root of the Quad-Tree representing the 
--- leetcode.js
+++ leetcode.js
@@ -372,6 +372,48 @@ var firstUniqChar = function(s) {
 };
 
 
+/*402. Remove K Digits (Medium)
+Given string num representing a non-negative integer num, and an integer k,
+return the smallest possible integer after removing k digits from num.
+
+Example 1:
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219
+             which is the smallest.
+
+Example 2:
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the
+             output must not contain leading zeroes.
+
+Example 3:
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with
+             nothing which is 0.
+
+Constraints:
+* 1 <= k <= num.length <= 10^5
+* num consists of only digits.
+* num does not have any leading zeros except for the zero itself.*/
+
+var removeKdigits = function(num, k) {
+    const ans = [];
+    for (const ch of num) {
+        while (k && ans.length && ans[ans.length-1] > ch) {
+            --k;
+            ans.pop();
+        }
+        if (ans.length || ch != '0') ans.push(ch);
+    }
+    while (k-- && ans.length)
+        ans.pop();
+    return ans.join('') || "0";
+};
+
+
 /*442. Find All Duplicates in an Array (Medium)
 Given an integer array nums of length n where all the integers of nums are
 in the range [1, n] and each integer appears once or twice, return an array
