9753be0b878be1be1fb078910c44c3e34aa6b959
Ye Gao <gaoyescm@amazon.com>
2024-04-16 17:55:43 -0400

    solve'em all (2705)

--- leetcode.js
+++ leetcode.js
@@ -1629,6 +1629,48 @@ var createInfiniteObject = function() {
 };
 
 
+/*2705. Compact Object (Medium)
+Given an object or array obj, return a compact object. A compact object is the
+same as the original object, except with keys containing falsy values removed.
+This operation applies to the object and any nested objects. Arrays are
+considered objects where the indices are keys. A value is considered falsy when
+Boolean(value) returns false. You may assume the obj is the output of
+JSON.parse. In other words, it is valid JSON.
+
+Example 1:
+Input: obj = [null, 0, false, 1]
+Output: [1]
+Explanation: All falsy values have been removed from the array.
+
+Example 2:
+Input: obj = {"a": null, "b": [false, 1]}
+Output: {"b": [1]}
+Explanation: obj["a"] and obj["b"][0] had falsy values and were removed.
+
+Example 3:
+Input: obj = [null, 0, 5, [0], [false, 16]]
+Output: [5, [], [16]]
+Explanation: obj[0], obj[1], obj[3][0], and obj[4][0] were falsy and removed.
+
+Constraints:
+* obj is a valid JSON object
+* 2 <= JSON.stringify(obj).length <= 10^6*/
+
+var compactObject = function(obj) {
+    if (obj == null) return null;
+    if (Array.isArray(obj)) return obj.filter(Boolean).map(compactObject);
+    if (typeof obj == "object") {
+        const ans = {};
+        for (let [key, val] of Object.entries(obj)) {
+            val = compactObject(val);
+            if (val) ans[key] = val;
+        }
+        return ans;
+    }
+    return obj;
+};
+
+
 /*2722. Join Two Arrays by ID (Medium)
 Given two arrays arr1 and arr2, return a new array joinedArray. All the objects
 in each of the two inputs arrays will contain an id field that has an integer
--- leetcode.ts
+++ leetcode.ts
@@ -1531,6 +1531,49 @@ function createInfiniteObject(): Record<string, () => string> {
 };
 
 
+/*2705. Compact Object (Medium)
+Given an object or array obj, return a compact object. A compact object is the
+same as the original object, except with keys containing falsy values removed.
+This operation applies to the object and any nested objects. Arrays are
+considered objects where the indices are keys. A value is considered falsy when
+Boolean(value) returns false. You may assume the obj is the output of
+JSON.parse. In other words, it is valid JSON.
+
+Example 1:
+Input: obj = [null, 0, false, 1]
+Output: [1]
+Explanation: All falsy values have been removed from the array.
+
+Example 2:
+Input: obj = {"a": null, "b": [false, 1]}
+Output: {"b": [1]}
+Explanation: obj["a"] and obj["b"][0] had falsy values and were removed.
+
+Example 3:
+Input: obj = [null, 0, 5, [0], [false, 16]]
+Output: [5, [], [16]]
+Explanation: obj[0], obj[1], obj[3][0], and obj[4][0] were falsy and removed.
+
+Constraints:
+* obj is a valid JSON object
+* 2 <= JSON.stringify(obj).length <= 10^6*/
+
+function compactObject(obj: Obj): Obj {
+    if (obj == null) return null;
+    if (Array.isArray(obj))
+        return obj.filter(Boolean).map((x) => typeof x == "object" ? compactObject(x) : x);
+    if (typeof obj == "object") {
+        const ans = {};
+        for (let [k, x] of Object.entries(obj)) {
+            if (typeof x == "object") x = compactObject(x);
+            if (x) ans[k] = x;
+        }
+        return ans;
+    }
+    return obj;
+};
+
+
 /*2722. Join Two Arrays by ID (Medium)
 Given two arrays arr1 and arr2, return a new array joinedArray. All the objects
 in each of the two inputs arrays will contain an id field that has an integer
