7622708866b4e220644033b4030c0a3961727bde
Ye Gao <gaoyescm@amazon.com>
2024-04-25 12:51:25 -0400

    biweekly 122 (3010|3011|3012|3013) 3013 javascript typescript pending

--- leetcode.java
+++ leetcode.java
@@ -21993,6 +21993,272 @@ class SegTreeLazy {
     }
 
 
+    /*3010. Divide an Array Into Subarrays With Minimum Cost I (Easy)
+    You are given an array of integers nums of length n. The cost of an array is
+    the value of its first element. For example, the cost of [1,2,3] is 1 while
+    the cost of [3,4,1] is 3. You need to divide nums into 3 disjoint contiguous
+    subarrays. Return the minimum possible sum of the cost of these subarrays.
+
+    Example 1:
+    Input: nums = [1,2,3,12]
+    Output: 6
+    Explanation: The best possible way to form 3 subarrays is: [1], [2], and
+                 [3,12] at a total cost of 1 + 2 + 3 = 6. The other possible
+                 ways to form 3 subarrays are:
+                 - [1], [2,3], and [12] at a total cost of 1 + 2 + 12 = 15.
+                 - [1,2], [3], and [12] at a total cost of 1 + 3 + 12 = 16.
+
+    Example 2:
+    Input: nums = [5,4,3]
+    Output: 12
+    Explanation: The best possible way to form 3 subarrays is: [5], [4], and [3]
+                 at a total cost of 5 + 4 + 3 = 12. It can be shown that 12 is
+                 the minimum cost achievable.
+
+    Example 3:
+    Input: nums = [10,3,1,1]
+    Output: 12
+    Explanation: The best possible way to form 3 subarrays is: [10,3], [1], and
+                 [1] at a total cost of 10 + 1 + 1 = 12. It can be shown that 12
+                 is the minimum cost achievable.
+
+    Constraints:
+    * 3 <= n <= 50
+    * 1 <= nums[i] <= 50*/
+
+    public int minimumCost(int[] nums) {
+        int m0 = 50, m1 = 50;
+        for (int i = 1; i < nums.length; ++i)
+            if (nums[i] < m0) {
+                m1 = m0;
+                m0 = nums[i];
+            } else if (nums[i] < m1) m1 = nums[i];
+        return nums[0] + m0 + m1;
+    }
+
+
+    /*3011. Find if Array Can Be Sorted (Medium)
+    You are given a 0-indexed array of positive integers nums. In one operation,
+    you can swap any two adjacent elements if they have the same number of set
+    bits. You are allowed to do this operation any number of times (including
+    zero). Return true if you can sort the array, else return false.
+
+    Example 1:
+    Input: nums = [8,4,2,30,15]
+    Output: true
+    Explanation: Let's look at the binary representation of every element. The
+                 numbers 2, 4, and 8 have one set bit each with binary
+                 representation "10", "100", and "1000" respectively. The
+                 numbers 15 and 30 have four set bits each with binary
+                 representation "1111" and "11110". We can sort the array using
+                 4 operations:
+                 - Swap nums[0] with nums[1]. This operation is valid because 8
+                   and 4 have one set bit each. The array becomes [4,8,2,30,15].
+                 - Swap nums[1] with nums[2]. This operation is valid because 8
+                   and 2 have one set bit each. The array becomes [4,2,8,30,15].
+                 - Swap nums[0] with nums[1]. This operation is valid because 4
+                   and 2 have one set bit each. The array becomes [2,4,8,30,15].
+                 - Swap nums[3] with nums[4]. This operation is valid because 30
+                   and 15 have four set bits each. The array becomes [2,4,8,15,30].
+                 The array has become sorted, hence we return true. Note that
+                 there may be other sequences of operations which also sort the
+                 array.
+
+    Example 2:
+    Input: nums = [1,2,3,4,5]
+    Output: true
+    Explanation: The array is already sorted, hence we return true.
+
+    Example 3:
+    Input: nums = [3,16,8,4,2]
+    Output: false
+    Explanation: It can be shown that it is not possible to sort the input array
+                 using any number of operations.
+
+    Constraints:
+    * 1 <= nums.length <= 100
+    * 1 <= nums[i] <= 2^8*/
+
+    public boolean canSortArray(int[] nums) {
+        int bits = 0, curr = 0, prev = 0;
+        for (var x : nums) {
+            int b = 0;
+            for (int xx = x; xx > 0; ++b, xx &= xx-1);
+            if (bits != b) {
+                bits = b;
+                prev = curr;
+            }
+            if (prev > x) return false;
+            curr = Math.max(curr, x);
+        }
+        return true;
+    }
+
+
+    /*3012. Minimize Length of Array Using Operations (Medium)
+    You are given a 0-indexed integer array nums containing positive integers.
+    Your task is to minimize the length of nums by performing the following
+    operations any number of times (including zero):
+    * Select two distinct indices i and j from nums, such that nums[i] > 0 and
+      nums[j] > 0.
+    * Insert the result of nums[i] % nums[j] at the end of nums.
+    * Delete the elements at indices i and j from nums.
+    Return an integer denoting the minimum length of nums after performing the
+    operation any number of times.
+
+    Example 1:
+    Input: nums = [1,4,3,1]
+    Output: 1
+    Explanation: One way to minimize the length of the array is as follows:
+                 - Operation 1: Select indices 2 and 1, insert nums[2] % nums[1]
+                   at the end and it becomes [1,4,3,1,3], then delete elements
+                   at indices 2 and 1. nums becomes [1,1,3].
+                 - Operation 2: Select indices 1 and 2, insert nums[1] % nums[2]
+                   at the end and it becomes [1,1,3,1], then delete elements at
+                   indices 1 and 2. nums becomes [1,1].
+                 - Operation 3: Select indices 1 and 0, insert nums[1] % nums[0]
+                   at the end and it becomes [1,1,0], then delete elements at
+                   indices 1 and 0. nums becomes [0].
+                 The length of nums cannot be reduced further. Hence, the answer
+                 is 1. It can be shown that 1 is the minimum achievable length.
+
+    Example 2:
+    Input: nums = [5,5,5,10,5]
+    Output: 2
+    Explanation: One way to minimize the length of the array is as follows:
+                 - Operation 1: Select indices 0 and 3, insert nums[0] % nums[3]
+                   at the end and it becomes [5,5,5,10,5,5], then delete
+                   elements at indices 0 and 3. nums becomes [5,5,5,5].
+                 - Operation 2: Select indices 2 and 3, insert nums[2] % nums[3]
+                   at the end and it becomes [5,5,5,5,0], then delete elements
+                   at indices 2 and 3. nums becomes [5,5,0].
+                 - Operation 3: Select indices 0 and 1, insert nums[0] % nums[1]
+                   at the end and it becomes [5,5,0,0], then delete elements at
+                   indices 0 and 1. nums becomes [0,0].
+                 The length of nums cannot be reduced further. Hence, the answer
+                 is 2. It can be shown that 2 is the minimum achievable length.
+
+    Example 3:
+    Input: nums = [2,3,4]
+    Output: 1
+    Explanation: One way to minimize the length of the array is as follows:
+                 - Operation 1: Select indices 1 and 2, insert nums[1] % nums[2]
+                   at the end and it becomes [2,3,4,3], then delete elements at
+                   indices 1 and 2. nums becomes [2,3].
+                 - Operation 2: Select indices 1 and 0, insert nums[1] % nums[0]
+                   at the end and it becomes [2,3,1], then delete elements at
+                   indices 1 and 0. nums becomes [1].
+                 The length of nums cannot be reduced further. Hence, the answer
+                 is 1. It can be shown that 1 is the minimum achievable length.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 1 <= nums[i] <= 10^9*/
+
+    public int minimumArrayLength(int[] nums) {
+        int m = IntStream.of(nums).min().getAsInt(), cnt = 0;
+        for (var x : nums)
+            if (x % m > 0) return 1;
+            else if (x == m) ++cnt;
+        return (cnt+1)/2;
+    }
+
+
+    /*3013. Divide an Array Into Subarrays With Minimum Cost II (Hard)
+    You are given a 0-indexed array of integers nums of length n, and two
+    positive integers k and dist. The cost of an array is the value of its first
+    element. For example, the cost of [1,2,3] is 1 while the cost of [3,4,1] is
+    3. You need to divide nums into k disjoint contiguous subarrays, such that
+    the difference between the starting index of the second subarray and the
+    starting index of the kth subarray should be less than or equal to dist. In
+    other words, if you divide nums into the subarrays nums[0..(i1 - 1)],
+    nums[i1..(i2 - 1)], ..., nums[ik-1..(n - 1)], then ik-1 - i1 <= dist. Return
+    the minimum possible sum of the cost of these subarrays.
+
+    Example 1:
+    Input: nums = [1,3,2,6,4,2], k = 3, dist = 3
+    Output: 5
+    Explanation: The best possible way to divide nums into 3 subarrays is:
+                 [1,3], [2,6,4], and [2]. This choice is valid because ik-1 - i1
+                 is 5 - 2 = 3 which is equal to dist. The total cost is
+                 nums[0] + nums[2] + nums[5] which is 1 + 2 + 2 = 5. It can be
+                 shown that there is no possible way to divide nums into 3
+                 subarrays at a cost lower than 5.
+
+    Example 2:
+    Input: nums = [10,1,2,2,2,1], k = 4, dist = 3
+    Output: 15
+    Explanation: The best possible way to divide nums into 4 subarrays is:
+                 [10], [1], [2], and [2,2,1]. This choice is valid because
+                 ik-1 - i1 is 3 - 1 = 2 which is less than dist. The total cost
+                 is nums[0] + nums[1] + nums[2] + nums[3] which is
+                 10 + 1 + 2 + 2 = 15. The division [10], [1], [2,2,2], and [1]
+                 is not valid, because the difference between ik-1 and i1 is
+                 5 - 1 = 4, which is greater than dist. It can be shown that
+                 there is no possible way to divide nums into 4 subarrays at a
+                 cost lower than 15.
+
+    Example 3:
+    Input: nums = [10,8,18,9], k = 3, dist = 1
+    Output: 36
+    Explanation: The best possible way to divide nums into 4 subarrays is:
+                 [10], [8], and [18,9]. This choice is valid because ik-1 - i1
+                 is 2 - 1 = 1 which is equal to dist.The total cost is
+                 nums[0] + nums[1] + nums[2] which is 10 + 8 + 18 = 36. The
+                 division [10], [8,18], and [9] is not valid, because the
+                 difference between ik-1 and i1 is 3 - 1 = 2, which is greater
+                 than dist. It can be shown that there is no possible way to
+                 divide nums into 3 subarrays at a cost lower than 36.
+
+    Constraints:
+    * 3 <= n <= 10^5
+    * 1 <= nums[i] <= 10^9
+    * 3 <= k <= n
+    * k - 2 <= dist <= n - 2*/
+
+    public long minimumCost(int[] nums, int k, int dist) {
+        long ans = Long.MAX_VALUE, count = 0, total = 0;
+        TreeMap<Integer, Integer> small = new TreeMap(), large = new TreeMap();
+        for (int i = 1; i < nums.length; ++i) {
+            if (i > dist+1) {
+                int v = nums[i-dist-1];
+                if (v <= small.lastKey()) {
+                    small.merge(v, -1, Integer::sum);
+                    if (small.get(v) == 0) small.remove(v);
+                    --count;
+                    total -= v;
+                    if (!large.isEmpty()) {
+                        int key = large.firstKey();
+                        small.merge(key, 1, Integer::sum);
+                        large.merge(key, -1, Integer::sum);
+                        if (large.get(key) == 0) large.remove(key);
+                        ++count;
+                        total += key;
+                    }
+                } else {
+                    large.merge(v, -1, Integer::sum);
+                    if (large.get(v) == 0) large.remove(v);
+                }
+            }
+            ++count;
+            small.merge(nums[i], 1, Integer::sum);
+            total += nums[i];
+            if (count > k-1) {
+                int key = small.lastKey();
+                small.merge(key, -1, Integer::sum);
+                if (small.get(key) == 0) small.remove(key);
+                large.merge(key, 1, Integer::sum);
+                --count;
+                total -= key;
+            }
+            if (i >= k-1) {
+                ans = Math.min(ans, total);
+            }
+        }
+        return nums[0] + ans;
+    }
+
+
     /*3019. Number of Changing Keys (Easy)
     You are given a 0-indexed string s typed by a user. Changing a key is
     defined as using a key different from the last used key. For example,
--- leetcode.js
+++ leetcode.js
@@ -5754,6 +5754,226 @@ var beautifulIndices = function(s, a, b, k) {
 };
 
 
+/*3010. Divide an Array Into Subarrays With Minimum Cost I (Easy)
+You are given an array of integers nums of length n. The cost of an array is
+the value of its first element. For example, the cost of [1,2,3] is 1 while
+the cost of [3,4,1] is 3. You need to divide nums into 3 disjoint contiguous
+subarrays. Return the minimum possible sum of the cost of these subarrays.
+
+Example 1:
+Input: nums = [1,2,3,12]
+Output: 6
+Explanation: The best possible way to form 3 subarrays is: [1], [2], and
+             [3,12] at a total cost of 1 + 2 + 3 = 6. The other possible
+             ways to form 3 subarrays are:
+             - [1], [2,3], and [12] at a total cost of 1 + 2 + 12 = 15.
+             - [1,2], [3], and [12] at a total cost of 1 + 3 + 12 = 16.
+
+Example 2:
+Input: nums = [5,4,3]
+Output: 12
+Explanation: The best possible way to form 3 subarrays is: [5], [4], and [3]
+             at a total cost of 5 + 4 + 3 = 12. It can be shown that 12 is
+             the minimum cost achievable.
+
+Example 3:
+Input: nums = [10,3,1,1]
+Output: 12
+Explanation: The best possible way to form 3 subarrays is: [10,3], [1], and
+             [1] at a total cost of 10 + 1 + 1 = 12. It can be shown that 12
+             is the minimum cost achievable.
+
+Constraints:
+* 3 <= n <= 50
+* 1 <= nums[i] <= 50*/
+
+var minimumCost = function(nums) {
+    let ans = nums.shift();
+    nums.sort((x, y) => x-y);
+    return ans + nums[0] + nums[1];
+};
+
+
+/*3011. Find if Array Can Be Sorted (Medium)
+You are given a 0-indexed array of positive integers nums. In one operation,
+you can swap any two adjacent elements if they have the same number of set
+bits. You are allowed to do this operation any number of times (including
+zero). Return true if you can sort the array, else return false.
+
+Example 1:
+Input: nums = [8,4,2,30,15]
+Output: true
+Explanation: Let's look at the binary representation of every element. The
+             numbers 2, 4, and 8 have one set bit each with binary
+             representation "10", "100", and "1000" respectively. The
+             numbers 15 and 30 have four set bits each with binary
+             representation "1111" and "11110". We can sort the array using
+             4 operations:
+             - Swap nums[0] with nums[1]. This operation is valid because 8
+               and 4 have one set bit each. The array becomes [4,8,2,30,15].
+             - Swap nums[1] with nums[2]. This operation is valid because 8
+               and 2 have one set bit each. The array becomes [4,2,8,30,15].
+             - Swap nums[0] with nums[1]. This operation is valid because 4
+               and 2 have one set bit each. The array becomes [2,4,8,30,15].
+             - Swap nums[3] with nums[4]. This operation is valid because 30
+               and 15 have four set bits each. The array becomes [2,4,8,15,30].
+             The array has become sorted, hence we return true. Note that
+             there may be other sequences of operations which also sort the
+             array.
+
+Example 2:
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: The array is already sorted, hence we return true.
+
+Example 3:
+Input: nums = [3,16,8,4,2]
+Output: false
+Explanation: It can be shown that it is not possible to sort the input array
+             using any number of operations.
+
+Constraints:
+* 1 <= nums.length <= 100
+* 1 <= nums[i] <= 2^8*/
+
+var canSortArray = function(nums) {
+    let bits = 0, curr = 0, prev = 0;
+    for (const x of nums) {
+        let b = 0;
+        for (let xx = x; xx; xx &= xx-1, ++b);
+        if (bits != b) bits = b, prev = curr;
+        if (prev > x) return false;
+        curr = Math.max(curr, x);
+    }
+    return true;
+};
+
+
+/*3012. Minimize Length of Array Using Operations (Medium)
+You are given a 0-indexed integer array nums containing positive integers.
+Your task is to minimize the length of nums by performing the following
+operations any number of times (including zero):
+* Select two distinct indices i and j from nums, such that nums[i] > 0 and
+  nums[j] > 0.
+* Insert the result of nums[i] % nums[j] at the end of nums.
+* Delete the elements at indices i and j from nums.
+Return an integer denoting the minimum length of nums after performing the
+operation any number of times.
+
+Example 1:
+Input: nums = [1,4,3,1]
+Output: 1
+Explanation: One way to minimize the length of the array is as follows:
+             - Operation 1: Select indices 2 and 1, insert nums[2] % nums[1]
+               at the end and it becomes [1,4,3,1,3], then delete elements
+               at indices 2 and 1. nums becomes [1,1,3].
+             - Operation 2: Select indices 1 and 2, insert nums[1] % nums[2]
+               at the end and it becomes [1,1,3,1], then delete elements at
+               indices 1 and 2. nums becomes [1,1].
+             - Operation 3: Select indices 1 and 0, insert nums[1] % nums[0]
+               at the end and it becomes [1,1,0], then delete elements at
+               indices 1 and 0. nums becomes [0].
+             The length of nums cannot be reduced further. Hence, the answer
+             is 1. It can be shown that 1 is the minimum achievable length.
+
+Example 2:
+Input: nums = [5,5,5,10,5]
+Output: 2
+Explanation: One way to minimize the length of the array is as follows:
+             - Operation 1: Select indices 0 and 3, insert nums[0] % nums[3]
+               at the end and it becomes [5,5,5,10,5,5], then delete
+               elements at indices 0 and 3. nums becomes [5,5,5,5].
+             - Operation 2: Select indices 2 and 3, insert nums[2] % nums[3]
+               at the end and it becomes [5,5,5,5,0], then delete elements
+               at indices 2 and 3. nums becomes [5,5,0].
+             - Operation 3: Select indices 0 and 1, insert nums[0] % nums[1]
+               at the end and it becomes [5,5,0,0], then delete elements at
+               indices 0 and 1. nums becomes [0,0].
+             The length of nums cannot be reduced further. Hence, the answer
+             is 2. It can be shown that 2 is the minimum achievable length.
+
+Example 3:
+Input: nums = [2,3,4]
+Output: 1
+Explanation: One way to minimize the length of the array is as follows:
+             - Operation 1: Select indices 1 and 2, insert nums[1] % nums[2]
+               at the end and it becomes [2,3,4,3], then delete elements at
+               indices 1 and 2. nums becomes [2,3].
+             - Operation 2: Select indices 1 and 0, insert nums[1] % nums[0]
+               at the end and it becomes [2,3,1], then delete elements at
+               indices 1 and 0. nums becomes [1].
+             The length of nums cannot be reduced further. Hence, the answer
+             is 1. It can be shown that 1 is the minimum achievable length.
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* 1 <= nums[i] <= 10^9*/
+
+var minimumArrayLength = function(nums) {
+    const m = Math.min(...nums);
+    let cnt = 0;
+    for (const x of nums)
+        if (x % m) return 1;
+        else if (x == m) ++cnt;
+    return Math.ceil(cnt/2);
+};
+
+
+/*3013. Divide an Array Into Subarrays With Minimum Cost II (Hard)
+You are given a 0-indexed array of integers nums of length n, and two
+positive integers k and dist. The cost of an array is the value of its first
+element. For example, the cost of [1,2,3] is 1 while the cost of [3,4,1] is
+3. You need to divide nums into k disjoint contiguous subarrays, such that
+the difference between the starting index of the second subarray and the
+starting index of the kth subarray should be less than or equal to dist. In
+other words, if you divide nums into the subarrays nums[0..(i1 - 1)],
+nums[i1..(i2 - 1)], ..., nums[ik-1..(n - 1)], then ik-1 - i1 <= dist. Return
+the minimum possible sum of the cost of these subarrays.
+
+Example 1:
+Input: nums = [1,3,2,6,4,2], k = 3, dist = 3
+Output: 5
+Explanation: The best possible way to divide nums into 3 subarrays is:
+             [1,3], [2,6,4], and [2]. This choice is valid because ik-1 - i1
+             is 5 - 2 = 3 which is equal to dist. The total cost is
+             nums[0] + nums[2] + nums[5] which is 1 + 2 + 2 = 5. It can be
+             shown that there is no possible way to divide nums into 3
+             subarrays at a cost lower than 5.
+
+Example 2:
+Input: nums = [10,1,2,2,2,1], k = 4, dist = 3
+Output: 15
+Explanation: The best possible way to divide nums into 4 subarrays is:
+             [10], [1], [2], and [2,2,1]. This choice is valid because
+             ik-1 - i1 is 3 - 1 = 2 which is less than dist. The total cost
+             is nums[0] + nums[1] + nums[2] + nums[3] which is
+             10 + 1 + 2 + 2 = 15. The division [10], [1], [2,2,2], and [1]
+             is not valid, because the difference between ik-1 and i1 is
+             5 - 1 = 4, which is greater than dist. It can be shown that
+             there is no possible way to divide nums into 4 subarrays at a
+             cost lower than 15.
+
+Example 3:
+Input: nums = [10,8,18,9], k = 3, dist = 1
+Output: 36
+Explanation: The best possible way to divide nums into 4 subarrays is:
+             [10], [8], and [18,9]. This choice is valid because ik-1 - i1
+             is 2 - 1 = 1 which is equal to dist.The total cost is
+             nums[0] + nums[1] + nums[2] which is 10 + 8 + 18 = 36. The
+             division [10], [8,18], and [9] is not valid, because the
+             difference between ik-1 and i1 is 3 - 1 = 2, which is greater
+             than dist. It can be shown that there is no possible way to
+             divide nums into 3 subarrays at a cost lower than 36.
+
+Constraints:
+* 3 <= n <= 10^5
+* 1 <= nums[i] <= 10^9
+* 3 <= k <= n
+* k - 2 <= dist <= n - 2*/
+
+
+
+
 /*3019. Number of Changing Keys (Easy)
 You are given a 0-indexed string s typed by a user. Changing a key is
 defined as using a key different from the last used key. For example,
