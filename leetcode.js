729fdf511d7ec4e8264863b793e638a2e1d079a6
Ye Gao <gaoyescm@amazon.com>
2024-05-01 10:45:56 -0400

    sovle'em all (2000)

--- leetcode.js
+++ leetcode.js
@@ -1698,6 +1698,44 @@ var findFarmland = function(land) {
 };
 
 
+/*2000. Reverse Prefix of Word (Easy)
+Given a 0-indexed string word and a character ch, reverse the segment of
+word that starts at index 0 and ends at the index of the first occurrence of
+ch (inclusive). If the character ch does not exist in word, do nothing. For
+example, if word = "abcdefd" and ch = "d", then you should reverse the
+segment that starts at 0 and ends at 3 (inclusive). The resulting string
+will be "dcbaefd". Return the resulting string.
+
+Example 1:
+Input: word = "abcdefd", ch = "d"
+Output: "dcbaefd"
+Explanation: The first occurrence of "d" is at index 3. Reverse the part of
+             word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+
+Example 2:
+Input: word = "xyxzxe", ch = "z"
+Output: "zxyxxe"
+Explanation: The first and only occurrence of "z" is at index 3. Reverse the
+             part of word from 0 to 3 (inclusive), the resulting string is
+             "zxyxxe".
+
+Example 3:
+Input: word = "abcd", ch = "z"
+Output: "abcd"
+Explanation: "z" does not exist in word. You should not do any reverse
+             operation, the resulting string is "abcd".
+
+Constraints:
+* 1 <= word.length <= 250
+* word consists of lowercase English letters.
+* ch is a lowercase English letter.*/
+
+var reversePrefix = function(word, ch) {
+    const k = word.indexOf(ch);
+    return word.split('').slice(0, k+1).reverse().join('') + word.substring(k+1);
+};
+
+
 /*2108. Find First Palindromic String in the Array (Easy)
 Given an array of strings words, return the first palindromic string in the
 array. If there is no such string, return an empty string "". A string is
