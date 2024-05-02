e821a41ee4ec83c4623bd8ff69dab18b31a92c19
Ye Gao <gaoyescm@amazon.com>
2024-05-01 22:10:48 -0400

    solve'em all (2441)

--- leetcode.ts
+++ leetcode.ts
@@ -1862,6 +1862,43 @@ function longestIdealString(s: string, k: number): number {
 };
 
 
+/*2441. Largest Positive Integer That Exists With Its Negative (Easy)
+Given an integer array nums that does not contain any zeros, find the
+largest positive integer k such that -k also exists in the array. Return the
+positive integer k. If there is no such integer, return -1.
+
+Example 1:
+Input: nums = [-1,2,-3,3]
+Output: 3
+Explanation: 3 is the only valid k we can find in the array.
+
+Example 2:
+Input: nums = [-1,10,6,7,-7,1]
+Output: 7
+Explanation: Both 1 and 7 have their corresponding negative values in the
+             array. 7 has a larger value.
+
+Example 3:
+Input: nums = [-10,8,6,7,-2,-3]
+Output: -1
+Explanation: There is no a single valid k, we return -1.
+
+Constraints:
+* 1 <= nums.length <= 1000
+* -1000 <= nums[i] <= 1000
+* nums[i] != 0*/
+
+function findMaxK(nums: number[]): number {
+    let ans = -1;
+    const seen = new Set();
+    for (const x of nums) {
+        if (seen.has(-x)) ans = Math.max(ans, Math.abs(x));
+        seen.add(x);
+    }
+    return ans;
+};
+
+
 /*2485. Find the Pivot Integer (Easy)
 Given a positive integer n, find the pivot integer x such that:
 * The sum of all elements between 1 and x inclusively equals the sum of all
