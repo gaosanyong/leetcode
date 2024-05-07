1387f38fea47580212d1976605959f08b6a76cef
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-05-06 23:43:56 -0400

    solve'em all (2816)

--- leetcode.js
+++ leetcode.js
@@ -3122,6 +3122,41 @@ Array.prototype.forEach = function(callback, context) {
 }
 
 
+/*2816. Double a Number Represented as a Linked List (Medium)
+You are given the head of a non-empty linked list representing a non-negative
+integer without leading zeroes. Return the head of the linked list after
+doubling it.
+
+Example 1:
+Input: head = [1,8,9]
+Output: [3,7,8]
+Explanation: The figure above corresponds to the given linked list which
+             represents the number 189. Hence, the returned linked list
+             represents the number 189 * 2 = 378.
+
+Example 2:
+Input: head = [9,9,9]
+Output: [1,9,9,8]
+Explanation: The figure above corresponds to the given linked list which
+             represents the number 999. Hence, the returned linked list
+             reprersents the number 999 * 2 = 1998.
+
+Constraints:
+* The number of nodes in the list is in the range [1, 10^4]
+* 0 <= Node.val <= 9
+* The input is generated such that the list represents a number that does not
+have leading zeros, except the number 0 itself.*/
+
+var doubleIt = function(head) {
+    if (head.val >= 5) head = new ListNode(0, head);
+    for (let node = head; node; node = node.next) {
+        node.val = 2*node.val % 10;
+        if (node.next && node.next.val >= 5) node.val += 1;
+    }
+    return head;
+};
+
+
 /*2821. Delay the Resolution of Each Promise (Easy)
 Given an array functions and a number ms, return a new array of functions.
 * functions is an array of functions that return promises.
