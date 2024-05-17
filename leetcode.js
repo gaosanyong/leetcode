f8395a9ddb64d48febec321e64c04a7a715ec24f
Ye Gao <gaoyescm@amazon.com>
2024-05-17 12:24:10 -0400

    solve'em all (1325)

--- leetcode.js
+++ leetcode.js
@@ -1597,6 +1597,43 @@ var minFallingPathSum = function(grid) {
 };
 
 
+/*1325. Delete Leaves With a Given Value (Medium)
+Given a binary tree root and an integer target, delete all the leaf nodes
+with value target. Note that once you delete a leaf node with value target,
+if its parent node becomes a leaf node and has the value target, it should
+also be deleted (you need to continue doing that until you cannot).
+
+Example 1:
+Input: root = [1,2,3,2,null,2,4], target = 2
+Output: [1,null,3,null,4]
+Explanation: Leaf nodes in green with value (target = 2) are removed
+             (Picture in left). After removing, new nodes become leaf nodes
+             with value (target = 2) (Picture in center).
+
+Example 2:
+Input: root = [1,3,3,3,2], target = 3
+Output: [1,3,null,null,2]
+
+Example 3:
+Input: root = [1,2,null,2,null,2], target = 2
+Output: [1]
+Explanation: Leaf nodes in green with value (target = 2) are removed at each
+             step.
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 3000].
+* 1 <= Node.val, target <= 1000*/
+
+var removeLeafNodes = function(root, target) {
+    if (root) {
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+        if (root.left || root.right || root.val != target) return root;
+    }
+    return null;
+};
+
+
 /*1544. Make The String Great (Easy)
 Given a string s of lower and upper case English letters. A good string is a
 string which doesn't have two adjacent characters s[i] and s[i + 1] where:
--- leetcode.py
+++ leetcode.py
@@ -33222,6 +33222,40 @@ class UnionFind:
         return fn(0)
 
 
+    """1325. Delete Leaves With a Given Value (Medium)
+    Given a binary tree root and an integer target, delete all the leaf nodes
+    with value target. Note that once you delete a leaf node with value target,
+    if its parent node becomes a leaf node and has the value target, it should
+    also be deleted (you need to continue doing that until you cannot).
+
+    Example 1:
+    Input: root = [1,2,3,2,null,2,4], target = 2
+    Output: [1,null,3,null,4]
+    Explanation: Leaf nodes in green with value (target = 2) are removed
+                 (Picture in left). After removing, new nodes become leaf nodes
+                 with value (target = 2) (Picture in center).
+
+    Example 2:
+    Input: root = [1,3,3,3,2], target = 3
+    Output: [1,3,null,null,2]
+
+    Example 3:
+    Input: root = [1,2,null,2,null,2], target = 2
+    Output: [1]
+    Explanation: Leaf nodes in green with value (target = 2) are removed at each
+                 step.
+
+    Constraints:
+    * The number of nodes in the tree is in the range [1, 3000].
+    * 1 <= Node.val, target <= 1000"""
+
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        if root:
+            root.left = self.removeLeafNodes(root.left, target)
+            root.right = self.removeLeafNodes(root.right, target)
+            if root.left or root.right or root.val != target: return root
+
+
     """1328. Break a Palindrome (Medium)
     Given a palindromic string of lowercase English letters palindrome, replace
     exactly one character with any lowercase English letter so that the
