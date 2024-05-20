0bd2d59faf1c89162b145c6d56ef9d8f50974572
Ye Gao <gaoyescm@amazon.com>
2024-05-20 16:13:41 -0400

    weekly 398 (3151|3152|3153|3154)

--- leetcode.cpp
+++ leetcode.cpp
@@ -70968,6 +70968,196 @@ public:
         }
         return lo;
     }
+
+
+    /*3151. Special Array I (Easy)
+    An array is considered special if every pair of its adjacent elements
+    contains two numbers with different parity. You are given an array of
+    integers nums. Return true if nums is a special array, otherwise, return
+    false.
+
+    Example 1:
+    Input: nums = [1]
+    Output: true
+    Explanation: There is only one element. So the answer is true.
+
+    Example 2:
+    Input: nums = [2,1,4]
+    Output: true
+    Explanation: There is only two pairs: (2,1) and (1,4), and both of them
+                 contain numbers with different parity. So the answer is true.
+
+    Example 3:
+    Input: nums = [4,3,1,6]
+    Output: false
+    Explanation: nums[1] and nums[2] are both odd. So the answer is false.
+
+    Constraints:
+    * 1 <= nums.length <= 100
+    * 1 <= nums[i] <= 100*/
+
+    bool isArraySpecial(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); ++i)
+            if ((nums[i-1] - nums[i]) % 2 == 0) return false;
+        return true;
+    }
+
+
+    /*3152. Special Array II (Medium)
+    An array is considered special if every pair of its adjacent elements
+    contains two numbers with different parity. You are given an array of
+    integer nums and a 2D integer matrix queries, where for
+    queries[i] = [fromi, toi] your task is to check that subarray
+    nums[fromi..toi] is special or not. Return an array of booleans answer such
+    that answer[i] is true if nums[fromi..toi] is special.
+
+    Example 1:
+    Input: nums = [3,4,1,2,6], queries = [[0,4]]
+    Output: [false]
+    Explanation: The subarray is [3,4,1,2,6]. 2 and 6 are both even.
+
+    Example 2:
+    Input: nums = [4,3,1,6], queries = [[0,2],[2,3]]
+    Output: [false,true]
+    Explanation: The subarray is [4,3,1]. 3 and 1 are both odd. So the answer to
+                 this query is false. The subarray is [1,6]. There is only one
+                 pair: (1,6) and it contains numbers with different parity. So
+                 the answer to this query is true.
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 1 <= nums[i] <= 10^5
+    * 1 <= queries.length <= 10^5
+    * queries[i].length == 2
+    * 0 <= queries[i][0] <= queries[i][1] <= nums.length - 1*/
+
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> dp;
+        for (int i = 0, v = 0; i < nums.size(); ++i) {
+            if (i && (nums[i-1] - nums[i]) % 2 == 0) v = i;
+            dp.push_back(v);
+        }
+        vector<bool> ans(queries.size());
+        transform(queries.begin(), queries.end(), ans.begin(), [&](auto& x) { return x[0] >= dp[x[1]]; });
+        return ans;
+    }
+
+
+    /*3153. Sum of Digit Differences of All Pairs (Medium)
+    You are given an array nums consisting of positive integers where all
+    integers have the same number of digits. The digit difference between two
+    integers is the count of different digits that are in the same position in
+    the two integers. Return the sum of the digit differences between all pairs
+    of integers in nums.
+
+    Example 1:
+    Input: nums = [13,23,12]
+    Output: 4
+    Explanation: We have the following:
+                 - The digit difference between 13 and 23 is 1.
+                 - The digit difference between 13 and 12 is 1.
+                 - The digit difference between 23 and 12 is 2.
+                 So the total sum of digit differences between all pairs of
+                 integers is 1 + 1 + 2 = 4.
+
+    Example 2:
+    Input: nums = [10,10,10,10]
+    Output: 0
+    Explanation: All the integers in the array are the same. So the total sum of
+                 digit differences between all pairs of integers will be 0.
+
+    Constraints:
+    * 2 <= nums.length <= 10^5
+    * 1 <= nums[i] < 10^9
+    * All integers in nums have the same number of digits.*/
+
+    long long sumDigitDifferences(vector<int>& nums) {
+        long long ans = 0;
+        while (nums[0]) {
+            vector<int> freq(10);
+            for (auto& x : nums) {
+                ++freq[x % 10];
+                x /= 10;
+            }
+            ans += accumulate(freq.begin(), freq.end(), 0ll, [&](long long x, int y) { return x + y*(nums.size()-y); });
+        }
+        return ans/2;
+    }
+
+
+    /*3154. Find Number of Ways to Reach the K-th Stair (Hard)
+    You are given a non-negative integer k. There exists a staircase with an
+    infinite number of stairs, with the lowest stair numbered 0. Alice has an
+    integer jump, with an initial value of 0. She starts on stair 1 and wants to
+    reach stair k using any number of operations. If she is on stair i, in one
+    operation she can:
+    * Go down to stair i - 1. This operation cannot be used consecutively or on
+      stair 0.
+    * Go up to stair i + 2jump. And then, jump becomes jump + 1.
+    Return the total number of ways Alice can reach stair k. Note that it is
+    possible that Alice reaches the stair k, and performs some operations to
+    reach the stair k again.
+
+    Example 1:
+    Input: k = 0
+    Output: 2
+    Explanation: The 2 possible ways of reaching stair 0 are:
+                 - Alice starts at stair 1.
+                 - Using an operation of the first type, she goes down 1 stair
+                   to reach stair 0.
+                 - Alice starts at stair 1.
+                 - Using an operation of the first type, she goes down 1 stair
+                   to reach stair 0.
+                 - Using an operation of the second type, she goes up 20 stairs
+                   to reach stair 1.
+                 - Using an operation of the first type, she goes down 1 stair
+                   to reach stair 0.
+
+    Example 2:
+    Input: k = 1
+    Output: 4
+    Explanation: The 4 possible ways of reaching stair 1 are:
+                 - Alice starts at stair 1. Alice is at stair 1.
+                 - Alice starts at stair 1.
+                 - Using an operation of the first type, she goes down 1 stair
+                   to reach stair 0.
+                 - Using an operation of the second type, she goes up 20 stairs
+                   to reach stair 1.
+                 - Alice starts at stair 1.
+                 - Using an operation of the second type, she goes up 20 stairs
+                   to reach stair 2.
+                 - Using an operation of the first type, she goes down 1 stair
+                   to reach stair 1.
+                 - Alice starts at stair 1.
+                 - Using an operation of the first type, she goes down 1 stair
+                   to reach stair 0.
+                 - Using an operation of the second type, she goes up 20 stairs
+                   to reach stair 1.
+                 - Using an operation of the first type, she goes down 1 stair
+                   to reach stair 0.
+                 - Using an operation of the second type, she goes up 21 stairs
+                   to reach stair 2.
+                 - Using an operation of the first type, she goes down 1 stair
+                   to reach stair 1.
+
+    Constraints: 0 <= k <= 10^9*/
+
+    int waysToReachStair(int k) {
+        int ans = 0;
+        for (int jump = 0; jump <= 30; ++jump) {
+            int n = jump+1, v = (1<<jump)-k;
+            v = min(v, n-v);
+            if (v >= 0) {
+                long val = 1;
+                for (int i = 0; i < v; ++i) {
+                    val *= n-i;
+                    val /= i+1;
+                }
+                ans += val;
+            }
+        }
+        return ans;
+    }
 }
 
 
--- leetcode.js
+++ leetcode.js
@@ -10124,3 +10124,189 @@ var medianOfUniquenessArray = function(nums) {
     }
     return lo;
 };
+
+
+/*3151. Special Array I (Easy)
+An array is considered special if every pair of its adjacent elements
+contains two numbers with different parity. You are given an array of
+integers nums. Return true if nums is a special array, otherwise, return
+false.
+
+Example 1:
+Input: nums = [1]
+Output: true
+Explanation: There is only one element. So the answer is true.
+
+Example 2:
+Input: nums = [2,1,4]
+Output: true
+Explanation: There is only two pairs: (2,1) and (1,4), and both of them
+             contain numbers with different parity. So the answer is true.
+
+Example 3:
+Input: nums = [4,3,1,6]
+Output: false
+Explanation: nums[1] and nums[2] are both odd. So the answer is false.
+
+Constraints:
+* 1 <= nums.length <= 100
+* 1 <= nums[i] <= 100*/
+
+var isArraySpecial = function(nums) {
+    return nums.reduce((acc, x, i) => acc && (x-nums[i-1])%2);
+};
+
+
+/*3152. Special Array II (Medium)
+An array is considered special if every pair of its adjacent elements
+contains two numbers with different parity. You are given an array of
+integer nums and a 2D integer matrix queries, where for
+queries[i] = [fromi, toi] your task is to check that subarray
+nums[fromi..toi] is special or not. Return an array of booleans answer such
+that answer[i] is true if nums[fromi..toi] is special.
+
+Example 1:
+Input: nums = [3,4,1,2,6], queries = [[0,4]]
+Output: [false]
+Explanation: The subarray is [3,4,1,2,6]. 2 and 6 are both even.
+
+Example 2:
+Input: nums = [4,3,1,6], queries = [[0,2],[2,3]]
+Output: [false,true]
+Explanation: The subarray is [4,3,1]. 3 and 1 are both odd. So the answer to
+             this query is false. The subarray is [1,6]. There is only one
+             pair: (1,6) and it contains numbers with different parity. So
+             the answer to this query is true.
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* 1 <= nums[i] <= 10^5
+* 1 <= queries.length <= 10^5
+* queries[i].length == 2
+* 0 <= queries[i][0] <= queries[i][1] <= nums.length - 1*/
+
+var isArraySpecial = function(nums, queries) {
+    const dp = [];
+    for (let i = 0, v = 0; i < nums.length; ++i) {
+        if (i && (nums[i-1]-nums[i]) % 2 == 0) v = i;
+        dp.push(v);
+    }
+    return queries.map(([lo, hi]) => lo >= dp[hi]);
+};
+
+
+/*3153. Sum of Digit Differences of All Pairs (Medium)
+You are given an array nums consisting of positive integers where all
+integers have the same number of digits. The digit difference between two
+integers is the count of different digits that are in the same position in
+the two integers. Return the sum of the digit differences between all pairs
+of integers in nums.
+
+Example 1:
+Input: nums = [13,23,12]
+Output: 4
+Explanation: We have the following:
+             - The digit difference between 13 and 23 is 1.
+             - The digit difference between 13 and 12 is 1.
+             - The digit difference between 23 and 12 is 2.
+             So the total sum of digit differences between all pairs of
+             integers is 1 + 1 + 2 = 4.
+
+Example 2:
+Input: nums = [10,10,10,10]
+Output: 0
+Explanation: All the integers in the array are the same. So the total sum of
+             digit differences between all pairs of integers will be 0.
+
+Constraints:
+* 2 <= nums.length <= 10^5
+* 1 <= nums[i] < 10^9
+* All integers in nums have the same number of digits.*/
+
+var sumDigitDifferences = function(nums) {
+    let ans = 0;
+    for (let n = nums.length; nums[0]; ) {
+        const freq = Array(10).fill(0);
+        for (let i = 0; i < n; ++i) {
+            ++freq[nums[i] % 10];
+            nums[i] = Math.floor(nums[i]/10);
+        }
+        ans += freq.reduce((x, y) => x + y*(n-y), 0);
+    }
+    return ans/2;
+};
+
+
+/*3154. Find Number of Ways to Reach the K-th Stair (Hard)
+You are given a non-negative integer k. There exists a staircase with an
+infinite number of stairs, with the lowest stair numbered 0. Alice has an
+integer jump, with an initial value of 0. She starts on stair 1 and wants to
+reach stair k using any number of operations. If she is on stair i, in one
+operation she can:
+* Go down to stair i - 1. This operation cannot be used consecutively or on
+  stair 0.
+* Go up to stair i + 2jump. And then, jump becomes jump + 1.
+Return the total number of ways Alice can reach stair k. Note that it is
+possible that Alice reaches the stair k, and performs some operations to
+reach the stair k again.
+
+Example 1:
+Input: k = 0
+Output: 2
+Explanation: The 2 possible ways of reaching stair 0 are:
+             - Alice starts at stair 1.
+             - Using an operation of the first type, she goes down 1 stair
+               to reach stair 0.
+             - Alice starts at stair 1.
+             - Using an operation of the first type, she goes down 1 stair
+               to reach stair 0.
+             - Using an operation of the second type, she goes up 20 stairs
+               to reach stair 1.
+             - Using an operation of the first type, she goes down 1 stair
+               to reach stair 0.
+
+Example 2:
+Input: k = 1
+Output: 4
+Explanation: The 4 possible ways of reaching stair 1 are:
+             - Alice starts at stair 1. Alice is at stair 1.
+             - Alice starts at stair 1.
+             - Using an operation of the first type, she goes down 1 stair
+               to reach stair 0.
+             - Using an operation of the second type, she goes up 20 stairs
+               to reach stair 1.
+             - Alice starts at stair 1.
+             - Using an operation of the second type, she goes up 20 stairs
+               to reach stair 2.
+             - Using an operation of the first type, she goes down 1 stair
+               to reach stair 1.
+             - Alice starts at stair 1.
+             - Using an operation of the first type, she goes down 1 stair
+               to reach stair 0.
+             - Using an operation of the second type, she goes up 20 stairs
+               to reach stair 1.
+             - Using an operation of the first type, she goes down 1 stair
+               to reach stair 0.
+             - Using an operation of the second type, she goes up 21 stairs
+               to reach stair 2.
+             - Using an operation of the first type, she goes down 1 stair
+               to reach stair 1.
+
+Constraints: 0 <= k <= 10^9*/
+
+var waysToReachStair = function(k) {
+    let ans = 0;
+    for (let jump = 0; jump <= 30; ++jump) {
+        let n = 1+jump, v = (1<<jump)-k;
+        v = Math.min(v, n-v);
+        if (v >= 0) {
+            let val = 1;
+            for (let i = 0; i < v; ++i) {
+                val *= n-i;
+                val /= i+1;
+            }
+            ans += val;
+        }
+    }
+    return ans;
+};
