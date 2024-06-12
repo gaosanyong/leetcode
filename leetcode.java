97d8853411785d514fabe9b929214ebcd139865f
Ye Gao <gaoyescm@amazon.com>
2024-06-12 12:44:15 -0400

    solve'em all (75)

--- leetcode.java
+++ leetcode.java
@@ -759,6 +759,45 @@ class Solution {
     }
 
 
+    /*75. Sort Colors (Medium)
+    Given an array nums with n objects colored red, white, or blue, sort them
+    in-place so that objects of the same color are adjacent, with the colors in
+    the order red, white, and blue. We will use the integers 0, 1, and 2 to
+    represent the color red, white, and blue, respectively. You must solve this
+    problem without using the library's sort function.
+
+    Example 1:
+    Input: nums = [2,0,2,1,1,0]
+    Output: [0,0,1,1,2,2]
+
+    Example 2:
+    Input: nums = [2,0,1]
+    Output: [0,1,2]
+
+    Constraints:
+    * n == nums.length
+    * 1 <= n <= 300
+    * nums[i] is either 0, 1, or 2.
+
+    Follow up: Could you come up with a one-pass algorithm using only constant
+               extra space?*/
+
+    public void sortColors(int[] nums) {
+        int lo = 0, mid = 0, hi = nums.length-1;
+        while (mid <= hi)
+            if (nums[mid] == 0) {
+                int temp = nums[lo];
+                nums[lo++] = nums[mid];
+                nums[mid++] = temp;
+            } else if (nums[mid] == 1) ++mid;
+            else {
+                int temp = nums[mid];
+                nums[mid] = nums[hi];
+                nums[hi--] = temp;
+            }
+    }
+
+
     /*78. Subsets (Medium)
     Given an integer array nums of unique elements, return all possible subsets
     (the power set). The solution set must not contain duplicate subsets. Return
--- leetcode.ts
+++ leetcode.ts
@@ -129,6 +129,39 @@ function lengthOfLastWord(s: string): number {
 };
 
 
+/*75. Sort Colors (Medium)
+Given an array nums with n objects colored red, white, or blue, sort them
+in-place so that objects of the same color are adjacent, with the colors in
+the order red, white, and blue. We will use the integers 0, 1, and 2 to
+represent the color red, white, and blue, respectively. You must solve this
+problem without using the library's sort function.
+
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+* n == nums.length
+* 1 <= n <= 300
+* nums[i] is either 0, 1, or 2.
+
+Follow up: Could you come up with a one-pass algorithm using only constant
+           extra space?*/
+
+function sortColors(nums: number[]): void {
+    let lo = 0, mid = 0, hi = nums.length-1;
+    while (mid <= hi)
+        if (nums[mid] == 0)
+            [nums[lo++], nums[mid++]] = [nums[mid], nums[lo]];
+        else if (nums[mid] == 1) ++mid;
+        else [nums[mid], nums[hi--]] = [nums[hi], nums[mid]];
+};
+
+
 /*78. Subsets (Medium)
 Given an integer array nums of unique elements, return all possible subsets
 (the power set). The solution set must not contain duplicate subsets. Return
