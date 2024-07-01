9cbe4a5fbc370e41f1bb553aaa2250e7d59cf2be
Ye Gao <gaoyescm@amazon.com>
2024-07-01 13:18:17 -0400

    solve'em all (1550)

--- leetcode.py
+++ leetcode.py
@@ -37858,6 +37858,33 @@ class UnionFind:
         return ans
 
 
+    """1550. Three Consecutive Odds (Easy)
+    Given an integer array arr, return true if there are three consecutive odd
+    numbers in the array. Otherwise, return false.
+
+    Example 1:
+    Input: arr = [2,6,4,1]
+    Output: false
+    Explanation: There are no three consecutive odds.
+
+    Example 2:
+    Input: arr = [1,2,34,3,4,5,7,23,12]
+    Output: true
+    Explanation: [5,7,23] are three consecutive odds.
+
+    Constraints:
+    * 1 <= arr.length <= 1000
+    * 1 <= arr[i] <= 1000"""
+
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        cnt = 0
+        for x in arr:
+            if x & 1: cnt += 1
+            else: cnt = 0
+            if cnt == 3: return True
+        return False
+
+
     """1552. Magnetic Force Between Two Balls (Medium)
     In the universe Earth C-137, Rick discovered a special form of magnetic
     force between two balls if they are put in his new invented basket. Rick has
