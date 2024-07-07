e3bcd11d27fa1ef9d5afaaadc89d4f70b237ae52
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-07-07 10:40:28 -0400

    solve'em all (1518)

--- leetcode.js
+++ leetcode.js
@@ -2734,6 +2734,41 @@ var minDifference = function(nums) {
 };
 
 
+/*1518. Water Bottles (Easy)
+There are numBottles water bottles that are initially full of water. You can
+exchange numExchange empty water bottles from the market with one full water
+bottle. The operation of drinking a full water bottle turns it into an empty
+bottle. Given the two integers numBottles and numExchange, return the
+maximum number of water bottles you can drink.
+
+Example 1:
+Input: numBottles = 9, numExchange = 3
+Output: 13
+Explanation: You can exchange 3 empty bottles to get 1 full water bottle.
+             Number of water bottles you can drink: 9 + 3 + 1 = 13.
+
+Example 2:
+Input: numBottles = 15, numExchange = 4
+Output: 19
+Explanation: You can exchange 4 empty bottles to get 1 full water bottle.
+             Number of water bottles you can drink: 15 + 3 + 1 = 19.
+
+Constraints:
+* 1 <= numBottles <= 100
+* 2 <= numExchange <= 100*/
+
+var numWaterBottles = function(numBottles, numExchange) {
+    let ans = 0, empty = 0;
+    while (numBottles) {
+        ans += numBottles;
+        numBottles += empty;
+        empty = numBottles % numExchange;
+        numBottles = Math.floor(numBottles / numExchange);
+    }
+    return ans;
+};
+
+
 /*1544. Make The String Great (Easy)
 Given a string s of lower and upper case English letters. A good string is a
 string which doesn't have two adjacent characters s[i] and s[i + 1] where:
