e2308f21635f6887408cbd2fbb05e8c41b0c7225
Ye Gao <gaoyescm@amazon.com>
2024-07-10 18:31:16 -0400

    solve'em all (1598)

--- leetcode.js
+++ leetcode.js
@@ -2888,6 +2888,51 @@ var maxDistance = function(position, m) {
 };
 
 
+/*1598. Crawler Log Folder (Easy)
+The Leetcode file system keeps a log each time some user performs a change
+folder operation. The operations are described below:
+* "../" : Move to the parent folder of the current folder. (If you are
+  already in the main folder, remain in the same folder).
+* "./" : Remain in the same folder.
+* "x/" : Move to the child folder named x (This folder is guaranteed to
+  always exist).
+You are given a list of strings logs where logs[i] is the operation
+performed by the user at the ith step. The file system starts in the main
+folder, then the operations in logs are performed. Return the minimum number
+of operations needed to go back to the main folder after the change folder
+operations.
+
+Example 1:
+Input: logs = ["d1/","d2/","../","d21/","./"]
+Output: 2
+Explanation: Use this change folder operation "../" 2 times and go back to
+             the main folder.
+
+Example 2:
+Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
+Output: 3
+
+Example 3:
+Input: logs = ["d1/","../","../","../"]
+Output: 0
+
+Constraints:
+* 1 <= logs.length <= 10^3
+* 2 <= logs[i].length <= 10
+* logs[i] contains lowercase English letters, digits, '.', and '/'.
+* logs[i] follows the format described in the statement.
+* Folder names consist of lowercase English letters and digits.*/
+
+var minOperations = function(logs) {
+    let ans = 0;
+    for (const log of logs)
+        if (log === "./") continue;
+        else if (log === "../") ans = Math.max(0, --ans);
+        else ++ans;
+    return ans;
+};
+
+
 /*1609. Even Odd Tree (Medium)
 A binary tree is named Even-Odd if it meets the following conditions:
 * The root of the binary tree is at level index 0, its children are at level
--- leetcode.py
+++ leetcode.py
@@ -38715,6 +38715,50 @@ class UnionFind:
         return stack.pop()
 
 
+    """1598. Crawler Log Folder (Easy)
+    The Leetcode file system keeps a log each time some user performs a change
+    folder operation. The operations are described below:
+    * "../" : Move to the parent folder of the current folder. (If you are
+      already in the main folder, remain in the same folder).
+    * "./" : Remain in the same folder.
+    * "x/" : Move to the child folder named x (This folder is guaranteed to
+      always exist).
+    You are given a list of strings logs where logs[i] is the operation
+    performed by the user at the ith step. The file system starts in the main
+    folder, then the operations in logs are performed. Return the minimum number
+    of operations needed to go back to the main folder after the change folder
+    operations.
+
+    Example 1:
+    Input: logs = ["d1/","d2/","../","d21/","./"]
+    Output: 2
+    Explanation: Use this change folder operation "../" 2 times and go back to
+                 the main folder.
+
+    Example 2:
+    Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
+    Output: 3
+
+    Example 3:
+    Input: logs = ["d1/","../","../","../"]
+    Output: 0
+
+    Constraints:
+    * 1 <= logs.length <= 10^3
+    * 2 <= logs[i].length <= 10
+    * logs[i] contains lowercase English letters, digits, '.', and '/'.
+    * logs[i] follows the format described in the statement.
+    * Folder names consist of lowercase English letters and digits."""
+
+    def minOperations(self, logs: List[str]) -> int:
+        ans = 0
+        for log in logs:
+            if log == "./": continue
+            elif log == "../": ans = max(0, ans-1)
+            else: ans += 1
+        return ans
+
+
     """1602. Find Nearest Right Node in Binary Tree (Medium)
     Given the root of a binary tree and a node u in the tree, return the
     nearest node on the same level that is to the right of u, or return null
