91e8f56a9438e4db57324dc71558867f7bf1950f
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-07-13 16:15:37 -0400

    solve'em all (2751)

--- leetcode.cpp
+++ leetcode.cpp
@@ -62195,6 +62195,85 @@ public:
     }
 
 
+    /*2751. Robot Collisions (Hard)
+    There are n 1-indexed robots, each having a position on a line, health, and
+    movement direction. You are given 0-indexed integer arrays positions,
+    healths, and a string directions (directions[i] is either 'L' for left or
+    'R' for right). All integers in positions are unique. All robots start
+    moving on the line simultaneously at the same speed in their given
+    directions. If two robots ever share the same position while moving, they
+    will collide. If two robots collide, the robot with lower health is removed
+    from the line, and the health of the other robot decreases by one. The
+    surviving robot continues in the same direction it was going. If both robots
+    have the same health, they are both removed from the line. Your task is to
+    determine the health of the robots that survive the collisions, in the same
+    order that the robots were given, i.e. final heath of robot 1 (if survived),
+    final health of robot 2 (if survived), and so on. If there are no survivors,
+    return an empty array. Return an array containing the health of the
+    remaining robots (in the order they were given in the input), after no
+    further collisions can occur. Note: The positions may be unsorted.
+
+    Example 1:
+    Input: positions = [5,4,3,2,1], healths = [2,17,9,15,10], directions = "RRRRR"
+    Output: [2,17,9,15,10]
+    Explanation: No collision occurs in this example, since all robots are
+                 moving in the same direction. So, the health of the robots in
+                 order from the first robot is returned, [2, 17, 9, 15, 10].
+
+    Example 2:
+    Input: positions = [3,5,2,6], healths = [10,10,15,12], directions = "RLRL"
+    Output: [14]
+    Explanation: There are 2 collisions in this example. Firstly, robot 1 and
+                 robot 2 will collide, and since both have the same health, they
+                 will be removed from the line. Next, robot 3 and robot 4 will
+                 collide and since robot 4's health is smaller, it gets removed,
+                 and robot 3's health becomes 15 - 1 = 14. Only robot 3 remains,
+                 so we return [14].
+
+    Example 3:
+    Input: positions = [1,2,5,6], healths = [10,10,11,11], directions = "RLRL"
+    Output: []
+    Explanation: Robot 1 and robot 2 will collide and since both have the same
+                 health, they are both removed. Robot 3 and 4 will collide and
+                 since both have the same health, they are both removed. So, we
+                 return an empty array, [].
+
+    Constraints:
+    * 1 <= positions.length == healths.length == directions.length == n <= 10^5
+    * 1 <= positions[i], healths[i] <= 10^9
+    * directions[i] == 'L' or directions[i] == 'R'
+    * All values in positions are distinct*/
+
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n = positions.size();
+        vector<int> ans(n);
+        vector<tuple<int, int, char, int>> aug;
+        for (int i = 0; i < n; ++i)
+            aug.emplace_back(positions[i], healths[i], directions[i], i);
+        sort(aug.begin(), aug.end());
+        stack<tuple<int, int, int, int>> stk;
+        for (auto& [p, h, d, i] : aug)
+            if (d == 'R') stk.emplace(p, h, d, i);
+            else {
+                while (stk.size() && h) {
+                    auto [pp, hh, dd, ii] = stk.top(); stk.pop();
+                    if (hh < h) --h;
+                    else {
+                        if (hh > h) stk.emplace(pp, --hh, dd, ii);
+                        h = 0;
+                    }
+                }
+                ans[i] = h;
+            }
+        while (stk.size()) {
+            auto [p, h, d, i] = stk.top(); stk.pop();
+            ans[i] = h;
+        }
+        ans.erase(remove(ans.begin(), ans.end(), 0), ans.end());
+        return ans;
+    }
+
+
     /*2764. Is Array a Preorder of Some Binary Tree (Medium）
     Given a 0-indexed integer 2D array nodes, your task is to determine if the
     given array represents the preorder traversal of some binary tree. For each
--- leetcode.js
+++ leetcode.js
@@ -4648,6 +4648,82 @@ var maximumNumberOfStringPairs = function(words) {
 };
 
 
+/*2751. Robot Collisions (Hard)
+There are n 1-indexed robots, each having a position on a line, health, and
+movement direction. You are given 0-indexed integer arrays positions,
+healths, and a string directions (directions[i] is either 'L' for left or
+'R' for right). All integers in positions are unique. All robots start
+moving on the line simultaneously at the same speed in their given
+directions. If two robots ever share the same position while moving, they
+will collide. If two robots collide, the robot with lower health is removed
+from the line, and the health of the other robot decreases by one. The
+surviving robot continues in the same direction it was going. If both robots
+have the same health, they are both removed from the line. Your task is to
+determine the health of the robots that survive the collisions, in the same
+order that the robots were given, i.e. final heath of robot 1 (if survived),
+final health of robot 2 (if survived), and so on. If there are no survivors,
+return an empty array. Return an array containing the health of the
+remaining robots (in the order they were given in the input), after no
+further collisions can occur. Note: The positions may be unsorted.
+
+Example 1:
+Input: positions = [5,4,3,2,1], healths = [2,17,9,15,10], directions = "RRRRR"
+Output: [2,17,9,15,10]
+Explanation: No collision occurs in this example, since all robots are
+             moving in the same direction. So, the health of the robots in
+             order from the first robot is returned, [2, 17, 9, 15, 10].
+
+Example 2:
+Input: positions = [3,5,2,6], healths = [10,10,15,12], directions = "RLRL"
+Output: [14]
+Explanation: There are 2 collisions in this example. Firstly, robot 1 and
+             robot 2 will collide, and since both have the same health, they
+             will be removed from the line. Next, robot 3 and robot 4 will
+             collide and since robot 4's health is smaller, it gets removed,
+             and robot 3's health becomes 15 - 1 = 14. Only robot 3 remains,
+             so we return [14].
+
+Example 3:
+Input: positions = [1,2,5,6], healths = [10,10,11,11], directions = "RLRL"
+Output: []
+Explanation: Robot 1 and robot 2 will collide and since both have the same
+             health, they are both removed. Robot 3 and 4 will collide and
+             since both have the same health, they are both removed. So, we
+             return an empty array, [].
+
+Constraints:
+* 1 <= positions.length == healths.length == directions.length == n <= 10^5
+* 1 <= positions[i], healths[i] <= 10^9
+* directions[i] == 'L' or directions[i] == 'R'
+* All values in positions are distinct*/
+
+var survivedRobotsHealths = function(positions, healths, directions) {
+    const n = positions.length, ans = Array(n).fill(0), stack = [], vals = [];
+    for (let i = 0; i < n; ++i)
+        vals.push([positions[i], healths[i], directions[i], i]);
+    vals.sort((x, y) => x[0]-y[0]);
+    for (let [p, h, d, i] of vals) {
+        if (d == 'R') stack.push([p, h, d, i]);
+        else {
+            while (stack.length && h) {
+                const [pp, hh, dd, ii] = stack.pop();
+                if (hh < h) --h;
+                else {
+                    if (hh > h) stack.push([pp, hh-1, dd, ii]);
+                    h = 0;
+                }
+            }
+            ans[i] = h;
+        }
+    }
+    while (stack.length) {
+        const [p, h, d, i] = stack.pop();
+        ans[i] = h;
+    }
+    return ans.filter(x => x != 0);
+};
+
+
 /*2758. Next Day (Easy)
 Write code that enhances all date objects such that you can call the
 date.nextDay() method on any date object and it will return the next day in the
