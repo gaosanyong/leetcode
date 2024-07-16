dfe6038ce2f4d384ae05b8eccf8ac8293af788b9
Ye Gao <gaoyescm@amazon.com>
2024-07-16 16:05:26 -0400

    biweekly 134 (3206|3207|3208|3209)

--- leetcode.java
+++ leetcode.java
@@ -29021,6 +29021,186 @@ class SegTreeLazy {
     }
 
 
+    /*3206. Alternating Groups I (Easy)
+    There is a circle of red and blue tiles. You are given an array of integers
+    colors. The color of tile i is represented by colors[i]:
+    * colors[i] == 0 means that tile i is red.
+    * colors[i] == 1 means that tile i is blue.
+    Every 3 contiguous tiles in the circle with alternating colors (the middle
+    tile has a different color from its left and right tiles) is called an
+    alternating group. Return the number of alternating groups. Note that since
+    colors represents a circle, the first and the last tiles are considered to
+    be next to each other.
+
+    Example 1:
+    Input: colors = [1,1,1]
+    Output: 0
+    Explanation:
+
+    Example 2:
+    Input: colors = [0,1,0,0,1]
+    Output: 3
+    Explanation:
+    Alternating groups:
+
+    Constraints:
+    * 3 <= colors.length <= 100
+    * 0 <= colors[i] <= 1*/
+
+    public int numberOfAlternatingGroups(int[] colors) {
+        int ans = 0;
+        for (int i = 0, cnt = 0, n = colors.length; i < n+2; ++i) {
+            if (i > 0 && colors[(i-1)%n] == colors[i%n]) cnt = 0;
+            if (++cnt >= 3) ++ans;
+        }
+        return ans;
+    }
+
+
+    /*3207. Maximum Points After Enemy Battles (Medium)
+    You are given an integer array enemyEnergies denoting the energy values of
+    various enemies. You are also given an integer currentEnergy denoting the
+    amount of energy you have initially. You start with 0 points, and all the
+    enemies are unmarked initially. You can perform either of the following
+    operations zero or multiple times to gain points:
+    * Choose an unmarked enemy, i, such that currentEnergy >= enemyEnergies[i].
+      By choosing this option:
+      + You gain 1 point.
+      + Your energy is reduced by the enemy's energy, i.e. currentEnergy =
+        currentEnergy - enemyEnergies[i].
+    * If you have at least 1 point, you can choose an unmarked enemy, i. By
+      choosing this option:
+      + Your energy increases by the enemy's energy, i.e. currentEnergy =
+        currentEnergy + enemyEnergies[i].
+      + The enemy i is marked.
+    Return an integer denoting the maximum points you can get in the end by
+    optimally performing operations.
+
+    Example 1:
+    Input: enemyEnergies = [3,2,2], currentEnergy = 2
+    Output: 3
+    Explanation: The following operations can be performed to get 3 points,
+                 which is the maximum:
+                 - First operation on enemy 1: points increases by 1, and
+                   currentEnergy decreases by 2. So, points = 1, and
+                   currentEnergy = 0.
+                 - Second operation on enemy 0: currentEnergy increases by 3,
+                   and enemy 0 is marked. So, points = 1, currentEnergy = 3, and
+                   marked enemies = [0].
+                 - First operation on enemy 2: points increases by 1, and
+                   currentEnergy decreases by 2. So, points = 2,
+                   currentEnergy = 1, and marked enemies = [0].
+                 - Second operation on enemy 2: currentEnergy increases by 2,
+                   and enemy 2 is marked. So, points = 2, currentEnergy = 3, and
+                   marked enemies = [0, 2].
+                 - First operation on enemy 1: points increases by 1, and
+                   currentEnergy decreases by 2. So, points = 3,
+                   currentEnergy = 1, and marked enemies = [0, 2].
+
+    Example 2:
+    Input: enemyEnergies = [2], currentEnergy = 10
+    Output: 5
+    Explanation: Performing the first operation 5 times on enemy 0 results in
+                 the maximum number of points.
+
+    Constraints:
+    * 1 <= enemyEnergies.length <= 10^5
+    * 1 <= enemyEnergies[i] <= 10^9
+    * 0 <= currentEnergy <= 10^9*/
+
+    public long maximumPoints(int[] enemyEnergies, int currentEnergy) {
+        int m = IntStream.of(enemyEnergies).min().getAsInt();
+        if (currentEnergy < m) return 0;
+        return (currentEnergy + IntStream.of(enemyEnergies).mapToLong(x -> (long) x).sum() - m)/m;
+    }
+
+
+    /*3208. Alternating Groups II (Medium）
+    There is a circle of red and blue tiles. You are given an array of integers
+    colors and an integer k. The color of tile i is represented by colors[i]:
+    * colors[i] == 0 means that tile i is red.
+    * colors[i] == 1 means that tile i is blue.
+    An alternating group is every k contiguous tiles in the circle with
+    alternating colors (each tile in the group except the first and last one has
+    a different color from its left and right tiles). Return the number of
+    alternating groups. Note that since colors represents a circle, the first
+    and the last tiles are considered to be next to each other.
+
+    Example 1:
+    Input: colors = [0,1,0,1,0], k = 3
+    Output: 3
+    Explanation:
+    Alternating groups:
+
+    Example 2:
+    Input: colors = [0,1,0,0,1,0,1], k = 6
+    Output: 2
+    Explanation:
+    Alternating groups:
+
+    Example 3:
+    Input: colors = [1,1,0,1], k = 4
+    Output: 0
+    Explanation:
+
+    Constraints:
+    * 3 <= colors.length <= 10^5
+    * 0 <= colors[i] <= 1
+    * 3 <= k <= colors.length*/
+
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int ans = 0;
+        for (int i = 0, cnt = 0, n = colors.length; i < n+k-1; ++i) {
+            if (i > 0 && colors[(i-1)%n] == colors[i%n]) cnt = 0;
+            if (++cnt >= k) ++ans;
+        }
+        return ans;
+    }
+
+
+    /*3209. Number of Subarrays With AND Value of K (Hard)
+    Given an array of integers nums and an integer k, return the number of
+    subarrays of nums where the bitwise AND of the elements of the subarray
+    equals k.
+
+    Example 1:
+    Input: nums = [1,1,1], k = 1
+    Output: 6
+    Explanation: All subarrays contain only 1's.
+
+    Example 2:
+    Input: nums = [1,1,2], k = 1
+    Output: 3
+    Explanation: Subarrays having an AND value of 1 are: [1,1,2], [1,1,2], [1,1,2].
+
+    Example 3:
+    Input: nums = [1,2,3], k = 2
+    Output: 2
+    Explanation: Subarrays having an AND value of 2 are: [1,2,3], [1,2,3].
+
+    Constraints:
+    * 1 <= nums.length <= 10^5
+    * 0 <= nums[i], k <= 10^9*/
+
+    public long countSubarrays(int[] nums, int k) {
+        long ans = 0;
+        Map<Integer, Integer> freq = new HashMap();
+        for (var x : nums) {
+            Map<Integer, Integer> temp = new HashMap();
+            if ((k & x) == k) {
+                freq.merge(x, 1, Integer::sum);
+                for (var elem : freq.entrySet()) {
+                    int key = elem.getKey(), val = elem.getValue();
+                    temp.merge(key & x, val, Integer::sum);
+                }
+                ans += temp.getOrDefault(k, 0);
+            }
+            freq = temp;
+        }
+        return ans;
+    }
+
+
     /*3216. Lexicographically Smallest String After a Swap (Easy)
     Given a string s containing only digits, return the lexicographically
     smallest string that can be obtained after swapping adjacent digits in s
