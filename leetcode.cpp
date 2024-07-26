fa17c1442ac062a178a5df78f83b392a0c6df54e
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-07-26 17:14:57 -0400

    solve'em all (48)

--- leetcode.cpp
+++ leetcode.cpp
@@ -1463,12 +1463,12 @@ public:
     * -1000 <= matrix[i][j] <= 1000*/
 
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        for (int i = 0; i < n; ++i)
+        int m = matrix.size();
+        reverse(matrix.begin(), matrix.end());
+        for (int i = 0; i < m; ++i)
             for (int j = 0; j < i; ++j)
                 swap(matrix[i][j], matrix[j][i]);
-        for (auto& row : matrix)
-            reverse(row.begin(), row.end());
+    }
 
 
     /*49. Group Anagrams (Medium)
--- leetcode.java
+++ leetcode.java
@@ -490,6 +490,41 @@ class Solution {
     }
 
 
+    /*48. Rotate Image (Medium)
+    You are given an n x n 2D matrix representing an image, rotate the image by
+    90 degrees (clockwise). You have to rotate the image in-place, which means
+    you have to modify the input 2D matrix directly. DO NOT allocate another 2D
+    matrix and do the rotation.
+
+    Example 1:
+    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+    Example 2:
+    Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+    Constraints:
+    * n == matrix.length == matrix[i].length
+    * 1 <= n <= 20
+    * -1000 <= matrix[i][j] <= 1000*/
+
+    public void rotate(int[][] matrix) {
+        int m = matrix.length;
+        for (int i = 0; i < m/2; ++i) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[m-1-i];
+            matrix[m-1-i] = temp;
+        }
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+    }
+
+
     /*49. Group Anagrams (Medium)
     Given an array of strings strs, group the anagrams together. You can return
     the answer in any order. An Anagram is a word or phrase formed by
