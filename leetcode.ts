ebefc8bbf20b379069eb81474e2b589e09cd74f6
Ye Gao <gaoyescm@amazon.com>
2024-07-31 22:53:02 -0400

    solve'em all (2678)

--- leetcode.ts
+++ leetcode.ts
@@ -4701,6 +4701,39 @@ function splitCircularLinkedList(list: ListNode | null): Array<ListNode | null> 
 };
 
 
+/*2678. Number of Senior Citizens (Easy)
+You are given a 0-indexed array of strings details. Each element of details
+provides information about a given passenger compressed into a string of
+length 15. The system is such that:
+* The first ten characters consist of the phone number of passengers.
+* The next character denotes the gender of the person.
+* The following two characters are used to indicate the age of the person.
+* The last two characters determine the seat allotted to that person.
+Return the number of passengers who are strictly more than 60 years old.
+
+Example 1:
+Input: details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+Output: 2
+Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40.
+             Thus, there are 2 people who are over 60 years old.
+
+Example 2:
+Input: details = ["1313579440F2036","2921522980M5644"]
+Output: 0
+Explanation: None of the passengers are older than 60.
+
+Constraints:
+* 1 <= details.length <= 100
+* details[i].length == 15
+* details[i] consists of digits from '0' to '9'.
+* details[i][10] is either 'M' or 'F' or 'O'.
+* The phone numbers and seat numbers of the passengers are distinct.*/
+
+function countSeniors(details: string[]): number {
+    return details.map(d => Number(d.substring(11, 13))).reduce((s, x) => s + (x > 60 ? 1 : 0), 0);
+};
+
+
 /*2690. Infinite Method Object (Easy)
 Write a function that returns an infinite-method object. An infinite-method
 object is defined as an object that allows you to call any method and it will
