676a44ef3ca87fb99a069ecddf694796f63fd119
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-08-02 23:00:21 -0400

    solve'em all (1460)

--- leetcode.cpp
+++ leetcode.cpp
@@ -29201,6 +29201,47 @@ public:
     }
 
 
+    /*1460. Make Two Arrays Equal by Reversing Subarrays (Easy)
+    You are given two integer arrays of equal length target and arr. In one
+    step, you can select any non-empty subarray of arr and reverse it. You are
+    allowed to make any number of steps. Return true if you can make arr equal
+    to target or false otherwise.
+
+    Example 1:
+    Input: target = [1,2,3,4], arr = [2,4,1,3]
+    Output: true
+    Explanation: You can follow the next steps to convert arr to target:
+                 1- Reverse subarray [2,4,1], arr becomes [1,4,2,3]
+                 2- Reverse subarray [4,2], arr becomes [1,2,4,3]
+                 3- Reverse subarray [4,3], arr becomes [1,2,3,4]
+                 There are multiple ways to convert arr to target, this is not
+                 the only way to do so.
+
+    Example 2:
+    Input: target = [7], arr = [7]
+    Output: true
+    Explanation: arr is equal to target without any reverses.
+
+    Example 3:
+    Input: target = [3,7,9], arr = [3,7,11]
+    Output: false
+    Explanation: arr does not have value 9 and it can never be converted to
+                 target.
+
+    Constraints:
+    * target.length == arr.length
+    * 1 <= target.length <= 1000
+    * 1 <= target[i] <= 1000
+    * 1 <= arr[i] <= 1000*/
+
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        unordered_map<int, int> freq;
+        for (auto& x : target) ++freq[x];
+        for (auto& x : arr) --freq[x];
+        return all_of(freq.begin(), freq.end(), [&](auto& elem) { return elem.second == 0; });
+    }
+
+
     /*1463. Cherry Pickup II (Hard)
     You are given a rows x cols matrix grid representing a field of cherries
     where grid[i][j] represents the number of cherries that you can collect
--- leetcode.ts
+++ leetcode.ts
@@ -2896,6 +2896,47 @@ function countTriplets(arr: number[]): number {
 };
 
 
+/*1460. Make Two Arrays Equal by Reversing Subarrays (Easy)
+You are given two integer arrays of equal length target and arr. In one
+step, you can select any non-empty subarray of arr and reverse it. You are
+allowed to make any number of steps. Return true if you can make arr equal
+to target or false otherwise.
+
+Example 1:
+Input: target = [1,2,3,4], arr = [2,4,1,3]
+Output: true
+Explanation: You can follow the next steps to convert arr to target:
+             1- Reverse subarray [2,4,1], arr becomes [1,4,2,3]
+             2- Reverse subarray [4,2], arr becomes [1,2,4,3]
+             3- Reverse subarray [4,3], arr becomes [1,2,3,4]
+             There are multiple ways to convert arr to target, this is not
+             the only way to do so.
+
+Example 2:
+Input: target = [7], arr = [7]
+Output: true
+Explanation: arr is equal to target without any reverses.
+
+Example 3:
+Input: target = [3,7,9], arr = [3,7,11]
+Output: false
+Explanation: arr does not have value 9 and it can never be converted to
+             target.
+
+Constraints:
+* target.length == arr.length
+* 1 <= target.length <= 1000
+* 1 <= target[i] <= 1000
+* 1 <= arr[i] <= 1000*/
+
+function canBeEqual(target: number[], arr: number[]): boolean {
+    const freq = new Map();
+    for (const x of target) freq.set(x, (freq.get(x) ?? 0) + 1);
+    for (const x of arr) freq.set(x, (freq.get(x) ?? 0) - 1);
+    return [...freq.values()].every(x => x == 0);
+};
+
+
 /*1482. Minimum Number of Days to Make m Bouquets (Medium)
 You are given an integer array bloomDay, an integer m and an integer k. You
 want to make m bouquets. To make a bouquet, you need to use k adjacent
