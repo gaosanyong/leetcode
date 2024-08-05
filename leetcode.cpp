5cc715d8fb780762d1a378b1dcefee3cc392c068
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-08-04 22:24:38 -0400

    solve'em all (2053)

--- leetcode.cpp
+++ leetcode.cpp
@@ -44048,6 +44048,48 @@ public:
     }
 
 
+    /*2053. Kth Distinct String in an Array (Easy)
+    A distinct string is a string that is present only once in an array. Given
+    an array of strings arr, and an integer k, return the kth distinct string
+    present in arr. If there are fewer than k distinct strings, return an empty
+    string "". Note that the strings are considered in the order in which they
+    appear in the array.
+
+    Example 1:
+    Input: arr = ["d","b","c","b","c","a"], k = 2
+    Output: "a"
+    Explanation: The only distinct strings in arr are "d" and "a".
+                 "d" appears 1st, so it is the 1st distinct string.
+                 "a" appears 2nd, so it is the 2nd distinct string.
+                 Since k == 2, "a" is returned.
+
+    Example 2:
+    Input: arr = ["aaa","aa","a"], k = 1
+    Output: "aaa"
+    Explanation: All strings in arr are distinct, so the 1st string "aaa" is
+                 returned.
+
+    Example 3:
+    Input: arr = ["a","b","a"], k = 3
+    Output: ""
+    Explanation: The only distinct string is "b". Since there are fewer than 3
+                 distinct strings, we return an empty string "".
+
+    Constraints:
+    * 1 <= k <= arr.length <= 1000
+    * 1 <= arr[i].length <= 5
+    * arr[i] consists of lowercase English letters.*/
+
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string, int> freq;
+        for (auto& s : arr) ++freq[s];
+        for (auto& s : arr)
+            if (freq[s] == 1 && --k == 0)
+                return s;
+        return "";
+    }
+
+
     /*2057. Smallest Index With Equal Value (Easy)
     Given a 0-indexed integer array nums, return the smallest index i of nums
     such that i mod 10 == nums[i], or -1 if such index does not exist. x mod y
--- leetcode.py
+++ leetcode.py
@@ -56548,9 +56548,10 @@ class Trie:
 
     def kthDistinct(self, arr: List[str], k: int) -> str:
         freq = Counter(arr)
-        for x in arr:
-            if freq[x] == 1: k -= 1
-            if k == 0: return x
+        for s in arr:
+            if freq[s] == 1:
+                k -= 1
+                if k == 0: return s
         return ""
 
 
