cfd52b54c583d26dd2adc5877c6a4c7ee1cb8807
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-08-29 14:06:52 -0400

    solve'em all (947)

--- leetcode.cpp
+++ leetcode.cpp
@@ -22356,6 +22356,70 @@ public:
     }
 
 
+    /*947. Most Stones Removed with Same Row or Column (Medium)
+    On a 2D plane, we place n stones at some integer coordinate points. Each
+    coordinate point may have at most one stone. A stone can be removed if it
+    shares either the same row or the same column as another stone that has not
+    been removed. Given an array stones of length n where stones[i] = [xi, yi]
+    represents the location of the ith stone, return the largest possible number
+    of stones that can be removed.
+
+    Example 1:
+    Input: stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
+    Output: 5
+    Explanation: One way to remove 5 stones is as follows:
+                 1. Remove stone [2,2] because it shares the same row as [2,1].
+                 2. Remove stone [2,1] because it shares the same column as
+                    [0,1].
+                 3. Remove stone [1,2] because it shares the same row as [1,0].
+                 4. Remove stone [1,0] because it shares the same column as
+                    [0,0].
+                 5. Remove stone [0,1] because it shares the same row as [0,0].
+                 Stone [0,0] cannot be removed since it does not share a
+                 row/column with another stone still on the plane.
+
+    Example 2:
+    Input: stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
+    Output: 3
+    Explanation: One way to make 3 moves is as follows:
+                 1. Remove stone [2,2] because it shares the same row as [2,0].
+                 2. Remove stone [2,0] because it shares the same column as
+                    [0,0].
+                 3. Remove stone [0,2] because it shares the same row as [0,0].
+                 Stones [0,0] and [1,1] cannot be removed since they do not
+                 share a row/column with another stone still on the plane.
+
+    Example 3:
+    Input: stones = [[0,0]]
+    Output: 0
+    Explanation: [0,0] is the only stone on the plane, so you cannot remove it.
+
+    Constraints:
+    * 1 <= stones.length <= 1000
+    * 0 <= xi, yi <= 10^4
+    * No two stones are at the same coordinate point.*/
+
+    int removeStones(vector<vector<int>>& stones) {
+        unordered_map<int, int> parent;
+
+        function<int(int)> find = [&](int p) {
+            if (!parent.contains(p)) parent[p] = p;
+            if (p != parent[p])
+                parent[p] = find(parent[p]);
+            return parent[p];
+        };
+
+        for (auto& s : stones) {
+            int x = s[0], y = s[1];
+            parent[find(x)] = find(-y-1);
+        }
+        unordered_set<int> group;
+        for (auto& s : stones)
+            group.insert(find(s[0]));
+        return stones.size() - group.size();
+    }
+
+
     /*948. Bag of Tokens (Medium)
     You start with an initial power of power, an initial score of 0, and a bag
     of tokens given as an integer array tokens, where each tokens[i] donates the
--- leetcode.py
+++ leetcode.py
@@ -24561,6 +24561,64 @@ class Solution:
         return not stack
 
 
+    """947. Most Stones Removed with Same Row or Column (Medium)
+    On a 2D plane, we place n stones at some integer coordinate points. Each
+    coordinate point may have at most one stone. A stone can be removed if it
+    shares either the same row or the same column as another stone that has not
+    been removed. Given an array stones of length n where stones[i] = [xi, yi]
+    represents the location of the ith stone, return the largest possible number
+    of stones that can be removed.
+
+    Example 1:
+    Input: stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
+    Output: 5
+    Explanation: One way to remove 5 stones is as follows:
+                 1. Remove stone [2,2] because it shares the same row as [2,1].
+                 2. Remove stone [2,1] because it shares the same column as
+                    [0,1].
+                 3. Remove stone [1,2] because it shares the same row as [1,0].
+                 4. Remove stone [1,0] because it shares the same column as
+                    [0,0].
+                 5. Remove stone [0,1] because it shares the same row as [0,0].
+                 Stone [0,0] cannot be removed since it does not share a
+                 row/column with another stone still on the plane.
+
+    Example 2:
+    Input: stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
+    Output: 3
+    Explanation: One way to make 3 moves is as follows:
+                 1. Remove stone [2,2] because it shares the same row as [2,0].
+                 2. Remove stone [2,0] because it shares the same column as
+                    [0,0].
+                 3. Remove stone [0,2] because it shares the same row as [0,0].
+                 Stones [0,0] and [1,1] cannot be removed since they do not
+                 share a row/column with another stone still on the plane.
+
+    Example 3:
+    Input: stones = [[0,0]]
+    Output: 0
+    Explanation: [0,0] is the only stone on the plane, so you cannot remove it.
+
+    Constraints:
+    * 1 <= stones.length <= 1000
+    * 0 <= xi, yi <= 10^4
+    * No two stones are at the same coordinate point."""
+
+    def removeStones(self, stones: List[List[int]]) -> int:
+        parent = {}
+
+        def find(p):
+            if p not in parent: parent[p] = p
+            if p != parent[p]:
+                parent[p] = find(parent[p])
+            return parent[p]
+
+        for x, y in stones:
+            parent[find(x)] = find(~y)
+        group = {find(x) for x, _ in stones}
+        return len(stones) - len(group)
+
+
     """948. Bag of Tokens (Medium)
     You have an initial power of P, an initial score of 0, and a bag of tokens
     where tokens[i] is the value of the ith token (0-indexed). Your goal is to
