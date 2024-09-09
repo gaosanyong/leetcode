553fe0d753f3c854f2f1fdf20f60ae05e723c311
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-09-08 23:11:49 -0400

    solve'em all (2326)

--- leetcode.cpp
+++ leetcode.cpp
@@ -51849,6 +51849,46 @@ public:
     }
 
 
+    /*2326. Spiral Matrix IV (Medium)
+    You are given two integers m and n, which represent the dimensions of a
+    matrix. You are also given the head of a linked list of integers. Generate
+    an m x n matrix that contains the integers in the linked list presented in
+    spiral order (clockwise), starting from the top-left of the matrix. If there
+    are remaining empty spaces, fill them with -1. Return the generated matrix.
+
+    Example 1:
+    Input: m = 3, n = 5, head = [3,0,2,6,8,1,7,9,4,2,5,5,0]
+    Output: [[3,0,2,6,8],[5,0,-1,-1,1],[5,2,4,9,7]]
+    Explanation: The diagram above shows how the values are printed in the
+                 matrix. Note that the remaining spaces in the matrix are filled
+                 with -1.
+
+    Example 2:
+    Input: m = 1, n = 4, head = [0,1,2]
+    Output: [[0,1,2,-1]]
+    Explanation: The diagram above shows how the values are printed from left to
+                 right in the matrix. The last space in the matrix is set to -1.
+
+    Constraints:
+    * 1 <= m, n <= 10^5
+    * 1 <= m * n <= 10^5
+    * The number of nodes in the list is in the range [1, m * n].
+    * 0 <= Node.val <= 1000*/
+
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> ans(m, vector<int>(n, -1));
+        ListNode *node = head;
+        for (int i = 0, j = 0, di = 0, dj = 1; node; i += di, j += dj, node = node->next) {
+            ans[i][j] = node->val;
+            if (!(0 <= i+di && i+di < m && 0 <= j+dj && j+dj < n && ans[i+di][j+dj] == -1)) {
+                swap(di, dj);
+                dj *= -1;
+            }
+        }
+        return ans;
+    }
+
+
     /*2330. Valid Palindrome IV (Medium)
     You are given a 0-indexed string s consisting of only lowercase English
     letters. In one operation, you can change any character of s to any other
