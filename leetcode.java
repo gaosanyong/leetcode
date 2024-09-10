229f8b48de99d973b6c06f073a8999bb433f0728
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-09-10 11:24:10 -0400

    biweekly 106 (2729|2730|2731|2732)

--- leetcode.java
+++ leetcode.java
@@ -19872,6 +19872,219 @@ class SegTreeLazy {
     }
 
 
+    /*2729. Check if The Number is Fascinating (Easy)
+    You are given an integer n that consists of exactly 3 digits. We call the
+    number n fascinating if, after the following modification, the resulting
+    number contains all the digits from 1 to 9 exactly once and does not contain
+    any 0's:
+    * Concatenate n with the numbers 2 * n and 3 * n.
+    Return true if n is fascinating, or false otherwise. Concatenating two
+    numbers means joining them together. For example, the concatenation of 121
+    and 371 is 121371.
+
+    Example 1:
+    Input: n = 192
+    Output: true
+    Explanation: We concatenate the numbers n = 192 and 2 * n = 384 and
+                 3 * n = 576. The resulting number is 192384576. This number
+                 contains all the digits from 1 to 9 exactly once.
+
+    Example 2:
+    Input: n = 100
+    Output: false
+    Explanation: We concatenate the numbers n = 100 and 2 * n = 200 and
+                 3 * n = 300. The resulting number is 100200300. This number
+                 does not satisfy any of the conditions.
+
+    Constraints: 100 <= n <= 999*/
+
+    public boolean isFascinating(int n) {
+        Set<Character> seen = new HashSet();
+        for (int x = n; x <= 3*n; x += n) {
+            if (x >= 1000) return false;
+            for (var ch : String.valueOf(x).toCharArray()) seen.add(ch);
+        }
+        return seen.size() == 9 && !seen.contains('0');
+    }
+
+
+    /*2730. Find the Longest Semi-Repetitive Substring (Medium)
+    You are given a digit string s that consists of digits from 0 to 9. A string
+    is called semi-repetitive if there is at most one adjacent pair of the same
+    digit. For example, "0010", "002020", "0123", "2002", and "54944" are semi-
+    repetitive while the following are not: "00101022" (adjacent same digit
+    pairs are 00 and 22), and "1101234883" (adjacent same digit pairs are 11 and
+    88). Return the length of the longest semi-repetitive substring of s.
+
+    Example 1:
+    Input: s = "52233"
+    Output: 4
+    Explanation: The longest semi-repetitive substring is "5223". Picking the
+                 whole string "52233" has two adjacent same digit pairs 22 and
+                 33, but at most one is allowed.
+
+    Example 2:
+    Input: s = "5494"
+    Output: 4
+    Explanation: s is a semi-repetitive string.
+
+    Example 3:
+    Input: s = "1111111"
+    Output: 2
+    Explanation: The longest semi-repetitive substring is "11". Picking the
+                 substring "111" has two adjacent same digit pairs, but at most
+                 one is allowed.
+
+    Constraints:
+    * 1 <= s.length <= 50
+    * '0' <= s[i] <= '9'*/
+
+    public int longestSemiRepetitiveSubstring(String s) {
+        int ans = 1;
+        for (int i = 1, ii = 0, q = 0; i < s.length(); ++i) {
+            if (s.charAt(i-1) == s.charAt(i)) {
+                if (q > 0) ii = q;
+                q = i;
+            }
+            ans = Math.max(ans, i-ii+1);
+        }
+        return ans;
+    }
+
+
+    /*2731. Movement of Robots (Medium)
+    Some robots are standing on an infinite number line with their initial
+    coordinates given by a 0-indexed integer array nums and will start moving
+    once given the command to move. The robots will move a unit distance each
+    second. You are given a string s denoting the direction in which robots will
+    move on command. 'L' means the robot will move towards the left side or
+    negative side of the number line, whereas 'R' means the robot will move
+    towards the right side or positive side of the number line. If two robots
+    collide, they will start moving in opposite directions. Return the sum of
+    distances between all the pairs of robots d seconds after the command. Since
+    the sum can be very large, return it modulo 109 + 7.
+
+    Note:
+    * For two robots at the index i and j, pair (i,j) and pair (j,i) are
+      considered the same pair.
+    * When robots collide, they instantly change their directions without
+      wasting any time.
+    * Collision happens when two robots share the same place in a moment.
+      - For example, if a robot is positioned in 0 going to the right and
+        another is positioned in 2 going to the left, the next second they'll be
+        both in 1 and they will change direction and the next second the first
+        one will be in 0, heading left, and another will be in 2, heading right.
+      - For example, if a robot is positioned in 0 going to the right and
+        another is positioned in 1 going to the left, the next second the first
+        one will be in 0, heading left, and another will be in 1, heading right.
+
+    Example 1:
+    Input: nums = [-2,0,2], s = "RLL", d = 3
+    Output: 8
+    Explanation: - After 1 second, the positions are [-1,-1,1]. Now, the robot
+                   at index 0 will move left, and the robot at index 1 will move
+                   right.
+                 - After 2 seconds, the positions are [-2,0,0]. Now, the robot
+                   at index 1 will move left, and the robot at index 2 will move
+                   right.
+                 - After 3 seconds, the positions are [-3,-1,1].
+                 - The distance between the robot at index 0 and 1 is
+                   abs(-3 - (-1)) = 2.
+                 - The distance between the robot at index 0 and 2 is
+                   abs(-3 - 1) = 4.
+                 - The distance between the robot at index 1 and 2 is
+                   abs(-1 - 1) = 2.
+                 - The sum of the pairs of all distances = 2 + 4 + 2 = 8.
+
+    Example 2:
+    Input: nums = [1,0], s = "RL", d = 2
+    Output: 5
+    Explanation: - After 1 second, the positions are [2,-1].
+                 - After 2 seconds, the positions are [3,-2].
+                 The distance between the two robots is abs(-2 - 3) = 5.
+
+    Constraints:
+    * 2 <= nums.length <= 10^5
+    * -2 * 10^9 <= nums[i] <= 2 * 10^9
+    * 0 <= d <= 10^9
+    * nums.length == s.length
+    * s consists of 'L' and 'R' only
+    * nums[i] will be unique.*/
+
+    public int sumDistance(int[] nums, String s, int d) {
+        final int mod = 1_000_000_007;
+        for (int i = 0; i < nums.length; ++i) {
+            if (s.charAt(i) == 'L') nums[i] -= d;
+            else nums[i] += d;
+        }
+        Arrays.sort(nums);
+        long ans = 0, prefix = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            ans = (ans + (long) i*nums[i] - prefix) % mod;
+            prefix = (prefix + nums[i]) % mod;
+        }
+        return (int) ans;
+    }
+
+
+    /*2732. Find a Good Subset of the Matrix (Hard)
+    You are given a 0-indexed m x n binary matrix grid. Let us call a non-empty
+    subset of rows good if the sum of each column of the subset is at most half
+    of the length of the subset. More formally, if the length of the chosen
+    subset of rows is k, then the sum of each column should be at most
+    floor(k / 2). Return an integer array that contains row indices of a good
+    subset sorted in ascending order. If there are multiple good subsets, you
+    can return any of them. If there are no good subsets, return an empty array.
+    A subset of rows of the matrix grid is any matrix that can be obtained by
+    deleting some (possibly none or all) rows from grid.
+
+    Example 1:
+    Input: grid = [[0,1,1,0],[0,0,0,1],[1,1,1,1]]
+    Output: [0,1]
+    Explanation: We can choose the 0th and 1st rows to create a good subset of rows.
+                 The length of the chosen subset is 2.
+                 - The sum of the 0th column is 0 + 0 = 0, which is at most half of the length of the subset.
+                 - The sum of the 1st column is 1 + 0 = 1, which is at most half of the length of the subset.
+                 - The sum of the 2nd column is 1 + 0 = 1, which is at most half of the length of the subset.
+                 - The sum of the 3rd column is 0 + 1 = 1, which is at most half of the length of the subset.
+
+    Example 2:
+    Input: grid = [[0]]
+    Output: [0]
+    Explanation: We can choose the 0th row to create a good subset of rows.
+                 The length of the chosen subset is 1.
+                 - The sum of the 0th column is 0, which is at most half of the length of the subset.
+
+    Example 3:
+    Input: grid = [[1,1,1],[1,1,1]]
+    Output: []
+    Explanation: It is impossible to choose any subset of rows to create a good subset.
+
+    Constraints:
+    * m == grid.length
+    * n == grid[i].length
+    * 1 <= m <= 10^4
+    * 1 <= n <= 5
+    * grid[i][j] is either 0 or 1.*/
+
+    public List<Integer> goodSubsetofBinaryMatrix(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < m; ++i) {
+            int v = 0;
+            for (int j = 0; j < n; ++j)
+                if (grid[i][j] == 1) v ^= 1<<j;
+            if (v == 0) return Arrays.asList(i);
+            for (var vv : seen.keySet()) {
+                int ii = seen.get(vv);
+                if ((vv & v) == 0) return Arrays.asList(ii, i);
+            }
+            seen.put(v, i);
+        }
+        return new ArrayList<>();
+    }
+
+
     /*2733. Neither Minimum nor Maximum (Easy)
     Given an integer array nums containing distinct positive integers, find and
     return any number from the array that is neither the minimum nor the maximum
