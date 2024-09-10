d39eaecf1b390f7dae1c2981a232c39b17056a76
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-09-10 11:08:07 -0400

    adjust order of 2733 & 2744

--- leetcode.py
+++ leetcode.py
@@ -81491,6 +81491,41 @@ class SegTreeLazy:
         return ans
 
 
+    """2733. Neither Minimum nor Maximum (Easy)
+    Given an integer array nums containing distinct positive integers, find and
+    return any number from the array that is neither the minimum nor the maximum
+    value in the array, or -1 if there is no such number. Return the selected
+    integer.
+
+    Example 1:
+    Input: nums = [3,2,1,4]
+    Output: 2
+    Explanation: In this example, the minimum value is 1 and the maximum value
+                 is 4. Therefore, either 2 or 3 can be valid answers.
+
+    Example 2:
+    Input: nums = [1,2]
+    Output: -1
+    Explanation: Since there is no number in nums that is neither the maximum
+                 nor the minimum, we cannot select a number that satisfies the
+                 given condition. Therefore, there is no answer.
+
+    Example 3:
+    Input: nums = [2,1,3]
+    Output: 2
+    Explanation: Since 2 is neither the maximum nor the minimum value in nums,
+                 it is the only valid answer.
+
+    Constraints:
+    * 1 <= nums.length <= 100
+    * 1 <= nums[i] <= 100
+    * All values in nums are distinct"""
+
+    def findNonMinOrMax(self, nums: List[int]) -> int:
+        if len(nums) < 3: return -1
+        return sorted(nums[:3])[1]
+
+
     """2737. Find the Closest Marked Node (Medium)
     You are given a positive integer n which is the number of nodes of a 0-
     indexed directed weighted graph and a 0-indexed 2D array edges where
@@ -81601,6 +81636,56 @@ class SegTreeLazy:
             ii = max(ii, prev[x])
             prev[x] = i
             ans += i-ii
+        return ans
+
+
+    """2744. Find Maximum Number of String Pairs (Easy)
+    You are given a 0-indexed array words consisting of distinct strings. The
+    string words[i] can be paired with the string words[j] if:
+    * The string words[i] is equal to the reversed string of words[j].
+    * 0 <= i < j < words.length.
+    Return the maximum number of pairs that can be formed from the array words.
+    Note that each string can belong in at most one pair.
+
+    Example 1:
+    Input: words = ["cd","ac","dc","ca","zz"]
+    Output: 2
+    Explanation: In this example, we can form 2 pair of strings in the following
+                 way:
+                 - We pair the 0th string with the 2nd string, as the reversed
+                   string of word[0] is "dc" and is equal to words[2].
+                 - We pair the 1st string with the 3rd string, as the reversed
+                   string of word[1] is "ca" and is equal to words[3].
+                 It can be proven that 2 is the maximum number of pairs that can
+                 be formed.
+
+    Example 2:
+    Input: words = ["ab","ba","cc"]
+    Output: 1
+    Explanation: In this example, we can form 1 pair of strings in the following
+                 way:
+                 - We pair the 0th string with the 1st string, as the reversed
+                   string of words[1] is "ab" and is equal to words[0].
+                 It can be proven that 1 is the maximum number of pairs that can
+                 be formed.
+
+    Example 3:
+    Input: words = ["aa","ab"]
+    Output: 0
+    Explanation: In this example, we are unable to form any pair of strings.
+
+    Constraints:
+    * 1 <= words.length <= 50
+    * words[i].length == 2
+    * words consists of distinct strings.
+    * words[i] contains only lowercase English letters."""
+
+    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
+        ans = 0
+        seen = set()
+        for word in words:
+            if word[::-1] in seen: ans += 1
+            seen.add(word)
         return ans
 
 
@@ -81901,91 +81986,6 @@ class SegTreeLazy:
             prefix = x
             if i >= k-1: prefix -= nums[i-k+1];
         return prefix == 0
-
-
-    """2733. Neither Minimum nor Maximum (Easy)
-    Given an integer array nums containing distinct positive integers, find and
-    return any number from the array that is neither the minimum nor the maximum
-    value in the array, or -1 if there is no such number. Return the selected
-    integer.
-
-    Example 1:
-    Input: nums = [3,2,1,4]
-    Output: 2
-    Explanation: In this example, the minimum value is 1 and the maximum value
-                 is 4. Therefore, either 2 or 3 can be valid answers.
-
-    Example 2:
-    Input: nums = [1,2]
-    Output: -1
-    Explanation: Since there is no number in nums that is neither the maximum
-                 nor the minimum, we cannot select a number that satisfies the
-                 given condition. Therefore, there is no answer.
-
-    Example 3:
-    Input: nums = [2,1,3]
-    Output: 2
-    Explanation: Since 2 is neither the maximum nor the minimum value in nums,
-                 it is the only valid answer.
-
-    Constraints:
-    * 1 <= nums.length <= 100
-    * 1 <= nums[i] <= 100
-    * All values in nums are distinct"""
-
-    def findNonMinOrMax(self, nums: List[int]) -> int:
-        if len(nums) < 3: return -1
-        return sorted(nums[:3])[1]
-
-
-    """2744. Find Maximum Number of String Pairs (Easy)
-    You are given a 0-indexed array words consisting of distinct strings. The
-    string words[i] can be paired with the string words[j] if:
-    * The string words[i] is equal to the reversed string of words[j].
-    * 0 <= i < j < words.length.
-    Return the maximum number of pairs that can be formed from the array words.
-    Note that each string can belong in at most one pair.
-
-    Example 1:
-    Input: words = ["cd","ac","dc","ca","zz"]
-    Output: 2
-    Explanation: In this example, we can form 2 pair of strings in the following
-                 way:
-                 - We pair the 0th string with the 2nd string, as the reversed
-                   string of word[0] is "dc" and is equal to words[2].
-                 - We pair the 1st string with the 3rd string, as the reversed
-                   string of word[1] is "ca" and is equal to words[3].
-                 It can be proven that 2 is the maximum number of pairs that can
-                 be formed.
-
-    Example 2:
-    Input: words = ["ab","ba","cc"]
-    Output: 1
-    Explanation: In this example, we can form 1 pair of strings in the following
-                 way:
-                 - We pair the 0th string with the 1st string, as the reversed
-                   string of words[1] is "ab" and is equal to words[0].
-                 It can be proven that 1 is the maximum number of pairs that can
-                 be formed.
-
-    Example 3:
-    Input: words = ["aa","ab"]
-    Output: 0
-    Explanation: In this example, we are unable to form any pair of strings.
-
-    Constraints:
-    * 1 <= words.length <= 50
-    * words[i].length == 2
-    * words consists of distinct strings.
-    * words[i] contains only lowercase English letters."""
-
-    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
-        ans = 0
-        seen = set()
-        for word in words:
-            if word[::-1] in seen: ans += 1
-            seen.add(word)
-        return ans
 
 
     """2778. Sum of Squares of Special Elements (Easy)
