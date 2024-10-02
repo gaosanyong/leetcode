db67d1851d1e54303ff24ee232592dfb0ad0f5a7
Ye Gao <gaoyescm@amazon.com>
2024-10-02 09:57:16 -0400

    solve'em all (1331)

--- leetcode.java
+++ leetcode.java
@@ -7246,6 +7246,43 @@ class Solution {
     }
 
 
+    /*1331. Rank Transform of an Array (Easy)
+    Given an array of integers arr, replace each element with its rank. The rank
+    represents how large the element is. The rank has the following rules:
+    * Rank is an integer starting from 1.
+    * The larger the element, the larger the rank. If two elements are equal,
+      their rank must be the same.
+    * Rank should be as small as possible.
+
+    Example 1:
+    Input: arr = [40,10,20,30]
+    Output: [4,1,2,3]
+    Explanation: 40 is the largest element. 10 is the smallest. 20 is the second
+                 smallest. 30 is the third smallest.
+
+    Example 2:
+    Input: arr = [100,100,100]
+    Output: [1,1,1]
+    Explanation: Same elements share the same rank.
+
+    Example 3:
+    Input: arr = [37,12,28,9,100,56,80,5,12]
+    Output: [5,3,4,2,8,6,7,1,3]
+
+    Constraints:
+    * 0 <= arr.length <= 10^5
+    * -10^9 <= arr[i] <= 10^9*/
+
+    public int[] arrayRankTransform(int[] arr) {
+        int[] vals = arr.clone();
+        Arrays.sort(vals);
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (var x : vals)
+            mp.putIfAbsent(x, mp.size()+1);
+        return Arrays.stream(arr).map(x -> mp.get(x)).toArray();
+    }
+
+
     /*1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance (Medium)
     There are n cities numbered from 0 to n-1. Given the array edges where
     edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted
--- leetcode.py
+++ leetcode.py
@@ -33919,6 +33919,37 @@ class UnionFind:
         return mat
 
 
+    """1331. Rank Transform of an Array (Easy)
+    Given an array of integers arr, replace each element with its rank. The rank
+    represents how large the element is. The rank has the following rules:
+    * Rank is an integer starting from 1.
+    * The larger the element, the larger the rank. If two elements are equal,
+      their rank must be the same.
+    * Rank should be as small as possible.
+
+    Example 1:
+    Input: arr = [40,10,20,30]
+    Output: [4,1,2,3]
+    Explanation: 40 is the largest element. 10 is the smallest. 20 is the second
+                 smallest. 30 is the third smallest.
+
+    Example 2:
+    Input: arr = [100,100,100]
+    Output: [1,1,1]
+    Explanation: Same elements share the same rank.
+
+    Example 3:
+    Input: arr = [37,12,28,9,100,56,80,5,12]
+    Output: [5,3,4,2,8,6,7,1,3]
+
+    Constraints:
+    * 0 <= arr.length <= 10^5
+    * -10^9 <= arr[i] <= 10^9"""
+
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        return map({x : i for i, x in enumerate(sorted(set(arr)), 1)}.get, arr)
+
+
     """1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance (Medium)
     There are n cities numbered from 0 to n-1. Given the array edges where
     edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted
