3c94ff9653525ec9f114f949d6f1ef6b7f909d3f
Ye Gao <gaoyescm@amazon.com>
2024-10-04 22:19:44 -0400

    solve'em all (567)

--- leetcode.js
+++ leetcode.js
@@ -1562,6 +1562,44 @@ var findMinDifference = function(timePoints) {
 };
 
 
+/*567. Permutation in String (Medium)
+Given two strings s1 and s2, return true if s2 contains a permutation of s1,
+or false otherwise. In other words, return true if one of s1's permutations
+is the substring of s2.
+
+Example 1:
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+
+Example 2:
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+
+Constraints:
+* 1 <= s1.length, s2.length <= 10^4
+* s1 and s2 consist of lowercase English letters.*/
+
+var checkInclusion = function(s1, s2) {
+    const freq = Array(26).fill(0);
+    for (const i in s1)
+        ++freq[s1.charCodeAt(i) - 97];
+    let bal = freq.filter(x => x > 0).length;
+    for (const i in s2) {
+        let c = s2.charCodeAt(i) - 97;
+        if (freq[c] == 0) ++bal;
+        if (--freq[c] == 0) --bal;
+        if (i >= s1.length) {
+            c = s2.charCodeAt(i-s1.length)-97;
+            if (freq[c] == 0) ++bal;
+            if (++freq[c] == 0) --bal;
+        }
+        if (bal == 0) return true;
+    }
+    return false;
+};
+
+
 /*590. N-ary Tree Postorder Traversal (Easy)
 Given the root of an n-ary tree, return the postorder traversal of its
 nodes' values. Nary-Tree input serialization is represented in their level
