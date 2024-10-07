d7ea245e1161dd97b959c85d239181934624b522
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-10-07 09:30:17 -0400

    solve'em all (2696)

--- leetcode.cpp
+++ leetcode.cpp
@@ -63448,6 +63448,44 @@ public:
     }
 
 
+    /*2696. Minimum String Length After Removing Substrings (Easy)
+    You are given a string s consisting only of uppercase English letters. You
+    can apply some operations to this string where, in one operation, you can
+    remove any occurrence of one of the substrings "AB" or "CD" from s. Return
+    the minimum possible length of the resulting string that you can obtain.
+    Note that the string concatenates after removing the substring and could
+    produce new "AB" or "CD" substrings.
+
+    Example 1:
+    Input: s = "ABFCACDB"
+    Output: 2
+    Explanation: We can do the following operations:
+    - Remove the substring "ABFCACDB", so s = "FCACDB".
+    - Remove the substring "FCACDB", so s = "FCAB".
+    - Remove the substring "FCAB", so s = "FC".
+    So the resulting length of the string is 2.
+    It can be shown that it is the minimum length that we can obtain.
+
+    Example 2:
+    Input: s = "ACBBD"
+    Output: 5
+    Explanation: We cannot do any operations on the string so the length remains
+                 the same.
+
+    Constraints:
+    * 1 <= s.length <= 100
+    * s consists only of uppercase English letters.*/
+
+    int minLength(string s) {
+        stack<char> stk;
+        for (auto& ch : s)
+            if (ch == 'B' && stk.size() && stk.top() == 'A') stk.pop();
+            else if (ch == 'D' && stk.size() && stk.top() == 'C') stk.pop();
+            else stk.push(ch);
+        return stk.size();
+    }
+
+
     /*2699. Modify Graph Edge Weights (Hard)
     You are given an undirected weighted connected graph containing n nodes
     labeled from 0 to n - 1, and an integer array edges where
--- leetcode.js
+++ leetcode.js
@@ -6832,6 +6832,44 @@ var createInfiniteObject = function() {
 };
 
 
+/*2696. Minimum String Length After Removing Substrings (Easy)
+You are given a string s consisting only of uppercase English letters. You
+can apply some operations to this string where, in one operation, you can
+remove any occurrence of one of the substrings "AB" or "CD" from s. Return
+the minimum possible length of the resulting string that you can obtain.
+Note that the string concatenates after removing the substring and could
+produce new "AB" or "CD" substrings.
+
+Example 1:
+Input: s = "ABFCACDB"
+Output: 2
+Explanation: We can do the following operations:
+- Remove the substring "ABFCACDB", so s = "FCACDB".
+- Remove the substring "FCACDB", so s = "FCAB".
+- Remove the substring "FCAB", so s = "FC".
+So the resulting length of the string is 2.
+It can be shown that it is the minimum length that we can obtain.
+
+Example 2:
+Input: s = "ACBBD"
+Output: 5
+Explanation: We cannot do any operations on the string so the length remains
+             the same.
+
+Constraints:
+* 1 <= s.length <= 100
+* s consists only of uppercase English letters.*/
+
+var minLength = function(s) {
+    const stack = [];
+    for (const ch of s)
+        if (ch == 'B' && stack.length && stack[stack.length-1] == 'A') stack.pop();
+        else if (ch == 'D' && stack.length && stack[stack.length-1] == 'C') stack.pop();
+        else stack.push(ch);
+    return stack.length;
+};
+
+
 /*2699. Modify Graph Edge Weights (Hard)
 You are given an undirected weighted connected graph containing n nodes
 labeled from 0 to n - 1, and an integer array edges where
