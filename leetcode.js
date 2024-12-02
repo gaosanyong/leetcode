a5dce398486fffa92bec509ea279076083f86afa
Ye Gao <ye_gao@mfe.berkeley.edu>
2024-12-01 22:13:57 -0500

    biweekly 141 (3314|3315|3316|3317)

--- leetcode.js
+++ leetcode.js
@@ -23278,6 +23278,207 @@ var kthCharacter = function(k, operations) {
 };
 
 
+/*3314. Construct the Minimum Bitwise Array I (Easy)
+You are given an array nums consisting of n prime integers. You need to
+construct an array ans of length n, such that, for each index i, the bitwise
+OR of ans[i] and ans[i] + 1 is equal to nums[i], i.e. ans[i] OR
+(ans[i] + 1) == nums[i]. Additionally, you must minimize each value of
+ans[i] in the resulting array. If it is not possible to find such a value
+for ans[i] that satisfies the condition, then set ans[i] = -1.
+
+Example 1:
+Input: nums = [2,3,5,7]
+Output: [-1,1,4,3]
+Explanation: * For i = 0, as there is no value for ans[0] that satisfies
+               ans[0] OR (ans[0] + 1) = 2, so ans[0] = -1.
+             * For i = 1, the smallest ans[1] that satisfies ans[1] OR
+               (ans[1] + 1) = 3 is 1, because 1 OR (1 + 1) = 3.
+             * For i = 2, the smallest ans[2] that satisfies ans[2] OR
+               (ans[2] + 1) = 5 is 4, because 4 OR (4 + 1) = 5.
+             * For i = 3, the smallest ans[3] that satisfies ans[3] OR
+               (ans[3] + 1) = 7 is 3, because 3 OR (3 + 1) = 7.
+
+Example 2:
+Input: nums = [11,13,31]
+Output: [9,12,15]
+Explanation: * For i = 0, the smallest ans[0] that satisfies ans[0] OR
+               (ans[0] + 1) = 11 is 9, because 9 OR (9 + 1) = 11.
+             * For i = 1, the smallest ans[1] that satisfies ans[1] OR
+               (ans[1] + 1) = 13 is 12, because 12 OR (12 + 1) = 13.
+             * For i = 2, the smallest ans[2] that satisfies ans[2] OR
+               (ans[2] + 1) = 31 is 15, because 15 OR (15 + 1) = 31.
+
+Constraints:
+* 1 <= nums.length <= 100
+* 2 <= nums[i] <= 1000
+* nums[i] is a prime number.*/
+
+var minBitwiseArray = function(nums) {
+    const ans = [];
+    for (const x of nums)
+        if (x & 1) {
+            const xx = x+1, bit = (xx & -xx) >> 1;
+            ans.push(x - bit);
+        } else ans.push(-1);
+    return ans;
+};
+
+
+/*3315. Construct the Minimum Bitwise Array II (Medium)
+You are given an array nums consisting of n prime integers. You need to
+construct an array ans of length n, such that, for each index i, the bitwise
+OR of ans[i] and ans[i] + 1 is equal to nums[i], i.e. ans[i] OR
+(ans[i] + 1) == nums[i]. Additionally, you must minimize each value of
+ans[i] in the resulting array. If it is not possible to find such a value
+for ans[i] that satisfies the condition, then set ans[i] = -1.
+
+Example 1:
+Input: nums = [2,3,5,7]
+Output: [-1,1,4,3]
+Explanation: * For i = 0, as there is no value for ans[0] that satisfies
+               ans[0] OR (ans[0] + 1) = 2, so ans[0] = -1.
+             * For i = 1, the smallest ans[1] that satisfies ans[1] OR
+               (ans[1] + 1) = 3 is 1, because 1 OR (1 + 1) = 3.
+             * For i = 2, the smallest ans[2] that satisfies ans[2] OR
+               (ans[2] + 1) = 5 is 4, because 4 OR (4 + 1) = 5.
+             * For i = 3, the smallest ans[3] that satisfies ans[3] OR
+               (ans[3] + 1) = 7 is 3, because 3 OR (3 + 1) = 7.
+
+Example 2:
+Input: nums = [11,13,31]
+Output: [9,12,15]
+Explanation: * For i = 0, the smallest ans[0] that satisfies ans[0] OR
+               (ans[0] + 1) = 11 is 9, because 9 OR (9 + 1) = 11.
+             * For i = 1, the smallest ans[1] that satisfies ans[1] OR
+               (ans[1] + 1) = 13 is 12, because 12 OR (12 + 1) = 13.
+             * For i = 2, the smallest ans[2] that satisfies ans[2] OR
+               (ans[2] + 1) = 31 is 15, because 15 OR (15 + 1) = 31.
+
+Constraints:
+* 1 <= nums.length <= 100
+* 2 <= nums[i] <= 10^9
+* nums[i] is a prime number.*/
+
+var minBitwiseArray = function(nums) {
+    const ans = [];
+    for (const x of nums)
+        if (x & 1) {
+            const xx = x+1, bit = (xx & -xx) >> 1;
+            ans.push(x - bit);
+        } else ans.push(-1);
+    return ans;
+};
+
+
+/*3316. Find Maximum Removals From Source String (Medium)
+You are given a string source of size n, a string pattern that is a
+subsequence of source, and a sorted integer array targetIndices that
+contains distinct numbers in the range [0, n - 1]. We define an operation as
+removing a character at an index idx from source such that:
+* idx is an element of targetIndices.
+* pattern remains a subsequence of source after removing the character.
+Performing an operation does not change the indices of the other characters
+in source. For example, if you remove 'c' from "acb", the character at index
+2 would still be 'b'. Return the maximum number of operations that can be
+performed.
+
+Example 1:
+Input: source = "abbaa", pattern = "aba", targetIndices = [0,1,2]
+Output: 1
+Explanation: We can't remove source[0] but we can do either of these two
+             operations:
+             * Remove source[1], so that source becomes "a_baa".
+             * Remove source[2], so that source becomes "ab_aa".
+
+Example 2:
+Input: source = "bcda", pattern = "d", targetIndices = [0,3]
+Output: 2
+Explanation: We can remove source[0] and source[3] in two operations.
+
+Example 3:
+Input: source = "dda", pattern = "dda", targetIndices = [0,1,2]
+Output: 0
+Explanation: We can't remove any character from source.
+
+Example 4:
+Input: source = "yeyeykyded", pattern = "yeyyd", targetIndices = [0,2,3,4]
+Output: 2
+Explanation: We can remove source[2] and source[3] in two operations.
+
+Constraints:
+* 1 <= n == source.length <= 3 * 10^3
+* 1 <= pattern.length <= n
+* 1 <= targetIndices.length <= n
+* targetIndices is sorted in ascending order.
+* The input is generated such that targetIndices contains distinct elements
+  in the range [0, n - 1].
+* source and pattern consist only of lowercase English letters.
+* The input is generated such that pattern appears as a subsequence in source.*/
+
+var maxRemovals = function(source, pattern, targetIndices) {
+    const m = source.length, n = pattern.length;
+    const seen = new Set(targetIndices);
+    const dp = Array(m+1).fill(0).map(() => Array(n+1).fill(0));
+    dp[m] = Array(n+1).fill(-Infinity);
+    for (let i = m, k = targetIndices.length-1; i >= 0; --i) {
+        if (k >= 0 && i == targetIndices[k]) --k;
+        dp[i][n] = targetIndices.length-1-k;
+    }
+    for (let i = m-1; i >= 0; --i)
+        for (let j = n-1; j >= 0; --j)
+            if (source[i] == pattern[j]) {
+                dp[i][j] = dp[i+1][j+1];
+                if (seen.has(i)) dp[i][j] = Math.max(dp[i][j], 1 + dp[i+1][j]);
+            } else {
+                dp[i][j] = dp[i+1][j];
+                if (seen.has(i)) ++dp[i][j];
+            }
+    return dp[0][0];
+};
+
+
+/*3317. Find the Number of Possible Ways for an Event (Hard)
+You are given three integers n, x, and y. An event is being held for n
+performers. When a performer arrives, they are assigned to one of the x
+stages. All performers assigned to the same stage will perform together as a
+band, though some stages might remain empty. After all performances are
+completed, the jury will award each band a score in the range [1, y]. Return
+the total number of possible ways the event can take place. Since the answer
+may be very large, return it modulo 10^9 + 7. Note that two events are
+considered to have been held differently if either of the following
+conditions is satisfied:
+* Any performer is assigned a different stage.
+* Any band is awarded a different score.
+
+Example 1:
+Input: n = 1, x = 2, y = 3
+Output: 6
+Explanation: There are 2 ways to assign a stage to the performer. The jury
+             can award a score of either 1, 2, or 3 to the only band.
+
+Example 2:
+Input: n = 5, x = 2, y = 1
+Output: 32
+Explanation: Each performer will be assigned either stage 1 or stage 2. All
+             bands will be awarded a score of 1.
+
+Example 3:
+Input: n = 3, x = 3, y = 4
+Output: 684
+
+Constraints: 1 <= n, x, y <= 1000*/
+
+var numberOfWays = function(n, x, y) {
+    const mod = 1_000_000_007;
+    const dp = Array(n+1).fill(0).map(() => Array(x+1).fill(0));
+    dp[0][0] = 1;
+    for (let i = 1; i <= n; ++i)
+        for (let j = 1; j <= x && j <= i; ++j)
+            dp[i][j] = (dp[i-1][j-1]*(x-j+1)*y + dp[i-1][j]*j) % mod;
+    return dp[n].reduce((x, y) => x+y, 0) % mod;
+};
+
+
 /*3324. Find the Sequence of Strings Appeared on the Screen (Medium)
 You are given a string target. Alice is going to type target on her computer
 using a special keyboard that has only two keys:
