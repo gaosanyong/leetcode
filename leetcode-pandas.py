5cc10fb20d9a2c267818da817d2eae7ee14ffb44
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-01-13 23:32:17 -0500

    solve'em all (3421)

--- leetcode-pandas.py
+++ leetcode-pandas.py
@@ -3442,3 +3442,73 @@ def sum_daily_odd_even(transactions: pd.DataFrame) -> pd.DataFrame:
     ).sort_values(
         by = 'transaction_date'
     )
+
+
+"""3421. Find Students Who Improved (Medium)
+Table: Scores
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student_id  | int     |
+| subject     | varchar |
+| score       | int     |
+| exam_date   | varchar |
++-------------+---------+
+(student_id, subject, exam_date) is the primary key for this table. Each row
+contains information about a student's score in a specific subject on a
+particular exam date. score is between 0 and 100 (inclusive).
+
+Write a solution to find the students who have shown improvement. A student is
+considered to have shown improvement if they meet both of these conditions:
+* Have taken exams in the same subject on at least two different dates
+* Their latest score in that subject is higher than their first score
+Return the result table ordered by student_id, subject in ascending order. The
+result format is in the following example.
+
+Example:
+Input:
+Scores table:
++------------+----------+-------+------------+
+| student_id | subject  | score | exam_date  |
++------------+----------+-------+------------+
+| 101        | Math     | 70    | 15-01-2023 |
+| 101        | Math     | 85    | 15-02-2023 |
+| 101        | Physics  | 65    | 15-01-2023 |
+| 101        | Physics  | 60    | 15-02-2023 |
+| 102        | Math     | 80    | 15-01-2023 |
+| 102        | Math     | 85    | 15-02-2023 |
+| 103        | Math     | 90    | 15-01-2023 |
+| 104        | Physics  | 75    | 15-01-2023 |
+| 104        | Physics  | 85    | 15-02-2023 |
++------------+----------+-------+------------+
+Output:
++------------+----------+-------------+--------------+
+| student_id | subject  | first_score | latest_score |
++------------+----------+-------------+--------------+
+| 101        | Math     | 70          | 85           |
+| 102        | Math     | 80          | 85           |
+| 104        | Physics  | 75          | 85           |
++------------+----------+-------------+--------------+
+Explanation:
+* Student 101 in Math: Improved from 70 to 85
+* Student 101 in Physics: No improvement (dropped from 65 to 60)
+* Student 102 in Math: Improved from 80 to 85
+* Student 103 in Math: Only one exam, not eligible
+* Student 104 in Physics: Improved from 75 to 85
+Result table is ordered by student_id, subject."""
+
+def find_students_who_improved(scores: pd.DataFrame) -> pd.DataFrame:
+    first = scores.loc[
+        scores.groupby(by = ["student_id", "subject"])["exam_date"].idxmin()
+        ].rename(columns = {"score" : "first_score", "exam_date" : "first_date"})
+    latest = scores.loc[
+        scores.groupby(by = ["student_id", "subject"])["exam_date"].idxmax()
+        ].rename(columns = {"score" : "latest_score", "exam_date" : "latest_date"})
+    return first.merge(
+        latest,
+        on = ["student_id", "subject"]
+    ).query(
+        "first_date < latest_date & first_score < latest_score"
+    ).sort_values(
+        by = ["student_id", "subject"]
+    )[["student_id", "subject", "first_score", "latest_score"]]
--- leetcode.sql
+++ leetcode.sql
@@ -10951,3 +10951,82 @@ SELECT
 FROM transactions
 GROUP BY 1
 ORDER BY 1
+
+
+/*3421. Find Students Who Improved (Medium)
+Table: Scores
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student_id  | int     |
+| subject     | varchar |
+| score       | int     |
+| exam_date   | varchar |
++-------------+---------+
+(student_id, subject, exam_date) is the primary key for this table. Each row
+contains information about a student's score in a specific subject on a
+particular exam date. score is between 0 and 100 (inclusive).
+
+Write a solution to find the students who have shown improvement. A student is
+considered to have shown improvement if they meet both of these conditions:
+* Have taken exams in the same subject on at least two different dates
+* Their latest score in that subject is higher than their first score
+Return the result table ordered by student_id, subject in ascending order. The
+result format is in the following example.
+
+Example:
+Input:
+Scores table:
++------------+----------+-------+------------+
+| student_id | subject  | score | exam_date  |
++------------+----------+-------+------------+
+| 101        | Math     | 70    | 15-01-2023 |
+| 101        | Math     | 85    | 15-02-2023 |
+| 101        | Physics  | 65    | 15-01-2023 |
+| 101        | Physics  | 60    | 15-02-2023 |
+| 102        | Math     | 80    | 15-01-2023 |
+| 102        | Math     | 85    | 15-02-2023 |
+| 103        | Math     | 90    | 15-01-2023 |
+| 104        | Physics  | 75    | 15-01-2023 |
+| 104        | Physics  | 85    | 15-02-2023 |
++------------+----------+-------+------------+
+Output:
++------------+----------+-------------+--------------+
+| student_id | subject  | first_score | latest_score |
++------------+----------+-------------+--------------+
+| 101        | Math     | 70          | 85           |
+| 102        | Math     | 80          | 85           |
+| 104        | Physics  | 75          | 85           |
++------------+----------+-------------+--------------+
+Explanation:
+* Student 101 in Math: Improved from 70 to 85
+* Student 101 in Physics: No improvement (dropped from 65 to 60)
+* Student 102 in Math: Improved from 80 to 85
+* Student 103 in Math: Only one exam, not eligible
+* Student 104 in Physics: Improved from 75 to 85
+Result table is ordered by student_id, subject.*/
+
+WITH FIRST AS (
+        SELECT
+            student_id,
+            subject,
+            score AS first_score,
+            exam_date AS first_date
+        FROM Scores JOIN (SELECT student_id, subject, MIN(exam_date) AS exam_date FROM Scores GROUP BY 1, 2) a USING(student_id, subject, exam_date)
+    ),
+    LATEST AS (
+        SELECT
+            student_id,
+            subject,
+            score AS latest_score,
+            exam_date AS latest_date
+        FROM Scores JOIN (SELECT student_id, subject, MAX(exam_date) AS exam_date FROM Scores GROUP BY 1, 2) a USING(student_id, subject, exam_date)
+    )
+SELECT
+    student_id,
+    subject,
+    first_score,
+    latest_score
+FROM FIRST JOIN LATEST USING (student_id, subject)
+WHERE first_date < latest_date AND first_score < latest_score
+ORDER BY 1,2
