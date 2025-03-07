c4ac520ffbe8fc7c36446df8e4f3a50d2df912bf
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-07 17:11:24 -0500

    weekly 381 (3014|3015|3016|3017)

--- leetcode.js
+++ leetcode.js
@@ -14503,6 +14503,126 @@ Constraints:
 * k - 2 <= dist <= n - 2*/
 
 
+/*3014. Minimum Number of Pushes to Type Word I (Easy)
+You are given a string word containing distinct lowercase English letters.
+Telephone keypads have keys mapped with distinct collections of lowercase
+English letters, which can be used to form words by pushing them. For
+example, the key 2 is mapped with ["a","b","c"], we need to push the key one
+time to type "a", two times to type "b", and three times to type "c" . It is
+allowed to remap the keys numbered 2 to 9 to distinct collections of
+letters. The keys can be remapped to any amount of letters, but each letter
+must be mapped to exactly one key. You need to find the minimum number of
+times the keys will be pushed to type the string word. Return the minimum
+number of pushes needed to type word after remapping the keys. An example
+mapping of letters to keys on a telephone keypad is given below. Note that
+1, *, #, and 0 do not map to any letters.
+
+Example 1:
+Input: word = "abcde"
+Output: 5
+Explanation: The remapped keypad given in the image provides the minimum
+             cost.
+             - "a" -> one push on key 2
+             - "b" -> one push on key 3
+             - "c" -> one push on key 4
+             - "d" -> one push on key 5
+             - "e" -> one push on key 6
+             Total cost is 1 + 1 + 1 + 1 + 1 = 5. It can be shown that no
+             other mapping can provide a lower cost.
+
+Example 2:
+Input: word = "xycdefghij"
+Output: 12
+Explanation: The remapped keypad given in the image provides the minimum
+             cost.
+             - "x" -> one push on key 2
+             - "y" -> two pushes on key 2
+             - "c" -> one push on key 3
+             - "d" -> two pushes on key 3
+             - "e" -> one push on key 4
+             - "f" -> one push on key 5
+             - "g" -> one push on key 6
+             - "h" -> one push on key 7
+             - "i" -> one push on key 8
+             - "j" -> one push on key 9
+             Total cost is 1 + 2 + 1 + 2 + 1 + 1 + 1 + 1 + 1 + 1 = 12. It
+             can be shown that no other mapping can provide a lower cost.
+
+Constraints:
+* 1 <= word.length <= 26
+* word consists of lowercase English letters.
+* All letters in word are distinct.*/
+
+var minimumPushes = function(word) {
+    const q = Math.floor(word.length / 8), r = word.length % 8;
+    return (4*q + r) * (q + 1);
+};
+
+
+/*3015. Count the Number of Houses at a Certain Distance I (Medium)
+You are given three positive integers n, x, and y. In a city, there exist
+houses numbered 1 to n connected by n streets. There is a street connecting
+the house numbered i with the house numbered i + 1 for all 1 <= i <= n - 1.
+An additional street connects the house numbered x with the house numbered
+y. For each k, such that 1 <= k <= n, you need to find the number of pairs
+of houses (house1, house2) such that the minimum number of streets that need
+to be traveled to reach house2 from house1 is k. Return a 1-indexed array
+result of length n where result[k] represents the total number of pairs of
+houses such that the minimum streets required to reach one house from the
+other is k. Note that x and y can be equal.
+
+Example 1:
+Input: n = 3, x = 1, y = 3
+Output: [6,0,0]
+Explanation: Let's look at each pair of houses:
+             - For the pair (1, 2), we can go from house 1 to house 2
+               directly.
+             - For the pair (2, 1), we can go from house 2 to house 1
+               directly.
+             - For the pair (1, 3), we can go from house 1 to house 3
+               directly.
+             - For the pair (3, 1), we can go from house 3 to house 1
+               directly.
+             - For the pair (2, 3), we can go from house 2 to house 3
+               directly.
+             - For the pair (3, 2), we can go from house 3 to house 2
+               directly.
+
+Example 2:
+Input: n = 5, x = 2, y = 4
+Output: [10,8,2,0,0]
+Explanation: For each distance k the pairs are:
+             - For k == 1, the pairs are (1, 2), (2, 1), (2, 3), (3, 2),
+               (2, 4), (4, 2), (3, 4), (4, 3), (4, 5), and (5, 4).
+             - For k == 2, the pairs are (1, 3), (3, 1), (1, 4), (4, 1),
+               (2, 5), (5, 2), (3, 5), and (5, 3).
+             - For k == 3, the pairs are (1, 5), and (5, 1).
+             - For k == 4 and k == 5, there are no pairs.
+
+Example 3:
+Input: n = 4, x = 1, y = 1
+Output: [6,4,2,0]
+Explanation: For each distance k the pairs are:
+             - For k == 1, the pairs are (1, 2), (2, 1), (2, 3), (3, 2),
+               (3, 4), and (4, 3).
+             - For k == 2, the pairs are (1, 3), (3, 1), (2, 4), and (4, 2).
+             - For k == 3, the pairs are (1, 4), and (4, 1).
+             - For k == 4, there are no pairs.
+
+Constraints:
+* 2 <= n <= 100
+* 1 <= x, y <= n*/
+
+var countOfPairs = function(n, x, y) {
+    const ans = Array(n).fill(0);
+    for (let i = 1; i <= n; ++i)
+        for (let j = i+1; j <= n; ++j) {
+            const dist = Math.min(j-i, Math.abs(i-x)+1+Math.abs(y-j), Math.abs(i-y)+1+Math.abs(x-j));
+            ans[dist-1] += 2;
+        }
+    return ans;
+};
+
 
 /*3016. Minimum Number of Pushes to Type Word II (Medium)
 You are given a string word containing lowercase English letters. Telephone
@@ -14574,6 +14694,79 @@ var minimumPushes = function(word) {
         ans += x * cnt;
     }
     return ans;
+};
+
+
+/*3017. Count the Number of Houses at a Certain Distance II (Hard)
+You are given three positive integers n, x, and y. In a city, there exist
+houses numbered 1 to n connected by n streets. There is a street connecting
+the house numbered i with the house numbered i + 1 for all 1 <= i <= n - 1.
+An additional street connects the house numbered x with the house numbered
+y. For each k, such that 1 <= k <= n, you need to find the number of pairs
+of houses (house1, house2) such that the minimum number of streets that need
+to be traveled to reach house2 from house1 is k. Return a 1-indexed array
+result of length n where result[k] represents the total number of pairs of
+houses such that the minimum streets required to reach one house from the
+other is k. Note that x and y can be equal.
+
+Example 1:
+Input: n = 3, x = 1, y = 3
+Output: [6,0,0]
+Explanation: Let's look at each pair of houses:
+             - For the pair (1, 2), we can go from house 1 to house 2
+               directly.
+             - For the pair (2, 1), we can go from house 2 to house 1
+               directly.
+             - For the pair (1, 3), we can go from house 1 to house 3
+               directly.
+             - For the pair (3, 1), we can go from house 3 to house 1
+               directly.
+             - For the pair (2, 3), we can go from house 2 to house 3
+               directly.
+             - For the pair (3, 2), we can go from house 3 to house 2
+               directly.
+
+Example 2:
+Input: n = 5, x = 2, y = 4
+Output: [10,8,2,0,0]
+Explanation: For each distance k the pairs are:
+             - For k == 1, the pairs are (1, 2), (2, 1), (2, 3), (3, 2),
+               (2, 4), (4, 2), (3, 4), (4, 3), (4, 5), and (5, 4).
+             - For k == 2, the pairs are (1, 3), (3, 1), (1, 4), (4, 1),
+               (2, 5), (5, 2), (3, 5), and (5, 3).
+             - For k == 3, the pairs are (1, 5), and (5, 1).
+             - For k == 4 and k == 5, there are no pairs.
+
+Example 3:
+Input: n = 4, x = 1, y = 1
+Output: [6,4,2,0]
+Explanation: For each distance k the pairs are:
+             - For k == 1, the pairs are (1, 2), (2, 1), (2, 3), (3, 2),
+               (3, 4), and (4, 3).
+             - For k == 2, the pairs are (1, 3), (3, 1), (2, 4), and (4, 2).
+             - For k == 3, the pairs are (1, 4), and (4, 1).
+             - For k == 4, there are no pairs.
+
+Constraints:
+* 2 <= n <= 10^5
+* 1 <= x, y <= n*/
+
+var countOfPairs = function(n, x, y) {
+    if (x > y) [x, y] = [y, x];
+    const diff = Array(n).fill(0);
+    for (let i = 1; i <= n; ++i) {
+        diff[0] += 2;
+        --diff[Math.min(i-1, Math.abs(i-y)+x)];
+        --diff[Math.min(n-i, Math.abs(i-x)+1+n-y)];
+        ++diff[Math.min(Math.abs(i-x), Math.abs(y-i)+1)];
+        ++diff[Math.min(Math.abs(i-x)+1, Math.abs(y-i))];
+        const val = Math.max(x-i, 0) + Math.max(i-y, 0);
+        --diff[val + Math.floor((y-x)/2)];
+        --diff[val + Math.ceil((y-x)/2)];
+    }
+    for (let i = 1; i < n; ++i)
+        diff[i] += diff[i-1];
+    return diff;
 };
 
 
