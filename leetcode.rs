257f1cd63689bfb91c414d8630eeac1fbaa7ebad
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-11 15:05:07 -0400

    solve'em all (9)

--- leetcode.rs
+++ leetcode.rs
@@ -40,3 +40,31 @@ impl Solution {
         panic!();
     }
 }
+
+
+/*9. Palindrome Number (Easy)
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Example 1:
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes
+             121-. Therefore it is not a palindrome.
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Constraints: -2^31 <= x <= 2^31 - 1*/
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        let s = x.to_string();
+        return s == s.chars().rev().collect::<String>();
+    }
+}
