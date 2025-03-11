ea223b20d6cb0479a3d46d1a2f4698c89828f65b
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-11 18:53:19 -0400

    solve'em all (20)

--- leetcode.rs
+++ leetcode.rs
@@ -174,3 +174,46 @@ impl Solution {
         }
     }
 }
+
+
+/*20. Valid Parentheses (Easy)
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid. An input string is valid if:
+* Open brackets must be closed by the same type of brackets.
+* Open brackets must be closed in the correct order.
+* Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+
+Example 4:
+Input: s = "([])"
+Output: true
+
+Constraints:
+* 1 <= s.length <= 10^4
+* s consists of parentheses only '()[]{}'.*/
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        for ch in s.chars() {
+            match ch {
+                '{' => stack.push('}'),
+                '(' => stack.push(')'),
+                '[' => stack.push(']'),
+                _ => if Some(ch) != stack.pop() { return false },
+            }
+        }
+        return stack.is_empty();
+    }
+}
