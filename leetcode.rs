61002511e6cd1bbdbd4f902462c8c1026aff9189
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-11 18:52:49 -0400

    solve'em all (14)

--- leetcode.rs
+++ leetcode.rs
@@ -137,3 +137,40 @@ impl Solution {
         return ans;
     }
 }
+
+
+/*14. Longest Common Prefix (Easy)
+Write a function to find the longest common prefix string amongst an array of
+strings. If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Constraints:
+* 1 <= strs.length <= 200
+* 0 <= strs[i].length <= 200
+* strs[i] consists of only lowercase English letters if it is non-empty.*/
+
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        match strs.is_empty() {
+            true => "".to_string(),
+            _ => {
+                strs.iter().skip(1).fold(strs[0].clone(), |acc, x| {
+                     acc
+                        .chars()
+                        .zip(x.chars())
+                        .take_while(|(x, y)| x == y)
+                        .map(|(x, _)| x)
+                        .collect()
+                })
+            }
+        }
+    }
+}
