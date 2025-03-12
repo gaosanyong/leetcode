4ad268f447b9b4eb3dac74ab413bb54e67656078
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-12 14:13:09 -0400

    solve'em all (35)

--- leetcode.rs
+++ leetcode.rs
@@ -349,3 +349,33 @@ impl Solution {
         return haystack.find(&needle).map(|x| x as i32).unwrap_or(-1);
     }
 }
+
+
+/*35. Search Insert Position (Easy)
+Given a sorted array of distinct integers and a target value, return the index
+if the target is found. If not, return the index where it would be if it were
+inserted in order. You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+Constraints:
+* 1 <= nums.length <= 10^4
+* -10^4 <= nums[i] <= 10^4
+* nums contains distinct values sorted in ascending order.
+* -10^4 <= target <= 10^4*/
+
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        return nums.binary_search(&target).unwrap_or_else(|x| x) as i32;
+    }
+}
