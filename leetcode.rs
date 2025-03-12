6868843def8273e227d5f002c4354517abcfe292
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-12 14:12:39 -0400

    solve'em all (28)

--- leetcode.rs
+++ leetcode.rs
@@ -323,3 +323,29 @@ impl Solution {
         return nums.len() as i32;
     }
 }
+
+
+/*28. Find the Index of the First Occurrence in a String (Easy)
+Given two strings needle and haystack, return the index of the first occurrence
+of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6. The first occurrence is at index 0,
+             so we return 0.
+
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+Constraints:
+* 1 <= haystack.length, needle.length <= 10^4
+* haystack and needle consist of only lowercase English characters.*/
+
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        return haystack.find(&needle).map(|x| x as i32).unwrap_or(-1);
+    }
+}
