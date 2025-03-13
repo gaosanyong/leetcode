1eaba6608da44a4391070fd8789d47bae5bd7a69
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-13 19:03:08 -0400

    solve'em all (70)

--- leetcode.rs
+++ leetcode.rs
@@ -461,3 +461,36 @@ impl Solution {
         return digits;
     }
 }
+
+
+/*70. Climbing Stairs (Easy)
+You are climbing a staircase. It takes n steps to reach the top. Each time you
+can either climb 1 or 2 steps. In how many distinct ways can you climb to the
+top?
+
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+             1. 1 step + 1 step
+             2. 2 steps
+
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+             1. 1 step + 1 step + 1 step
+             2. 1 step + 2 steps
+             3. 2 steps + 1 step
+
+Constraints: 1 <= n <= 45*/
+
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        let (mut f0, mut f1) = (0, 1);
+        for i in 0..n {
+            (f0, f1) = (f1, f0+f1);
+        }
+        return f1;
+    }
+}
