681def241107798f88f68df6b7f6a7ac0592c9ce
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-13 11:46:58 -0400

    solve'em all (58)

--- leetcode.rs
+++ leetcode.rs
@@ -379,3 +379,38 @@ impl Solution {
         return nums.binary_search(&target).unwrap_or_else(|x| x) as i32;
     }
 }
+
+
+/*58. Length of Last Word (Easy)
+Given a string s consisting of words and spaces, return the length of the last
+word in the string. A word is a maximal substring consisting of non-space
+characters only.
+
+Example 1:
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2:
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Example 3:
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
+Constraints:
+* 1 <= s.length <= 10^4
+* s consists of only English letters and spaces ' '.
+* There will be at least one word in s.*/
+
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        s.split_whitespace()
+         .last()
+         .unwrap()
+         .len() as i32
+    }
+}
