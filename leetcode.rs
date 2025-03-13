8ea0106b84bfb3eaf605c95b66d326866d9d7f6c
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-13 19:12:57 -0400

    solve'em all (83)

--- leetcode.rs
+++ leetcode.rs
@@ -494,3 +494,37 @@ impl Solution {
         return f1;
     }
 }
+
+
+/*83. Remove Duplicates from Sorted List (Easy)
+Given the head of a sorted linked list, delete all duplicates such that each
+element appears only once. Return the linked list sorted as well.
+
+Example 1:
+Input: head = [1,1,2]
+Output: [1,2]
+
+Example 2:
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+
+Constraints:
+* The number of nodes in the list is in the range [0, 300].
+* -100 <= Node.val <= 100
+* The list is guaranteed to be sorted in ascending order.*/
+
+impl Solution {
+    pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if (head.is_some()) {
+            let mut node = head.as_mut().unwrap();
+            while let Some(temp) = node.next.as_mut() {
+                if (node.val == temp.val) {
+                    node.next = temp.next.take();
+                } else {
+                    node = node.next.as_mut().unwrap();
+                }
+            }
+        }
+        return head;
+    }
+}
