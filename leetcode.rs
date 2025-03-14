d85a10f182433d66df231aac958ba83a7326dcf6
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-14 18:29:29 -0400

    solve'em all (94)

--- leetcode.rs
+++ leetcode.rs
@@ -585,3 +585,51 @@ impl Solution {
 }
 
 
+/*94. Binary Tree Inorder Traversal (Easy)
+Given the root of a binary tree, return the inorder traversal of its nodes'
+values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Explanation:
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+Explanation:
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 100].
+* -100 <= Node.val <= 100
+
+Follow up: Recursive solution is trivial, could you do it iteratively?*/
+
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut ans = vec![];
+        let mut node = root;
+        let mut stack = vec![];
+        while node.is_some() || !stack.is_empty() {
+            if let Some(temp) = node {
+                node = temp.borrow().left.clone();
+                stack.push(temp);
+            } else {
+                let temp = stack.pop().unwrap();
+                ans.push(temp.borrow().val);
+                node = temp.borrow().right.clone();
+            }
+        }
+        return ans;
+    }
+}
