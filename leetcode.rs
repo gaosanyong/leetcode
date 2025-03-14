421a324012a7d43cdebbd25ad9574f00e4c53ea8
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-14 18:30:11 -0400

    solve'em all (100)

--- leetcode.rs
+++ leetcode.rs
@@ -633,3 +633,43 @@ impl Solution {
         return ans;
     }
 }
+
+
+/*100. Same Tree (Easy)
+Given the roots of two binary trees p and q, write a function to check if they
+are the same or not. Two binary trees are considered the same if they are
+structurally identical, and the nodes have the same value.
+
+Example 1:
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+Input: p = [1,2], q = [1,null,2]
+Output: false
+
+Example 3:
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+
+Constraints:
+* The number of nodes in both trees is in the range [0, 100].
+* -10^4 <= Node.val <= 10^4*/
+
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        match (p, q) {
+            (None, None) => true,
+            (Some(p), Some(q)) => {
+                let p = p.borrow();
+                let q = q.borrow();
+                p.val == q.val
+                    && Self::is_same_tree(p.left.clone(), q.left.clone())
+                    && Self::is_same_tree(p.right.clone(), q.right.clone())
+            }
+            _ => false,
+        }
+    }
+}
