52895261d657d71d7736cf0ec066a387ec568f22
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-14 22:51:50 -0400

    solve'em all (112)

--- leetcode.rs
+++ leetcode.rs
@@ -855,3 +855,52 @@ impl Solution {
         }
     }
 }
+
+
+/*112. Path Sum (Easy)
+Given the root of a binary tree and an integer targetSum, return true if the
+tree has a root-to-leaf path such that adding up all the values along the path
+equals targetSum. A leaf is a node with no children.
+
+Example 1:
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+Example 2:
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There are two root-to-leaf paths in the tree:
+             (1 --> 2): The sum is 3.
+             (1 --> 3): The sum is 4.
+             There is no root-to-leaf path with sum = 5.
+
+Example 3:
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 5000].
+* -1000 <= Node.val <= 1000
+* -1000 <= targetSum <= 1000*/
+
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn has_path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> bool {
+        let mut stack = vec![(root, 0)];
+        while let Some((node, mut v)) = stack.pop() {
+            if let Some(node) = node {
+                let node = node.borrow();
+                v += node.val;
+                if node.left.is_none() && node.right.is_none() && v == target_sum {
+                    return true;
+                }
+                stack.push((node.left.clone(), v));
+                stack.push((node.right.clone(), v));
+            }
+        }
+        return false;
+    }
+}
