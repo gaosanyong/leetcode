65487e2ba550cd83ec93b3977f7f3921434afcaa
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-14 20:57:38 -0400

    solve'em all (104)

--- leetcode.rs
+++ leetcode.rs
@@ -704,3 +704,39 @@ impl Solution {
         }
     }
 }
+
+
+/*104. Maximum Depth of Binary Tree (Easy)
+Given the root of a binary tree, return its maximum depth. A binary tree's
+maximum depth is the number of nodes along the longest path from the root node
+down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 10^4].
+* -100 <= Node.val <= 100*/
+
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut ans = 0;
+        let mut stack = vec![(root, 1)];
+        while let Some((node, depth)) = stack.pop() {
+            if let Some(node) = node {
+                let node = node.borrow();
+                ans = ans.max(depth);
+                stack.push((node.left.clone(), depth+1));
+                stack.push((node.right.clone(), depth+1));
+            }
+        }
+        return ans;
+    }
+}
