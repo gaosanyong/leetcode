9c6411a58d2105c293885e2eca89156a52a2f78e
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-14 22:34:39 -0400

    solve'em all (111)

--- leetcode.rs
+++ leetcode.rs
@@ -818,3 +818,40 @@ impl Solution {
         return Self::calc(root) >= 0;
     }
 }
+
+
+/*111. Minimum Depth of Binary Tree (Easy)
+Given a binary tree, find its minimum depth. The minimum depth is the number of
+nodes along the shortest path from the root node down to the nearest leaf node.
+Note: A leaf is a node with no children.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+
+Example 2:
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 10^5].
+* -1000 <= Node.val <= 1000*/
+
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn min_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(node) => {
+                let left = Self::min_depth(node.borrow().left.clone());
+                let right = Self::min_depth(node.borrow().right.clone());
+                if left == 0 || right == 0 {
+                    return 1 + left.max(right);
+                } else {
+                    return 1 + left.min(right);
+                }
+            },
+            None => 0
+        }
+    }
+}
