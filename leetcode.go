aa89bfc9310f89afc3d0bd2b6ea020c34a5c3f24
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-19 17:02:05 -0400

    solve'em all (9)

--- leetcode.go
+++ leetcode.go
@@ -34,3 +34,35 @@ func twoSum(nums []int, target int) []int {
     }
     return []int{}
 }
+
+
+/*9. Palindrome Number (Easy)
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Example 1:
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes
+             121-. Therefore it is not a palindrome.
+
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Constraints: -2^31 <= x <= 2^31 - 1
+
+Follow up: Could you solve it without converting the integer to a string?*/
+
+func isPalindrome(x int) bool {
+    r := 0
+    for xx := x; xx > 0; xx /= 10 {
+        r = 10*r + xx % 10
+    }
+    return x == r
+}
