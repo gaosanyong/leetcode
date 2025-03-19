5b34f65a86b592b87741a73d38103f02a8144098
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-19 17:44:47 -0400

    solve'em all (14)

--- leetcode.go
+++ leetcode.go
@@ -131,3 +131,33 @@ func romanToInt(s string) int {
     }
     return ans
 }
+
+
+/*14. Longest Common Prefix (Easy)
+Write a function to find the longest common prefix string amongst an array of
+strings. If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Constraints:
+* 1 <= strs.length <= 200
+* 0 <= strs[i].length <= 200
+* strs[i] consists of only lowercase English letters if it is non-empty.*/
+
+func longestCommonPrefix(strs []string) string {
+    for j, _ := range strs[0] {
+        for i := 1; i < len(strs); i++ {
+            if j == len(strs[i]) || strs[0][j] != strs[i][j] {
+                return strs[0][:j]
+            }
+        }
+    }
+    return strs[0]
+}
