92f44e6ae4b6a4097392f5cd1a51a88c084c0960
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-20 15:07:58 -0400

    solve'em all (35)

--- leetcode.go
+++ leetcode.go
@@ -385,3 +385,40 @@ Constraints:
 func strStr(haystack string, needle string) int {
     return strings.Index(haystack, needle)
 }
+
+
+/*35. Search Insert Position (Easy)
+Given a sorted array of distinct integers and a target value, return the index
+if the target is found. If not, return the index where it would be if it were
+inserted in order. You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+Constraints:
+* 1 <= nums.length <= 10^4
+* -10^4 <= nums[i] <= 10^4
+* nums contains distinct values sorted in ascending order.
+* -10^4 <= target <= 10^4*/
+
+func searchInsert(nums []int, target int) int {
+    lo, hi := 0, len(nums)
+    for lo < hi {
+        mid := (lo+hi) / 2
+        if target <= nums[mid] {
+            hi = mid
+        } else {
+            lo = mid+1
+        }
+    }
+    return lo
+}
