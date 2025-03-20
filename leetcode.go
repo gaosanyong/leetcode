880a8152ec454fb3d576b067f6c79344598c205b
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-20 15:07:38 -0400

    solve'em all (28)

--- leetcode.go
+++ leetcode.go
@@ -361,3 +361,27 @@ func removeElement(nums []int, val int) int {
     }
     return k
 }
+
+
+/*28. Find the Index of the First Occurrence in a String (Easy)
+Given two strings needle and haystack, return the index of the first occurrence
+of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6. The first occurrence is at index 0,
+             so we return 0.
+
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+Constraints:
+* 1 <= haystack.length, needle.length <= 10^4
+* haystack and needle consist of only lowercase English characters.*/
+
+func strStr(haystack string, needle string) int {
+    return strings.Index(haystack, needle)
+}
