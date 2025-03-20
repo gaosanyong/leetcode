a376b4c3700f97e6fc418bf675602ab0234cd00e
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-20 16:20:48 -0400

    solve'em all (94)

--- leetcode.go
+++ leetcode.go
@@ -610,3 +610,51 @@ func merge(nums1 []int, m int, nums2 []int, n int)  {
         }
     }
 }
+
+
+/*94. Binary Tree Inorder Traversal (Easy)
+Given the root of a binary tree, return the inorder traversal of its nodes'
+values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Explanation:
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+Explanation:
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 100].
+* -100 <= Node.val <= 100
+
+Follow up: Recursive solution is trivial, could you do it iteratively?*/
+
+func inorderTraversal(root *TreeNode) []int {
+    ans := []int{}
+    stack := []*TreeNode{}
+    node := root
+    for node != nil || len(stack) > 0 {
+        if node != nil {
+            stack = append(stack, node)
+            node = node.Left
+        } else {
+            sz := len(stack)
+            node = stack[sz-1]
+            stack = stack[:sz-1]
+            ans = append(ans, node.Val)
+            node = node.Right
+        }
+    }
+    return ans
+}
