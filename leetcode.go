23f97ef40ea3294de125a67287b89bfbe152bddf
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-20 16:09:53 -0400

    solve'em all (83)

--- leetcode.go
+++ leetcode.go
@@ -528,3 +528,33 @@ func climbStairs(n int) int {
     }
     return f1
 }
+
+
+/*83. Remove Duplicates from Sorted List (Easy)
+Given the head of a sorted linked list, delete all duplicates such that each
+element appears only once. Return the linked list sorted as well.
+
+Example 1:
+Input: head = [1,1,2]
+Output: [1,2]
+
+Example 2:
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+
+Constraints:
+* The number of nodes in the list is in the range [0, 300].
+* -100 <= Node.val <= 100
+* The list is guaranteed to be sorted in ascending order.*/
+
+func deleteDuplicates(head *ListNode) *ListNode {
+    node := head
+    for node != nil && node.Next != nil {
+        if node.Val == node.Next.Val {
+            node.Next = node.Next.Next
+        } else {
+            node = node.Next
+        }
+    }
+    return head
+}
