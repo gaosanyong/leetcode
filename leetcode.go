a983c4a3b1c10875ead760e23ae65e3b946b77ca
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-20 16:32:13 -0400

    solve'em all (100)

--- leetcode.go
+++ leetcode.go
@@ -658,3 +658,35 @@ func inorderTraversal(root *TreeNode) []int {
     }
     return ans
 }
+
+
+/*100. Same Tree (Easy)
+Given the roots of two binary trees p and q, write a function to check if they
+are the same or not. Two binary trees are considered the same if they are
+structurally identical, and the nodes have the same value.
+
+Example 1:
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+Input: p = [1,2], q = [1,null,2]
+Output: false
+
+Example 3:
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+
+Constraints:
+* The number of nodes in both trees is in the range [0, 100].
+* -10^4 <= Node.val <= 10^4*/
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil && q == nil {
+        return true
+    } else if (p == nil || q == nil) {
+        return false
+    } else {
+        return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+    }
+}
