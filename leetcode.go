9e640d2b2058403e2cad22439f903ee104e065d3
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-20 14:24:24 -0400

    solve'em all (21)

--- leetcode.go
+++ leetcode.go
@@ -161,3 +161,40 @@ func longestCommonPrefix(strs []string) string {
     }
     return strs[0]
 }
+
+
+/*21. Merge Two Sorted Lists (Easy)
+You are given the heads of two sorted linked lists list1 and list2. Merge the
+two lists into one sorted list. The list should be made by splicing together the
+nodes of the first two lists. Return the head of the merged linked list.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+Constraints:
+* The number of nodes in both lists is in the range [0, 50].
+* -100 <= Node.val <= 100
+* Both list1 and list2 are sorted in non-decreasing order.*/
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+    dummy := &ListNode{}
+    for node := dummy; list1 != nil || list2 != nil; node = node.Next {
+        if list1 == nil || list2 != nil && list1.Val >= list2.Val {
+            node.Next = list2
+            list2 = list2.Next
+        } else {
+            node.Next = list1
+            list1 = list1.Next
+        }
+    }
+    return dummy.Next
+}
