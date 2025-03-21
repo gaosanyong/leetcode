787a86e336f60bc28467701b3805653c1170c230
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 12:20:59 -0400

    solve'em all (104)

--- leetcode.go
+++ leetcode.go
@@ -723,3 +723,28 @@ func isSymmetric(root *TreeNode) bool {
 
     return check(root.Left, root.Right)
 }
+
+
+/*104. Maximum Depth of Binary Tree (Easy)
+Given the root of a binary tree, return its maximum depth. A binary tree's
+maximum depth is the number of nodes along the longest path from the root node
+down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 10^4].
+* -100 <= Node.val <= 100*/
+
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
