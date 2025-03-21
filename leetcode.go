85c9859c5fe7d064f8378e01630670c14df91e15
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 12:18:06 -0400

    solve'em all (101)

--- leetcode.go
+++ leetcode.go
@@ -690,3 +690,36 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
         return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
     }
 }
+
+
+/*101. Symmetric Tree (Easy)
+Given the root of a binary tree, check whether it is a mirror of itself (i.e.,
+symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 1000].
+* -100 <= Node.val <= 100
+
+Follow up: Could you solve it both recursively and iteratively?*/
+
+func isSymmetric(root *TreeNode) bool {
+    var check func(left, right *TreeNode) bool
+
+    check = func(left, right *TreeNode) bool {
+        if left == nil || right == nil {
+            return left == right
+        } else {
+            return left.Val == right.Val && check(left.Left, right.Right) && check(left.Right, right.Left)
+        }
+    }
+
+    return check(root.Left, root.Right)
+}
