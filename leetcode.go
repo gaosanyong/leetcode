543657cf9a734654470d6e76d5224828da050429
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 13:42:58 -0400

    solve'em all (110)

--- leetcode.go
+++ leetcode.go
@@ -768,3 +768,41 @@ func sortedArrayToBST(nums []int) *TreeNode {
     mid := len(nums)/2
     return &TreeNode{nums[mid], sortedArrayToBST(nums[:mid]), sortedArrayToBST(nums[mid+1:])}
 }
+
+
+/*110. Balanced Binary Tree (Easy)
+Given a binary tree, determine if it is height-balanced.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+
+Example 3:
+Input: root = []
+Output: true
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 5000].
+* -10^4 <= Node.val <= 10^4*/
+
+func isBalanced(root *TreeNode) bool {
+    var calc func(node *TreeNode) int
+
+    calc = func(node *TreeNode) int {
+        if node == nil {
+            return 0
+        }
+        left := calc(node.Left)
+        right := calc(node.Right)
+        if left == -1 || right == -1 || math.Abs(float64(left-right)) > 1 {
+            return -1
+        }
+        return 1 + max(left, right)
+    }
+
+    return calc(root) != -1
+}
