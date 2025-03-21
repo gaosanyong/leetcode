15ccb8c7414ada334a9bc6cf3bebfa5aef81d04c
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 18:23:05 -0400

    solve'em all (121)

--- leetcode.go
+++ leetcode.go
@@ -935,3 +935,37 @@ func getRow(rowIndex int) []int {
     }
     return ans
 }
+
+
+/*121. Best Time to Buy and Sell Stock (Easy)
+You are given an array prices where prices[i] is the price of a given stock on
+the ith day. You want to maximize your profit by choosing a single day to buy
+one stock and choosing a different day in the future to sell that stock. Return
+the maximum profit you can achieve from this transaction. If you cannot achieve
+any profit, return 0.
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6),
+             profit = 6-1 = 5. Note that buying on day 2 and selling on day 1 is
+             not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+Constraints:
+* 1 <= prices.length <= 10^5
+* 0 <= prices[i] <= 10^4*/
+
+func maxProfit(prices []int) int {
+    ans := 0
+    val := 0
+    for i := 1; i < len(prices); i++ {
+        val = max(0, val + prices[i] - prices[i-1])
+        ans = max(ans, val)
+    }
+    return ans
+}
