72b064fccaff471ec6b861e9a1ed7e984cf22f16
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 19:52:55 -0400

    solve'em all (125)

--- leetcode.go
+++ leetcode.go
@@ -969,3 +969,46 @@ func maxProfit(prices []int) int {
     }
     return ans
 }
+
+
+/*125. Valid Palindrome (Easy)
+A phrase is a palindrome if, after converting all uppercase letters into
+lowercase letters and removing all non-alphanumeric characters, it reads the
+same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+             Since an empty string reads the same forward and backward, it is a
+             palindrome.
+
+Constraints:
+* 1 <= s.length <= 2 * 10^5
+* s consists only of printable ASCII characters.*/
+
+func isPalindrome(s string) bool {
+    s = strings.Map(func(ch rune) rune {
+        if unicode.IsLetter(ch) || unicode.IsNumber(ch) {
+            return unicode.ToLower(ch)
+        }
+        return -1
+    }, s)
+    for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+        if (s[i] != s[j]) {
+            return false
+        }
+    }
+    return true
+}
