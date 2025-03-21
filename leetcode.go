9d75ddfa2961f05eb48b8634a24b00cf56bd6312
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 14:37:44 -0400

    solve'em all (118)

--- leetcode.go
+++ leetcode.go
@@ -875,3 +875,32 @@ func hasPathSum(root *TreeNode, targetSum int) bool {
     }
     return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
 }
+
+
+/*118. Pascal's Triangle (Easy)
+Given an integer numRows, return the first numRows of Pascal's triangle. In
+Pascal's triangle, each number is the sum of the two numbers directly above it
+as shown:
+
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+Input: numRows = 1
+Output: [[1]]
+
+Constraints: 1 <= numRows <= 30*/
+
+func generate(numRows int) [][]int {
+    ans := make([][]int, numRows)
+    for i := 0; i < numRows; i++ {
+        ans[i] = make([]int, i+1)
+        ans[i][0] = 1
+        ans[i][i] = 1
+        for j := 1; j < i; j++ {
+            ans[i][j] = ans[i-1][j-1] + ans[i-1][j]
+        }
+    }
+    return ans
+}
