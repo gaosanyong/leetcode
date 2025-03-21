8623d88259fd3fe1c4c7e107931d252117a63120
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 18:21:40 -0400

    solve'em all (119)

--- leetcode.go
+++ leetcode.go
@@ -904,3 +904,34 @@ func generate(numRows int) [][]int {
     }
     return ans
 }
+
+
+/*119. Pascal's Triangle II (Easy)
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's
+triangle. In Pascal's triangle, each number is the sum of the two numbers
+directly above it as shown:
+
+Example 1:
+Input: rowIndex = 3
+Output: [1,3,3,1]
+
+Example 2:
+Input: rowIndex = 0
+Output: [1]
+
+Example 3:
+Input: rowIndex = 1
+Output: [1,1]
+
+Constraints: 0 <= rowIndex <= 33
+
+Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?*/
+
+func getRow(rowIndex int) []int {
+    ans := make([]int, rowIndex+1)
+    ans[0] = 1
+    for i := 1; i <= rowIndex; i++ {
+        ans[i] = ans[i-1] * (rowIndex-i+1) / i
+    }
+    return ans
+}
