787679b4878f600aeb7b7f032f5c90fbb33045a1
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 12:42:34 -0400

    solve'em all (108)

--- leetcode.go
+++ leetcode.go
@@ -740,3 +740,31 @@ func maxDepth(root *TreeNode) int {
     }
     return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
 }
+
+
+/*108. Convert Sorted Array to Binary Search Tree (Easy)
+Given an integer array nums where the elements are sorted in ascending order,
+convert it to a height-balanced binary search tree.
+
+Example 1:
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+Constraints:
+* 1 <= nums.length <= 10^4
+* -10^4 <= nums[i] <= 10^4
+* nums is sorted in a strictly increasing order.*/
+
+func sortedArrayToBST(nums []int) *TreeNode {
+    if len(nums) == 0 {
+        return nil
+    }
+    mid := len(nums)/2
+    return &TreeNode{nums[mid], sortedArrayToBST(nums[:mid]), sortedArrayToBST(nums[mid+1:])}
+}
