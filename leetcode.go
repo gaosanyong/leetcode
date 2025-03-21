50b5453c5346bc88b69aefed132c996a641baaf8
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 13:43:11 -0400

    solve'em all (111)

--- leetcode.go
+++ leetcode.go
@@ -806,3 +806,33 @@ func isBalanced(root *TreeNode) bool {
 
     return calc(root) != -1
 }
+
+
+/*111. Minimum Depth of Binary Tree (Easy)
+Given a binary tree, find its minimum depth. The minimum depth is the number of
+nodes along the shortest path from the root node down to the nearest leaf node.
+Note: A leaf is a node with no children.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+
+Example 2:
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 10^5].
+* -1000 <= Node.val <= 1000*/
+
+func minDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    left, right := minDepth(root.Left), minDepth(root.Right)
+    if left == 0 || right == 0 {
+        return 1 + left + right
+    } else {
+        return 1 + min(left, right)
+    }
+}
