7bf213420c223ae6dec551c3b85f03e8baa30115
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-22 18:18:40 -0400

    solve'em all (217)

--- leetcode.go
+++ leetcode.go
@@ -1604,3 +1604,37 @@ func reverseList(head *ListNode) *ListNode {
     }
     return prev
 }
+
+
+/*217. Contains Duplicate (Easy)
+Given an integer array nums, return true if any value appears at least twice in
+the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+Explanation: The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: All elements are distinct.
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* -10^9 <= nums[i] <= 10^9*/
+
+func containsDuplicate(nums []int) bool {
+    seen := map[int]struct{}{}
+    for _, x := range nums {
+        if _, ok := seen[x]; ok {
+            return true
+        }
+        seen[x] = struct{}{}
+    }
+    return false
+}
