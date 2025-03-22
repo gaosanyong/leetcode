47f67c4083ac3c49f255a35d9eef79f6d21eb289
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 20:48:27 -0400

    solve'em all (145)

--- leetcode.go
+++ leetcode.go
@@ -1134,3 +1134,48 @@ func preorderTraversal(root *TreeNode) []int {
     }
     return ans
 }
+
+
+/*145. Binary Tree Postorder Traversal (Easy)
+Given the root of a binary tree, return the postorder traversal of its nodes'
+values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Explanation:
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,6,7,5,2,9,8,3,1]
+Explanation:
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+* The number of the nodes in the tree is in the range [0, 100].
+* -100 <= Node.val <= 100
+
+Follow up: Recursive solution is trivial, could you do it iteratively?*/
+
+func postorderTraversal(root *TreeNode) []int {
+    ans := []int{}
+
+    var dfs func(node *TreeNode)
+    dfs = func(node *TreeNode) {
+        if node != nil {
+            dfs(node.Left)
+            dfs(node.Right)
+            ans = append(ans, node.Val)
+        }
+    }
+
+    dfs(root)
+    return ans
+}
