9aa1688fa294c9183762d05a9a0497a0decd8f84
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 21:37:42 -0400

    solve'em all (169)

--- leetcode.go
+++ leetcode.go
@@ -1306,3 +1306,38 @@ func convertToTitle(columnNumber int) string {
     }
     return string(ans)
 }
+
+
+/*169. Majority Element (Easy)
+Given an array nums of size n, return the majority element. The majority element
+is the element that appears more than ⌊n / 2⌋ times. You may assume that the
+majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Constraints:
+* n == nums.length
+* 1 <= n <= 5 * 10^4
+* -10^9 <= nums[i] <= 10^9
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?*/
+
+func majorityElement(nums []int) int {
+    cand := 0
+    vote := 0
+    for _, x := range nums {
+        if vote == 0 || cand == x{
+            vote++
+            cand = x
+        } else {
+            vote--
+        }
+    }
+    return cand
+}
