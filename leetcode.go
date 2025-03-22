705a06b8c7e5cad3c3ad493a64858cdc36e4d6ad
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 20:41:34 -0400

    solve'em all (144)

--- leetcode.go
+++ leetcode.go
@@ -1089,3 +1089,48 @@ func hasCycle(head *ListNode) bool {
     }
     return false
 }
+
+
+/*144. Binary Tree Preorder Traversal (Easy)
+Given the root of a binary tree, return the preorder traversal of its nodes'
+values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Explanation:
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [1,2,4,5,6,7,3,8,9]
+Explanation:
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 100].
+* -100 <= Node.val <= 100
+
+Follow up: Recursive solution is trivial, could you do it iteratively?*/
+
+func preorderTraversal(root *TreeNode) []int {
+    ans := []int{}
+    if root != nil {
+        stack := []*TreeNode{root}
+        for len(stack) > 0 {
+            sz := len(stack)
+            node := stack[sz-1]
+            stack = stack[:sz-1]
+            ans = append(ans, node.Val)
+            if node.Right != nil { stack = append(stack, node.Right) }
+            if node.Left != nil {stack = append(stack, node.Left) }
+        }
+    }
+    return ans
+}
