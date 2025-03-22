c577b59c8a2074a03e76ce6a2c1bb1a83c4bc3b7
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-22 18:23:55 -0400

    solve'em all (219)

--- leetcode.go
+++ leetcode.go
@@ -1638,3 +1638,37 @@ func containsDuplicate(nums []int) bool {
     }
     return false
 }
+
+
+/*219. Contains Duplicate II (Easy)
+Given an integer array nums and an integer k, return true if there are two
+distinct indices i and j in the array such that nums[i] == nums[j] and
+abs(i - j) <= k.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* -10^9 <= nums[i] <= 10^9
+* 0 <= k <= 10^5*/
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+    seen := map[int]int{}
+    for i, x := range nums {
+        if j, ok := seen[x]; ok && i-j <= k {
+            return true
+        }
+        seen[x] = i
+    }
+    return false
+}
