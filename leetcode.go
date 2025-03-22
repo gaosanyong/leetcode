2bf2b7f449a35d5ee51a83fade9e71f6b9549c8c
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 21:28:18 -0400

    solve'em all (168)

--- leetcode.go
+++ leetcode.go
@@ -1266,3 +1266,43 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
     }
     return nodeA
 }
+
+
+/*168. Excel Sheet Column Title (Easy)
+Given an integer columnNumber, return its corresponding column title as it
+appears in an Excel sheet. For example:
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+    ...
+
+Example 1:
+Input: columnNumber = 1
+Output: "A"
+
+Example 2:
+Input: columnNumber = 28
+Output: "AB"
+
+Example 3:
+Input: columnNumber = 701
+Output: "ZY"
+
+Constraints: 1 <= columnNumber <= 2^31 - 1*/
+
+func convertToTitle(columnNumber int) string {
+    ans := []byte{}
+    for columnNumber > 0 {
+        columnNumber--
+        ans = append(ans, byte(columnNumber%26 + 'A'))
+        columnNumber /= 26
+    }
+    for i, j := 0, len(ans)-1; i < j; i, j = i+1, j-1 {
+        ans[i], ans[j] = ans[j], ans[i]
+    }
+    return string(ans)
+}
