79baf3250d4a5e0727f42e0f6dcf1baa212ee4dc
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-22 18:13:10 -0400

    solve'em all (206)

--- leetcode.go
+++ leetcode.go
@@ -1570,3 +1570,37 @@ func isIsomorphic(s string, t string) bool {
     }
     return true
 }
+
+
+/*206. Reverse Linked List (Easy)
+Given the head of a singly linked list, reverse the list, and return the
+reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+* The number of nodes in the list is the range [0, 5000].
+* -5000 <= Node.val <= 5000
+
+Follow up: A linked list can be reversed either iteratively or recursively.
+           Could you implement both?*/
+
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    for curr := head; curr != nil; {
+        next := curr.Next
+        curr.Next = prev
+        prev, curr = curr, next
+    }
+    return prev
+}
