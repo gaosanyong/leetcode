46eb0b21459d3378244b31806684372f1c832f75
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-22 19:16:18 -0400

    solve'em all (222)

--- leetcode.go
+++ leetcode.go
@@ -1672,3 +1672,53 @@ func containsNearbyDuplicate(nums []int, k int) bool {
     }
     return false
 }
+
+
+/*222. Count Complete Tree Nodes (Easy)
+Given the root of a complete binary tree, return the number of the nodes in the
+tree. According to Wikipedia, every level, except possibly the last, is
+completely filled in a complete binary tree, and all nodes in the last level are
+as far left as possible. It can have between 1 and 2h nodes inclusive at the
+last level h. Design an algorithm that runs in less than O(n) time complexity.
+
+Example 1:
+Input: root = [1,2,3,4,5,6]
+Output: 6
+
+Example 2:
+Input: root = []
+Output: 0
+
+Example 3:
+Input: root = [1]
+Output: 1
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 5 * 10^4].
+* 0 <= Node.val <= 5 * 10^4
+* The tree is guaranteed to be complete.*/
+
+func countNodes(root *TreeNode) int {
+    ht := func(node *TreeNode) int {
+        ans := 0
+        for ; node != nil; node = node.Left {
+            ans++
+        }
+        return ans
+    }
+
+    var fn func(node *TreeNode) int
+    fn = func(node *TreeNode) int {
+        if node == nil {
+            return 0
+        }
+        h := ht(node.Left)
+        if h == ht(node.Right) {
+            return 1<<h + fn(node.Right)
+        } else {
+            return 1<<(h-1) + fn(node.Left)
+        }
+    }
+
+    return fn(root)
+}
