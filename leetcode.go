11e7fdf0023378dcad12bd33ab375c71dd76d056
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-22 17:28:37 -0400

    solve'em all (203)

--- leetcode.go
+++ leetcode.go
@@ -1493,3 +1493,37 @@ func isHappy(n int) bool {
     }
     return true
 }
+
+
+/*203. Remove Linked List Elements (Easy)
+Given the head of a linked list and an integer val, remove all the nodes of the
+linked list that has Node.val == val, and return the new head.
+
+Example 1:
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+Input: head = [], val = 1
+Output: []
+
+Example 3:
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+Constraints:
+* The number of nodes in the list is in the range [0, 10^4].
+* 1 <= Node.val <= 50
+* 0 <= val <= 50*/
+
+func removeElements(head *ListNode, val int) *ListNode {
+    dummy := &ListNode{0, head}
+    for node := dummy; node.Next != nil;  {
+        if node.Next.Val == val {
+            node.Next = node.Next.Next
+        } else {
+            node = node.Next
+        }
+    }
+    return dummy.Next
+}
