f52c19fe541aa57d4903a00ad93e467db6e1d540
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 21:40:45 -0400

    solve'em all (171)

--- leetcode.go
+++ leetcode.go
@@ -1341,3 +1341,41 @@ func majorityElement(nums []int) int {
     }
     return cand
 }
+
+
+/*171. Excel Sheet Column Number (Easy)
+Given a string columnTitle that represents the column title as appears in an
+Excel sheet, return its corresponding column number. For example:
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+    ...
+
+Example 1:
+Input: columnTitle = "A"
+Output: 1
+
+Example 2:
+Input: columnTitle = "AB"
+Output: 28
+
+Example 3:
+Input: columnTitle = "ZY"
+Output: 701
+
+Constraints:
+* 1 <= columnTitle.length <= 7
+* columnTitle consists only of uppercase English letters.
+* columnTitle is in the range ["A", "FXSHRXW"].*/
+
+func titleToNumber(columnTitle string) int {
+    ans := 0
+    for _, ch := range columnTitle {
+        ans = 26*ans + int(ch-'A') + 1
+    }
+    return ans
+}
