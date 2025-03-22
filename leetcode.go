d8bef43699a6a39d90929f0e21f731b87c0a41b0
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-21 22:00:42 -0400

    solve'em all (191)

--- leetcode.go
+++ leetcode.go
@@ -1418,3 +1418,36 @@ func reverseBits(num uint32) uint32 {
     }
     return ans
 }
+
+
+/*191. Number of 1 Bits (Easy)
+Given a positive integer n, write a function that returns the number of set bits
+in its binary representation (also known as the Hamming weight).
+
+Example 1:
+Input: n = 11
+Output: 3
+Explanation: The input binary string 1011 has a total of three set bits.
+
+Example 2:
+Input: n = 128
+Output: 1
+Explanation: The input binary string 10000000 has a total of one set bit.
+
+Example 3:
+Input: n = 2147483645
+Output: 30
+Explanation: The input binary string 1111111111111111111111111111101 has a total
+             of thirty set bits.
+
+Constraints: 1 <= n <= 2^31 - 1
+
+Follow up: If this function is called many times, how would you optimize it?*/
+
+func hammingWeight(n int) int {
+    ans := 0
+    for ; n > 0; n &= n-1 {
+        ans++
+    }
+    return ans
+}
