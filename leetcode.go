ca62f58283bdce709b4a3da7147bef0418e77193
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 15:29:05 -0400

    solve'em all (344)

--- leetcode.go
+++ leetcode.go
@@ -2504,6 +2504,30 @@ func isPowerOfFour(n int) bool {
 }
 
 
+/*344. Reverse String (Easy)
+Write a function that reverses a string. The input string is given as an array
+of characters s. You must do this by modifying the input array in-place with
+O(1) extra memory.
+
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+Constraints:
+* 1 <= s.length <= 10^5
+* s[i] is a printable ascii character.*/
+
+func reverseString(s []byte)  {
+    for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+        s[i], s[j] = s[j], s[i]
+    }
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
