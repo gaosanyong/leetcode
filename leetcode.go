0fb7ab40e8ec1910ce0944a14801aedc673301f8
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 00:46:48 -0400

    solve'em all (290)

--- leetcode.go
+++ leetcode.go
@@ -2277,6 +2277,55 @@ func moveZeroes(nums []int)  {
 }
 
 
+/*290. Word Pattern (Easy)
+Given a pattern and a string s, find if s follows the same pattern. Here follow
+means a full match, such that there is a bijection between a letter in pattern
+and a non-empty word in s. Specifically:
+* Each letter in pattern maps to exactly one unique word in s.
+* Each unique word in s maps to exactly one letter in pattern.
+* No two letters map to the same word, and no two words map to the same letter.
+
+Example 1:
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Explanation: The bijection can be established as:
+             - 'a' maps to "dog".
+             - 'b' maps to "cat".
+
+Example 2:
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+
+Example 3:
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+
+Constraints:
+* 1 <= pattern.length <= 300
+* pattern contains only lower-case English letters.
+* 1 <= s.length <= 3000
+* s contains only lowercase English letters and spaces ' '.
+* s does not contain any leading or trailing spaces.
+* All the words in s are separated by a single space.*/
+
+func wordPattern(pattern string, s string) bool {
+    words := strings.Split(s, " ")
+    if len(pattern) != len(words) {
+        return false
+    }
+    pi := map[rune]int{}
+    wi := map[string]int{}
+    for i, ch := range pattern {
+        if pi[ch] != wi[words[i]] {
+            return false
+        }
+        pi[ch] = i+1
+        wi[words[i]] = i+1
+    }
+    return true
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
