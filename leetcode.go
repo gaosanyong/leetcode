0fbd8a5ac4ae8fa1dc980dcd7a3660a17447deb5
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-23 22:32:23 -0400

    solve'em all (232)

--- leetcode.go
+++ leetcode.go
@@ -1899,6 +1899,95 @@ func isPowerOfTwo(n int) bool {
 }
 
 
+/*232. Implement Queue using Stacks (Easy)
+Implement a first in first out (FIFO) queue using only two stacks. The
+implemented queue should support all the functions of a normal queue (push,
+peek, pop, and empty). Implement the MyQueue class:
+* void push(int x) Pushes element x to the back of the queue.
+* int pop() Removes the element from the front of the queue and returns it.
+* int peek() Returns the element at the front of the queue.
+* boolean empty() Returns true if the queue is empty, false otherwise.
+
+Notes:
+* You must use only standard operations of a stack, which means only push to
+  top, peek/pop from top, size, and is empty operations are valid.
+* Depending on your language, the stack may not be supported natively. You may
+  simulate a stack using a list or deque (double-ended queue) as long as you use
+  only a stack's standard operations.
+
+Example 1:
+Input: ["MyQueue", "push", "push", "peek", "pop", "empty"]
+       [[], [1], [2], [], [], []]
+Output: [null, null, null, 1, 1, false]
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+
+Constraints:
+* 1 <= x <= 9
+* At most 100 calls will be made to push, pop, peek, and empty.
+* All the calls to pop and peek are valid.
+
+Follow-up: Can you implement the queue such that each operation is amortized
+           O(1) time complexity? In other words, performing n operations will
+           take overall O(n) time even if one of those operations may take
+           longer.*/
+
+type MyQueue struct {
+    sin []int
+    sout []int
+}
+
+
+func Constructor() MyQueue {
+    return MyQueue{
+        sin: []int{},
+        sout: []int{},
+    }
+}
+
+
+func (this *MyQueue) Push(x int) {
+    this.sin = append(this.sin, x)
+}
+
+
+func (this *MyQueue) Move() {
+    for sz := len(this.sin); sz > 0; sz-- {
+        this.sout = append(this.sout, this.sin[sz-1])
+        this.sin = this.sin[:sz-1]
+    }
+}
+
+func (this *MyQueue) Pop() int {
+    if len(this.sout) == 0 {
+        this.Move()
+    }
+    sz := len(this.sout)
+    elem := this.sout[sz-1]
+    this.sout = this.sout[:sz-1]
+    return elem
+}
+
+
+func (this *MyQueue) Peek() int {
+    if len(this.sout) == 0 {
+        this.Move()
+    }
+    sz := len(this.sout)
+    return this.sout[sz-1]
+}
+
+
+func (this *MyQueue) Empty() bool {
+    return len(this.sin) == 0 && len(this.sout) == 0
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
