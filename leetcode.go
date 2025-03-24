71a61359d32551919709ed3da24862f38e29560a
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 00:40:07 -0400

    solve'em all (283)

--- leetcode.go
+++ leetcode.go
@@ -2247,6 +2247,36 @@ func firstBadVersion(n int) int {
 }
 
 
+/*283. Move Zeroes (Easy)
+Given an integer array nums, move all 0's to the end of it while maintaining the
+relative order of the non-zero elements. Note that you must do this in-place
+without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+* 1 <= nums.length <= 10^4
+* -2^31 <= nums[i] <= 2^31 - 1
+
+Follow up: Could you minimize the total number of operations done?*/
+
+func moveZeroes(nums []int)  {
+    k := 0
+    for i, x := range nums {
+        if x != 0 {
+            nums[k], nums[i] = nums[i], nums[k]
+            k++
+        }
+    }
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
