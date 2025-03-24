d848988c9b1f4dc7862919888c5a35b8c93cdcfe
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 15:27:16 -0400

    solve'em all (342)

--- leetcode.go
+++ leetcode.go
@@ -2478,6 +2478,32 @@ func countBits(n int) []int {
 }
 
 
+/*342. Power of Four (Easy)
+Given an integer n, return true if it is a power of four. Otherwise, return
+false. An integer n is a power of four, if there exists an integer x such that
+n == 4x.
+
+Example 1:
+Input: n = 16
+Output: true
+
+Example 2:
+Input: n = 5
+Output: false
+
+Example 3:
+Input: n = 1
+Output: true
+
+Constraints: -2^31 <= n <= 2^31 - 1
+
+Follow up: Could you solve it without loops/recursion?*/
+
+func isPowerOfFour(n int) bool {
+    return n > 0 && n&(n-1) == 0 && (n-1) % 3 == 0
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
