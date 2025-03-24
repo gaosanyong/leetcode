e2c17d7f43e1a7490af5e2e904ef60cfb4605fbf
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 17:33:10 -0400

    solve'em all (367)

--- leetcode.go
+++ leetcode.go
@@ -2641,6 +2641,34 @@ func intersect(nums1 []int, nums2 []int) []int {
 }
 
 
+/*367. Valid Perfect Square (Easy)
+Given a positive integer num, return true if num is a perfect square or false
+otherwise. A perfect square is an integer that is the square of an integer. In
+other words, it is the product of some integer with itself. You must not use any
+built-in library function, such as sqrt.
+
+Example 1:
+Input: num = 16
+Output: true
+Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+
+Example 2:
+Input: num = 14
+Output: false
+Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an
+             integer.
+
+Constraints: 1 <= num <= 2^31 - 1*/
+
+func isPerfectSquare(num int) bool {
+    x := max(1, num-1)
+    for x > num/x {
+        x = (x + num/x)/2
+    }
+    return x * x == num
+}
+
+
 /*374. Guess Number Higher or Lower (Easy)
 We are playing the Guess Game. The game is as follows: I pick a number from 1 to
 n. You have to guess which number I picked. Every time you guess wrong, I will
