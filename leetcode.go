8d523f7a0b4408f088b6c26b820638cb30f477e3
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-23 21:53:04 -0400

    solve'em all (225)

--- leetcode.go
+++ leetcode.go
@@ -1724,6 +1724,74 @@ func countNodes(root *TreeNode) int {
 }
 
 
+/*225. Implement Stack using Queues (Easy)
+Implement a last-in-first-out (LIFO) stack using only two queues. The
+implemented stack should support all the functions of a normal stack (push, top,
+pop, and empty). Implement the MyStack class:
+* void push(int x) Pushes element x to the top of the stack.
+* int pop() Removes the element on the top of the stack and returns it.
+* int top() Returns the element on the top of the stack.
+* boolean empty() Returns true if the stack is empty, false otherwise.
+
+Notes:
+* You must use only standard operations of a queue, which means that only push
+  to back, peek/pop from front, size and is empty operations are valid.
+* Depending on your language, the queue may not be supported natively. You may
+  simulate a queue using a list or deque (double-ended queue) as long as you use
+  only a queue's standard operations.
+
+Example 1:
+Input: ["MyStack", "push", "push", "top", "pop", "empty"]
+       [[], [1], [2], [], [], []]
+Output: [null, null, null, 2, 2, false]
+Explanation: MyStack myStack = new MyStack();
+             myStack.push(1);
+             myStack.push(2);
+             myStack.top(); // return 2
+             myStack.pop(); // return 2
+             myStack.empty(); // return False
+
+Constraints:
+* 1 <= x <= 9
+* At most 100 calls will be made to push, pop, top, and empty.
+* All the calls to pop and top are valid.
+
+Follow-up: Can you implement the stack using only one queue?*/
+
+type MyStack struct {
+    queue []int
+}
+
+
+func Constructor() MyStack {
+    return MyStack{
+        queue : []int{},
+    }
+}
+
+
+func (this *MyStack) Push(x int)  {
+    this.queue = append([]int{x}, this.queue...)
+}
+
+
+func (this *MyStack) Pop() int {
+    ans := this.queue[0]
+    this.queue = this.queue[1:]
+    return ans
+}
+
+
+func (this *MyStack) Top() int {
+    return this.queue[0]
+}
+
+
+func (this *MyStack) Empty() bool {
+    return len(this.queue) == 0
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
