cfb4f1c916ff134244992fe496ef3344e8d65a7a
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-23 21:59:03 -0400

    solve'em all (231)

--- leetcode.go
+++ leetcode.go
@@ -1871,6 +1871,34 @@ func summaryRanges(nums []int) []string {
 }
 
 
+/*231. Power of Two (Easy)
+Given an integer n, return true if it is a power of two. Otherwise, return
+false. An integer n is a power of two, if there exists an integer x such that
+n == 2^x.
+
+Example 1:
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+
+Example 2:
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+
+Example 3:
+Input: n = 3
+Output: false
+
+Constraints: -2^31 <= n <= 2^31 - 1
+
+Follow up: Could you solve it without loops/recursion?*/
+
+func isPowerOfTwo(n int) bool {
+    return n > 0 && n&(n-1) == 0
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
