b5a185bf294b1995acbb12d27101e111eeb273f8
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 14:52:02 -0400

    solve'em all (326)

--- leetcode.go
+++ leetcode.go
@@ -2411,6 +2411,35 @@ func (this *NumArray) SumRange(left int, right int) int {
 }
 
 
+/*326. Power of Three (Easy)
+Given an integer n, return true if it is a power of three. Otherwise, return
+false. An integer n is a power of three, if there exists an integer x such that
+n == 3x.
+
+Example 1:
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+
+Example 2:
+Input: n = 0
+Output: false
+Explanation: There is no x where 3x = 0.
+
+Example 3:
+Input: n = -1
+Output: false
+Explanation: There is no x where 3x = (-1).
+
+Constraints: -2^31 <= n <= 2^31 - 1
+
+Follow up: Could you solve it without loops/recursion?*/
+
+func isPowerOfThree(n int) bool {
+    return n > 0 && 1162261467 % n == 0
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
