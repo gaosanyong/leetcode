af34d092a0e00f081c453de67de3cd8edc39bb9a
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 16:24:14 -0400

    solve'em all (350)

--- leetcode.go
+++ leetcode.go
@@ -2599,6 +2599,48 @@ func intersection(nums1 []int, nums2 []int) []int {
 }
 
 
+/*350. Intersection of Two Arrays II (Easy)
+Given two integer arrays nums1 and nums2, return an array of their intersection.
+Each element in the result must appear as many times as it shows in both arrays
+and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+
+Constraints:
+* 1 <= nums1.length, nums2.length <= 1000
+* 0 <= nums1[i], nums2[i] <= 1000
+
+Follow up:
+* What if the given array is already sorted? How would you optimize your
+  algorithm?
+* What if nums1's size is small compared to nums2's size? Which algorithm is
+  better?
+* What if elements of nums2 are stored on disk, and the memory is limited such
+  that you cannot load all elements into the memory at once?*/
+
+func intersect(nums1 []int, nums2 []int) []int {
+    freq := map[int]int{}
+    for _, x := range nums1 {
+        freq[x]++
+    }
+    ans := []int{}
+    for _, x := range nums2 {
+        if freq[x] > 0 {
+            ans = append(ans, x)
+            freq[x]--
+        }
+    }
+    return ans
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
