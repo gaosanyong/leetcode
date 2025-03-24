354b2d86014a627b714f0c217554fb5c5235eef4
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 16:39:22 -0400

    solve'em all (374)

--- leetcode.go
+++ leetcode.go
@@ -2641,6 +2641,46 @@ func intersect(nums1 []int, nums2 []int) []int {
 }
 
 
+/*374. Guess Number Higher or Lower (Easy)
+We are playing the Guess Game. The game is as follows: I pick a number from 1 to
+n. You have to guess which number I picked. Every time you guess wrong, I will
+tell you whether the number I picked is higher or lower than your guess. You
+call a pre-defined API int guess(int num), which returns three possible results:
+* -1: Your guess is higher than the number I picked (i.e. num > pick).
+* 1: Your guess is lower than the number I picked (i.e. num < pick).
+* 0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+Example 1:
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+Input: n = 1, pick = 1
+Output: 1
+
+Example 3:
+Input: n = 2, pick = 1
+Output: 1
+
+Constraints:
+* 1 <= n <= 2^31 - 1
+* 1 <= pick <= n*/
+
+func guessNumber(n int) int {
+    lo, hi := 1, n
+    for lo <= hi {
+        mid := (lo+hi) / 2
+        switch guess(mid) {
+            case -1: hi = mid-1
+            case 1: lo = mid+1
+            default: return mid
+        }
+    }
+    return lo
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
