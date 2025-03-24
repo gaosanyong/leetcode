86f29fe9f8aee31d5d03d2e593622afc7bed0d34
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 17:05:23 -0400

    solve'em all (392)

--- leetcode.go
+++ leetcode.go
@@ -2784,6 +2784,41 @@ func findTheDifference(s string, t string) byte {
 }
 
 
+/*392. Is Subsequence (Easy)
+Given two strings s and t, return true if s is a subsequence of t, or false
+otherwise. A subsequence of a string is a new string that is formed from the
+original string by deleting some (can be none) of the characters without
+disturbing the relative positions of the remaining characters. (i.e., "ace" is a
+subsequence of "abcde" while "aec" is not).
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+Constraints:
+* 0 <= s.length <= 100
+* 0 <= t.length <= 10^4
+* s and t consist only of lowercase English letters.
+
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where
+           k >= 10^9, and you want to check one by one to see if t has its
+           subsequence. In this scenario, how would you change your code?*/
+
+func isSubsequence(s string, t string) bool {
+    i := 0
+    for j := 0; i < len(s) && j < len(t); j++ {
+        if s[i] == t[j] {
+            i++
+        }
+    }
+    return i == len(s)
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
