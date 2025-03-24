8d49b34605fabcde6c104acce77f3fad8856c691
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 15:26:10 -0400

    solve'em all (338)

--- leetcode.go
+++ leetcode.go
@@ -2440,6 +2440,44 @@ func isPowerOfThree(n int) bool {
 }
 
 
+/*338. Counting Bits (Easy)
+Given an integer n, return an array ans of length n + 1 such that for each i
+(0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+Example 1:
+Input: n = 2
+Output: [0,1,1]
+Explanation: 0 --> 0
+             1 --> 1
+             2 --> 10
+
+Example 2:
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation: 0 --> 0
+             1 --> 1
+             2 --> 10
+             3 --> 11
+             4 --> 100
+             5 --> 101
+
+Constraints: 0 <= n <= 10^5
+
+Follow up:
+* It is very easy to come up with a solution with a runtime of O(n log n). Can
+  you do it in linear time O(n) and possibly in a single pass?
+* Can you do it without using any built-in function (i.e., like
+  __builtin_popcount in C++)?*/
+
+func countBits(n int) []int {
+    ans := make([]int, n+1)
+    for x := 0; x <= n; x++ {
+        ans[x] = ans[x >> 1] + x&1
+    }
+    return ans
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
