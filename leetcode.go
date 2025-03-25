c1320a884c14967ec69debbad83fae2aea3f1298
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-24 23:12:03 -0400

    solve'em all (412)

--- leetcode.go
+++ leetcode.go
@@ -2954,6 +2954,47 @@ func longestPalindrome(s string) int {
 }
 
 
+/*412. Fizz Buzz (Easy)
+Given an integer n, return a string array answer (1-indexed) where:
+* answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+* answer[i] == "Fizz" if i is divisible by 3.
+* answer[i] == "Buzz" if i is divisible by 5.
+* answer[i] == i (as a string) if none of the above conditions are true.
+
+Example 1:
+Input: n = 3
+Output: ["1","2","Fizz"]
+
+Example 2:
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+
+Example 3:
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+Constraints: 1 <= n <= 10^4*/
+
+func fizzBuzz(n int) []string {
+    ans := []string{}
+    for x := 1; x <= n; x++ {
+        var elem string
+        switch true {
+            case x % 3 == 0 && x % 5 == 0:
+                elem = "FizzBuzz"
+            case x % 3 == 0:
+                elem = "Fizz"
+            case x % 5 == 0:
+                elem = "Buzz"
+            default:
+                elem = strconv.Itoa(x)
+        }
+        ans = append(ans, elem)
+    }
+    return ans
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
