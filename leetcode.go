f335051d54a580d513a202447a0c8129de4ad104
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-26 22:26:04 -0400

    solve'em all (441)

--- leetcode.go
+++ leetcode.go
@@ -3096,6 +3096,29 @@ func countSegments(s string) int {
 }
 
 
+/*441. Arranging Coins (Easy)
+You have n coins and you want to build a staircase with these coins. The
+staircase consists of k rows where the ith row has exactly i coins. The last row
+of the staircase may be incomplete. Given the integer n, return the number of
+complete rows of the staircase you will build.
+
+Example 1:
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+
+Example 2:
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+
+Constraints: 1 <= n <= 2^31 - 1*/
+
+func arrangeCoins(n int) int {
+    return int((math.Sqrt(1+float64(8*n))-1)/2)
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
