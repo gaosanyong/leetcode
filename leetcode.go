f9b9875c24d354f9aace744caade313be1fbf838
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-26 22:36:27 -0400

    solve'em all (448)

--- leetcode.go
+++ leetcode.go
@@ -3119,6 +3119,44 @@ func arrangeCoins(n int) int {
 }
 
 
+/*448. Find All Numbers Disappeared in an Array (Easy)
+Given an array nums of n integers where nums[i] is in the range [1, n], return
+an array of all the integers in the range [1, n] that do not appear in nums.
+
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+
+Example 2:
+Input: nums = [1,1]
+Output: [2]
+
+Constraints:
+* n == nums.length
+* 1 <= n <= 10^5
+* 1 <= nums[i] <= n
+
+Follow up: Could you do it without extra space and in O(n) runtime? You may
+           assume the returned list does not count as extra space.*/
+
+func findDisappearedNumbers(nums []int) []int {
+    for _, x := range nums {
+        x = int(math.Abs(float64(x)))
+        if nums[x-1] > 0 {
+            nums[x-1] *= -1
+        }
+    }
+    k := 0
+    for i, x := range nums {
+        if x >= 0 {
+            nums[k] = i+1
+            k++
+        }
+    }
+    return nums[:k]
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
