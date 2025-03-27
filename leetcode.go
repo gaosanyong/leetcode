82ee99c1c4cda54fc0f8c7976d86db71fab121ab
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-27 01:01:09 -0400

    solve'em all (500)

--- leetcode.go
+++ leetcode.go
@@ -3527,6 +3527,61 @@ func nextGreaterElement(nums1 []int, nums2 []int) []int {
 }
 
 
+/*500. Keyboard Row (Easy)
+Given an array of strings words, return the words that can be typed using
+letters of the alphabet on only one row of American keyboard like the image
+below. Note that the strings are case-insensitive, both lowercased and
+uppercased of the same letter are treated as if they are at the same row. In the
+American keyboard:
+* the first row consists of the characters "qwertyuiop",
+* the second row consists of the characters "asdfghjkl", and
+* the third row consists of the characters "zxcvbnm".
+
+Example 1:
+Input: words = ["Hello","Alaska","Dad","Peace"]
+Output: ["Alaska","Dad"]
+Explanation: Both "a" and "A" are in the 2nd row of the American keyboard due to
+             case insensitivity.
+
+Example 2:
+Input: words = ["omk"]
+Output: []
+
+Example 3:
+Input: words = ["adsdf","sfd"]
+Output: ["adsdf","sfd"]
+
+Constraints:
+* 1 <= words.length <= 20
+* 1 <= words[i].length <= 100
+* words[i] consists of English letters (both lowercase and uppercase).*/
+
+func findWords(words []string) []string {
+    rows := []string{"qwertyuiop", "asdfghjkl", "zxcvbnm"}
+    loc := [26]int{}
+    for i, row := range rows {
+        for _, ch := range row {
+            loc[ch - 'a'] = i
+        }
+    }
+    ans := []string{}
+    for _, word := range words {
+        val := -1
+        for _, ch := range strings.ToLower(word) {
+            if val != -1 && val != loc[ch - 'a'] {
+                val = -1
+                break
+            }
+            val = loc[ch - 'a']
+        }
+        if val >= 0 {
+            ans = append(ans, word)
+        }
+    }
+    return ans
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
