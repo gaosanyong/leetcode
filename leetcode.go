b981cfbb08a939acf7fcfad280623b5e2670ba18
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-26 23:11:02 -0400

    solve'em all (461)

--- leetcode.go
+++ leetcode.go
@@ -3201,6 +3201,33 @@ func findContentChildren(g []int, s []int) int {
 }
 
 
+/*461. Hamming Distance (Easy)
+The Hamming distance between two integers is the number of positions at which
+the corresponding bits are different. Given two integers x and y, return the
+Hamming distance between them.
+
+Example 1:
+Input: x = 1, y = 4
+Output: 2
+Explanation: 1   (0 0 0 1)
+             4   (0 1 0 0)
+                    ↑   ↑
+             The above arrows point to positions where the corresponding bits
+             are different.
+
+Example 2:
+Input: x = 3, y = 1
+Output: 1
+
+Constraints: 0 <= x, y <= 2^31 - 1
+
+Note: This question is the same as 2220: Minimum Bit Flips to Convert Number.*/
+
+func hammingDistance(x int, y int) int {
+    return bits.OnesCount(uint(x ^ y))
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
