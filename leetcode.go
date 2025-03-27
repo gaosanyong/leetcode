7db96d9b6812c67b16ba472e4a5674ba90cfc8bf
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-26 23:00:15 -0400

    solve'em all (455)

--- leetcode.go
+++ leetcode.go
@@ -3157,6 +3157,50 @@ func findDisappearedNumbers(nums []int) []int {
 }
 
 
+/*455. Assign Cookies (Easy)
+Assume you are an awesome parent and want to give your children some cookies.
+But, you should give each child at most one cookie. Each child i has a greed
+factor g[i], which is the minimum size of a cookie that the child will be
+content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign
+the cookie j to the child i, and the child i will be content. Your goal is to
+maximize the number of your content children and output the maximum number.
+
+Example 1:
+Input: g = [1,2,3], s = [1,1]
+Output: 1
+Explanation: You have 3 children and 2 cookies. The greed factors of 3 children
+             are 1, 2, 3. And even though you have 2 cookies, since their size
+             is both 1, you could only make the child whose greed factor is 1
+             content. You need to output 1.
+
+Example 2:
+Input: g = [1,2], s = [1,2,3]
+Output: 2
+Explanation: You have 2 children and 3 cookies. The greed factors of 2 children
+             are 1, 2. You have 3 cookies and their sizes are big enough to
+             gratify all of the children, You need to output 2.
+
+Constraints:
+* 1 <= g.length <= 3 * 10^4
+* 0 <= s.length <= 3 * 10^4
+* 1 <= g[i], s[j] <= 2^31 - 1
+
+Note: This question is the same as 2410: Maximum Matching of Players With
+      Trainers.*/
+
+func findContentChildren(g []int, s []int) int {
+    sort.Ints(g)
+    sort.Ints(s)
+    k := 0
+    for _, v := range s {
+        if k < len(g) && g[k] <= v {
+            k++
+        }
+    }
+    return k
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
