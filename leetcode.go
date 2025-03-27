c5591920970134655316ec72ca0047bb9b0e5c62
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-26 23:16:59 -0400

    solve'em all (463)

--- leetcode.go
+++ leetcode.go
@@ -3228,6 +3228,52 @@ func hammingDistance(x int, y int) int {
 }
 
 
+/*463. Island Perimeter (Easy)
+You are given row x col grid representing a map where grid[i][j] = 1 represents
+land and grid[i][j] = 0 represents water. Grid cells are connected horizontally/
+vertically (not diagonally). The grid is completely surrounded by water, and
+there is exactly one island (i.e., one or more connected land cells). The island
+doesn't have "lakes", meaning the water inside isn't connected to the water
+around the island. One cell is a square with side length 1. The grid is
+rectangular, width and height don't exceed 100. Determine the perimeter of the
+island.
+
+Example 1:
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+
+Example 2:
+Input: grid = [[1]]
+Output: 4
+
+Example 3:
+Input: grid = [[1,0]]
+Output: 4
+
+Constraints:
+* row == grid.length
+* col == grid[i].length
+* 1 <= row, col <= 100
+* grid[i][j] is 0 or 1.
+* There is exactly one island in grid.*/
+
+func islandPerimeter(grid [][]int) int {
+    m, n := len(grid), len(grid[0])
+    ans := 0
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if grid[i][j] == 1 {
+                ans += 4
+                if i > 0 && grid[i-1][j] == 1 { ans -= 2 }
+                if j > 0 && grid[i][j-1] == 1 { ans -= 2 }
+            }
+        }
+    }
+    return ans
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
