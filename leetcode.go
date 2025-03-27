1de5e267bb653eb50760f7ce1af057e0767904a8
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-26 23:55:33 -0400

    solve'em all (482)

--- leetcode.go
+++ leetcode.go
@@ -3303,6 +3303,49 @@ func findComplement(num int) int {
 }
 
 
+/*482. License Key Formatting (Easy)
+You are given a license key represented as a string s that consists of only
+alphanumeric characters and dashes. The string is separated into n + 1 groups by
+n dashes. You are also given an integer k. We want to reformat the string s such
+that each group contains exactly k characters, except for the first group, which
+could be shorter than k but still must contain at least one character.
+Furthermore, there must be a dash inserted between two groups, and you should
+convert all lowercase letters to uppercase. Return the reformatted license key.
+
+Example 1:
+Input: s = "5F3Z-2e-9-w", k = 4
+Output: "5F3Z-2E9W"
+Explanation: The string s has been split into two parts, each part has 4
+             characters. Note that the two extra dashes are not needed and can
+             be removed.
+
+Example 2:
+Input: s = "2-5g-3-J", k = 2
+Output: "2-5G-3J"
+Explanation: The string s has been split into three parts, each part has 2
+             characters except the first part as it could be shorter as
+             mentioned above.
+
+Constraints:
+* 1 <= s.length <= 10^5
+* s consists of English letters, digits, and dashes '-'.
+* 1 <= k <= 10^4*/
+
+func licenseKeyFormatting(s string, k int) string {
+    s = strings.ToUpper(strings.Replace(s, "-", "", -1))
+    ans := []string{}
+    i := 0
+    if r := len(s)%k; r != 0 {
+        ans = append(ans, s[:r])
+        i = r
+    }
+    for ; i < len(s); i += k {
+        ans = append(ans, s[i:i+k])
+    }
+    return strings.Join(ans, "-")
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
