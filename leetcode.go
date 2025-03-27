1850d2d62b37c874d77dd46da2e66304affb5820
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-26 23:27:03 -0400

    solve'em all (476)

--- leetcode.go
+++ leetcode.go
@@ -3274,6 +3274,35 @@ func islandPerimeter(grid [][]int) int {
 }
 
 
+/*476. Number Complement (Easy)
+The complement of an integer is the integer you get when you flip all the 0's to
+1's and all the 1's to 0's in its binary representation. For example, The
+integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+Given an integer num, return its complement.
+
+Example 1:
+Input: num = 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and
+             its complement is 010. So you need to output 2.
+
+Example 2:
+Input: num = 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its
+             complement is 0. So you need to output 0.
+
+Constraints: 1 <= num < 2^31
+
+Note: This question is the same as 1009:
+      https://leetcode.com/problems/complement-of-base-10-integer/*/
+
+func findComplement(num int) int {
+    m := 1 << bits.Len(uint(num))-1
+    return m ^ num
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
