33bbecdd250080c9cc12d83ec03288d00b6a1609
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-27 23:51:40 -0400

    solve'em all (501)

--- leetcode.go
+++ leetcode.go
@@ -3622,6 +3622,57 @@ func findWords(words []string) []string {
 }
 
 
+/*501. Find Mode in Binary Search Tree (Easy)
+Given the root of a binary search tree (BST) with duplicates, return all the
+mode(s) (i.e., the most frequently occurred element) in it. If the tree has more
+than one mode, return them in any order. Assume a BST is defined as follows:
+* The left subtree of a node contains only nodes with keys less than or equal to
+  the node's key.
+* The right subtree of a node contains only nodes with keys greater than or
+  equal to the node's key.
+* Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+Input: root = [1,null,2,2]
+Output: [2]
+
+Example 2:
+Input: root = [0]
+Output: [0]
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 10^4].
+* -10^5 <= Node.val <= 10^5
+
+Follow up: Could you do that without using any extra space? (Assume that the
+           implicit stack space incurred due to recursion does not count).*/
+
+func findMode(root *TreeNode) []int {
+    ans := []int{}
+    cnt, most, prev := 0, 0, 0
+    node := root
+    stack := []*TreeNode{}
+    for node != nil || len(stack) > 0 {
+        if node != nil {
+            stack = append(stack, node)
+            node = node.Left
+        } else {
+            node = stack[len(stack)-1]
+            stack = stack[:len(stack)-1]
+            if cnt > 0 && prev != node.Val { cnt = 0 }
+            cnt++
+            if cnt >= most {
+                if cnt > most { ans, most = nil, cnt }
+                ans = append(ans, node.Val)
+            }
+            prev = node.Val
+            node = node.Right
+        }
+    }
+    return ans
+}
+
+
 /*506. Relative Ranks (Easy)
 You are given an integer array score of size n, where score[i] is the score of
 the ith athlete in a competition. All the scores are guaranteed to be unique.
