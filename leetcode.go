7d2f6430bdacf2ad07bf2530bc7d406a179130ad
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-27 23:22:32 -0400

    solve'em all (506)

--- leetcode.go
+++ leetcode.go
@@ -3622,6 +3622,58 @@ func findWords(words []string) []string {
 }
 
 
+/*506. Relative Ranks (Easy)
+You are given an integer array score of size n, where score[i] is the score of
+the ith athlete in a competition. All the scores are guaranteed to be unique.
+The athletes are placed based on their scores, where the 1st place athlete has
+the highest score, the 2nd place athlete has the 2nd highest score, and so on.
+The placement of each athlete determines their rank:
+* The 1st place athlete's rank is "Gold Medal".
+* The 2nd place athlete's rank is "Silver Medal".
+* The 3rd place athlete's rank is "Bronze Medal".
+* For the 4th place to the nth place athlete, their rank is their placement
+  number (i.e., the xth place athlete's rank is "x").
+Return an array answer of size n where answer[i] is the rank of the ith athlete.
+
+Example 1:
+Input: score = [5,4,3,2,1]
+Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+
+Example 2:
+Input: score = [10,3,8,9,4]
+Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+
+Constraints:
+* n == score.length
+* 1 <= n <= 10^4
+* 0 <= score[i] <= 10^6
+* All the values in score are unique.*/
+
+func findRelativeRanks(score []int) []string {
+    aug := [][]int{}
+    for i, x := range score {
+        aug = append(aug, []int{i, x})
+    }
+    sort.Slice(aug, func(i, j int) bool {
+        return aug[i][1] > aug[j][1]
+    })
+    ans := make([]string, len(score))
+    for k, val := range aug {
+        elem := ""
+        switch k {
+            case 0: elem = "Gold Medal"
+            case 1: elem = "Silver Medal"
+            case 2: elem = "Bronze Medal"
+            default: elem = strconv.Itoa(k+1)
+        }
+        ans[val[0]] = elem
+    }
+    return ans
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
