7cf1d84a9fbb3e7ce0904147d75a1c9b31975799
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-28 00:13:00 -0400

    solve'em all (509)

--- leetcode.go
+++ leetcode.go
@@ -3772,6 +3772,43 @@ func checkPerfectNumber(num int) bool {
 }
 
 
+/*509. Fibonacci Number (Easy)
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the
+Fibonacci sequence, such that each number is the sum of the two preceding ones,
+starting from 0 and 1. That is,
+    F(0) = 0, F(1) = 1
+    F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+Example 1:
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Example 2:
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+Example 3:
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+Constraints: 0 <= n <= 30*/
+
+func fib(n int) int {
+    if n <= 1 {
+        return n
+    }
+    f0, f1 := 0, 1
+    for i := 2; i <= n; i++ {
+        f0, f1 = f1, f0+f1
+    }
+    return f1
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
