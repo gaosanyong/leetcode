4e41dd95c50d5ad174bbda419fef5f62ed70e656
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-03-29 11:42:10 -0400

    solve'em all (520)

--- leetcode.go
+++ leetcode.go
@@ -3809,6 +3809,33 @@ func fib(n int) int {
 }
 
 
+/*520. Detect Capital (Easy)
+We define the usage of capitals in a word to be right when one of the following
+cases holds:
+* All letters in this word are capitals, like "USA".
+* All letters in this word are not capitals, like "leetcode".
+* Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+Example 1:
+Input: word = "USA"
+Output: true
+
+Example 2:
+Input: word = "FlaG"
+Output: false
+
+Constraints:
+* 1 <= word.length <= 100
+* word consists of lowercase and uppercase English letters.*/
+
+func detectCapitalUse(word string) bool {
+    return word == strings.ToUpper(word) ||
+           word == strings.ToLower(word) ||
+           word == strings.ToUpper(word[:1]) + strings.ToLower(word[1:])
+}
+
+
 /*3492. Maximum Containers on a Ship (Easy)
 You are given a positive integer n representing an n x n cargo deck on a
 ship. Each cell on the deck can hold one container with a weight of exactly
