9381c974b7586dc8666e8723c1b3830ba0981798
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-08 00:45:43 -0400

    solve'em all (1)

---
+++ leetcode.rb
@@ -0,0 +1,39 @@
+=begin
+1. Two Sum (Easy)
+Given an array of integers nums and an integer target, return indices of the two
+numbers such that they add up to target. You may assume that each input would
+have exactly one solution, and you may not use the same element twice. You can
+return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+Constraints:
+* 2 <= nums.length <= 10^4
+* -10^9 <= nums[i] <= 10^9
+* -10^9 <= target <= 10^9
+* Only one valid answer exists.
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time
+           complexity?
+=end
+
+def two_sum(nums, target)
+    loc = {}
+    nums.each_with_index do |x, i|
+        if loc.key?(target-x)
+            return [loc[target-x], i]
+        end
+        loc[x] = i
+    end
+end
