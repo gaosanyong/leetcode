e21be7c6f3aaf93b4b881d2712cfae424aa175d7
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-15 23:47:57 -0400

    solve'em all (20)

--- leetcode.rb
+++ leetcode.rb
@@ -164,3 +164,52 @@ def longest_common_prefix(strs)
     end
     return ans.join
 end
+
+
+=begin
+20. Valid Parentheses (Easy)
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid. An input string is valid if:
+* Open brackets must be closed by the same type of brackets.
+* Open brackets must be closed in the correct order.
+* Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+
+Example 4:
+Input: s = "([])"
+Output: true
+
+Example 5:
+Input: s = "([)]"
+Output: false
+
+Constraints:
+* 1 <= s.length <= 10^4
+* s consists of parentheses only '()[]{}'.
+=end
+
+def is_valid(s)
+    pair = {'(' => ')', '[' => ']', '{' => '}'}
+    stack = []
+    s.each_char do |ch|
+        if pair.key?(ch)
+            stack.push(pair[ch])
+        elsif stack.empty? || ch != stack[-1]
+            return false
+        else
+            stack.pop()
+        end
+    end
+    return stack.empty?
+end
