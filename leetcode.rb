50c509f186f36ce51a9f50bfc83acb1dc2ca7782
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-15 22:28:45 -0400

    solve'em all (9)

--- leetcode.rb
+++ leetcode.rb
@@ -37,3 +37,38 @@ def two_sum(nums, target)
         loc[x] = i
     end
 end
+
+=begin
+9. Palindrome Number (Easy)
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Example 1:
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes
+             121-. Therefore it is not a palindrome.
+
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Constraints: -2^31 <= x <= 2^31 - 1
+
+Follow up: Could you solve it without converting the integer to a string?
+=end
+
+def is_palindrome(x)
+    return false if x < 0 || x % 10 == 0 && x != 0
+    y = 0
+    while x > y
+        y = 10*y + x % 10
+        x /= 10
+    end
+    return [y, y / 10].include?(x)
+end
