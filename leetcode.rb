375f08c6d15030388ba30decb5b86081506cc13a
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-15 23:01:21 -0400

    solve'em all (14)

--- leetcode.rb
+++ leetcode.rb
@@ -132,3 +132,35 @@ def roman_to_int(s)
     end
     return ans
 end
+
+
+=begin
+14. Longest Common Prefix (Easy)
+Write a function to find the longest common prefix string amongst an array of
+strings. If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Constraints:
+* 1 <= strs.length <= 200
+* 0 <= strs[i].length <= 200
+* strs[i] consists of only lowercase English letters if it is non-empty.
+=end
+
+def longest_common_prefix(strs)
+    ans = []
+    strs[0].each_char.with_index do |ch, j|
+        break unless (1..strs.size-1).each do |i|
+            break if j == strs[i].size || ch != strs[i][j]
+        end
+        ans.push(ch)
+    end
+    return ans.join
+end
