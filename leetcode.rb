bab318f38c88d7bd350f901a43ff84f28308dcb5
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-15 23:55:05 -0400

    solve'em all (21)

--- leetcode.rb
+++ leetcode.rb
@@ -213,3 +213,44 @@ def is_valid(s)
     end
     return stack.empty?
 end
+
+
+=begin
+21. Merge Two Sorted Lists (Easy)
+You are given the heads of two sorted linked lists list1 and list2. Merge the
+two lists into one sorted list. The list should be made by splicing together the
+nodes of the first two lists. Return the head of the merged linked list.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+Constraints:
+* The number of nodes in both lists is in the range [0, 50].
+* -100 <= Node.val <= 100
+* Both list1 and list2 are sorted in non-decreasing order.
+=end
+
+def merge_two_lists(list1, list2)
+    dummy = node = ListNode.new
+    while list1 && list2
+        if list1.val < list2.val
+            node.next = list1
+            list1 = list1.next
+        else
+            node.next = list2
+            list2 = list2.next
+        end
+        node = node.next
+    end
+    node.next = list1 || list2
+    return dummy.next
+end
