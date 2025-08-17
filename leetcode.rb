ae011e770c09625a3a58da0bc3175255bc1ee853
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-17 12:16:09 -0400

    solve'em all (67)

--- leetcode.rb
+++ leetcode.rb
@@ -502,3 +502,36 @@ def plus_one(digits)
     digits.unshift(carry) if carry == 1
     return digits
 end
+
+
+=begin
+67. Add Binary (Easy)
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+Constraints:
+* 1 <= a.length, b.length <= 10^4
+* a and b consist only of '0' or '1' characters.
+* Each string does not contain leading zeros except for the zero itself.
+=end
+
+def add_binary(a, b)
+    ans = []
+    carry, i, j = 0, a.size-1, b.size-1
+    while i >= 0 || j >= 0 || carry == 1 do
+        carry += 1 if i >= 0 && a[i] == '1'
+        carry += 1 if j >= 0 && b[j] == '1'
+        ans.push(carry % 2)
+        carry /= 2
+        i -= 1
+        j -= 1
+    end
+    return ans.reverse.map(&:to_s).join
+end
