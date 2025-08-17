5261b7fafb6ef3b6df8b705dba0221b2324b1d9e
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-17 13:03:28 -0400

    solve'em all (94)

--- leetcode.rb
+++ leetcode.rb
@@ -684,3 +684,51 @@ def merge(nums1, m, nums2, n)
         end
     end
 end
+
+
+=begin
+94. Binary Tree Inorder Traversal (Easy)
+Given the root of a binary tree, return the inorder traversal of its nodes'
+values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Explanation:
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+Explanation:
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 100].
+* -100 <= Node.val <= 100
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+=end
+
+def inorder_traversal(root)
+    ans = []
+    node = root
+    stack = []
+    while node || stack.any?
+        if node
+            stack.push(node)
+            node = node.left
+        else
+            node = stack.pop
+            ans.push(node.val)
+            node = node.right
+        end
+    end
+    return ans
+end
