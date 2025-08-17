410d8ef2228355a2124f14cfd15614b191bff1f1
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-17 12:01:27 -0400

    solve'em all (66)

--- leetcode.rb
+++ leetcode.rb
@@ -457,3 +457,48 @@ Constraints:
 def length_of_last_word(s)
     return s.split.last.size
 end
+
+
+=begin
+66. Plus One (Easy)
+You are given a large integer represented as an integer array digits, where each
+digits[i] is the ith digit of the integer. The digits are ordered from most
+significant to least significant in left-to-right order. The large integer does
+not contain any leading 0's. Increment the large integer by one and return the
+resulting array of digits.
+
+Example 1:
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123. Incrementing by one gives
+             123 + 1 = 124. Thus, the result should be [1,2,4].
+
+Example 2:
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321. Incrementing by one gives
+             4321 + 1 = 4322. Thus, the result should be [4,3,2,2].
+
+Example 3:
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9. Incrementing by one gives
+             9 + 1 = 10. Thus, the result should be [1,0].
+
+Constraints:
+* 1 <= digits.length <= 100
+* 0 <= digits[i] <= 9
+* digits does not contain any leading 0's.
+=end
+
+def plus_one(digits)
+    carry = 1
+    (digits.size-1).downto(0) do |i|
+        carry += digits[i]
+        digits[i] = carry % 10
+        carry /= 10
+        break if carry == 0
+    end
+    digits.unshift(carry) if carry == 1
+    return digits
+end
