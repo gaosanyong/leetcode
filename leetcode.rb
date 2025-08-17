d980f4948ff4242e001f72883892a8571ae3ce11
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-17 14:50:04 -0400

    solve'em all (110)

--- leetcode.rb
+++ leetcode.rb
@@ -847,3 +847,35 @@ def sorted_array_to_bst(nums)
 
     return check.call(0, nums.size)
 end
+
+
+=begin
+110. Balanced Binary Tree (Easy)
+Given a binary tree, determine if it is height-balanced.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+
+Example 3:
+Input: root = []
+Output: true
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 5000].
+* -10^4 <= Node.val <= 10^4
+=end
+
+def is_balanced(root)
+    check = lambda do |node|
+        return 0 if node.nil?
+        lh = check.call(node.left)
+        rh = check.call(node.right)
+        return lh == -1 || rh == -1 || (lh-rh).abs > 1 ? -1 : 1 + [lh, rh].max
+    end
+    return check.call(root) >= 0
+end
