5c31b0dad7d98a8a239854cd856f6c4aa6c4c69a
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-16 22:09:44 -0400

    solve'em all (28)

--- leetcode.rb
+++ leetcode.rb
@@ -369,3 +369,29 @@ def remove_element(nums, val)
     end
     return k
 end
+
+
+=begin
+28. Find the Index of the First Occurrence in a String (Easy)
+Given two strings needle and haystack, return the index of the first occurrence
+of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6. The first occurrence is at index 0,
+             so we return 0.
+
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+Constraints:
+* 1 <= haystack.length, needle.length <= 10^4
+* haystack and needle consist of only lowercase English characters.
+=end
+
+def str_str(haystack, needle)
+    return haystack.index(needle) || -1
+end
