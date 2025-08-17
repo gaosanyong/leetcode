96711d4f1ad97c98bece35684588e5fda08b3334
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-16 23:47:15 -0400

    solve'em all (58)

--- leetcode.rb
+++ leetcode.rb
@@ -425,3 +425,35 @@ Constraints:
 def search_insert(nums, target)
     return nums.bsearch_index{|x| target <= x} || nums.size
 end
+
+
+=begin
+58. Length of Last Word (Easy)
+Given a string s consisting of words and spaces, return the length of the last
+word in the string. A word is a maximal substring consisting of non-space
+characters only.
+
+Example 1:
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2:
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Example 3:
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
+Constraints:
+* 1 <= s.length <= 10^4
+* s consists of only English letters and spaces ' '.
+* There will be at least one word in s.
+=end
+
+def length_of_last_word(s)
+    return s.split.last.size
+end
