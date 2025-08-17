cda47d81692d4246c60ff5e54662193aa7c5f13b
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-17 12:38:18 -0400

    solve'em all (83)

--- leetcode.rb
+++ leetcode.rb
@@ -598,3 +598,35 @@ def climb_stairs(n)
     end
     return f1
 end
+
+
+=begin
+83. Remove Duplicates from Sorted List (Easy)
+Given the head of a sorted linked list, delete all duplicates such that each
+element appears only once. Return the linked list sorted as well.
+
+Example 1:
+Input: head = [1,1,2]
+Output: [1,2]
+
+Example 2:
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+
+Constraints:
+* The number of nodes in the list is in the range [0, 300].
+* -100 <= Node.val <= 100
+* The list is guaranteed to be sorted in ascending order.
+=end
+
+def delete_duplicates(head)
+    node = head
+    while node && node.next
+        if node.val == node.next.val
+            node.next = node.next.next
+        else
+            node = node.next
+        end
+    end
+    return head
+end
