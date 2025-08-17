c5d57d6681999a7b9baf67204ac3e85bc385283c
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-17 12:35:09 -0400

    solve'em all (70)

--- leetcode.rb
+++ leetcode.rb
@@ -565,3 +565,36 @@ def my_sqrt(x)
     end
     return ans
 end
+
+
+=begin
+70. Climbing Stairs (Easy)
+You are climbing a staircase. It takes n steps to reach the top. Each time you
+can either climb 1 or 2 steps. In how many distinct ways can you climb to the
+top?
+
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+             1. 1 step + 1 step
+             2. 2 steps
+
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+             1. 1 step + 1 step + 1 step
+             2. 1 step + 2 steps
+             3. 2 steps + 1 step
+
+Constraints: 1 <= n <= 45
+=end
+
+def climb_stairs(n)
+    f0 = f1 = 1
+    (1..n-1).each do
+        f0, f1 = f1, f0+f1
+    end
+    return f1
+end
