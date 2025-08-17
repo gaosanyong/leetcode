b3ad942aa1621ef366f593391e68dc5547a133d0
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-16 23:44:15 -0400

    solve'em all (35)

--- leetcode.rb
+++ leetcode.rb
@@ -395,3 +395,33 @@ Constraints:
 def str_str(haystack, needle)
     return haystack.index(needle) || -1
 end
+
+
+=begin
+35. Search Insert Position (Easy)
+Given a sorted array of distinct integers and a target value, return the index
+if the target is found. If not, return the index where it would be if it were
+inserted in order. You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+Constraints:
+* 1 <= nums.length <= 10^4
+* -10^4 <= nums[i] <= 10^4
+* nums contains distinct values sorted in ascending order.
+* -10^4 <= target <= 10^4
+=end
+
+def search_insert(nums, target)
+    return nums.bsearch_index{|x| target <= x} || nums.size
+end
