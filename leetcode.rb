f1eacb1a6b9ec4377a8640f1a926e02d9c911c42
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-17 13:54:09 -0400

    solve'em all (104)

--- leetcode.rb
+++ leetcode.rb
@@ -790,3 +790,28 @@ def is_symmetric(root)
     end
     return check.call(root, root)
 end
+
+
+=begin
+104. Maximum Depth of Binary Tree (Easy)
+Given the root of a binary tree, return its maximum depth. A binary tree's
+maximum depth is the number of nodes along the longest path from the root node
+down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 10^4].
+* -100 <= Node.val <= 100
+=end
+
+def max_depth(root)
+    return 0 if root.nil?
+    return 1 + [max_depth(root.left), max_depth(root.right)].max
+end
