896ca56816b011c6961a1b39d783f3740bbb9603
Ye Gao <gaoyescm@meta.com>
2025-08-18 19:21:19 -0400

    solve'em all (169)

--- leetcode.rb
+++ leetcode.rb
@@ -1380,3 +1380,39 @@ def convert_to_title(column_number)
     end
     return ans.join.reverse
 end
+
+
+=begin
+169. Majority Element (Easy)
+Given an array nums of size n, return the majority element. The majority element
+is the element that appears more than ⌊n / 2⌋ times. You may assume that the
+majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Constraints:
+* n == nums.length
+* 1 <= n <= 5 * 10^4
+* -10^9 <= nums[i] <= 10^9
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+=end
+
+def majority_element(nums)
+    ans = cnt = 0
+    nums.each do |x|
+        if ans == x || cnt == 0
+            ans = x
+            cnt += 1
+        else
+            cnt -= 1
+        end
+    end
+    return ans
+end
