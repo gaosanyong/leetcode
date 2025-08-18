1356083c71a04928e451b5776f2c3e96c69aff83
Ye Gao <gaoyescm@meta.com>
2025-08-18 09:42:36 -0400

    solve'em all (125)

--- leetcode.rb
+++ leetcode.rb
@@ -1053,3 +1053,37 @@ def max_profit(prices)
     end
     return ans
 end
+
+=begin
+125. Valid Palindrome (Easy)
+A phrase is a palindrome if, after converting all uppercase letters into 
+lowercase letters and removing all non-alphanumeric characters, it reads the 
+same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+             Since an empty string reads the same forward and backward, it is a 
+             palindrome.
+
+Constraints:
+* 1 <= s.length <= 2 * 105
+* s consists only of printable ASCII characters.
+=end
+
+def is_palindrome(s)
+    s = s.downcase.delete("^a-z0-9")
+    return s == s.reverse
+end
