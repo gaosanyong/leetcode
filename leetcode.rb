dee7ee1ed480a54a720cadae40dccaebba789dd0
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-17 23:48:21 -0400

    solve'em all (118)

--- leetcode.rb
+++ leetcode.rb
@@ -955,3 +955,33 @@ def has_path_sum(root, target_sum)
     end
     return false
 end
+
+
+=begin
+118. Pascal's Triangle (Easy)
+Given an integer numRows, return the first numRows of Pascal's triangle. In
+Pascal's triangle, each number is the sum of the two numbers directly above it
+as shown:
+
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+Input: numRows = 1
+Output: [[1]]
+
+Constraints: 1 <= numRows <= 30
+=end
+
+def generate(num_rows)
+    ans = [[1]]
+    (2..num_rows).each do |i|
+        row = Array.new(i, 1)
+        (1..i-2).each do |j|
+            row[j] = ans[-1][j-1] + ans[-1][j]
+        end
+        ans.push(row)
+    end
+    return ans
+end
