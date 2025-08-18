5b30688c8cf77cc95528e4a05763fda8ce5ede45
Ye Gao <gaoyescm@meta.com>
2025-08-18 09:59:08 -0400

    solve'em all (144)

--- leetcode.rb
+++ leetcode.rb
@@ -1162,3 +1162,48 @@ def hasCycle(head)
     end 
     return false 
 end
+
+
+=begin
+144. Binary Tree Preorder Traversal (Easy)
+Given the root of a binary tree, return the preorder traversal of its nodes' 
+values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Explanation:
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [1,2,4,5,6,7,3,8,9]
+Explanation:
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 100].
+* -100 <= Node.val <= 100
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+=end
+
+def preorder_traversal(root)
+    ans = []
+    if root
+        stack = [root]
+        while stack.any?
+            node = stack.pop
+            ans.push(node.val)
+            stack.push(node.right) if node.right
+            stack.push(node.left) if node.left
+        end
+    end
+    return ans 
+end
