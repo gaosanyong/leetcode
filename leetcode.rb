38a3f9ab76b29e81daebf7c726083181b9821300
Ye Gao <gaoyescm@meta.com>
2025-08-18 09:47:16 -0400

    solve'em all (136)

--- leetcode.rb
+++ leetcode.rb
@@ -1087,3 +1087,33 @@ def is_palindrome(s)
     s = s.downcase.delete("^a-z0-9")
     return s == s.reverse
 end
+
+
+=begin
+136. Single Number (Easy)
+Given a non-empty array of integers nums, every element appears twice except for 
+one. Find that single one. You must implement a solution with a linear runtime 
+complexity and use only constant extra space.
+
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+
+Constraints:
+* 1 <= nums.length <= 3 * 10^4
+* -3 * 104 <= nums[i] <= 3 * 10^4
+* Each element in the array appears twice except for one element which appears 
+  only once.
+=end
+
+def single_number(nums)
+    return nums.reduce(0) {|s, x| s ^ x}
+end
