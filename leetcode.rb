26c0a5fef6358a559cb2086790e063d9a0721449
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-18 00:06:43 -0400

    solve'em all (119)

--- leetcode.rb
+++ leetcode.rb
@@ -985,3 +985,35 @@ def generate(num_rows)
     end
     return ans
 end
+
+
+=begin
+119. Pascal's Triangle II (Easy)
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's
+triangle. In Pascal's triangle, each number is the sum of the two numbers
+directly above it as shown:
+
+Example 1:
+Input: rowIndex = 3
+Output: [1,3,3,1]
+
+Example 2:
+Input: rowIndex = 0
+Output: [1]
+
+Example 3:
+Input: rowIndex = 1
+Output: [1,1]
+
+Constraints: 0 <= rowIndex <= 33
+
+Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+=end
+
+def get_row(row_index)
+    ans = Array.new(row_index+1, 1)
+    (1..row_index-1).each do |j|
+        ans[j] = ans[j-1]*(row_index-j+1)/j
+    end
+    return ans
+end
