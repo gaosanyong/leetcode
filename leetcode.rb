dbd187d6d889532a3504e35a7e6e086911a00138
Ye Gao <gaoyescm@meta.com>
2025-08-18 19:38:26 -0400

    solve'em all (190)

--- leetcode.rb
+++ leetcode.rb
@@ -1456,3 +1456,41 @@ def title_to_number(column_title)
     end
     return ans
 end
+
+
+=begin
+190. Reverse Bits (Easy)
+Reverse bits of a given 32 bits unsigned integer.
+
+Note:
+* Note that in some languages, such as Java, there is no unsigned integer type.
+  In this case, both input and output will be given as a signed integer type.
+  They should not affect your implementation, as the integer's internal binary
+  representation is the same, whether it is signed or unsigned.
+* In Java, the compiler represents the signed integers using 2's complement
+  notation.
+
+Example 1:
+Input: n = 43261596
+Output: 964176192
+Explanation: Integer	Binary
+             43261596	00000010100101000001111010011100
+             964176192	00111001011110000010100101000000
+
+Example 2:
+Input: n = 2147483644
+Output: 1073741822
+Explanation: Integer	Binary
+             2147483644	01111111111111111111111111111100
+             1073741822	00111111111111111111111111111110
+
+Constraints:
+* 0 <= n <= 231 - 2
+* n is even.
+
+Follow up: If this function is called many times, how would you optimize it?
+=end
+
+def reverse_bits(n)
+    32.times.reduce(0) { (_1 << 1) | (n & 1).tap { n >>= 1 } }
+end
