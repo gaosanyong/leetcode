82431a7524ef41bc2259ef74e501e3bf5ac78560
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-18 23:26:58 -0400

    solve'em all (203)

--- leetcode.rb
+++ leetcode.rb
@@ -1530,3 +1530,39 @@ def hamming_weight(n)
     end
     return ans
 end
+
+
+=begin
+203. Remove Linked List Elements (Easy)
+Given the head of a linked list and an integer val, remove all the nodes of the
+linked list that has Node.val == val, and return the new head.
+
+Example 1:
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+Input: head = [], val = 1
+Output: []
+
+Example 3:
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+Constraints:
+* The number of nodes in the list is in the range [0, 10^4].
+* 1 <= Node.val <= 50
+* 0 <= val <= 50
+=end
+
+def remove_elements(head, val)
+    dummy = node = ListNode.new(0, head)
+    while node.next
+        if node.next.val == val
+            node.next = node.next.next
+        else
+            node = node.next
+        end
+    end
+    return dummy.next
+end
