da66d1bc618e942da0d04cf32c740d6e921352a7
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-08-18 23:25:47 -0400

    solve'em all (191)

--- leetcode.rb
+++ leetcode.rb
@@ -1494,3 +1494,39 @@ Follow up: If this function is called many times, how would you optimize it?
 def reverse_bits(n)
     32.times.reduce(0) { (_1 << 1) | (n & 1).tap { n >>= 1 } }
 end
+
+
+=begin
+191. Number of 1 Bits (Easy)
+Given a positive integer n, write a function that returns the number of set bits
+in its binary representation (also known as the Hamming weight).
+
+Example 1:
+Input: n = 11
+Output: 3
+Explanation: The input binary string 1011 has a total of three set bits.
+
+Example 2:
+Input: n = 128
+Output: 1
+Explanation: The input binary string 10000000 has a total of one set bit.
+
+Example 3:
+Input: n = 2147483645
+Output: 30
+Explanation: The input binary string 1111111111111111111111111111101 has a total
+             of thirty set bits.
+
+Constraints: 1 <= n <= 2^31 - 1
+
+Follow up: If this function is called many times, how would you optimize it?
+=end
+
+def hamming_weight(n)
+    ans = 0
+    while n > 0
+        ans += 1
+        n &= n-1
+    end
+    return ans
+end
