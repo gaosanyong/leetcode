13b8bbda999838b27d6b48d547d79e1a09a12957
Ye Gao <gaoyescm@meta.com>
2025-08-19 22:02:37 -0400

    solve'em all (205)

--- leetcode.rb
+++ leetcode.rb
@@ -1605,3 +1605,44 @@ def remove_elements(head, val)
     end
     return dummy.next
 end
+
+
+=begin
+205. Isomorphic Strings (Easy)
+Given two strings s and t, determine if they are isomorphic. Two strings s and
+t are isomorphic if the characters in s can be replaced to get t. All
+occurrences of a character must be replaced with another character while
+preserving the order of characters. No two characters may map to the same
+character, but a character may map to itself.
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+Explanation: The strings s and t can be made identical by:
+             Mapping 'e' to 'a'.
+             Mapping 'g' to 'd'.
+
+Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+Explanation: The strings s and t can not be made identical as 'o' needs to be
+             mapped to both 'a' and 'r'.
+
+Example 3:
+Input: s = "paper", t = "title"
+Output: true
+
+Constraints:
+* 1 <= s.length <= 5 * 104
+* t.length == s.length
+* s and t consist of any valid ascii character.
+=end
+
+def is_isomorphic(s, t)
+    ss, tt = {}, {}
+    s.chars.zip(t.chars).each_with_index do |(x, y), i|
+        return false if ss[x] != tt[y]
+        ss[x] = tt[y] = i
+    end
+    return true
+end
