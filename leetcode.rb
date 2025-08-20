bd7da75ec2db134eb33a9c70a35da2fce1ead00a
Ye Gao <gaoyescm@meta.com>
2025-08-20 19:11:58 -0400

    solve'em all (303)

--- leetcode.rb
+++ leetcode.rb
@@ -2336,3 +2336,42 @@ Constraints: 1 <= n <= 2^31 - 1
 def can_win_nim(n)
     return n % 4 != 0
 end
+
+
+=begin
+303. Range Sum Query - Immutable (Easy)
+Given an integer array nums, handle multiple queries of the following type:
+* Calculate the sum of the elements of nums between indices left and right
+  inclusive where left <= right.
+Implement the NumArray class:
+* NumArray(int[] nums) Initializes the object with the integer array nums.
+* int sumRange(int left, int right) Returns the sum of the elements of nums
+  between indices left and right inclusive (i.e. nums[left] + nums[left + 1] +
+  ... + nums[right]).
+
+Example 1:
+Input: ["NumArray", "sumRange", "sumRange", "sumRange"]
+       [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output: [null, 1, -1, -3]
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+
+Constraints:
+* 1 <= nums.length <= 10^4
+* -10^5 <= nums[i] <= 10^5
+* 0 <= left <= right < nums.length
+* At most 10^4 calls will be made to sumRange.
+=end
+
+class NumArray
+    def initialize(nums)
+        @prefix = [s=0] + nums.map{|x| s += x}
+    end
+
+    def sum_range(left, right)
+        return @prefix[right+1] - @prefix[left]
+    end
+end
