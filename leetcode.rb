f7e8fe2a73e64345123f7839887d796e3ac5f42f
Ye Gao <gaoyescm@meta.com>
2025-08-20 10:46:10 -0400

    solve'em all (228)

--- leetcode.rb
+++ leetcode.rb
@@ -1888,3 +1888,55 @@ def invert_tree(root)
     end
     return root
 end
+
+
+=begin
+228. Summary Ranges (Easy)
+You are given a sorted unique integer array nums. A range [a,b] is the set of
+all integers from a to b (inclusive). Return the smallest sorted list of ranges
+that cover all the numbers in the array exactly. That is, each element of nums
+is covered by exactly one of the ranges, and there is no integer x such that x
+is in one of the ranges but not in nums. Each range [a,b] in the list should be
+output as:
+* "a->b" if a != b
+* "a" if a == b
+
+Example 1:
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+             [0,2] --> "0->2"
+             [4,5] --> "4->5"
+             [7,7] --> "7"
+
+Example 2:
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+             [0,0] --> "0"
+             [2,4] --> "2->4"
+             [6,6] --> "6"
+             [8,9] --> "8->9"
+
+Constraints:
+* 0 <= nums.length <= 20
+* -2^31 <= nums[i] <= 2^31 - 1
+* All the values of nums are unique.
+* nums is sorted in ascending order.
+=end
+
+def summary_ranges(nums)
+    ans = []
+    lo = nil
+    nums.each_with_index do |x, i|
+        lo = x if i == 0 || nums[i-1]+1 < x
+        if i+1 == nums.size || x+1 < nums[i+1]
+            if lo < x
+                ans.push("#{lo}->#{x}")
+            else
+                ans.push("#{x}")
+            end
+        end
+    end
+    return ans
+end
