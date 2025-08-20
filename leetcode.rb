0966d4bb7456d0e6358344040832ef02740a9048
Ye Gao <gaoyescm@meta.com>
2025-08-19 22:04:08 -0400

    solve'em all (206)

--- leetcode.rb
+++ leetcode.rb
@@ -1646,3 +1646,37 @@ def is_isomorphic(s, t)
     end
     return true
 end
+
+
+=begin
+206. Reverse Linked List (Easy)
+Given the head of a singly linked list, reverse the list, and return the
+reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+* The number of nodes in the list is the range [0, 5000].
+* -5000 <= Node.val <= 5000
+
+Follow up: A linked list can be reversed either iteratively or recursively.
+           Could you implement both?
+=end
+
+def reverse_list(head)
+    prev, curr = nil, head
+    while curr
+        curr.next, curr, prev = prev, curr.next, curr
+    end
+    return prev
+end
