5eb22fa69544733580213e020674552627b672c9
Ye Gao <gaoyescm@meta.com>
2025-08-19 22:05:43 -0400

    solve'em all (217)

--- leetcode.rb
+++ leetcode.rb
@@ -1680,3 +1680,32 @@ def reverse_list(head)
     end
     return prev
 end
+
+
+=begin
+217. Contains Duplicate (Easy)
+Given an integer array nums, return true if any value appears at least twice in
+the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+Explanation: The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: All elements are distinct.
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* -10^9 <= nums[i] <= 10^9
+=end
+
+def contains_duplicate(nums)
+    return !!nums.uniq!
+end
