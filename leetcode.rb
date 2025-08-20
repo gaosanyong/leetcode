3d852f14bc5716374060d0d897b5b4e4dcc266c3
Ye Gao <gaoyescm@meta.com>
2025-08-19 22:08:53 -0400

    solve'em all (219)

--- leetcode.rb
+++ leetcode.rb
@@ -1709,3 +1709,37 @@ Constraints:
 def contains_duplicate(nums)
     return !!nums.uniq!
 end
+
+
+=begin
+219. Contains Duplicate II (Easy)
+Given an integer array nums and an integer k, return true if there are two
+distinct indices i and j in the array such that nums[i] == nums[j] and
+abs(i - j) <= k.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* -10^9 <= nums[i] <= 10^9
+* 0 <= k <= 10^5
+=end
+
+def contains_nearby_duplicate(nums, k)
+    loc = {}
+    nums.each_with_index do |x, i|
+        return true if loc.key?(x) && i-loc[x] <= k
+        loc[x] = i
+    end
+    return false
+end
