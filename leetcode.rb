57e078fa04e5f6713c5aa294407ea1db69004f18
Ye Gao <gaoyescm@meta.com>
2025-08-20 11:05:25 -0400

    solve'em all (257)

--- leetcode.rb
+++ leetcode.rb
@@ -2102,3 +2102,35 @@ def is_anagram(s, t)
     freq = t.chars.inject(freq) { |f, ch| f[ch] -= 1; f }
     freq.values.all?(&:zero?)
 end
+
+
+=begin
+257. Binary Tree Paths (Easy)
+Given the root of a binary tree, return all root-to-leaf paths in any order. A
+leaf is a node with no children.
+
+Example 1:
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+
+Example 2:
+Input: root = [1]
+Output: ["1"]
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 100].
+* -100 <= Node.val <= 100
+=end
+
+def binary_tree_paths(root)
+    ans, path = [], []
+    fn = lambda do |node|
+        path << node.val
+        ans.push << path.map(&:to_s).join("->") if !node.left && !node.right
+        fn.call(node.left) if node.left
+        fn.call(node.right) if node.right
+        path.pop
+    end
+    fn.call(root)
+    return ans
+end
