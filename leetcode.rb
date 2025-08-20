2a0c7e20e4f7d22fc6be8d016ce8ae9ec63e831b
Ye Gao <gaoyescm@meta.com>
2025-08-20 16:40:21 -0400

    solve'em all (283)

--- leetcode.rb
+++ leetcode.rb
@@ -2265,3 +2265,35 @@ Constraints: 1 <= bad <= n <= 2^31 - 1
 def first_bad_version(n)
     (1..n).bsearch(&method(:is_bad_version))
 end
+
+
+=begin
+283. Move Zeroes (Easy)
+Given an integer array nums, move all 0's to the end of it while maintaining
+the relative order of the non-zero elements. Note that you must do this
+in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+* 1 <= nums.length <= 10^4
+* -2^31 <= nums[i] <= 2^31 - 1
+
+Follow up: Could you minimize the total number of operations done?
+=end
+
+def move_zeroes(nums)
+    k = 0
+    nums.each_with_index do |x, i|
+        if x != 0
+            nums[k], nums[i] = nums[i], nums[k]
+            k += 1
+        end
+    end
+end
