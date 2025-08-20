2e487b227091a5e44d95d791a05182076c787fcf
Ye Gao <gaoyescm@meta.com>
2025-08-20 10:47:57 -0400

    solve'em all (231)

--- leetcode.rb
+++ leetcode.rb
@@ -1940,3 +1940,33 @@ def summary_ranges(nums)
     end
     return ans
 end
+
+
+=begin
+231. Power of Two (Easy)
+Given an integer n, return true if it is a power of two. Otherwise, return
+false. An integer n is a power of two, if there exists an integer x such that
+n == 2x.
+
+Example 1:
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+
+Example 2:
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+
+Example 3:
+Input: n = 3
+Output: false
+
+Constraints: -2^31 <= n <= 2^31 - 1
+
+Follow up: Could you solve it without loops/recursion?
+=end
+
+def is_power_of_two(n)
+    return n > 0 && n&(n-1) == 0
+end
