674c91a9a4b83306126e008360a160f0c7eaf711
Ye Gao <gaoyescm@meta.com>
2025-08-19 22:38:10 -0400

    solve'em all (222)

--- leetcode.rb
+++ leetcode.rb
@@ -1743,3 +1743,53 @@ def contains_nearby_duplicate(nums, k)
     end
     return false
 end
+
+
+=begin
+222. Count Complete Tree Nodes (Easy)
+Given the root of a complete binary tree, return the number of the nodes in the
+tree. According to Wikipedia, every level, except possibly the last, is
+completely filled in a complete binary tree, and all nodes in the last level
+are as far left as possible. It can have between 1 and 2h nodes inclusive at
+the last level h. Design an algorithm that runs in less than O(n) time
+complexity.
+
+Example 1:
+Input: root = [1,2,3,4,5,6]
+Output: 6
+
+Example 2:
+Input: root = []
+Output: 0
+
+Example 3:
+Input: root = [1]
+Output: 1
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 5 * 10^4].
+* 0 <= Node.val <= 5 * 10^4
+* The tree is guaranteed to be complete.
+=end
+
+def height(node)
+    ans = 0
+    while node
+        node = node.left
+        ans += 1
+    end
+    return ans
+end
+
+def count_nodes(root)
+    ans = 0
+    if root
+        hl, hr = height(root.left), height(root.right)
+        if hl == hr
+            ans += 2**hl + count_nodes(root.right)
+        else
+            ans += count_nodes(root.left) + 2**hr
+        end
+    end
+    return ans
+end
