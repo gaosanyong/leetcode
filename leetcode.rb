a7803ee07420e9f429b66c0a34b3a9b098757df9
Ye Gao <gaoyescm@meta.com>
2025-08-20 11:08:35 -0400

    solve'em all (258)

--- leetcode.rb
+++ leetcode.rb
@@ -2134,3 +2134,30 @@ def binary_tree_paths(root)
     fn.call(root)
     return ans
 end
+
+
+=begin
+258. Add Digits (Easy)
+Given an integer num, repeatedly add all its digits until the result has only
+one digit, and return it.
+
+Example 1:
+Input: num = 38
+Output: 2
+Explanation: The process is
+             38 --> 3 + 8 --> 11
+             11 --> 1 + 1 --> 2
+             Since 2 has only one digit, return it.
+
+Example 2:
+Input: num = 0
+Output: 0
+
+Constraints: 0 <= num <= 2^31 - 1
+
+Follow up: Could you do it without any loop/recursion in O(1) runtime?
+=end
+
+def add_digits(num)
+    return num == 0 ? 0 : (num-1)%9 + 1
+end
