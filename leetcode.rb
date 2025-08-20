aa1cf1198e723af06090a5069620e20d689e2c64
Ye Gao <gaoyescm@meta.com>
2025-08-20 10:52:11 -0400

    solve'em all (234)

--- leetcode.rb
+++ leetcode.rb
@@ -2036,3 +2036,41 @@ class MyQueue
     end
 
 end
+
+
+=begin
+234. Palindrome Linked List (Easy)
+Given the head of a singly linked list, return true if it is a palindrome or
+false otherwise.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false
+
+Constraints:
+* The number of nodes in the list is in the range [1, 105].
+* 0 <= Node.val <= 9
+
+Follow up: Could you do it in O(n) time and O(1) space?
+=end
+
+def is_palindrome(head)
+    fast = slow = head
+    while fast && fast.next
+        fast = fast.next.next
+        slow = slow.next
+    end
+    prev = nil
+    while slow
+        slow.next, slow, prev = prev, slow.next, slow
+    end
+    while prev && head.val == prev.val
+        head = head.next
+        prev = prev.next
+    end
+    return !prev
+end
