ee39f4db1e93f4c8c8c461f49fa755426f0d0f26
Ye Gao <gaoyescm@meta.com>
2025-08-20 19:28:08 -0400

    solve'em all (338)

--- leetcode.rb
+++ leetcode.rb
@@ -2408,3 +2408,40 @@ def is_power_of_three(n)
     p = (Math.log(n)/Math.log(3)).round
     return 3**p == n
 end
+
+
+=begin
+338. Counting Bits (Easy)
+Given an integer n, return an array ans of length n + 1 such that for each i (0
+<= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+Example 1:
+Input: n = 2
+Output: [0,1,1]
+Explanation: 0 --> 0
+             1 --> 1
+             2 --> 10
+
+Example 2:
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation: 0 --> 0
+             1 --> 1
+             2 --> 10
+             3 --> 11
+             4 --> 100
+             5 --> 101
+
+Constraints: 0 <= n <= 10^5
+
+Follow up:
+* It is very easy to come up with a solution with a runtime of O(n log n). Can
+  you do it in linear time O(n) and possibly in a single pass?
+* Can you do it without using any built-in function (i.e., like
+  __builtin_popcount in C++)?
+=end
+
+def count_bits(n)
+    ans = (0..n).to_a
+    return ans.map! {|x| x%2 + ans[x>>1]}
+end
