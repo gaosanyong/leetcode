8604039ae48e45785b5970dec269942cb54f8cbf
Ye Gao <gaoyescm@meta.com>
2025-08-20 19:26:27 -0400

    solve'em all (326)

--- leetcode.rb
+++ leetcode.rb
@@ -2375,3 +2375,36 @@ class NumArray
         return @prefix[right+1] - @prefix[left]
     end
 end
+
+
+=begin
+326. Power of Three (Easy)
+Given an integer n, return true if it is a power of three. Otherwise, return
+false. An integer n is a power of three, if there exists an integer x such that
+n == 3x.
+
+Example 1:
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+
+Example 2:
+Input: n = 0
+Output: false
+Explanation: There is no x where 3x = 0.
+
+Example 3:
+Input: n = -1
+Output: false
+Explanation: There is no x where 3x = (-1).
+
+Constraints: -2^31 <= n <= 2^31 - 1
+
+Follow up: Could you solve it without loops/recursion?
+=end
+
+def is_power_of_three(n)
+    return false if n <= 0
+    p = (Math.log(n)/Math.log(3)).round
+    return 3**p == n
+end
