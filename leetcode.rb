529f30b96e084d02badea8efb7d78965556171fa
Ye Gao <gaoyescm@meta.com>
2025-08-20 10:42:54 -0400

    solve'em all (226)

--- leetcode.rb
+++ leetcode.rb
@@ -1860,3 +1860,31 @@ class MyStack
 end
 
 
+=begin
+226. Invert Binary Tree (Easy)
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+Input: root = []
+Output: []
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 100].
+* -100 <= Node.val <= 100
+=end
+
+
+def invert_tree(root)
+    if root
+        root.left, root.right = invert_tree(root.right), invert_tree(root.left)
+    end
+    return root
+end
