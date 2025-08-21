33720b34b8bfd56771fb6692931feea1056d3ba2
Ye Gao <gaoyescm@meta.com>
2025-08-20 22:26:22 -0400

    solve'em all (344)

--- leetcode.rb
+++ leetcode.rb
@@ -2473,3 +2473,27 @@ Follow up: Could you solve it without loops/recursion?
 def is_power_of_four(n)
     return n > 0 && n&(n-1) == 0 && n.bit_length%2 == 1
 end
+
+
+=begin
+344. Reverse String (Easy)
+Write a function that reverses a string. The input string is given as an array
+of characters s. You must do this by modifying the input array in-place with
+O(1) extra memory.
+
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+Constraints:
+* 1 <= s.length <= 10^5
+* s[i] is a printable ascii character.
+=end
+
+def reverse_string(s)
+    s.reverse!
+end
