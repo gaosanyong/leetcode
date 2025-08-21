74b5a6e01a468127c0e6d229969ed12a2d05b256
Ye Gao <gaoyescm@meta.com>
2025-08-20 22:57:54 -0400

    solve'em all (387)

--- leetcode.rb
+++ leetcode.rb
@@ -2684,3 +2684,36 @@ def can_construct(ransom_note, magazine)
     end
     return true
 end
+
+
+=begin
+387. First Unique Character in a String (Easy)
+Given a string s, find the first non-repeating character in it and return its
+index. If it does not exist, return -1.
+
+Example 1:
+Input: s = "leetcode"
+Output: 0
+Explanation: The character 'l' at index 0 is the first character that does not
+             occur at any other index.
+
+Example 2:
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+Input: s = "aabb"
+Output: -1
+
+Constraints:
+* 1 <= s.length <= 10^5
+* s consists of only lowercase English letters.
+=end
+
+def first_uniq_char(s)
+    freq = s.chars.tally
+    s.chars.each_with_index do |ch, i|
+        return i if freq[ch] == 1
+    end
+    return -1
+end
