7dbde4e358c4d703884ed524d6394bc48d694a4e
Ye Gao <gaoyescm@meta.com>
2025-08-20 22:27:34 -0400

    solve'em all (345)

--- leetcode.rb
+++ leetcode.rb
@@ -2497,3 +2497,30 @@ Constraints:
 def reverse_string(s)
     s.reverse!
 end
+
+
+=begin
+345. Reverse Vowels of a String (Easy)
+Given a string s, reverse only all the vowels in the string and return it. The
+vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and
+upper cases, more than once.
+
+Example 1:
+Input: s = "IceCreAm"
+Output: "AceCreIm"
+Explanation: The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels,
+             s becomes "AceCreIm".
+
+Example 2:
+Input: s = "leetcode"
+Output: "leotcede"
+
+Constraints:
+* 1 <= s.length <= 3 * 10^5
+* s consist of printable ASCII characters.
+=end
+
+def reverse_vowels(s)
+    vowels = s.scan(/[aeiou]/i)
+    s.gsub(/[aeiou]/i) { vowels.pop }
+end
