53c5f6b90b880842f1769166f7af5c72edd2fb3f
Ye Gao <gaoyescm@meta.com>
2025-08-20 22:30:04 -0400

    solve'em all (350)

--- leetcode.rb
+++ leetcode.rb
@@ -2549,3 +2549,44 @@ Constraints:
 def intersection(nums1, nums2)
     return nums1 & nums2
 end
+
+
+=begin
+350. Intersection of Two Arrays II (Easy)
+Given two integer arrays nums1 and nums2, return an array of their
+intersection. Each element in the result must appear as many times as it shows
+in both arrays and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+
+Constraints:
+* 1 <= nums1.length, nums2.length <= 1000
+* 0 <= nums1[i], nums2[i] <= 1000
+
+Follow up:
+* What if the given array is already sorted? How would you optimize your
+  algorithm?
+* What if nums1's size is small compared to nums2's size? Which algorithm is
+  better?
+* What if elements of nums2 are stored on disk, and the memory is limited such
+  that you cannot load all elements into the memory at once?
+=end
+
+def intersect(nums1, nums2)
+    freq = nums1.inject(Hash.new(0)) { |f, x| f[x] += 1; f }
+    ans = []
+    nums2.each do |x|
+        if freq[x] > 0
+            ans << x
+            freq[x] -= 1
+        end
+    end
+    return ans
+end
