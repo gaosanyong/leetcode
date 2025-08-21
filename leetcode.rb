8d05c8877eb53d60214e4943e528f9607713d444
Ye Gao <gaoyescm@meta.com>
2025-08-20 22:25:08 -0400

    solve'em all (342)

--- leetcode.rb
+++ leetcode.rb
@@ -2445,3 +2445,31 @@ def count_bits(n)
     ans = (0..n).to_a
     return ans.map! {|x| x%2 + ans[x>>1]}
 end
+
+
+=begin
+342. Power of Four (Easy)
+Given an integer n, return true if it is a power of four. Otherwise, return
+false. An integer n is a power of four, if there exists an integer x such that
+n == 4x.
+
+Example 1:
+Input: n = 16
+Output: true
+
+Example 2:
+Input: n = 5
+Output: false
+
+Example 3:
+Input: n = 1
+Output: true
+
+Constraints: -2^31 <= n <= 2^31 - 1
+
+Follow up: Could you solve it without loops/recursion?
+=end
+
+def is_power_of_four(n)
+    return n > 0 && n&(n-1) == 0 && n.bit_length%2 == 1
+end
