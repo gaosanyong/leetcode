4309733f7a819425ff9d6c8309c8aabdce77f2b2
Ye Gao <gaoyescm@meta.com>
2025-08-20 22:28:36 -0400

    solve'em all (349)

--- leetcode.rb
+++ leetcode.rb
@@ -2524,3 +2524,28 @@ def reverse_vowels(s)
     vowels = s.scan(/[aeiou]/i)
     s.gsub(/[aeiou]/i) { vowels.pop }
 end
+
+
+=begin
+349. Intersection of Two Arrays (Easy)
+Given two integer arrays nums1 and nums2, return an array of their
+intersection. Each element in the result must be unique and you may return the
+result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+
+Constraints:
+* 1 <= nums1.length, nums2.length <= 1000
+* 0 <= nums1[i], nums2[i] <= 1000
+=end
+
+def intersection(nums1, nums2)
+    return nums1 & nums2
+end
