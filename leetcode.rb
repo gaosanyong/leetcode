d3439aa1c2c6b600c69356557e89be9c2535dd82
Ye Gao <gaoyescm@meta.com>
2025-08-20 23:02:16 -0400

    solve'em all (392)

--- leetcode.rb
+++ leetcode.rb
@@ -2747,3 +2747,38 @@ def find_the_difference(s, t)
         freq[ch] -= 1
     end
 end
+
+
+=begin
+392. Is Subsequence (Easy)
+Given two strings s and t, return true if s is a subsequence of t, or false
+otherwise. A subsequence of a string is a new string that is formed from the
+original string by deleting some (can be none) of the characters without
+disturbing the relative positions of the remaining characters. (i.e., "ace" is
+a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+Constraints:
+* 0 <= s.length <= 100
+* 0 <= t.length <= 10^4
+* s and t consist only of lowercase English letters.
+
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where
+           k >= 10^9, and you want to check one by one to see if t has its
+           subsequence. In this scenario, how would you change your code?
+=end
+
+def is_subsequence(s, t)
+    i = 0
+    t.each_char do |ch|
+        i += 1 if s[i] == ch
+    end
+    return i == s.size
+end
