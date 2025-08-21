5df51a7a4160498ea200bb861e9aae16e0e26ff0
Ye Gao <gaoyescm@meta.com>
2025-08-20 22:31:28 -0400

    solve'em all (367)

--- leetcode.rb
+++ leetcode.rb
@@ -2590,3 +2590,30 @@ def intersect(nums1, nums2)
     end
     return ans
 end
+
+
+=begin
+367. Valid Perfect Square (Easy)
+Given a positive integer num, return true if num is a perfect square or false
+otherwise. A perfect square is an integer that is the square of an integer. In
+other words, it is the product of some integer with itself. You must not use
+any built-in library function, such as sqrt.
+
+Example 1:
+Input: num = 16
+Output: true
+Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+
+Example 2:
+Input: num = 14
+Output: false
+Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an
+             integer.
+
+Constraints: 1 <= num <= 2^31 - 1
+=end
+
+def is_perfect_square(num)
+    x = Math.sqrt(num).to_i
+    return x*x == num
+end
