3ca77b220c2976c5eb99b24fda0dd65548a05276
Ye Gao <gaoyescm@meta.com>
2025-08-25 19:20:05 -0400

    solve'em all (404)

--- leetcode.rb
+++ leetcode.rb
@@ -2782,3 +2782,38 @@ def is_subsequence(s, t)
     end
     return i == s.size
 end
+
+
+=begin
+404. Sum of Left Leaves (Easy)
+Given the root of a binary tree, return the sum of all left leaves. A leaf is a
+node with no children. A left leaf is a leaf that is the left child of another
+node.
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15
+             respectively.
+
+Example 2:
+Input: root = [1]
+Output: 0
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 1000].
+* -1000 <= Node.val <= 1000
+=end
+
+def sum_of_left_leaves(root)
+    ans = 0
+    stack = [[root, 0]]
+    while stack.any?
+        node, d = stack.pop
+        ans += node.val if !node.left && !node.right && d == -1
+        stack.append([node.left, -1]) if node.left
+        stack.append([node.right, 1]) if node.right
+    end
+    return ans
+end
