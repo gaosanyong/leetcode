a8bb763a94b78874350d5cb6b54843c35ed77c98
Ye Gao <gaoyescm@meta.com>
2025-08-25 21:56:16 -0400

    solve'em all (412)

--- leetcode.rb
+++ leetcode.rb
@@ -2847,3 +2847,36 @@ def longest_palindrome(s)
         memo + (v & ~1) | v & 1
     end
 end
+
+
+=begin
+412. Fizz Buzz (Easy)
+Given an integer n, return a string array answer (1-indexed) where:
+* answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+* answer[i] == "Fizz" if i is divisible by 3.
+* answer[i] == "Buzz" if i is divisible by 5.
+* answer[i] == i (as a string) if none of the above conditions are true.
+
+Example 1:
+Input: n = 3
+Output: ["1","2","Fizz"]
+
+Example 2:
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+
+Example 3:
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+Constraints: 1 <= n <= 10^4
+=end
+
+def fizz_buzz(n)
+    return (1..n).map do |x|
+        next "FizzBuzz" if (x % 15).zero?
+        next "Fizz" if (x % 3).zero?
+        next "Buzz" if (x % 5).zero?
+        x.to_s
+    end
+end
