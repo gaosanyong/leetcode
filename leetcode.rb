1202db35f28aeb2e47a22a19adb888ccde697050
Ye Gao <gaoyescm@meta.com>
2025-08-26 19:40:14 -0400

    solve'em all (461)

--- leetcode.rb
+++ leetcode.rb
@@ -3040,3 +3040,32 @@ def find_content_children(g, s)
     end
     k
 end
+
+
+=begin
+461. Hamming Distance (Easy)
+The Hamming distance between two integers is the number of positions at which
+the corresponding bits are different. Given two integers x and y, return the
+Hamming distance between them.
+
+Example 1:
+Input: x = 1, y = 4
+Output: 2
+Explanation: 1   (0 0 0 1)
+             4   (0 1 0 0)
+                    ↑   ↑
+             The above arrows point to positions where the corresponding bits
+             are different.
+
+Example 2:
+Input: x = 3, y = 1
+Output: 1
+
+Constraints: 0 <= x, y <= 2^31 - 1
+
+Note: This question is the same as 2220: Minimum Bit Flips to Convert Number.
+=end
+
+def hamming_distance(x, y)
+    (x ^ y).to_s(2).count('1')
+end
