5fc4b4142f4bc322c9c36dc434b9cff4839aee94
Ye Gao <gaoyescm@meta.com>
2025-08-25 21:43:31 -0400

    solve'em all (409)

--- leetcode.rb
+++ leetcode.rb
@@ -2817,3 +2817,33 @@ def sum_of_left_leaves(root)
     end
     return ans
 end
+
+
+=begin
+409. Longest Palindrome (Easy)
+Given a string s which consists of lowercase or uppercase letters, return the
+length of the longest palindrome that can be built with those letters. Letters
+are case sensitive, for example, "Aa" is not considered a palindrome.
+
+Example 1:
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose
+             length is 7.
+
+Example 2:
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is
+             1.
+
+Constraints:
+* 1 <= s.length <= 2000
+* s consists of lowercase and/or uppercase English letters only.
+=end
+
+def longest_palindrome(s)
+    s.chars.tally.reduce(0) do |memo, (_, v)|
+        memo + (v & ~1) | v & 1
+    end
+end
