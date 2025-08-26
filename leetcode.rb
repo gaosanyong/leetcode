d1c40c5ecc794fb74e58f0b619ad2e871d49d474
Ye Gao <gaoyescm@meta.com>
2025-08-26 19:32:36 -0400

    solve'em all (448)

--- leetcode.rb
+++ leetcode.rb
@@ -2970,3 +2970,30 @@ Constraints: 1 <= n <= 2^31 - 1
 def arrange_coins(n)
     (Math.sqrt(8*n+1).to_i-1)/2
 end
+
+
+=begin
+448. Find All Numbers Disappeared in an Array (Easy)
+Given an array nums of n integers where nums[i] is in the range [1, n], return
+an array of all the integers in the range [1, n] that do not appear in nums.
+
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+
+Example 2:
+Input: nums = [1,1]
+Output: [2]
+
+Constraints:
+* n == nums.length
+* 1 <= n <= 10^5
+* 1 <= nums[i] <= n
+
+Follow up: Could you do it without extra space and in O(n) runtime? You may
+           assume the returned list does not count as extra space.
+=end
+
+def find_disappeared_numbers(nums)
+    (1..nums.size).to_a - nums
+end
