5e3a8a4ef2850920f8b6c1c109d82fed9e2de89b
Ye Gao <gaoyescm@meta.com>
2025-08-25 22:45:50 -0400

    solve'em all (434)

--- leetcode.rb
+++ leetcode.rb
@@ -2919,3 +2919,29 @@ Follow up: Can you find an O(n) solution?
 def third_max(nums)
     nums.uniq.sort[-3] || nums.max
 end
+
+
+=begin
+434. Number of Segments in a String (Easy)
+Given a string s, return the number of segments in the string. A segment is
+defined to be a contiguous sequence of non-space characters.
+
+Example 1:
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+
+Example 2:
+Input: s = "Hello"
+Output: 1
+
+Constraints:
+* 0 <= s.length <= 300
+* s consists of lowercase and uppercase English letters, digits, or one of the
+  following characters "!@#$%^&*()_+-=',.:".
+* The only space character in s is ' '.
+=end
+
+def count_segments(s)
+    s.split.size
+end
