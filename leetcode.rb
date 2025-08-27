230eaab9c1fa730e1d7db6c460f3b9abf63c012f
Ye Gao <gaoyescm@meta.com>
2025-08-26 23:08:48 -0400

    solve'em all (482)

--- leetcode.rb
+++ leetcode.rb
@@ -3148,3 +3148,46 @@ Note: This question is the same as 1009:
 def find_complement(num)
     (1<<num.bit_length)-1 ^ num
 end
+
+
+=begin
+482. License Key Formatting (Easy)
+You are given a license key represented as a string s that consists of only
+alphanumeric characters and dashes. The string is separated into n + 1 groups
+by n dashes. You are also given an integer k. We want to reformat the string s
+such that each group contains exactly k characters, except for the first group,
+which could be shorter than k but still must contain at least one character.
+Furthermore, there must be a dash inserted between two groups, and you should
+convert all lowercase letters to uppercase. Return the reformatted license key.
+
+Example 1:
+Input: s = "5F3Z-2e-9-w", k = 4
+Output: "5F3Z-2E9W"
+Explanation: The string s has been split into two parts, each part has 4
+             characters. Note that the two extra dashes are not needed and can
+             be removed.
+
+Example 2:
+Input: s = "2-5g-3-J", k = 2
+Output: "2-5G-3J"
+Explanation: The string s has been split into three parts, each part has 2
+             characters except the first part as it could be shorter as
+             mentioned above.
+
+Constraints:
+* 1 <= s.length <= 10^5
+* s consists of English letters, digits, and dashes '-'.
+* 1 <= k <= 10^4
+=end
+
+def license_key_formatting(s, k)
+    ans = []
+    cnt = 0
+    s.reverse.upcase.each_char do |ch|
+        next if ch == '-'
+        ans << '-' and cnt = 0 if cnt == k
+        ans << ch
+        cnt += 1
+    end
+    ans.join.reverse
+end
