56f73dd88bf8bf0d182261037dd4cce56cfbddb6
Ye Gao <gaoyescm@meta.com>
2025-08-26 22:24:55 -0400

    solve'em all (463)

--- leetcode.rb
+++ leetcode.rb
@@ -3069,3 +3069,51 @@ Note: This question is the same as 2220: Minimum Bit Flips to Convert Number.
 def hamming_distance(x, y)
     (x ^ y).to_s(2).count('1')
 end
+
+
+=begin
+463. Island Perimeter (Easy)
+You are given row x col grid representing a map where grid[i][j] = 1 represents
+land and grid[i][j] = 0 represents water. Grid cells are connected
+horizontally/vertically (not diagonally). The grid is completely surrounded by
+water, and there is exactly one island (i.e., one or more connected land
+cells). The island doesn't have "lakes", meaning the water inside isn't
+connected to the water around the island. One cell is a square with side length
+1. The grid is rectangular, width and height don't exceed 100. Determine the
+perimeter of the island.
+
+Example 1:
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+
+Example 2:
+Input: grid = [[1]]
+Output: 4
+
+Example 3:
+Input: grid = [[1,0]]
+Output: 4
+
+Constraints:
+* row == grid.length
+* col == grid[i].length
+* 1 <= row, col <= 100
+* grid[i][j] is 0 or 1.
+* There is exactly one island in grid.
+=end
+
+def island_perimeter(grid)
+    m, n = grid.size, grid[0].size
+    ans = 0
+    (0..m-1).each do |i|
+        (0..n-1).each do |j|
+            if grid[i][j] == 1
+                ans += 4
+                ans -= 2 if i > 0 && grid[i-1][j] == 1
+                ans -= 2 if j > 0 && grid[i][j-1] == 1
+            end
+        end
+    end
+    return ans
+end
