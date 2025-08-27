11d9b7c9fadf55d10d8ab32a5285107026ce163e
Ye Gao <gaoyescm@meta.com>
2025-08-26 22:31:31 -0400

    solve'em all (476)

--- leetcode.rb
+++ leetcode.rb
@@ -3117,3 +3117,34 @@ def island_perimeter(grid)
     end
     return ans
 end
+
+
+=begin
+476. Number Complement (Easy)
+The complement of an integer is the integer you get when you flip all the 0's
+to 1's and all the 1's to 0's in its binary representation.
+* For example, The integer 5 is "101" in binary and its complement is "010"
+  which is the integer 2.
+Given an integer num, return its complement.
+
+Example 1:
+Input: num = 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and
+             its complement is 010. So you need to output 2.
+
+Example 2:
+Input: num = 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and
+             its complement is 0. So you need to output 0.
+
+Constraints: 1 <= num < 2^31
+
+Note: This question is the same as 1009:
+      https://leetcode.com/problems/complement-of-base-10-integer/
+=end
+
+def find_complement(num)
+    (1<<num.bit_length)-1 ^ num
+end
