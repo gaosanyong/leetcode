4d1fb06fcc3371e140e678a7d08cf8e7c0aa1a63
Ye Gao <gaoyescm@meta.com>
2025-08-29 15:13:32 -0400

    solve'em all (500)

--- leetcode.rb
+++ leetcode.rb
@@ -3360,3 +3360,39 @@ def next_greater_element(nums1, nums2)
     end
     nums1.map {|x| hs[x]}
 end
+
+
+=begin
+500. Keyboard Row (Easy)
+Given an array of strings words, return the words that can be typed using
+letters of the alphabet on only one row of American keyboard like the image
+below. Note that the strings are case-insensitive, both lowercased and
+uppercased of the same letter are treated as if they are at the same row. In
+the American keyboard:
+* the first row consists of the characters "qwertyuiop",
+* the second row consists of the characters "asdfghjkl", and
+* the third row consists of the characters "zxcvbnm".
+
+Example 1:
+Input: words = ["Hello","Alaska","Dad","Peace"]
+Output: ["Alaska","Dad"]
+Explanation: Both "a" and "A" are in the 2nd row of the American keyboard due
+             to case insensitivity.
+
+Example 2:
+Input: words = ["omk"]
+Output: []
+
+Example 3:
+Input: words = ["adsdf","sfd"]
+Output: ["adsdf","sfd"]
+
+Constraints:
+* 1 <= words.length <= 20
+* 1 <= words[i].length <= 100
+* words[i] consists of English letters (both lowercase and uppercase).
+=end
+
+def find_words(words)
+    words.grep(/^([qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*)$/i)
+end
