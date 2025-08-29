a1c841abed264273bc0c07675b98b7dab4911be4
Ye Gao <gaoyescm@meta.com>
2025-08-29 10:47:34 -0400

    solve'em all (485)

--- leetcode.rb
+++ leetcode.rb
@@ -3191,3 +3191,33 @@ def license_key_formatting(s, k)
     end
     ans.join.reverse
 end
+
+
+=begin
+485. Max Consecutive Ones (Easy)
+Given a binary array nums, return the maximum number of consecutive 1's in the
+array.
+
+Example 1:
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s.
+             The maximum number of consecutive 1s is 3.
+
+Example 2:
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+
+Constraints:
+* 1 <= nums.length <= 10^5
+* nums[i] is either 0 or 1.
+=end
+
+def find_max_consecutive_ones(nums)
+    ans = cnt = 0
+    nums.each do |x|
+        cnt = x == 0 ? 0 : cnt + 1
+        ans = cnt if cnt > ans
+    end
+    return ans
+end
