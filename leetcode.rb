a43fd6fbb3b6f71ea84dde4f5d195cb30338a2c2
Ye Gao <gaoyescm@meta.com>
2025-08-30 13:13:07 -0400

    solve'em all (501)

--- leetcode.rb
+++ leetcode.rb
@@ -3396,3 +3396,56 @@ Constraints:
 def find_words(words)
     words.grep(/^([qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*)$/i)
 end
+
+
+=begin
+501. Find Mode in Binary Search Tree (Easy)
+Given the root of a binary search tree (BST) with duplicates, return all the
+mode(s) (i.e., the most frequently occurred element) in it. If the tree has
+more than one mode, return them in any order. Assume a BST is defined as
+follows:
+* The left subtree of a node contains only nodes with keys less than or equal
+  to the node's key.
+* The right subtree of a node contains only nodes with keys greater than or
+  equal to the node's key.
+* Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+Input: root = [1,null,2,2]
+Output: [2]
+
+Example 2:
+Input: root = [0]
+Output: [0]
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 10^4].
+* -10^5 <= Node.val <= 10^5
+
+Follow up: Could you do that without using any extra space? (Assume that the
+           implicit stack space incurred due to recursion does not count).
+=end
+
+def find_mode(root)
+    ans = []
+    cnt = most = 0
+    val = nil
+    node, stack = root, []
+    while node || stack.any?
+        if node
+            stack << node
+            node = node.left
+        else
+            node = stack.pop
+            cnt, val = 0, node.val if val != node.val
+            cnt += 1
+            if cnt >= most
+                ans = [] if cnt > most
+                ans << val
+                most = cnt
+            end
+            node = node.right
+        end
+    end
+    ans
+end
