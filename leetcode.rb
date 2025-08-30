3cd581c94e4ff2299e281756054ebed9c85b8872
Ye Gao <gaoyescm@meta.com>
2025-08-30 13:15:36 -0400

    solve'em all (506)

--- leetcode.rb
+++ leetcode.rb
@@ -3469,3 +3469,42 @@ Constraints: -10^7 <= num <= 10^7
 def convert_to_base7(num)
     num.to_s(7)
 end
+
+
+=begin
+506. Relative Ranks (Easy)
+You are given an integer array score of size n, where score[i] is the score of
+the ith athlete in a competition. All the scores are guaranteed to be unique.
+The athletes are placed based on their scores, where the 1st place athlete has
+the highest score, the 2nd place athlete has the 2nd highest score, and so on.
+The placement of each athlete determines their rank:
+* The 1st place athlete's rank is "Gold Medal".
+* The 2nd place athlete's rank is "Silver Medal".
+* The 3rd place athlete's rank is "Bronze Medal".
+* For the 4th place to the nth place athlete, their rank is their placement
+  number (i.e., the xth place athlete's rank is "x").
+Return an array answer of size n where answer[i] is the rank of the ith
+athlete.
+
+Example 1:
+Input: score = [5,4,3,2,1]
+Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+
+Example 2:
+Input: score = [10,3,8,9,4]
+Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+
+Constraints:
+* n == score.length
+* 1 <= n <= 10^4
+* 0 <= score[i] <= 10^6
+* All the values in score are unique.
+=end
+
+def find_relative_ranks(score)
+    hs = Hash[score.sort.reverse.zip(0...score.size)]
+    medal = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+    score.map{|x| (v = hs[x]) < 3 ? medal[v] : (v+1).to_s}
+end
