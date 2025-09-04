b3a2f38fc4e4621a5cf4bea4ffd2429fd9b54919
Ye Gao <gaoyescm@meta.com>
2025-09-04 09:34:37 -0400

    solve'em all (557)

--- leetcode.rb
+++ leetcode.rb
@@ -3777,3 +3777,29 @@ Constraints:
 def check_record(s)
     s.count('A') < 2 && !s.include?('LLL')
 end
+
+
+=begin
+557. Reverse Words in a String III (Easy)
+Given a string s, reverse the order of characters in each word within a
+sentence while still preserving whitespace and initial word order.
+
+Example 1:
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+Example 2:
+Input: s = "Mr Ding"
+Output: "rM gniD"
+
+Constraints:
+* 1 <= s.length <= 5 * 10^4
+* s contains printable ASCII characters.
+* s does not contain any leading or trailing spaces.
+* There is at least one word in s.
+* All the words in s are separated by a single space.
+=end
+
+def reverse_words(s)
+    s.split.map(&:reverse).join(' ')
+end
