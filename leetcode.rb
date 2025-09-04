e097083a211cb4f477f37f132ff093a874614188
Ye Gao <gaoyescm@meta.com>
2025-09-04 09:47:16 -0400

    solve'em all (566)

--- leetcode.rb
+++ leetcode.rb
@@ -3922,3 +3922,44 @@ def find_tilt(root)
     fn.call(root)
     ans
 end
+
+
+=begin
+566. Reshape the Matrix (Easy)
+In MATLAB, there is a handy function called reshape which can reshape an m x n
+matrix into a new one with a different size r x c keeping its original data.
+You are given an m x n matrix mat and two integers r and c representing the
+number of rows and the number of columns of the wanted reshaped matrix. The
+reshaped matrix should be filled with all the elements of the original matrix
+in the same row-traversing order as they were. If the reshape operation with
+given parameters is possible and legal, output the new reshaped matrix;
+Otherwise, output the original matrix.
+
+Example 1:
+Input: mat = [[1,2],[3,4]], r = 1, c = 4
+Output: [[1,2,3,4]]
+
+Example 2:
+Input: mat = [[1,2],[3,4]], r = 2, c = 4
+Output: [[1,2],[3,4]]
+
+Constraints:
+* m == mat.length
+* n == mat[i].length
+* 1 <= m, n <= 100
+* -1000 <= mat[i][j] <= 1000
+* 1 <= r, c <= 300
+=end
+
+def matrix_reshape(mat, r, c)
+    m, n = mat.size, mat[0].size
+    return mat if m * n != r * c
+    ans = Array.new(r) {Array.new(c)}
+    (0...m).each do |i|
+        (0...n).each do |j|
+            x, y = (i*n + j).divmod(c)
+            ans[x][y] = mat[i][j]
+        end
+    end
+    ans
+end
