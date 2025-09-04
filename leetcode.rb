ea58ada2c39afd5e41b2190874de221b860b2aad
Ye Gao <gaoyescm@meta.com>
2025-09-04 09:25:14 -0400

    solve'em all (541)

--- leetcode.rb
+++ leetcode.rb
@@ -3677,3 +3677,32 @@ def get_minimum_difference(root)
     end
     ans
 end
+
+
+=begin
+541. Reverse String II (Easy)
+Given a string s and an integer k, reverse the first k characters for every 2k
+characters counting from the start of the string. If there are fewer than k
+characters left, reverse all of them. If there are less than 2k but greater
+than or equal to k characters, then reverse the first k characters and leave
+the other as original.
+
+Example 1:
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+
+Example 2:
+Input: s = "abcd", k = 2
+Output: "bacd"
+
+Constraints:
+* 1 <= s.length <= 10^4
+* s consists of only lowercase English letters.
+* 1 <= k <= 10^4
+=end
+
+def reverse_str(s, k)
+    s.chars.each_slice(k).map.with_index { |x, i|
+        i % 2 == 0 ? x.reverse : x
+    }.join
+end
