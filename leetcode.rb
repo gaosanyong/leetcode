82c59e0900c622954cb125e6d142f8b7101de41e
Ye Gao <gaoyescm@meta.com>
2025-09-04 09:12:36 -0400

    solve'em all (520)

--- leetcode.rb
+++ leetcode.rb
@@ -3573,3 +3573,30 @@ def fib(n)
     phi = (1 + Math.sqrt(5))/2
     (phi**n/Math.sqrt(5)).round
 end
+
+
+=begin
+520. Detect Capital (Easy)
+We define the usage of capitals in a word to be right when one of the following
+cases holds:
+* All letters in this word are capitals, like "USA".
+* All letters in this word are not capitals, like "leetcode".
+* Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+Example 1:
+Input: word = "USA"
+Output: true
+
+Example 2:
+Input: word = "FlaG"
+Output: false
+
+Constraints:
+* 1 <= word.length <= 100
+* word consists of lowercase and uppercase English letters.
+=end
+
+def detect_capital_use(word)
+    /^([A-Z]+|[A-Z]{0,1}[a-z]*)$/ === word
+end
