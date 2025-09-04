3d2b15cc5b429d562ebc70244e5af057f39cfae1
Ye Gao <gaoyescm@meta.com>
2025-09-04 09:30:51 -0400

    solve'em all (543)

--- leetcode.rb
+++ leetcode.rb
@@ -3706,3 +3706,37 @@ def reverse_str(s, k)
         i % 2 == 0 ? x.reverse : x
     }.join
 end
+
+
+=begin
+543. Diameter of Binary Tree (Easy)
+Given the root of a binary tree, return the length of the diameter of the tree.
+The diameter of a binary tree is the length of the longest path between any two
+nodes in a tree. This path may or may not pass through the root. The length of
+a path between two nodes is represented by the number of edges between them.
+
+Example 1:
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Example 2:
+Input: root = [1,2]
+Output: 1
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 10^4].
+* -100 <= Node.val <= 100
+=end
+
+def diameter_of_binary_tree(root)
+    ans = 0
+    fn = lambda do |node|
+        return 0 if !node
+        left, right = fn.call(node.left), fn.call(node.right)
+        ans = [ans, left + right].max
+        return 1 + [left, right].max
+    end
+    fn.call(root)
+    ans
+end
