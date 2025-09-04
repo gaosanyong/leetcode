cf4465183cd8758489f7e181e76e2dcf71ac2d3a
Ye Gao <gaoyescm@meta.com>
2025-09-04 09:42:45 -0400

    solve'em all (559)

--- leetcode.rb
+++ leetcode.rb
@@ -3803,3 +3803,40 @@ Constraints:
 def reverse_words(s)
     s.split.map(&:reverse).join(' ')
 end
+
+
+=begin
+559. Maximum Depth of N-ary Tree (Easy)
+Given a n-ary tree, find its maximum depth. The maximum depth is the number of
+nodes along the longest path from the root node down to the farthest leaf node.
+Nary-Tree input serialization is represented in their level order traversal,
+each group of children is separated by the null value (See examples).
+
+Example 1:
+Input: root = [1,null,3,2,4,null,5,6]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: 5
+
+Constraints:
+* The total number of nodes is in the range [0, 10^4].
+* The depth of the n-ary tree is less than or equal to 1000.
+=end
+
+def maxDepth(root)
+    ans = 0
+    queue = Queue.new
+    queue << root if root
+    while !queue.empty?
+        ans += 1
+        (1..queue.size).each do
+            node = queue.pop
+            node.children.each do |child|
+                queue << child
+            end
+        end
+    end
+    ans
+end
