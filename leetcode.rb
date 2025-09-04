632aa44ea19a1a0bfc71f224cf8389790eaee16a
Ye Gao <gaoyescm@meta.com>
2025-09-04 09:17:53 -0400

    solve'em all (530)

--- leetcode.rb
+++ leetcode.rb
@@ -3637,3 +3637,43 @@ def find_lu_slength(a, b)
     return [a.size, b.size].max if a.size != b.size
     return a == b ? -1 : a.size
 end
+
+
+=begin
+530. Minimum Absolute Difference in BST (Easy)
+Given the root of a Binary Search Tree (BST), return the minimum absolute
+difference between the values of any two different nodes in the tree.
+
+Example 1:
+Input: root = [4,2,6,1,3]
+Output: 1
+
+Example 2:
+Input: root = [1,0,48,null,null,12,49]
+Output: 1
+
+Constraints:
+* The number of nodes in the tree is in the range [2, 10^4].
+* 0 <= Node.val <= 10^5
+
+Note: This question is the same as 783:
+      https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+=end
+
+def get_minimum_difference(root)
+    ans = Float::INFINITY
+    prev = nil
+    node, stack = root, []
+    while node || stack.any?
+        if node
+            stack << node
+            node = node.left
+        else
+            node = stack.pop
+            ans = [ans, node.val - prev].min if prev
+            prev = node.val
+            node = node.right
+        end
+    end
+    ans
+end
