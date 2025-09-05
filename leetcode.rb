9aa30096e8dbd7b510f7e793dc0bc871b1f8bbbb
Ye Gao <gaoyescm@meta.com>
2025-09-05 11:16:45 -0400

    solve'em all (572)

--- leetcode.rb
+++ leetcode.rb
@@ -3963,3 +3963,50 @@ def matrix_reshape(mat, r, c)
     end
     ans
 end
+
+
+=begin
+572. Subtree of Another Tree (Easy)
+Given the roots of two binary trees root and subRoot, return true if there is a
+subtree of root with the same structure and node values of subRoot and false
+otherwise. A subtree of a binary tree tree is a tree that consists of a node in
+tree and all of this node's descendants. The tree tree could also be considered
+as a subtree of itself.
+
+Example 1:
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+
+Example 2:
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+
+Constraints:
+* The number of nodes in the root tree is in the range [1, 2000].
+* The number of nodes in the subRoot tree is in the range [1, 1000].
+* -10^4 <= root.val <= 10^4
+* -10^4 <= subRoot.val <= 10^4
+=end
+
+require 'digest'
+
+def is_subtree(root, sub_root)
+    dfs = lambda do |node|
+        return "" if !node
+        left, right = dfs.call(node.left), dfs.call(node.right)
+        Digest::SHA2.hexdigest(left + node.val.to_s + right)
+    end
+    target = dfs.call(sub_root)
+    found = false
+
+    dfs = lambda do |node|
+        return "" if !node
+        left, right = dfs.call(node.left), dfs.call(node.right)
+        cand = Digest::SHA2.hexdigest(left + node.val.to_s + right)
+        found = true if cand == target
+        cand
+    end
+
+    dfs.call(root)
+    return found
+end
