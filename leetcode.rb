d7beb7f7d634ab95c202174be0bb2808b17b2821
Ye Gao <gaoyescm@meta.com>
2025-09-06 19:09:53 -0400

    solve'em all (696)

--- leetcode.rb
+++ leetcode.rb
@@ -4702,3 +4702,35 @@ Constraints: 1 <= n <= 2^31 - 1
 def has_alternating_bits(n)
     (n ^= n>>1) & n+1 == 0
 end
+
+
+=begin
+696. Count Binary Substrings (Easy)
+Given a binary string s, return the number of non-empty substrings that have
+the same number of 0's and 1's, and all the 0's and all the 1's in these
+substrings are grouped consecutively. Substrings that occur multiple times are
+counted the number of times they occur.
+
+Example 1:
+Input: s = "00110011"
+Output: 6
+Explanation: There are 6 substrings that have equal number of consecutive 1's
+             and 0's: "0011", "01", "1100", "10", "0011", and "01". Notice that
+             some of these substrings repeat and are counted the number of
+             times they occur. Also, "00110011" is not a valid substring
+             because all the 0's (and 1's) are not grouped together.
+
+Example 2:
+Input: s = "10101"
+Output: 4
+Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal
+             number of consecutive 1's and 0's.
+
+Constraints:
+* 1 <= s.length <= 10^5
+* s[i] is either '0' or '1'.
+=end
+
+def count_binary_substrings(s)
+    s.scan(/0+|1+/).map(&:size).each_cons(2).map(&:min).sum
+end
