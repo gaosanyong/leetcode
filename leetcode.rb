8740b293e86c392f286488b7cacf15b97ddf6d76
Ye Gao <gaoyescm@meta.com>
2025-09-06 19:08:35 -0400

    solve'em all (693)

--- leetcode.rb
+++ leetcode.rb
@@ -4674,3 +4674,31 @@ Constraints:
 def valid_palindrome(s)
     s.each_char.tally.values.count {|x| x.odd?} <= 2
 end
+
+
+=begin
+693. Binary Number with Alternating Bits (Easy)
+Given a positive integer, check whether it has alternating bits: namely, if two
+adjacent bits will always have different values.
+
+Example 1:
+Input: n = 5
+Output: true
+Explanation: The binary representation of 5 is: 101
+
+Example 2:
+Input: n = 7
+Output: false
+Explanation: The binary representation of 7 is: 111.
+
+Example 3:
+Input: n = 11
+Output: false
+Explanation: The binary representation of 11 is: 1011.
+
+Constraints: 1 <= n <= 2^31 - 1
+=end
+
+def has_alternating_bits(n)
+    (n ^= n>>1) & n+1 == 0
+end
