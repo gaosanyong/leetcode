31df02819847c6738835899bf1bc3bbddc32c2f5
Ye Gao <gaoyescm@meta.com>
2025-09-06 13:54:20 -0400

    solve'em all (605)

--- leetcode.rb
+++ leetcode.rb
@@ -4246,3 +4246,37 @@ def find_restaurant(list1, list2)
     end
     ans
 end
+
+
+=begin
+605. Can Place Flowers (Easy)
+You have a long flowerbed in which some of the plots are planted, and some are
+not. However, flowers cannot be planted in adjacent plots. Given an integer
+array flowerbed containing 0's and 1's, where 0 means empty and 1 means not
+empty, and an integer n, return true if n new flowers can be planted in the
+flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+
+Example 2:
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+
+Constraints:
+* 1 <= flowerbed.length <= 2 * 10^4
+* flowerbed[i] is 0 or 1.
+* There are no two adjacent flowers in flowerbed.
+* 0 <= n <= flowerbed.length
+=end
+
+def can_place_flowers(flowerbed, n)
+    flowerbed.each_with_index do |x, i|
+        if x == 0 && (i == 0 || flowerbed[i-1] == 0) && (i+1 == flowerbed.size || flowerbed[i+1] == 0)
+            n -= 1
+            flowerbed[i] = 1
+        end
+    end
+    n <= 0
+end
