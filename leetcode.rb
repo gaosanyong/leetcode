54218f82ad2594c2d49b39c3c0dd8e94804b6fef
Ye Gao <gaoyescm@meta.com>
2025-09-06 14:00:47 -0400

    solve'em all (645)

--- leetcode.rb
+++ leetcode.rb
@@ -4420,3 +4420,38 @@ def find_max_average(nums, k)
     end
     most.to_f/k
 end
+
+
+=begin
+645. Set Mismatch (Easy)
+You have a set of integers s, which originally contains all the numbers from 1
+to n. Unfortunately, due to some error, one of the numbers in s got duplicated
+to another number in the set, which results in repetition of one number and
+loss of another number. You are given an integer array nums representing the
+data status of this set after the error. Find the number that occurs twice and
+the number that is missing and return them in the form of an array.
+
+Example 1:
+Input: nums = [1,2,2,4]
+Output: [2,3]
+
+Example 2:
+Input: nums = [1,1]
+Output: [1,2]
+
+Constraints:
+* 2 <= nums.length <= 10^4
+* 1 <= nums[i] <= 10^4
+=end
+
+def find_error_nums(nums)
+    seen = Array.new(nums.size, -1)
+    nums.each do |x|
+        seen[x-1] *= -1
+    end
+    ans = []
+    seen.each_with_index do |x, i|
+        ans << i+1 if x < 0
+    end
+    ans
+end
