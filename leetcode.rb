ed5637f954a13f8dc3cbc1dd596daa60d82bab01
Ye Gao <gaoyescm@meta.com>
2025-09-06 16:18:37 -0400

    soolve'em all (671)

--- leetcode.rb
+++ leetcode.rb
@@ -4577,3 +4577,41 @@ def image_smoother(img)
     end
     ans
 end
+
+
+=begin
+671. Second Minimum Node In a Binary Tree (Easy)
+Given a non-empty special binary tree consisting of nodes with the non-negative
+value, where each node in this tree has exactly two or zero sub-node. If the
+node has two sub-nodes, then this node's value is the smaller value among its
+two sub-nodes. More formally, the property root.val = min(root.left.val,
+root.right.val) always holds. Given such a binary tree, you need to output the
+second minimum value in the set made of all the nodes' value in the whole tree.
+If no such second minimum value exists, output -1 instead.
+
+Example 1:
+Input: root = [2,2,5,null,null,5,7]
+Output: 5
+Explanation: The smallest value is 2, the second smallest value is 5.
+
+Example 2:
+Input: root = [2,2,2]
+Output: -1
+Explanation: The smallest value is 2, but there isn't any second smallest
+             value.
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 25].
+* 1 <= Node.val <= 2^31 - 1
+* root.val == min(root.left.val, root.right.val) for each internal node of the
+  tree.
+=end
+
+def find_second_minimum_value(root)
+    return -1 if !root.left
+    left = root.val != root.left.val ? root.left.val : find_second_minimum_value(root.left)
+    right = root.val != root.right.val ? root.right.val : find_second_minimum_value(root.right)
+    return left if right == -1
+    return right if left == -1
+    [left, right].min
+end
