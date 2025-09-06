e6ebc5a3cc00b5d0cd26063f67ba1783c0667d8d
Ye Gao <gaoyescm@meta.com>
2025-09-06 16:20:43 -0400

    solve'em all (674)

--- leetcode.rb
+++ leetcode.rb
@@ -4615,3 +4615,34 @@ def find_second_minimum_value(root)
     return right if left == -1
     [left, right].min
 end
+
+
+=begin
+674. Longest Continuous Increasing Subsequence (Easy)
+Given an unsorted array of integers nums, return the length of the longest
+continuous increasing subsequence (i.e. subarray). The subsequence must be
+strictly increasing. A continuous increasing subsequence is defined by two
+indices l and r (l < r) such that it is [nums[l], nums[l + 1], ..., nums[r -
+1], nums[r]] and for each l <= i < r, nums[i] < nums[i + 1].
+
+Example 1:
+Input: nums = [1,3,5,4,7]
+Output: 3
+Explanation: The longest continuous increasing subsequence is [1,3,5] with
+             length 3. Even though [1,3,5,7] is an increasing subsequence, it
+             is not continuous as elements 5 and 7 are separated by element 4.
+
+Example 2:
+Input: nums = [2,2,2,2,2]
+Output: 1
+Explanation: The longest continuous increasing subsequence is [2] with length
+             1. Note that it must be strictly increasing.
+
+Constraints:
+* 1 <= nums.length <= 10^4
+* -10^9 <= nums[i] <= 10^9
+=end
+
+def find_length_of_lcis(nums)
+    nums.chunk_while(&:<).map(&:size).max
+end
