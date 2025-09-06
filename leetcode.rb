4065c854b981181f0b16911a981656d3bcd8bf53
Ye Gao <gaoyescm@meta.com>
2025-09-06 14:02:26 -0400

    solve'em all (653)

--- leetcode.rb
+++ leetcode.rb
@@ -4455,3 +4455,38 @@ def find_error_nums(nums)
     end
     ans
 end
+
+
+=begin
+653. Two Sum IV - Input is a BST (Easy)
+Given the root of a binary search tree and an integer k, return true if there
+exist two elements in the BST such that their sum is equal to k, or false
+otherwise.
+
+Example 1:
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+
+Example 2:
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 10^4].
+* -10^4 <= Node.val <= 10^4
+* root is guaranteed to be a valid binary search tree.
+* -10^5 <= k <= 10^5
+=end
+
+def find_target(root, k)
+    seen = Set.new
+    stack = [root]
+    while stack.any?
+        node = stack.pop
+        return true if seen.include?(k - node.val)
+        seen << node.val
+        stack << node.left if node.left
+        stack << node.right if node.right
+    end
+    false
+end
