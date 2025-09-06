affc6c6de78e96dde024fed96590e231a2c70043
Ye Gao <gaoyescm@meta.com>
2025-09-06 14:12:06 -0400

    soolve'em all (661)

--- leetcode.rb
+++ leetcode.rb
@@ -4526,3 +4526,54 @@ Constraints:
 def judge_circle(moves)
     moves.count('L') == moves.count('R') && moves.count('U') == moves.count('D')
 end
+
+
+=begin
+661. Image Smoother (Easy)
+An image smoother is a filter of the size 3 x 3 that can be applied to each
+cell of an image by rounding down the average of the cell and the eight
+surrounding cells (i.e., the average of the nine cells in the blue smoother).
+If one or more of the surrounding cells of a cell is not present, we do not
+consider it in the average (i.e., the average of the four cells in the red
+smoother). Given an m x n integer matrix img representing the grayscale of an
+image, return the image after applying the smoother on each cell of it.
+
+Example 1:
+Input: img = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[0,0,0],[0,0,0],[0,0,0]]
+Explanation: For the points (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
+             For the points (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
+             For the point (1,1): floor(8/9) = floor(0.88888889) = 0
+
+Example 2:
+Input: img = [[100,200,100],[200,50,200],[100,200,100]]
+Output: [[137,141,137],[141,138,141],[137,141,137]]
+Explanation: For the points (0,0), (0,2), (2,0), (2,2): floor((100+200+200+50)/4) = floor(137.5) = 137
+             For the points (0,1), (1,0), (1,2), (2,1): floor((200+200+50+200+100+100)/6) = floor(141.666667) = 141
+             For the point (1,1): floor((50+200+200+200+200+100+100+100+100)/9) = floor(138.888889) = 138
+
+Constraints:
+* m == img.length
+* n == img[i].length
+* 1 <= m, n <= 200
+* 0 <= img[i][j] <= 255
+=end
+
+def image_smoother(img)
+    m, n = img.size, img[0].size
+    ans = Array.new(m) {Array.new(n)}
+    (0...m).each do |i|
+        (0...n).each do |j|
+        ilo, ihi = [0, i-1].max, [m-1, i+1].min
+        jlo, jhi = [0, j-1].max, [n-1, j+1].min
+            total = 0
+            (ilo..ihi).each do |ii|
+                (jlo..jhi).each do |jj|
+                    total += img[ii][jj]
+                end
+            end
+            ans[i][j] = total/((ihi-ilo+1)*(jhi-jlo+1))
+        end
+    end
+    ans
+end
