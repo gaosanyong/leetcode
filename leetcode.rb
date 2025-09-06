d9728b3b3b624207e0765597488a8a030e53d42c
Ye Gao <gaoyescm@meta.com>
2025-09-06 13:59:46 -0400

    solve'em all (643)

--- leetcode.rb
+++ leetcode.rb
@@ -4384,3 +4384,39 @@ def average_of_levels(root)
     end
     ans
 end
+
+
+=begin
+643. Maximum Average Subarray I (Easy)
+You are given an integer array nums consisting of n elements, and an integer k.
+Find a contiguous subarray whose length is equal to k that has the maximum
+average value and return this value. Any answer with a calculation error less
+than 10-5 will be accepted.
+
+Example 1:
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+Example 2:
+Input: nums = [5], k = 1
+Output: 5.00000
+
+Constraints:
+* n == nums.length
+* 1 <= k <= n <= 10^5
+* -10^4 <= nums[i] <= 10^4
+=end
+
+def find_max_average(nums, k)
+    total = 0
+    most = -Float::INFINITY
+    nums.each_with_index do |x, i|
+        total += x
+        if i >= k-1
+            most = [most, total].max
+            total -= nums[i-k+1]
+        end
+    end
+    most.to_f/k
+end
