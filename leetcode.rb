c6aba09bc39e3f096b6eb73a1a9e7b9f0230013a
Ye Gao <gaoyescm@meta.com>
2025-09-06 13:45:54 -0400

    solve'em all (589)

--- leetcode.rb
+++ leetcode.rb
@@ -4050,3 +4050,38 @@ Constraints:
 def distribute_candies(candy_type)
     [candy_type.size/2, candy_type.uniq.size].min
 end
+
+
+=begin
+589. N-ary Tree Preorder Traversal (Easy)
+Given the root of an n-ary tree, return the preorder traversal of its nodes'
+values. Nary-Tree input serialization is represented in their level order
+traversal. Each group of children is separated by the null value (See examples)
+
+Example 1:
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [1,3,5,6,2,4]
+
+Example 2:
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 10^4].
+* 0 <= Node.val <= 10^4
+* The height of the n-ary tree is less than or equal to 1000.
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+=end
+
+def preorder(root)
+    ans = []
+    stack = []
+    stack << root if root
+    while stack.any?
+        node = stack.pop
+        ans << node.val
+        stack += node.children.reverse
+    end
+    ans
+end
