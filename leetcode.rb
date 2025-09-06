25775b142aac40b8ce8feeb20a3d8acbd6ad8253
Ye Gao <gaoyescm@meta.com>
2025-09-06 13:48:24 -0400

    solve'em all (590)

--- leetcode.rb
+++ leetcode.rb
@@ -4085,3 +4085,41 @@ def preorder(root)
     end
     ans
 end
+
+
+=begin
+590. N-ary Tree Postorder Traversal (Easy)
+Given the root of an n-ary tree, return the postorder traversal of its nodes'
+values. Nary-Tree input serialization is represented in their level order
+traversal. Each group of children is separated by the null value (See examples)
+
+Example 1:
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [5,6,3,2,4,1]
+
+Example 2:
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+
+Constraints:
+* The number of nodes in the tree is in the range [0, 10^4].
+* 0 <= Node.val <= 10^4
+* The height of the n-ary tree is less than or equal to 1000.
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+=end
+
+def postorder(root)
+    ans = []
+
+    dfs = lambda do |node|
+        return if !node
+        node.children.each do |child|
+            dfs.call(child)
+        end
+        ans << node.val
+    end
+
+    dfs.call(root)
+    ans
+end
