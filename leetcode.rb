472d97e327edb1daa079b756dbfd7b4c202f629f
Ye Gao <gaoyescm@meta.com>
2025-09-06 13:51:43 -0400

    solve'em all (598)

--- leetcode.rb
+++ leetcode.rb
@@ -4157,3 +4157,36 @@ def find_lhs(nums)
     freq = nums.tally
     freq.keys.map {|k| freq[k] + (freq[k+1] || 0)}.max
 end
+
+
+=begin
+598. Range Addition II (Easy)
+You are given an m x n matrix M initialized with all 0's and an array of
+operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented by
+one for all 0 <= x < ai and 0 <= y < bi. Count and return the number of maximum
+integers in the matrix after performing all the operations.
+
+Example 1:
+Input: m = 3, n = 3, ops = [[2,2],[3,3]]
+Output: 4
+Explanation: The maximum integer in M is 2, and there are four of it in M. So return 4.
+
+Example 2:
+Input: m = 3, n = 3, ops = [[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3]]
+Output: 4
+
+Example 3:
+Input: m = 3, n = 3, ops = []
+Output: 9
+
+Constraints:
+* 1 <= m, n <= 4 * 10^4
+* 0 <= ops.length <= 10^4
+* ops[i].length == 2
+* 1 <= ai <= m
+* 1 <= bi <= n
+=end
+
+def max_count(m, n, ops)
+    ops.inject([m, n]) {|(m, n), (x, y)| [[m, x].min, [n, y].min]}.inject(:*)
+end
