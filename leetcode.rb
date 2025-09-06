ceaf043473e7e2abe244502d1dc44bc529b9007e
Ye Gao <gaoyescm@meta.com>
2025-09-06 13:55:38 -0400

    solve'em all (617)

--- leetcode.rb
+++ leetcode.rb
@@ -4280,3 +4280,40 @@ def can_place_flowers(flowerbed, n)
     end
     n <= 0
 end
+
+
+=begin
+617. Merge Two Binary Trees (Easy)
+You are given two binary trees root1 and root2. Imagine that when you put one
+of them to cover the other, some nodes of the two trees are overlapped while
+the others are not. You need to merge the two trees into a new binary tree. The
+merge rule is that if two nodes overlap, then sum node values up as the new
+value of the merged node. Otherwise, the NOT null node will be used as the node
+of the new tree. Return the merged tree. Note: The merging process must start
+from the root nodes of both trees.
+
+Example 1:
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
+
+Example 2:
+Input: root1 = [1], root2 = [1,2]
+Output: [2,2]
+
+Constraints:
+ * The number of nodes in both trees is in the range [0, 2000].
+ * -10^4 <= Node.val <= 10^4
+=end
+
+def merge_trees(root1, root2)
+
+    fn = lambda do |node1, node2|
+        return node1 || node2 if !node1 || !node2
+        node = TreeNode.new(node1.val + node2.val)
+        node.left = fn.call(node1.left, node2.left)
+        node.right = fn.call(node1.right, node2.right)
+        node
+    end
+
+    fn.call(root1, root2)
+end
