b2f8b1eaaf448e435c1b82eddd923507229e58e5
Ye Gao <gaoyescm@meta.com>
2025-09-06 13:56:59 -0400

    solve'em all (628)

--- leetcode.rb
+++ leetcode.rb
@@ -4317,3 +4317,31 @@ def merge_trees(root1, root2)
 
     fn.call(root1, root2)
 end
+
+
+=begin
+628. Maximum Product of Three Numbers (Easy)
+Given an integer array nums, find three numbers whose product is maximum and
+return the maximum product.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: 6
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: 24
+
+Example 3:
+Input: nums = [-1,-2,-3]
+Output: -6
+
+Constraints:
+* 3 <= nums.length <= 10^4
+* -1000 <= nums[i] <= 1000
+=end
+
+def maximum_product(nums)
+    nums.sort!
+    [nums.last(3).reduce(:*), nums[0]*nums[1]*nums[-1]].max
+end
