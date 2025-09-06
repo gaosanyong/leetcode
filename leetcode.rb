01d073da6746c68c477ab230ab57135e32d206df
Ye Gao <gaoyescm@meta.com>
2025-09-06 13:49:57 -0400

    solve'em all (594)

--- leetcode.rb
+++ leetcode.rb
@@ -4123,3 +4123,37 @@ def postorder(root)
     dfs.call(root)
     ans
 end
+
+
+=begin
+594. Longest Harmonious Subsequence (Easy)
+We define a harmonious array as an array where the difference between its
+maximum value and its minimum value is exactly 1. Given an integer array nums,
+return the length of its longest harmonious subsequence among all its possible
+subsequences.
+
+Example 1:
+Input: nums = [1,3,2,2,5,2,3,7]
+Output: 5
+Explanation: The longest harmonious subsequence is [3,2,2,2,3].
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: 2
+Explanation: The longest harmonious subsequences are [1,2], [2,3], and [3,4],
+             all of which have a length of 2.
+
+Example 3:
+Input: nums = [1,1,1,1]
+Output: 0
+Explanation: No harmonic subsequence exists.
+
+Constraints:
+* 1 <= nums.length <= 2 * 10^4
+* -10^9 <= nums[i] <= 10^9
+=end
+
+def find_lhs(nums)
+    freq = nums.tally
+    freq.keys.map {|k| freq[k] + (freq[k+1] || 0)}.max
+end
