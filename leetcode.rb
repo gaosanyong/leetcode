7827e9983a379fe4aaeb06dba9a6f0b3957354e1
Ye Gao <gaoyescm@meta.com>
2025-09-06 19:12:22 -0400

    solve'em all (700)

--- leetcode.rb
+++ leetcode.rb
@@ -4774,3 +4774,31 @@ def find_shortest_sub_array(nums)
     end
     ans
 end
+
+
+=begin
+700. Search in a Binary Search Tree (Easy)
+You are given the root of a binary search tree (BST) and an integer val. Find
+the node in the BST that the node's value equals val and return the subtree
+rooted with that node. If such a node does not exist, return null.
+
+Example 1:
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+Example 2:
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+
+Constraints:
+* The number of nodes in the tree is in the range [1, 5000].
+* 1 <= Node.val <= 10^7
+* root is a binary search tree.
+* 1 <= val <= 10^7
+=end
+
+def search_bst(root, val)
+    return root if !root || root.val == val
+    return search_bst(root.right, val) if root.val < val
+    search_bst(root.left, val)
+end
