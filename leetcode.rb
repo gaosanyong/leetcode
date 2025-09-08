dbb5cd51b14a34703ff952474eb92754f228b3e8
Ye Gao <gaoyescm@meta.com>
2025-09-08 11:00:35 -0400

    solve'em all (705)

--- leetcode.rb
+++ leetcode.rb
@@ -4832,3 +4832,54 @@ def search(nums, target)
     k = nums.bsearch_index {|x| target <=> x}
     k || -1
 end
+
+
+=begin
+705. Design HashSet (Easy)
+Design a HashSet without using any built-in hash table libraries. Implement
+MyHashSet class:
+* void add(key) Inserts the value key into the HashSet.
+* bool contains(key) Returns whether the value key exists in the HashSet or
+  not.
+* void remove(key) Removes the value key in the HashSet. If key does not exist
+  in the HashSet, do nothing.
+
+Example 1:
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
+
+Explanation: MyHashSet myHashSet = new MyHashSet();
+             myHashSet.add(1);      // set = [1]
+             myHashSet.add(2);      // set = [1, 2]
+             myHashSet.contains(1); // return True
+             myHashSet.contains(3); // return False, (not found)
+             myHashSet.add(2);      // set = [1, 2]
+             myHashSet.contains(2); // return True
+             myHashSet.remove(2);   // set = [1]
+             myHashSet.contains(2); // return False, (already removed)
+
+Constraints:
+* 0 <= key <= 10^6
+* At most 10^4 calls will be made to add, remove, and contains.
+=end
+
+class MyHashSet
+    def initialize()
+        @data = Array.new(1_000_001)
+    end
+
+    def add(key)
+        @data[key] = 1
+    end
+
+    def remove(key)
+        @data[key] = nil
+    end
+
+    def contains(key)
+        !!@data[key]
+    end
+end
