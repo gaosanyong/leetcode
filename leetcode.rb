5f51777c8b8bec493cd160500ea8cb5101877357
Ye Gao <gaoyescm@meta.com>
2025-09-08 11:06:16 -0400

    solve'em all (709)

--- leetcode.rb
+++ leetcode.rb
@@ -4936,3 +4936,29 @@ class MyHashMap
     end
 end
 
+
+=begin
+709. To Lower Case (Easy)
+Given a string s, return the string after replacing every uppercase letter with
+the same lowercase letter.
+
+Example 1:
+Input: s = "Hello"
+Output: "hello"
+
+Example 2:
+Input: s = "here"
+Output: "here"
+
+Example 3:
+Input: s = "LOVELY"
+Output: "lovely"
+
+Constraints:
+* 1 <= s.length <= 100
+* s consists of printable ASCII characters.
+=end
+
+def to_lower_case(s)
+    s.downcase
+end
