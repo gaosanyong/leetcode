80b72ce1939aabaa66440be4e6ebc2755c648143
Ye Gao <gaoyescm@meta.com>
2025-09-08 10:51:08 -0400

    solve'em all (704)

--- leetcode.rb
+++ leetcode.rb
@@ -4802,3 +4802,33 @@ def search_bst(root, val)
     return search_bst(root.right, val) if root.val < val
     search_bst(root.left, val)
 end
+
+
+=begin
+704. Binary Search (Easy)
+Given an array of integers nums which is sorted in ascending order, and an
+integer target, write a function to search target in nums. If target exists,
+then return its index. Otherwise, return -1. You must write an algorithm with
+O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+Constraints:
+* 1 <= nums.length <= 10^4
+* -10^4 < nums[i], target < 10^4
+* All the integers in nums are unique.
+* nums is sorted in ascending order.
+=end
+
+def search(nums, target)
+    k = nums.bsearch_index {|x| target <=> x}
+    k || -1
+end
