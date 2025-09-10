6aee60acce9dfce813929bb3a35a28be808c0eba
Ye Gao <gaoyescm@meta.com>
2025-09-09 23:15:25 -0400

    solve'em all (744)

--- leetcode.rb
+++ leetcode.rb
@@ -5104,3 +5104,42 @@ def flood_fill(image, sr, sc, color)
     end
     image
 end
+
+
+=begin
+744. Find Smallest Letter Greater Than Target (Easy)
+You are given an array of characters letters that is sorted in non-decreasing
+order, and a character target. There are at least two different characters in
+letters. Return the smallest character in letters that is lexicographically
+greater than target. If such a character does not exist, return the first
+character in letters.
+
+Example 1:
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Explanation: The smallest character that is lexicographically greater than 'a'
+             in letters is 'c'.
+
+Example 2:
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Explanation: The smallest character that is lexicographically greater than 'c'
+in letters is 'f'.
+
+Example 3:
+Input: letters = ["x","x","y","y"], target = "z"
+Output: "x"
+Explanation: There are no characters in letters that is lexicographically
+             greater than 'z' so we return letters[0].
+
+Constraints:
+* 2 <= letters.length <= 10^4
+* letters[i] is a lowercase English letter.
+* letters is sorted in non-decreasing order.
+* letters contains at least two different characters.
+* target is a lowercase English letter.
+=end
+
+def next_greatest_letter(letters, target)
+    letters[letters.bsearch_index {|ch| target < ch} || 0]
+end
