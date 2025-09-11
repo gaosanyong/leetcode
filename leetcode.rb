fc0386322006ea2a7b6c346f420a94109811e830
Ye Gao <gaoyescm@meta.com>
2025-09-10 23:25:40 -0400

    solve'em all (747)

--- leetcode.rb
+++ leetcode.rb
@@ -5179,3 +5179,41 @@ Constraints:
 def min_cost_climbing_stairs(cost)
     cost.reverse.inject([0, 0]) {|(f1, f2), x| [f2, [f1, f2].min + x]}.min
 end
+
+
+=begin
+747. Largest Number At Least Twice of Others (Easy)
+You are given an integer array nums where the largest integer is unique.
+Determine whether the largest element in the array is at least twice as much as
+every other number in the array. If it is, return the index of the largest
+element, or return -1 otherwise.
+
+Example 1:
+Input: nums = [3,6,1,0]
+Output: 1
+Explanation: 6 is the largest integer. For every other number in the array x, 6
+             is at least twice as big as x. The index of value 6 is 1, so we
+             return 1.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: -1
+Explanation: 4 is less than twice the value of 3, so we return -1.
+
+Constraints:
+* 2 <= nums.length <= 50
+* 0 <= nums[i] <= 100
+* The largest element in nums is unique.
+=end
+
+def dominant_index(nums)
+    m1 = m2 = nil
+    nums.each_with_index do |x, i|
+        if !m1 || x >= nums[m1]
+            m1, m2 = i, m1
+        elsif !m2 || x > nums[m2]
+            m2 = i
+        end
+    end
+    nums[m1] >= 2*nums[m2] ? m1 : -1
+end
