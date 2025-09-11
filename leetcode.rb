86538c7e9000256183d7526a92bda4cbf1b77430
Ye Gao <gaoyescm@meta.com>
2025-09-11 19:37:34 -0400

    solve'em all (766)

--- leetcode.rb
+++ leetcode.rb
@@ -5305,3 +5305,45 @@ Constraints:
 def count_prime_set_bits(left, right)
     (left..right).sum {|x| 665772 >> x.digits(2).sum & 1}
 end
+
+
+=begin
+766. Toeplitz Matrix (Easy)
+Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return
+false. A matrix is Toeplitz if every diagonal from top-left to bottom-right has
+the same elements.
+
+Example 1:
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation: In the above grid, the diagonals are:
+             "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+             In each diagonal all elements are the same, so the answer is True.
+
+Example 2:
+Input: matrix = [[1,2],[2,2]]
+Output: false
+Explanation: The diagonal "[1, 2]" has different elements.
+
+Constraints:
+* m == matrix.length
+* n == matrix[i].length
+* 1 <= m, n <= 20
+* 0 <= matrix[i][j] <= 99
+
+Follow up:
+* What if the matrix is stored on disk, and the memory is limited such that you
+  can only load at most one row of the matrix into the memory at once?
+* What if the matrix is so large that you can only load up a partial row into
+  the memory at once?
+=end
+
+def is_toeplitz_matrix(matrix)
+    m, n = matrix.size, matrix[0].size
+    (1...m).each do |i|
+        (1...n).each do |j|
+            return false if i > 0 && j > 0 && matrix[i-1][j-1] != matrix[i][j]
+        end
+    end
+    true
+end
