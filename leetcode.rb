b5c60cf2b6f8050db9204de48163ba68354675dc
Ye Gao <gaoyescm@meta.com>
2025-09-12 12:05:11 -0400

    solve'em all (796)

--- leetcode.rb
+++ leetcode.rb
@@ -5414,3 +5414,28 @@ def min_diff_in_bst(root)
     end
     ans
 end
+
+
+=begin
+796. Rotate String (Easy)
+Given two strings s and goal, return true if and only if s can become goal
+after some number of shifts on s. A shift on s consists of moving the leftmost
+character of s to the rightmost position. For example, if s = "abcde", then it
+will be "bcdea" after one shift.
+
+Example 1:
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+Input: s = "abcde", goal = "abced"
+Output: false
+
+Constraints:
+* 1 <= s.length, goal.length <= 100
+* s and goal consist of lowercase English letters.
+=end
+
+def rotate_string(s, goal)
+    s.size == goal.size && (s+s).include?(goal)
+end
