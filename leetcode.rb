cb5a1b8d83fd281836dd2c02d604af907d42c9cd
Ye Gao <gaoyescm@meta.com>
2025-09-26 11:03:54 -0400

    solve'em all (812)

--- leetcode.rb
+++ leetcode.rb
@@ -5535,3 +5535,35 @@ def number_of_lines(widths, s)
     end
     [line, prefix]
 end
+
+
+=begin
+812. Largest Triangle Area (Easy)
+Given an array of points on the X-Y plane points where points[i] = [xi, yi],
+return the area of the largest triangle that can be formed by any three
+different points. Answers within 10-5 of the actual answer will be accepted.
+
+Example 1:
+Input: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+Output: 2.00000
+Explanation: The five points are shown in the above figure. The red triangle is
+             the largest.
+
+Example 2:
+Input: points = [[1,0],[0,0],[0,1]]
+Output: 0.50000
+
+Constraints:
+* 3 <= points.length <= 50
+* -50 <= xi, yi <= 50
+* All the given points are unique.
+=end
+
+def largest_triangle_area(points)
+    ans = 0
+    points.combination(3) do |(xi, yi), (xj, yj), (xk, yk)|
+        cand = 0.5*(xi*(yj-yk) + xj*(yk-yi) + xk*(yi-yj)).abs
+        ans = cand if cand > ans
+    end
+    ans
+end
