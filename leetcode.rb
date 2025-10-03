e906a44c8000a5c897cd1c1e1856460ca604cc5d
Ye Gao <gaoyescm@meta.com>
2025-10-02 22:49:38 -0400

    solve'em all (824)

--- leetcode.rb
+++ leetcode.rb
@@ -5658,3 +5658,49 @@ def shortest_to_char(s, c)
     end
     ans
 end
+
+
+=begin
+824. Goat Latin (Easy)
+You are given a string sentence that consist of words separated by spaces. Each
+word consists of lowercase and uppercase letters only. We would like to convert
+the sentence to "Goat Latin" (a made-up language similar to Pig Latin.) The
+rules of Goat Latin are as follows:
+* If a word begins with a vowel ('a', 'e', 'i', 'o', or 'u'), append "ma" to
+  the end of the word.
+  * For example, the word "apple" becomes "applema".
+* If a word begins with a consonant (i.e., not a vowel), remove the first
+  letter and append it to the end, then add "ma".
+  * For example, the word "goat" becomes "oatgma".
+* Add one letter 'a' to the end of each word per its word index in the
+  sentence, starting with 1.
+  * For example, the first word gets "a" added to the end, the second word gets
+    "aa" added to the end, and so on.
+Return the final sentence representing the conversion from sentence to Goat
+Latin.
+
+Example 1:
+Input: sentence = "I speak Goat Latin"
+Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+
+Example 2:
+Input: sentence = "The quick brown fox jumped over the lazy dog"
+Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa
+        hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+
+Constraints:
+* 1 <= sentence.length <= 150
+* sentence consists of English letters and spaces.
+* sentence has no leading or trailing spaces.
+* All the words in sentence are separated by a single space.
+=end
+
+def to_goat_latin(sentence)
+    ans = []
+    sentence.split.each_with_index do |w, i|
+        w = w.chars.rotate.join if !w.start_with?(/[aeiouAEIOU]/)
+        w << "ma" + "a"*(i+1)
+        ans.push(w)
+    end
+    ans.join(" ")
+end
