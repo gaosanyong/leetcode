780e66ff9907218c1bd1d1cc2b7bb744fa65e92d
Ye Gao <gaoyescm@meta.com>
2025-10-13 19:37:08 -0400

    solve'em all (830)

--- leetcode.rb
+++ leetcode.rb
@@ -5704,3 +5704,44 @@ def to_goat_latin(sentence)
     end
     ans.join(" ")
 end
+
+
+=begin
+830. Positions of Large Groups (Easy)
+In a string s of lowercase letters, these letters form consecutive groups
+of the same character. For example, a string like s = "abbxxxxzyy" has the
+groups "a", "bb", "xxxx", "z", and "yy". A group is identified by an
+interval [start, end], where start and end denote the start and end indices
+(inclusive) of the group. In the above example, "xxxx" has the interval
+[3,6]. A group is considered large if it has 3 or more characters. Return
+the intervals of every large group sorted in increasing order by start
+index.
+
+Example 1:
+Input: s = "abbxxxxzzy"
+Output: [[3,6]]
+Explanation: "xxxx" is the only large group with start index 3 and end
+             index 6.
+
+Example 2:
+Input: s = "abc"
+Output: []
+Explanation: We have groups "a", "b", and "c", none of which are large
+             groups.
+
+Example 3:
+Input: s = "abcdddeeeeaabbbcd"
+Output: [[3,5],[6,9],[12,14]]
+Explanation: The large groups are "ddd", "eeee", and "bbb".
+
+Constraints:
+* n1 <= s.length <= 1000
+* ns contains lowercase English letters only.
+=end
+
+def large_group_positions(s)
+    s.each_char.with_index
+    .chunk_while{_1[0] == _2[0]}
+    .select{_1.size > 2}
+    .map{[_1.first[1], _1.last[1]]}
+end
