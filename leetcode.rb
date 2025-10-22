0a8b791b8950486296cee79eed7e858a3a66d96f
Ye Gao <gaoyescm@meta.com>
2025-10-22 19:16:42 -0400

    solve'em all (867)

--- leetcode.rb
+++ leetcode.rb
@@ -5944,3 +5944,30 @@ def lemonade_change(bills)
     end
     true
 end
+
+
+=begin
+867. Transpose Matrix (Easy)
+Given a 2D integer array matrix, return the transpose of matrix. The transpose
+of a matrix is the matrix flipped over its main diagonal, switching the
+matrix's row and column indices.
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+Example 2:
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+
+Constraints:
+* m == matrix.length
+* n == matrix[i].length
+* 1 <= m, n <= 1000
+* 1 <= m * n <= 10^5
+* -10^9 <= matrix[i][j] <= 10^9
+=end
+
+def transpose(matrix)
+    matrix.transpose
+end
